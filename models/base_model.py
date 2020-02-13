19011cc8bfb479ccbc55facc32452261d79b4c12
eNobreg <ezra.nobrega@outlook.com>
2020-02-13 14:52:15 -0500

    Update BaseModel class to receive Kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -8,10 +8,15 @@ from datetime import datetime
 
 class BaseModel:
     """A base class for all hbnb models"""
-    def __init__(self, id=None, created_at=None, updated_at=None):
+    def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+        else:
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            self.__dict__.update(kwargs) 
         self.save()
 
     def __str__(self):
