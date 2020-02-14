bdf872eededb6cb63bfb645a631fa3aacd1bd17e
justinmajetich <justinmajetich@gmail.com>
2020-02-14 20:39:12 +0000

    Dynamically assign class name in __str__

--- models/base_model.py
+++ models/base_model.py
@@ -25,8 +25,8 @@ class BaseModel:
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        return '[{}] ({}) {}'.format(BaseModel.__name__,
-                                     self.id, self.__dict__)
+        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
