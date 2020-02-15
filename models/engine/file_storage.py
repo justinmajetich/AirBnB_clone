d4938b5cec8addf5f8ece10e2db53f3413d07986
justinmajetich <justinmajetich@gmail.com>
2020-02-15 18:54:45 +0000

    Fix improper line-break

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -36,7 +36,7 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        FileStorage.__objects[key] =
-                        classes[val['__class__']](**val)
+                        FileStorage.__objects[key] =\
+                         classes[val['__class__']](**val)
         except IOError:
             pass
