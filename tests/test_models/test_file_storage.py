ca77a6b35305ca1f68fdd4dec42fa12ab2a48d45
eNobreg <ezra.nobrega@outlook.com>
2020-02-18 16:08:34 -0500

    New test file for file_storage

---
+++ tests/test_models/test_file_storage.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Module for testing file storage"""
+import unittest
+
+
+class test_fileStorage(unittest.TestCase):
+    """ Class to test the file storage method """
+    pass
