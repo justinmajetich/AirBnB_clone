e6f3ac1a3f0d5d3a58d64a7dbcb26c8a3a49dc7b
eNobreg <ezra.nobrega@outlook.com>
2020-02-18 20:14:51 -0500

    Added two tests for file storage to test checks

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -9,5 +9,20 @@ class test_fileStorage(unittest.TestCase):
     """ Class to test the file storage method """
 
     def test_save(self):
-        """ docsting """
-        pass
+        """ Testing if the file is created """
+        new = BaseModel()
+        self.assertFalse(os.path.exists('file.json'))
+
+    def tearDown(self):
+        try:
+            os.remove('file.json')
+        except:
+            pass
+
+    def test_empty(self):
+        """ Testing empty file """
+        new = BaseModel()
+        thing = new.to_dict()
+        new.save()
+        new2 = BaseModel(**thing)
+        self.assertEqual(os.path.getsize('file.json'), 10238)
