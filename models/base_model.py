9478bfc228edf81b17071bf59ad0bd5f18f6c785
AndrewB4y <andresbaymon@gmail.com>
2020-08-10 23:57:31 +0000

    Adding db features on files: console - models/__init__ - base_model - city - db_storage - state

--- models/base_model.py
+++ models/base_model.py
@@ -2,14 +2,29 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer, DateTime
+import datetime
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime,
+                        nullable=False,
+                        default=datetime.datetime.utcnow())
+    updated_at = Column(DateTime,
+                        nullable=False,
+                        default=datetime.datetime.utcnow())
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
+        from datetime import datetime
+        from models import storage
+
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
@@ -31,14 +46,22 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
+        if '_sa_instance_state' in dictionary:
+            del(dictionary['_sa_instance_state'])
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
+
+    def delete(self):
+        """ Deletes the current instance from the storage """
+        from models import storage
+        storage.delete(self)
