b106b96a1c55d96fdea8c934140b24179cc7e0c9
Norman <isaza00@hotmail.com>
2020-08-10 10:09:50 -0500

    5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,7 +11,7 @@ class FileStorage:
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         dic = {}
-        if cls == None:
+        if cls is None:
                 return FileStorage.__objects
         for key, value in FileStorage.__objects.items():
             lista = key.split(".")
