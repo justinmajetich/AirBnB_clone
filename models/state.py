f7b3c649974cb8390e11a0be1d81415c2744f647
JoseR98 <josealonsoingeniero@hotmail.com>
2020-08-10 22:42:41 +0000

    Update sqlalchemy class attributes, stablish relationship with City class

--- models/state.py
+++ models/state.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from file_storage import Filestorage
+from city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
+
+    def cities(self):
+        """Returns the list of City instances for current state"""
+        state_cities = []
+        cities_all = Filestorage.all(City)
+        for city in cities_all.values():
+            if city.state_id == self.id:
+                state_cities.append(city)
+        return state_cities
