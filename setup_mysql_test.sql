7a7cb0d0ec80abf4b1177d6108f46adabbb57dd2
amateurdullahan <zabrak12@gmail.com>
2020-08-10 18:49:13 +0000

    pld

---
+++ setup_mysql_test.sql
@@ -0,0 +1,9 @@
+-- coomegtageg
+-- !!!!!!!!
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+DROP USER IF EXISTS 'hbnb_test'@'localhost';
+CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
