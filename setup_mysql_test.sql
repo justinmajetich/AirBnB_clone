ae2be63ccb69588462b4842a129a20ed5a57450a
ronniebm <ronnie.coding@gmail.com>
2020-08-10 18:36:29 -0500

    task#4

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- SQL Script to create a Database.
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+USE hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
