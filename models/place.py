84714944edddc9c108e67c1ebc7b24d0f5c2f3c3
dairof7 <dairof7@gmail.com>
2020-08-11 17:14:56 -0500

    fix

--- models/place.py
+++ models/place.py
@@ -19,6 +19,7 @@ class Place(BaseModel, Base):
     longitude = Column(Float, nullable=False)
     amenity_ids = []
 
+
     reviews = relationship("Review", backref="place", cascade="all, delete")
 
     @property
