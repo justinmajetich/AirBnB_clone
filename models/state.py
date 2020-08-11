88e6c72fedf7156980e70bf123c40ccbbbc48d25
FatChicken277 <1637@holbertonschool.com>
2020-08-11 17:50:59 -0500

    added new stuff

--- models/state.py
+++ models/state.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, ForeignKey
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
+
+    @property
+    def cities(self):
+        from models import storage
+        result = []
+        dict_cities = storage.all("City")
+        for key, obj in dict_cities.items():
+            if self.id == obj["state_id"]:
+                result.append(obj)
+        return result
