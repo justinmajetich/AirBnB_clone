2546aa76693f4890dad066f9c1135be8f397408a
JoseR98 <josealonsoingeniero@hotmail.com>
2020-08-11 15:52:32 +0000

    Add @property to cities getter method

--- models/state.py
+++ models/state.py
@@ -13,6 +13,7 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="all, delete")
 
+    @property
     def cities(self):
         """Returns the list of City instances for current state"""
         state_cities = []
