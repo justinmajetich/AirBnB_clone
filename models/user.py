61f285be6daaec33474f49a13c143356a207f983
Steven Brand <steven.brand1999@gmail.com>
2020-08-11 17:25:56 -0500

    Update User

--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
+from models.reviews import Review
 
 
 class User(BaseModel):
@@ -9,3 +10,8 @@ class User(BaseModel):
     password = ''
     first_name = ''
     last_name = ''
+    reviews = relationship(
+        "Review",
+        backref="user",
+        cascade="all, delete-orphan"
+    )
