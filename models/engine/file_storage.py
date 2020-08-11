64da3987c117f972687edbca9cc931dcc05d7607
Katorea132 <katorea132@gmail.com>
2020-08-11 13:22:27 -0500

    Added changes to file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,20 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls:
+            return {k: v for k, v in self.__objects.items() if type(v) == cls}
         return FileStorage.__objects
+
+    def delete(self, obj=None):
+        """Deletes obj from __object
+        """
+        if obj:
+            key = '{}.{}'.format(type(obj).__name__, obj.id)
+            if key in self.__objects:
+                del self.__objects[key]
+                self.save()
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
