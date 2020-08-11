c67410865c453f22049a53d5dd43bd7a3e3199ed
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-08-10 22:15:13 -0500

    fix add data to do_create funtion consol.py

--- models/base_model.py
+++ models/base_model.py
@@ -2,25 +2,43 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            if 'updated_at' in kwargs.keys():
+                kwargs['updated_at'] = datetime.strptime(
+                    kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            else:
+                kwargs.update({'updated_at': datetime.now()})
+
+            if 'created_at' in kwargs.keys():
+                kwargs['created_at'] = datetime.strptime(
+                    kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            else:
+                kwargs.update({'created_at': datetime.now()})
+
+            if 'id' not in kwargs.keys():
+                kwargs.update({'id': str(uuid.uuid4())})
+
+            if '__class__' in kwargs.keys():
+                del kwargs['__class__']
+            # print("**********************************************")
+            # print(kwargs)
+            # print("**********************************************")
             self.__dict__.update(kwargs)
+            # print(self.__dict__)
+            storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -38,7 +56,7 @@ class BaseModel:
         dictionary = {}
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
