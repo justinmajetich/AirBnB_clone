877c3d7ea3acba8e9abeabdf4bdc3007891c6e90
Ang3l1t0 <1546@holbertonschool.com>
2020-08-11 16:52:02 -0500

    prepares a MySQL server for the project

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,7 @@
+-- MySQL setup development
+-- Create database an user with privileges
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
