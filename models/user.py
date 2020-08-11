614ddb24978c6ac4632a9444722838b30e52b0b3
JoseR98 <josealonsoingeniero@hotmail.com>
2020-08-11 16:29:55 +0000

    Fix nullable parameter

--- models/user.py
+++ models/user.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from models.place import Place
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
@@ -11,6 +10,6 @@ class User(BaseModel, Base):
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
     places = relationship("Place", backref="user", cascade="all, delete")
