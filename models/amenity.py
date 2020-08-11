1d123722547e906879f0b52f428fc247cd63f5c1
Steven Brand <steven.brand1999@gmail.com>
2020-08-11 17:31:37 -0500

    Update Amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column
 
-
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    __tablename__ = amenities
+    name = 
