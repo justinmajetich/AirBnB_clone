2f752dd78a8b1bd46ddbe9f2f86ed62b8ac28d69
Felipe Layos <jfelipelayos@gmail.com>
2020-08-11 15:00:13 +0000

    MySQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
--- Create new User
+-- MySQL server
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL  ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
