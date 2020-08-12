c2e053f2dbfd6bdcfc05f2d48c313f7fef4d3df7
angellovc <vc.angel@outlook.com>
2020-08-12 22:17:58 +0000

    Feat modified: User was modified
    Place relationship was created, User will be treated as place parent

--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 from os import getenv
 
 
@@ -13,6 +14,12 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
+        places = relationship(
+            "Place",
+            backref='user',
+            cascade="all, delete-orphan"
+            )
+
     else:
         email = ''
         password = ''
