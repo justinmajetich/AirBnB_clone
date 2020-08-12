2a6d30f4b45405647f33f421f751b7f8421d6f54
Abdel Mejia <bhalut.bear@gmail.com>
2020-08-12 21:45:56 +0000

    Fix import DBStorage at __init__ models

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
-from models.engine.file_storage import DBStorage
+from models.engine.db_storage import DBStorage
 import os
 
 if os.getenv("HBNB_TYPE_STORAGE") == "db":
