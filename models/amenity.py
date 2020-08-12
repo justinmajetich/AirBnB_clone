9e9fc2e195106fc59fdb0e29c90226e6d51e86aa
Steven Brand <steven.brand1999@gmail.com>
2020-08-12 15:12:59 -0500

    style and doc

--- models/amenity.py
+++ models/amenity.py
@@ -5,7 +5,10 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.place import place_amenity
 
+
 class Amenity(BaseModel, Base):
+    """Object representation of table amenities
+    """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
     place_amenities = relationship(
