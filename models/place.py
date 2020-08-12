d5b1358526602d1f0f85fc68637eace09263cb19
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-08-12 15:33:08 +0000

    Hot fix place file

--- models/place.py
+++ models/place.py
@@ -9,7 +9,7 @@ class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, nullable=False, default=0)
