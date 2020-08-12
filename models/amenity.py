1b27a232a986d11a3b31f0b3d8c35597a6e7a57c
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-08-12 21:24:49 +0000

    Update amenity and place files

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey, Float, Integer
+from sqlalchemy.orm import relationship
+from models.place import Place, place_amenity
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship('Place', secondary=place_amenity)
--- models/place.py
+++ models/place.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Float, Integer
+from sqlalchemy import Column, String, ForeignKey, Float, Integer, Table
 from sqlalchemy.orm import relationship
-from models.review import Review
 import os
+
+
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey('places.id'),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'),
+                             primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
@@ -23,7 +30,10 @@ class Place(BaseModel, Base):
     amenity_ids = []
 
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        reviews = relationship("Review", backref="place", cascade="all, delete")
+        reviews = relationship("Review", backref="place",
+                               cascade="all, delete")
+        amenities = relationship('Amenity', secondary=place_amenity,
+                                 viewonly=False)
     else:
         @property
         def reviews(self):
@@ -33,3 +43,17 @@ class Place(BaseModel, Base):
                 if v.place_id == self.id:
                     all_reviews.append(v)
             return all_reviews
+
+            @property
+            def amenities(self):
+                """ get the amenities in FileStorage """
+                all_amenities = []
+                for v in models.storage.all(Amenity).values():
+                    if v.amenity_ids == self.id:
+                        all_amenities.append(v)
+                    return all_amenities
+
+            @amenities.setter
+            def amenities(self, obj):
+                if obj.__class__.__name__ == 'Amenity':
+                    self.amenity_ids.append(obj.id)
