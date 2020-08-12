1388922113951593ed68f42891d769428b523e11
Samuel Pomeroy <1690@hoblertonschool.com>
2020-08-12 16:50:38 +0000

    first attempt t6 `City` class

--- models/city.py
+++ models/city.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
+""" 0x02. AirBnB clone - MySQL, task 6. DBStorage - States and Cities """
+from sqlalchemy import Column, String
+from sqlalchemy.schema import ForeignKey
 from models.base_model import BaseModel
+from models.base_model import Base
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """Defines attributes for `City` as it inherits from `BaseModel`,
+    and ORM properties in relation to table `cities`.
+    
+    Attributes:
+        name (Column): name of state, string of max 128 chars 
+        state_id (Column): string of max 60 chars, foreign key to `states.id` 
+    """
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), nullable=False, ForeignKey('states.id'))
