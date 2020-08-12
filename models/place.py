bddb7ca92290e74bcbdf0b84265b97e3e8b93146
derric-d <derric.donehoo@gmail.com>
2020-08-12 21:16:47 +0000

    commit debug branch

--- models/place.py
+++ models/place.py
@@ -8,8 +8,8 @@ from sqlalchemy.orm import relationship
 class Place(BaseModel):
     """ A place to stay """
     __tablename__ = 'places'
-    city_id = Column(String(60), nullable=False, ForeignKey('cities.id'))
-    user_id = Column(String(60), nullable=False, ForeignKey('users.id'))
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, nullable=False, default=0)
