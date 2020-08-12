666f488121a11cafd8352de11ddbb6ecd79b061b
julianfrancor <julianfrancor@gmail.com>
2020-08-11 21:23:34 -0500

    fixing all() function in dbstorage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,8 +2,16 @@
 """This module defines a class to manage db storage for hbnb clone"""
 import json
 from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 from models.base_model import Base
 from os import getenv
+
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class DBStorage:
@@ -25,12 +33,18 @@ class DBStorage:
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         dic_obj = {}
+        classes = {'State': State, 'City': City}
         if cls is None:
-            query = self.__session.query().all()
+            for k, v in classes.items():
+                query = self.__session.query(v).all()
+                for obj in query:
+                    delattr(obj, "_sa_instance_state")
+                    dic_obj[obj.__class__.__name__ + "." + str(obj.id)] = obj
         else:
             query = self.__session.query(cls).all()
-        for obj in query:
-            dic_obj[obj.__class__.__name__ + "." + str(obj.id)] = obj
+            for obj in query:
+                delattr(obj, "_sa_instance_state")
+                dic_obj[obj.__class__.__name__ + "." + str(obj.id)] = obj
         return dic_obj
 
     def new(self, obj):
@@ -47,13 +61,6 @@ class DBStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-        from sqlalchemy.orm import sessionmaker
 
         # create all tables in the database
         Base.metadata.create_all(self.__engine)
