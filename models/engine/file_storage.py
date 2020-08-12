9bb2a0c0fab44136cd7a6a73c604e56b12c661f2
jchois <choisjuliana@gmail.com>
2020-08-12 13:01:49 -0500

    Adding comments for public instance method 'def delete'

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -52,11 +52,16 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
+        """public instance method to delete obj if it’s inside
+
+        Args:
+            obj: [None]. Defaults to None.
+        """
         if obj:
             key = "{}.{}".format(obj.__class__.__name__, obj.id)
             del self.__objects[key]
