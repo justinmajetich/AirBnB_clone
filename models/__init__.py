4403f686aae4361f855f9756413aaca7d3a04c2b
Cris-Mur <yomy98@gmail.com>
2020-08-12 04:55:53 +0000

    [ADD TASK 6]implement DB_engine and update models: base_model, city, state

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+from os import environ
 
-
-storage = FileStorage()
+Data_type = environ.get('HBNB_TYPE_STORAGE')
+if Data_type is 'db':
+    from models.engine.db_storage import  DBStorage
+    storage = DBStorage()
+else
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 storage.reload()
