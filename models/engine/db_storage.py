dfb792eb3b2d3b4ef87b7967c0fe5184d14beed0
kimha1030 <1478@holbertonschool.com>
2020-08-12 00:12:49 +0000

    Add db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,57 +1,16 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
+"""This module defines a class to manage dbstorage for hbnb clone"""
 import json
 
 
-class FileStorage:
+class DBStorage:
     """This class manages storage of hbnb models in JSON format"""
-    __file_path = 'file.json'
-    __objects = {}
+    __engine = None
+    __session = None
 
-    def all(self, cls=None):
+    def __init__(self):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
-
-    def new(self, obj):
-        """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
-
-    def save(self):
-        """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
-            json.dump(temp, f)
-
-    def reload(self):
-        """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
-        classes = {
-            'BaseModel': BaseModel, 'User': User, 'Place': Place,
-            'State': State, 'City': City, 'Amenity': Amenity,
-            'Review': Review
-        }
-        try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
-        except FileNotFoundError:
-            pass
-
-    def delete(self, obj=None):
-        """Method that deletes obj from __objects if it’s inside"""
-        key = type(obj).__name__ + "." + obj.id
-        if key in self.__objects:
-            del self.__objects[key]
-            self.save()
+        self.__engine = create_engine(
+        'mysql+mysqldb://{}:{}@{}/{}'.format(HBNB_MYSQL_USER, HBNB_MYSQL_PWD, HBNB_MYSQL_HOST, pool_pre_ping=True)
+        if HBNB_ENV == 'test':
+            #Eliminar todas las tablas
