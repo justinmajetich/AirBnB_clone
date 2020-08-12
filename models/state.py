fc1a2a157272d627a65f8a747b486f1574b35c12
Steven Brand <steven.brand1999@gmail.com>
2020-08-12 16:48:52 -0500

    Validate if type

--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import os
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -9,18 +10,21 @@ import models
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete")
 
-    @property
-    def cities(self):
-        """Returns the list of City instances for current state"""
-        from models import storage
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade="all, delete")
+    else:
+        name = ""
+        @property
+        def cities(self):
+            """Returns the list of City instances for current state"""
+            from models import storage
 
-        state_cities = []
-        cities_all = models.storage.all(City)
-        for city in cities_all.values():
-            if city.state_id == self.id:
-                state_cities.append(city)
-        return state_cities
+            state_cities = []
+            cities_all = models.storage.all(City)
+            for city in cities_all.values():
+                if city.state_id == self.id:
+                    state_cities.append(city)
+            return state_cities
