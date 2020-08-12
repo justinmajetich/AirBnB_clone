59835863ac27f8d9621c3ec22e6d2d067b3c1ef3
Andmontc <oscarandresmontes@gmail.com>
2020-08-12 02:01:12 +0000

    task 6 second try

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -66,9 +66,10 @@ class DBStorage:
     def reload(self):
         """ Reload objects from the database """
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine)
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
         Session = scoped_session(session_factory)
-        self.__session = Session(expire_on_commit=False)
+        self.__session = Session()
 
     def close(self):
         """ close the session """
