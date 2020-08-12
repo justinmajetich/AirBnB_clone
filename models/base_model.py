46f8a45d4c0b0ddef2e5141199c609b58ca5523b
ymcastellar <yoycas@hotmail.com>
2020-08-12 18:34:16 -0500

    imported models

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
+import models
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 
