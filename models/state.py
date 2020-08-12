0cbaec2adb35abb9a9f240ca3d69d97817faa6a3
Katorea132 <katorea132@gmail.com>
2020-08-12 18:14:34 -0500

    Added missing file for 6

--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship, backref
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship(
+        'City',
+        cascade='all,delete,delete-orphan',
+        backref=backref('state', cascade='all,delete'),
+        passive_deletes=True,
+        single_parent=True
+    )
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """Man, so tired
+            """
+            from models import storage
+            from models.city import City
+            return [v for k, v in storage.all(City).items()
+                    if v.state_id == self.id]
