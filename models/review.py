5d63e3e5fa1aebc7151093d809ad04c0da650e17
Andmontc <oscarandresmontes@gmail.com>
2020-08-12 16:03:30 +0000

    Update review class

--- models/review.py
+++ models/review.py
@@ -3,8 +3,9 @@
 from models.base_model import BaseModel
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = 'reviews'
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
