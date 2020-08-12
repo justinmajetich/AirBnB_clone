683dfc6c44e8521b598ff3b831bd58d9c0435352
ymcastellar <yoycas@hotmail.com>
2020-08-12 13:48:24 -0500

    nullabletrue added

--- models/user.py
+++ models/user.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-     __tablename__ = 'users'
+    __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(Sring(128), nullable=False)
-    first_name = Column(Sring(128))
-    last_name = Column(Sring(128))
+    first_name = Column(Sring(128),  nullable=True)
+    last_name = Column(Sring(128),  nullable=True)
