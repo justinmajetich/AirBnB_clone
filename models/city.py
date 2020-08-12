cf1f8152dd01a6035f00035028bfbeda18673345
Andmontc <oscarandresmontes@gmail.com>
2020-08-12 01:06:49 +0000

    task 6 try

--- models/city.py
+++ models/city.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-
+from sqlalchemy.orm import relationship
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    __table_name__ = 'cities'
-    state_id = Column(String(60), nullable=False, ForeignKey='states.id')
+    __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    
--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+import os
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref="state", cascade="all, delete")
+    else:
+        @property
+        def cities(self):
+            """ get the cities in FileStorage """
+            all_cities = []
+            for k, v in models.storage.all(City).items():
+                if v.state_id == self.id:
+                    all_cities.append(v)
+            return all_cities
