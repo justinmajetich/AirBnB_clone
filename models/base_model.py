1a2524d163892fd8004275c0b3a7555708d7887e
FatChicken277 <1637@holbertonschool.com>
2020-08-12 11:53:27 -0500

    added type_storage conditionals

--- models/base_model.py
+++ models/base_model.py
@@ -4,8 +4,12 @@ import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime
+from os import getenv
 
-Base = declarative_base()
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    Base = declarative_base()
+else:
+    Base = object
 
 
 class BaseModel:
