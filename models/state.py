7884513facaf7ab52c13c11ae5014d24ff4119f1
untalinfo <racso1607@gmail.com>
2020-08-12 15:38:50 +0000

    fixes: change import in amenity (import place_amenity), in place and state adde new condicion when HBNB_TYPE_STORAGE' != 'db'

--- models/state.py
+++ models/state.py
@@ -12,18 +12,20 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="all, delete")
 
-    @property
-    def cities(self):
-        """ cities getter
-        Returns the list of City instances with
-        state_id equals to the current State.id
-        """
-        from models import storage
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
 
-        our_cts = storage.all(City)
-        state_cts = []
-        for city in our_cts.values():
-            if self.id == city.id:
-                state_cts.append(city)
+        @property
+        def cities(self):
+            """ cities getter
+            Returns the list of City instances with
+            state_id equals to the current State.id
+            """
+            from models import storage
 
-        return state_cts
+            our_cts = storage.all(City)
+            state_cts = []
+            for city in our_cts.values():
+                if self.id == city.id:
+                    state_cts.append(city)
+
+            return state_cts
