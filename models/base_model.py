b142cffb3940dc69f3405c44cdd543a728aa78b5
Andmontc <oscarandresmontes@gmail.com>
2020-08-12 22:23:34 +0000

    Fix Task 10

--- models/base_model.py
+++ models/base_model.py
@@ -18,19 +18,23 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                if k != "__class__":
+                    setattr(self, k, v)
+            if self.id is None:
+                setattr(self, 'id', str(uuid.uuid4()))
+            now = datetime.now()
+            if self.created_at is None:
+                setattr(self, 'created_at', now)
+            if self.updated_at is None:
+                setattr(self, 'updated_at', now)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
-            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
