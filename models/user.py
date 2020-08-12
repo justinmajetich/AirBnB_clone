a3b27df38e16b980a1d0515d8a64b87de6c16c1a
Steven Brand <steven.brand1999@gmail.com>
2020-08-12 16:49:12 -0500

    Validate if type

--- models/user.py
+++ models/user.py
@@ -4,18 +4,26 @@ from models.review import Review
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import os
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    places = relationship("Place", backref="user", cascade="all, delete")
-    reviews = relationship(
-        "Review",
-        backref="user",
-        cascade="all, delete-orphan"
-    )
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user", cascade="all, delete")
+        reviews = relationship(
+            "Review",
+            backref="user",
+            cascade="all, delete-orphan"
+        )
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
