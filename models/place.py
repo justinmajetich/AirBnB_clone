80185b0bdbace83b0e1babc7ab8cedfaea1185d9
FatChicken277 <1637@holbertonschool.com>
2020-08-12 15:21:58 -0500

    fixed wrong class name

--- models/place.py
+++ models/place.py
@@ -20,7 +20,7 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
-        reviews = relationship("Reviews", backref="place",
+        reviews = relationship("Review", backref="place",
                                cascade="all, delete")
     else:
         city_id = ""
