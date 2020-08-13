20ad503c36941df9b656e473ff2c217e11c5b490
kimha1030 <1478@holbertonschool.com>
2020-08-13 02:15:17 +0000

    update import in class State

--- models/state.py
+++ models/state.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.city import City
+from models.engine.file_storage import FileStorage
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import os
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref='state', cascade="all, delete")
+    cities = relationship("City", backref='state', cascade="all, delete-orphan")
 
     @property
     def cities(self):
-        from models.engine.file_storage import FileStorage
-        from models.city import City
-        import os
-
         if os.getenv("HBNB_TYPE_STORAGE") == "db":
             return
         list_cities = []
         for c in FileStorage.all(City).values():
-            if c.states_id == State.id:
+            if c.states_id == self.id:
                 list_cities.append(c)
-
         return list_cities
