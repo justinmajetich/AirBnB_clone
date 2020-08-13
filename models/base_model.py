230baabdf46577340397b0ee23813d482d39d3f3
ymcastellar <yoycas@hotmail.com>
2020-08-13 16:41:24 -0500

    parentesis fixed

--- models/base_model.py
+++ models/base_model.py
@@ -14,9 +14,9 @@ class BaseModel:
 
     id = Column(String(60), nullable=False, primary_key=True)
     created_at = Column(DateTime, nullable=False,
-                        default=datetime.utcnow
+                        default=datetime.utcnow)
     updated_at = Column(DateTime, nullable=False,
-                        default=datetime.utcnow
+                        default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
