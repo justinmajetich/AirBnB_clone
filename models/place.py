ccfdf30c2409d13ac7d9762db63296baf36b2a54
Tony Sims <zocle@zocle.net>
2020-08-13 12:49:43 -0500

    fixed do_create and added Base to place

--- models/place.py
+++ models/place.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
 
     __tablename__ = 'places'
