7be682831a1d0e0b5da9ea13004ae45fdcadc30b
ymcastellar <yoycas@hotmail.com>
2020-08-13 15:06:37 -0500

    review updated colums added amd updated usaer with relations

--- models/review.py
+++ models/review.py
@@ -1,10 +1,25 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base, Table, Column, String
+from sqlalchemy import ForeignKey
+from os import getenv
 
 
 class Review(BaseModel):
     """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    
+    if getenv('HBNB_TYPE_STORAGE', 'fs') == 'db':
+        __tablename__ = "reviews"
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        text = Column(String(1024), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
+
+    def __init__(self, *args, **kwargs):
+        """
+        initialize from the BaseModel class
+        """
+        super().__init__(*args, **kwargs)
--- models/user.py
+++ models/user.py
@@ -17,3 +17,6 @@ class User(BaseModel, Base):
 
     places = relationship("Place", cascade="all, delete, delete-orphan",
                           backref="user")
+    
+    reviews = relationship("Review", cascade="all, delete, delete-orphan",
+                           backref="user")
