1e830542960dbd63866df04cc9bd2f71178afbfd
kimha1030 <1478@holbertonschool.com>
2020-08-13 03:49:38 +0000

    Update attributes of places

--- models/place.py
+++ models/place.py
@@ -2,6 +2,7 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class Place(BaseModel, Base):
@@ -11,10 +12,10 @@ class Place(BaseModel, Base):
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=False)
-    number_rooms = Column(Integer(0), nullable=False)
-    number_bathrooms = Column(Integer(0), nullable=False)
-    max_guest = Column(Integer(0), nullable=False)
-    price_by_night = Column(Integer(0), nullable=False)
-    latitude = Column(Float(0), nullable=False)
-    longitude = Column(Float(0), nullable=False)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float, nullable=False)
+    longitude = Column(Float, nullable=False)
     amenity_ids = []
