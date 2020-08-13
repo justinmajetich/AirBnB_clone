78a2494886ce2068be30cdc56b7d01b31b09801f
FatChicken277 <1637@holbertonschool.com>
2020-08-13 18:04:56 -0500

    added storage conditional

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -2,18 +2,21 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
+from os import getenv
 
 
 class test_state(test_basemodel):
     """ """
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "State"
-        self.value = State
+    if getenv("HBNB_TYPE_STORAGE") != "db":
 
-    def test_name3(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+        def __init__(self, *args, **kwargs):
+            """ """
+            super().__init__(*args, **kwargs)
+            self.name = "State"
+            self.value = State
+
+        def test_name3(self):
+            """ """
+            new = self.value()
+            self.assertEqual(type(new.name), str)
