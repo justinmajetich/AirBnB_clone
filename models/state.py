795fcf5fed29d6fa8309a5f211f20b4fed1d6399
jchois <choisjuliana@gmail.com>
2020-08-13 11:45:44 -0500

    Updating States file

--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import models
+from models.city import City
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, ForeignKey
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete, delete-orphan",
+                          backref='state')
+
+    @property
+    def cities(self):
+        """
+        Returns the cities
+        """
+
+        new_dict = models.storage.all(City)
+        new_list = []
+        for key, value in new_dict.items():
+            if value.state_id == self.id:
+                new_list.append(value)
+        return new_list
