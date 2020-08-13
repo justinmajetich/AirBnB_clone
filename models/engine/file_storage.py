4d80409d3aaebd691a1d659d9f8729550af44a5b
JoseR98 <josealonsoingeniero@hotmail.com>
2020-08-13 18:20:00 +0000

    Update testt

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -21,7 +21,9 @@ class FileStorage:
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        if obj:
+            key = type(obj).__name__ + "." + str(obj.id)
+            FileStorage.__objects[key] = obj
 
     def save(self):
         """Saves storage dictionary to file"""
