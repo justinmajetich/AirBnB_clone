24117353095c6c4f586ac8c2c0b380eaa16a0184
kimha1030 <1478@holbertonschool.com>
2020-08-13 03:50:41 +0000

    Update Class State

--- models/state.py
+++ models/state.py
@@ -10,9 +10,9 @@ import os
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref='state', cascade="all, delete-orphan")
+    cities = relationship("City", backref="state", cascade="all, delete-orphan")
 
     @property
     def cities(self):
