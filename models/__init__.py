43c27f55b6fb3a1d0f5a65b0ab508d48c1f66603
kimha1030 <1478@holbertonschool.com>
2020-08-13 16:19:27 +0000

    Add importto classes

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,12 @@
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
+from models.state import State
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 import os
 
 if os.getenv("HBNB_TYPE_STORAGE") == "db":
