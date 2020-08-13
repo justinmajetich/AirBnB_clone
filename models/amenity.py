44ce860067d9a5322481ae5b2cc4571933cde728
angellovc <vc.angel@outlook.com>
2020-08-13 22:34:17 +0000

    Feat modified: Amenities class modified
    Back_populates was added to Amenity relationship

--- models/amenity.py
+++ models/amenity.py
@@ -14,6 +14,7 @@ class Amenity(BaseModel, Base):
         place_amenities = relationship(
             "Place",
             secondary='place_amenity',
+            back_populates='amenities'
             )
     else:
         name = ""
