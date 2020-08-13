1bd83ead087af25bc6edd84977ea3a3e4f639190
andreshugueth <andreshugueth@gmail.com>
2020-08-12 21:00:56 -0500

    6th task first try

--- models/city.py
+++ models/city.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+import models
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+
+    if models.HBNB_TYPE_STORAGE == "db":
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey('states.id') ,nullable=False)
+        name = Column(String(128), nullable=False)
+    else:
+        state_id = ""
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)
