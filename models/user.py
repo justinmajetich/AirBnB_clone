3dfb41ca4a64010ec98559beb9995083bf0e4d7b
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-08-13 23:15:25 +0000

    Fix errors

--- models/user.py
+++ models/user.py
@@ -1,27 +1,11 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from models.base_model import BaseModel
 
 
-class User(BaseModel, Base):
+class User(BaseModel):
     """This class defines a user by various attributes"""
-
-    __tablename__ = 'users',
-    email = Column(
-        'email',
-        String(128),
-        nullable=False
-    )
-    password = Column(
-        String(128),
-        nullable=False
-    )
-    first_name = Column(
-        String(128),
-        nullable=False
-    )
-    last_name = Column(
-        String(128),
-        nullable=False
-    )
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
