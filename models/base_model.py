fd99cbafe6336cee90a92973b3706f85c06f2166
bryanbuiles <1482@holbertonschool.com>
2020-08-13 12:41:58 -0500

    task 10 done

--- models/base_model.py
+++ models/base_model.py
@@ -24,13 +24,18 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            # crear nuevos atributos de la instancia punto 6. all ?
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+                if "id" not in kwargs.keys():
+                    self.id = str(uuid.uuid4())
+                if "created_at" not in kwargs.keys():
+                    self.created_at = datetime.now()
+                if "updated_at" not in kwargs.keys():
+                    self.updated_at = datetime.now()
+                # self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -60,5 +65,4 @@ class BaseModel:
 
     def delete(self):
         """ Delete a instance of the DB_Storage """
-        from models import storage
-        storage.delete(self)
+        models.storage.delete(self)
--- models/place.py
+++ models/place.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, Table
 from sqlalchemy.orm import relationship
 from models.review import Review
+from models.amenity import Amenity
+import models
 import os
+
+place_amenity = Table("place_amenity", Base.metadata,
+                      Column("place_id", String(60),
+                             ForeignKey("places.id"),
+                             primary_key=True, nullable=False),
+                      Column("amenity_id", String(60),
+                             ForeignKey("amenities.id"),
+                             primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
@@ -25,17 +35,46 @@ class Place(BaseModel, Base):
         longitude = Column(Float, nullable=True)
         reviews = relationship('Review',
                                backref='place', cascade='all, delete-orphan')
+        amenities = relationship(
+            'Amenity', secondary="place_amenity", viewonly=False)
+        amenity_ids = []
 
     else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
 
         @property
         def reviews(self):
-            """ getter to filestorage """
-            lista = []
-            # Returns the list of City instances with
-            # state_id == to the current State.id
-            for value in storage.all(Review).values():
-                dict_obj = value.to_dict()
-                if dict_obj["place_id"] == self.id:
-                    lista.append(value)
-            return (lista)
+            """ getter to reviews return a list of reviews for
+            the actual isntance Placce """
+            list_reviews_inst = []
+            for reviews_inst in models.storage.all(Review).values():
+                if reviews_inst.place_id == self.id:
+                    list_reviews_inst.append(reviews_inst)
+            return (list_reviews_inst)
+
+        @property
+        def amenities(self):
+            """ getter amenities is attribute of class Place
+            Returns a list of instances of type class Amenities"""
+            list_amenities_inst = []
+            for amenity_inst in models.storage.all(Amenity).values():
+                if amenity_inst.id in self.amenity_ids:
+                    list_amenities_inst.append(amenity_inst)
+            return (list_amenities_inst)
+
+        @amenities.setter
+        def amenities(self, inst_amenity):
+            """ setter amenities is an attribute of class Place that adds
+            an Instance of type amenity into the list amenities_ids"""
+            if isinstance(inst_amenity, Amenity):
+                self.amenity_ids.append(inst_amenity)
