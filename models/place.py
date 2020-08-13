529da27d7705c370ea545336a01399d93701e8a7
julianfrancor <julianfrancor@gmail.com>
2020-08-13 11:20:20 -0500

    putting the table out of the class place

--- models/place.py
+++ models/place.py
@@ -7,15 +7,16 @@ from sqlalchemy import Column, Integer, String, Float, ForeignKey, Table
 from os import getenv
 
 
+place_amenity = Table('place_amenity', Base.metadata,
+                        Column('place_id', String(60), ForeignKey(
+                            'places.id'),
+                            primary_key=True, nullable=False),
+                        Column('amenity_id', String(60), ForeignKey(
+                            'amenities.id'),
+                            primary_key=True, nullable=False))
+
 class Place(BaseModel, Base):
     """ A place to stay """
-    place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60), ForeignKey(
-                              'places.id'),
-                              primary_key=True, nullable=False),
-                          Column('amenity_id', String(60), ForeignKey(
-                              'amenities.id'),
-                              primary_key=True, nullable=False))
     __tablename__ = 'places'
     if getenv("HBNB_TYPE_STORAGE") == "db":
         city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
