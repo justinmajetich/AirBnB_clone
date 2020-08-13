b7eaa0065b271e42049b3c8762b058eb58baec5c
R6Doc <fiendgear@gmail.com>
2020-08-13 16:34:38 -0500

    added init and fixed files

--- models/user.py
+++ models/user.py
@@ -7,6 +7,16 @@ import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
+if models.storage_t == 'db':
+    place_amenity = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id', onupdate='CASCADE',
+                                            ondelete='CASCADE'),
+                                 primary_key=True),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id', onupdate='CASCADE',
+                                            ondelete='CASCADE'),
+                                 primary_key=True))
 
 class User(BaseModel, Base):
     """A user"""
