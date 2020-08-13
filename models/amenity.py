efe527948989b905f38b75dea8ca518369cfa5db
angellovc <vc.angel@outlook.com>
2020-08-13 22:47:43 +0000

    Feat modified: db_storage modified
    unnecesary delattr statement was deleted

--- models/amenity.py
+++ models/amenity.py
@@ -14,7 +14,6 @@ class Amenity(BaseModel, Base):
         place_amenities = relationship(
             "Place",
             secondary='place_amenity',
-            back_populates='amenities'
             )
     else:
         name = ""
