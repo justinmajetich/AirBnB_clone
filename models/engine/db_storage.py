cfabae01e3d65ff0a0039d4f16c8f0d6c31b44a1
Corbin Enterline <corbinenterline1@gmail.com>
2020-08-14 00:05:13 +0000

    8,7

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -36,6 +36,7 @@ class DBStorage:
             objs = self.__session.query(State).all()
             objs += self.__session.query(City).all()
             objs += self.__session.query(User).all()
+            objs += self.__session.query(Place).all()
         return {"{}.{}".format(type(x).__name__, x.id): x for x in objs}
 
     def new(self, obj):
