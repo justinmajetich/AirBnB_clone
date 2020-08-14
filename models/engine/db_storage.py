8bc0d4e365d958203f5f2621f10c0a4ffbd57961
R6Doc <fiendgear@gmail.com>
2020-08-13 21:46:36 -0500

    added db storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""File for DBStorage module"""
+"""
+cLass DBStorage to use databasel 
+"""
+
 import models
 from models.amenity import Amenity
 from models.base_model import BaseModel, Base
@@ -13,66 +16,61 @@ import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
 
 class DBStorage:
-	"""Comment but im lazy"""
-	__engine = None
-	___session = None
+    """INTER WITH DATABASE"""
+    __engine = None
+    __session = None
 
-
-	def __init__(self):
-		"""Init"""
-		HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
-		HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
-		HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
-		HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
-		HBNB_ENV = getenv('HBNB_ENV')
-		self.engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
-									format(HBNB_MYSQL_USER,
-										   HBNB_MYSQL_PWD,
-										   HBNB_MYSQL_HOST,
-										   HBNB_MYSQL_DB,
-										   HBNB_ENV))
-		if HBNB_ENV == "test":
-			Base.metadata.drop_all(self.__engine)
+    def __init__(self):
+        """INIT"""
+        HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
+        HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
+        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
+        HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
+        HBNB_ENV = getenv('HBNB_ENV')
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
+                                      format(HBNB_MYSQL_USER,
+                                             HBNB_MYSQL_PWD,
+                                             HBNB_MYSQL_HOST,
+                                             HBNB_MYSQL_DB))
+        if HBNB_ENV == "test":
+            Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """Returns a dictionary of models currently in storage"""
-        if cls is not None:
-            dictio = {}
-            for key, val in self.__objects.items():
-                if cls == val.__class__ or cls == val.__class__.__name__:
-                    dictio[key] = val
-            return dictio
-        return FileStorage.__objects
+        """COMMENT"""
+        new_dict = {}
+        for clss in classes:
+            if cls is None or cls is classes[clss] or cls is clss:
+                objs = self.__session.query(classes[clss]).all()
+                for obj in objs:
+                    key = obj.__class__.__name__ + '.' + obj.id
+                    new_dict[key] = obj
+        return (new_dict)
 
     def new(self, obj):
-    	"""COMMENT"""
-    	self.___session.add(obj)
-    
+        """COMMENT"""
+        self.__session.add(obj)
 
     def save(self):
-    	"""COMMENT"""
-    		self.___session.commit()
-    
+        """IDK"""
+        self.__session.commit()
+
     def delete(self, obj=None):
-    	"""COMMENT"""
-    	if obj is not None:
-    		self.___session.delete(obj)
+        """UHHH YEA"""
+        if obj is not None:
+            self.__session.delete(obj)
 
     def reload(self):
-    	"""sadsdasd """
-    	Base.metadata.create_all(self.__engine)
-    	sessionf = sessionmaker(bind=self.__engine, expire_on_commit=(False))
-    	Session = scoped_session(sessionf)
-    	self.session = Session
+        """COMMENT LMAO"""
+        Base.metadata.create_all(self.__engine)
+        sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sess_factory)
+        self.__session = Session
 
     def close(self):
-    	"""tasdasd"""
-		self.___session.remove()
+        """THIS CLOSES LOL"""
+        self.__session.remove()
