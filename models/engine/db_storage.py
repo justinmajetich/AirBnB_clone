1822847a01a5674135c5a92cb697b64911ad1239
R6Doc <fiendgear@gmail.com>
2020-08-13 21:43:13 -0500

    added db storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+"""File for DBStorage module"""
 import models
 from models.amenity import Amenity
 from models.base_model import BaseModel, Base
@@ -12,33 +14,33 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 
-classes = {
-            'BaseModel': BaseModel, 'User': User, 'Place': Place,
-            'State': State, 'City': City, 'Amenity': Amenity,
-            'Review': Review
-            }
-
+        classes = {
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+                  }
 
 class DBStorage:
-    """Comment but im lazy"""
-    __engine = None
-    ___session = None
+	"""Comment but im lazy"""
+	__engine = None
+	___session = None
 
-    def __init__(self):
-        """Init"""
-        HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
-        HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
-        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
-        HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
-        HBNB_ENV = getenv('HBNB_ENV')
-        self.engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
-                                    format(HBNB_MYSQL_USER,
-                                           HBNB_MYSQL_PWD,
-                                           HBNB_MYSQL_HOST,
-                                           HBNB_MYSQL_DB,
-                                           HBNB_ENV))
-        if HBNB_ENV == "test":
-            Base.metadata.drop_all(self.__engine)
+
+	def __init__(self):
+		"""Init"""
+		HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
+		HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
+		HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
+		HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
+		HBNB_ENV = getenv('HBNB_ENV')
+		self.engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
+									format(HBNB_MYSQL_USER,
+										   HBNB_MYSQL_PWD,
+										   HBNB_MYSQL_HOST,
+										   HBNB_MYSQL_DB,
+										   HBNB_ENV))
+		if HBNB_ENV == "test":
+			Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
@@ -51,25 +53,26 @@ class DBStorage:
         return FileStorage.__objects
 
     def new(self, obj):
-        """COMMENT"""
-        self.___session.add(obj)
+    	"""COMMENT"""
+    	self.___session.add(obj)
+    
 
     def save(self):
-        """COMMENT"""
-        self.___session.commit()
-
+    	"""COMMENT"""
+    		self.___session.commit()
+    
     def delete(self, obj=None):
-        """COMMENT"""
-        if obj is not None:
-            self.___session.delete(obj)
+    	"""COMMENT"""
+    	if obj is not None:
+    		self.___session.delete(obj)
 
     def reload(self):
-        """sadsdasd """
-        Base.metadata.create_all(self.__engine)
-        sessionf = sessionmaker(bind=self.__engine, expire_on_commit=(False))
-        Session = scoped_session(sessionf)
-        self.session = Session
+    	"""sadsdasd """
+    	Base.metadata.create_all(self.__engine)
+    	sessionf = sessionmaker(bind=self.__engine, expire_on_commit=(False))
+    	Session = scoped_session(sessionf)
+    	self.session = Session
 
     def close(self):
-        """tasdasd"""
-        self.___session.remove()
+    	"""tasdasd"""
+		self.___session.remove()
