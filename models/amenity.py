aa29f8a6dd259d16a173a023d3b85a764e3fa0d3
jchois <choisjuliana@gmail.com>
2020-08-13 20:53:53 -0500

    Updating amenity.py file

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+import models
+from os import getenv
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """This class defines a Amenity by various attributes"""
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
