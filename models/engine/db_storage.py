629b861083fb9aff948c7281966ed3f71255f4c4
erikaosgue <1476@holbertonschool.com>
2020-08-14 04:29:40 +0000

    Add close() to db_storage file

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -72,3 +72,7 @@ class DBStorage:
             bind=self.__engine, expire_on_commit=False,)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+    def close(self):
+        """ Close the session """
+        self.__session.close()
