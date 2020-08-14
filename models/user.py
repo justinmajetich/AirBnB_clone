7d99fc921e033ca596b214cbd557994206931908
julianfrancor <julianfrancor@gmail.com>
2020-08-13 19:40:10 -0500

    modifying user

--- models/user.py
+++ models/user.py
@@ -22,3 +22,19 @@ class User(BaseModel, Base):
         password = ""
         first_name = ""
         last_name = ""
+
+
+  if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user", cascade="all, delete")
+        reviews = relationship("Review", backref="user",
+                               cascade="all, delete")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
