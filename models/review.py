96b30674d22bf2c43b46d498a3711ae733cc0832
jchois <choisjuliana@gmail.com>
2020-08-13 20:55:11 -0500

    some modules removd

--- models/review.py
+++ models/review.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
+"""Review module for the HBNB project."""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Float, Integer, String, ForeignKey
+from sqlalchemy import Column, String, ForeignKey
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
+class Review(BaseModel, Base):
+    """Review classto store review information."""
 
     __tablename__ = 'reviews'
     text = Column(String(1024), nullable=False)
