aa26897a1e3712a9c3bd0b9095a4f7e37d80a4c9
bryanbuiles <1482@holbertonschool.com>
2020-08-13 22:59:32 -0500

    add test for db engine

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -10,63 +10,85 @@ import pep8
 
 
 class test_basemodel(unittest.TestCase):
-    """Class test using unittest"""
+    """ """
 
-    @classmethod
-    def setUpClass(cls):
-        cls.prueba = BaseModel()
-        cls.prueba.name = "team"
-        cls.prueba.age = 25
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = 'BaseModel'
+        self.value = BaseModel
 
-    @classmethod
-    def tearDown(cls):
-        del cls.prueba
+    def setUp(self):
+        """ """
+        pass
 
     def tearDown(self):
         try:
-            os.remove("file.json")
-        except Exception:
+            os.remove('file.json')
+        except:
             pass
 
-    def test_pep8_conformance(self):
-        """Test that we conform to PEP8."""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/base_model.py'])
-        self.assertEqual(
-            result.total_errors, 0, "Found code style errors (and warnings).")
+    def test_default(self):
+        """ """
+        i = self.value()
+        self.assertEqual(type(i), self.value)
 
-    def test_documentation(self):
-        self.assertTrue(len(BaseModel.__init__.__doc__) > 1)
-        self.assertTrue(len(BaseModel.save.__doc__) > 1)
-        self.assertTrue(len(BaseModel.__str__.__doc__) > 1)
-        self.assertTrue(len(BaseModel.to_dict.__doc__) > 1)
+    def test_kwargs(self):
+        """ """
+        i = self.value()
+        copy = i.to_dict()
+        new = BaseModel(**copy)
+        self.assertFalse(new is i)
 
-    def test_args_kwargs(self):
-        self.assertEqual(self.prueba.name, "team")
-        self.assertEqual(self.prueba.age, 25)
-        self.assertTrue(hasattr(self.prueba, "name"))
-        self.assertTrue(hasattr(self.prueba, "age"))
-
-    def test_BaseModel(self):
-        self.assertTrue(hasattr(BaseModel, "__init__"))
-        self.assertTrue(hasattr(BaseModel, "save"))
-        self.assertTrue(hasattr(BaseModel, "to_dict"))
-        self.assertTrue(hasattr(BaseModel, "__str__"))
+    def test_kwargs_int(self):
+        """ """
+        i = self.value()
+        copy = i.to_dict()
+        copy.update({1: 2})
+        with self.assertRaises(TypeError):
+            new = BaseModel(**copy)
 
     def test_save(self):
-        self.prueba.save()
-        self.assertNotEqual(self.prueba.created_at, self.prueba.updated_at)
-
-    def test_to_dict(self):
-        self.assertEqual(self.prueba.__class__.__name__, "BaseModel")
-        b = self.prueba.to_dict()
-        self.assertIsInstance(b['created_at'], str)
+        """ Testing save """
+        i = self.value()
+        i.save()
+        key = self.name + "." + i.id
+        with open('file.json', 'r') as f:
+            j = json.load(f)
+            self.assertEqual(j[key], i.to_dict())
 
     def test_str(self):
-        bstr = self.prueba.__str__()
-        self.assertEqual(bstr, "[BaseModel] ({}) {}".format(
-            self.prueba.id, self.prueba.__dict__))
+        """ """
+        i = self.value()
+        self.assertEqual(str(i), '[{}] ({}) {}'.format(self.name, i.id,
+                                                       i.__dict__))
 
+    def test_todict(self):
+        """ """
+        i = self.value()
+        n = i.to_dict()
+        self.assertEqual(i.to_dict(), n)
 
-if __name__ == "__main__":
-    unittest.main()
+    def test_kwargs_none(self):
+        """ """
+        n = {None: None}
+        with self.assertRaises(TypeError):
+            new = self.value(**n)
+
+    def test_id(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.id), str)
+
+    def test_created_at(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.created_at), datetime.datetime)
+
+    def test_updated_at(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.updated_at), datetime.datetime)
+        n = new.to_dict()
+        new = BaseModel(**n)
+        self.assertFalse(new.created_at == new.updated_at)
--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ """
+
 from models.base_model import BaseModel
 import unittest
 import datetime
@@ -7,11 +8,24 @@ from uuid import UUID
 import json
 import os
 import pep8
+from models.place import Place
+import MySQLdb
+from models.state import State
 from models.city import City
+from models.user import User
+from models.review import Review
+from models.amenity import Amenity
+from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 
 
 class test_City(unittest.TestCase):
     """ """
+
+    def setUp(self):
+        """ check for the right type of datastorage otherwise skiptheTest """
+        if os.getenv("HBNB_TYPE_STORAGE") != "FileStorage":
+            raise unittest.SkipTest("because is not using Filstorage")
 
     def test_pep8_conformance(self):
         """Test that we conform to PEP8."""
@@ -38,3 +52,76 @@ class test_City(unittest.TestCase):
         """ """
         new = self.value()
         self.assertEqual(type(new.name), str)
+
+
+class test_city_db_storage(unittest.TestCase):
+    """ test for checking deb_storage """
+
+    @classmethod
+    def setUpClass(cls):
+        if os.getenv("HBNB_TYPE_STORAGE") == "db":
+            cls.storage = DBStorage()
+            cls.storage.reload()
+
+            cls.state = State(name="California")
+            cls.state.save()
+            # creation of a City
+            cls.city = City(state_id=cls.state.id, name="San Francisco")
+            cls.city.save()
+            # creation of a User
+
+            cls.storage.save()
+
+    def setUp(self):
+        """ check for the right type of datastorage otherwise skiptheTest """
+        if os.getenv("HBNB_TYPE_STORAGE") != "db":
+            self.skipTest("because is not using db_storage")
+        self.connection()
+
+    def tearDown(self):
+        self.cursor.close()
+        self.storage.close()
+
+    def connection(self):
+        """ set the connection for the database MYSQL """
+        my_user = os.getenv("HBNB_MYSQL_USER")
+        my_passw = os.getenv("HBNB_MYSQL_PWD")
+        my_host = os.getenv("HBNB_MYSQL_HOST")
+        my_db = os.getenv("HBNB_MYSQL_DB")
+
+        self.db = MySQLdb.connect(host=my_host, db=my_db, user=my_user,
+                                  passwd=my_passw, port=3306)
+        # creation of 2 Places
+        self.cursor = self.db.cursor()
+
+    def test_City_id(self):
+        """ Test when creating Places """
+        self.cursor.execute(
+            """SELECT id FROM cities""")
+        query = self.cursor.fetchall()
+        lista = []
+        for id in query:
+            lista.append(id[0])
+        self.assertIn(self.city.id, lista)
+
+        self.cursor.close()
+
+    def test_City_attribute(self):
+        """test Place adds an attribute when instanciating it"""
+
+        self.cursor.execute(
+            """SELECT name FROM cities""")
+        query = self.cursor.fetchall()
+        lista = []
+        for name in query:
+            lista.append(name[0])
+        self.assertIn(self.city.name, lista)
+        self.cursor.close()
+
+    def test_type(self):
+        """test the type of the class """
+        self.assertTrue(type(self.city), City)
+
+
+if __name__ == "__main__":
+    unittest.main()
