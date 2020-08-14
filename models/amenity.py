f2ba1bf03ba3f6e96c8bbed214ddc47d200c1dd4
GabrielAbdul <gabrielabdul1@gmail.com>
2020-08-14 04:37:38 +0000

    Amenity... and BOOM

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
 
 
 class Amenity(BaseModel):
-    name = ""
+    '''class Amenity'''
+
+    __tablename__ = 'amenities'
+    name = Column(String(128))
+    place_amenities = relationship(
+        "Place", secondary=place_amenity, backref='amenities')
