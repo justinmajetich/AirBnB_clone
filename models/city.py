3a3faefe0b3fc322fa1748039c1f5b628bdd5ad4
Tony Sims <zocle@zocle.net>
2020-08-13 21:39:19 -0500

    added check for columns that should remain strings, for task 8, and cleaned up a couple of rogue declarations

--- models/city.py
+++ models/city.py
@@ -2,7 +2,7 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
