36adc94756cdbad33bd07b146ca0a989e32740dd
Corbin Enterline <corbinenterline1@gmail.com>
2020-08-15 08:37:31 +0000

    10?

--- models/city.py
+++ models/city.py
@@ -5,6 +5,7 @@ from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.place import Place
 
+
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
