bbe127d167a1ebf56880d27334e0cc1a5e53f085
R6Doc <fiendgear@gmail.com>
2020-08-17 22:24:19 -0500

    added files, and finally got the 2

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+create tgz using frabric
+"""
+
+from datetime import datetime
+from fabric.api import local
+from os.path import isdir
+
+
+def do_pack():
+    """generates a tgz archive"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
+        return None
