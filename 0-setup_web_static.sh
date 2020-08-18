7dedd8a18b3a7eedcdacd4c1abd7a5ed49eb20c0
Fabian Andres <f4bian.andres@gmail.com>
2020-08-18 00:24:49 +0000

    [feature] Setup web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 # Prepare your web servers
-if ! which nginx > /dev/null 2>&1; then
-    sudo apt-get update
-    sudo apt-get -y install nginx
-fi
-mkdir /data/web_static/shared/
-mkdir /data/web_static/releases/test/
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 echo -e '<html>
   <head>
   </head>
@@ -15,6 +13,5 @@ echo -e '<html>
 </html>' > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current 
 chown -R ubuntu:ubuntu /data/
-new_redirect="server_name _;\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n"
-sudo sed -i 's|server_name _;|'"$new_redirect"'|' /etc/nginx/sites-enabled/default
+sudo sed -i '/pass the PHP/i \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-enabled/default
 sudo service nginx restart
