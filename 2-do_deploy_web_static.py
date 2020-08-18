dbec462bbd6e0ee02203c8466a403702476302aa
Fabian Andres <f4bian.andres@gmail.com>
2020-08-18 04:54:51 +0000

    [feature] 0 setup web static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,19 +8,22 @@ def do_deploy(archive_path):
     """ Function deploy  """
 
     env.user = 'ubuntu'
-    env.hosts = ['19.hbtn-cod.io']
+    env.hosts = ['35.243.178.160', '35.243.142.199']
     if path.exits(archive_path):
+        full_name = archive_path.split('/')[-1]
+        filename = full_name.split('.')[0]
+        path_file = "/data/".apend(filename)
 
         """ send archive """
-        value = put(archive_path, "/tmp/")
+        value = put(archive_path, "/tmp/{}".format(full_name))
         if value.failed:
             return False
 
-        filename = archive_path.split('/')[-1].split('.')[0]
-        path_file = "/data/".apend(filename)
-
         """ uncompress file """
-        run("mkdir -p /data/web_static/releases/")
+        value = run("mkdir -p /data/web_static/releases/")
+        if value.failed:
+            return False
+
         value = run(
             "tar -xzf {} -C /data/web_static/releases/".format(path_file))
         if value.failed:
@@ -31,11 +34,20 @@ def do_deploy(archive_path):
         if value.failed:
             return False
 
+        value = run("mv /data/web_static/releases/{}/web_static/* "
+            "/data/web_static/releases/{}/".format(filename, filename))
+        if value.failed:
+            return False
+
         """ softlink to new deploy """
-        run("rm /data/web_static/current")
+        value = run("rm -rf /data/web_static/current")
+        if value.failed:
+            return False
         value = run(
             "ln -sf /data/web_static/releases/{}\
                 /data/web_static/current".format(filename))
+        if value.failed:
+            return False
         return True
     else:
         return False
