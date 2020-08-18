307a395293247c98a25d1dc3cad80198f2768b6a
Fabian Andres <f4bian.andres@gmail.com>
2020-08-18 05:43:20 +0000

    [feature] 0 setup web static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,25 +8,23 @@ env.hosts = ['35.243.178.160', '35.243.142.199']
 
 
 def do_deploy(archive_path):
-    """ Function deploy  """
-
-    if path.exits(archive_path):
-
-        put(archive_path, "/tmp/")
-        full_name = archive_path.split('/')[-1]
-        name = full_name.split('.')[0]
-        final_path = "/data/web_static/releases/" + name
-        run("mkdir -p " + final_path)
-        run("tar -xzf /tmp/" + full_name + " -C " + final_path)
-        run("rm /tmp/" + full_name)
-
-        run("mv " + final_path + "/web_static/* " + final_path)
-        run("rm -rf " + final_path + "/web_static/")
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(final_path))
-
-        disconnect_all()
-
-        return True
-    else:
+    """deploy and uncompress file to server."""
+    if not exists(archive_path):
         return False
+
+    put(archive_path, "/tmp/")
+    name_of_file = archive_path.split('/')[-1]
+    name = name_of_file.split('.')[0]
+    final_path = "/data/web_static/releases/" + name
+    run("mkdir -p " + final_path)
+    run("tar -xzf /tmp/" + name_of_file + " -C " + final_path)
+    run("rm /tmp/" + name_of_file)
+
+    run("mv " + final_path + "/web_static/* " + final_path)
+    run("rm -rf " + final_path + "/web_static/")
+    run("rm -rf /data/web_static/current")
+    run("ln -s {} /data/web_static/current".format(final_path))
+
+    disconnect_all()
+
+    return True
