0839a1c308947011cb476fc72636860fa30a1ad7
julianfrancor <julianfrancor@gmail.com>
2020-08-17 23:14:12 -0500

    Uncompress file and deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -35,9 +35,56 @@ def do_pack():
     else:
         return None
 
+"""
+def do_deploy(archive_path):
+    #
+    #     script that distributes an archive to your web servers
+    #     using the function do_deploy
+
+    if path.isfile(archive_path):
+        # Upload the archive to the /tmp/ directory of the web server
+        local_path = archive_path
+        remote_path = "/tmp/"
+
+        put(archive_path, remote_path)
+
+        path_to_uncompress = "/data/web_static/releases/"
+        file_name = archive_path.split('/')[1]
+        filename_noextension = file_name.split('.')[0]
+
+        # Creating folder /data/web_static/releases/web_static_202081720267
+        sudo("mkdir -p {}{}/".format(path_to_uncompress, filename_noextension))
+
+        # Uncompress the archive to the folder "/data/web_static/releases/"
+        sudo("tar -xzf {}{} -C {}{}/".format(remote_path, file_name,
+                                             path_to_uncompress,
+                                             filename_noextension))
+
+        # Delete the archive from the web server
+        sudo('rm {}{}'.format(remote_path, file_name))
+        # move files
+        sudo("mv {}{}/web_static/* {}{}/.".format(path_to_uncompress,
+                                                  filename_noextension,
+                                                  path_to_uncompress,
+                                                  filename_noextension))
+        # Delete the symbolic link /data/web_static/current from the web server
+        sudo("rm -rf {}{}/web_static/".format(path_to_uncompress,
+                                              filename_noextension))
+        sudo('rm -rf /data/web_static/current')
+        # Create a new the symbolic link on the web server
+        # linked to the new version of your code
+        sudo("ln -s {}{}/ /data/web_static/current".format(
+            remote_path, filename_noextension))
+
+        print("New version deployed!")
+        return True
+    else:
+        return False
+"""
+
 
 def do_deploy(archive_path):
-    """deploys the web_static
+    """deploys the
     """
     if isfile(archive_path):
         pre_path = archive_path.split("/")[1]
