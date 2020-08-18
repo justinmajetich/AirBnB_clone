34e93431384fbe46260f8feb9d7153d3c7baddbd
julianfrancor <julianfrancor@gmail.com>
2020-08-17 23:21:05 -0500

    Uncompress file and deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -86,23 +86,21 @@ def do_deploy(archive_path):
 def do_deploy(archive_path):
     """deploys the
     """
-    noext = archive_path.replace(".tgz", "").split("/")[1]
-    if not path.exists(archive_path):
+    if isfile(archive_path):
+        pre_path = archive_path.split("/")[1]
+        put(archive_path, "/tmp/")
+        tmp_path = "/tmp/" + pre_path
+        releases_path = "/data/web_static/releases/" + pre_path.split(".")[0]
+        sudo("mkdir -p {:s}".format(releases_path))
+        sudo("tar -xzf {:s} -C {:s}".format(tmp_path, releases_path))
+        sudo("rm {:s}".format(tmp_path))
+        all_path_w = releases_path + "/web_static/*"
+        dictory_path = releases_path + "/web_static/"
+        sudo("mv {:s} {:s}".format(all_path_w, releases_path))
+        sudo("rm -rf {:s}".format(dictory_path))
+        sudo("rm -rf /data/web_static/current")
+        sudo("ln -s {:s} /data/web_static/current".format(releases_path))
+        print("New version deployed!")
+        return True
+    else:
         return False
-
-    try:
-        put('{}'.format(archive_path), '/tmp/')
-        archive_path = archive_path.split("/")[1]
-
-        server_dir = "/data/web_static/releases/{}".format(noext)
-        run('mkdir -p {}'.format(server_dir))
-
-        run(' tar -xzf /tmp/{} -C {}'.format(archive_path, server_dir))
-        run('rm /tmp/{}'.format(archive_path))
-        run('mv {}/web_static/* {}/'.format(server_dir, server_dir))
-        run('rm -rf {}/web_static'.format(server_dir))
-        run('unlink /data/web_static/current')
-        run('ln -s {} /data/web_static/current'.format(server_dir))
-    except Exception:
-        return False
-    return True
