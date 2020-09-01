38cf2726c3be0b9843c18249e1c2e725fe39b648
untalinfo <racso1607@gmail.com>
2020-09-01 21:30:22 +0000

    task 4 and task 5 added

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application
+"""
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    Print a str
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def holberton():
+    """
+    Print a str
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>/', strict_slashes=False)
+def is_fun(text):
+    """
+    Print a string, display “C ” followed by the value of
+    the text variable
+    """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def is_magic(text="is cool"):
+    """
+    Print a str, display “Python ”, followed by the
+    value of the text variable
+    """
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_number(n):
+    """
+    Print a str, display “n is a number” only if n
+    is an integer
+    """
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def template(n):
+    """
+    Print a str, display “n is a number” only if n
+    is an integer
+    """
+    return render_template('5-number.html', number=n)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
