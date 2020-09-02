ceffc8345174adf5e1249ea8324066cd52bd55e2
Fabian Andres <f4bian.andres@gmail.com>
2020-09-02 20:01:45 +0000

    [feature] update pep8 an upload basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -15,26 +15,29 @@ class BaseModel:
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
-    def __init__(self, *args, **kwargs):  # Pendienteee
-        """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
+    def __init__(self, *args, **kwargs):
+        """Instantiates a new model"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "updated_at" or key == "created_at":
+                    kwargs[key] = datetime.strptime(
+                        kwargs[key],
+                        '%Y-%m-%dT%H:%M:%S.%f'
+                        )
+                if key != "__class__":
+                    setattr(self, key, value)
+                if 'id' not in kwargs:
+                    self.id = str(uuid.uuid4())
+                if 'created_at' not in kwargs:
+                    self.created_at = datetime.now()
+                if 'updated_at' not in kwargs:
+                    self.updated_at = self.created_at
+                else:
+                    self.updated_at = datetime.now()
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-
-        else:
-            if "__class__" in kwargs.keys():
-                kwargs['updated_at'] = datetime.strptime(
-                    kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-                kwargs['created_at'] = datetime.strptime(
-                    kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
-                del kwargs['__class__']
-                self.__dict__.update(kwargs)
-
-            for key in kwargs.keys():
-                if key not in self.__dict__.keys():
-                    setattr(self, key, kwargs[key])
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -56,6 +59,8 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        dictionary.pop("_sa_instance_state", None)
+
         return dictionary
 
     def delete(self):
--- models/state.py
+++ models/state.py
@@ -7,6 +7,7 @@ import os
 import models
 from models.city import City
 
+
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
