b5edf5eb2986c5372ecd2feb5f1ab28c85b6b5da
Fabian Andres <f4bian.andres@gmail.com>
2020-09-02 16:17:41 +0000

    [feature] script that starts a Flask web application, 4 number route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application  """
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_route():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb_route():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_route(text):
+    text = text.replace('_', ' ')
+    return 'C %s' % escape(text)
+
+
+@app.route('/python/', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def python_route(text):
+    text = text.replace('_', ' ')
+    return 'python %s' % escape(text)
+
+
+@app.route('/number/<int:n>')
+def number_route(n):
+    return '%s is a number' % escape(n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
