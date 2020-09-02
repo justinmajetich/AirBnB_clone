6e9e2975e9d61c538d1f60c118e9ede416bddadd
Fabian Andres <f4bian.andres@gmail.com>
2020-09-02 03:40:52 +0000

    [featude] Hello Flask  script that starts a Flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,7 +9,7 @@ app.url_map.strict_slashes = False
 
 @app.route('/')
 def hello():
-    return 'Hello, HBNB!'
+    return 'Hello HBNB!'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
