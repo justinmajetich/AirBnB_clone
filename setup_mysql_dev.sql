29e34db1b1a5b278b9c41f6774e6565cf6182be5
daniel <2057@holbertonschool.com>
2020-12-14 19:05:41 -0500

    fix tasks 3 and 5 with comments

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,4 @@
+--Create hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS "hbnb_dev"@"localhost" IDENTIFIED BY "hbnb_dev_pwd";
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO "hbnb_dev"@"localhost";
