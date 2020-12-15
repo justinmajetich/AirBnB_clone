4f5d510e61d8bd09293679a240daf88f3d9490cc
Victor Zuluaga <zrvictor@outlook.com>
2020-12-15 09:49:27 -0500

    setup my sql test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,9 @@
+-- Write a script that prepares a MySQL server for the project
+-- Create a DataBase hbnb_dev_db
+-- Create a User hbnb_dev
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
