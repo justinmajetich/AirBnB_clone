7dc7723cdfb9ac107faf8bfa478c62e9981f9fe6
onsjannet <onsjannet@gmail.com>
2020-12-15 18:59:58 +0100

    correcting output

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,6 @@
--- Creats a MySQL server with a user hbnb_dev
+-- Creats a MySQL server with a user test_dev
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON 'hbnb_dev_db' . * TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON 'performance_schema'. * TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
