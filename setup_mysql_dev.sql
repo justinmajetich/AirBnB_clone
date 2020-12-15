f32073104d3936bf69e2f0cebddad24304705081
daniela2001-png <dmbmora@outlook.com>
2020-12-15 14:23:32 -0500

    [new] Adding changes to file sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
--- script that prepares a MySQL server for the project
+-- script that prepares a MySQL server for the project htbn
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
