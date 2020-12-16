c00d86fd4aaa2b4acbf8dd0c7acf43d1c7d1d3e8
onsjannet <onsjannet@gmail.com>
2020-12-16 18:46:23 +0100

    added file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,14 +10,14 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        all_objects = {} 
         if cls is None:
             return FileStorage.__objects
         else:
-            for key, values in FileStorage.__objects.items():
-                if cls.__name__ == values.__class__.__name__:
-                    all_objects[key] = values
-            return all_objects
+            new = {}
+            for key, val in FileStorage.__objects.items():
+                if val.__class__ == cls:
+                    new[key] = val
+            return new
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -43,21 +43,21 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+            }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
-        """Deletes object from storage dictionary"""
+        """ Deletes object from storage dictionary """
         unwanted = obj.__class__.__name__ + "." + obj.id
         if unwanted in FileStorage.__objects.keys():
             del(FileStorage.__objects[unwanted])
