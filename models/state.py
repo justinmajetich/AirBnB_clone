8872a20bef6a01556c5507ec33459e4582d01f49
MarcosPimienta <1676@holbertonschool.com>
2020-12-16 22:45:28 +0000

    city and state updated

--- models/state.py
+++ models/state.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.storage import all
+from models.city import City
+from sqlalchemy import Column, String, ForeignKey
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state', cascade="all, delete-orphan")
+
+    @property
+    def cities(self):
+        list_cities = []
+        for cities in storage.all(City).values():
+            if city.state_id == self.id:
+                list_cities.append(cities)
+        return (list_cities)
