bbad065c8a2763c7d1877be71b99aea677d873f6
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-12-16 20:18:51 +0100

    my file task 6

--- models/state.py
+++ models/state.py
@@ -3,6 +3,19 @@
 from models.base_model import BaseModel
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City",  backref="state", cascade="delete")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Get a list of all related City objects."""
+            city_list = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
+
