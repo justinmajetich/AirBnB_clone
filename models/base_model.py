5b360e241a4a82a72e4d06b055ad42686796f3b2
DamnDanielV <1821@holbertonschool.com>
2020-12-15 21:24:48 -0500

    updated with sqlalchemy

--- models/base_model.py
+++ models/base_model.py
@@ -3,21 +3,28 @@
 import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
-
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.types import DateTime
 
 Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
+        """Instantiates a new model"""
+        if not kwargs or ("updated_at" not in kwargs and
+                          "created_at" not in kwargs):
             from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            if kwargs:
+                self.__dict__.update(kwargs)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -45,4 +52,6 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if '_sa_instance_state' in dictionary:
+            dictionary.pop('_sa_instance_state', None)
         return dictionary
