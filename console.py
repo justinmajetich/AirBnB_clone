037103c9d73c36cbe2c997f52954dd49a04f6a5a
ronald0204 <ronaldaguirre0204@gmail.com>
2020-12-16 14:34:57 -0500

    Hot fix

--- console.py
+++ console.py
@@ -222,11 +222,11 @@ class HBNBCommand(cmd.Cmd):
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage.all.items():
+            for k, v in storage.all().items():
                 if k.split('.')[0] == args:
                     print_list.append(str(v))
         else:
-            for k, v in storage.all.items():
+            for k, v in storage.all().items():
                 print_list.append(str(v))
         print(print_list)
 
@@ -238,7 +238,7 @@ class HBNBCommand(cmd.Cmd):
     def do_count(self, args):
         """Count current number of class instances"""
         count = 0
-        for k, v in storage.all.items():
+        for k, v in storage.all().items():
             if args == k.split('.')[0]:
                 count += 1
         print(count)
--- models/city.py
+++ models/city.py
@@ -15,5 +15,3 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
     name = Column(String(128), nullable=False)
-    # One2many relationship to places
-    places = relationship("Place", backref="cities")
