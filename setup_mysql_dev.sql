2d8283560acacebc64c0a2a7565e466f75ba5c40
GreenCyberNinja <1824@holbertonschool.com>
2020-12-16 22:05:14 +0000

    dev completed

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,9 @@
---sql script to create database
-CREATE DATABASE IF NOT EXIST hbnb_dev_db
-CREATE USER IF NOT EXIST 'hbnb_dev'@'localhost' IDENTIFIED BY "hbnb_dev"
-GRANT ALL ON hbnb_dev_db TO 'hbnb_dev'@'localhost'
-GRANT SELECT ON performance_schema TO 'hbnb_dev'@'localhost'
+-- sql script to create database
 
+CREATE DATABASE If NOT EXISTS hbnb_dev_db;
+DROP USER if exists 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY "hbnb_dev";
+GRANT USAGE on *.* to 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
