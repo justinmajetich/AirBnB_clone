df438ffcdf2a42450067879b21570a4c81812c9f
RicardoArquimedes <richardcqt28@gmail.com>
2020-12-16 15:30:13 -0500

    hot fix

--- models/user.py
+++ models/user.py
@@ -2,7 +2,8 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-
+from os import getenv
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
