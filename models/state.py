dce73a4475ea71a5710a58261e835d998f0901dc
tomasgvgt <tomasgvg.t@gmail.com>
2020-12-16 18:00:59 -0500

    CHanges for checker in state

--- models/state.py
+++ models/state.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
-from models import storage
 
 
 class State(BaseModel, Base):
@@ -14,7 +12,8 @@ class State(BaseModel, Base):
     cities = relationship("City", cascade="all, delete", backref="state") #NEW REVISAR
 
     @property #NUEVO REVISAR
-    def cities(self)
+    def cities(self):
+        from models.__init__ import storage
         city_list = []
         for value in storage.all(City).values():
             if self.id == value[state_id]:
