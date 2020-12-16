973e913b5dbcca00677d4f5c725ba16ea3150650
RicardoArquimedes <richardcqt28@gmail.com>
2020-12-15 20:44:56 -0500

    reverting initial changes

--- models/city.py
+++ models/city.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from models.base_model import BaseModel
 
 
-class City(BaseModel, Base):
+class City(BaseModel):
     """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('state.id'), nullable=False)
+    state_id = ""
+    name = ""
--- models/state.py
+++ models/state.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from models.city import City
-from sqlalchemy import Column, Integer, String, ForeignKey
+from models.base_model import BaseModel
 
 
-class State(BaseModel, Base):
+class State(BaseModel):
     """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    # cities = relationship("City")
+    name = ""
