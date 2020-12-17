93e33d0df0cf852b06de66ead9e2c52438914cd0
steffanynaranjov <steffany.naranjo@gmail.com>
2020-12-17 15:48:35 +0000

    update places correct

--- models/place.py
+++ models/place.py
@@ -9,13 +9,6 @@ import models
 from os import getenv
 from sqlalchemy import Table
 
-place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id', String(60), ForeignKey('places.id'),
-                             primary_key=True, nullable=False),
-                      Column('amenity_id', String(60),
-                             ForeignKey('amenities.id'),
-                             primary_key=True, nullable=False))
-
 
 class Place(BaseModel, Base):
     """This is the class for Place
