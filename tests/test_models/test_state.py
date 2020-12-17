4f985542f214c50f0b8caf384bab2854dc619e84
steffanynaranjov <steffany.naranjo@gmail.com>
2020-12-17 22:54:23 +0000

    some test for state

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+"""test for state"""
+import unittest
+import os
 from models.state import State
+from models.base_model import BaseModel
+import pep8
 
 
-class test_state(test_basemodel):
-    """ """
+class TestState(unittest.TestCase):
+    """this will test the State class"""
 
     def __init__(self, *args, **kwargs):
         """ """
@@ -17,3 +20,29 @@ class test_state(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.name), str)
+
+    def test_pep8_Review(self):
+        """Tests pep8 style"""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/state.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
+
+    def test_checking_for_docstring_State(self):
+        """checking for docstrings"""
+        self.assertIsNotNone(State.__doc__)
+
+    def test_attributes_State(self):
+        """chekcing if State have attributes"""
+        self.assertTrue('id' in self.state.__dict__)
+        self.assertTrue('created_at' in self.state.__dict__)
+        self.assertTrue('updated_at' in self.state.__dict__)
+        self.assertTrue('name' in self.state.__dict__)
+
+    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") == "db", "Test_DB")
+    def test_save(self):
+        """Test save"""
+        self.state.save()
+        self.assertFalse(self.state.created_at == self.state.updated_at)
+
+if __name__ == "__main__":
+    unittest.main()
