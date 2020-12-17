0899873cb1c3eb3fb1459a1c9a9a5118c045368e
rogerrendons <1584@holbertonschool.com>
2020-12-17 12:33:40 -0500

    Repair amenity and place

--- models/amenity.py
+++ models/amenity.py
@@ -1,21 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-from models.place import Place
-import os
+from models.base_model import BaseModel
 
 
-
-class Amenity(BaseModel, Base):
-    """ Class Amenity """
-    __tablename__ = amenities
-
-    name = Column(String(128), nullable=False)
-    place_amenities = ("Place", secondary=place_amenity) 
-
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        place_amenities = relationship("Place", secondary=Place.place_amenity,
-                                       back_populates="amenities")
+class Amenity(BaseModel):
+    name = ""
