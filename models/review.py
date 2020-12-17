9edc4034db984fdd837734535bae13179f99b8c8
cristian-bedoya <crisbed321@gmail.com>
2020-12-17 23:08:02 +0000

    fix base inherit

--- models/review.py
+++ models/review.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel
+""" State Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy.orm import relationship
 
 
-class Review(BaseModel):
-    """ Review class to store review information """
+class Amenity(BaseModel, Base):
+    """ State amenity"""
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
 
-    __tablename__ = "reviews"
-
-    text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity")
