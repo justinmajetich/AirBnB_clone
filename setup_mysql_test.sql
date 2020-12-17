bbc5f174f44443ec0be7cde491176bc32fddb13e
Andrescortes23 <andresccortes23@icloud.com>
2020-12-17 19:00:32 +0000

    Correction

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,6 @@
 -- Comments
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
