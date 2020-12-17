7adcec3b296d669dc3d205b26b39f9e0c6447350
BigOBasheer <838@holbertonschool.com>
2020-12-17 04:32:14 +0000

    Add Task 7

--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
 
 
 class User(BaseModel):
@@ -9,3 +10,9 @@ class User(BaseModel):
     password = ''
     first_name = ''
     last_name = ''
+
+    __tablename__ = users
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
