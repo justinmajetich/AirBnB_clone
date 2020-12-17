797800fda17b7d2f24245bf4ac77d9db465a1ecd
jossvega <jossvega103@gmail.com>
2020-12-16 20:54:47 -0500

    Task 9- DBStorage - Review

--- models/place.py
+++ models/place.py
@@ -4,6 +4,9 @@ from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 from os import getenv
 from sqlalchemy import Column, Float, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.review import Review
+import models
 
 
 class Place(BaseModel, Base):
@@ -22,3 +25,16 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
+    reviews = relationship(
+        "Review", backref="place", cascade="all, delete-orphan")
+
+    if getenv("HBNB_TYPE_STORAGE") == "file":
+        @property
+        def reviews(self):
+            """ Get a list of all related Review current Place"""
+            reviews = models.storage.all(Review)
+            listReviews = []
+            for review in reviews.items():
+                if review.place_id == self.id:
+                    listReviews.append(review)
+            return listReviews
--- models/user.py
+++ models/user.py
@@ -13,4 +13,7 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-    places = relationship("Place", backref="user")
+    places = relationship(
+        "Place", backref="user", cascade="all, delete-orphan")
+    reviews = relationship(
+        "Review", backref="user", cascade="all, delete-orphan")
