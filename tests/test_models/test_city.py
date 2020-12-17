70ffea7e5f55f0f4681509e41deaf140361c9356
steffanynaranjov <steffany.naranjo@gmail.com>
2020-12-17 22:58:11 +0000

    test for city

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """ """
-from tests.test_models.test_base_model import test_basemodel
+import unittest
+import os
 from models.city import City
+from models.base_model import BaseModel
+import pep8
 
 
 class test_City(test_basemodel):
-    """ """
+    """Test for city """
 
     def __init__(self, *args, **kwargs):
         """ """
@@ -22,3 +25,31 @@ class test_City(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.name), str)
+
+    def test_pep8_City(self):
+        """Tests pep8 style"""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/city.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
+
+    def test_checking_for_docstring_City(self):
+        """checking for docstrings"""
+        self.assertIsNotNone(City.__doc__)
+
+    def test_attributes_City(self):
+        """chekcing if City have attributes"""
+        self.assertTrue('id' in self.city.__dict__)
+        self.assertTrue('created_at' in self.city.__dict__)
+        self.assertTrue('updated_at' in self.city.__dict__)
+        self.assertTrue('state_id' in self.city.__dict__)
+        self.assertTrue('name' in self.city.__dict__)
+
+    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") == "db", "Test_DB")
+    def test_save(self):
+        """Test save"""
+        self.city.save()
+        self.assertFalse(self.city.created_at == self.city.updated_at)
+
+
+if __name__ == "__main__":
+    unittest.main()
