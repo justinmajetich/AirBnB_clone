3de7a6900619db4777ebea06316e09241242d69f
juanjts <1843@holbertonschool.com>
2020-12-17 23:14:53 +0000

    amty and place

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""This is the amenity class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.place import Place
+import os
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+
+    """ This is Amenity class"""
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        place_amenities = relationship("Place", secondary=Place.place_amenity,
+                                       back_populates="amenities")
