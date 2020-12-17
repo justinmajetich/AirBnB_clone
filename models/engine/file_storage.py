8def51f4a0ffc7598dd98227cf6530737fea7038
Andrescortes23 <andresccortes23@icloud.com>
2020-12-17 21:48:28 +0000

    All problem

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,16 +10,15 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        if cls is not None:
-            if type(cls) == str:
-                cls = eval(cls)
+        if cls is None:
+            return FileStorage.__objects
+        else:
             new = {}
             for key, value in self.__objects.items():
-                if type(value) == cls:
+                clsval = type(value)
+                if cls == clsval:
                     new[key] = value
             return new
-        else:
-            return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
