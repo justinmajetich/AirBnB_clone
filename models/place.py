aa9f15dc1665fd56c51fc8fb391d856aca773268
rogerrendons <1584@holbertonschool.com>
2020-12-17 12:24:07 -0500

    deleted file probe console

--- models/place.py
+++ models/place.py
@@ -4,10 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Integer, Float
-import os
-from sqlalchemy import Table
-from models.review import Review
-import models.amenity
+
 
 
 class Place(BaseModel, Base):
