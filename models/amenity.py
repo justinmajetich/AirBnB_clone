bcc34b88727ef1395925ff974de29bf0b3087ef1
tomasgvgt <tomasgvg.t@gmail.com>
2020-12-17 06:56:16 -0500

    amenity update

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from slqalchemy import Column, Integer, String, ForegnKey
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    place_amenities = 
+
