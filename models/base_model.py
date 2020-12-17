a4c5967f760b36f91bb785093591f76906112261
Andrescortes23 <andresccortes23@icloud.com>
2020-12-17 16:37:56 +0000

    base

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,7 @@ Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
-    id = Column(string(60), nullable=False, primary_key=True)
+    id = Column(String(60), nullable=False, primary_key=True)
     created_at = Column(DateTime, nullable=False,
                         default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False,
