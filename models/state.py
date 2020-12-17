42e2ebf1ee8e1a9209be62c9603e0ddf1f09dd2f
steffanynaranjov <steffany.naranjo@gmail.com>
2020-12-17 00:53:20 +0000

    state connected by db

--- models/state.py
+++ models/state.py
@@ -20,8 +20,10 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
 
-    cities = relationship("City", backref="state",
-                          cascade="all, delete")
+    if getenv('HBNB_TYPE_STORAGE') == "db":
+        cities = relationship("City",
+                              backref="state",
+                              cascade="all, delete")
 
     @property
     def cities(self):
