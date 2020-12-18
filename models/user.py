a7d3e366c5002bfc21ea7cd11d27466fbb510f17
JHS1790 <1790@holbertonschool.com>
2020-12-18 03:17:08 +0000

    updated documentation for user

--- models/user.py
+++ models/user.py
@@ -4,7 +4,13 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """This class defines a user by various attributes"""
+    """This class defines a user
+		Attributes:
+			email -> email address
+			password -> don't look silly
+			first_name -> John
+			last_name -> Smith
+	"""
     email = ''
     password = ''
     first_name = ''
