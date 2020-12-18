80123cbe020a2ad40d7e30c8e2a3ebd9bbbbd5ca
JHS1790 <1790@holbertonschool.com>
2020-12-18 03:21:50 +0000

    trying to get git to work

--- models/user.py
+++ models/user.py
@@ -5,20 +5,6 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-<<<<<<< HEAD
-class User(BaseModel):
-    """This class defines a user
-		Attributes:
-			email -> email address
-			password -> don't look silly
-			first_name -> John
-			last_name -> Smith
-	"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
-=======
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
@@ -32,4 +18,3 @@ class User(BaseModel, Base):
     reviews = relationship('Review',
                            cascade='all,delete',
                            backref='user')
->>>>>>> 7a38b0fde34d61e1bb21423f8dfa398ec535d750
