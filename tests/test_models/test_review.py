7f536cbc0d8586d0c74ab0b2420ee906f02aeb28
daniel <2057@holbertonschool.com>
2020-12-17 19:34:23 -0500

    tests models

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -2,6 +2,7 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
+import pep8
 
 
 class test_review(test_basemodel):
@@ -16,14 +17,28 @@ class test_review(test_basemodel):
     def test_place_id(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.place_id), str)
+        self.assertEqual(type(new), self.value)
 
     def test_user_id(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.user_id), str)
+        self.assertEqual(type(new), self.value)
 
     def test_text(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.text), str)
+        self.assertEqual(type(new), self.value)
+
+    def test_pep8_conformance_review(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/review.py'])
+        self.assertEqual(result.total_errors, 0, "Found style errors")
+
+    def test_class(self):
+        rev1 = Review()
+        self.assertEqual(rev1.__class__.__name__, "Review")
+
+    def test_father(self):
+        rev1 = Review()
+        self.assertTrue(issubclass(rev1.__class__, BaseModel))
