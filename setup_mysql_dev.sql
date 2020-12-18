a3ecde7ebe81008502dbf6daaa489fb31ad1ec46
MIlenaMontoya <1852@holbertonschool.com>
2020-12-17 22:28:10 -0500

    changes files

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,12 @@
--- Write a script that prepares a MySQL server for the project
--- Create a DataBase hbnb_dev_db
--- Create a User hbnb_dev
+-- Create firts DATABASE
+-- Database hbtn_0c_0
+-- create a new user
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
+
+-- with full privileges on localhost
+-- should have SELECT privilege on the database performance_schema
+
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO `hbnb_dev`@`localhost`;
+GRANT SELECT ON `performance_schema`.* TO `hbnb_dev`@`localhost`;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,9 @@
--- Write a script that prepares a MySQL server for the project
--- Create a DataBase hbnb_dev_db
--- Create a User hbnb_dev
+-- Set ALL privileges on the database `hbnb_dev_db` to the `hbnb_dev` user.
+-- Set SELECT privileges on the database `performance_schema` to the `hbnb_dev` user.
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+DROP USER IF EXISTS 'hbnb_test'@'localhost';
+CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
