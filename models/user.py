aa2e7b64a4c8f994c6a4780b60edcc50ae292b55
Andrescortes23 <andresccortes23@icloud.com>
2020-12-18 01:36:08 +0000

    user

--- models/user.py
+++ models/user.py
@@ -11,7 +11,7 @@ class User(BaseModel, Base):
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
     places = relationship('Place', cascade="all, delete", backref='user')
     reviews = relationship('Review', cascade="all, delete", backref='user')
