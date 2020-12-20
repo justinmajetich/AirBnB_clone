b04bd9d3b61f2e97a919ac03d0b96b8092f2b6d8
didierrevelo <1829@holbertonschool.com>
2020-12-20 11:02:38 -0500

    add correction

--- models/__init__.py
+++ models/__init__.py
@@ -7,6 +7,6 @@ if getenv('HBNB_TYPE_STORAGE') == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
-    from models.engine.file_storage import FileStorage
+    from models.engine.file_storage import FileStorage          
     storage = FileStorage()
 storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime
+import models
 
 Base = declarative_base()
 
@@ -19,20 +20,22 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
             for key, value in kwargs.items():
-                setattr(self, key, value)
-
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+                if key in ["created_at", "updated_at"]:
+                    val = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, val)
+                elif key != "__class__":
+                    setattr(self, key, value)
+        if 'id' not in kwargs:
+            setattr(self, 'id', str(uuid.uuid4()))
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.now()
+            if 'updated_at' not in kwargs:
+                self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
