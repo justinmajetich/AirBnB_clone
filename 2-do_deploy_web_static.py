6f074775a03bca07389aa0632d5edf2eacfb3a8e
LizethVictoria20 <lizvictoria754@gmail.com>
2020-12-22 13:34:38 +0000

    Deploy archive!

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Deploy archive """
+from fabric.api import *
+from os.path import exists
+from fabric.network import disconnect_all
+env.user = 'ubuntu'
+env.hosts = ['34.74.39.16', '35.196.57.229']
+
+
+def do_deploy(archive_path):
+    """deploy and uncompress file to server."""
+    if not exists(archive_path):
+        return False
+
+    put(archive_path, "/tmp/")
+    name_of_file = archive_path.split('/')[-1]
+    name = name_of_file.split('.')[0]
+    final_path = "/data/web_static/releases/" + name
+    run("mkdir -p " + final_path)
+    run("tar -xzf /tmp/" + name_of_file + " -C " + final_path)
+    run("rm /tmp/" + name_of_file)
+
+    run("mv " + final_path + "/web_static/* " + final_path)
+    run("rm -rf " + final_path + "/web_static/")
+    run("rm -rf /data/web_static/current")
+    run("ln -s {} /data/web_static/current".format(final_path))
+
+    disconnect_all()
+
+    return True
