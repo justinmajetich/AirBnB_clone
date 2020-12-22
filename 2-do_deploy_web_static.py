1bc9c1b07c3ba00b1eba65ab52c7082df7a1b3ff
rogerrendons <1584@holbertonschool.com>
2020-12-22 17:40:34 -0500

    task 2.1

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,33 +3,34 @@
 web servers, using the function do_deploy: """
 
 from fabric.api import *
-from fabric.operations import *
-from os import path
 import os.path
-import os
-
+from os import path
 
 env.user = ['ubuntu']
 env.hosts = ['34.75.252.164', '35.237.149.147']
 
 
 def do_deploy(archive_path):
-    """ Prototype do_deploy """
+    """ func to distribute archive """
+    """ run("do_deploy") """
 
     if (path.exists(archive_path) is not True):
         return False
-    Directory = archive_path.replace(".tgz", "")
-    put("archive_path", "/tmp/{}".format(archive_path))
-    run("mkdir -p /data/web_static/releases/{}/".format(Directory))
+
+    archive_p = archive_path
+    archive_folder = archive_path.replace(".tgz", "")
+
+    put("archive_path", "/tmp/{}".format(archive_p))
+    run("mkdir -p /data/web_static/releases/{}/".format(archive_folder))
     run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-        .format(archive_path, Directory))
+        .format(archive_p, archive_folder))
     run("rm /tmp/{}/".format(archive_path))
     run("mv /data/web_static/releases/{}/* /data/web_static/releases/{}/"
-        .format(Directory, Directory))
+        .format(archive_folder, archive_folder))
     run("rm -rf /data/web_static/releases/{}/web_static"
-        .format(Directory))
+        .format(archive_folder))
     run("rm -rf /data/web_static/current")
     run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-        .format(Directory))
+        .format(archive_folder))
     print("New version deployed!")
     return True
