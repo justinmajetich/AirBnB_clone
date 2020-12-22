503859ebc3df051ba37bc9aefc03fcd4d43721fd
rogerrendons <1584@holbertonschool.com>
2020-12-22 18:16:32 -0500

    task 2.2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,34 +3,35 @@
 web servers, using the function do_deploy: """
 
 from fabric.api import *
-import os.path
 from os import path
 
-env.user = ['ubuntu']
-env.hosts = ['34.75.252.164', '35.237.149.147']
+env.hosts = ['35.237.149.147', '34.75.252.164']
 
 
 def do_deploy(archive_path):
-    """ func to distribute archive """
-    """ run("do_deploy") """
-
-    if (path.exists(archive_path) is not True):
+    """Deploy in the servers"""
+    if not path.exists(archive_path):
         return False
 
-    archive_p = archive_path
-    archive_folder = archive_path.replace(".tgz", "")
+    ufile = put(archive_path, '/tmp/')
+    if ufile.failed:
+        return False
 
-    put("archive_path", "/tmp/{}".format(archive_p))
-    run("mkdir -p /data/web_static/releases/{}/".format(archive_folder))
-    run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-        .format(archive_p, archive_folder))
-    run("rm /tmp/{}/".format(archive_path))
-    run("mv /data/web_static/releases/{}/* /data/web_static/releases/{}/"
-        .format(archive_folder, archive_folder))
-    run("rm -rf /data/web_static/releases/{}/web_static"
-        .format(archive_folder))
+    complete = archive_path.split("/")[-1]
+    fileok = complete.split(".")[0]
+
+    run("mkdir -p /data/web_static/releases/{}/".format(fileok))
+    run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".
+        format(fileok, fileok))
+
+    run("rm /tmp/{}.tgz".format(fileok))
+
+    run("mv /data/web_static/releases/{}/web_static/* "
+        "/data/web_static/releases/{}/".format(fileok, fileok))
+    run("rm -rf /data/web_static/releases/{}/web_static".
+        format(fileok))
     run("rm -rf /data/web_static/current")
-    run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-        .format(archive_folder))
+    run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+        format(fileok))
     print("New version deployed!")
     return True
