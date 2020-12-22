e7e3d1bc14d2cdfc243fd56277f571326e46ae1a
cristian-bedoya <crisbed321@gmail.com>
2020-12-22 21:31:56 +0000

    change permission to executable

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,7 +2,6 @@
 # sets up your web servers for the deployment of web_static
 apt-get -y update
 apt-get -y install nginx
-ufw allow 'Nginx HTTP'
 mkdir -p /data/web_static/
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
