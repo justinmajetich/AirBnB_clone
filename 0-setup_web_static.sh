4d0295be9cc801e9d3798db546e33afb4415cfdf
tomasgvgt <tomasgvg.t@gmail.com>
2020-12-23 10:35:13 -0500

    0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 sudo apt-get install -y nginx
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-sudo echo "Holberton School" > /data/web_static/releases/test/index.html
+echo "Holberton School" > /data/web_static/releases/test/index.html
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
