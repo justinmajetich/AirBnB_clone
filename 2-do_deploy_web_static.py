fd41ea7275ffdfab900c3caae82c4f115b7fcdc4
rogerrendons <1584@holbertonschool.com>
2020-12-23 08:21:53 -0500

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,37 +1,28 @@
 #!/usr/bin/python3
-""" Fabric script, distributes an archive to your
-web servers, using the function do_deploy: """
-
-from fabric.api import *
-from os import path
+"""Fabric script that distributes an archive to your web servers"""
+from fabric.api import run, put, env
+from os.path import exists
 
 env.hosts = ['34.75.252.164', '35.237.149.147']
 
 
 def do_deploy(archive_path):
-    """Deploy in the servers"""
-    if not path.exists(archive_path):
-        return False
-
-    ufile = put(archive_path, '/tmp/')
-    if ufile.failed:
-        return False
-
-    complete = archive_path.split("/")[-1]
-    fileok = complete.split(".")[0]
-
-    run("mkdir -p /data/web_static/releases/{}/".format(fileok))
-    run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".
-        format(fileok, fileok))
-
-    run("rm /tmp/{}.tgz".format(fileok))
-
-    run("mv /data/web_static/releases/{}/web_static/* "
-        "/data/web_static/releases/{}/".format(fileok, fileok))
-    run("rm -rf /data/web_static/releases/{}/web_static".
-        format(fileok))
-    run("rm -rf /data/web_static/current")
-    run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-        format(fileok))
-    print("New version deployed!")
-    return True
+    """ Fabric script that distributes an archive to your web servers"""
+    if exists(archive_path) is True:
+        try:
+            filename = archive_path.split('/')[-1]
+            no_ex = filename.split('.')[0]
+            put(archive_path, '/tmp/')
+            foldername = "/data/web_static/releases/" + no_ex
+            run("mkdir -p {}/".format(foldername))
+            run("tar -xzf /tmp/{} -C {}/".format(filename, foldername))
+            run("rm  /tmp/{}".format(filename))
+            run('mv {}/web_static/* {}/'.format(foldername, foldername))
+            run("rm -rf {}/web_static".format(foldername))
+            run("rm -rf /data/web_static/current")
+            run("ln -s {}/\
+                /data/web_static/current".format(foldername))
+            return True
+        except:
+            return False
+    return False
