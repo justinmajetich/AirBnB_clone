c9c0aca317d84d49322eb04024a2314dc7a349a2
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-12-27 01:35:13 +0000

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -12,15 +12,16 @@ def do_pack():
     that fenerates a .tgz archive from the contents of the web_static folder
     """
     dt = datetime.utcnow()
-    file = "versions/web_statis_{}{}{}{}{}{}.tgz".fotmat(dt.year,
+    file = "versions/web_statis_{}{}{}{}{}{}.tgz".format(dt.year,
                                                          dt.month,
                                                          dt.day,
                                                          dt.hour,
                                                          dt.minute,
                                                          dt.second)
-    if os.path.isdir("versions") is false:
+    if os.path.isdir("versions") is False:
         if local("mkdir -p versions").failed is True:
             return None
     if local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
     return file
+
