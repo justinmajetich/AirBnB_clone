f4fd1806e46f137eb558ad201b6ce7a2f95e9075
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-12-27 00:59:48 +0000

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-# Fabric script that generates a .tgz archive from the contents of the web_static
-# folder of your AirBnB Colne repo, using the function do_pack
+# Fabric script that generates a .tgz archive from the contents
+# of the web_static folder of your AirBnB Colne repo,
+# using the function do_pack
 import os.path
 from datetime import datetime
 from fabric.api import local
 
 
 def do_pack():
-	"""
-	that fenerates a .tgz archive from the contents of the web_static folder
-	"""
-	dt = datetime.utchnow()
-	file = "versions/web_statis_{}{}{}{}{}{}.tgz".fotmat(dt.year,
-							     dt.month,
-							     dt.day,
-							     dt.hour,
-							     dt.minute,
-  							     dt.second)
-	if os.path.isdir("versions") is false:
-		if local("mkdir -p versions").failed is True:
-			return None
-	if local("tar - cvzf {} web_static".format(file)).failed is True:
-		return None
-	return file
-
+    """
+    that fenerates a .tgz archive from the contents of the web_static folder
+    """
+    dt = datetime.utchnow()
+    file = "versions/web_statis_{}{}{}{}{}{}.tgz".fotmat(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    if os.path.isdir("versions") is false:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar - cvzf {} web_static".format(file)).failed is True:
+        return None
+    return file
