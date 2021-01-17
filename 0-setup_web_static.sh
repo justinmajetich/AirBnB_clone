d3519bb2d07d6acf26ec1d75156dccbb26b86afe
Juaco121 <juanfernandogranada@gmail.com>
2021-01-17 17:27:02 -0500

    Fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-<<<<<<< HEAD
-# Sets up the web servers for the deployment of web_static
+# Sets up the web servers for the deployment of web_static o
 
 # Install NGINX
 sudo apt-get -y update
@@ -25,23 +24,3 @@ content="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\
 sudo sed -i "37i\ $content" /etc/nginx/sites-enabled/default
 sudo service nginx reload
 sudo service nginx restart
-=======
-#web servers for the deployment 
-apt-get -y update
-apt-get -y install nginx
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Test Code
-  </body>
-</html>" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test /data/web_static/current
-chown -R ubuntu:ubuntu /data
-sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
-service nginx restart
-exit 0
->>>>>>> 76c50d006b6549abb644e7e6a4e1f8f89ef3866e
