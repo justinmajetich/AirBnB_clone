76b649bf9fae4c9419be6a0b79f72e1b3400f0f8
LizethVictoria20 <lizvictoria754@gmail.com>
2021-01-19 17:45:14 +0000

    Update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""Hello Flask!"""
-
+""" Hello Flask!  """
 from flask import Flask
 
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
-def hello_world():
-    """Hello Holberton"""
+@app.route('/')
+def hello_route():
     return 'Hello HBNB!'
 
-if __name__ == '__main__':
-    """Initial"""
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
