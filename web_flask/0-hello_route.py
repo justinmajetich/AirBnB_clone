b6d758be6d85b5de4663bed9fea9f6f2366072ff
LizethVictoria20 <lizvictoria754@gmail.com>
2021-01-19 12:47:42 +0000

    Hello Flask!

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+  """Hello Word"""
+  return 'Hello HBNB!'
+
+if __name__ == '__main__':
+  """Main"""
+  app.run(host='0.0.0.0', port=5000)
