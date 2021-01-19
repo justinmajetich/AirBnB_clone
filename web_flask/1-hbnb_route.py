616def7f62310ac27977b69141bad276b9c2f379
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-01-19 16:09:23 +0000

    HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''Script that starts a Flask web application'''
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    return "HBNB"
