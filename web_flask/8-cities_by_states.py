23c5861ba9c31c161aa90ccde8f14591530f72c7
rogerrendons <1584@holbertonschool.com>
2021-01-20 17:44:43 -0500

    Task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,8 +3,7 @@
 from flask import Flask
 from flask import render_template
 from models import storage
-from models.state import State
-from models.state import City
+
 
 app = Flask(__name__)
 
@@ -15,11 +14,11 @@ def teardown_session(self):
     storage.close()
 
 
-@app.route("/cities_by_states", strict_slashes=False, methods=["GET", "POST"])
-def state_list():
-    """ route to display a HTML page """
-    states = storage.all(State).values()
-    return render_template('8-cities_by_states.html', states=states)
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """ Display a HTML page: showing the cities by states"""
+    return render_template('8-cities_by_states.html',
+                           states=storage.all('State').values())
 
 
 if __name__ == "__main__":
