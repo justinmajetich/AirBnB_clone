159d8e0463efab1e3c15a14cef0c7fa61a0e4aa1
rogerrendons <1584@holbertonschool.com>
2021-01-20 21:55:15 -0500

    Task 10.1

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,34 +1,41 @@
 #!/usr/bin/python3
 """Start Flask application"""
-
+from flask import Flask, render_template
 from models import storage
-from flask import Flask
-from flask import render_template
+
 
 app = Flask(__name__)
 
 
 @app.route('/states', strict_slashes=False)
-@app.route('/states/<id>', strict_slashes=False)
-def states_id(id=None):
+def cities_by_state():
     """ displays a state with specified id  """
     states = storage.all('State')
-    if id:
-        key = 'State.' + id
-        if key in states:
-            states = states[key]
-        else:
-            states = None
-    else:
-        states = states.values()
-    return render_template('9-states.html', states=states,
-                           id=id)
+    state_list = [v for v in states.values()]
+    return render_template('9-states.html', states=state_list)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def state_by_id(id):
+    """ displays a state with specified id  """
+    state_found = None
+    states = storage.all('State')
+    for v in states.values():
+        if id == v.id:
+            state_found = v
+    cities = storage.all('City')
+    city_list = [v for v in cities.values() if v.state_id == id]
+    return render_template(
+        '9-states.html',
+        state_found=state_found,
+        cities=city_list, states=None)
 
 
 @app.teardown_appcontext
-def teardown(self):
+def teardown(value):
     """Remove session"""
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
