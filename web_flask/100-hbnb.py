433c9aef3348b54a6682d0038127517135f17594
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-01-21 03:55:32 +0000

    HBNB is alive!

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -5,15 +5,17 @@ from models import storage
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
+from models.place import Place
 
 app = Flask(__name__)
 
 
-@app.route("/hbnb_filters", strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb_filters(id='0'):
-        return render_template("10-hbnb_filters.html",
+        return render_template("100-hbnb.html",
                                states=storage.all(State).values(),
-                               amenities=storage.all(Amenity).values(), id=id)
+                               amenities=storage.all(Amenity).values(),
+                               places=storage.all(Place).values())
 
 
 @app.teardown_appcontext
