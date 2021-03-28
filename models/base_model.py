22a34763cf5e21c29f74d7dd1f1c33ac85f79d06
huy75 <huy.nguyen66@gmail.com>
2021-03-28 13:27:43 +0000

    feat task 6 basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
+from models import storage
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(Datetime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(Datetime, nullable=False, default=datetime.utcnow())
+
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -31,7 +40,12 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
+
+    def delete(self):
+        """ delete the current instance from the storage """
+        storage.delete(self)
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -41,4 +55,6 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if "_sa_instance_state" in dictionnary:
+            del(dictionnary['_sa_instance_state'])
         return dictionary
