2d92224d8f89ed097584ec2f6a8304c31468bd49
Wiston Venera Macias <wisvem@hotmail.com>
2021-03-28 17:09:21 +0000

    State class modified

--- models/state.py
+++ models/state.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state",
+                          cascade="all, delete, delete-orphan")
+    
