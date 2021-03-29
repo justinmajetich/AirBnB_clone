d11ff2361126fae53891520920eb372b8664884b
Mfeliciano778 <1652@holbertonschool.com>
2021-03-29 13:26:41 +0000

    Adding empty test file for console

---
+++ tests/test_console.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" """
+import console 
+import unittest
+import datetime
+from uuid import UUID
+import json
+import os
+
+
+class test_console(unittest.TestCase):
+    """ """
