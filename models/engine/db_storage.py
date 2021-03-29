11a2863201415cb428a0b74808bfabf308d35ce1
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-03-29 21:08:12 +0000

    Add User class to work with databases

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -17,7 +17,7 @@ pwd = getenv('HBNB_MYSQL_PWD')
 host = getenv('HBNB_MYSQL_HOST')
 db = getenv('HBNB_MYSQL_DB')
 env = getenv('HBNB_ENV')
-dbtables = [State, City]
+dbtables = [State, City, User]
 
 
 class DBStorage:
