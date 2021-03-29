94b854b6b017891ffaf3ad9833e63ea065999e27
mlaizure <maddi.laizure@gmail.com>
2021-03-29 01:11:54 +0000

    Updating syntax

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,5 @@
+-- Prepares MySQL development server
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
-GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT SELECT ON performace_schema.* TO 'hbnb_dev'@'localhost';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
