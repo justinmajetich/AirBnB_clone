b2b07d27556585f910510d21532db395b4c1e8b2
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-03-29 16:42:17 +0000

    added task 6 - 10 reworks

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    __tablename__ = "amenities"
+    name = Column('name', String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
