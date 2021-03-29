4a8a351391e231bbd00d12f97b14d02c6518592b
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-03-29 00:00:34 +0000

    fixed task 3 Removed flush and extra privlages in seconds grant

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,6 @@
---setup
+-- Prepares machines MySQL server for poject
+-- This is script is for the dev section of the project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT PRIVILEGES ON performance_schema.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
