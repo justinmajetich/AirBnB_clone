ea4673c14c2ee0a91fabe06cb4354fefcd8fb218
Mfeliciano778 <1652@holbertonschool.com>
2021-03-29 18:08:49 +0000

    Adding test files

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-""" """
-import console 
+"""testing the console"""
+from console import HBNBCommand
+from io import StringIO
 import unittest
 import datetime
 from uuid import UUID
@@ -9,4 +10,11 @@ import os
 
 
 class test_console(unittest.TestCase):
-    """ """
+    """test_console"""
+
+    def test_create(self):
+        '''test_create'''
+        error = "** class name missing **"
+        with unittest.mock.patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create")
+            self.assertEqual(error, f.getvalue().strip())
