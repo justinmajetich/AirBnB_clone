1d9c5f821923cf7715771239696f22d4729cc819
Kellie Mogg <kelliedmogg@gmail.com>
2021-03-29 19:46:27 +0000

    Took out drop check

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,8 +3,6 @@
 -- Grant all privileges to new user on hbnb_dev_db
 -- Grant select privileges on performance_schema
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-DROP USER hbnb_dev@localhost;
-FLUSH PRIVILEGES;
 CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
