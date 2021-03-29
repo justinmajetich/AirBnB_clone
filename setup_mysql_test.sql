f16b0fc110f91a00d8a70d9c9817d529327c9bf5
ramsteven <ramsteven@gmail.com>
2021-03-29 06:00:24 +0000

    adding task 4 everything working

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- Creating DB and set permitions
+-- firts file sql
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db . * TO hbnb_test@localhost;
+FLUSH PRIVILEGES;
+GRANT SELECT ON performance_schema . * TO hbnb_test@localhost;
