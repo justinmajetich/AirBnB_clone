3d5cee1da6149f3ec0ad1e10ebadab0c0b5115a9
huy75 <huy.nguyen66@gmail.com>
2021-03-29 15:33:45 +0000

    fix add flush

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,9 @@
 -- prepares a MySQL server for the project
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- grant all privileges on the db hbnb_dev_db
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO hbnb_dev@localhost;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 -- grant select privilege on the db performance_schema
-GRANT SELECT ON performance_schema.* TO hbnb_dev@localhost;
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
