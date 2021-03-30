f2606632a59b925e2684707b02ed63784d14fcbc
mlaizure <maddi.laizure@gmail.com>
2021-03-30 21:19:40 +0000

    Added filestorage relationship between city and state

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
