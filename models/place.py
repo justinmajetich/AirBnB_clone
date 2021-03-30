b2cfa7eb100bb296e0e97d91c14fb87bdf56f9b7
Estephania Calvo <2177@holbertonschool.com>
2021-03-30 23:34:18 +0000

    Fix pep8

--- models/place.py
+++ models/place.py
@@ -8,8 +8,18 @@ from sqlalchemy import Column, Integer, String, ForeignKey, Float
 from sqlalchemy.orm import relationship
 from sqlalchemy import Table
 
-table_relation = Table('place_amenity', Base.metadata, Column('place_id', String(60), ForeignKey('places.id'), primary_key=True,
-                       nullable=False), Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
+table_relation = Table('place_amenity',
+                       Base.metadata,
+                       Column('place_id',
+                              String(60),
+                              ForeignKey('places.id'),
+                              primary_key=True,
+                              nullable=False),
+                       Column('amenity_id',
+                              String(60),
+                              ForeignKey('amenities.id'),
+                              primary_key=True,
+                              nullable=False))
 
 
 class Place(BaseModel, Base):
