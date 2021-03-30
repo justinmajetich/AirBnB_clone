95377cae6b2d8d432ddc7ba146a4c747d861a73d
Ethan-23 <emanroberts@gmail.com>
2021-03-30 16:59:07 +0000

    added imports at the top

--- models/place.py
+++ models/place.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Float
+from sqlalchemy.orm import relationship
 
 
 class Place(BaseModel):
--- models/user.py
+++ models/user.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
-
+from models.base_model import BaseModel, Base
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
