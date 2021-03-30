4a9eb9896e8c47240b2c0ccdd1c2c290ad04d659
ramsteven <ramsteven@gmail.com>
2021-03-30 22:54:48 +0000

    adding changes models city and place v2

--- models/city.py
+++ models/city.py
@@ -7,5 +7,10 @@ from sqlalchemy import Column, String, ForeignKey
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    else:
+        state_id = ""
+        name = ""
