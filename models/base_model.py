74f8d99466e08093fda6964dc90b3bff4025a089
didierrevelo <1829@holbertonschool.com>
2021-03-30 17:02:30 -0500

    prueba 5

--- models/base_model.py
+++ models/base_model.py
@@ -58,7 +58,8 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        dictionary.pop("_sa_instance_state", None)
+        if "_sa_instance_state" in dictionary:
+            del dictionary["_sa_instance_state"]
         return dictionary
 
     def delete(self):
