aac6949955dd27d1f3fd02c68c2e1485effda20f
Aleia DeVore <2496@holbertonschool.com>
2021-03-30 16:22:43 +0000

    Pep8, drops tables in test mode

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,11 +4,13 @@ import os
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
 
-""" Setting env variables """        
+""" Setting env variables """
 HBNB_MYSQL_USER = os.getenv('HBNB_MYSQL_USER')
 HBNB_MYSQL_PWD = os.getenv('HBNB_MYSQL_PWD')
 HBNB_MYSQL_HOST = os.getenv('HBNB_MYSQL_HOST')
 HBNB_MYSQL_DB = os.getenv('HBNB_MYSQL_DB')
+HBNB_ENV = os.getenv('HBNB_ENV')
+
 
 class DBStorage:
         """ Class to run database engine """
@@ -17,5 +19,12 @@ class DBStorage:
 
         def __init__(self):
                 """ Initializes class """
-                self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
-                HBNB_MYSQL_USER, HBNB_MYSQL_PWD, HBNB_MYSQL_HOST, HBNB_MYSQL_DB), pool_pre_ping=True)
+                self.__engine = create_engine(
+                        'mysql+mysqldb://{}:{}@{}/{}'.format(
+                                HBNB_MYSQL_USER, HBNB_MYSQL_PWD,
+                                HBNB_MYSQL_HOST, HBNB_MYSQL_DB),
+                        pool_pre_ping=True)
+
+                # Drop all tables if env is test
+                if HBNB_ENV == 'test':
+                        drop_all(self.__engine)
