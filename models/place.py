966c49640753b7a9fc5308ffea3c987298089661
BardoftheOzarks <1886@holbertonschool.com>
2021-03-30 23:54:47 +0000

    corrected Base imports for mapping

--- models/place.py
+++ models/place.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.review import Review
 from sqlalchemy import Column, String, ForeignKey, Integer, Float
 from sqlalchemy.orm import relationship
 from os import getenv
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
