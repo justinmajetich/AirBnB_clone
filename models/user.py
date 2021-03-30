075c99e68ef3d66071b2051fabb4c53aacceab83
Julian Mendez <julianlx@hotmail.com>
2021-03-30 17:12:19 -0500

    chanching var to can be null;

--- models/user.py
+++ models/user.py
@@ -11,8 +11,8 @@ class User(BaseModel, Base):
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
 
     else:
         email = ''
