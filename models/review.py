b3fa89d454f99cc05a2a3fafa1203af3526eef24
BardoftheOzarks <1886@holbertonschool.com>
2021-03-30 23:21:45 +0000

    added imports for relationships

--- models/review.py
+++ models/review.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel
 from sqlalchemy import Column, String, ForeignKey
 
+
 class Review(BaseModel):
     """ Review classto store review information """
     __tablename__ = 'reviews'
