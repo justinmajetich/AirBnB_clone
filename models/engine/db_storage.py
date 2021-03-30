a9d0dd899d0acb345d8ebe6c8bd37275c78d340d
Estephania Calvo <2177@holbertonschool.com>
2021-03-30 22:22:21 +0000

    Update db sotorage whitother models

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,8 +2,12 @@
 """This module defines a class to manage file storage for hbnb clone"""
 import os
 from models.base_model import BaseModel, Base
+from models.amenity import Amenity
 from models.city import City
+from models.place import Place
+from models.review import Review
 from models.state import State
+from models.user import User
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 
@@ -30,7 +34,7 @@ class DBStorage:
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         objects = {}
-        classes = [City, State]
+        classes = [City, State, User, Place, Amenity, Review]
 
         if cls is None:
             l_objs = []
