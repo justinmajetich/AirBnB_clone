62d5839f420dc8b38dfa24947ee31ced3a0ab334
mlaizure <maddi.laizure@gmail.com>
2021-03-30 22:36:22 +0000

    Amenity and class DB updates

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel):
-    name = ""
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
