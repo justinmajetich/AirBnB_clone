e465bac12e9994611e38db836cbb480bafaf3626
BardoftheOzarks <1886@holbertonschool.com>
2021-03-31 21:05:21 +0000

    reformatted sql setup files

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,7 @@
 -- Prepares a MySQL test server
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-GRANT USAGE ON *.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+GRANT SELECT ON performace_schema.* TO 'hbnb_test'@'localhost';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
