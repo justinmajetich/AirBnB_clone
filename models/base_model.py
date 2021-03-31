b4e016f15332ddd03445a2d6be8ca4c6f971cb59
AdrianVides56 <adrianvj18@gmail.com>
2021-03-31 02:20:01 +0000

    Changed name of class attributes (updated_at & crated_at)

--- models/base_model.py
+++ models/base_model.py
@@ -13,8 +13,8 @@ Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), primary_key=True, unique=True, nullable=False)
-    create_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
-    update_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
