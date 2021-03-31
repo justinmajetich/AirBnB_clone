e5c2cc2578533fdd5856dad223b53aeb05ade6ac
Mfeliciano778 <1652@holbertonschool.com>
2021-03-31 01:58:17 +0000

    Console

--- console.py
+++ console.py
@@ -152,7 +152,7 @@ class HBNBCommand(cmd.Cmd):
                 setattr(new_instance, att_key, value)
 
         print(new_instance.id)
-        storage.save()
+        new_instance.save()
 
     def help_create(self):
         """ Help information for the create method """
@@ -234,11 +234,11 @@ class HBNBCommand(cmd.Cmd):
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.all().items():
                 if k.split('.')[0] == args:
                     print_list.append(str(v))
         else:
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.all().items():
                 print_list.append(str(v))
 
         print(print_list)
