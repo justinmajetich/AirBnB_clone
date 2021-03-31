65da59089d024f6a032ec83d198cf00037669ae2
mlaizure <maddi.laizure@gmail.com>
2021-03-31 05:20:51 +0000

    pep8 fix

--- models/place.py
+++ models/place.py
@@ -6,12 +6,14 @@ from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 from sqlalchemy.orm import relationship
 from os import getenv
 
+
 place_amenity = Table('place_amenity', Base.metadata,
                       Column('place_id', String(60), ForeignKey('places.id'),
                              primary_key=True, nullable=False),
                       Column('amenity_id', String(60),
                              ForeignKey('amenities.id'),
                              primary_key=True, nullable=False))
+
 
 class Place(BaseModel, Base):
     """ A place to stay """
