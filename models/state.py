b5efe3d2c5280f9d81bd1bef6348ad0a153ad128
nehpets <nehpets0701@gmail.com>
2021-03-31 20:39:51 +0000

    pep8

--- models/state.py
+++ models/state.py
@@ -11,7 +11,7 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship("City", cascade="all, delete", backref="state")
-    
+
     @property
     def cities(self):
         """ this is a temp comment """
