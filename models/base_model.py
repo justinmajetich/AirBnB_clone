8458ff8001d0b2ce3d38d44f17dacd1101c8334b
Estephania Calvo <2177@holbertonschool.com>
2021-03-31 03:08:07 +0000

    Fix base model init
    Fix pep8 review model

--- models/base_model.py
+++ models/base_model.py
@@ -24,13 +24,18 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
         else:
+            if "create_at" not in kwargs.keys():
+                self.created_at = datetime.now()
+            if "update_at" not in kwargs.keys():
+                self.updated_at = datetime.now()
+            if "id" not in kwargs.keys():
+                self.id = str(uuid.uuid4())
             for k, v in kwargs.items():
                 if k in ["created_at", "updated_at"]:
                     fmt = "%Y-%m-%dT%H:%M:%S.%f"
                     setattr(self, k, datetime.strptime(v, fmt))
                 elif k != "__class__":
                     setattr(self, k, v)
-            self.id = str(uuid.uuid4())
 
     def save(self):
         """ Save Method """
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -29,7 +29,8 @@ class FileStorage:
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        # self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        self.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
 
     def save(self):
         """Saves storage dictionary to file"""
