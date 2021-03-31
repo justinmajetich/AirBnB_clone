f56e06e870bed2095cd799a8c83474d68e7b3e7f
Peter5793 <masaba.peter68@gmail.com>
2021-03-31 19:57:50 +0300

    create sql server

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,6 @@
 -- Scripts that prepares a MySQL server
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db. * to 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema. * TO 'hbnb_dev'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db. * to 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema. * TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
