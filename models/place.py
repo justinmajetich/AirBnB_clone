eb75216a7e4ade6c2dd5a4ffeec6dece665fdb7d
mauriciosierrac <2376@holbertonschool.com>
2021-03-30 21:01:28 -0500

    update and fixed place.pu

--- models/place.py
+++ models/place.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
+import models
+from os import getenv
 from models.base_model import BaseModel
 from models.base_model import Base
 from models.amenity import Amenity
@@ -42,3 +44,27 @@ class Place(BaseModel, Base):
         'Amenity', secondary='place_amenity', viewonly=False)
 
     amenity_ids = []
+
+    if getenv("HBNB_TYPE_STORAGE", None) != "db":
+        @property
+        def reviews(self):
+            """Get a list of all linked Reviews."""
+            review_list = []
+            for review in list(models.storage.all(Review).values()):
+                if review.place_id == self.id:
+                    review_list.append(review)
+            return review_list
+
+        @property
+        def amenities(self):
+            """Get/set linked Amenities."""
+            amenity_list = []
+            for amenity in list(models.storage.all(Amenity).values()):
+                if amenity.id in self.amenity_ids:
+                    amenity_list.append(amenity)
+            return amenity_list
+
+        @amenities.setter
+        def amenities(self, value):
+            if type(value) == Amenity:
+                self.amenity_ids.append(value.id)
