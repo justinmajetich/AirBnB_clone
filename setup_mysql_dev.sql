2ffb3fa5436cf1a57da405e15efc658eec927b13
BardoftheOzarks <1886@holbertonschool.com>
2021-03-31 22:12:00 +0000

    trying removing user before creating again

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,9 @@
 -- Prepares MySQL development server
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+DROP USER IF EXISTS 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
+CREATE USER 'hbnb_dev'@'localhost';
 SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
-GRANT SELECT ON `performace_schema`.* TO 'hbnb_dev'@'localhost';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performace_schema.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
