f3b521b6e91c72da758daec42453de3b51972cad
Estephania Calvo <2177@holbertonschool.com>
2021-03-31 04:59:32 +0000

    Add atribute state to state model

--- models/state.py
+++ models/state.py
@@ -13,6 +13,7 @@ class State(BaseModel, Base):
 
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
+    cities = relationship("City",  backref="state", cascade="delete")
 
     # Database storage
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
