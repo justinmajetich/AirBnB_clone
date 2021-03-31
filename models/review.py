817f1f777d10821e71d7fc23ee1397e0db699351
nehpets <nehpets0701@gmail.com>
2021-03-31 20:38:57 +0000

    pep8

--- models/review.py
+++ models/review.py
@@ -8,6 +8,8 @@ from sqlalchemy.orm import relationship
 class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = "reviews"
-    place_id = Column('place_id', String(60), ForeignKey("places.id"), nullable=False)
-    user_id = Column('user_id', String(60), ForeignKey("users.id"), nullable=False)
+    place_id = Column('place_id', String(60),
+                      ForeignKey("places.id"), nullable=False)
+    user_id = Column('user_id', String(60),
+                     ForeignKey("users.id"), nullable=False)
     text = Column('text', String(1024), nullable=False)
