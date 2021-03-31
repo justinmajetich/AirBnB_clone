65e4f04eae3d12ffcf5d8954e324a9f1070545ea
BardoftheOzarks <1886@holbertonschool.com>
2021-03-31 19:24:51 +0000

    verified classes

--- models/place.py
+++ models/place.py
@@ -18,8 +18,10 @@ place_amenity = Table('place_amenity', Base.metadata,
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    city_id = Column(String(60), ForeignKey('cities.id', ondelete='CASCADE'),
+                     nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id', ondelete='CASCADE'),
+                     nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, default=0, nullable=False)
@@ -46,14 +48,14 @@ class Place(BaseModel, Base):
                     review_related.append(val)
             return review_related
 
-            @property
-            def amenities(self):
-                """returns list of amenity instances based on amenity_ids"""
-                return self.amenity_ids
+        @property
+        def amenities(self):
+            """returns list of amenity instances based on amenity_ids"""
+            return self.amenity_ids
 
-            @amenities.setter
-            def amenities(self, obj):
-                """appends Amenity.ids to amenity_ids"""
-                from models.amenity import Amenity
-                if type(obj) == Amenity:
-                    amenity_ids.append(obj.id)
+        @amenities.setter
+        def amenities(self, obj):
+            """appends Amenity.ids to amenity_ids"""
+            from models.amenity import Amenity
+            if type(obj) == Amenity:
+                self.amenity_ids.append(obj.id)
