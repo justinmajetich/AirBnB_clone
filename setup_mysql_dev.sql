fc96cb45f4a64a0994da221cd7bc035765d741ad
AndresSern <andrecamposern@gmail.com>
2021-03-31 02:34:21 +0000

    CHanging self to dictioanry.pop

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,9 +4,5 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-<<<<<<< HEAD
-GRANT SELECT ON performance_schema.* 'hbnb_dev'@'localhost';
-=======
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
->>>>>>> 82b83840612920ec20226ed66f674a5defd471fc
