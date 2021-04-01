1d179cae941d3a4195276eb95f97360c218e3878
laurendobratz00 <laurendobratz@icloud.com>
2021-04-01 02:44:24 +0000

    moved __tablename__ to the top

--- models/review.py
+++ models/review.py
@@ -6,7 +6,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
+    __tablename__ = 'reviews'
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     text = Column(String(1024), nullable=False)
-    __tablename__ = 'reviews'
--- models/user.py
+++ models/user.py
@@ -7,10 +7,10 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+    __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
-    __tablename__ = 'users'
     places = relationship("Place", backref="user")
     reviews = relationship("Review", backref="user")
