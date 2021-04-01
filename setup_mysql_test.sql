780fad08f202c184e94e5f03b3fda18df8d244b1
BardoftheOzarks <1886@holbertonschool.com>
2021-04-01 01:07:16 +0000

    performaNce

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,6 +2,6 @@
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
 SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
-GRANT SELECT ON performace_schema.* TO 'hbnb_test'@'localhost';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* to 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
