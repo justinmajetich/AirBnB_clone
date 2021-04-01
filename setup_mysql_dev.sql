f31bc45f8a72c864b5cab0e2813798c9e61b9e75
BardoftheOzarks <1886@holbertonschool.com>
2021-04-01 00:47:42 +0000

    swapped privilege order

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,7 @@
 -- Prepares MySQL development server
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-DROP USER IF EXISTS 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
 CREATE USER 'hbnb_dev'@'localhost';
 SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
-GRANT SELECT ON performace_schema.* TO 'hbnb_dev'@'localhost';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performace_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
