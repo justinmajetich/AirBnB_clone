ea2d385c1d73bd4d77234a859a7ddb6988308162
BardoftheOzarks <1886@holbertonschool.com>
2021-04-01 01:00:40 +0000

    performaNce

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,5 +3,5 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER 'hbnb_dev'@'localhost';
 SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performace_schema.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
