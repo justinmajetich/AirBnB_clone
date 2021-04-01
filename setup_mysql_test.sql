ce3aeb6da2b8660a548ea33415c6f4c3bd9f4993
mlaizure <maddi.laizure@gmail.com>
2021-04-01 02:53:03 +0000

    Fixing typo

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -4,4 +4,3 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
 SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* to 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
