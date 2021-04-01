7dc0b4c80b9910d025e6417b3aa7eac915c9bfea
laurendobratz00 <laurendobratz@icloud.com>
2021-04-01 03:57:51 +0000

    add if/else statement

--- models/user.py
+++ models/user.py
@@ -3,14 +3,22 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String, ForeignKey
+from os import getenv
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
-    places = relationship("Place", backref="user")
-    reviews = relationship("Review", backref="user")
+
+    if getenv("HBNB_TYPE_STORAGE", "fs") != "db":
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
+    else:
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
