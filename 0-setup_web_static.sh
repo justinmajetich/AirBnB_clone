f3fc1bc32bce49815e6801ca27328c1cce710739
AdrianVides56 <adrianvj18@gmail.com>
2021-04-16 20:19:17 -0500

    Added first step to deployment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,5 +7,5 @@ sudo mkdir -p /data/web_static/shared/
 printf "<html>\n\t<head></head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>\n" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "/listen 80 default_server;/hbnb_static {\\n\\talias /data/web_static/current/;\\n\\t}\\n" /etc/nginx/sites-available/default
+sudo sed -i "/listen 80 default_server;/a location /hbnb_static {\\n\\talias /data/web_static/current/;\\n\\t}\\n" /etc/nginx/sites-available/default
 sudo service nginx restart
