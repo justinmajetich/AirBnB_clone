dadf2522ae31c810dbe17ada501bf7eebb35408e
AdrianVides56 <adrianvj18@gmail.com>
2021-04-30 18:55:17 -0500

    Fixed pep8 style

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,7 +4,7 @@ starts a Flask web application listening on 0.0.0.0, port 5000
 Routes:
         - /: display “Hello HBNB!”
         - /hbnb: display “HBNB”
-        - /c/<text>: display “C ” followed by the value of the 
+        - /c/<text>: display “C ” followed by the value of the
                      text variable (replace underscore _ symbols with a space )
 """
 from flask import Flask
