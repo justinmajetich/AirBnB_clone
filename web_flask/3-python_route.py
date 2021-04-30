180e9cb16c8fd1cbe7f9529f8fc558eeae7cfc49
AdrianVides56 <adrianvj18@gmail.com>
2021-04-30 18:54:58 -0500

    Added task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application listening on 0.0.0.0, port 5000
+Routes:
+        - /: display “Hello HBNB!”
+        - /hbnb: display “HBNB”
+        - /c/<text>: display “C ” followed by the value of the
+                     text variable (replace underscore _ symbols with a space )
+        - /python/(<text>): display “Python ”, followed by the value of text
+                            variable(replace underscore _ symbols with a space)
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+        """Display 'Hello HBNB!'"""
+        return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+        """Display 'HBNB'"""
+        return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+        """Display C followed by text"""
+        return "C %s" % text.replace("_", " ")
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
+        """Display python followed by text"""
+        return "Python %s" % text.replace("_", " ")
+
+
+if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=5000)
