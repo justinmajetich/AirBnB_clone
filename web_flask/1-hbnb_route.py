408ac145fe7a133b4b2eecb3c118743a153c1a4e
didierrevelo <1829@holbertonschool.com>
2021-04-30 19:07:05 -0500

    1-hbnb_route.py and corretion of 0 tacks

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,5 +11,12 @@ def hello():
     """ display “Hello HBNB!”"""
     return 'Hello HBNB!'
 
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """/hbnb: display “HBNB”"""
+    return 'HBNB'
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
