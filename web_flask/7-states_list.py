5e07a9c68887a4aae5d55fe42d4750147c390ea9
AdrianVides56 <adrianvj18@gmail.com>
2021-05-01 12:50:07 -0500

    Added task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""starts a Flask web application listening on 0.0.0.0, port 5000"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """remove the current sqlalchemy session"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """list all states"""
+    states = list(storage.all(State).values())
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
