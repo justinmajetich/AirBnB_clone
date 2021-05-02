d7ce09687ec8a80bbafc7f0f23fa9f229f744048
didierrevelo <1829@holbertonschool.com>
2021-05-01 20:08:11 -0500

    correction city

--- models/city.py
+++ models/city.py
@@ -9,4 +9,8 @@ class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+
+    places = relationship("Place",
+                          cascade="all, delete",
+                          backref="cities")
