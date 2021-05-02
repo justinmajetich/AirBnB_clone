e92c8ee8c888375ab5a1c65de51c27c1f96de29d
Peter5793 <masaba.peter68@gmail.com>
2021-05-02 23:08:27 +0300

    add replace file

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+scripts that starts a Flask web application
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return ('Hello HBNB!')
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    return ('HBNB')
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_replace(text):
+    return "C " + text.replace("_", " ")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
