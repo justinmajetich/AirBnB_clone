c37af39c118953810dce43ce8de2545ab6ce31a7
AdrianVides56 <adrianvj18@gmail.com>
2021-05-01 19:43:10 -0500

    Added task 10

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""starts a Flask web application listening on 0.0.0.0, port 5000"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """remove the current sqlalchemy session"""
+    storage.close()
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    """Display airbnb page until filters construct"""
+    states = storage.all(State)
+    amenities = storage.all(Amenity)
+    return render_template('10-hbnb_filters.html',
+                            states=states, amenities=amenities)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
