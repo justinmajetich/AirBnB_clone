fc999d3075b3361804acd2a60205b4db3621ea63
Lucha3sr <2278@holbertonschool.com>
2021-05-05 19:42:56 +0000

    AirBnB clone - MySQL

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,10 +10,13 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        if cls:
-            return {key: obj for (key, obj) in self.__objects.items()
-                    if isinstance(obj, cls)}
-        return self.__objects
+        if cls is not None:
+            obj_dict = {}
+            for key, value in self.__objects.items():
+                if cls == value.__class__:
+                    obj_dict[key] = value
+            return obj_dict
+        return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
