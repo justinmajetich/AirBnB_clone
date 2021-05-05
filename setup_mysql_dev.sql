93766747ae3005dd4de42e9fd23f626e6bf9efaa
Lucha3sr <2278@holbertonschool.com>
2021-05-05 17:23:22 +0000

    AirBnB clone - MySQL

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,8 @@
+-- Create a database if not exists
+-- Create a new user and grant permissions
+
+CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
---
+++ setup_mysql_test.sql
@@ -0,0 +1,8 @@
+-- Create a database if not exists
+-- Create a new user and grant permissions
+
+CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
