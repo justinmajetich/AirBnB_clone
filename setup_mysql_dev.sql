832e50c9d6a79191a1703d8c22addb4cbabc2184
Rachel Lewis <RLewis11769@gmail.com>
2021-06-04 22:17:28 +0000

    Add test.sql and correct db references

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -6,8 +6,8 @@ DROP USER IF EXISTS 'hbnd_dev'@'localhost';
 CREATE USER 'hbnb_dev'@'localhost'
 IDENTIFIED BY 'hbnb_dev_pwd';
 
-GRANT ALL PRIVILEGES ON 'hbnd_dev_db'.*
+GRANT ALL PRIVILEGES ON hbnd_dev_db.*
 TO 'hbnb_dev'@'localhost';
 
-GRANT SELECT ON 'performance_schema'.*
+GRANT SELECT ON performance_schema.*
 TO 'hbnb_dev'@'localhost';
