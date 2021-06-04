b65c961db1f60439686827e0b7e23d8598185ba5
Rachel Lewis <RLewis11769@gmail.com>
2021-06-04 22:30:15 +0000

    Different way of adding all privileges

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,7 +5,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
 IDENTIFIED BY 'hbnb_dev_pwd';
 
-GRANT ALL PRIVILEGES ON *.*
+GRANT ALL PRIVILEGES ON 'hbtn_dev_db'.*
 TO 'hbnb_dev'@'localhost';
 
 GRANT SELECT ON performance_schema.*
