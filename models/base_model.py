f7ad415ad766793e6404c2f33e7b14664083e6a1
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-06-05 20:51:04 +0000

    BaseModel Updated for FS changes to SQLAl

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime, 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql.expression import delete
 # from sqlalchemy.orm import relationship
