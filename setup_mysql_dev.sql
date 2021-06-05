d41e8d91fc19af283c419548fdc65a65f0ed42dc
JakeFC <jacobchavera@yahoo.com>
2021-06-05 22:00:09 +0000

    removed quotes on db and added workaround for user creation bug

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,8 @@
 -- creates a MySQL database and user with password and all privileges in new db,
 -- plus SELECT privileges in performance_schema
-CREATE DATABASE IF NOT EXISTS 'hbnb_dev_db';
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnd_dev_pwd';
-GRANT ALL ON 'hbnb_dev_db'.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON 'performance_schema'.* TO 'hbnb_dev'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+DROP USER IF EXISTS hbnb_dev@localhost;
+FLUSH PRIVILEGES;
+CREATE USER hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL ON hbnb_dev_db.* TO hbnb_dev@localhost;
+GRANT SELECT ON performance_schema.* TO hbnb_dev@localhost;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,8 @@
 -- creates a MySQL database and user with password and all privileges in new db,
 -- plus SELECT privileges in performance_schema
-CREATE DATABASE IF NOT EXISTS 'hbnb_test_db';
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnd_test_pwd';
-GRANT ALL ON 'hbnb_test_db'.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON 'performance_schema'.* TO 'hbnb_test'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+DROP USER IF EXISTS hbnb_test@localhost;
+FLUSH PRIVILEGES;
+CREATE USER hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL ON hbnb_test_db.* TO hbnb_test@localhost;
+GRANT SELECT ON performance_schema.* TO hbnb_test@localhost;
