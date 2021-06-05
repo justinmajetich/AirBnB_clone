5601e9ce0c78a6b20fed96c6bceb895ae7a68790
chrisvanndy <chrisvannyc@gmail.com>
2021-06-05 21:17:48 +0000

    working on task 2

--- console.py
+++ console.py
@@ -115,15 +115,29 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        if not args:
-            print("** class name missing **")
-            return
-        elif args not in HBNBCommand.classes:
-            print("** class doesn't exist **")
-            return
-        new_instance = HBNBCommand.classes[args]()
+    #    if not args:
+      #      print("** class name missing **")
+       #     return
+     #   elif args not in HBNBCommand.classes:
+       #    print("** class doesn't exist **")
+        #    return
+    #    new_instance = HBNBCommand.classes[args]()
+        if args:
+            print(args) 
+            split_line = args.split(' ')
+            split_instance = eval("{}()".format(split_line[0]))
+            param = []
+            for item in split_line:
+                if ('=' in item):
+                    param = item.split('=')
+                print(item)
+                for i in param:
+		    # i 0 = attr 1 = value
+                    # accomodate for "<value>"
+                    print(i)
+        print(split_instance)
         storage.save()
-        print(new_instance.id)
+       #  print(new_instance.id)
         storage.save()
 
     def help_create(self):
