5f094ff5e6925d7083aa78c0325927338dfd6efc
JakeFC <jacobchavera@yahoo.com>
2021-06-05 22:26:22 +0000

    added delete() method and updated all()

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,13 +8,29 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+    def all(self, cls=None):
+        """Returns a dictionary of models of class 'cls' currently in storage"""
+        if cls:
+            d = {}
+            for k, v in FileStorage.__objects.items():
+                if type(v) is cls:
+                    d[k] = v
+            return d
+        else:
+            return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+
+    def delete(self, obj=None):
+        """Deletes obj from __objects if inside"""
+        if obj:
+            for k, v in self.all().items():
+                if v == obj:
+                    self.all().pop(k)
+                    break
+            self.save()
 
     def save(self):
         """Saves storage dictionary to file"""
