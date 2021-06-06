295625910215b9e7e0153f0b9e88e787e8a1853e
tayloradam1999 <tayloradam1999@gmail.com>
2021-06-06 19:42:07 +0000

    Pushing new unittest file for testing console

---
+++ tests/test_console.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+Unittesting for console in interactive and non-interacitve
+"""
+
+import unittest
+import os
+from models.base_model import BaseModel
+from models import storage
+from console import HBNBCommand
+from unittest.mock import patch
+from io import StringIO
+
+
+fs = FileStorage()
+
+
+class test_console(unittest.TestCase):
+    """Console unit tests"""
+    def test_create_state(self):
+        if os.path.exists(fs._FileStorage__file_path):
+            os.remove(fs._FileStorage__file_path)
+        with patch('sys.stdout', new = StringIO()) as boy:
+            HBNBCommand().onecmd("create State")
+            state_id = boy.getvalue()[:-1]
+        with open(fs._FileStorage__file_path, 'r') as guy:
+            self.assertIn(state_id, guy.read())
