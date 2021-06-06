f8d09c36b2275d6547673d55893bda5b8be10a9e
tayloradam1999 <tayloradam1999@gmail.com>
2021-06-06 21:55:44 +0000

    Added Columns for place.py, added relationships in user and city

--- models/city.py
+++ models/city.py
@@ -12,3 +12,4 @@ class City(BaseModel, Base):
 
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Place', backref='cities')
