4b5534ecf868d880bc3abde9d958675902fb8c4c
JakeFC <jacobchavera@yahoo.com>
2021-06-07 01:20:45 +0000

    removed check for db storage on relationship attribute

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from os import getenv
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -12,6 +11,4 @@ class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
 
     name = Column(String(128), nullable=False)
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        place_amenities = relationship('Place', secondary='place_amenity',
-                                        backref='amenities')
+    place_amenities = relationship('Place', secondary='place_amenity', backref='amenities')
