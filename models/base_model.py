6f2ad2a1be59a14b9bf29ec228746de5756e878b
rkbrian <rkbriankong@gmail.com>
2021-06-07 05:56:48 +0000

    DBStorage-States and Cities: basemodel typo and pep8 correction, city inheritance and attr updates, state inheritance and attr updates and storage attrs creation

--- models/base_model.py
+++ models/base_model.py
@@ -3,16 +3,17 @@
 import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Datetime
+from sqlalchemy import Column, Integer, String, DateTime
 
 Base = declarative_base()
+
 
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), primary_key=True)
     created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
     updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
-    
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
