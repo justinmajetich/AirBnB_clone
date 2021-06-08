5cf9072c4fa75c428ec055e40a8b5a0ecc7e3f07
JakeFC <jacobchavera@yahoo.com>
2021-06-08 01:11:30 +0000

    made first test in console work for both types

--- tests/test_console.py
+++ tests/test_console.py
@@ -14,21 +14,27 @@ from models.state import State
 from models.city import City
 import MySQLdb
 from models.engine.db_storage import user, passwd, host, database
+from models.engine.file_storage import FileStorage
 fs = models.storage
 
 
 class test_console(unittest.TestCase):
     """Console unit tests"""
 
-    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE"), "Using FileStorage")
     def test_create_state_fs(self):
-        if os.path.exists(fs._FileStorage__file_path):
-            os.remove(fs._FileStorage__file_path)
-        with patch('sys.stdout', new=StringIO()) as boy:
-            HBNBCommand().onecmd("create State")
-            state_id = boy.getvalue()[:-1]
-        with open(fs._FileStorage__file_path, 'r') as guy:
-            self.assertIn(state_id, guy.read())
+        if type(fs) != FileStorage:
+            with self.assertRaises(Exception):
+                open(fs.__FileStorage__file_path, 'r')
+        else:
+            if os.path.exists(fs._FileStorage__file_path):
+                os.remove(fs.FileStorage__file_path)
+            with patch('sys.stdout', new=StringIO()) as boy:
+                HBNBCommand().onecmd("create State name=\"Hawaii\"")
+                state_id = boy.getvalue()[:-1]
+            with open(fs._FileStorage__file_path, 'r') as guy:
+                self.assertIn(state_id, guy.read())
+
+
 
     @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") != 'db', "Using DBStorage")
     def test_create_state_db(self):
