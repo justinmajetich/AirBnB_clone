c8b64d41c02f9c8c9a9719a85749fe3e392fbc47
tayloradam1999 <tayloradam1999@gmail.com>
2021-06-10 21:40:50 +0000

    Actually added 2 db testsa tests/test_models/test_engine/test_db_storage.py !

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -15,17 +15,45 @@ import unittest
 import MySQLdb
 
 evars = {
-    'env' = environ.get('HBNB_ENV'),
-    'user' = environ.get('HBNB_MYSQL_USER'),
-    'pass' = environ.get('HBNB_MYSQL_PWD'),
-    'host' = environ.get('HBNB_MYSQL_HOST'),
-    'dbName' = environ.get('HBNB_MYSQL_DB')m
-    'storageType' = environ.get('HBNB_TYPE_STORAGE')
+    'env': environ.get('HBNB_ENV'),
+    'user': environ.get('HBNB_MYSQL_USER'),
+    'pass': environ.get('HBNB_MYSQL_PWD'),
+    'host': environ.get('HBNB_MYSQL_HOST'),
+    'dbName': environ.get('HBNB_MYSQL_DB'),
+    'storageType': environ.get('HBNB_TYPE_STORAGE')
 }
 
 
-@unittest.skipIf(storageType != 'db', "DB_STORAGE")
+@unittest.skipIf('storageType' != 'db', "DB_STORAGE")
 class test_db_storage(unittest.TestCase):
-    """Unit test class for db_storage"
+    """Unit test class for db_storage"""
 
-    def
+    def test_create_state(self):
+        """ Tests state creation """
+        with patch('sys.stdout', new=StringIO()) as boy:
+            HBNBCommand().onecmd("create State name='ChaveraLand'")
+            state = boy.getvalue()
+            state = state[:-1]
+        with patch('sys.stdout', new=StringIO()) as guy:
+            HBNBCommand().onecmd("show State ()".format(state))
+            state_all = guy.getvalue()
+        self.assertIn(state, state_all)
+
+        def test_storing_city(self):
+            """ Tests if creating a city stores it in cities """
+            with patch('sys.stdout', new=StringIO()) as boy:
+                HBNBCommand().onecmd("create State name='ChaveraLand'")
+                state = boy.getvalue()
+                state = state[:-1]
+            with patch('sys.stdout', new=StringIO()) as guy:
+                HBNBCommand().onecmd("create City name='lilgoober' state_id={}"
+                                     .format(state))
+                city = guy.getvalue()
+                city = city[:-1]
+            with patch('sys.stdout', new=StringIO()) as man:
+                HBNBCommand().onecmd("show City {}".format(city))
+                cities = man.getvalue()
+            self.assertIn(city, cities)
+
+if __name__ == '__main__':
+    unittest.main()
