4bf266c410bb6e87a27d61801974d780dcb90759
Adrien MILLOT <adrien.millot@gmail.com>
2021-08-09 16:33:10 +0200

    Fix datetime base_model

--- models/base_model.py
+++ models/base_model.py
@@ -47,8 +47,8 @@ class BaseModel:
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
-        # dictionary['created_at'] = self.created_at.isoformat()
-        # dictionary['updated_at'] = self.updated_at.isoformat()
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
         del dictionary['_sa_instance_state']
         return dictionary
 
