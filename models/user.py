38fe61bf15b26aea90f67bc71d13f8dba8c60b17
SimonBr017 <simonbrard017@gmail.com>
2021-08-09 16:24:21 +0200

    pep8 user.py

--- models/user.py
+++ models/user.py
@@ -7,12 +7,10 @@ from sqlalchemy import Column, String
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    
+
     __tablename__ = "users"
-    
+
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
-    #places = relationship("Place", cascade="all, delete", backref="user")
-    #reviews = relationship("Review", cascade="all, delete", backref="place")
