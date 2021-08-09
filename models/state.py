3cf3492afde105779ccf9886fccde82b8db0d0f8
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-08-09 15:53:49 +0300

    state model

--- models/state.py
+++ models/state.py
@@ -1,8 +1,34 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+'''
+    Implementation of the State class
+'''
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+import os
+import models
+from models.city import City
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    '''
+        Implementation for the State.
+    '''
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
+                              cascade="delete")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            city_dict = models.storage.all(City)
+            state_query = self.id
+            city_list = []
+            for k, v in city_dict.items():
+                if v.state_id == self.id:
+                    city_list.append(v)
+            return city_list
