4688b28e1c47991a98e3d8f6a421691f777d50f8
SimonBr017 <simonbrard017@gmail.com>
2021-08-09 16:25:57 +0200

    clean user.py task 7

--- models/user.py
+++ models/user.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 
