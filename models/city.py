48279a258bfe7e98f6116814cef7f08af9c42ae1
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-08-09 16:02:10 +0300

    city

--- models/city.py
+++ models/city.py
@@ -1,24 +1,23 @@
-#!/usr/bin/python
-""" holds class City"""
-import models
+#!/usr/bin/python3
+'''
+    Define the class City.
+'''
+import os
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """Representation of city """
-    if models.storage_t == "db":
-        __tablename__ = 'cities'
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    '''
+        Define the class City that inherits from BaseModel.
+    '''
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "cities"
         name = Column(String(128), nullable=False)
-        places = relationship("Place", backref="cities")
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place", backref="cities",
+                              cascade="delete")
     else:
-        state_id = ""
         name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes city"""
-        super().__init__(*args, **kwargs)
+        state_id = ""
