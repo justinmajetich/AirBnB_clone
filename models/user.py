e4eee541f6a8d668dfc3d692a80bb6b5d866cf44
SimonBr017 <simonbrard017@gmail.com>
2021-08-09 16:19:31 +0200

    comment places & reviews

--- models/user.py
+++ models/user.py
@@ -14,5 +14,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
-    places = relationship("Place", cascade="all, delete", backref="user")
-    reviews = relationship("Review", cascade="all, delete", backref="place")
+    #places = relationship("Place", cascade="all, delete", backref="user")
+    #reviews = relationship("Review", cascade="all, delete", backref="place")
