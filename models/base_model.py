19d35c8717633fadf0116db91ef10787db204bd9
Jaricapa-holberton <2241@holbertonschool.com>
2021-08-09 20:54:15 -0500

    task 7 v1

--- models/base_model.py
+++ models/base_model.py
@@ -59,5 +59,5 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         if "_sa_instance_state" in dictionary:
-                del(dictionary['_sa_instance_state'])
+            del(dictionary['_sa_instance_state'])
         return dictionary
