3c9a2f32e89a26d74ec2d014dc812c6bd4f827ed
Adrien MILLOT <adrien.millot@gmail.com>
2021-08-11 00:35:10 +0200

    DBStorage - Amenity... and BOOM!

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy.sql.schema import Column
+from sqlalchemy.sql.sqltypes import String
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """class amenity"""
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
--- models/place.py
+++ models/place.py
@@ -1,13 +1,33 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
+from models.amenity import Amenity
 from models.review import Review
 import models
 from os import getenv
-from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.sql.schema import ForeignKey, Table
 from sqlalchemy.sql.sqltypes import Float, Integer
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+
+
+if getenv("HBNB_TYPE_STORAGE") == 'db':
+    association_table = Table(
+        'place_amenity',
+        Base.metadata,
+        Column(
+            'amenity_id',
+            String(60),
+            ForeignKey('amenities.id'),
+            primary_key=True
+        ),
+        Column(
+            'place_id',
+            String(60),
+            ForeignKey('places.id'),
+            primary_key=True
+        )
+    )
 
 
 class Place(BaseModel, Base):
@@ -30,6 +50,8 @@ class Place(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == "db":
         reviews = relationship("Review",
                                cascade="all, delete", backref="place")
+        amenities = relationship("Amenity", secondary=association_table,
+                                 backref='place_amenities', viewonly=False)
 
     else:
         @property
@@ -43,3 +65,21 @@ class Place(BaseModel, Base):
                     placeReviews.append(objReview)
 
             return (placeReviews)
+
+        @property
+        def amenities(self):
+            """getter atribute amenities"""
+            allAmenities = models.storage.all(Amenity)
+            placeAmenities = []
+
+            for objAmenity in list(allAmenities.values()):
+                if objAmenity.place_id == self.id:
+                    placeAmenities.append(objAmenity)
+
+            return (placeAmenities)
+
+        @amenities.setter
+        def amenities(self, prmObj):
+            """amenities setter"""
+            if isinstance(prmObj, Amenity):
+                self.amenity_ids.append(prmObj.id)
