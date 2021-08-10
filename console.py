9052b63778e73d7eccf6e67f9cc5dc21704ffd23
JudgeFudge19 <dremukare@gmail.com>
2021-08-10 13:17:52 +0000

    added feature to create function

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 """ Console Module """
 import cmd
 import sys
+import re
 from models.base_model import BaseModel
 from models.__init__ import storage
 from models.user import User
@@ -19,16 +20,16 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) ' if sys.__stdin__.isatty() else ''
 
     classes = {
-               'BaseModel': BaseModel, 'User': User, 'Place': Place,
-               'State': State, 'City': City, 'Amenity': Amenity,
-               'Review': Review
-              }
+        'BaseModel': BaseModel, 'User': User, 'Place': Place,
+        'State': State, 'City': City, 'Amenity': Amenity,
+        'Review': Review
+    }
     dot_cmds = ['all', 'count', 'show', 'destroy', 'update']
     types = {
-             'number_rooms': int, 'number_bathrooms': int,
-             'max_guest': int, 'price_by_night': int,
-             'latitude': float, 'longitude': float
-            }
+        'number_rooms': int, 'number_bathrooms': int,
+        'max_guest': int, 'price_by_night': int,
+        'latitude': float, 'longitude': float
+    }
 
     def preloop(self):
         """Prints if isatty is false"""
@@ -36,10 +37,7 @@ class HBNBCommand(cmd.Cmd):
             print('(hbnb)')
 
     def precmd(self, line):
-        """Reformat command line for advanced command syntax.
-
-        Usage: <class name>.<command>([<id> [<*args> or <**kwargs>]])
-        (Brackets denote optional fields in usage example.)
+        """Reformat command line for advanced command syntmple.)
         """
         _cmd = _cls = _id = _args = ''  # initialize line elements
 
@@ -73,7 +71,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] is '{' and pline[-1] is '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -118,13 +116,35 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        elif args.split(' ')[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
+
+        dict_args = {}
+        pattern = '\\s+(?=(?:(?:[^"]*"){2})*[^"]*"[^"]*$)'
+        params = args.partition(' ')[2]
+        params = re.sub(pattern, '_', params)
+        params = params.split(' ')
+        for i in range(0, len(params)):
+            key, value = params[i].split('=')
+            if value[0] == '"' and value[-1] == '"':
+                value = '"' + value[1:-1].replace('"', '\""') + '"'
+            try:
+                value = eval(value)
+            except BaseException:
+                raise Exception
+            dict_args[key] = value
+
+        if dict_args == {}:
+            new_instance = HBNBCommand.classes[args.split(' ')[0]]()
+            # print('No args added')
+        else:
+            new_instance = HBNBCommand.classes[args.split(' ')[0]]()
+            for k, v in dict_args.items():
+                new_instance.__setattr__(k, v)
+
         storage.save()
         print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
@@ -319,6 +339,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
