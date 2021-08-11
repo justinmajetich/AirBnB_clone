4701ba588a26f6b4cfcc75ee46ddf92043c0ac1c
George-byt <jmoralesvalencia750@gmail.com>
2021-08-11 02:51:14 +0000

    Update: <pep8>

--- models/city.py
+++ models/city.py
@@ -5,6 +5,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship, backref
 
+
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
