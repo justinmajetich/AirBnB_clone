9eeefeff8d143087bcdd6b07416cda471fc6e651
Rode1o <dumirola.dr@gmail.com>
2021-08-11 17:03:58 -0500

    update place

--- models/place.py
+++ models/place.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.amenity import Amenity
-from models.reviews import Review
+from models.review import Review
 from models.base_model import BaseModel, Base
 from sqlalchemy import *
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
