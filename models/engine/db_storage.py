d33650c036ec1f3ca0a387ad66221685eb7f7b7e
SimonBr017 <simonbrard017@gmail.com>
2021-08-11 13:29:50 +0200

    deseperate remove

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -47,8 +47,8 @@ class DBStorage:
         if type(cls) == str:
             cls = eval(cls)
         if cls is None:
-            for clas in (City, Place, State, User, Amenity, Review):
-                for obj in self.__session.query(clas).all():
+            for cls in (City, Place, State, User, Amenity, Review):
+                for obj in self.__session.query(cls).all():
                     key = "{}.{}".format(type(obj).__name__, obj.id)
                     list[key] = obj
         else:
