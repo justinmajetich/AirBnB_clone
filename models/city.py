316df6c149f20d393fef5c0ef347125a7eee4f54
Jaricapa-holberton <2241@holbertonschool.com>
2021-08-10 21:48:35 -0500

    task 9 and 10 v1

--- models/city.py
+++ models/city.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
+
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
--- models/place.py
+++ models/place.py
@@ -4,6 +4,21 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 
+# instance SQLAlchemy Table for relation Many-To-Many(Place-Amenity)
+# https://docs.sqlalchemy.org/en/14/core/metadata.html
+table_relation = Table('place_amenity',
+                       Base.metadata,
+                       Column('place_id',
+                              String(60),
+                              ForeignKey('places.id'),
+                              primary_key=True,
+                              nullable=False),
+                       Column('amenity_id',
+                              String(60),
+                              ForeignKey('amenities.id'),
+                              primary_key=True,
+                              nullable=False))
+
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -19,4 +34,38 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0, nullable=False)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    amenity_ids = []
+    # check if the origin of the data is from db
+    origin_data = os.getenv("HBNB_TYPE_STORAGE")
+    # if come from db_storage
+    if (origin_data == "db"):
+        reviews = relationship('Review', backref='place',
+                               cascade='all, delete')
+        amenities = relationship("Amenity", secondary=place_amenity,
+                                 backref="place", viewonly=False)
+    # if come from file_storage
+    else:
+        @property
+        def reviews(self):
+            """Getter reviews"""
+            reviews = models.storage.all(City)
+            list_reviews = []
+            for idx in reviews.values():
+                if self.id == idx.state_id:
+                    list_reviews.append(idx)
+            return list_reviews
+
+        @property
+        def amenities(self):
+            """Getter for amenities with FileStorage engine"""
+            amenities = models.sotrage.all(Amenity)
+            list_amenity = []
+            for idx in amenities.values():
+                if self.id == idx.amenity_ids:
+                    list_amenity.append(idx)
+            return list_amenity
+
+        @amenities.setter
+        def amenities(self, value):
+            """ Setter for amenities method"""
+            if type(value) is Amenity:
+                amenities.append(value)
