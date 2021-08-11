b8c1bc869fac967445162c3d16b44bb8458ef651
SimonBr017 <simonbrard017@gmail.com>
2021-08-11 13:16:20 +0200

    back nochange

--- models/place.py
+++ models/place.py
@@ -24,35 +24,26 @@ class Place(BaseModel, Base):
                               Column('amenity_id', String(60),
                                      ForeignKey('amenities.id'),
                                      primary_key=True))
-        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-        name = Column(String(128), nullable=False)
-        description = Column(String(1024), nullable=True)
-        number_rooms = Column(Integer, nullable=False, default=0)
-        number_bathrooms = Column(Integer, nullable=False, default=0)
-        max_guest = Column(Integer, nullable=False, default=0)
-        price_by_night = Column(Integer, nullable=False, default=0)
-        latitude = Column(Float, nullable=True)
-        longitude = Column(Float, nullable=True)
-        amenity_ids = []
+
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+    amenity_ids = []
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
         reviews = relationship("Review",
                                cascade="all, delete", backref="place")
         amenities = relationship("Amenity",
                                  secondary=place_amenity, viewonly=False)
 
     else:
-        city_id = ""
-        user_id = ""
-        name = ""
-        description = ""
-        number_rooms = 0
-        number_bathrooms = 0
-        max_guest = 0
-        price_by_night = 0
-        latitude = 0.0
-        longitude = 0.0
-        amenity_ids = []
-
         @property
         def reviews(self):
             """getter atribute reviews"""
@@ -71,8 +62,8 @@ class Place(BaseModel, Base):
             allAmenities = models.storage.all(Amenity)
             placeAmenities = []
 
-            for objAmenity in allAmenities.values():
-                if objAmenity.id in self.amenity_ids:
+            for objAmenity in list(allAmenities.values()):
+                if objAmenity.place_id == self.id:
                     placeAmenities.append(objAmenity)
 
             return (placeAmenities)
