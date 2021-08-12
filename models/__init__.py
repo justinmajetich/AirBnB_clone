419a36f56e1782d2953e46c5ec14a9df308a8d0d
djso89 <danielso89@gmail.com>
2021-08-11 22:25:33 -0700

    task 6

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+import os
+
+HBNB_TYPE_STORAGE = os.getenv('HBNB_TYPE_STORAGE')
+
+if HBNB_TYPE_STORAGE =="db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.enging.file_storage import FileStorage
+    storage = FileStorage()
 
 
-storage = FileStorage()
 storage.reload()
--- models/city.py
+++ models/city.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
+    __tablename__ = 'cities'
     state_id = ""
-    name = ""
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", cascade="all, delete", backref='cities')
