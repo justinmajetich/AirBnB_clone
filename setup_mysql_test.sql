d9349a69ef0a2199919565f3cad91ea004620bb9
ArtisanGray <1697@holbertonschool.com>
2021-08-12 01:49:35 +0000

    created 3 and 4, permission change and cache cleared

---
+++ setup_mysql_test.sql
@@ -0,0 +1,5 @@
+-- creates a database for AirBnb Clone V2
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* to 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema to 'hbnb_test'@'localhost';
