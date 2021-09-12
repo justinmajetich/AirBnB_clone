859a1400d17c741ae8a8ce18f73f92a2e7f8fe32
Adrien MILLOT <adrien.millot@gmail.com>
2021-09-12 09:55:24 +0200

    HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+    script that starts a Flask web application
+"""
+
+from flask import Flask, escape, request
+
+# creates instance of Flask, app is bound to gunicorn when running app server
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+        Hello route
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def index():
+    """
+        Index route
+    """
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    # runs the Flask application through port 5000 from local host
+    app.run(host='0.0.0.0', port='5000')
