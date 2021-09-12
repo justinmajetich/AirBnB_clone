0d14695082e775747e0bcd2a6bc87e2875976a89
Adrien MILLOT <adrien.millot@gmail.com>
2021-09-12 10:01:42 +0200

    C is fun!

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+    script that starts a Flask web application
+"""
+
+from flask import Flask, escape, request
+
+# creates instance of Flask, app is bound to gunicorn when running app server
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+        Hello route
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def index():
+    """
+        Index route
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def show(text):
+    """
+        Show text
+    """
+    text = str(text).replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+if __name__ == '__main__':
+    # runs the Flask application through port 5000 from local host
+    app.run(host='0.0.0.0', port='5000')
