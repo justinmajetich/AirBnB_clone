46b95d53d5c86bb44f6cabde390544c3f2a7f07b
RockoGilbert <evolve2122@gmail.com>
2021-11-22 10:07:58 -0600

    created a new instance method to delete obj from __object if it's equal to none.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,13 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls:
+            return {key: self.__objects[key] for key in self.__objects
+                    if self.__objects[key].__class__ == cls}
+        else:
+            return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +52,9 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+        def delete(self, obj=None):
+            """Method that deletes obj from __objects"""
+            if obj is not None:
+                key = obj.__class__.__name__ + '.' + obj.id
+                if key in self.__objects:
+                    del self.__objects[key]
