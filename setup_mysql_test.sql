a72a6ae46931347505657277f78409eaf9c57062
Ronroeandassociates <3321@holbertonschool.com>
2021-11-22 16:05:09 -0600

    fixed some issues with SQL

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,6 +3,11 @@ CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
 
 USE `hbnb_test_db`;
 
+CREATE USER IF NOT EXISTS 'hbnb_test_db';
+
+SET
+    PASSWORD FOR 'hbnb_test' @'localhost' = 'hbnb_test_pwd';
+
 GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test' @'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 GRANT
