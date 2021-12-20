02ae86b2c0d927147b3b5a5b0a29c920c2952fb1
Juan Bocanegra <jbocane6@gmail.com>
2021-12-20 15:43:51 -0500

    [CREATE] task 4

---
+++ setup_mysql_test.sql
@@ -0,0 +1,5 @@
+-- prepares a MySQL server
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
