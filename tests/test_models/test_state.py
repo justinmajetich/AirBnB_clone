995fce13d50d15663165802df5bf2251fc735f7e
Laurajcb <3398@holbertonschool.com>
2021-12-20 15:03:02 -0500

    update do_create

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-""" """
+""" Test for the State class"""
+import models
+import unittest
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
+from sqlalchemy.exc import OperationalError
 
 
 class test_state(test_basemodel):
-    """ """
+    """ Defines State tests for State class """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ Initialisation of State instance """
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
+        self.state = State(name="California")
+
 
     def test_name3(self):
-        """ """
+        """ Test name of an State instance """
         new = self.value()
-        self.assertEqual(type(new.name), str)
+        self.assertEqual(type(self.state.name), str)
+        self.assertEqual(self.state.name, "California")
