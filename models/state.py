8b6bc0573f0fe5f026075127f96f62d4b61036b5
Sandra Calero <3358@holbertonschool.com>
2021-12-20 22:39:19 -0500

    add attribute cities that represent the relationship between city and state

--- models/state.py
+++ models/state.py
@@ -4,10 +4,23 @@ from sqlalchemy.sql.schema import Column
 from sqlalchemy.sql.sqltypes import String
 from models.base_model import Base, BaseModel
 from sqlalchemy.orm import relationship
+import os
+import models
+from models.city import City
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade='delete')
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref='state', cascade='delete')
+    else:
+        @property
+        def cities(self):
+            """Get cities.
+            Returns the list of City instances with state_id equals to the
+            current State.id"""
+            cities = models.storage.all(City)
+            return [instance for instance in cities.values() if self.id == instance.state_id]
