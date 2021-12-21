ec43f3f9a1a398dccb4bc70a4d361e02c1a7c2fb
JuanSebastianGB <3393@holbertonschool.com>
2021-12-21 01:51:28 +0000

    UPD: Reviewing peep8 and adding condition of drop table if test passed like env

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class to manage database storage for hbnb clone"""
 
+from models.base_model import BaseModel, Base
 from sqlalchemy import create_engine
 from os import getenv
 
@@ -12,14 +13,19 @@ class DBStorage:
     __engine = None
     __session = None
 
-
     def __init__(self):
         """Instantiate an Object of DBStorage"""
         HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
         HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
         HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
         HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
+        HBNB_ENV = getenv('HBNB_ENV')
 
         self.__engine = create_engine(
-            f'mysql+mysqldb://{HBNB_MYSQL_USER}:{HBNB_MYSQL_PWD}@{HBNB_MYSQL_HOST}/{HBNB_MYSQL_DB}'
+            f'mysql+mysqldb://{HBNB_MYSQL_USER}:{HBNB_MYSQL_PWD}@\
+            {HBNB_MYSQL_HOST}/{HBNB_MYSQL_DB}',
+            pool_pre_ping=True
         )
+
+        if HBNB_ENV == 'test':
+            Base.metadata.drop_all(self.__engine)
