99e36298db70ed152b56428ea386a6402c234d7a
Sandra Calero <3358@holbertonschool.com>
2021-12-21 18:24:19 -0500

    update place, review and user to allow to storage in a database

--- models/place.py
+++ models/place.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy.orm import relationship
+import models
 from models.base_model import Base, BaseModel
+import os
 
 
 class Place(BaseModel, Base):
@@ -17,4 +20,16 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0, nullable=False)
     latitude = Column(Float)
     longitude = Column(Float)
-    amenity_ids = []
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        reviews = relationship('Review', backref='place', cascade='delete')
+    else:
+        @property
+        def reviews(self):
+            """Get reviews
+            Returns the list of Review instances with place_id equals to the
+            current Place.id
+            """
+            reviews = models.storage.all(Review)
+            return [instance for instance in reviews.values()
+                    if self.id == instance.place_id]
--- models/user.py
+++ models/user.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from sqlalchemy.orm import relationship
-from sqlalchemy.sql.expression import delete
 from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String
 
@@ -14,3 +13,4 @@ class User(BaseModel, Base):
     first_name = Column(String(128))
     last_name = Column(String(128))
     places = relationship('Place', backref='user', cascade='delete')
+    reviews = relationship('Review', backref='user', cascade='delete')
