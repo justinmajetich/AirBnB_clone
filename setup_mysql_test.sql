ce6b08a46324b42365db7dbd55fba96c5866f70e
Shirley <shircar16@gmail.com>
2021-12-21 12:38:43 -0500

    fixing name databases in USE

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,7 +2,7 @@
 -- create a DataBase and USER
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-USE hbnb_dev_db;
+USE hbnb_test_db;
 GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
 USE performance_schema;
