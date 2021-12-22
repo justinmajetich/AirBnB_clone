b5154fea0da7e3cc2e97b1307912e9b4e46d4b57
johanne101 <jlopez0493@gmail.com>
2021-12-22 21:00:19 +0000

    [add code] pending methods

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,1 +1,34 @@
 
+class DBStorage:
+    """Database storage engine.
+    Private class attributes:
+        __engine: set to None.
+        __session: set to None.
+    """
+
+    __engine = None
+    __session = None
+
+
+    """Public instance methods:
+        __init__(self):
+        all(self, cls=None):
+        new(self, obj):
+        save(self):
+        delete(self, obj=None):
+        reload(self):
+    """
+
+    def __init__(self):
+        """Initialize new engine instance
+        Engine must be linked to:
+         MySQL database, and
+         user created before
+         (hbnb_dev and hbnb_dev_db):
+        """
+        self.__engine = create_engine("mysql".
+                                      format(getenv("HBNB_MYSQL_USER"),
+                                             getenv("HBNB_MYSQL_PWD"),
+                                             getenv("HBNB_MYSQL_HOST"),
+                                             getenv("HBNB_MYSQL_DB")),
+                                      pool_pre_ping=True)
