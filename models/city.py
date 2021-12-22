fffb7b659ec24fc37a987e41d5a6941538657a70
Cristian Oliveros <wolfmanoliveros@gmail.com>
2021-12-21 21:15:14 -0500

    [UPDATED] task 8

--- models/city.py
+++ models/city.py
@@ -10,5 +10,4 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-
-    state = relationship('State', backref="cities")
+    places = relationship('Place', backref="cities")
--- models/place.py
+++ models/place.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.sql.expression import false, null
-from sqlalchemy.sql.sqltypes import Float
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, Float
 from models.user import User
 from models.city import City
 from sqlalchemy.sql.schema import ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-
-
-Base = declarative_base()
+from sqlalchemy.orm import relationship
 
 
 class Place(BaseModel, Base):
@@ -20,11 +15,11 @@ class Place(BaseModel, Base):
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=False)
+    description = Column(String(1024))
     number_rooms = Column(Integer, nullable=False, default=0)
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=False)
-    longitude = Column(Float, nullable=False)
+    latitude = Column(Float)
+    longitude = Column(Float)
     amenity_ids = []
