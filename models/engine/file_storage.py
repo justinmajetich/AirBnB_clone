0908ffd929374ad72a5d28553c5382ea293c744f
KevinAndresG <3348@holbertonschool.com>
2021-12-21 19:11:29 -0500

    fixing the conditions

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -36,22 +36,22 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
         """delete the obj passed as argument"""
-        if obj is None:
+        if not obj:
             return
         else:
             key = "{}.{}".format(type(obj).__name__, obj.id)
