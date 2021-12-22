cf7d2d1a98b94e8b030bb5349555be4acf1ea1e0
Cristian Oliveros <wolfmanoliveros@gmail.com>
2021-12-21 22:12:34 -0500

    [UPDATED] V.2

--- models/review.py
+++ models/review.py
@@ -8,5 +8,5 @@ class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = 'reviews'
     text = Column(String(1024), nullable=False)
-    places_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
