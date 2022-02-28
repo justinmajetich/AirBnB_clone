07451c0ff4f5c78a83bf6e779c746bf29ebcb42f
Code Slayer <3613@holbertonschool.com>
2022-02-28 14:44:51 -0600

    task 2 implemented

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] =='}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -118,13 +118,39 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        args = args.split()
+        if args[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
+        new_instance = HBNBCommand.classes[args[0]]()
         storage.save()
         print(new_instance.id)
         storage.save()
+        if len(args) > 1:
+            key = args[0] + "." + new_instance.id
+            objects = storage.all()
+            new_dict = objects[key]
+            for i in range(1, len(args)):
+                kwargs = args[i].split("=")
+                if kwargs[1][0] == "\"":
+                    value = \
+                    kwargs[1].replace("\"", "").replace("\'", "").replace("_", " ")
+                    setattr(new_dict, kwargs[0], value)
+                    storage.save()
+                elif '.'  in kwargs[1]:
+                    try:
+                        value = float(kwargs[1])
+                        setattr(new_dict, kwargs[0], value)
+                        storage.save()
+                    except:
+                        pass
+                else:
+                    try:
+                        value = int(kwargs[1])
+                        setattr(new_dict, kwargs[0], value)
+                        storage.save()
+                    except:
+                        pass
 
     def help_create(self):
         """ Help information for the create method """
@@ -272,7 +298,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +306,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
