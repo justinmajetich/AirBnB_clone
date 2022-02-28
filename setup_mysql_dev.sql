9b9db460920e1c1db1394476f2148d6c230ed06e
MontyWilliams <3650@holbertonschool.com>
2022-02-28 14:30:14 -0600

    removed incorect syntax in grant statement

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,5 +2,5 @@
 USE mysql;
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'hbnb_dev'@'hbnb_dev_db' WITH GRANT OPTION;;
+GRANT ALL PRIVILEGES ON *.* TO 'hbnb_dev'@'hbnb_dev_db';
 GRANT SELECT PRIVILEGES ON *.* TO 'hbnb_dev'@'performance_schema';
