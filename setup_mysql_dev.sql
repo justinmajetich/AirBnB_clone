dd7d8f52b050176449b2cf51ad5e43d7efdfe236
MontyWilliams <3650@holbertonschool.com>
2022-02-28 14:22:56 -0600

    i think it works but cant run locally

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,6 @@
+-- write a script that cretes database, adds user and sets privs
+USE mysql;
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'hbnb_dev'@'hbnb_dev_db' WITH GRANT OPTION;;
+GRANT SELECT PRIVILEGES ON *.* TO 'hbnb_dev'@'performance_schema';
