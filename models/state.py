ba41d4aa217b5c3cab21e4c3d2243b358bd0434b
tsims115 <tsims115@yahoo.com>
2022-03-02 14:04:14 -0800

    Adding task 6

--- models/state.py
+++ models/state.py
@@ -10,5 +10,16 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship('City', cascade="all, delete, delete-orphan",
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', cascade="all, delete, delete-orphan",
                           backref="state")
+    else:
+        @property
+        def cities(self):
+            """Cities getter for FileStorage"""
+            from models import storage
+            city_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
