0cba906d11d72793ce62673401393a7ba9420d13
adamclear <3589@holbertonschool.com>
2022-03-03 04:02:54 +0000

    updated user

--- models/user.py
+++ models/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import Base, BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 
 
