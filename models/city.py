f1a0d4cb2499458111ec3e27336e6b33baae7166
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-03-10 00:56:52 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -7,3 +7,7 @@ class City(BaseModel):
     """ The city class, contains state ID and name """
     state_id = ""
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Place"""
+        super().__init__(*args, **kwargs)
