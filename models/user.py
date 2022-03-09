243b50980895aedf73e5abf9df5220c543a2a758
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-03-10 00:59:32 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -9,3 +9,7 @@ class User(BaseModel):
     password = ''
     first_name = ''
     last_name = ''
+
+    def __init__(self, *args, **kwargs):
+        """initializes Place"""
+        super().__init__(*args, **kwargs)
