f034099c1fa5dc2ca66b92cd5a2f144ddd0decba
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-03-10 00:59:01 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -6,3 +6,7 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """ State class """
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Place"""
+        super().__init__(*args, **kwargs)
