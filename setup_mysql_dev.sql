78df53e1012a941986d056aff5cb03c646921bab
EricAltez <ericaltez1111@gmail.com>
2022-04-10 15:36:51 -0700

    corrected file 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,5 @@
--- creating our database
-
+--  prepares a MySQL server for the project: hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-USE hbnb_dev_db;
-CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY hbnb_dev_pwd;
-GRANT ALL PRIVILEGES ON hbnb_dev_db TO hbnb_dev@localhost;
-GRANT SELECT PRIVILEGES ON performance_schema TO hbnb_dev@localhost;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
