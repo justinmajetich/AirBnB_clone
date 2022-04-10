ae23b002603687f78570c35c5906a353a8e4b65e
EricAltez <ericaltez1111@gmail.com>
2022-04-10 15:44:38 -0700

    corrected file 4 typo

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,4 @@
---  prepares a MySQL server for the project: hbnb_dev_db
+--  prepares a MySQL server for the project: hbnb_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
