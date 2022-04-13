8f30e46b3d678c955400a2649ea04cb093c84f0f
EricAltez <ericaltez1111@gmail.com>
2022-04-13 10:39:47 -0700

    various changes

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -9,3 +10,4 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Place', backref='cities')
--- models/state.py
+++ models/state.py
@@ -10,7 +10,7 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state')
+    cities = relationship('cities', backref='state')
 
     @property
     def cities(self):
