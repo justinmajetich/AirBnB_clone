954ba4d425d5f035d2f8c6463cb26fc9d2eb570a
Marcosozhol <3956@holbertonschool.com>
2022-04-15 14:50:39 -0700

    Correction error

--- models/state.py
+++ models/state.py
@@ -1,23 +1,37 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""This is the state class"""
 from models.base_model import BaseModel, Base
-from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import environ
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """This is the class for State
+    Attributes:
+        __tablename__: name of MySQL table
+        name: input name
+    """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship('Cities', backref='state')
 
-    @property
-    def cities(self):
-        """List of City instances with id of state"""
-        from models import storage
-        new = []
-        for key, obje in storage.all(City).items():
-            if obje.state_id == self.id:
-                new.append(obje)
-        return new
+    if environ['HBNB_TYPE_STORAGE'] == 'db':
+        cities = relationship('City', cascade='all, delete', backref='state')
+    else:
+        @property
+        def cities(self):
+            """Getter method for cities
+            Return: list of cities with state_id equal to self.id
+            """
+            from models import storage
+            from models.city import City
+            # return list of City objs in __objects
+            cities_dict = storage.all(City)
+            cities_list = []
+
+            # copy values from dict to list
+            for city in cities_dict.values():
+                if city.state_id == self.id:
+                    cities_list.append(city)
+
+            return cities_list
