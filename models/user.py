ef46a5bc548afe769cb8e9a975661e0a4f7c505d
Marcosozhol <3956@holbertonschool.com>
2022-04-15 15:48:01 -0700

    Correction code prev

--- models/user.py
+++ models/user.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
-"""This is the user class"""
+"""This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
-    """
+    """This class defines a user by various attributes"""
+    __tablename__ = 'users'
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    reviews = relationship('Review', backref='user', cascade='all, delete')
-    places = relationship('Place', backref='user', cascade='all, delete')
+    places = relationship('Place', backref='user')
+    reviews = relationship('Review', backref='user')
