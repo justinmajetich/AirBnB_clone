a5385722cf66a2146657139132071e61a34690cb
Marcosozhol <3956@holbertonschool.com>
2022-04-15 15:44:42 -0700

    Correction code

--- models/user.py
+++ models/user.py
@@ -14,11 +14,9 @@ class User(BaseModel, Base):
         last_name: last name
     """
     __tablename__ = 'users'
-
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-
-    reviews = relationship('Review', cascade='all, delete', backref='user')
-    places = relationship('Place', cascade='all, delete', backref='user')
+    reviews = relationship('Review', backref='user', cascade='all, delete')
+    places = relationship('Place', backref='user', cascade='all, delete')
