555b4fa707dc6425b33f27fc287394ed84488cab
Marcosozhol <3956@holbertonschool.com>
2022-04-15 15:37:17 -0700

    Correction comments

--- models/place.py
+++ models/place.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import relationship
 
 
 class Place(BaseModel, Base):
+    """ A place to stay """
     __tablename__ = 'places'
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
