1a2941a5f19d129185add47a66b5b98d422a9a61
Marcosozhol <3956@holbertonschool.com>
2022-05-13 11:41:32 -0300

    Task 0 - Correction errors pep8

--- 0-hello_route.py
+++ 0-hello_route.py
@@ -6,12 +6,13 @@
 """
 from flask import Flask
 
-app=Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
     """Print Hello HBNB!"""
     return 'Hello HBNB!'
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(host="0.0.0.0")
