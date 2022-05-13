c8d9eddff1c8b4b9678507bc8ae0c549b925e08d
Marcosozhol <3956@holbertonschool.com>
2022-05-13 11:37:50 -0300

    Task 0

---
+++ 0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Your web application must be listening on 0.0.0.0, port 5000
+    Routes:
+    /: display “Hello HBNB!”
+    ou must use the option strict_slashes=False in your route definition
+"""
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Print Hello HBNB!"""
+    return 'Hello HBNB!'
+
+if __name__=='__main__':
+    app.run(host="0.0.0.0")
