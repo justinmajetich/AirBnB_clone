ee69fcffc511f15d01319e19e150620a298b5860
Andrés Cataño <3866@holbertonschool.com>
2022-05-14 13:09:39 -0500

    Task 6 Partial

--- models/state.py
+++ models/state.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from os import getenv
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+
+    if getenv("HBNB_TYPE_STORAGE") is "db":
+        cities = relationship("City", cascade="all, delete")
+    else:
+        @property
+        def cities(self):
+            """Return list of cities """
+            
