601b89fd79c0703100f9b64d376729c692b19e9c
Camiloarceg <camiloarceg@gmail.com>
2022-05-15 10:23:03 -0500

    try to fix task 2 checker

--- console.py
+++ console.py
@@ -118,30 +118,19 @@ class HBNBCommand(cmd.Cmd):
         try:
             if not args:
                 raise SyntaxError()
-        
-            # new code
             arg_list = args.split(" ")
-            
             kw = {}
             for arg in arg_list[1:]:
                 arg_splited = arg.split("=")
-                
                 arg_splited[1] = eval(arg_splited[1])
-
                 if type(arg_splited[1]) is str:
-                    arg_splited[1] = arg_splited[1].replace("_", " ")
-
+                    arg_splited[1] = arg_splited[1].replace("_", " ").replace('"', '\\"')
                 kw[arg_splited[0]] = arg_splited[1]
-
         except SyntaxError:
             print("** class name missing **")
         except NameError:
             print("** class doesn't exist **")
-
-        # end new code
-        #print(kw)
         new_instance = HBNBCommand.classes[arg_list[0]](**kw)
-        # new_instance = HBNBCommand.classes[args]()
         storage.new(new_instance)
         storage.save()
         print(new_instance.id)
