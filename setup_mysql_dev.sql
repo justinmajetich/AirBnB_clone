8588df224634d15daa4cece18ce3c26fd7ece096
Medi <medi@Medis-MacBook-Pro.local>
2022-05-15 03:28:05 -0500

    Adding sql

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,5 @@
+-- PREPARING A MYSQL SERVER (dev_medi)
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
