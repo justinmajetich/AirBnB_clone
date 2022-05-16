7e00c92f50ed7606e77f32588a9d029a695f9c48
Andrés Cataño <3866@holbertonschool.com>
2022-05-16 13:51:52 -0500

    Task 10

--- models/place.py
+++ models/place.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
+from curses import setupterm
 from os import getenv
+from re import A
+from sys import settrace
+from AirBnB_clone_v2.models import amenity
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Integer, Float
 from sqlalchemy.orm import relationship
+
+place_amenity = Table('place_amenity', Base.metadata, Column('place_id',
+                String(60), ForeignKey('places.id'), nullable=False),
+                Column('amenity_id', String(60),
+                ForeignKey('amenities.id'), nullable=False))
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -19,9 +28,14 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
+    
+
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
         reviews = relationship("Review", backref='place', cascade="all, delete")
+        amenities = relationship("Amenity", secondary=place_amenity,
+                            viewonly=False, cascade="all, delete",
+                            backref="places")
     else:
         from models import storage
         @property
@@ -34,3 +48,21 @@ class Place(BaseModel, Base):
                 if place.id == self.place_id:
                     reviews.append(place)
             return reviews
+
+        @property
+        def amenities(self):
+            """ Function that returns the list of City
+            instances with state_id """
+
+            amenities_list = []
+            for amenity in storage.all(Amenity).values():
+                if amenity.id == self.amenity_id:
+                    amenities_list.append(amenity)
+            return amenities_list
+ 
+
+        @amenities.setter 
+        def ameninities(self, arg):
+            """ Function setter amenities """
+            if arg.__class__.name__ == "Amenity":
+                self.amenity_ids.append(arg)
