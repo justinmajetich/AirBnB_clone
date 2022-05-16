e507d2956bba16c47ee51cea77bf54da577ca399
Andrés Cataño <3866@holbertonschool.com>
2022-05-16 11:49:41 -0500

    Fix syntaxwarning task 2

--- models/state.py
+++ models/state.py
@@ -12,7 +12,7 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
 
-    if getenv("HBNB_TYPE_STORAGE") is "db":
+    if getenv("HBNB_TYPE_STORAGE") == "db":
         cities = relationship("City", backref="state", cascade="all, delete")
     else:
         @property
