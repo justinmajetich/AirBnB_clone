d1ad025bda9c76383914cb78db1dd5f96a3b1727
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-05-16 20:53:33 -0500

    fix: Try on 10

--- models/place.py
+++ models/place.py
@@ -4,13 +4,20 @@ from models.base_model import BaseModel, Base
 from models.review import Review
 from sqlalchemy import Column, Integer, Float, String, ForeignKey, Table
 from sqlalchemy.orm import relationship
+from os import getenv
 import models
-import os
+
+place_amenity = Table('place_amenity', Base.metadata,
+        Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+        Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)
+    )
+
+env = getenv('HBNB_TYPE_STORAGE')
 
 class Place(BaseModel, Base):
     """ A place to stay """
-    __tablename__ = 'places'
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    if env == 'db':
+        __tablename__ = 'places'
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
@@ -21,23 +28,8 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
-        reviews = relationship("Review", backref="user", cascade="delete")
-        place_amenity = Table('place_amenity', Base.metadata,
-            Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
-            Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)
-        )
-        amenities = relationship("Amenity", secondary=place_amenity, viewonly=False)
-    else:
-        city_id = ""
-        user_id = ""
-        name = ""
-        description = ""
-        number_rooms = 0
-        number_bathrooms = 0
-        max_guest = 0
-        price_by_night = 0
-        latitude = 0.0
-        longitude = 0.0
+        reviews = relationship("Review", backref="place", cascade="delete")
+        amenities = relationship("Amenity", secondary=place_amenity, viewonly=False, backref='places')
         amenity_ids = []
 
         @property
