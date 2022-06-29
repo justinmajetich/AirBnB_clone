190e1f51577dc3b0250c3ca4370669818fc6e601
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-06-29 14:29:28 -0500

    Add files via upload

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,6 +2,8 @@
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
+USE hbnb_test_db;
+
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
