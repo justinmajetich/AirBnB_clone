f501b5340b28899574811d289f5d72a41158f15a
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-06-29 13:52:19 -0500

    Add files via upload

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,14 +2,12 @@
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
-USE hbnb_test_db;
-
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
-GRANT USAGE ON *.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
 
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
 FLUSH PRIVILEGES;
