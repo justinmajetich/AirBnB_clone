f1d968f5c1c7bff96cc977748c95e16369b7dd8e
bashthat <3249@holbertonschool.com>
2022-06-30 16:49:39 -0500

    ameneties

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from re import X
 from models.base_model import BaseModel
-
+import os
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy.orm import relationship
 
 class Amenity(BaseModel):
-    name = ""
+    """
+    creates the tables of amenities
+    """
+    __tablename__ = "amenities"
+    name = Column(128), nullable = False
+    #ideas
+    #age = Column(Integer, default=1)
+    #gender = Column(String(30), default='female')
+    #name = ""
+    #def __init__(self, *args, **kwargs):
+    #super().__init__(*args, **kwargs)
