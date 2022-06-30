71973a9bc367fa43eb8bf1683e6fcf1148736417
root <blackfox950@gmail.com>
2022-06-30 11:52:20 -0700

    update

---
+++ models/engine/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""makes OBJ for file storage"""
+import os
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+if os.getenv('HBNB_TYPE_STORAGE') == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
+
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
