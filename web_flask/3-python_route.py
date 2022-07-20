9cead1836028a3141827a65acb785418a7055b38
SamuelTownsend <samjaytee13@gmail.com>
2022-07-20 08:07:34 -0700

    3 commit

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+This module starts a Flask web application.
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """
+    Displays "Hello HBNB!" on the main page.
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    Displays "HBNB"
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """
+    Displays "C" followed by the text in <text>. All
+    underscores replaced with whitespace.
+    """
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text='is cool'):
+    """
+    Displays "Python" followed by the text in <text>. If
+    no text is provided, default is "is cool". All
+    underscores replaced with whitespace.
+    """
+    return "Python {}".format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
