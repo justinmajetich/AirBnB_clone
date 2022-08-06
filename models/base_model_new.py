c9f8fb50068d36781d2bedf2e8c5b140a12fa3ec
James Honey <jashoney@hotmail.com>
2022-08-06 15:14:58 +1000

    updated base_model_new

--- models/base_model_new.py
+++ models/base_model_new.py
@@ -26,7 +26,7 @@ class BaseModel:
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
-        self.__dict__.update(kwargs)
+            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
