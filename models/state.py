875108570b790e75b56ccc3d6455129a96b8f045
yousseffabid <abidyoussef58@gmail.com>
2022-08-07 12:38:20 +0100

    task 6 and corrected task 3 & 4

--- models/state.py
+++ models/state.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import String, ForeignKey, Column
+from models.base_model import Base
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,15 +1,18 @@
 -- Create a MySQL server
--- Database: hbnb_test_db
--- User: hbnb_test
--- Password: hbnb_test_pwd
+-- Database: hbnb_dev_db
+-- User: hbnb_dev
+-- Password: hbnb_dev_pwd
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
-IDENTIFIED BY 'hbnb_dev_pwd';
-
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
+CREATE USER
+    IF NOT EXISTS 'hbnb_dev'@'localhost'
+    IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES
+   ON `hbnb_dev_db`.*
+   TO 'hbnb_dev'@'localhost'
+   IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT
+   ON `performance_schema`.*
+   TO 'hbnb_dev'@'localhost'
+   IDENTIFIED BY 'hbnb_dev_pwd';
 FLUSH PRIVILEGES;
