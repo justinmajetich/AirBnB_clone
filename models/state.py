d7d9be6d94a4df3077164d97ed34633128a3f8f8
Karoline-S <karoline.amy@gmail.com>
2022-08-07 20:05:39 -0700

    fix - minor typos

--- models/state.py
+++ models/state.py
@@ -8,8 +8,8 @@ import models
 
 class State(BaseModel):
     """ State class """
-     __tablename__ = "states"
-    name = Column(String(128) nullable=False)
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
 
     cities = relationship("City", backref="state", cascade="all, delete-orphan")
 
@@ -19,7 +19,6 @@ class State(BaseModel):
         state_id equals to the current State.id"""
         city_list = []
         for city in  models.storage.all("City").values():
-            if city.state_id = self.id:
+            if city.state_id == self.id:
                 city_list.append(city)
         return city_list
-
