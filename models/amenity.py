dfb6a4fa7c2ea3fd29f14617931542218b7f12a4
jql11 <4325@holbertonschool.com>
2022-08-08 03:08:40 -0700

    update for task10

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 
 
 class Amenity(BaseModel):
-    name = ""
+    """amenity"""
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
