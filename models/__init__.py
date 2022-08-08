fa558950cfa0dec1ab47ee8a69d14d10447ac3c5
jql11 <4325@holbertonschool.com>
2022-08-08 00:39:53 -0700

    update for task6

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+import os
 
+"""Add a conditional depending of the value of the environment variable"""
+if os.getenv("HBNB_TYPE_STORAGE") == "db":
+    storage = DBStorage()
+else:
+    storage = FileStorage()
 
-storage = FileStorage()
 storage.reload()
