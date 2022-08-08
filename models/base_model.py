91601fe93c792b02d6c5d46dbe6e956379f76c23
jql11 <4325@holbertonschool.com>
2022-08-07 17:49:26 -0700

    update for task6

--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,16 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 
 class BaseModel:
     """A base class for all hbnb models"""
+    __tablename__ = "cities"
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    state_id = Column(DateTime, default=datetime.utcnow(), nullable=False)
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
@@ -13,12 +19,15 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
+            for key, val in kwargs.items():
+                if "__class__" not in key:
+                    setattr(self, key, val)
+
             del kwargs['__class__']
             self.__dict__.update(kwargs)
 
@@ -31,6 +40,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
@@ -41,4 +51,10 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if hasattr(self, '_sa_instance_state'):
+            del dictionary["_sa_instance_state"]
         return dictionary
+
+    def delete(self):
+        """deletes an object"""
+        models.storage.delete(self)
