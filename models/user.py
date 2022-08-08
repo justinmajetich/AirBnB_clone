43987ef33dd17f11c4843de11c56d3a5ffbe3a26
OmaymaK <emmablink050@gmail.com>
2022-08-08 12:33:42 +0100

    FML

--- models/user.py
+++ models/user.py
@@ -9,14 +9,8 @@ from sqlalchemy.orm import relationship
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     
-    if models.storage_t == 'db':
         __tablename__ = "users"
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
