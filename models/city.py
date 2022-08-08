0dc5e8c1fffc3c1f0365373187696ae1dab8636c
Karoline-S <karoline.amy@gmail.com>
2022-08-07 20:48:05 -0700

    fix - add import of State

--- models/city.py
+++ models/city.py
@@ -2,10 +2,11 @@
 """ City Module for HBNB project """
 from sqlalchemy import Column, String, ForeignKey
 from models.base_model import BaseModel, Base
+from models.state import State
 
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-     __tablename__ = "cities"
+    __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey(State.id), nullable=False)
