47ca3a8435156294f360578c340682a2b120c2e5
Chiara Caprasi <91517809+chiaracaprasi@users.noreply.github.com>
2022-08-08 10:36:45 +1000

    fix: add documentation to setup_mysql_test.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,5 @@
+-- setting up MySQL server for the project
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db; 
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY hbnb_dev_pwd;
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
