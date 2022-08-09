43bf2cac0724571d8b803226409d204eb13a75b5
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-09 11:22:40 -0400

    Updated FileStorage with delete instance

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,7 +8,7 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         return FileStorage.__objects
 
@@ -48,3 +48,13 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        if obj is None:
+            return
+        temp = {}
+        key = obj.to_dict()['__class__'] + '.' + obj.id
+        try: 
+            del FileStorage.__objects[key]
+        except (KeyError):
+            return
