77817e298635eb303465eedcfd99b358bfa63048
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-09 11:46:18 -0400

    Updating the all instance

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,6 +10,8 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls is None:
+            return FileStorage.__objects
         return FileStorage.__objects
 
     def new(self, obj):
