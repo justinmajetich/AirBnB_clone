57ac2251c7e769367b4b717ddcaa258deb9f47f9
Your Name <yohel.cruz.espinoza@gmail.com>
2022-08-10 15:14:13 -0700

    Added DB to city.py

--- models/city.py
+++ models/city.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
 
+from models.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy import Column, String, ForeignKey
 
-class City(BaseModel):
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+
+else:
+    class City(BaseModel):
+    """ The city class, contains state ID and name """
+        state_id = ''
+        name = ''
