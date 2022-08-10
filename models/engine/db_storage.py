10c2833d383afe082a9a9096d60a82ad98b710b3
Joanfa7 <jafabre95@gmail.com>
2022-08-10 16:09:15 +0000

    ading function names to file db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -12,15 +12,35 @@ env = getenv(HBNB_ENV)
 
 class DBStorage():
 
-        __engine = None
+    __engine = None
         __session = None
 
 
     def __init__(self):
         __engine = self.__engine
         __engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                    format(user, passwd, host, db), pool_pre_ping=True)
+                format(user, passwd, host, db), pool_pre_ping=True)
 
-        def drop():
-            if env = 'test':
-                .drop()
+    def drop():
+        " Delete table is env value is 'test'"
+        if env = 'test':
+            .drop()
+
+
+    def all (self, cls=None):
+        " Return dictionary of all the objects in the file "
+
+
+    def new(self, obj):
+        " add the object to the current database session "
+
+
+    def save(self):
+        " Commit all changes of hte current database session "
+
+
+    def delete(self, obj=None):
+        " delete form the current database session"
+
+    def reload(self):
+        " Create all tables in the database, and creates the current session "
