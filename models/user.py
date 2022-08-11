c4c7a546c01e7febdac3e0831003368592ff9710
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-08-10 19:21:09 -0500

    Update User (task 7)

--- models/user.py
+++ models/user.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    class User(BaseModel, Base):
+        """ The User class, contains email, password, first_name
+        and last_name """
+
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+        places = relationship('Place', backref='user',
+                              cascade='all, delete, delete-orphan')
+else:
+    class User(BaseModel):
+        """This class defines a user by various attributes"""
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
