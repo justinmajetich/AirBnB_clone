5ca7181109c9cf6a61eea972e93464417179586d
Your Name <yohel.cruz.espinoza@gmail.com>
2022-08-10 18:02:38 -0700

    Added Base in import

--- models/state.py
+++ models/state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from os import getenv
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
