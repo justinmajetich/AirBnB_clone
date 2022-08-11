401388ed0f6e83a0c4d87e2219c18ea57c0bfa29
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-08-10 19:22:01 -0500

    Update Place, User and City (task 8)

--- models/city.py
+++ models/city.py
@@ -4,17 +4,20 @@
 from models.base_model import BaseModel, Base
 from os import getenv
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
     class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+        """ The city class, contains state ID and name """
 
         __tablename__ = 'cities'
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
+        places = relationship('Place', backref='cities',
+                              cascade='all, delete, delete-orphan')
 
 else:
     class City(BaseModel):
-    """ The city class, contains state ID and name """
+        """ The city class, contains state ID and name """
         state_id = ''
         name = ''
--- models/place.py
+++ models/place.py
@@ -1,18 +1,38 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 
 
-class Place(BaseModel):
-    """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    class Place(BaseModel, Base):
+        """ The Place class, contains city_id, user_id, name,
+        description, number_rooms, number_bathrooms, max_guest,
+        price_by_night, latitude and longitude """
+
+        __tablename__ = 'places'
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=False)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float, nullable=False)
+        longitude = Column(Float, nullable=False)
+else:
+    class Place(BaseModel):
+        """This class defines a place by various attributes"""
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
