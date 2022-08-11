16da11497ddc277a29808eee35dba4e6f8a3372e
Joanfa7 <jafabre95@gmail.com>
2022-08-11 18:03:28 +0000

    quick fix

--- models/city.py
+++ models/city.py
@@ -6,24 +6,9 @@ from os import getenv
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name
-
+    """ The city class, contains state ID and name"""
     __tablename__ = "cities"
-<<<<<<< HEAD
-
-    if getenv("HBNB_TYPE_STORAGE") != "db":
-        name = ""
-        state_id = ""
-    else:
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    """
-    __tablename__ = "cities"
-    name = ""
-    state_id = ""
-=======
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id", ondelete="CASCADE"),
                       nullable=False)
-    places = relationship("Places", cascade="all, delete-orphan", backref="cities")
->>>>>>> jose
+    places = relationship("Places", cascade="all, delete-orphan", backref="cities"
