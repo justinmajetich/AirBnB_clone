613e18640735120da00c98c9912a2af372b9111f
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-08-11 00:05:41 -0500

    Fix typos in console.py

--- console.py
+++ console.py
@@ -230,7 +230,7 @@ class HBNBCommand(cmd.Cmd):
                 if k.split('.')[0] == args:
                     print_list.append(str(v))
         else:
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.all().items():
                 print_list.append(str(v))
 
         print(print_list)
