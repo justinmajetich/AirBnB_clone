3e7589d567fba89fe0efaec9e7149b370f57b2c1
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-11 10:57:16 -0400

    User file

--- models/user.py
+++ models/user.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from ast import Str
 from sqlalchemy import Column, String
 from models.base_model import BaseModel, Base
 from os import getenv
@@ -10,14 +9,7 @@ class User(BaseModel, Base):
     """This class defines a user by various attributes"""
 
     __tablename__ = "users"
-
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
