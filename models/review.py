fe798a53c1a126330f11842453c3236b1b423ac0
Joanfa7 <jafabre95@gmail.com>
2022-08-12 13:35:12 +0000

    fix typo

--- models/review.py
+++ models/review.py
@@ -11,4 +11,4 @@ class Review(BaseModel, Base):
     __tablename__ = "reviews"
     text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("user.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
