d1ba67e5afc38f03c8822713e2836d62c31624f7
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-25 15:44:05 -0400

    Hot Fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,32 +4,37 @@
 apt-get update -y;
 apt-get install -y nginx;
 
-mkdir -p /data/web_static/releases/;
 mkdir -p /data/web_static/shared/;
 mkdir -p /data/web_static/releases/test/;
-touch -p /data/web_static/releases/test/index.html;
+
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
 echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" >> /data/web_static/releases/test/index.html;
+</html>" > /data/web_static/releases/test/index.html
 
-sudo ln -sf /data/web_static/current /data/web_static/releases/test/;
-sudo chown -R ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
 printf %s "server {
   listen 80 default_server;
   listen [::]:80 default_server;
 
-  root /data/web_static/current;
+  root /etc/nginx/html;
   index index.html;
 
-  location /static {
-    alias /hbnb_static/;
+  location /hbnb_static {
+    alias /data/web_static/current/;
+    index index.html;
+  }
+
+  location /redirect_me {
+    return 301 https://i.imgur.com/guDXtsC.png;
   }
 }
-" >> /etc/nginx/sites-available/default;
+" > /etc/nginx/sites-available/default
 
 sudo service nginx restart
