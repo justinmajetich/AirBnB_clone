fe0b030609b2ba38b308267a6e873b25da8e420f
Alejo Aragon <alejo.aragon.05@gmail.com>
2022-09-06 14:04:56 -0500

    update_project

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -5,3 +5,4 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
 SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
