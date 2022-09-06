fea4bca0b9817ecef0c407700c2513456621811b
soft-insight <j0108rodriguez@gmail.com>
2022-09-06 18:28:54 -0500

    changes in console

--- models/__init__.py
+++ models/__init__.py
@@ -3,13 +3,6 @@
 import os
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
 
 
 if os.getenv("HBNB_TYPE_STORAGE") == 'db':
