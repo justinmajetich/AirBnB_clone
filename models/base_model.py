eff171ff9efb358dca15005315285881a0817d72
Edwin-9603 <egc4611@gmail.com>
2022-09-05 21:44:00 -0400

    new updates

--- models/base_model.py
+++ models/base_model.py
@@ -14,6 +14,12 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
+        elif kwargs and kwargs.get('updated_at') is None:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            for key, value in kwargs.items():
+                setattr(self, key, value)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -31,6 +37,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
