3fcf4860ad9a49af3da2dca51dbd94ab8c429570
Edwin-9603 <egc4611@gmail.com>
2022-09-07 12:26:07 -0400

    new updates

--- models/place.py
+++ models/place.py
@@ -11,7 +11,6 @@ class Place(BaseModel, Base):
 	""" A place to stay """
 	__tablename__ = 'places'
 	if (getenv('HBNB_TYPE_STORAGE') == 'db'):
-		
 		city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
 		user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
 		name = Column(String(128), nullable=False)
