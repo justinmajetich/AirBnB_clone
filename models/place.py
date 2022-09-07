7a59540485aebbf369db250f85a6c9b8f061814b
Edwin-9603 <egc4611@gmail.com>
2022-09-07 12:00:36 -0400

    new updates

--- models/place.py
+++ models/place.py
@@ -33,7 +33,7 @@ class Place(BaseModel, Base):
 			return reviews_by_place
 		place_amenity = Table('place_amenity', Base.metadata,
 			Column('place_id', String(60),ForeignKey('places.id') ,primary_key=True, nullable=False),
-			Column('amenity_id',String(60),ForeignKey('amenities.id') ,primary_key=True, nullable=False )
+			Column('amenity_id',String(60),ForeignKey('amenities.id') ,primary_key=True, nullable=False)
 		)
 		amenities = relationship('Amenity', secondary=place_amenity, viewonly=False)
 	else:
