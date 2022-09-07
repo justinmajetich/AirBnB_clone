52889a546881515ec62f4dd6272ca5d1ae255f13
Edwin-9603 <egc4611@gmail.com>
2022-09-07 00:48:03 -0400

    new updates

--- models/place.py
+++ models/place.py
@@ -2,6 +2,7 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, ForeignKey, String, Float, Integer
+from models.review import Review
 from os import getenv
 from sqlalchemy.orm import relationship
 
@@ -19,3 +20,13 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
+    reviews = relationship('Review', backref='place', cascade='all, delete')
+    @property
+    def reviews(self):
+        """ Return the list of Reviews by Place """
+        from models import storage
+        reviews_by_place = []
+        for rev in storage.all(Review).values():
+            if rev.place_id == self.id:
+                reviews_by_place.append(rev)
+        return reviews_by_place
