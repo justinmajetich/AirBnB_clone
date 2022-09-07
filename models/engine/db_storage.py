41e059e25e7ff7dc35b0ff6ec245dc4cb254ce8b
tluciad <tluciad@gmail.com>
2022-09-06 20:36:07 -0500

    pycodestyle

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -29,9 +29,9 @@ class DBStorage:
         HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
         HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
         HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
-            HBNB_MYSQL_USER, HBNB_MYSQL_PWD, HBNB_MYSQL_HOST, HBNB_MYSQL_DB),
-                pool_pre_ping=True)
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(HBNB_MYSQL_USER, HBNB_MYSQL_PWD, HBNB_MYSQL_HOST, HBNB_MYSQL_DB),
+                                      pool_pre_ping=True)
 
         if getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
--- models/state.py
+++ models/state.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, DateTime
 from os import getenv
 from sqlalchemy.orm import relationship
+from models.city import City
 import models
 import os
 
@@ -11,17 +12,19 @@ import os
 class State(BaseModel):
     """ State class """
     if getenv("HBNB_TYPE_STORAGE") == 'db':
-        name = Column(String(128), nullable=False)
         __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
         cities = relationship('City', backref='State', cascade='delete')
     else:
+        name =""
 
+    if getenv("HBNB_TYPE_STORAGE") != 'db':
         @property
         def cities(self):
             """getter method"""
             city_list = []
-
-            for city in models.storage.all("City").values():
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
