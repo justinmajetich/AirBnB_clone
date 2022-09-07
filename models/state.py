f68dad1ce2dce50f65e29955d429ab2f3286b54a
DanielN24 <capdaniel_98@outlook.com>
2022-09-06 21:27:09 -0500

    correction file

--- models/state.py
+++ models/state.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, DateTime
-from os import getenv
-from sqlalchemy.orm import relationship
-from models.city import City
 import models
+import os
+from models.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy import Column, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
     """ State class """
+    
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-        
+    
     if getenv("HBNB_TYPE_STORAGE") != 'db':
+
         @property
         def cities(self):
-            """getter method"""
+            """getter method, returns list de City objs"""
             city_list = []
-            all_cities = models.storage.all(City)
-            for city in all_cities.values():
-                if city.state_id == self.id:
-                    city_list.append(city)
+            city_dict = models.storage.all(models.city.City)
+            for key, value in city_dict.items():            
+                if value.state_id == self.id:
+                    city_list.append(value)
             return city_list
-
     else:
         cities = relationship('City', backref='state', cascade='delete')
