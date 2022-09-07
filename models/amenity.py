8708ab750b50dd232df4ba4fd305ee2db2c4bf48
tluciad <tluciad@gmail.com>
2022-09-06 23:33:08 -0500

    amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class Amenity(BaseModel):
+    """Update Amenity"""
+    if (getenv('HBNB_TYPE_STORAGE') == 'db'):
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+
     name = ""
