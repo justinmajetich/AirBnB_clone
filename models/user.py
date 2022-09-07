019b35b0580cf30af70f8fa1fb4c7c41a96966f4
Edwin-9603 <egc4611@gmail.com>
2022-09-06 20:18:47 -0400

    new updates

---
+++ models/user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module defines a class User"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+
+
+class User(BaseModel):
+    """This module defines a class User"""
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
