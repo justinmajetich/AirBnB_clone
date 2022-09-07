3c16d4180e0031a107076fe27ed8a23b4779d30a
DanielN24 <capdaniel_98@outlook.com>
2022-09-06 19:12:21 -0500

    file city and file user

--- models/city.py
+++ models/city.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    state_id = Column(String(128), ForeignKey('state.id'), nullable=False)
+    state_id = Column(String(60), ForeignKey('state.id'), nullable=False)
     name = Column(String(128), nullable=False)
     __tablename__ = 'cities'
 
