dc1d304b5c6d8701ec3a6bf9667c93a0fd5eef45
DanielN24 <capdaniel_98@outlook.com>
2022-09-06 21:17:51 -0500

    correction file

--- console.py
+++ console.py
@@ -131,7 +131,6 @@ class HBNBCommand(cmd.Cmd):
         new_instance = HBNBCommand.classes[args[0]](**parameter)
         new_instance.save()
         print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from os import getenv
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
-from models.engine.file_storage import FileStorage
 
 if (getenv('HBNB_TYPE_STORAGE') == 'db'):
 	from models.engine.db_storage import DBStorage
