7f4b565cd82da56e5ecd6352eec5419f9bd047c7
Edwin-9603 <egc4611@gmail.com>
2022-09-07 01:04:54 -0400

    new updates

--- models/review.py
+++ models/review.py
@@ -6,7 +6,7 @@ from sqlalchemy import Column, String, ForeignKey
 
 
 class Review(BaseModel, Base):
-    """ Review class to store review information """
+    """ Review class to store review  information """
     __tablename__ = 'reviews'
     text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
