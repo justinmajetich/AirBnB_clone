1fef03804d252152257047dad69b6f7dc9deab2c
borrell-44 <alexanderborrell44@hotmail.com>
2022-09-08 11:17:36 -0400

    Committing task 0 and 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,9 +5,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    return "Hello HBNB!"
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=False)
