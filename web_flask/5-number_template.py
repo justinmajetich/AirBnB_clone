44abee02e85532b9141be931d69e2814a884591a
borrell-44 <alexanderborrell44@hotmail.com>
2022-09-09 09:27:44 -0400

    Committign task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Starts a Flask web application that
+accepts a route with a variable and checks
+if variable is an integer"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python/", strict_slashes=False)
+def python_text(text="is cool"):
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def is_number(n):
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    return render_template("5-number.html", n=n)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=False)
