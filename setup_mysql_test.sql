1bd59c8d810b5b9c2864edb0e5a3496c55499c3b
Jasmine <jasmineolofin@gmail.com>
2022-09-12 10:50:33 -0700

    Added a script for test development set-up

---
+++ setup_mysql_test.sql
@@ -0,0 +1,14 @@
+-- MySQL setup test:
+-- Create database hbnb_test_db
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create a database user with following credentials:
+-- username: hbnb_test
+-- password: hbnb_test_pwd
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant permissions on hbnb_test_db database user
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Grant permission on performance_schema database
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
