429fab8306f6324b2db46431cdd8939b18b669ec
Jasmine <jasmineolofin@gmail.com>
2022-09-12 10:10:49 -0700

    Added configuration content for MySQL storage

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,14 @@
+-- MySQL setup development:
+-- Create database hbnb_dev_db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create a database user with following credentials:
+-- username: hbnb_dev
+-- password: hbnb_dev_pwd
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant permissions on hbnb_dev_db database user
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant permission on performance_schema database
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
