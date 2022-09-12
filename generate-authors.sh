0f89954bf77f43549389dcae2f7dcf718dabbb81
MwangiGregory <gmwaburi@gmail.com>
2022-09-12 13:44:23 +0000

    Fix typo

--- generate-authors.sh
+++ generate-authors.sh
@@ -3,8 +3,8 @@ set -e
 
 {
 	cat <<- 'EOH'
-		# This file lists all M*Éividuals having contributed content to the repository.
-		# For how it is generated, see `hack/generate-authors.sh`.
+		# This file lists all individuals having contributed content to the repository.
+		# For how it is generated, see `generate-authors.sh`.
 	EOH
 	echo
 	git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf
