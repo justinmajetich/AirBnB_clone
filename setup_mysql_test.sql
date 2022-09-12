325d16521b555c9f3abe776edd8890b6ffa875a1
devmutinda <davidkymutinda@gmail.com>
2022-09-12 11:02:59 +0000

    MySQL setup test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,4 @@
--- This script repares a MySQL server for the project
+-- This script prepares a MySQL server for the project
 CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
