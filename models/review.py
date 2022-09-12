2e7afb79f1c797cfbf8a655c832f5d9439a9d77d
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-09-12 22:03:48 +0300

    My commit

---
+++ models/review.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines the Review class."""
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+
+
+class Review(BaseModel, Base):
+    """Represents a review for a MySQL database.
+    Inherits from SQLAlchemy Base and links to the MySQL table reviews.
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store Reviews.
+        text (sqlalchemy String): The review description.
+        place_id (sqlalchemy String): The review's place id.
+        user_id (sqlalchemy String): The review's user id.
+    """
+    __tablename__ = "reviews"
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    text = Column(String(1024), nullable=False)
