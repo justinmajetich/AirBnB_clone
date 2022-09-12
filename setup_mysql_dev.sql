3451e42854d816add169f79fb738b1cbd0a3c447
Tijani1402 <99301758+Tijani1402@users.noreply.github.com>
2022-09-12 14:58:42 +0100

    user hbnd_dev having SELECT privileges on the database performance_schema

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,3 +5,6 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 GRANT ALL ON hbnb_dev_db.*
 TO 'hbnb_dev'@'localhost'
 IDENTIFIED BY 'hbnb_dev_pwd';
+-- grant SELECT privileges on performance_schema
+GRANT SELECT ON performance_schema.*
+TO 'hbnb_dev'@'localhost';
