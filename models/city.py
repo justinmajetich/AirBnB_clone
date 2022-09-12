9ab3e757795d14d5b8802c914fed0b89910bcaab
Unique-Red <vnoah410@gmail.com>
2022-09-12 18:17:19 +0100

    city model

--- models/city.py
+++ models/city.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel, Base
+import os
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+from models.base_model import BaseModel, Base
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-
-    __tablename__ = "cities"
-    state_id = Column('state_id', String(60), ForeignKey('states.id'))
-    name = Column('name', String(128), nullable=False)
+    __tablename__ = 'cities'
+    name = Column(
+        String(128), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    state_id = Column(
+        String(60), ForeignKey('states.id'), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    places = relationship(
+        'Place',
+        cascade='all, delete, delete-orphan',
+        backref='cities'
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
