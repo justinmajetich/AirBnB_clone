c1e7ba49cefb66efc804e4244259b0d7f56d3992
Joakim-animate90 <joakimbwire23@gmail.com>
2022-09-13 18:35:25 +0000

    [Update]:new functionalities on State

--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" This is State class"""
+from os import getenv
+from sqlalchemy import String, DateTime, Column, ForeignKey
+from sqlalchemy.orm import relationship
+import models
+from models.base_model import BaseModel, Base
+from models.city import City
+
 
 
 class State(BaseModel):
-    """ State class """
-    name = ""
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref='state',
+                              cascade='all, delete-orphan')
+    else:
+        @property
+        def cities(self):
+            """Getter attribute in case of file storage"""
+            return [city for city in models.storage.all(City).values()
+                    if city.state_id == self.id]
