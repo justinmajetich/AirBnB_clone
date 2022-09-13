86409e765b81962fe10b16dc10be004ebb1cf7e9
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-09-14 00:17:43 +0100

    Add task 7-9

--- models/place.py
+++ models/place.py
@@ -1,18 +1,42 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base, Relationship
+import models
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    FLOAT,
+
+)
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+    if models.storage_type == "db":
+        __tablename__ = "places"
+        city_id = Column(String(128), nullable=False)
+        user_id = Column(String(128), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(128), nullable=False)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(FLOAT(precision=10, scale=2), nullable=True)
+        longitude = Column(FLOAT(precision=10, scale=2), nullable=True)
+        # amenity_ids = "I no sabi weytin i go use dis one do"
+        reviews = Relationship(
+            "Review", cascade="all, delete", backref="place")
+    else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
--- models/user.py
+++ models/user.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base, Relationship
+import models
+from sqlalchemy import (
+    Column,
+    String
+)
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    if models.storage_type == "db":
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+        reviews = Relationship(
+            "Review", cascade="all, delete", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
