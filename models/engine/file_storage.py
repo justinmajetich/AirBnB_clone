84b17f297d3d756986cba08545d02aeb9c1cc524
Veralee225 <Veralee225@gmail.com>
2022-09-13 23:54:56 +0100

    feat: task 5 - updated a prototype

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,8 +8,17 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+# I updated the prototype to def all(self, cls=None)
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls:
+            all = dict()
+            for i in self.__objects.key():
+                if i.split('.')[0] == cls.__name__:
+                    all[i] = self.__objects[i]
+
+            return all
+        
         return FileStorage.__objects
 
     def new(self, obj):
@@ -48,3 +57,12 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+# Added def delete(self, obj = None) public instance method
+def delete(self, obj=None):
+        """delete obj from __objects if it’s inside
+        if obj is equal to None, the method should not do anything"""
+
+        if obj:
+            del(self.__objects["{}.{}".format(type(obj).__name__, obj.id)])
+            del(obj)
