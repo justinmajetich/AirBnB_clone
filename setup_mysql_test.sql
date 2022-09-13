158159a2eaacf5c7dc94af3020a509ed504374c7
akakiplimo <akakiplimo@gmail.com>
2022-09-13 06:04:02 -0700

    Fix test sql setup

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -5,15 +5,7 @@
 --   Grants User SELECT privilege on the database performance_schema (and only this database)
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER
-       IF NOT EXISTS 'hbnb_test'@'localhost'
-       IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES
-      ON `hbnb_test_db`.*
-      TO 'hbnb_test'@'localhost'
-      IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT
-      ON `performance_schema`.*
-      TO 'hbnb_test'@'localhost'
-      IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
