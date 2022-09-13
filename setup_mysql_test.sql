fe8483ee7d215622d63ae01a90a6c70e6b6a958d
holabayor <liasu.olabayo@gmail.com>
2022-09-13 12:51:26 +0100

    fixed user privileges

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,9 +2,7 @@
 -- creates a database
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- create a user and grant privileges
-GRANT ALL ON hbnb_test_db.*
-TO 'hbnb_test'@'localhost'
-IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 -- grant SELECT privileges
-GRANT SELECT ON performance_schema.*
-TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
