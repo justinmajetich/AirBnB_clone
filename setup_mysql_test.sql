5ee1b4146d913578d8dc86ce9f231242c5fe9610
theocode12 <vchidubem9@gmail.com>
2022-09-13 12:03:13 +0100

    setting up database [Task 4, Task 5]

---
+++ setup_mysql_test.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE hbnb_test_db;
+CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
