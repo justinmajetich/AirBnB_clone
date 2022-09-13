dcd07fc30615573395114e9e32e47eca65e889b4
akakiplimo <akakiplimo@gmail.com>
2022-09-13 05:30:09 -0700

    Fix Code pycodestyle

--- console.py
+++ console.py
@@ -14,10 +14,10 @@ from models.amenity import Amenity
 from models.review import Review
 
 
-classes = { 'BaseModel': BaseModel, 'User': User, 'Place': Place,
-            'State': State, 'City': City, 'Amenity': Amenity,
-            'Review': Review
-        }
+classes = {'BaseModel': BaseModel, 'User': User, 'Place': Place,
+           'State': State, 'City': City, 'Amenity': Amenity,
+           'Review': Review
+           }
 
 
 class HBNBCommand(cmd.Cmd):
@@ -75,7 +75,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -119,7 +119,7 @@ class HBNBCommand(cmd.Cmd):
         """ Create an object of any class
         Syntax: create <Class name> <key1>=<value1> <key2>=<value2>...
         """
-        try:        
+        try:
             if not arg:
                 raise SyntaxError()
             my_params = arg.split(" ")
@@ -236,21 +236,6 @@ class HBNBCommand(cmd.Cmd):
         print("[", end="")
         print(", ".join(obj_list), end="")
         print("]")
-#        print_list = []
-#
-#        if args:
-#            args = args.split(' ')[0]  # remove possible trailing args
-#            if args not in HBNBCommand.classes:
-#print("** class doesn't exist **")
-#                return
-#            for k, v in models.storage.all():
-#                if k.split('.')[0] == args:
-#                    print_list.append(str(v))
-#        else:
-#            for k, v in models.storage.all():
-#                print_list.append(str(v))
-#
-#        print(print_list)
 
     def help_all(self):
         """ Help information for the all command """
@@ -309,7 +294,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -317,10 +302,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
@@ -356,6 +341,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
