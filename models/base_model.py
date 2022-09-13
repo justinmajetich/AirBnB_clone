13e32b64523ee0cec8f73da1f90f2f9d319f9b8b
Joakim-animate90 <joakimbwire23@gmail.com>
2022-09-13 19:29:29 +0000

    [Update]:new functionalities

--- models/base_model.py
+++ models/base_model.py
@@ -47,13 +47,13 @@ class BaseModel:
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        if "_sa_instance_state" in my_dict:
+            del my_dict["_sa_instance_state"]
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['updated_at'] = self.updated_at.isoformat()
+        return my_dict
     def delete(self):
         """Delete the current instance from the storage
         (models.storage) by calling the method delete"""
