b8a848c1384e6899ea72e43a88062a1c5e9f31d5
Bolaji5821 <oyedelebolaji84@gmail.com>
2022-09-13 00:00:06 -0700

    MYSQL setup test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,9 @@
+--A script that prepares a test server for the MSQL server for the project
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+USE hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+ALTER USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
