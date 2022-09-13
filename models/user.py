9c41ea07cc7c8262afc681fb21cd000a99ae32a9
Terry <terrybmochire@gmail.com>
2022-09-14 01:04:28 +0300

    Update User model.

--- models/user.py
+++ models/user.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+
+
     email = ''
     password = ''
     first_name = ''
