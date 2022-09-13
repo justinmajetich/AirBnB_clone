f3fbe433403b78ed858a3c4219d28f64b01afaf0
MwangiGregory <gmwaburi@gmail.com>
2022-09-13 23:07:21 +0000

    Update class Review and User
    Added class attributes to class Review that define a schema that has
    a table called reviews that class Review maps to.
    Add a relationship between classes User and Review

--- models/user.py
+++ models/user.py
@@ -12,5 +12,7 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
-    places = relationship("Place", cascade="delete,
-                          delete-orphan", backref='user')
+    places = relationship("Place", cascade="delete, delete-orphan",
+                          backref='user')
+    reviews = relationship("Review", cascade="delete, delete-orphan",
+                           backref='user')
