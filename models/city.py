a9addce5c791d043e4b7d8c3dd2e9e53b755e3bb
middo1 <hamidadeshina1@gmail.com>
2022-09-13 23:34:44 +0100

    okay

--- models/city.py
+++ models/city.py
@@ -16,4 +16,4 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    places = relationship('Place', cascade="all, delete", backref='cities')
+    # places = relationship('Place', cascade="all, delete", backref='cities')
