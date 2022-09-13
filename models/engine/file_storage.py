1f87b7bfc2846aa4ff509f392ee6c3492489c304
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-09-13 09:15:48 +0100

    5. Delete object nouveau protetype de fonction

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,8 +8,9 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        #print("hello all: ",FileStorage.__objects)
         return FileStorage.__objects
 
     def new(self, obj):
@@ -48,3 +49,9 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        if obj is not None:
+            
+            self.save()
+        pass
