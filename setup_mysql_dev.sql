4b4147ddfc9d75c7e812527b8f64ea4c2fefb29c
middo1 <hamidadeshina1@gmail.com>
2022-09-13 13:10:07 +0100

    leggo

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,8 @@
+-- A script that prepares a dev database and user
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+USE hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
