35b4f4cbf2891fb91595051ea4f51a31349f3223
Terry <terrybmochire@gmail.com>
2022-09-14 01:05:14 +0300

    Update User model.

--- models/user.py
+++ models/user.py
@@ -8,15 +8,15 @@ from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        __tablename__ = 'users'
+        __tablename__ = "users"
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
 
-
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
