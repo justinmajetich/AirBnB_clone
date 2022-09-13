4e12af379cd73d60eab9fc34b6312dbf013874dd
Jasmine <jasmineolofin@gmail.com>
2022-09-13 09:54:19 -0700

    Removed trailing whitespace and fault indent

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -63,13 +63,13 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
         """
-            Search and remove a given obj in a list of 
+            Search and remove a given obj in a list of
             instances, otherwise do nothing.
 
             Args:
