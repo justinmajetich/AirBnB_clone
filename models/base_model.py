166c82b1021141baa1ffe9aee962b5eb9d5e3968
abrhamgg <64625512+abrhamgg@users.noreply.github.com>
2022-09-13 23:40:46 +0300

    pep8

--- models/base_model.py
+++ models/base_model.py
@@ -4,17 +4,11 @@ import uuid
 from datetime import datetime
 
 from sqlalchemy import Column, String, DATETIME
-#from sqlalchemy.orm import declarative_base
-
-#Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
-    id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DATETIME, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DATETIME, nullable=False, default=datetime.utcnow())
-
+    
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
