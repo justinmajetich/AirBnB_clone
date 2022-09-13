2b14e0a3bfbcbd1244fb269cb104ce0fca1f37e9
VicvekSr <Olamideoso65@gmail.com>
2022-09-13 22:20:41 +0000

    update user model

--- models/user.py
+++ models/user.py
@@ -1,27 +1,37 @@
 #!/usr/bin/python3
 """This is the user class"""
-from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-from models.place import Place
-from models.review import Review
+from sqlalchemy import Column, String
 
-
-class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
+s = "HBNB_TYPE_STORAGE"
+if cs in environ.keys() and environ["HBNB_TYPE_STORAGE"] == "db":
+    class User(BaseModel, Base):
+        """This is the class for user
+        Attributes:
         email: email address
         password: password for you login
         first_name: first name
         last_name: last name
-    """
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="user")
-    reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                           backref="user")
+        """
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+
+        def __init__(self, **kwargs):
+            setattr(self, "id", str(uuid4()))
+            for i, j in kwargs.items():
+                setattr(self, i, j)
+else:
+    class User(BaseModel):
+        """This is the class for user
+        Attributes:
+        """
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
