d5bbdc09f4df6b644ec553c3142e53ed7f4b1aae
akakiplimo <akakiplimo@gmail.com>
2022-09-13 05:59:10 -0700

    Fix dev sql setup

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,15 +5,7 @@
 --   Grants User SELECT privilege on the database performance_schema (and only this database)
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER 
-       IF NOT EXISTS 'hbnb_dev'@'localhost'
-       IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES 
-      ON `hbnb_dev_db`.*
-      TO 'hbnb_dev'@'localhost'
-      IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT
-      ON `performance_schema`.*
-      TO 'hbnb_dev'@'localhost'
-      IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
