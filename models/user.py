5657e473f8757b9e960a930f32a62be9f24113f6
Terry <terrybmochire@gmail.com>
2022-09-14 01:10:16 +0300

    Update user model.

--- models/user.py
+++ models/user.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+""" holds class User"""
+
+import models
 from models.base_model import BaseModel, Base
 from os import getenv
-from sqlalchemy import Column, Integer, String, ForeignKey
+import sqlalchemy
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from hashlib import md5
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
+    """Representation of a user"""
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
         __tablename__ = "users"
@@ -15,8 +19,20 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
 
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
+
+    def __setattr__(self, name, value):
+        """sets a password with md5 encryption"""
+        if name == "password":
+            value = md5(value.encode()).hexdigest()
+        super().__setattr__(name, value)
