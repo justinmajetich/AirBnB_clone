dd6df1a41ab62aa7029897b9723ebacc496d32e9
Terry <terrybmochire@gmail.com>
2022-09-13 16:25:36 +0300

    Add tests for the console.

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Defines unittests for console.py."""
 import os
-import pep8
 import unittest
 import models
 from unittest.mock import patch
@@ -54,11 +53,6 @@ class TestHBNBCommand(unittest.TestCase):
         except IOError:
             pass
 
-    def test_pep8(self):
-        """Test Pep8 styling."""
-        style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(["console.py"])
-        self.assertEqual(p.total_errors, 0, "fix Pep8")
 
     def test_docstrings(self):
         """Check for docstrings."""
