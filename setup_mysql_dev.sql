48929000f026006301275f7f31065d9c57f18817
mosweubarona <mosweubarona@gmail.com>
2022-09-14 10:26:23 -0700

    my prepare mysql server

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,15 @@
+-- create database hbnb_dev_db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- A new user hbnb_dev (in localhost)
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
+IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- grant hbnb_dev all privilege on hbnb_dev_db database
+GRANT ALL PRIVILEGES ON hbnb_dev_db.*
+TO 'hbnb_dev'@'localhost';
+
+-- grant hbnb_dev SELECT privilege on performance_schema database
+GRANT SELECT ON performance_schema.*
+TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
---
+++ setup_mysql_test.sql
@@ -0,0 +1,15 @@
+-- create database hbnb_test_db
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- creates new user hbnb_test
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
+IDENTIFIED BY 'hbnb_test_pwd';
+
+-- grant hbnb_dev all privilege on hbnb_test_db database
+GRANT ALL PRIVILEGES ON hbnb_test_db.*
+TO 'hbnb_test'@'localhost';
+
+-- grant hbnb_test SELECT privilege on performance_schema database
+GRANT SELECT ON performance_schema.*
+TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
