ed1279e5e4181fc1d4374576b95c7f6c16eca347
middo1 <hamidadeshina1@gmail.com>
2022-09-14 08:23:53 +0100

    oya na

--- models/base_model.py
+++ models/base_model.py
@@ -7,8 +7,6 @@ from sqlalchemy.ext.declarative import declarative_base
 import uuid
 from datetime import datetime
 import models
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
 
 
 Base = declarative_base()
