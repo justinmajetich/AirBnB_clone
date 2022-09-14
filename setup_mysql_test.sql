a365866da7a1308086d56e1ca421d587d05e9736
Natnael <99090574+NatnaelDemelash@users.noreply.github.com>
2022-09-14 08:21:56 +0300

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,7 @@
--- Script that prepares a MySQL server for the project
+-- Creates a database called hbnb_test_db in the current MySQL server
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- creates the MySQL server user hbnb_test
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- Grants Permissions for user hbnb_test
+GRANT ALL ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
-GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
