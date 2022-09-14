d02cd149abaf3e202575d95fafd6ea6b71e6a97b
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2022-09-14 10:10:59 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""State class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column
+from sqlalchemy.orm import relationship
+import os
+import models
+from models.city import City
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """state class"""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref="state",
+                              cascade="all, delete-orphan")
+    else:
+        @property
+        def cities(self):
+            """getter"""
+            list_of_city = []
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    list_of_city.append(city)
+            return list_of_city
