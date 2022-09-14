77892f1b81f572c36865c4e29abc9d2d8006a1a6
GMercury <99487197+GMercury@users.noreply.github.com>
2022-09-15 00:55:30 +0300

    Update templates

---
+++ web_flask/templates/4-number_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""This script starts a Flask web application"""
+from flask import Flask, escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def route():
+    """Return two words"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def route_hbnb():
+    """Return a word"""
+    return "HBNB"
+
+
+@app.route('/c/<path:subpath>', strict_slashes=False)
+def route_c(subpath):
+    """Return subpath"""
+    return "C {}".format(escape(subpath).replace('_', ' '))
+
+
+@app.route('/python', defaults={'subpath': 'is cool'}, strict_slashes=False)
+@app.route('/python/<path:subpath>', strict_slashes=False)
+def route_python(subpath):
+    """Return subpath"""
+    return "Python {}".format(escape(subpath).replace('_', ' '))
+
+
+@app.route('/number/<int:num>', strict_slashes=False)
+def route_number(num):
+    """Return only if num is a int"""
+    return "{} is a number".format(escape(num))
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
---
+++ web_flask/templates/6-number_odd_or_even.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""This script starts a Flask web application"""
+from flask import Flask, escape, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def route():
+    """Return two words"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def route_hbnb():
+    """Return a word"""
+    return "HBNB"
+
+
+@app.route('/c/<path:subpath>', strict_slashes=False)
+def route_c(subpath):
+    """Return subpath"""
+    return "C {}".format(escape(subpath).replace('_', ' '))
+
+
+@app.route('/python', defaults={'subpath': 'is cool'}, strict_slashes=False)
+@app.route('/python/<path:subpath>', strict_slashes=False)
+def route_python(subpath):
+    """Return subpath"""
+    return "Python {}".format(escape(subpath).replace('_', ' '))
+
+
+@app.route('/number/<int:num>', strict_slashes=False)
+def route_number(num):
+    """Return only if num is a int"""
+    return "{} is a number".format(escape(num))
+
+
+@app.route('/number_template/<int:num>', strict_slashes=False)
+def route_template(num):
+    """Return a file HTML only if num is a int"""
+    return render_template("5-number.html", n=num)
+
+
+@app.route('/number_odd_or_even/<int:num>', strict_slashes=False)
+def route_odd_even(num):
+    """Returns an HTML file only if num is an int and describes
+    if num is odd or even"""
+    return render_template("6-number_odd_or_even.html", n=num)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
