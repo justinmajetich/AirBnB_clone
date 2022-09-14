f5178797f88c832a02ac7c89b3d804e4d0f456ca
GMercury <99487197+GMercury@users.noreply.github.com>
2022-09-15 01:13:23 +0300

    update engine

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,71 +1,60 @@
 #!/usr/bin/python3
-
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker, scoped_session
-from os import getenv
-from models.base_model import BaseModel, Base
-from models.amenity import Amenity
+""" New Engine """
+from sqlalchemy import create_engine
+from models.base_model import Base
+from models.state import State
 from models.city import City
+from models.user import User
 from models.place import Place
 from models.review import Review
-from models.state import State
-from models.user import User
+from models.amenity import Amenity
+from sqlalchemy.orm import sessionmaker
+from os import getenv
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
-classList = {"BaseModel": BaseModel, "Amenity": Amenity, "City": City,
-             "Place": Place, "Review": Review, "State": State, "User": User}
 
-
-class DBStorage():
-    """"""
+class DBStorage:
+    """This class manages storage of hbnb models in JSON format"""
     __engine = None
     __session = None
 
     def __init__(self):
-        """"""
-        user = getenv('HBNB_MYSQL_USER')
-        password = getenv('HBNB_MYSQL_PWD')
-        host = getenv('HBNB_MYSQL_HOST')
-        database = getenv('HBNB_MYSQL_DB')
-
         self.__engine = create_engine(
-            'mysql+mysqldb://{}:{}@{}/{}'
-            .format(user, password, host, database), pool_pre_ping=True)
-
+            "mysql+mysqldb://{}:{}@{}/{}"
+            .format(getenv('HBNB_MYSQL_USER'),
+                    getenv('HBNB_MYSQL_PWD'),
+                    getenv('HBNB_MYSQL_HOST'),
+                    getenv('HBNB_MYSQL_DB')),
+            pool_pre_ping=True)
         if getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """this method return a dictionary of
-        all objects depending of the class name"""
-        if cls is None:
-            objs = self.__session.query(State).all()
-            objs.extend(self.__session.query(City).all())
-            objs.extend(self.__session.query(User).all())
-            objs.extend(self.__session.query(Place).all())
-            objs.extend(self.__session.query(Amenity).all())
-            objs.extend(self.__session.query(Review).all())
-        else:
-            if type(cls) == str:
-                cls = eval(cls)
-            objs = self.__session.query(cls)
-        return {"{}.{}".format(type(i).__name__, i.id): i for i in objs}
+        new_dict = {}
+        for clss in classes:
+            if cls is None or cls is classes[clss] or cls is clss:
+                objs = self.__session.query(classes[clss]).all()
+                for obj in objs:
+                    key = obj.__class__.__name__ + '.' + obj.id
+                    new_dict[key] = obj
+        return (new_dict)
 
     def new(self, obj):
-        """add the object to the current dtabase session"""
         self.__session.add(obj)
 
     def save(self):
-        """"""""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """"""""
-        if obj is not None:
+        if obj:
             self.__session.delete(obj)
 
     def reload(self):
-        """"""""
         Base.metadata.create_all(self.__engine)
-        session = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(session)
+        Session = sessionmaker()
+        Session.configure(bind=self.__engine, expire_on_commit=False)
         self.__session = Session()
+
+    def close(self):
+        self.__session.close()
