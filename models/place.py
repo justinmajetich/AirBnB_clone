c79ed9943f58b213eac21f1102677488d97c0ad8
Veralee225 <Veralee225@gmail.com>
2022-09-14 03:11:06 +0100

    build: sqlalchemy module import

--- models/place.py
+++ models/place.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
+import models
 
 
 class Place(BaseModel):
