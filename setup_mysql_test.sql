62e8cefdd7e13fabb11039f5e5faad37230f112f
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2022-09-14 10:27:27 +0100

    Create setup_mysql_test.sql

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- Script to create a MySQL server with the database hbnb_test_db.
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
