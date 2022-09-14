4a17356dd0e1af7b25f53feaf2ecb3184d3c9d7e
Veralee225 <Veralee225@gmail.com>
2022-09-14 21:54:36 +0100

    fix: bug fix

--- models/user.py
+++ models/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""This is the user class"""
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
@@ -8,18 +8,20 @@ from models.place import Place
 from models.review import Review
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes
+class User(BaseModel, Base):
+    """This is the class for user
     Attributes:
-    email = email address
-    password = password for your login
-    first_name = first name
-    last_name = last name
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
     """
-__tablename__ = "users"
-email = Column(String(128), nullable=False)
-password = Column(String(128), nullable=False)
-first__name = Column(String(128))
-last_name = Column(String(128))
-places = relationship("Place", cascade='all, delete, delete-orphan', backref="user")
-reviews = relationship("Review", cascade='all, delete, delete-orphan', backref="user")
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
