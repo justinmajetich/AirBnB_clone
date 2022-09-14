c84ead8e0f1317407492951df09955e0773ff974
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2022-09-14 10:26:42 +0100

    Create setup_mysql_dev.sql

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,7 @@
+-- Script to create a MySQL server with the database hbnb_dev_db.
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
