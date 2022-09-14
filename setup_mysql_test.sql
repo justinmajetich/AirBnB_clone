d0a8bd7cb86215606f8b1135fd488bfcc8ffa76f
melakualehegn <melakualehegn34@gmail.com>
2022-09-14 22:39:18 +0300

    mysql fix

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,4 @@
-/* A script that prepares a MySQL server for the project */
+-- A script that prepares a MySQL server for the project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_test;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGE ON `hbnb_dev_db`.* TO 'hbnb_test'@'localhost';
