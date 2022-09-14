d3a5b664c8bccf7ecadaff65e7c10b31e244e276
Veralee225 <Veralee225@gmail.com>
2022-09-14 22:03:03 +0100

    fix: bug fix

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """This is the amenity class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
 from models.place import place_amenity
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
