98759af8785123ee9af87d755d05567e72879043
melakualehegn <melakualehegn34@gmail.com>
2022-09-14 22:36:33 +0300

    mysql fix

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,6 @@
 /* A script that prepares a MySQL server for the project */
 CREATE DATABASE IF NOT EXISTS hbnb_dev_test;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT PRIVILEGE ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGE ON `hbnb_dev_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
