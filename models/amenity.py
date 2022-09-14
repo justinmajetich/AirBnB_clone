82341a713d05f998836bd99d7b38e444723c3031
adexino0606 <adexino0606@github.com>
2022-09-14 16:58:54 -0700

    bad

--- models/amenity.py
+++ models/amenity.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""This is the amenity class"""
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
-from models.place import place_amenity
+import models
 
 
 class Amenity(BaseModel, Base):
-    """This is the class for Amenity
-    Attributes:
-        name: input name
-    """
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
+    """Amenity Class"""
+    if models.storage_type == 'db':
+        __tablename__ = "amenities"
+        name = Column(String(128), nullable=False)
+
+    else:
+        name = ""
