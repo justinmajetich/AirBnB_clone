18818b05004187a2e4f935ae5977b82666379558
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2022-09-14 09:13:30 +0100

    Create 0-setup_web_static.sh
    Signed-off-by: Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Script that sets up your web servers for the deployment of web_static.
+
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" >> /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "26i \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+sudo service nginx restart
