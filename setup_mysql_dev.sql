a1e3d19498344dbc79d7c74bbe5e4843e7e83b93
captjay98WSL <captjay98@gmail.com>
2022-09-15 03:01:49 +0100

    sets up database

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,10 @@
 --  prepares a MYSQL server for Airbnb
-
+-- sets up database
+-- creates new user 
+-- grants permissions
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev' @'localhost'
-IDENTIFIED BY 'hbnb_dev_pwd';
+    IDENTIFIED BY 'hbnb_dev_pwd';
 
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
