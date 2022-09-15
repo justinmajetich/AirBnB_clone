9afb42325724a2c312db8d2afb689be43dd9f78d
captjay98WSL <captjay98@gmail.com>
2022-09-15 05:03:37 +0100

    city fic

--- models/city.py
+++ models/city.py
@@ -2,14 +2,17 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, ForeignKey
-from os import environ
+from os import getenv
 from sqlalchemy.orm import relationship
 
-
+storage = getenv('HBNB_TYPE_STORAGE')
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'city'
-
-    name = Column(String(128), nullable=False)
-    state_id = Column((String(60), nullable=False, ForiegnKey('states.id'))
+    if storage  == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('state.id'), nullable=False)
+    else:
+        state_id = ''
+        name = ''
