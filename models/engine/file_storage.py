29166e7fb2dc53d09f8df994d939d9936954f36d
Tech-sis <abilawonadeola@gmail.com>
2022-09-14 23:58:25 -0400

    Create a delete function

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,3 +55,9 @@ class FileStorage:
                     self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Deletes obj from __objects if it’s inside"""
+        if obj:
+            del(self.__objects["{}.{}".format(type(obj).__name__, obj.id)])
+            del(obj)
