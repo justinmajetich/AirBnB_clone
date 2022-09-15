d9587e9dd41231b4c1dd7795000b728e67132d56
captjay98WSL <captjay98@gmail.com>
2022-09-15 05:19:08 +0100

    fixes

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 
-from models import *
+from models.amenity import Amenity
+from models.base_model import Base
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from models.base_model import Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.scoping import scoped_session
 from os import getenv
+
+
 class DBStorage:
 
     classes = ["User", "State", "City", "Amenity", "Place", "Review"]
@@ -19,17 +27,16 @@ class DBStorage:
         HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
         HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
         HBNB_ENV = getenv('HBNB_ENV')
-        self.__engine  = create_engine("""mysql+mysqldb:
-                                    \\HBNB_MYSQL_USER:HBNB_MYSQL_PWD
-                                    @HBNB_MYSQL_HOST\HBNB_MYSQL_DB
-                                    """, pool_pre_ping=True)
+        self.__engine = create_engine(f"mysql+mysqldb:\\{HBNB_MYSQL_USER}:\
+                                      {HBNB_MYSQL_PWD}@{HBNB_MYSQL_HOST}\
+                                      {HBNB_MYSQL_DB}", pool_pre_ping=True)
 
-        if  HBNB_ENV == 'test':
+        if HBNB_ENV == 'test':
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
         temp = {}
-        if cls == None:
+        if cls is None:
             for name in self.classes:
                 data = self.__session.query(name).all()
                 for obj in data:
@@ -54,7 +61,7 @@ class DBStorage:
 
     def reload(self):
         Base.metadata.create_all(self.__engine)
-        Session = sessionmaker(bind=self.__engine, expire_on_commit=False))
+        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
         self.__session = scoped_session(Session)
 
     def close(self):
