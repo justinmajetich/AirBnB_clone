251f59b27c98d46791bf871b0fcd3d4bff2c0651
Tishlyy <ekaetteessien28@gmail.com>
2022-09-14 17:07:05 -0700

    Test sql

---
+++ setup_mysql_test.sql
@@ -0,0 +1,6 @@
+-- A script that prepares a MYSQL server for the project
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost' WITH GRANT OPTION;
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
