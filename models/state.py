0f313e021f4eb1d8a982041bf6a9c75a8c0b262c
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2022-09-15 13:01:23 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python3
-"""State class"""
+"""This is the state class"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import String, Column
 from sqlalchemy.orm import relationship
-import os
+from sqlalchemy import Column, Integer, String
 import models
 from models.city import City
+import shlex
 
 
 class State(BaseModel, Base):
-    """state class"""
-    __tablename__ = 'states'
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
 
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship("City", backref="state",
-                              cascade="all, delete-orphan")
-    else:
-        @property
-        def cities(self):
-            """getter"""
-            list_of_city = []
-            for city in models.storage.all(City).values():
-                if city.state_id == self.id:
-                    list_of_city.append(city)
-            return list_of_city
+    @property
+    def cities(self):
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
