5ee8e9177ffcbfd74ed57a320603786c80b3298b
captjay98WSL <captjay98@gmail.com>
2022-09-15 03:09:21 +0100

    set up test db

---
+++ setup_mysql_test.sql
@@ -0,0 +1,10 @@
+--  prepares MySQL server for project
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
+    IDENTIFIED BY 'hbnb_test_pwd';
+
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@localhost;
+
+GRANT SELECT ON perforance_schema.* TO ' hbnb_test'@localhost;
