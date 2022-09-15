c76ab3a70aab6a3614a39ea7c42c6da85b13dafc
smith2eric <ericsmithsonian2@gmail.com>
2022-09-15 06:04:26 +0100

    [feat]: wrote script for MySQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
--- Script to prepare a MySQL server for this project
+-- Script to prepare a MySQL server for the project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
