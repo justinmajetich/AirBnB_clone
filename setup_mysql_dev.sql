79ad9a6c31df6b044758d29514a4939992095e89
Alatise Oluwaseun <holynation667@gmail.com>
2022-09-15 03:22:57 +0100

    refactor: Adjusted the setup_mysql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -8,7 +8,10 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grant permissions on hbnb_dev_db database user
+USE hbnb_dev_db;
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- Grant permission on performance_schema database
+USE performance_schema;
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -8,7 +8,10 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 -- Grant permissions on hbnb_test_db database user
+USE hbnb_test_db;
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
 -- Grant permission on performance_schema database
+USE performance_schema;
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
