ee065305ce82382ef39f37f92c03e72fdfbb2b73
Menor25 <ajirimenor@gmail.com>
2022-09-17 06:37:00 +0100

    Error fixed

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -26,12 +26,11 @@ class FileStorage:
             json.dump(temp, f)
 
     def delete(self, obj=None):
-        """ delete obj from __objects if obj is inside """
-        if obj is None:
-            return
-        for k, v in self.__objects.items():
-            if obj == v:
-                del self.__objects[k]
+        """delete obj from __objects if it’s inside"""
+        if obj is not None:
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in self.__objects:
+                del self.__objects[key]
 
     def reload(self):
         """Loads storage dictionary from file"""
@@ -56,3 +55,8 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def close(self):
+        """call reload() method for deserializing the JSON file to objects"""
+        self.reload()
+        
