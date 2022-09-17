aa2d3b2d8e437ee9fbaf6acfc7dcf5e9b0c2c648
Menor25 <ajirimenor@gmail.com>
2022-09-17 06:32:48 +0100

    Error fixed

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,16 +1,7 @@
--- prepares a MySQL server for the AirBnB clone
--- create a database hbnb_dev_db
--- create a user hbnb_dev in localhost and grant all privileges.
--- grant SELECT privileges on performance_schema to the user hbnb_dev
+-- prepares a MySQL server for the project
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER
-    IF NOT EXISTS 'hbnb_dev'@'localhost'
-    IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES
-    ON hbnb_dev_db.*
-    TO 'hbnb_dev'@'localhost';
-GRANT SELECT
-    ON perfomance_schema.*
-    TO 'hbnb_dev'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
