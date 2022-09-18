4be16236cde952b5c645970b219673fd0fc13bd5
Akinde Titilayo Tunmise <93851293+Akinde1@users.noreply.github.com>
2022-09-18 03:32:00 +0100

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,10 +1,37 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel
+'''
+    Implementation of the State class
+'''
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
+import os
+import models
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+class State(BaseModel, Base):
+    '''
+        Implementation for the State.
+    '''
+    __tablename__ = 'states'
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", passive_deletes=True, backref="state")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """
+            cities property
+            """
+            city_list = []
+            for key, val in models.storage.all().items():
+                try:
+                    if val.state_id == self.id:
+                        city_list.append(val)
+                except AttributeError:
+                    pass
+            return city_list
