52d2aedaa05f195cc1e5d8c6a71a8d18a1bc10ab
Akinde Titilayo Tunmise <93851293+Akinde1@users.noreply.github.com>
2022-09-18 03:23:26 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,26 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+'''
+    Package initializer
+'''
+
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+import os
 
+classes = {"User": User, "BaseModel": BaseModel,
+           "Place": Place, "State": State,
+           "City": City, "Amenity": Amenity,
+           "Review": Review}
 
-storage = FileStorage()
+if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    storage = FileStorage()
 storage.reload()
