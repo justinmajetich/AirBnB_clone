df74b56650ba50dcb690525dec326b89e6d15223
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-09-22 02:22:05 -0500

    Change redirect to 'sudo tee' (task 0)

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -16,7 +16,7 @@ sudo apt-get install -y nginx
 
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-sudo echo 'Test Content to test Nginx config' > /data/web_static/releases/test/index.html
+echo 'Test Content to test Nginx config' | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu /data/
