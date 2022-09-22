23aba4b6b86e7153d63b2a404a75a026b56244c3
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-09-22 09:39:21 -0500

    v2 of task 101 (puppet script emulates task#0)

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,34 +1,89 @@
-# Puppet script that sets up web servers for the deployment of web_static
+# Puppet script that configures a web server for deployment of web_static
+$nginx_conf = "server {
+    listen 80;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
 
-exec {'/usr/bin/env apt-get update':}
-->package {'nginx':
-ensure => installed,
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 https:\/\/www.youtube.com\/watch\?v=QH2-TGUlwu4;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+        root /var/www/html;
+        internal;
+    }
+}"
+
+package { 'nginx':
+  ensure   => 'present',
+  provider => 'apt'
+} ->
+
+file { '/data':
+  ensure  => 'directory'
+} ->
+
+file { '/data/web_static':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases/test':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/shared':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => "Test Content to test Nginx config\n"
+} ->
+
+file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
+} ->
+
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
-->file { [ '/data/',
-  '/data/web_static/',
-  '/data/web_static/releases/',
-  '/data/web_static/releases/test/',
-  '/data/web_static/shared/', ]:
-  ensure => directory,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-}
+file { '/var/www':
+  ensure => 'directory'
+} ->
 
-->file {'/data/web_static/releases/test/index.html':
-  ensure  => present,
-  content => "Test Content to test Nginx config",
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-}
+file { '/var/www/html':
+  ensure => 'directory'
+} ->
 
--> exec {'/usr/bin/env ln -sf /data/web_static/releases/test/ /data/web_static/current':}
--> exec {'/usr/bin/env chown -R ubuntu:ubuntu /data/':}
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "Holberton School\n"
+} ->
 
-->file_line {'add protocol':
-ensure => present,
-path   => '/etc/nginx/sites-available/default',
-after  => 'index  index.html index.htm;',
-line   => 'location /hbnb_static { alias /data/web_static/current/; index index.html index.htm;}',
-require => Package['nginx'],
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n"
+} ->
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $nginx_conf
+} ->
+
+exec { 'nginx restart':
+  path => '/etc/init.d/'
 }
