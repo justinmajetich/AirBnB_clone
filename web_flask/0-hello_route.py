c4551efb7f7d1fb3724a4dc88b360147e3ba447b
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-09-24 17:15:09 -0500

    Fix docstring task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-# Script that starts a Flask web application:
-# Web application must be listening on 0.0.0.0, port 5000
-# Routes:
-# /: display “Hello HBNB!”
+"""Script that starts a Flask web application:
+
+Web application must be listening on 0.0.0.0, port 5000
+Routes:
+    /: display “Hello HBNB!”
+"""
 from flask import Flask
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """Index page"""
+    """Displays Hello HBNB!"""
     return "Hello HBNB!"
 
 if __name__ == '__main__':
