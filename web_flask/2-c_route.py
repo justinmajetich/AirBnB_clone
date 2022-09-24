cea62ddaa22f55003d4df2c483c1e2bd28bbdbd9
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-09-24 17:25:06 -0500

    v1 of task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application:
+
+Web application must be listening on 0.0.0.0, port 5000
+Routes:
+    /: display “Hello HBNB!”
+    /hbnb: display “HBNB”
+    /c/<text>: display “C ” followed by the value of the text
+    variable (replace underscore _ symbols with a space)
+"""
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    """Displays C <text>"""
+    txt = text.replace("_", " ")
+    return f"C {escape(txt)}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
