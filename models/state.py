73feed0ebd0f5ab4c55b43851d5ba138f2fb6994
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-09-25 10:23:26 -0500

    change state.py

--- models/state.py
+++ models/state.py
@@ -8,21 +8,26 @@ from sqlalchemy.orm import relationship
 from models.city import City
 import models
 
-class State(BaseModel, Base):
-    """ State class for State """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state',
-                            cascade='all, delete, delete-orphan')
+if getenv('HBNB_TYPE_STORAGE') != 'db':
+    class State(BaseModel, Base):
+        """ State class for State """
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state',
+                              cascade='all, delete, delete-orphan')
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
-        @property
-        def cities(self):
-            """ Returns the list of `City` instances
-            with `state_id` equals to the current
-            """
-            cities = []
-            for key, value in models.storage.all(City).items():
-                if value.state_id == self.id:
-                    cities.append(value)
-            return cities
+else:
+    class State(BaseModel):
+        """ State class for State """
+        name = ''
+
+    @property
+    def cities(self):
+        """ Returns the list of `City` instances
+        with `state_id` equals to the current
+        """
+        cities = []
+        for key, value in models.storage.all(City).items():
+            if value.state_id == self.id:
+                cities.append(value)
+        return cities
