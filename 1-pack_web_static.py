d1adf61662eab4bc0486dee2688e00a215033fb8
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-09-29 10:57:14 +0100

    fix bugs

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 """
-Fabric file to generate a .tgz compresssed file for
+Fabric file to generate a .tgz compressed file for
 the web_static directory
 """
 from fabric.api import local
 from datetime import datetime
 
+
 def do_pack():
-    curr_datetime = datetime.now().strftime("%Y%m%d%H%M%S")
+    """
+    Function to create the compressed file
+    """
+    curr_datetime = datetime.utcnow().strftime("%Y%m%d%H%M%S")
     path = "./versions/web_static_" + curr_datetime + ".tgz"
     local("mkdir -p ./versions")
-    if local("tar -cvzf " + path + " ./web_static", capture = True).succeeded:
+    if local("tar -cvzf " + path + " ./web_static").succeeded:
         return path
     return None
