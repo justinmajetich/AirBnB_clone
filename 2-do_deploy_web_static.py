6eda17fe4f8d44a392f10e65f42ae1af916246b2
Tijani1402 <ridwanomofolarin14@gmail.com>
2022-09-29 10:06:36 -0700

    commit

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,64 +1,49 @@
 #!/usr/bin/python3
-"""Generates a .tgz archive from the
-contents of the web_static folder
-Distributes an archive to a web server"""
+"""
+This script (based on the file 1-pack_web_static.py) that
+distributes an archive to your web servers, using the function do_deploy
+Usage:
+    fab -f 2-do_deploy_web_static.py do_deploy:
+    archive_path=versions/<file_name> -i my_ssh_private_key
 
-from fabric.operations import local, run, put
-from datetime import datetime
-import os
-from fabric.api import env
-import re
+Example:
+    fab -f 2-do_deploy_web_static.py do_deploy:
+    archive_path=versions/web_static_20170315003959.tgz -i my_ssh_private_key
+"""
 
-
-env.hosts = ['35.190.176.186', '35.196.156.157']
-
-
-def do_pack():
-    """Function to compress files in an archive"""
-    local("mkdir -p versions")
-    result = local("tar -cvzf versions/web_static_{}.tgz web_static"
-                   .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
-                   capture=True)
-    if result.failed:
-        return None
-    return result
+from fabric.api import env, put, run
+from os.path import exists
+import shlex
+env.hosts = ['3.236.55.133', '44.192.95.89']
+env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
-    """Function to distribute an archive to a server"""
-    if not os.path.exists(archive_path):
+    """All remote commands must be executed on your both web servers
+    (using env.hosts = ['<IP web-01>', 'IP web-02'] variable in your script)"""
+    if not exists(archive_path):
         return False
-    rex = r'^versions/(\S+).tgz'
-    match = re.search(rex, archive_path)
-    filename = match.group(1)
-    res = put(archive_path, "/tmp/{}.tgz".format(filename))
-    if res.failed:
+    try:
+        package_name = archive_path.replace('/', ' ')
+        package_name = shlex.split(package_name)
+        package_name = package_name[-1]
+
+        updated_package = package_name.replace('.', ' ')
+        updated_package = shlex.split(updated_package)
+        updated_package = updated_package[0]
+
+        releases_path = "/data/web_static/releases/{}/".format(updated_package)
+        tmp_path = "/tmp/{}".format(package_name)
+
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(releases_path))
+        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
+        run("rm {}".format(tmp_path))
+        run("mv {}web_static/* {}".format(releases_path, releases_path))
+        run("rm -rf {}web_static".format(releases_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(releases_path))
+        print("New version deployed!")
+        return True
+    except BaseException:
         return False
-    res = run("mkdir -p /data/web_static/releases/{}/".format(filename))
-    if res.failed:
-        return False
-    res = run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
-              .format(filename, filename))
-    if res.failed:
-        return False
-    res = run("rm /tmp/{}.tgz".format(filename))
-    if res.failed:
-        return False
-    res = run("mv /data/web_static/releases/{}"
-              "/web_static/* /data/web_static/releases/{}/"
-              .format(filename, filename))
-    if res.failed:
-        return False
-    res = run("rm -rf /data/web_static/releases/{}/web_static"
-              .format(filename))
-    if res.failed:
-        return False
-    res = run("rm -rf /data/web_static/current")
-    if res.failed:
-        return False
-    res = run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-              .format(filename))
-    if res.failed:
-        return False
-    print('New version deployed!')
-    return True
