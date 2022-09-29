ad7120affa46833982891fead6e1e419ad9ec8f6
NatnaelDemelash <natidemelash074@gmail.com>
2022-09-28 21:09:04 -0700

    Update file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,48 +1,30 @@
-
 #!/usr/bin/python3
 """
 Fabric script based on the file 1-pack_web_static.py that distributes an
 archive to the web servers
 """
 
-from fabric.api import *
-from os import path
-from datetime import datetime
-from shlex import split
-
-env.user = 'ubuntu'
-env.hosts = ['34.74.70.205', '34.224.30.177']
+from fabric.api import put, run, env
+from os.path import exists
+env.hosts = ['3.238.201.120', '34.139.190.16']
 
 
 def do_deploy(archive_path):
-    """Deplay the archive tgz"""
-    if not path.exists(archive_path) or (
-                                         path.exists(archive_path) and
-                                         path.isdir(archive_path)):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
     try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        name_file_ext = archive_path.split("/")[1]
-        name_file = name_file_ext.split(".")[0]
-        cmd_mkdir = 'mkdir -p /data/web_static/releases/{}'.format(name_file)
-        run(cmd_mkdir)
-        cmd_uncom = 'tar -xzf /tmp/{}'.format(name_file_ext)
-        cmd_uncom += ' -C /data/web_static/releases/{}'.format(name_file)
-        run(cmd_uncom)
-        cmd_rm_file = 'rm /tmp/{}'.format(name_file_ext)
-        run(cmd_rm_file)
-        cmd_mv = 'mv /data/web_static/releases/'
-        cmd_mv += '{}/web_static/*'.format(name_file)
-        cmd_mv += ' /data/web_static/releases/{}/'.format(name_file)
-        run(cmd_mv)
-        cmd_rm_dir = 'rm -rf /data/web_static/releases/{}'.format(name_file)
-        cmd_rm_dir += '/web_static'
-        run(cmd_rm_dir)
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
-        cmd_cre_sym = 'ln -s /data/web_static/releases/{}/'.format(name_file)
-        cmd_cre_sym += ' /data/web_static/current'
-        run(cmd_cre_sym)
-        print("New version deployed!")
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except:
         return False
