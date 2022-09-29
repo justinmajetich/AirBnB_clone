fec80b233e5ffacc2a975b87f39fac67032e8bc9
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2022-09-29 09:20:27 +0100

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
 """
-Created on Mon Aug 13 14:21:54 2020
-@author: Robinson Montes
+Write a Fabric script that generates a .tgz
+@author: Bashir AmirKano 
 """
 from fabric.api import local, env
 from datetime import datetime
 
 env.user = 'ubuntu'
-env.hosts = ['35.227.35.75', '100.24.37.33']
+env.hosts = ["3.235.176.169", "3.227.240.66"]
 
 
 def do_pack():
