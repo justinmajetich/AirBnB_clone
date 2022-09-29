edd87aa0ae95fe6d8622a6a75b4e200bb632e2ae
TaiwoForbes <soyemihissa2000@gmail.com>
2022-09-29 13:10:57 -0700

    correction

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
-# Script that configures Nginx server with some folders and files
-if [ ! -x /usr/sbin/nginx ]; then
-    sudo apt-get -y update
-    sudo apt-get -y install nginx
-fi
-sudo service nginx start
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-sudo echo "Holberton School" > /data/web_static/releases/test/index.html
+# sets up your web servers for the deployment of web_static.
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+	Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-enabled/default
-
-sudo service nginx restart
-exit 0
+sudo chown -R ubuntu:ubuntu /data
+sudo sed -i '53i \\tlocation \/hbnb_static {\n\t\t alias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
+/etc/init.d/nginx restart
