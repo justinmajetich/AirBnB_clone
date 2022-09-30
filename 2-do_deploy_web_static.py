b31f6242771ec82a31fb744deab5babc4a9e77cf
Tijani1402 <ridwanomofolarin14@gmail.com>
2022-09-29 17:10:52 -0700

    changes

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,49 +1,42 @@
 #!/usr/bin/python3
-"""
-This script (based on the file 1-pack_web_static.py) that
-distributes an archive to your web servers, using the function do_deploy
-Usage:
-    fab -f 2-do_deploy_web_static.py do_deploy:
-    archive_path=versions/<file_name> -i my_ssh_private_key
+"""Distribute an archive to web servers"""
+import os.path
+import time
+from fabric.api import local
+from fabric.operations import env, put, run
 
-Example:
-    fab -f 2-do_deploy_web_static.py do_deploy:
-    archive_path=versions/web_static_20170315003959.tgz -i my_ssh_private_key
-"""
+env.hosts = ['34.75.39.5', '34.74.147.195']
 
-from fabric.api import env, put, run
-from os.path import exists
-import shlex
-env.hosts = ['3.236.55.133', '44.192.95.89']
-env.user = 'ubuntu'
+
+def do_pack():
+    """Generate an tgz archive from web_static folder"""
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz web_static/".
+              format(time.strftime("%Y%m%d%H%M%S")))
+        return ("versions/web_static_{}.tgz".format(time.
+                                                    strftime("%Y%m%d%H%M%S")))
+    except:
+        return None
 
 
 def do_deploy(archive_path):
-    """All remote commands must be executed on your both web servers
-    (using env.hosts = ['<IP web-01>', 'IP web-02'] variable in your script)"""
-    if not exists(archive_path):
+    """Distribute an archive to web servers"""
+    if (os.path.isfile(archive_path) is False):
         return False
+
     try:
-        package_name = archive_path.replace('/', ' ')
-        package_name = shlex.split(package_name)
-        package_name = package_name[-1]
-
-        updated_package = package_name.replace('.', ' ')
-        updated_package = shlex.split(updated_package)
-        updated_package = updated_package[0]
-
-        releases_path = "/data/web_static/releases/{}/".format(updated_package)
-        tmp_path = "/tmp/{}".format(package_name)
-
+        file = archive_path.split("/")[-1]
+        folder = ("/data/web_static/releases/" + file.split(".")[0])
         put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(releases_path))
-        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
-        run("rm {}".format(tmp_path))
-        run("mv {}web_static/* {}".format(releases_path, releases_path))
-        run("rm -rf {}web_static".format(releases_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(releases_path))
-        print("New version deployed!")
+        run("mkdir -p {}".format(folder))
+        run("tar -xzf /tmp/{} -C {}".format(file, folder))
+        run("rm /tmp/{}".format(file))
+        run("mv {}/web_static/* {}/".format(folder, folder))
+        run("rm -rf {}/web_static".format(folder))
+        run('rm -rf /data/web_static/current')
+        run("ln -s {} /data/web_static/current".format(folder))
+        print("Deployment done")
         return True
-    except BaseException:
+    except:
         return False
