5f2c368823e951bab10c7602ff4a15393d1abae0
akakiplimo <akakiplimo@gmail.com>
2022-10-02 02:06:02 -0700

    setup web static v2.0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,44 @@
 #!/usr/bin/env bash
-# Sets up your web servers for the deployment of web_static
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get install nginx -y
-sudo mkdir -p /data
-sudo mkdir -p /data/web_static
-sudo mkdir -p /data/web_static/releases
-sudo mkdir -p /data/web_static/shared
-sudo mkdir -p /data/web_static/releases/test
-sudo echo -e "<html>\n<head>\n</head>\n<body>\nHolberton School\n</body>\n</html>" > /data/web_static/releases/test/index.html
-sudo ln -fs /data/web_static/releases/test /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data
-sudo sed -i '/^\tserver_name/ a\\tlocation /hbnb_static \{\n\t\talias /data/web_static/current;\n\t\}\n' /etc/nginx/sites-available/default
+# Prepare my webservers (web-01 & web-02)
+
+# uncomment for easy debugging
+#set -x
+
+# colors
+blue='\e[1;34m'
+#brown='\e[0;33m'
+green='\e[1;32m'
+reset='\033[0m'
+
+echo -e "${blue}Updating and doing some minor checks...${reset}\n"
+
+# install nginx if not present
+if [ ! -x /usr/sbin/nginx ]; then
+        sudo apt-get update -y -qq && \
+             sudo apt-get install -y nginx
+fi
+
+echo -e "\n${blue}Setting up some minor stuff.${reset}\n"
+
+# Create directories...
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared/
+
+# create index.html for test directory
+sudo echo -e "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" > /data/web_static/releases/test/index.html
+
+# create symbolic link
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+
+# give user ownership to directory
+sudo chown -R ubuntu:ubuntu /data/
+
+# backup default server config file
+sudo cp /etc/nginx/sites-enabled/default nginx-sites-enabled_default.backup
+
+# Set-up the content of /data/web_static/current/ to redirect
+# to domain.tech/hbnb_static
+sudo sed -i '37i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
 sudo service nginx restart
+
+echo -e "${green}Completed${reset}"
