7ceda81004573ab99041ab6e05256dcac8e06abd
Teepee78 <eniolatoluwalase78@gmail.com>
2022-10-08 04:50:37 -0700

    Task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,6 @@
 -- Prepares mysql server
 
-CREATE DATABASE IF NOT EXISTS 'hbnb_dev_db';
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
