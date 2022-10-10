61a395b00ecf32822d668ea8e0fbef524bba8aea
Chalo1996 <emusyoka759@gmail.com>
2022-10-10 22:27:43 +0300

    script to fix pep8 errors and run pycodestyle

--- autopep8.sh
+++ autopep8.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+# Fix pep8 errors with autopep8
 echo "Running autopep..."
 find -type f -name '*.py' ! -path '*/migrations/*' -exec autopep8 --in-place --aggressive --aggressive '{}' \;
 
+# Run pycodestyle checker
 echo "Running pycodestyle..."
 find -type f -name '*.py' ! -path '*/migrations/*' -exec pycodestyle --first '{}' \;
