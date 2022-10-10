da1b94769ab79b30930556ac14059a58a4486cd6
Reid-T-W <rediettadesse100@gmail.com>
2022-10-10 17:42:33 +0300

    orphan used on cascase

--- models/user.py
+++ models/user.py
@@ -12,4 +12,4 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
-    places = relationship('Place', backref='user', cascade='all, delete')
+    places = relationship('Place', backref='user', cascade='all, delete-orphan')
