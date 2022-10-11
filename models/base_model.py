b0c013d0eac02c30972f5bbeee9dcc3faae0f2a0
ListowelAdolwin <listoweladolwin@gmail.com>
2022-10-11 14:48:31 +0000

    Updated the __init__ method to handle  kwargs properly

--- models/base_model.py
+++ models/base_model.py
@@ -27,7 +27,13 @@ class BaseModel:
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            for key, value in kwargs.items():
+                self.key = value
+            
+            if "id" not in kwargs.keys():
+                self.id = str(uuid.uuid4())
+
+            #self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
