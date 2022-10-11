b2d619cba80e9098bdcc092ac3b6e547aefecc8e
Alausa Aishat <qtaisha.ao@gmail.com>
2022-10-11 08:19:38 +0100

    typo error fixed

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,7 @@
 --This script prepares a MYSQL server for the project.
---Creating project database: hbnb_dev_db
+--Creating project development database: hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
---Creating a new use: hbnb_dev (in localhost)
+--Creating a new user: hbnb_dev (in localhost)
 --and setting the password to hbnb_dev_pwd
 CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 --Granting all privileges on the database to user
