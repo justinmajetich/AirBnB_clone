79a628139be1234bd14309c65e0292ede6fa8597
kenkomu <komukenneth216@gmail.com>
2022-10-11 17:00:18 +0300

    setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,3 +4,4 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES O 'hbnb_dev_db'.* TO 'hbnb_dev'@'localhost';
 GARNT SELECT ON 'performance_schema'.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
