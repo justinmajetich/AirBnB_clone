e22c7eae11c7492530a2fb4b9fe4fd8db44717c2
Bibest <bibhestee@gmail.com>
2022-10-11 17:42:46 +0000

    Give base model access to create new instance attribute

--- models/base_model.py
+++ models/base_model.py
@@ -1,26 +1,34 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
+import models
 from datetime import datetime
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    fmt = '%Y-%m-%dT%H:%M:%S.%f'
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """Instantiates a new model"""
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            for key, value in kwargs.items():
+                if key == 'updated_at':
+                    self.updated_at = datetime.strptime(kwargs['updated_at'],
+                                                            self.fmt)
+                elif key == 'created_at':
+                    self.created_at = datetime.strptime(kwargs['created_at'],
+                                                             self.fmt)
+                else:
+                    setattr(self, key, value)
+            models.storage.new(self)
+            # self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
