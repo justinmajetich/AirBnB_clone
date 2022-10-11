84aeef0fb4557479584a6aa2b240cec979dccc12
ListowelAdolwin <listoweladolwin@gmail.com>
2022-10-11 15:11:43 +0000

    Corrected the path to import declarative_base

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
 
 Base = declarative_base()
