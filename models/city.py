6a76fae86c137693a5354a54ce7c75acf0890f1d
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-10-11 14:22:59 +0000

    Update: 'models/city.py' defines relationship to place

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationaship
 
 
 class City(BaseModel, Base):
@@ -10,3 +11,4 @@ class City(BaseModel, Base):
 
     state_id = Column(String(60), ForeignKey('states.id'),  nullable=False)
     name = Column(String(128), nullable=False)
+    places = relationship("Place", backref='cities', cascade="all, delete")
