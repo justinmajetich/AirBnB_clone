f8f70253dc40f0548d88773c12fe4bedd934031e
n1klaus <nicknyanjui@gmail.com>
2022-10-12 13:18:19 +0300

    Update console module

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 """ Console Module """
 import cmd
 import sys
+import shlex
 from models.base_model import BaseModel
 from models.__init__ import storage
 from models.user import User
@@ -19,16 +20,16 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) ' if sys.__stdin__.isatty() else ''
 
     classes = {
-               'BaseModel': BaseModel, 'User': User, 'Place': Place,
-               'State': State, 'City': City, 'Amenity': Amenity,
-               'Review': Review
-              }
+        'BaseModel': BaseModel, 'User': User, 'Place': Place,
+        'State': State, 'City': City, 'Amenity': Amenity,
+        'Review': Review
+    }
     dot_cmds = ['all', 'count', 'show', 'destroy', 'update']
     types = {
-             'number_rooms': int, 'number_bathrooms': int,
-             'max_guest': int, 'price_by_night': int,
-             'latitude': float, 'longitude': float
-            }
+        'number_rooms': int, 'number_bathrooms': int,
+        'max_guest': int, 'price_by_night': int,
+        'latitude': float, 'longitude': float
+    }
 
     def preloop(self):
         """Prints if isatty is false"""
@@ -73,7 +74,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -114,17 +115,37 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
-        """ Create an object of any class"""
-        if not args:
-            print("** class name missing **")
-            return
-        elif args not in HBNBCommand.classes:
-            print("** class doesn't exist **")
-            return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
-        print(new_instance.id)
-        storage.save()
+        """ Create an object of any class """
+        arg_list = shlex.split(args)
+        try:
+            class_name = arg_list[0]
+            param_list = list(arg_list[1:].strip().split())
+            if not class_name:
+                print("** class name missing **")
+                return
+            elif class_name not in HBNBCommand.classes:
+                print("** class doesn't exist **")
+                return
+            else:
+                if param_list:
+                    params = {}
+                    for item in param_list:
+                        key = item.split("=")[0].strip()
+                        value = item.split("=")[1].strip()
+                        if '\"' in value:
+                            value = str(value.translate({95: 32}))
+                        elif '.' in value:
+                            value = float(value)
+                        elif isinstance(value, int):
+                            value = int(value)
+                        params.update({key: value})
+                    new_instance = HBNBCommand.classes[class_name](**params)
+                else:
+                    new_instance = HBNBCommand.classes[class_name]()
+                storage.save()
+                print(new_instance.id)
+        except BaseException:
+            pass
 
     def help_create(self):
         """ Help information for the create method """
@@ -272,7 +293,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +301,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
@@ -319,6 +340,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 """ """
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 from models.base_model import Base
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 import os
 import urllib.parse
 
 workspace = os.getenv("HBNB_ENV")
 username = os.getenv("HBNB_MYSQL_USER")
-passwd = os.getenv("HBNB_MYSQL_PWD")
+passwd = urllib.parse.quoteplus(os.getenv("HBNB_MYSQL_PWD"))
 host = os.getenv("HBNB_MYSQL_HOST")
 port = 3306
 db = os.getenv("HBNB_MYSQL_DB")
@@ -37,35 +44,28 @@ class DBStorage:
             for obj in self.__session.query(cls):
                 dictionary.update({f"{cls}.{cls.id}": obj})
         else:
-            for obj in self.__session.query(*):
+            for obj in self.__session.query().all():
                 dictionary.update({f"{cls}.{cls.id}": obj})
         return dictionary
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        self.__session.Add(obj)
 
     def save(self):
         """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
-            json.dump(temp, f)
+        self.__session.commit()
 
     def reload(self):
         """Loads storage dictionary from file"""
-        classes = {
-            'BaseModel': BaseModel, 'User': User, 'Place': Place,
-            'State': State, 'City': City, 'Amenity': Amenity,
-            'Review': Review
-        }
-        try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
-        except FileNotFoundError:
-            pass
+        Base.metadata.create(self.__engine)
+        scope = sessionmaker.configure(bind=self.__engine,
+                                       expire_on_commit=False)
+        self.__session = scoped_session(scope)
+
+    def delete(self, obj=None):
+        """ Deletes object from __objects """
+        if obj is not None:
+            obj_name = f"{obj}.{obj.id}"
+            if obj_name in self.__objects.keys():
+                del self.__objects[obj]
