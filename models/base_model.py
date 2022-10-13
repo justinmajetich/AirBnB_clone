d74acbf44eb6e04d38e4b9bde3f1fdc2668370a6
Sylvester254 <slynganga59@gmail.com>
2022-10-13 15:35:13 +0300

    delete obj

--- models/base_model.py
+++ models/base_model.py
@@ -42,3 +42,8 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
+    def delete(self):
+        """Deletes the current instance from the storage (models.storage) by
+        calling the method delete."""
+        from models import storage
+        storage.delete(self)
