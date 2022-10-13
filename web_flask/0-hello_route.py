5ef06a36e91f085df7aaea80862b0cc2249ae0a1
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-10-13 12:43:44 +0100

    0. Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,11 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello_world():
+    """return Hello HBNB"""
     return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
