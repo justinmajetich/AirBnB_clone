8a82cc2b3c9fb1942dc7c492e5ac9218e3a2a0aa
Sylvester254 <slynganga59@gmail.com>
2022-10-13 12:05:13 +0300

    mysql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,11 @@
 -- prepares a MySQL server for the AirBnB clone
 -- create a database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- create a user and grant privileges
-GRANT ALL ON hbnb_dev_db.*
-TO 'hbnb_dev'@'localhost'
+-- create a user and grant ALL privileges
+CREATE USER 'hbnb_dev'@'localhost'
 IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL ON hbnb_dev_db.*
+TO 'hbnb_dev'@'localhost';
 -- grant SELECT privileges on performance_schema
 GRANT SELECT ON performance_schema.*
 TO 'hbnb_dev'@'localhost';
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,10 +1,11 @@
 -- prepares a MySQL test server for the AirBnb clone
 -- creates a database
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
--- create a user and grant privileges
-GRANT ALL ON hbnb_test_db.*
-TO 'hbnb_test'@'localhost'
+-- create a user and grant ALL privileges
+CREATE USER 'hbnb_test'@'localhost'
 IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL ON hbnb_test_db.*
+TO 'hbnb_test'@'localhost';
 -- grant SELECT privileges
 GRANT SELECT ON performance_schema.*
 TO 'hbnb_test'@'localhost';
