7878f4fefca564de04054f16d82830d458714a15
jonathan <achola.jon@yahoo.com>
2022-10-13 09:56:17 +0300

    chmod

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
-- Prepares a MySQL server for the project.
+-- Prepares a MySQL server for the project.
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
