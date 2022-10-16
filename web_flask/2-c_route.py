0f23dcea10ff7b16a0ff1b30d1ebc9f862140092
Kaleab <59199833+kaldeme48@users.noreply.github.com>
2022-10-16 22:57:10 +0300

    update task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -18,7 +18,6 @@ def hbnb():
     """returns HBNB"""
     return 'HBNB'
 
-
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
     """display “C ” followed by the value of the text variable"""
