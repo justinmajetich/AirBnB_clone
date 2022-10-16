816ef6a0f220bcecf54426dcf711aa677608230b
Menor25 <ajirimenor@gmail.com>
2022-10-16 22:06:31 +0100

    a script that starts a Flask web application and the web application must be listening on 0.0.0.0, port 5000

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" 3. Add third view func that redirects and has default val for variable """
+
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_world():
+    """ Returns some text. """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hello():
+    """ Return other text. """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """ replace text with variable. """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_text(text='is cool'):
+    """ replace more text with another variable. """
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
