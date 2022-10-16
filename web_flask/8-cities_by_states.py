d13b145d32a9736a51c423339184e60ceaf4948c
peter-wairimu <peter.wairimu@student.moringaschool.com>
2022-10-16 22:13:33 +0300

    intitial commit

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
-""" Script that runs a Flask app """
-from flask import Flask, render_template
+"""starts a Flask web application"""
+
 from models import storage
+from models.state import State
+from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 
+@app.route('/cities_by_states', strict_slashes=False)
+def cities():
+    """returns html template at /cities_by_states route,
+    listing cities by state."""
+    return render_template('8-cities_by_states.html',
+                           states=storage.all('State').values())
+
+
 @app.teardown_appcontext
-def closing(error):
-    """closes session """
+def teardown(self):
+    """Removes the current SQLAlchemy Session"""
     storage.close()
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def city():
-    """ function that returns text"""
-    state = storage.all('State')
-    return render_template('8-cities_by_states.html', state=state)
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
