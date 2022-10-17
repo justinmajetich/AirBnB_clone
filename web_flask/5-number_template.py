71827c6cf179aab7342a1f426791dc45065e1d61
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2022-10-17 14:44:16 +0100

    Update 5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,49 +1,62 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""
-Script that starts flash in web Applications. 
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+     /: Displays 'Hello HBNB!'.
+     /hbnb: Displays 'HBNB'.
+     /c/<text>: Displays 'C' followed by the value of <text>.
+      /python/(<text>): Displays 'Python' followed by the value of <text>.
+      /number/<n>: Displays 'n is a number' only if <n> is an integer.
+      /number_template/<n>: Displays an HTML page only if <n> is an integer.
 """
 from flask import Flask
 from flask import render_template
+
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello():
-    """Start a basic Flask web application"""
-    return 'Hello HBNB!'
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Adding a specific route /hbnb"""
-    return 'HBNB'
+    """Displays 'HBNB'"""
+    return "HBNB"
 
 
-@app.route('/c/<string:text>', strict_slashes=False)
-def c_text(text=None):
-    """Dynamic inputed text: C + replace _ for space and show text"""
-    return "C {}".format(text.replace('_', ' '))
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Displays 'C' followed by the value of <text>
+    Replaces any underscores in <text> with slashes.
+    """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<string:text>', strict_slashes=False)
-def python_text(text='is cool'):
-    """Dynamic inputed text: Python + replace _ for space and show text"""
-    return "Python {}".format(text.replace('_', ' '))
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """Displays 'Python' followed by the value of <text>
+    Replaces any underscores in <text> with slashes.
+    """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def only_digits_dynamic(n=None):
-    """Dynamic inputted integer"""
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """Displays 'n is a number' only if <n> is an integer."""
     return "{} is a number".format(n)
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def first_template(n=None):
-    """Display a HTML page only if n is an integer"""
-    return render_template('5-number.html', n=n)
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """Displays an HTML page only if <n> is an integer."""
+    return render_template("5-number.html", n=n)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
