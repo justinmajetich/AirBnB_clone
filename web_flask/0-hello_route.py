e5c664077b2ba40c4cde5a9f0dba1de9c05d20f9
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2022-10-17 14:46:30 +0100

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """Starts a Flask web application.
+
 The application listens on 0.0.0.0, port 5000.
-Routes: /: Displays 'Hello HBNB!'
+Routes: 
+     /: Displays 'Hello HBNB!'
 """
 from flask import Flask
 
