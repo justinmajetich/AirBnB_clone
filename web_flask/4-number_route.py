fe8b5e6a338a055688420e442574c1f7541a662e
Menor25 <ajirimenor@gmail.com>
2022-10-19 06:32:43 +0100

    script that starts a Flask web application and accept different route with int and string

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,50 +1,46 @@
-#!usr/bin/python3
-"""
-    Script that starts a Flask web application
-    Your web application must be listening on 0.0.0.0, port 5000
-    Routes:
-        /: display “Hello HBNB!”
-        /hbnb: display “HBNB”
-        /c/<text>: display “C ”,
-        /python/(<text>)
-        /number/<n>
-"""
+#!/usr/bin/python3
+""" 4. Add fourth view function that displays var only if is integer """
 
-from email.policy import strict
 from flask import Flask
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_world():
-    """Return a string"""
+    """ Returns some text. """
     return 'Hello HBNB!'
 
-@app.route('/hbnb', strict_slashes=False)
+
+@app.route('/hbnb')
 def hello():
-    """Return a string"""
+    """ Return other text. """
     return 'HBNB'
 
-@app.route('/c/<text>', strict_slashes=False)
-def hello_text(text):
-    """Return a string and a variable"""
+
+@app.route('/c/<text>')
+def c_text(text):
+    """ replace text with variable. """
     text = text.replace('_', ' ')
     return 'C {}'.format(text)
 
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/text', strict_slashes=False)
-def hello_python(text='is cool'):
-    """Return a string and a variable"""
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_text(text='is cool'):
+    """ replace more text with another variable. """
     text = text.replace('_', ' ')
     return 'Python {}'.format(text)
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    """Return a number” only if n is an integer"""
+
+@app.route('/number/<int:n>')
+def number_text(n):
+    """ replace with int only if given int. """
     n = str(n)
     return '{} is a number'.format(n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
