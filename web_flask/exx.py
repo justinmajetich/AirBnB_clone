e883077bba6c75a4d6c0405baa93aca575887e06
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-10-22 13:07:52 +0100

    0 flask web application

--- web_flask/exx.py
+++ web_flask/exx.py
@@ -1,16 +1,16 @@
-#!/usr/bin/python3
-"""
-Script that starts a flask web application
-"""
+#!/usr/bin/python4
+""" Script that runs an app with Flask framework """
 from flask import Flask
+
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Handles appropriate display for index route"""
+    """ Function called with / route """
     return 'Hello HBNB!'
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
+    
