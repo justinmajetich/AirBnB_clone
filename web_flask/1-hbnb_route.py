3da0126e5d553de3d2f416a18657ba1c28e931a2
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-10-22 13:17:03 +0100

    hbnb route

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-""" Script that runs an app with Flask framework """
+"""
+Script that starts a flask web application
+"""
 from flask import Flask
-
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """ Function called with / route """
+    """Handles appropriate display for index route"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """ Function called with /hbnb route """
+def render_hbnb():
+    """Handles appropriate display for /hbnb route"""
     return 'HBNB'
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
-    
