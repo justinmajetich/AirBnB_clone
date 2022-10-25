577dc43457da190f139979bb26c107f4bd620799
mdallen5393 <allen_matthew@outlook.com>
2022-10-25 14:18:11 +0000

    Task 6. Update City class

--- models/city.py
+++ models/city.py
@@ -3,7 +3,10 @@
 from models.base_model import BaseModel
 
 
-class City(BaseModel):
+class City(BaseModel, Base): # +T6: Base
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities'
+    # state_id = "" -T6
+    state_id = Column(String(60), nullable=False, ForeignKey('states.id')) # +T6
+    # name = "" # -T6
+    name = Column(String(128), nullable=False) # +T6
