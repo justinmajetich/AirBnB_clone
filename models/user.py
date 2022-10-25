0e9db8333d92151412b25414ae772770d9013568
root <ben.sbanotto@gmail.com>
2022-10-25 09:16:41 -0700

    Commit changes to user.py for task 7

--- models/user.py
+++ models/user.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+# commented out code base for task 7
+# class User(BaseModel):
+#     """File Storage Method for User"""
+#     email = ''
+#     password = ''
+#     first_name = ''
+#     last_name = ''
+
+class User(BaseModel, Base):
+    """
+    Database Storage Method for User
+    """
+    __tablename__ = "users"
+    email = Column(string(128), nullable=False)
+    password = Column(string(128), nullable=False)
+    first_name = Column(string(128), nullable=False)
+    last_name = Column(string(128), nullable=False)
