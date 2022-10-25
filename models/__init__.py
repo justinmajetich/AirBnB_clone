9a2dad86c0b639b95bad9e351b81e902aac012be
root <ben.sbanotto@gmail.com>
2022-10-25 08:52:26 -0700

    Committing engine/db_storage and updates to __init__

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+"""Initializes models module"""
+import os
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
-storage = FileStorage()
-storage.reload()
+if os.getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
+
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
