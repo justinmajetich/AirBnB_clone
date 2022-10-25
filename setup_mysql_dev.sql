fc26c43ea48da5f355b2a7af11d8616a73ce56c3
Baylee Duncan <90226747+b-duncs@users.noreply.github.com>
2022-10-25 12:32:23 -0500

    updated file

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,6 +2,6 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 USE hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
-    IDENTIFIED BY 'hbnb_dev_pwd';
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
