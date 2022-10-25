c8cdd6ae9bf9b37df6b9da836c71960fbca1eb1a
Baylee Duncan <90226747+b-duncs@users.noreply.github.com>
2022-10-25 12:20:52 -0500

    updated file

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,7 @@
 --script that prepares a MySQL server for the project
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+USE hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
     IDENTIFIED BY PASSWORD 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES on hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
