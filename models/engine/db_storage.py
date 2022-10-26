53ae6417524541e3dc3b63c1204b1a10d6bb88db
wilstermanz <wilstermanz@gmail.com>
2022-10-25 20:30:24 -0500

    Created all method for dbstorage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,6 +3,11 @@
 from sqlalchemy import create_engine
 from os import getenv
 from models.base_model import Base
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 class DBStorage:
     """database storage engine"""
@@ -22,3 +27,18 @@ class DBStorage:
         Base.metadata.create_all(engine)
         if env == "test":
             Base.metadata.drop_all()
+
+    def all(self, cls=None):
+        """ """
+        classes = [User, State, City, Amenity, Place, Review]
+
+        cls_dict = {}
+        if cls in classes:
+            list_obj = self.__session.query(cls).all()
+        elif cls is None:
+            list_obj =[]
+            for item in classes:
+                list_obj += self.__session.query(cls).all()
+        for obj in list_obj:
+            cls_dict[f"{cls.__name__}.{obj.id}"] = obj
+        return cls_dict
