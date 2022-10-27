930a5b09040ab8be79641984ffba1780954016c6
AaronManuel15 <aaronmanuel15@gmail.com>
2022-10-26 17:34:43 -0700

    Another for Task 6

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Database storage engine module"""
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
 from os import getenv
 from models.base_model import Base
 from models.city import City
@@ -9,6 +10,7 @@ from models.review import Review
 from models.state import State
 from models.user import User
 from models.amenity import Amenity
+
 
 class DBStorage:
     """database storage engine"""
@@ -37,9 +39,29 @@ class DBStorage:
         if cls in classes:
             list_obj = self.__session.query(cls).all()
         elif cls is None:
-            list_obj =[]
+            list_obj = []
             for item in classes:
                 list_obj += self.__session.query(cls).all()
         for obj in list_obj:
             cls_dict[f"{cls.__name__}.{obj.id}"] = obj
         return cls_dict
+
+    def new(self, obj):
+        """ Adds object to current db session"""
+        self.__session.add(obj)
+
+    def save(self):
+        """ Commits changes of the current db session"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """ Deletes obj from the current db session"""
+        if obj:
+            self.__session.delete(obj)
+        else:
+            pass
+
+    def reload(self):
+        """Creates all tables in the db and creates the current db sesh"""
+        Base.metadata.create_all(self.__engine)
+        self.__session = scoped_session(sessionmaker(bind=self.__engine, expire_on_commit=False))
