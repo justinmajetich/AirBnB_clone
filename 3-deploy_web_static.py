8fce98f30b4d514b5ea35f22cd1fdc480235fa3c
Doro256 <dorothynantumbwe44@gmail.com>
2022-10-27 19:49:38 +0000

    edit servers

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ distributes an archive to the web servers
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['142.44.167.228', '144.217.246.195']
+env.hosts = ['3.235.144.29', '54.237.37.164']
 
 
 def do_pack():
