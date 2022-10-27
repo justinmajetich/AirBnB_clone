64f8a62acacfbd85da286d558449cb364b40c3d7
Doro256 <dorothynantumbwe44@gmail.com>
2022-10-27 19:16:12 +0000

    Task 1

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# sets up the web servers for the deployment of web_static
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "<html>
+    <head>
+    </head>
+    <body>
+        This is a test
+    </body>
+</html>" > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i 'listen 80 default_server/a location /hbnb_static/ {\n\t\t alias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx start
