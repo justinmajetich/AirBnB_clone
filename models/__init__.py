af0ffe51026f9fd3674912b944fa6c8a7becfd20
Baylee Duncan <90226747+b-duncs@users.noreply.github.com>
2022-10-27 19:16:44 -0500

    chmod files

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 import os
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from models.user import User
 from models.place import Place
 from models.state import State
--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class to manage db storage for hbnb clone"""
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from models.base_model import BaseModel, Base
 from models.user import User
