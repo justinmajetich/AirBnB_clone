b2a2f136a5dfbc76a84e001a725b863b2b44d59c
Baylee Duncan <90226747+b-duncs@users.noreply.github.com>
2022-10-27 23:23:38 -0500

    revert files

--- models/amenity.py
+++ models/amenity.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import String, Column, ForeignKey
-from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship(
-        "Place",
-        secondary='place_amenity',
-        viewonly=False
-    )
+    __tablename__ = 'amenities'
+    name = ""
