2f0241b4697d2de6bf08acd492cd408ffbd3f836
MwangiGregory <gmwaburi@gmail.com>
2022-11-01 11:36:23 +0000

    Delete do_create logic

--- console.py
+++ console.py
@@ -115,16 +115,6 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        if not args:
-            print("** class name missing **")
-            return
-        elif args not in HBNBCommand.classes:
-            print("** class doesn't exist **")
-            return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
-        print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
