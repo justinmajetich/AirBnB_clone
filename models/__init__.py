64abf47225917ed0a4f19c633fc2dc8ff037401f
AturiheihiBlendon <2020blendon@gmail.com>
2022-11-13 23:48:52 +0300

    improved database storage

--- models/__init__.py
+++ models/__init__.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
 from os import getenv
-from models.engine.db_storage import DBStorage
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
-    storage.reload()
 else:
     storage = FileStorage()
-    storage.reload()
+    from models.engine.file_storage import FileStorage
+storage.reload()
