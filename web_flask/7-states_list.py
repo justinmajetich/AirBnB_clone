46efb08056460e9a977e3f32765178a096b36af2
AturiheihiBlendon <2020blendon@gmail.com>
2022-11-15 11:47:05 +0300

    Fixed pycodestyle

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def states():
-    """returns a rendered html template at /state_list route."""
+    """returns a rendered html template at /state_list route"""
     states = storage.all("State")
     return render_template('7-states_list.html', states=states)
 
