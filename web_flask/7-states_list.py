a0cb8be98faa96f491faeaf14d12a46b18683d8d
AturiheihiBlendon <2020blendon@gmail.com>
2022-11-15 11:41:20 +0300

    fetch data from database and render it in html

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,6 +4,8 @@
 from models import storage
 from flask import Flask
 from flask import render_template
+
+
 app = Flask(__name__)
 
 
@@ -13,10 +15,12 @@ def states():
     states = storage.all("State")
     return render_template('7-states_list.html', states=states)
 
+
 @app.teardown_appcontext
 def teardown(self):
     """Removes the current SQLAlchemy Session"""
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
