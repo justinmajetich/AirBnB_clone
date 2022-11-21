e8e03921a547bbd4703e56c0423c2bf61514721c
tizihoxha <tizihoxha@gmail.com>
2022-11-21 04:57:37 -0800

    added

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,15 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls:
+            if type(cls) == str:
+                cls = eval(cls)
+            dict_1 = {}
+            for key, value in self.__objects.items():
+                if type(value) == cls:
+                    dict_1[key] = value
+                return dict_1
+        return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
