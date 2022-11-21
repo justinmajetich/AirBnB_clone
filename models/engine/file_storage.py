3d584d202c4fd06d2b244ddf128fdaa37dc94e63
Rbrogniez <5013@holbertonschool.com>
2022-11-21 14:07:26 +0100

    add cls = None in def all

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,7 +8,7 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         return FileStorage.__objects
 
