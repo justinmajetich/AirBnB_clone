c36eaa1c3f19e240766dbbb8c3748bc1f6518143
Rbrogniez <5013@holbertonschool.com>
2022-11-21 12:21:10 +0100

    adding : grand select privilege for hbnb_dev in performance schema database

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,3 +4,5 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@localhost IDENTIFIED BY 'hbnb_dev_pwd';
 -- GIVE ALL PRIVILEGES for USER in DATABASE
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@localhost;
+-- GIVE SELECT PRIVILEGES for USER in DATABASE
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@localhost;
