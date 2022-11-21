727c9922bc937eecd6e44f15348582b5dd08d943
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-11-21 19:21:23 -0300

    setup_mysql_dev

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,4 @@
+-- creates the database hbnb_dev_db and the user hbnb_dev
 CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
