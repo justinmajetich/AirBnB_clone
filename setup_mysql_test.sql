14aea1238db95172f2aa7bdd5c5c47565e19966b
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-11-21 19:29:52 -0300

    setup_mysql_test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,5 @@
+-- creates the database hbnb_dev_db and the user hbnb_dev
+CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
