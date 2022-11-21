f33217d10f96dd637a1697e1e2e20e9168c3b585
Rbrogniez <5013@holbertonschool.com>
2022-11-21 15:04:49 +0100

    fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,7 @@
 """This module defines a class to manage file storage for hbnb clone"""
 import json
 
+
 from models.base_model import BaseModel
 from models.user import User
 from models.place import Place
