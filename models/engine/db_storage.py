9029eca96c8c8d8dbb8e908c8ad53fecd061292c
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 <l.leroty@gmail.com>
2022-11-22 17:23:33 +0100

    update DB_storage, reload

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -5,9 +5,11 @@ import json
 import MySQLdb
 import sys
 from sqlalchemy import (create_engine)
+from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
 from models.base_model import BaseModel, Base
 import os
+from file_storage import FileStorage
 
 __engine = None
 __session = None
@@ -22,7 +24,37 @@ def __init__(self):
         Base.metadata.drop_all(__engine)
 
 def all(self, cls=None):
-    self.__session.query(cls).all()
+    my_dict = {}
+    if cls:
+        # with .items, use the key and value
+        for key, val in self.__objects.items():
+            # returns the list of objects of one type of class
+            if cls is type(val):
+                # attribute the value to the key
+                my_dict[key] = val
+        return my_dict
+    return FileStorage.__objects
 
-    if cls == None:
+def new(self, obj):
+    """add the object to the current database session """
+    self.__session.add(obj)
 
+def save(self):
+    """commit all changes of the current database session"""
+    self.__session.commit()
+
+def delete(self, obj=None):
+    """delete from the current database session obj if not None"""
+    if obj is not None:
+        self.__session.delete(obj)
+
+def reload(self):
+    """
+    create all tables in the db (all classes who inherit from Base are
+    imported before calling)
+    """
+    Base.metadata.create_all(self.__engine)
+    session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+    # scoped_session - to make sure your Session is thread-safe
+    Session = scoped_session(session_factory)
+    self.__session = Session
