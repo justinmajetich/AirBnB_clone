3c44acaf73326f71b96250c2cce4968f3c8465ca
tizihoxha <tizihoxha@gmail.com>
2022-11-22 04:18:36 -0800

    added

--- models/base_model.py
+++ models/base_model.py
@@ -20,36 +20,35 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwarg.items():
+                if key = "created_at" or key = "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
 
-    def __str__(self):
-        """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.new(self)
+
+    def delete(self):
+        models.storage.delete(self)
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+        dict1 = self.__dict__.copy()
+        dict1["__class__"] = str(type(self).__name__)
+        dict1['created_at'] = self.created_at.isoformat()
+        dict1['updated_at'] = self.updated_at.isoformat()
+        dict1.pop("_sa_instance_state", None)
+
+    def __str__(self):
+        """Returns a string representation of the instance"""
+        cls = type(self).__name__
+        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+
