6a5b62dba49343827cf56091bab9da8024a9a449
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-11-22 16:44:04 +0100

    done

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,43 +1,41 @@
 #!/usr/bin/python3
-""" Script that runs an app with Flask framework """
+"""
+Script that starts a flask web application
+"""
 from flask import Flask
-
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """ Function called with / route """
+    """Handles appropriate display for index route"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """ Function called with /hbnb route """
+def render_hbnb():
+    """Handles appropriate display for /hbnb route"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    """ Function called with /c/<text> route """
+    """Handles appropriate display for /c/:text route """
     return 'C %s' % text.replace('_', ' ')
 
 
-@app.route('/python/', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(text='is cool'):
-    """ Function called with /python/<text> route """
-    if text is not 'is cool':
-        text = text.replace('_', ' ')
-    return 'Python %s' % text
+def python_text(text='is_cool'):
+    """Handles appropriate display for /python/:text route """
+    return 'Python %s' % text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    """ Function called with /number/<n> route """
-    return "%d is a number" % n
+def number_n(n):
+    """Handles appropriate display for /number/:n route """
+    return '%d is a number' % n
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
-    
