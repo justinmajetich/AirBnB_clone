0c878e0b9ac102e84d0ba4e728debc273f3a5075
Rbrogniez <5013@holbertonschool.com>
2022-11-22 15:06:00 +0100

    add tablesnames and names

--- models/state.py
+++ models/state.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-
+from sqlalchemy import Column, String, ForeignKey
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablesname__ = 'states'
+    name = Column(String(128), nullable=False)
