f38ef8fa91a5689da559c55ce466e5c47a652b7b
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-11-22 19:24:16 -0300

    Update state.py for relationship with cities

--- models/state.py
+++ models/state.py
@@ -2,10 +2,22 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel
 from models.base_model import Base
-from sqlalchemy import Column, String
+from models.city import City
+from models import storage
+from sqlalchemy import Column, String, relationship
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state', cascade='delete')
+
+    @property
+    def cities(self):
+        """Returns list of city instances in same state"""
+        my_list = []
+        for i in storage.all(City):
+            if self.id == i.state_id:
+                my_list.append(i)
+        return my_list
