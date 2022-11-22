7716e72ffa7474e568ee7697fd2678579919da69
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-11-22 19:29:48 -0300

    Update mysql files

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,5 @@
 -- creates the database hbnb_dev_db and the user hbnb_dev
-CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
