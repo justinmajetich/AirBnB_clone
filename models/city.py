0db5c75293b5530a418818fb53338fc96c371435
Rbrogniez <5013@holbertonschool.com>
2022-11-22 12:42:05 +0100

    add tablesname/stateid/name

--- models/city.py
+++ models/city.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
-
+from sqlalchemy import Column, String
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablesname__ = 'cities'
+    state_id = Column(String(60), nullable=False, primary_key=False)
+    name = Column(String(128), nullable=False)
