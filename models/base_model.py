4921c86dabfa5655a037e5538fd7adc832a01933
Eno Gupe <91369667+eno007@users.noreply.github.com>
2022-11-22 16:07:22 +0100

    update base model

--- models/base_model.py
+++ models/base_model.py
@@ -25,7 +25,7 @@ class BaseModel:
         self.updated_at = datetime.now()
         if kwargs:
             for key, value in kwarg.items():
-                if key = "created_at" or key = "updated_at":
+                if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
