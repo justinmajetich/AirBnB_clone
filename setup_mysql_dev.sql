813b3018e800716dc1d71a01a712af63e952ee86
root <you@example.com>
2022-11-22 01:20:31 -0800

    script that prepares a MySQL server for the project

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -6,3 +6,4 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON 'hbnb_dev_db'.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- Set SELECT privilege for performance_schema DB
 GRANT SELECT ON 'performance_schema'.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+FLUSH PRIVILEGES;
