815aa170e66f0d8e2b88b91e6998de0dbedc6a3a
luisvillegasbf23 <luisville.lv@gmail.com>
2022-11-22 16:25:58 -0800

    task 7 y 8

--- models/city.py
+++ models/city.py
@@ -5,5 +5,14 @@ from models.base_model import BaseModel
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+
+    if (getenv('HBNB_TYPE_STORAGE') == 'db'):
+        __tablename__ = 'cities'
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        places = relationship("Place", backref="cities", cascade="all, delete")
+
+    if (getenv('HBNB_TYPE_STORAGE') != 'db'):
+        state_id = ''
+        name = ''
+        places = []
--- models/user.py
+++ models/user.py
@@ -5,7 +5,18 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+
+    if (getenv('HBNB_TYPE_STORAGE') == 'db'):
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+
+        places = relationship("Place", backref="user", cascade="all, delete")
+        reviews = relationship("Review", backref="user", cascade="all, delete")
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
