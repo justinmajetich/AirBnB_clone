d728172ae74c5453922fca6a097a012380277ee3
Mateo Bonino <mbonino1810@gmail.com>
2022-11-23 10:53:15 -0300

    Privilege fixes

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,4 +5,3 @@ USE hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON * . * TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema. * TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
