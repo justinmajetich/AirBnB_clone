0d73cca5985c0f9c707209bf5b1b2443e63d5e82
Eno Gupe <91369667+eno007@users.noreply.github.com>
2022-11-23 14:45:16 +0100

    test

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -52,17 +52,11 @@ class FileStorage:
         from models.amenity import Amenity
         from models.review import Review
         
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
         try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+            with open(self.__file_path, 'r', encoding="UTF-8") as f:
+                for key, value in (json.load(f)).items():
+                    value = eval(value["__class__"])(**value)
+                    self.__objects[key] = value
         except FileNotFoundError:
             pass
 
