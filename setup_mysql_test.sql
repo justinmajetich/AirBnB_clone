d58ade0ea7dfc80378255ee8a40eded321daa68e
Maykha27 <5144@holbertonschool.com>
2022-11-23 14:44:59 +0100

    tache 3

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,17 +3,9 @@
 -- User hbnb_test with password hbnb_test_pwd in localhost.
 -- Grants all privileges for hbnb_test on hbnb_test_db.
 -- Grants SELECT privilege for hbnb_test on performance_schema.
-
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER
-	IF NOT EXISTS 'hbnb_test'@'localhost'
-	IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES
-	ON `hbnb_test_db`.*
-	TO 'hbnb_test'@'localhost'
-	IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT
-	ON `performance_schema`.*
-	TO 'hbnb_test'@'localhost'
-	IDENTIFIED BY 'hbnb_test_pwd';
-FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
