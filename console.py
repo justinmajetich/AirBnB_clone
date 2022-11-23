936b50cc262cc2dce67e3b7e94785251bececfab
Eno Gupe <91369667+eno007@users.noreply.github.com>
2022-11-23 12:14:05 +0100

    update

--- console.py
+++ console.py
@@ -283,7 +283,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
