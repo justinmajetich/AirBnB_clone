6b1f74b7a3df17088404994d02efed56f5c3e857
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-11-23 00:23:19 -0300

    Old files

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-#!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
 
 
