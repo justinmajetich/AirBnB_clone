f1e7eb998fdb4c6d3854332bfccacff046cd6890
Rbrogniez <5013@holbertonschool.com>
2022-11-23 16:04:59 +0100

    back to last version

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,4 +55,4 @@ class FileStorage:
     def delete(self, obj=None):
         """delete obj from __objects if it's inside - if obj is equal to None"""
         if obj is not None in self.__objects:
-            del self.objects[obj]
+            del self.__objects[obj]
