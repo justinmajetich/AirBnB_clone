09eac0483a9cb94f5b4db014c84f15bc67cc13ea
Eno Gupe <91369667+eno007@users.noreply.github.com>
2022-11-23 12:56:43 +0100

    update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -28,7 +28,7 @@ class DBStorage:
                                        getenv("HBNB_MYSQL_HOST"),
                                        getenv("HBNB_MYSQL_DB")),
                                pool_pre_ping=True)
-        if getenv("HBNB_ENV") ==  "test":
+        if os.getenv("HBNB_ENV") ==  "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
