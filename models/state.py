46fbb9cec8c26777a1bd31baefe6789e9807f3ce
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-11-23 00:22:53 -0300

    Old files

--- models/state.py
+++ models/state.py
@@ -1,24 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-from models.base_model import Base
-from models.city import City
-from models import storage
-from sqlalchemy import Column, String
-from sqalchemy.orm import relationship
 
 
-class State(BaseModel, Base):
+class State(BaseModel):
     """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade='delete')
-
-    @property
-    def cities(self):
-        """Returns list of city instances in same state"""
-        my_list = []
-        for i in storage.all(City):
-            if self.id == i.state_id:
-                my_list.append(i)
-        return my_list
+    name = ""
