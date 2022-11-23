45ddf15d0b4cbc1f98fbdcc7909cb4f6be73d3e4
Mateo Bonino <mbonino1810@gmail.com>
2022-11-23 10:51:32 -0300

    Privilege fixes

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,6 +3,6 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 USE hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON * . * TO 'hbnb_dev'@'localhost'
+GRANT ALL PRIVILEGES ON * . * TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema. * TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
