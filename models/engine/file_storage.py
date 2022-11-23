b35448a802e7a2a103394a72d2d4d07922161ff6
Eno Gupe <91369667+eno007@users.noreply.github.com>
2022-11-23 12:37:47 +0100

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -42,7 +42,14 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+        
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
                     'State': State, 'City': City, 'Amenity': Amenity,
