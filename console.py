57905bf24abd4e1f6048243b0bdf365a4e20d3ea
Eno Gupe <91369667+eno007@users.noreply.github.com>
2022-11-23 12:27:08 +0100

    update to fix errors

--- console.py
+++ console.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """ Console Module """
 
+
+import cmd
 from models.base_model import BaseModel
-from models.__init__ import storage
+from models import storage
 from models.user import User
 from models.place import Place
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
-import cmd
 import sys
+from datetime import datetime
 
 
 class HBNBCommand(cmd.Cmd):
