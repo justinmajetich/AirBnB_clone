d92efad7c123de452b62536541237806fdc6c4a3
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-11-24 10:38:24 -0300

    Fix city

--- models/city.py
+++ models/city.py
@@ -12,4 +12,3 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'),nullable=False)
-    places = relationship('Place', backref='cities')
