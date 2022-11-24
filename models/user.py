a2f23464f16918fa26689a941dcca62c29133cc9
tizihoxha <tizihoxha@gmail.com>
2022-11-24 05:18:14 -0800

    added

--- models/user.py
+++ models/user.py
@@ -13,6 +13,6 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-    places = relationship("Place", backref="user", 
+    places = relationship("Place", backref="user",
                           cascade="all, delete-orphan")
     reviews = relationship('Review', backref='user', cascade='delete')
