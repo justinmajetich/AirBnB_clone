c17ba72681d7bc5ec78d2741b3bfa4415c2a7404
Eno Gupe <91369667+eno007@users.noreply.github.com>
2022-11-24 16:24:39 +0100

    edit

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,49 +1,25 @@
 #!/usr/bin/python3
 """test for file storage"""
 import unittest
-import pep8
-import json
-import os
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+from models.engine import db_storage
 from models.engine.db_storage import DBStorage
-import MySQLdb
 
 
-class TestDBStorage(unittest.TestCase):
-    '''this will test the FileStorage'''
+class TestConsole(unittest.TestCase):
+    """ Test class for DBStorage """
 
-    @classmethod
-    def setUp(self):
-        """set up MySQLdb"""
-        self.db = MySQLdb.connect(host="localhost",
-                                  port=3306,
-                                  user='hbnb_test',
-                                  passwd='hbnb_test_pwd',
-                                  db='hbnb_test_db')
-        self.cursor = self.db.cursor()
-        self.storage = DBStorage()
-        self.storage.reload()
+    def test_documentation(self):
+        """ Test docstrings documentation"""
 
-    @classmethod
-    def tearDown(self):
-        """teardown"""
-        self.cursor.close()
-        self.db.close()
+        self.assertTrue(db_storage.__doc__)
+        self.assertTrue(db_storage.DBStorage.__doc__)
 
-    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db',
-                     'Skip if >>NOT<< in db mode')
-    def test_pep8_DBStorage(self):
-        """Tests pep8 style"""
-        style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(['models/engine/db_storage.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
+    def test_methods_doc(self):
+        """ Test all docstrings of each method"""
+
+        for all_methods in dir(DBStorage):
+            self.assertTrue(all_methods.__doc__)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
