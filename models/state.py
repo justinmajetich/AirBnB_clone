7b9085c6dc183e0f3d28218cad10fd68f2c78411
tizihoxha <tizihoxha@gmail.com>
2022-11-24 04:35:58 -0800

    update

--- models/state.py
+++ models/state.py
@@ -6,7 +6,7 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
