9307543626aa2c322e08e52aac1142bcd96b1b89
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-11-24 12:01:35 -0300

    Update db_storage create_engine

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -22,10 +22,10 @@ class DBStorage:
     def __init__(self):
         """Constructor for DBStorage"""
         self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".
-                                      format(getenv("HBNB_MYSQL_USER"),
-                                             getenv("HBNB_MYSQL_PWD"),
-                                             getenv("HBNB_MYSQL_HOST"),
-                                             getenv("HBNB_MYSQL_DB")),
+                                      format(getenv('HBNB_MYSQL_USER'),
+                                             getenv('HBNB_MYSQL_PWD'),
+                                             getenv('HBNB_MYSQL_HOST'),
+                                             getenv('HBNB_MYSQL_DB')),
                                       pool_pre_ping=True)
         if getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
