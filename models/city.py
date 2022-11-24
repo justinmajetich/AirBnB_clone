89e4e669ee13d1a904200a126b27df476f1f0b9c
Mateo Bonino <mbonino1810@gmail.com>
2022-11-24 01:41:11 -0300

    Task 6

--- models/city.py
+++ models/city.py
@@ -3,11 +3,10 @@
 from models.base_model import BaseModel, Base
 import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey, Integer
-
+from sqlalchemy.orm import relationship
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    """ state_id = ""
-    name = "" """
+    
--- models/state.py
+++ models/state.py
@@ -3,8 +3,25 @@
 from models.base_model import BaseModel, Base
 import sqlalchemy
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import os
+from models import city
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state', cascade='delete')
+
+    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            from models import storage
+            obj_list = []
+            for i, j in storage.all(city.City).items():
+                if j.state_id == self.id:
+                    obj_list.append(j)
+            return obj_list
+            
+            
