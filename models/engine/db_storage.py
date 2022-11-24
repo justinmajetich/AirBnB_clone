3dbdc5d319fa3190776a0ca2bd261598fce260b7
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 <l.leroty@gmail.com>
2022-11-24 23:08:19 +0100

    update the db storage classes

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -8,9 +8,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
 from models.base_model import BaseModel, Base
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 import os
 
 
+classes = {
+        'BaseModel': BaseModel, 'User': User, 'Place': Place,
+        'State': State, 'City': City, 'Amenity': Amenity,
+        'Review': Review
+         }
 
 class DBStorage:
     __engine = None
