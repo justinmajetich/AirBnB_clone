649b9a0f00652ed20e1db119c71239918bafdc9b
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-11-24 10:32:27 -0300

    Update syntax of state and city

--- models/city.py
+++ models/city.py
@@ -9,11 +9,7 @@ from os import getenv
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'cities'
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'),nullable=False)
-        places = relationship('Place', backref='cities')
-    else:
-        state_id = ''
-        name = ''
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'),nullable=False)
+    places = relationship('Place', backref='cities')
--- models/state.py
+++ models/state.py
@@ -10,12 +10,9 @@ from os import getenv
 
 class State(BaseModel, Base):
     """ State class """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state', cascade='delete')
-    else:
-        name=''
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state', cascade='delete')
 
     @property
     def cities(self):
