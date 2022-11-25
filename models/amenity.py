e033efb7b976aa4029fc9726e25275b20fcc3c5c
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 <l.leroty@gmail.com>
2022-11-25 10:49:54 +0100

    update classes

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from models.base_model import BaseModel
 
 
-class Amenity(BaseModel, Base):
-    __tablename__ = 'Amenities'
-    name = Column(String(128), nullable=False)
+class Amenity(BaseModel):
+    name = ""
--- models/state.py
+++ models/state.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from models.base_model import BaseModel
 
-class State(BaseModel, Base):
+
+class State(BaseModel):
     """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
+    name = ""
