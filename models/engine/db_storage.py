182689d3db868c144b821e14f546daafeedaf9e1
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 <l.leroty@gmail.com>
2022-11-25 21:47:48 +0100

    add the storages

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,1 +1,80 @@
+#!/usr/bin/python3
+"""This module defines a class to manage file storage for hbnb clone"""
+import json
 
+import MySQLdb
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
+from models.base_model import BaseModel, Base
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+import os
+
+
+classes_list = {'City', 'User', 'Place', 'State', 'Amenity', 'Review'}
+
+class DBStorage:
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        # Return the value of the environment variable key as a string if it exists
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@localhost{}/{}'.format(os.getenv("HBNB_MYSQL_USER"), os.getenv("HBNB_MYSQL_PWD"), os.getenv("HBNB_MYSQL_HOST"), os.getenv("HBNB_MYSQL_DB")),
+        pool_pre_ping=True)
+
+        # drop all tables if the environment variable HBNB_ENV is equal to test
+        if os.getenv("HBNB_ENV") == "test":
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """
+        query on the current database session (self.__session) all objects
+        depending of the class name (argument cls)
+        """
+        if cls is None:
+            My_obj = self.__session.query(State).all()
+            My_obj.extend(self.__session.query(City).all())
+            My_obj.extend(self.__session.query(User).all())
+            My_obj.extend(self.__session.query(Place).all())
+            My_obj.extend(self.__session.query(Review).all())
+            My_obj.extend(self.__session.query(Amenity).all())
+        else:
+            if type(cls) == str:
+                cls = eval(cls)
+            My_obj = self.__session.query(cls)
+        return {"{}.{}".format(type(o).__name__, o.id): o for o in My_obj}
+
+    def new(self, obj):
+        """add the object to the current database session """
+        self.__session.add(obj)
+
+    def save(self):
+        """commit all changes of the current database session"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """delete from the current database session obj if not None"""
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """
+        create all tables in the db (all classes who inherit from Base are
+        imported before calling)
+        """
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+
+        # scoped_session - to make sure your Session is thread-safe
+        Session = scoped_session(session_factory)
+        self.__session = Session()
+
+    def close(self):
+        """ close the session """
+        self.__session.close()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,15 +1,40 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
+
+classes = {
+        'BaseModel': BaseModel, 'User': User, 'Place': Place,
+        'State': State, 'City': City, 'Amenity': Amenity,
+        'Review': Review
+         }
+
+
+__objects = {}
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        dict = {}
+        if cls:
+            # with .items, use the key and value
+            for key, val in self.__objects.items():
+                # returns the list of objects of one type of class
+                if cls is type(val):
+                    # attribute the value to the key
+                    dict[key] = val
+            return dict
         return FileStorage.__objects
 
     def new(self, obj):
@@ -27,19 +52,6 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
@@ -48,3 +60,17 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """delete obj from __objects if it's inside - if obj is equal to None"""
+        if obj is not None:
+            # define the key
+            key = obj.__class__.__name__ + '.' + obj.id
+            # if there is a key, delete it
+            if self.__objects[key]:
+                del self.__objects[key]
+                self.save()
+
+    def close(self):
+        """ close"""
+        self.reload()
