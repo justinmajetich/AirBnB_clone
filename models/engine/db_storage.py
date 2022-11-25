f06521101880c034e1d7037fce58656e6be7fc62
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 <l.leroty@gmail.com>
2022-11-25 22:05:25 +0100

    update db_storage and filestorage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -17,7 +17,11 @@ from models.review import Review
 import os
 
 
-classes_list = {'City', 'User', 'Place', 'State', 'Amenity', 'Review'}
+classes_list = {
+        'User', 'Place',
+        'State', 'City', 'Amenity',
+        'Review'
+         }
 
 class DBStorage:
     __engine = None
@@ -25,30 +29,29 @@ class DBStorage:
 
     def __init__(self):
         # Return the value of the environment variable key as a string if it exists
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@localhost{}/{}'.format(os.getenv("HBNB_MYSQL_USER"), os.getenv("HBNB_MYSQL_PWD"), os.getenv("HBNB_MYSQL_HOST"), os.getenv("HBNB_MYSQL_DB")),
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(os.getenv("HBNB_MYSQL_USER"), os.getenv("HBNB_MYSQL_PWD"), os.getenv("HBNB_MYSQL_HOST"), os.getenv("HBNB_MYSQL_DB")),
         pool_pre_ping=True)
 
         # drop all tables if the environment variable HBNB_ENV is equal to test
         if os.getenv("HBNB_ENV") == "test":
-            Base.metadata.drop_all(self.__engine)
+            Base.metadata.drop_all(DBStorage.__engine)
 
     def all(self, cls=None):
         """
         query on the current database session (self.__session) all objects
         depending of the class name (argument cls)
         """
-        if cls is None:
-            My_obj = self.__session.query(State).all()
-            My_obj.extend(self.__session.query(City).all())
-            My_obj.extend(self.__session.query(User).all())
-            My_obj.extend(self.__session.query(Place).all())
-            My_obj.extend(self.__session.query(Review).all())
-            My_obj.extend(self.__session.query(Amenity).all())
-        else:
-            if type(cls) == str:
-                cls = eval(cls)
-            My_obj = self.__session.query(cls)
-        return {"{}.{}".format(type(o).__name__, o.id): o for o in My_obj}
+        my_dict = {}
+
+        if cls:
+            # with .items, use the key and value
+            for key, val in classes_list.items():
+                # returns the list of objects of one type of class
+                if cls is type(val):
+                    # attribute the value to the key
+                    my_dict[key] = val
+            return my_dict
+        return DBStorage.__engine
 
     def new(self, obj):
         """add the object to the current database session """
@@ -70,11 +73,6 @@ class DBStorage:
         """
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
-
         # scoped_session - to make sure your Session is thread-safe
         Session = scoped_session(session_factory)
-        self.__session = Session()
-
-    def close(self):
-        """ close the session """
-        self.__session.close()
+        self.__session = Session
