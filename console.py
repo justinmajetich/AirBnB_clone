0ac7895fdee860335fc5c29e5d56bcd62361bedc
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 <l.leroty@gmail.com>
2022-11-25 22:46:53 +0100

    update console

--- console.py
+++ console.py
@@ -190,6 +190,7 @@ class HBNBCommand(cmd.Cmd):
             return
 
         key = c_name + "." + c_id
+
         try:
             print(storage._FileStorage__objects[key])
         except KeyError:
@@ -236,6 +237,7 @@ class HBNBCommand(cmd.Cmd):
     def do_all(self, args):
         """ Shows all objects, or all objects of a class"""
         print_list = []
+
         if args:
             args = args.split(' ')[0]  # remove possible trailing args
             if args not in HBNBCommand.classes:
