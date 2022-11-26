c0a6851dabbca3677beb9911e6041ddcb1f27d47
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 <l.leroty@gmail.com>
2022-11-26 11:49:49 +0100

    update import Filestorage

--- console.py
+++ console.py
@@ -10,7 +10,6 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
-from models.__init__ import FileStorage
 
 
 class HBNBCommand(cmd.Cmd):
