426891c6a5b84f3ca0bb76f68be981b6df89b64f
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 <l.leroty@gmail.com>
2022-11-26 13:02:31 +0100

    fix the state and city

--- models/city.py
+++ models/city.py
@@ -5,13 +5,9 @@ from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import declarative_base, relationship
 
 
-Base = declarative_base()
-
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
     places = relationship("Place", backref="cities")
-
-
--- models/state.py
+++ models/state.py
@@ -11,18 +11,3 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-
-
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-
-        @property
-        def cities(self):
-            citylist = []
-            city_dict = models.storage.all(models.city.City)
-            for key, value in city_dict.items():
-                if value.state_id == self.id:
-                    citylist.append(value)
-            return citylist
-    else:
-        cities = relationship("City", backref="state",
-                              cascade="all, delete-orphan")
