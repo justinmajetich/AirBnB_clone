b80828ba1b9c8ec846c18389783c72482462719c
Mateo Bonino <mateo@localhost.localdomain>
2022-11-28 17:06:17 -0300

    Task fixes

--- models/state.py
+++ models/state.py
@@ -13,14 +13,11 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship('City', backref='state', cascade='delete')
 
-    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
-        @property
-        def cities(self):
-            from models import storage
-            obj_list = []
-            for i, j in storage.all(city.City).items():
-                if j.state_id == self.id:
-                    obj_list.append(j)
-            return obj_list
-        
-            
+    @property
+    def cities(self):
+        from models import storage
+        obj_list = []
+        for i, j in storage.all(city.City).items():
+            if j.state_id == self.id:
+                obj_list.append(j)
+        return obj_list
