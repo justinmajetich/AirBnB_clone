2ce9a028394f276e3af3452287906dcf47359140
Mateo Bonino <mateo@localhost.localdomain>
2022-11-28 02:25:25 -0300

    Task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Task 1 for Flask Project Web Framework
+"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """
+    Returns Hello HBNB for the '/' url
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    """
+    Returns HBNB for the /hbnb route
+    """
+    return "HBNB"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_magic(text):
+    """display “Python ”, followed by the value of the text"""
+    if text is None:
+        return "Python is cool"
+    return f"Python {text.replace('_', ' ')}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
