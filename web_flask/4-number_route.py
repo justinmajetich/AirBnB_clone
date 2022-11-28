203c08c5546fe19a1edbd2331edd8c83453da9f8
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 <l.leroty@gmail.com>
2022-11-28 13:20:40 +0100

    add the task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -29,7 +29,7 @@ def python(text="is cool"):
     return "Python {}".format(text)
 
 
-@app.route("/number/<n>", strict_slashes=False)
+@app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
     return "{} is a number".format(n)
 
