b521615a3b78e912929af5ebe9ac79d2a4bc0c40
Mateo Bonino <mateo@localhost.localdomain>
2022-11-28 17:37:33 -0300

    task 7

--- models/city.py
+++ models/city.py
@@ -8,14 +8,7 @@ from os import getenv
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-
-    if (getenv('HBNB_TYPE_STORAGE') == 'db'):
-        __tablename__ = 'cities'
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-        places = relationship("Place", backref="cities", cascade="all, delete")
-
-    if (getenv('HBNB_TYPE_STORAGE') != 'db'):
-        state_id = ''
-        name = ''
-        places = []
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", backref="cities", cascade="all, delete")
