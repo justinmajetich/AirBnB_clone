f910aa332412e575c3f5803c7acf43477c1e7711
tizihoxha <tizihoxha@gmail.com>
2022-11-28 04:28:14 -0800

    added

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """returns hello hbnb"""
+    return("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return("HBNB")
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """replaces _ with space"""
+    text = text.replace("_", " ")
+    return(F"{C} {text}")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
