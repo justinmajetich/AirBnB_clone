53231b0d3a42ce562a4baeed816c5bb99cd33ca6
tizihoxha <tizihoxha@gmail.com>
2022-11-28 04:41:02 -0800

    Added

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -22,7 +22,7 @@ def hbnb():
 def c_cool(text):
     """replaces underscores with space"""
     text = text.replace("_", " ")
-    return(F"{C} {text}")
+    return(F"C {text}")
 
 
 if __name__ == "__main__":
