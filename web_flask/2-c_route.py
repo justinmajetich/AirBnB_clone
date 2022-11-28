98c7048ba336df4e2f45d487133cc970273373db
Mateo Bonino <mateo@localhost.localdomain>
2022-11-28 02:12:40 -0300

    Task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -27,8 +27,7 @@ def hbnb_route():
 @app.route('/c/<text>', strict_slashes=False)
 def c_isfunc(text):
     """
-    Returns 'C' + the text with underscores
-    replaced for spaces
+    Returns 'C' + the text for /c/text route
     """
     return f"C {text.replace('_', ' ')}"
 
