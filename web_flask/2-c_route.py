8efc45cf507000129d008207368e64b885e710aa
Romain Bayle <5014@holbertonstudents.com>
2022-11-28 16:55:52 +0100

    2-c_route.py fixed underscores replaced by spaces

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -28,7 +28,7 @@ def hbnb():
 @app.route("/c/<text>", strict_slashes=False)
 def c_text(text):
     """display c + a text in the url"""
-    return "C {}".format(text)
+    return "C {}".format(text).replace("_", " ")
 
 
 if __name__ == "__main__":
