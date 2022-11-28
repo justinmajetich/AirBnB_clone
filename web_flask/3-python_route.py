753bc4d82e5aa5571e68eb198cc4e8a86fe8802c
tizihoxha <tizihoxha@gmail.com>
2022-11-28 05:06:44 -0800

    Added

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """returns hello hbnb"""
+    return("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return("HBNB")
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Retturns C and the text that follows"""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """Returns Python an the text that follows"""
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
