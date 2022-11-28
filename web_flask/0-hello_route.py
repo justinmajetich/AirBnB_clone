d9e864bbc8b1c446f6b234fca3a8dc92c5392d29
tizihoxha <tizihoxha@gmail.com>
2022-11-28 04:05:31 -0800

    added

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,9 +6,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.route("/", strict_slashes=False)
 
-
+@app.route("/", strict_slashes=False)
 def hello():
     """displays Hello text"""
     return("Hello HBNB!")
