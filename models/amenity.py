7b3018dd02b83334608d3e478e5347906c59cc91
Mateo Bonino <mateo@localhost.localdomain>
2022-11-28 17:32:48 -0300

    task 7

--- models/amenity.py
+++ models/amenity.py
@@ -4,10 +4,9 @@ from models.base_model import BaseModel, Base
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
+    place_amenities = relationship("Place", secondary='place_amenity', viewOnly=False)
--- models/place.py
+++ models/place.py
@@ -11,11 +11,10 @@ from models.amenity import Amenity
 
 place_amenity = Table("place_amenity", Base.metadata, 
                       Column("place_id", String(60),
-                             ForeignKey("places.id"), primary_key=True,
+                             ForeignKey("places.id"),
                              nullable=False),
                       Column("amenity_id", String(60),
-                             ForeignKey("amenities.id"),
-                             primary_key=True, nullable=False))
+                             ForeignKey("amenities.id"), nullable=False))
 
 class Place(BaseModel, Base):
     """ A place to stay """
