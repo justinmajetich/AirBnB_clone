175cea7be43fb9e342789575b104e6256cb5dca3
VCruz910 <vscruz910@gmail.com>
2022-11-28 17:29:41 -0400

    Created scrip that starts flask app

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask application.
+"""
+from flask import Flask
+
+
+APP = Flask(__name__)
+
+
+@APP.route("/", strict_slashes=False)
+def hello_holberton():
+    """
+    Returns 'Hello HBNB!'.
+    """
+    return "Hello HBNB!"
+
+@APP.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    Returns 'HBNB'.
+    """
+    return "HBNB"
+
+@APP.route("/c/text", strict_slashes=False)
+def cisfun(text):
+    text = text.replace("_", " ")
+    return "C " + text
+    
+if __name__ == "__main__":
+    APP.run(host="0.0.0.0", port="5000")
