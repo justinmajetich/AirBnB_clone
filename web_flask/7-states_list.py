24427ee00412c3316e108b9e5bd99d60c7215cf4
Mateo Bonino <mateo@localhost.localdomain>
2022-11-28 19:18:04 -0300

    Task 7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Displays the states lists
+"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Lists all the states with the storage.all() method"""
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown():
+    """Closes the database when the session ends"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
