59cbe706ca9b1176eaeb22893dc487ffbc8e8127
Romain Bayle <5014@holbertonstudents.com>
2022-11-28 17:54:00 +0100

    6-number_odd_or_even.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -52,7 +52,7 @@ def is_num(n):
 
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
-def template(n):
+def num_template(n):
     """display a html page only if n is an integer"""
     return render_template("5-number.html", n=n)
 
--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -5,14 +5,19 @@ Routes:
 /: display “Hello HBNB!”
 /hbnb: display “HBNB”
 /c/<text>: display “C ”, followed by the value of the text variable
-(replace underscore _ symbols with a space )
+    (replace underscore _ symbols with a space )
 /python/<text>: display “Python ”, followed by the value of the text variable
-(replace underscore _ symbols with a space )
+    (replace underscore _ symbols with a space )
 The default value of text is “is cool”
 /number/<n>: display “n is a number” only if n is an integer
+/number_template/<n>: display a HTML page only if n is an integer:
+H1 tag: “Number: n” inside the tag BODY
+/number_odd_or_even/<n>: display a HTML page only if n is an integer:
+H1 tag: “Number: n is even|odd” inside the tag BODY
 You must use the option strict_slashes=False in your route definition
 """
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -46,6 +51,19 @@ def is_cool(text="is_cool"):
 def is_num(n):
     """display 'n is a number' if n is a number"""
     return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def num_template(n):
+    """display a html page only if n is an integer"""
+    return render_template("5-number.html", n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def odd_or_even(n):
+    """display a html page only if n is an integer
+    with n even or odd"""
+    return render_template("6-number_odd_or_even.html", n=n)
 
 
 if __name__ == "__main__":
