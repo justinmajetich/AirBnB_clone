3a73cb4655d4a37db760f873124887c24aa3b024
Mateo Bonino <mateo@localhost.localdomain>
2022-11-28 01:55:15 -0300

    Task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Task 0 for Flask Project
+"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """
+    Returns Hello HBNB for the '/' url
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_route():
+    """
+    Returns HBNB for the '/hbnb' url
+    """
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
