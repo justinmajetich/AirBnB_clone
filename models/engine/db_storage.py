cacecccf4bcbed6465c5fffc80cc534e43074df3
Mateo Bonino <mateo@localhost.localdomain>
2022-11-28 16:30:47 -0300

    Task 7 fixes

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -57,7 +57,7 @@ class DBStorage():
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
         Session = scoped_session(session_factory)
-        self.__session = Session()
+        self.__session = scoped_session(Session)
 
     def close(self):
         self.__session.close()
