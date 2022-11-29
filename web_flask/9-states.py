8b0f062521fad1ff867c92fb5f648e7de7b6a5b3
tizihoxha <tizihoxha@gmail.com>
2022-11-29 02:13:48 -0800

    added

---
+++ web_flask/9-states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+from models import storage
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """closes the current SQLAlchemy session"""
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def list_states():
+    """displays the list1 of states"""
+    states = storage.all("State")
+    return render_template("9-states.html", state=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """Displays an HTML page with info about <id>, if it exists."""
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
