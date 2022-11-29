c0dbb9fbf64d56d483c939789d8db685984bd629
tizihoxha <tizihoxha@gmail.com>
2022-11-29 01:56:00 -0800

    added

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python3
-"""
-Starts a Flask web application
-"""
-
-
+"""Starts a Flask web application"""
 from models import storage
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown(self):
-    """ removes current SQLAlchemy Session """
+def teardown(exception):
+    """closes the current SQLAlchemy session"""
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """ displays 7-states_list.html """
-    states = storage.all("State").values()
-    return render_template('7-states_list.html', states=states)
+@app.route("/states_list", strict_slashes=False)
+def list_states():
+    """displays the list1 of states"""
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port="5000")
