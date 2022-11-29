fb4715bb95009be9ef0fb4182b8d8d91f23d2f0c
tizihoxha <tizihoxha@gmail.com>
2022-11-29 01:54:16 -0800

    added

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /cities_by_states: HTML page with a list of all states and related cities.
 """
-from models import storage
-from flask import Flask
-from flask import render_template
+Starts a Flask web application
+"""
 
+
+from models import storage
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
-    """Displays an HTML page with a list of all states and related cities.
-    States/cities are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
-
-
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown(self):
+    """ removes current SQLAlchemy Session """
     storage.close()
 
 
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ displays 7-states_list.html """
+    states = storage.all("State").values()
+    return render_template('7-states_list.html', states=states)
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host='0.0.0.0', port=5000)
