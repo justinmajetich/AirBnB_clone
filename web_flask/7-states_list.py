41633e617d8ebe9bdb411517d673e14073f45262
tizihoxha <tizihoxha@gmail.com>
2022-11-29 01:37:07 -0800

    added

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+from models import storage
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """closes the current SQLAlchemy session"""
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def list_states():
+    """displays the list of states"""
+    states = storage.all("State")
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
