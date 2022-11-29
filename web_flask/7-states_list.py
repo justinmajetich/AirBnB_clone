ec6d71f333aa5868812b22c23fa1c063f6f8cf9b
tizihoxha <tizihoxha@gmail.com>
2022-11-29 01:48:26 -0800

    added

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+"""#!/usr/bin/python3
 """Starts a Flask web application"""
 from models import storage
 from flask import Flask
@@ -15,10 +15,40 @@ def teardown(exception):
 
 @app.route("/states_list", strict_slashes=False)
 def list_states():
-    """displays the list of states"""
+    """displays the list1 of states"""
     states = storage.all("State")
     return render_template('7-states_list.html', states=states)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port="5000")"""
+#!/usr/bin/python3
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /cities_by_states: HTML page with a list of all states and related cities.
+"""
+from models import storage
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """Displays an HTML page with a list of all states and related cities.
+    States/cities are sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
