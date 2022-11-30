0ee04bd48fd9e6873c4807d8e2d4b06bbe59d965
VCruz910 <vscruz910@gmail.com>
2022-11-30 12:26:37 -0400

    Created scrip that starts flask app

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -28,7 +28,7 @@ def cisfun(text):
     Returns C, followed by text value.
     """
     text = text.replace("_", " ")
-    return "C " + text
+    return "C {}".format(text)
 
 if __name__ == "__main__":
     APP.run(host="0.0.0.0", port="5000")
