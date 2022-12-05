bbf4c55cf1f0116ea86f9203a75f7baa3813740f
orahpsalm <psalmorahcle@gmail.com>
2022-12-05 12:41:05 +0100

    Pycodestyle error in place.py

--- models/place.py
+++ models/place.py
@@ -33,7 +33,7 @@ class Place(BaseModel, Base):
     latitude = Column(Float)
     longitude = Column(Float)
     amenity_ids = []
-    
+
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         reviews = relationship("Review", cascade="all, delete",
                                backref="place", passive_deletes=True)
