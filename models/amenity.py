2ae02e1e5fc7daafc277174d27d3e3df9088e4fa
Ehoneah Obed <ehoneahobed@hotmail.com>
2022-12-06 17:12:20 +0000

    Add solution to remaining tasks

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel):
-    name = ""
+    """_summary_
+
+    Args:
+        BaseModel (_type_): _description_
+    """
+    __tablename__ = "amenities"
+
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship(
+        'Place', secondary='place_amenity', back_populate='amenities')
