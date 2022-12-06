700124ab52ffea44577d80177e0bad8be4f267dc
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-12-06 16:38:49 +0100

    cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -23,6 +23,6 @@ def display_html():
                            Table="States",
                            states=states)
 
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
-    
