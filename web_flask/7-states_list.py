c02782dd1ddb20156264c20a6f21448683daefb1
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-12-06 16:15:42 +0100

    done

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -23,6 +23,6 @@ def display_html():
                            Table="States",
                            items=dict_to_html)
 
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
-    
