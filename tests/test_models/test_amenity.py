77b6c660e566995c61962d6ccb43f17114f2eafd
Solomon Enanaw Kebede <101396246+solomonenanaw@users.noreply.github.com>
2022-12-07 21:09:44 +0300

    Update test_amenity.py
    all

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 """ """
-from tests.test_models.test_base_model import test_basemodel
+import os
+
+from tests.test_models.test_base_model import TestBasemodel
 from models.amenity import Amenity
 
 
-class test_Amenity(test_basemodel):
-    """ """
-
+class TestAmenity(TestBasemodel):
+    """Represents the tests for the Amenity model."""
     def __init__(self, *args, **kwargs):
-        """ """
+        """Initializes the test class."""
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
     def test_name2(self):
-        """ """
+        """Tests the type of name."""
         new = self.value()
-        self.assertEqual(type(new.name), str)
+        self.assertEqual(
+            type(new.name),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
