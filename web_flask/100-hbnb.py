f28bbf75d3d07ea6404c2807ce682690d8e7ff19
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-12-07 08:04:15 +0100

    done

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -28,6 +28,6 @@ def display_html():
                            amenities=amenities.values(),
                            places=places.values())
 
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
-    
