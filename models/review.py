08c6425af04a4c4f4ef2512a6a882561b19e50fb
reinhard <reinhard2057@gmail.com>
2022-12-08 00:00:43 +0000

    task 9

--- models/review.py
+++ models/review.py
@@ -1,10 +1,25 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
+st = getenv("HBNB_TYPE_STORAGE")
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+
+    __tablename__ = "reviews"
+    if st == "db":
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey("places.id"),nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"),nullable=False)
+
+        user = relationship("User", back_populates="reviews")
+        place = relationship("Place", back_populates="reviews")
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
