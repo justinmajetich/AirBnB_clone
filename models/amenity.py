c0f589f612f317e4fd650dcc861b1cbb80904c89
reinhard <reinhard2057@gmail.com>
2022-12-09 10:49:42 +0000

    task 10 BETA

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.place import place_amenity
+from models.base_model import Base
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from sqlalchemy.orm import relationship
+from os import getenv
 
+st = getenv("HBNB_TYPE_STORAGE")
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+
+    __tablename__ = "amenities"
+    if st == "db":
+        name = Column(String(128),
+                nullable=False)
+
+        place_amenities = relationship(
+                "Place",
+                secondary=place_amenity,
+                viewonly=False,
+                back_populates="amenities")
+    else:
+        name = ""
--- models/base_model.py
+++ models/base_model.py
@@ -23,11 +23,18 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+            lst = list(kwargs)
+            if 'updated_at' in lst:
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+            if 'created_at' in lst:
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            if '__class__' in lst:
+                del kwargs['__class__']
+            if "id" not in lst:
+                self.id = str(uuid.uuid4())
+
             self.__dict__.update(kwargs)
 
     def __str__(self):
