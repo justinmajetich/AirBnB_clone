0f73c9d84f075986907cc9cef58a8b1df3695791
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-12-10 03:02:29 -0500

    Add conditional to create Columns only for DBStorage in base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -3,8 +3,9 @@
 import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, String, DateTime
 import models
+from os import getenv
 
 
 Base = declarative_base()
@@ -12,9 +13,10 @@ Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
-    id = Column(String(60), primary_key=True, nullable=False, unique=True)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        id = Column(String(60), primary_key=True, nullable=False, unique=True)
+        created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+        updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
