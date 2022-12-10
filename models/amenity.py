30d2f816952b66b3de2db00f478caca60605308f
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-12-10 03:00:33 -0500

    Comment relationship place_amenities in class Amenities for DBStorage

--- models/amenity.py
+++ models/amenity.py
@@ -3,15 +3,15 @@
 from models.base_model import BaseModel, Base
 from os import getenv
 from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
     class Amenity(BaseModel, Base):
         """ This is the class for Amenity """
         __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
-        place_amenities = relationship('Place', secondary='place_amenity',
-                                       back_populates='amenities')
+        # place_amenities = relationship('Place', secondary='place_amenity',
+        #                                back_populates='amenities',
+        #                                viewonly=False)
 
 else:
     class Amenity(BaseModel):
