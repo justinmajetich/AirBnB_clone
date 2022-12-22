3eb8f25c8f8e5ffff11829bf9258f8775439f382
Olamide Williams <circleofwilliams@gmail.com>
2022-12-22 00:23:03 -0800

    fix 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ Function that compress a folder """
 from datetime import datetime
 from fabric.api import local
@@ -6,13 +6,17 @@ import os
 
 
 def do_pack():
-    try:
-        if not os.path.exists("versions"):
-            local('mkdir versions')
-        t = datetime.now()
-        f = "%Y%m%d%H%M%S"
-        archive_path = 'versions/web_static_{}.tgz'.format(t.strftime(f))
-        local('tar -cvzf {} web_static'.format(archive_path))
-        return archive_path
-    except:
-        return None
+	"""Create a tar gzipped archive of the directory web_static."""
+	dt = datetime.utcnow()
+	file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+							    dt.month,
+							    dt.day,
+							    dt.hour,
+							    dt.minute, 
+							    dt.second)
+	if os.path.isdir("versions") is False:
+		if local("mkdir -p versions").failed is True:
+			return None
+	if local("tar -cvzf {} web_static".format(file)).failed is True:
+		return None
+	return file
