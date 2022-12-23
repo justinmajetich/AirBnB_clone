f8ff676cb3c3c03991b7b668c82522beb750e904
solomonenanaw <sekewbeg@gmail.com>
2022-12-23 02:54:01 +0000

    fix

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -7,10 +7,10 @@ env.hosts = ['54.144.222.58', '54.144.222.58']
 
 def do_clean(number=0):
     """Delete out-of-date archives.
-   
+
     Args:
         number (int): The number of archives to keep.
-        
+
     If number is 0 or 1, keeps only the most recent archive. If
     number is 2, keeps the most and second-most recent archives,
     etc.
