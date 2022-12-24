0948d7f8aa5f497fb193d14b2e731a78697633a6
Ifiok Ekott <ekottifiok@gmail.com>
2022-12-24 19:44:12 +0100

    SCAM

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,33 +1,40 @@
 #!/usr/bin/python3
-"""Write a Fabric script that generates a .tgz archive from the
-contents of the web_static folder of your
-AirBnB Clone repo, using the function do_pack."""
-from fabric.api import env, put, run
-from os import path
+""" Function that compress a folder """
+from datetime import datetime
+from fabric.api import *
+import shlex
+import os
+
 
 env.hosts = ['35.175.132.172', '52.91.116.127']
-env.user = 'ubuntu'
+env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
-    """that distributes an archive to your web servers"""
+    """ Deploys """
+    if not os.path.exists(archive_path):
+        return False
     try:
-        if not path.exists(archive_path):
-            raise Exception
-        file_name_tgz = archive_path.split('/')[-1]
-        file_name = file_name_tgz.split('.')[0]
-        release_path = "/data/web_static/releases/{}/".format(file_name)
-        tmp_path = "/tmp/{}".format(file_name_tgz)
-        static_current = "/data/web_static/current"
+        name = archive_path.replace('/', ' ')
+        name = shlex.split(name)
+        name = name[-1]
+
+        wname = name.replace('.', ' ')
+        wname = shlex.split(wname)
+        wname = wname[0]
+
+        releases_path = "/data/web_static/releases/{}/".format(wname)
+        tmp_path = "/tmp/{}".format(name)
+
         put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(release_path))
-        run("tar -xzf {} -C {}".format(tmp_path, release_path))
+        run("mkdir -p {}".format(releases_path))
+        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
         run("rm {}".format(tmp_path))
-        run("mv {}web_static/* {}".format(release_path))
-        run("rm -rf {}web_static".format(release_path))
-        run("rm -rf {}".format(static_current))
-        run("ln -s {} {}".format(release_path, static_current))
+        run("mv {}web_static/* {}".format(releases_path, releases_path))
+        run("rm -rf {}web_static".format(releases_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(releases_path))
         print("New version deployed!")
         return True
-    except Exception:
+    except:
         return False
