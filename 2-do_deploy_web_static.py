362273afb37b231f2cfd52cb5ec42d263c3202d0
Ifiok Ekott <ekottifiok@gmail.com>
2022-12-24 20:38:16 +0100

    trial 1

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,9 +2,7 @@
 """ Function that compress a folder """
 from datetime import datetime
 from fabric.api import *
-import shlex
-import os
-
+from os import path
 
 env.hosts = ['35.175.132.172', '52.91.116.127']
 env.user = "ubuntu"
@@ -12,19 +10,14 @@ env.user = "ubuntu"
 
 def do_deploy(archive_path):
     """ Deploys """
-    if not os.path.exists(archive_path):
-        return False
+
     try:
-        name = archive_path.replace('/', ' ')
-        name = shlex.split(name)
-        name = name[-1]
-
-        wname = name.replace('.', ' ')
-        wname = shlex.split(wname)
-        wname = wname[0]
-
-        releases_path = "/data/web_static/releases/{}/".format(wname)
-        tmp_path = "/tmp/{}".format(name)
+        if not path.exists(archive_path):
+            raise Exception
+        file_name_tgz = archive_path.split('/')[-1]
+        file_name = file_name_tgz.split('.')[0]
+        releases_path = "/data/web_static/releases/{}/".format(file_name)
+        tmp_path = "/tmp/{}".format(file_name_tgz)
 
         put(archive_path, "/tmp/")
         run("mkdir -p {}".format(releases_path))
@@ -36,5 +29,5 @@ def do_deploy(archive_path):
         run("ln -s {} /data/web_static/current".format(releases_path))
         print("New version deployed!")
         return True
-    except:
+    except Exception:
         return False
