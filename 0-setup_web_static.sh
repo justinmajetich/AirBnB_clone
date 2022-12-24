587c18d8e72e61fca6fa29330d8ea458701aa52e
Ifiok Ekott <ekottifiok@gmail.com>
2022-12-24 19:03:04 +0100

    update to 0-setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # sets up your web servers for the deployment of web_static
 
-sudo apt-get update && sudo apt-get install -y nginx
+apt-get update && apt-get install -y nginx
 mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 echo "Hello Friends" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R "$USER":"$USER" /data
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx restart
+chown -R ubuntu:ubuntu /data
+sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
+service nginx restart
 exit 0
---
+++ 101-setup_web_static.pp
@@ -0,0 +1,2 @@
+exec {'apt-get update':
+}
