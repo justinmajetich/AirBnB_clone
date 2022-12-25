a6f410b3933c9531d242b1ad4ce9c4645f8f54b6
Ifiok Ekott <ekottifiok@gmail.com>
2022-12-26 00:39:37 +0100

    scam 101

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,33 +1,57 @@
-# Redo the task #0 but by using Puppet:
+# Script that configures Nginx server with some folders and files
 
-$data_dirs = ['/data', '/data/web_static', '/data/web_static/releases',
-	'/data/web_static/releases/test', '/data/web_static/shared/']
-
-$user = 'ubuntu'
-
-exec {'get update and install nginx and start nginx':
+exec {'update':
   provider => shell,
-  command  => 'sudo apt-get update && sudo apt-get -y install nginx && sudo service nginx star',
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
 }
 
-file {$data_dirs:
-	ensure	=> directory,
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['start Nginx'],
 }
 
-file {"/data/web_static/releases/test/index.html":
-	ensure 	=> file,
-	content => "Hello Friends",
+exec {'start Nginx':
+  provider => shell,
+  command  => 'sudo service nginx start',
+  before   => Exec['create first directory'],
 }
 
-file { '/data/web_static/current':
-	ensure 	=> link,
-	target 	=> '/data/web_static/releases/test/',
-	force 	=> true
+exec {'create first directory':
+  provider => shell,
+  command  => 'sudo mkdir -p /data/web_static/releases/test/',
+  before   => Exec['create second directory'],
+}
+
+exec {'create second directory':
+  provider => shell,
+  command  => 'sudo mkdir -p /data/web_static/shared/',
+  before   => Exec['content into html'],
+}
+
+exec {'content into html':
+  provider => shell,
+  command  => 'echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html',
+  before   => Exec['symbolic link'],
+}
+
+exec {'symbolic link':
+  provider => shell,
+  command  => 'sudo ln -sf /data/web_static/releases/test/ /data/web_static/current',
+  before   => Exec['put location'],
 }
 
 exec {'put location':
   provider => shell,
-  command  => 'sudo sed -i \'38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n\' /etc/nginx/sites-available/default'
+  command  => 'sudo sed -i \'38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n\' /etc/nginx/sites-available/default',
+  before   => Exec['restart Nginx'],
+}
+
+exec {'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+  before   => File['/data/']
 }
 
 file {'/data/':
@@ -35,9 +59,4 @@ file {'/data/':
   owner   => 'ubuntu',
   group   => 'ubuntu',
   recurse => true,
-}
-
-exec {'restart Nginx':
-  provider => shell,
-  command  => 'sudo service nginx restart',
 }
