08d8685f690ec75bb24253a50f9994d44535bd73
Ifiok Ekott <ekottifiok@gmail.com>
2022-12-25 23:53:43 +0100

    done with 101

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,2 +1,36 @@
-exec {'apt-get update':
+$data_dirs = ['/data', '/data/web_static', '/data/web_static/releases',
+	'/data/web_static/releases/test', '/data/web_static/shared/']
+
+$user = 'ubuntu'
+
+exec {'get update and install nginx and start nginx':
+  provider => shell,
+  command  => 'sudo apt-get update && sudo apt-get -y install nginx && sudo service nginx star',
+}
+
+file {$data_dirs:
+	ensure	=> directory,
+	group 	=> $user,
+	owner 	=> $user
+}
+
+file {"/data/web_static/releases/test/index.html":
+	ensure 	=> file,
+	content => "Hello Friends",
+}
+
+file { '/data/web_static/current':
+	ensure 	=> link,
+	target 	=> '/data/web_static/releases/test/',
+	force 	=> true
+}
+
+exec {'put location':
+  provider => shell,
+  command  => 'sudo sed -i \'38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n\' /etc/nginx/sites-available/default'
+}
+
+exec {'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
