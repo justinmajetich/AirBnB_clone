225f16f056ed558794932de82bb4f0ec153f1a6b
Arinze Nwutobo <tim.arinze@outlook.com>
2022-12-26 02:34:42 +0000

    task

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,12 +3,12 @@
 
 from fabric.api import local
 from datetime import datetime
-
 from fabric.decorators import runs_once
+
 
 @runs_once
 def do_pack():
-        '''generates .tgz archive from the contents of the web_static folder'''
+        """generates .tgz archive from the contents of the web_static folder"""
         local("mkdir -p versions")
         path = ("versions/web_static_{}.tgz"
 
