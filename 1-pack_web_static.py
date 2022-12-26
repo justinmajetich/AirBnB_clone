4b13086c78227862c422d096ded2e0609384d224
Arinze Nwutobo <tim.arinze@outlook.com>
2022-12-26 16:25:16 +0100

    task

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,11 +9,11 @@ import tarfile
 
 
 def do_pack():
-	"""generates an archive"""
-	now = datetime.now().strftime("%Y%m%d%H%M%S")
-	path = local('mkdir -p versions')
-	tar = local('tar -czvf versions/web_static_{}.tgz web_static'.format(now))
-	if os.path.exists("/versions/web_static_{}.tgz".format(now)):
-		return os.path.normpath("/versions/web_static_{}.tgz".format(now))
-	else:
-		return "None"
+    """generates an archive"""
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    path = local('mkdir -p versions')
+    tar = local('tar -czvf versions/web_static_{}.tgz web_static'.format(now))
+    if os.path.exists("/versions/web_static_{}.tgz".format(now)):
+        return os.path.normpath("/versions/web_static_{}.tgz".format(now))
+    else:
+        return "None"
