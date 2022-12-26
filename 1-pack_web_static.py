006493fa594c2297c171ac50381aa8af6c325416
Arinze Nwutobo <tim.arinze@outlook.com>
2022-12-26 13:38:35 +0100

    task

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,9 +3,17 @@
 compress before sending
 """
 from fabric.api import local
+from datetime import datetime
+import os
 
 
 def do_pack():
 	"""generates an archive"""
-	myVar = var()
-	local(tar -czvf my)
+	myVar = vars()
+    now = datetime.now().strftime("%Y%M%D%H%M%S")
+    local(mkdir -p /versions/)
+	local(tar -czvf "web_static_{}.tgz".format(now) /versions)
+    if os.path.exists("/versions/web_static_{}.tgz".format(now)):
+        return os.path.normpath("/versions/web_static_{}.tgz".format(now))
+    else:
+        return None
