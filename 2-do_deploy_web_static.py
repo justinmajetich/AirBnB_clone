dd3d02937ec8cef68b603d103cc458e18cd32bed
Arinze Nwutobo <tim.arinze@outlook.com>
2022-12-29 20:46:42 +0100

    task

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -16,13 +16,14 @@ def do_deploy(archive_path):
             print(res)
         archive = archive_path.split('/')[-1]
         free = archive.split('.')[0]
+        tomove = "/data/web_static/releases/{}/web_static/*".format(free)
         sudo('mkdir -p /data/web_static/releases/{}/'.format(free))
         sudo('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(archive, free))
         sudo('rm /tmp/{}'.format(archive))
-        sudo('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}'.format(free, free))
+        sudo('mv tomove /data/web_static/releases/{}'.format(free))
         sudo('rm -rf /data/web_static/releases/{}/web_static'.format(free))
         sudo('rm -rf /data/web_static/current')
         sudo('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(free))
         return True
-    except BaseException:
+    except:
     return False
