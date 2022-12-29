e523797468dccc5ef8bf11a913a56b7b18bda6b3
Arinze Nwutobo <tim.arinze@outlook.com>
2022-12-29 14:45:46 +0000

    task

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,29 +2,27 @@
 """
 Deploy archive
 """
-import os
-from fabric.operations import run, put, sudo
-from fabric.api import env
-env.user = 'ubuntu'
+from fabric.api import *
 env.hosts = ['18.215.160.48','34.201.174.39']
-env.key_filename = '~/id_rsa'
 
 
 def do_deploy(archive_path):
 	"""A fabric script that distributes archive to your web servers"""
-	if os.path.exists(archive_path) is False:
+	if not archive_path:
 		return False
 	try:
-		put('archive_path', '/tmp')
+		with cd('/tmp'):
+			res = put(archive_path, "/tmp")
+			print(res)
 		archive = archive_path.split('/')[-1]
-		free = archive.strip('.tgz')
-		run('mkdir -p /data/web_static/releases/{}/'.format(free))
-		run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(archive_path, free))
-		run('rm /tmp/{}'.format(archive_path))
-		run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}'.format(free, free))
-		run('rm -rf /data/web_static/releases/{}/web_static'.format(free))
-		run('rm -rf /data/web_static/current')
-		run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(free))
+		free = archive.split('.')[0]
+		sudo('mkdir -p /data/web_static/releases/{}/'.format(free))
+		sudo('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(archive, free))
+		sudo('rm /tmp/{}'.format(archive))
+		sudo('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}'.format(free, free))
+		sudo('rm -rf /data/web_static/releases/{}/web_static'.format(free))
+		sudo('rm -rf /data/web_static/current')
+		sudo('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(free))
 		return True
 	except:
 		return False
