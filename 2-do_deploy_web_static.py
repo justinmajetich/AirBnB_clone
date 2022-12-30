ea22b4d8725bf9ad08f9d7a2188d35546753ac63
Arinze Nwutobo <tim.arinze@outlook.com>
2022-12-30 02:15:44 +0100

    task

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,12 +17,15 @@ def do_deploy(archive_path):
         archive = archive_path.split('/')[-1]
         free = archive.split('.')[0]
         sudo('mkdir -p /data/web_static/releases/{}/'.format(free))
-        sudo('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(archive, free))
+        sudo('tar -xzf /tmp/{} -C /data/web_static\
+            /releases/{}/'.format(archive, free))
         sudo('rm /tmp/{}'.format(archive))
-        sudo('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}'.format(free, free))
+        sudo('mv /data/web_static/releases/{}\
+            /web_static/* /data/web_static/releases/{}'.format(free, free))
         sudo('rm -rf /data/web_static/releases/{}/web_static'.format(free))
         sudo('rm -rf /data/web_static/current')
-        sudo('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(free))
+        sudo('ln -s /data/web_static/releases\
+            /{}/ /data/web_static/current'.format(free))
         return True
     except:
         return False
