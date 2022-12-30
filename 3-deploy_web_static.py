8874e108c0d5aa1e3de25d2a33cb33caa3afd512
Arinze Nwutobo <tim.arinze@outlook.com>
2022-12-30 02:51:07 +0000

    task

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,9 +1,57 @@
 #!/usr/bin/python3
-# Full deployment
-from 1-pack_web_static import do_pack
-from 2-do_deploy_web_static import do_deploy
+"""
+Just packing
+"""
+from datetime import datetime
+import os
+from fabric.api import *
+import tarfile
 
-env.hosts = [18.215.160.48, 34.201.174.39]
+env.hosts = ['18.215.160.48', '34.201.174.39']
+
+
+def do_pack():
+	"""compress before sending"""
+	now = datetime.now().strftime("%Y%m%d%H%M%S")
+	path = local('mkdir -p versions/web_static_{}'.format(now))
+	tar = local('tar -czvf versions/web_static_{}.tgz web_static'.format(now))
+	if os.path.exists("versions/web_static_{}.tgz web_static".format(now)):
+		return os.path.normpath("versions/web_static_{}.tgz web_static".format(now))
+	else:
+		return None
+
+"""
+Just Deploying
+"""
+
+
+def do_deploy(archive_path):
+	"""Deploy archive after compressing"""
+	if not archive_path:
+		return False
+	try:
+		with cd('/tmp'):
+			res = put(archive_path, "/tmp")
+			print(res)
+		archive = archive_path.split("/")[-1]
+		free = archive.split(".")[0]
+		sudo('mkdir -p /data/web_static/releases/{}/'.format(free))
+		sudo('tar -xzf /tmp/{} -C /data/web_static\
+/releases/{}/'.format(archive, free))
+		sudo('rm /tmp/{}'.format(archive))
+		sudo('mv /data/web_static/releases/{}\
+/web_static/* /data/web_static/releases/{}'.format(free, free))
+		sudo('rm -rf /data/web_static/releases/{}/web_static'.format(free))
+		sudo('rm -rf /data/web_static/current')
+		sudo('ln -s /data/web_static/releases\
+/{}/ /data/web_static/current'.format(free))
+		return True
+	except:
+		return False
+
+"""
+Full deployment
+"""
 
 
 def deploy():
