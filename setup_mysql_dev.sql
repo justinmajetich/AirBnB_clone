e1a2d4ee01159d48f1c49bf5e70c9fef52ac3b8e
scaarifintl <scaarifintl@gmail.com>
2023-01-03 20:05:41 +0300

    setup databases and users for the project

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,10 @@
+-- Prepares a MySQL server for the project AirBNB Clone by:
+-- Creating a Database, hbnb_dev_db
+-- Creating a user, hbnb_dev (in localhost) with the password: hbnb_dev_pwd
+-- the user should have all privileges on the database hbnb_dev_db (and only this database)
+-- they should also have SELECT privileges on the database performance_schema (and only this database)
+-- the script should NOT fail if the database and/ user already exists
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db; -- create the db
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd'; -- craete user
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost'; -- grant all privileges
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost'; -- grant SELECTprivileges
---
+++ setup_mysql_test.sql
@@ -0,0 +1,10 @@
+-- Prepares a MySQL server for the project AirBNB Clone by:
+-- Creating a Database, hbnb_test_db
+-- Creating a user, hbnb_test (in localhost) with the password: hbnb_test_pwd
+-- the user should have all privileges on the database hbnb_test_db (and only this database)
+-- they should also have SELECT privileges on the database performance_schema (and only this database)
+-- the script should NOT fail if the database and/ user already exists
+CREATE DATABASE IF NOT EXISTS hbnb_test_db; -- create the db
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd'; -- create user
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_dev'@'localhost'; -- grant all privileges
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost'; -- grant SELECTprivileges
