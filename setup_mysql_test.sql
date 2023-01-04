1cebed281fecd8cd025c2a878f07a28ec1b8cb1b
scaarifintl <scaarifintl@gmail.com>
2023-01-04 07:55:37 +0300

    fix sql script

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -6,5 +6,5 @@
 -- the script should NOT fail if the database and/ user already exists
 CREATE DATABASE IF NOT EXISTS hbnb_test_db; -- create the db
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd'; -- create user
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_dev'@'localhost'; -- grant all privileges
+GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost'; -- grant all privileges
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost'; -- grant SELECTprivileges
