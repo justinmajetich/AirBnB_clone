17c6d3c04949e7122c20cfd12c5630a42a0a6755
Cyberguru1 <hamzacypher34@gmail.com>
2023-01-05 07:27:38 -0600

    update

--- models/state.py
+++ models/state.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    id = Column(Integer, unique=True, primary_key=True, nullable=Flase)
+    name = Column(String(128), nullable=False,)
