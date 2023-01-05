ba9dc22b37c4c124382444fc826e65d0490e2282
Cyberguru1 <hamzacypher34@gmail.com>
2023-01-05 04:58:47 -0600

    update

--- console.py
+++ console.py
@@ -124,13 +124,14 @@ class HBNBCommand(cmd.Cmd):
                 arg_splited = arg.split("=")
                 arg_splited[1] = eval(arg_splited[1])
                 if type(arg_splited[1]) is str:
-                    arg_splited[1] = arg_splited[1].replace("_", " ").replace('"', '\\"')
+                    arg_splited[1] = arg_splited[1]\
+                        .replace("_", " ").replace('"', '\\"')
                 kw[arg_splited[0]] = arg_splited[1]
+            new_instance = HBNBCommand.classes[arg_list[0]](**kw)
         except SyntaxError:
             print("** class name missing **")
         except NameError:
             print("** class doesn't exist **")
-        new_instance = HBNBCommand.classes[arg_list[0]](**kw)
         new_instance.save()
         print(new_instance.id)
 
@@ -195,7 +196,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del(storage.all()[key])
+            del storage.all()[key]
             storage.save()
         except KeyError:
             print("** no instance found **")
@@ -325,6 +326,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
--- tests/test_console.py
+++ tests/test_console.py
@@ -129,3 +129,4 @@ class TestCommand(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+    
