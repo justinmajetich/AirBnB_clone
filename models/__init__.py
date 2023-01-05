9c7fb4ba8ed955a87d02e1c368700030fe3fa47b
Cyberguru1 <hamzacypher34@gmail.com>
2023-01-05 08:14:16 -0600

    update

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""This module instantiates an object of class FileStorage and DBStorage
+   depending on the type storage specified in the enviromental variable
+"""
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 from os import getenv
