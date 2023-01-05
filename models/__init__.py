2bb1157bdd4a4091ec64130c5fa74398da2281cb
codedjay672 <emmajoel672@gmail.com>
2023-01-05 11:14:44 +0000

    update from Emmanuel

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """create a unique FileStorage instance for your application"""
 from models.engine.file_storage import FileStorage
-from models.engine.database_storage import DBStorage
+from models.engine.db_storage import DBStorage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
