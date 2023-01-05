fea9908a94ea987e32db5d77eda22ceca7abe19d
Cyberguru1 <hamzacypher34@gmail.com>
2023-01-05 07:27:53 -0600

    update

--- models/state.py
+++ models/state.py
@@ -7,5 +7,4 @@ from sqlalchemy import Column, Integer, String
 class State(BaseModel):
     """ State class """
     __tablename__ = "states"
-    id = Column(Integer, unique=True, primary_key=True, nullable=Flase)
     name = Column(String(128), nullable=False,)
