cad9f9b81f03d9584e6d2a3b631004a69b1b4689
Trefania <107484364+Trefania@users.noreply.github.com>
2023-01-06 00:47:14 +0200

    dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,21 +1,8 @@
--- Setup MySQL server for dev
--- a script that prepares a MySQL server for the project
+-- A Script that prepares a MySQL server for the project.
 
---A database hbnb_dev_db
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- A new user hbnb_dev (in localhost)
--- The password of hbnb_dev should be set to hbnb_dev_pwd
-CREATE USER 
-	IF NOT EXISTS 'hbnb_dev'@'localhost'
-	IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
--- hbnb_dev should have SELECT privilege on the database performance_schema
---  (and only this database)
-GRANT SELECT 
-	ON `performance_schema`.* 
-	TO 'hbnb_dev'@'localhost';
--- hbnb_dev should have all privileges on the database hbnb_dev_db (and only this database)
-GRANT ALL PRIVILEGES 
-	ON `hbnb_dev_db`.* 
-	TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* to 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
