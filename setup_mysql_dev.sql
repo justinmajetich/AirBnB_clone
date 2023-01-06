da7b021b29de2138ccb309f68da56519e56f5543
Domengo <dominicsengo@gmail.com>
2023-01-06 18:22:55 +0300

    MySQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,15 +1,11 @@
-mysql -u root -p
--- Creating User and givig it permissions
+-- Create the hbnb_dev_db database and grant all privileges on it to the hbnb_dev user
+CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-GRANT USAGE ON *.*
-      TO 'hbnb_dev'@'localhost'
-      IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.*
-      TO 'hbnb_dev'@'localhost'
-      IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT ON performance_schema.*
-      TO 'hbnb_dev'@'localhost'
-      IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+
+-- Grant SELECT privilege on the performance_schema database to the hbnb_dev user
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Flush the privileges to apply the changes
 FLUSH PRIVILEGES;
-QUIT;
