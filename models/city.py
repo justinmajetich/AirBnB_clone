b76405870b70011f22dfa18ca052e141beffd9b8
Bernmuturi <107024820+ber0622@users.noreply.github.com>
2023-01-06 16:00:35 +0300

    Delete models directory

--- models/city.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
-
-
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
