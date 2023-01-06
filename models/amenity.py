af8540025270c8186eba62393bb8065f61b7a8bc
theeduke <oscarian528@gmail.com>
2023-01-06 14:16:30 -0800

    adding files

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from models.place import Place_amenity
 
 
 class Amenity(BaseModel):
-    name = ""
+    """This is the class for amenity
+    attributes:
+        name = ""
+    """
+    __tablename__ =  "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
--- models/city.py
+++ models/city.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseMode, Base
+from sqlalchemy improt Column, Integer, String
+from sqlalchemy import ForeignKey
+from sglalchemy.orm import relationship
+from models.place import Place
 
 
 class City(BaseModel):
-    """ The city class, contains state ID and name """
+    """ The city class, contains state ID and name
     state_id = ""
     name = ""
+    """
+    __tablename__ = "citites"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('state.id'), nullable=False)
+    places = relationship("Places", cascade='all, delete, delete-orphan', backref="cities")
