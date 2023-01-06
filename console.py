e9e20ed3f899790246cdd792ab998f4c32b8c370
Trefania <107484364+Trefania@users.noreply.github.com>
2023-01-07 01:23:28 +0200

    amenities

--- console.py
+++ console.py
@@ -2,8 +2,12 @@
 """ Console Module """
 import cmd
 import sys
+import os
+import re
+from datetime import datetime
+import uuid
 from models.base_model import BaseModel
-from models.__init__ import storage
+from models import storage
 from models.user import User
 from models.place import Place
 from models.state import State
--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -2,6 +2,7 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
+import os
 
 
 class test_Amenity(test_basemodel):
