8fbed663c228f1bb0cfec775aed046ba874da33b
DMMutua <lengowriter99@gmail.com>
2023-01-09 12:28:03 +0300

    Attempt to make console improvement to do_create

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -121,7 +121,25 @@ class HBNBCommand(cmd.Cmd):
         elif args not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
+
+        args = shlex.split(arg)
+        class_name = args.pop(0)
+
+        new_instance = HBNBCommand.classes[class_name]()
+
+        for param in args:
+            key, value = param.split("=")
+            if value.startswith('"'):
+                value = value[1:-1]
+
+                value = value.replace("_", " ")
+
+                setattr(new_instance, key, value)
+            elif "." in value:
+                setattr(new_instance, key, float(value))
+            else:
+                setattr(new_instance, key, int(value))
+
         storage.save()
         print(new_instance.id)
         storage.save()
@@ -272,7 +290,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +298,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
--- models/state.py
+++ models/state.py
@@ -6,3 +6,18 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """ State class """
     name = ""
+    
+    @property
+    def cities(self):
+        """
+        Returns the list of `City` instances 
+        where `state_id` equals to the current
+        """
+
+        cities = list()
+
+        for _id, city in models.storage.all(City).items():
+            if city.state_id == self.id:
+                cities.append(city)
+
+        return cities
