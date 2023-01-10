3f0c2f19408f0e315c53224c864513e7565e6ce5
Arinze Nwutobo <tim.arinze@outlook.com>
2023-01-10 01:52:02 +0100

    task

--- models/review.py
+++ models/review.py
@@ -8,7 +8,7 @@ from models.place import Place
 
 class Review(BaseModel, Base):
     """ Review class to store review information """
-    __table__ = 'reviews'
+    __tablename__ = 'reviews'
     if dbstorage == 'db':
         text = Column(String(1024), nullable=False)
         place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
