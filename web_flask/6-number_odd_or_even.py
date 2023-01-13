6854dfe5ea5a9214972b339fb62d230f93c3ac3b
Arinze Nwutobo <tim.arinze@outlook.com>
2023-01-13 23:55:36 +0100

    task

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -36,6 +36,7 @@ def numbers(n):
 def number_template(n):
     return render_template("5-number.html", n=n)
 
+
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def odd_or_even(n):
     return render_template("6-number_odd_or_even.html", n=n)
