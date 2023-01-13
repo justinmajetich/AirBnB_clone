7792e3ddd3ebb4b8ace034826827ebccd96a462d
Arinze Nwutobo <tim.arinze@outlook.com>
2023-01-13 12:52:19 +0100

    task

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+A script that starts a flask web application
+/c/<text>: display “C ” followed by the value of the text
+variable (replace underscore _ symbols with a space )
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    return 'HBNB'
+
+
+@app.route('/c/<c_variable>', strict_slashes=False)
+def cisfun(c_variable):
+    return 'C {}'.format(c_variable.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
