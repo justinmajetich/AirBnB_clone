f877bbb03cfb33f5fcd843886979accfd8ddf2d6
Arinze Nwutobo <tim.arinze@outlook.com>
2023-01-13 12:09:37 +0100

    task

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """The Amenity class, contains amenity name"""
+    if dbstorage == 'db':
+        __tablename__ = 'amenties'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
