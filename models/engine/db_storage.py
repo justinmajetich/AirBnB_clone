f35c6d0010997060ac7c2464823e26cd0fc754d4
Arinze Nwutobo <tim.arinze@outlook.com>
2023-01-14 21:36:53 +0100

    task

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -80,4 +80,4 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session
         attribute (self.__session)"""
-        self.__session.close()
+        self.__session.close_all()
