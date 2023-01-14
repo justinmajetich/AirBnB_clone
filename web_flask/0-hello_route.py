45f7f879b1fa540e979f7fad61ca60dfad4b7a89
owuordove@gmail.com <owuordove@gmail.com>
2023-01-15 02:43:10 +0300

    Update web static functionality

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
 """
-Start the flask web framework
-The application listens on 0.0.0.0, port 5000
-Routes:
-    /:Display 'Hello HBNB'
+starts a Flask web application
 """
-from flask import Flask
 
+from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Display 'Hello HBNB'"""
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
