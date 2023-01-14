5d14be877bfbfcdea48c7da1e8c6876c20643c60
owuordove@gmail.com <owuordove@gmail.com>
2023-01-15 02:30:20 +0300

    Update web static functionality

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-# Bash script that sets up your web servers for the deployment of web_static
+# sets up the web servers for the deployment of web_static
 
+sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test/
-echo '<h1>hello world, this is a test</h1>' | sudo tee /data/web_static/releases/test/index.html
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '11i alias /data/web_static/current/;' /etc/nginx/sites-available/default
-sudo service nginx restart
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx start
