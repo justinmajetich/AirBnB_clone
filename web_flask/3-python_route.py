d05351cac37aeaa6868a1c79a6902baa75e5f50a
owuordove@gmail.com <owuordove@gmail.com>
2023-01-15 02:43:41 +0300

    Update web static functionality

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,38 +1,35 @@
 #!/usr/bin/python3
-"""starts a Flask web application"""
+"""
+starts a Flask web application
+"""
 
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """displays a string at / route."""
+def index():
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """displays a string at /hbnb route."""
+    """returns HBNB"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
-    """displays "C" followed by the value
-    of the text variable at /c/<text> route."""
-    new = text.replace('_', ' ')
-    return 'C %s' % new
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
 
 
-@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python(text):
-    """displays "Python", followed by the value
-    of the text variable at /python/(<text>) route."""
-    new = text.replace('_', ' ')
-    return 'Python %s' % new
+def pythoniscool(text='is cool'):
+    """display “Python ”, followed by the value of the text variable"""
+    return 'Python ' + text.replace('_', ' ')
 
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
