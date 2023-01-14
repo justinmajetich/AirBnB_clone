5d013a06da647bab32faa85fb78400d5a728faa0
owuordove@gmail.com <owuordove@gmail.com>
2023-01-15 02:44:50 +0300

    Update web static functionality

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -6,7 +6,6 @@ starts a Flask web application
 from flask import Flask, render_template
 from models import *
 from models import storage
-
 app = Flask(__name__)
 
 
@@ -21,7 +20,6 @@ def cities_by_states():
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
