7314ede46206cc7e64c63f27c85649b4f8e12732
owuordove@gmail.com <owuordove@gmail.com>
2023-01-15 02:43:30 +0300

    Update web static functionality

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
-""" Script that runs a Flask app """
-from flask import Flask, escape
+"""
+starts a Flask web application
+"""
+
+from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    """ function that returns on url /"""
+def index():
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ function that returns on url /hbnb"""
+    """returns HBNB"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def hello_c(text):
-    """ function that returns a input"""
-    return 'C %s' % escape(text.replace('_', ' '))
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
 
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
