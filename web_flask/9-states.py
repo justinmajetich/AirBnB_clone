67bcba4b21e77a19997b7d652274071b34f9d6c8
owuordove@gmail.com <owuordove@gmail.com>
2023-01-15 02:44:57 +0300

    Update web static functionality

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
-""" Script that runs a Flask app """
-from flask import Flask, render_template
-from models import storage
+"""
+starts a Flask web application
+"""
 
+from flask import Flask, render_template
+from models import *
+from models import storage
 app = Flask(__name__)
 
 
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<state_id>', strict_slashes=False)
+def states(state_id=None):
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State")
+    if state_id is not None:
+        state_id = 'State.' + state_id
+    return render_template('9-states.html', states=states, state_id=state_id)
+
+
 @app.teardown_appcontext
-def closing(error):
-    """closes session """
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
-
-@app.route('/states/', strict_slashes=False)
-def states():
-    """ function that list states"""
-    state = storage.all('State')
-    return render_template('9-states.html', state=state)
-
-
-@app.route('/states/<id>', strict_slashes=False)
-def states_id(id):
-    """ function that list a state by id"""
-    state = storage.all('State')
-    return render_template('9-states.html', state=state, id=id)
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
