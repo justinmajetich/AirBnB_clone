bbd9c68d9cba909b753324a1431f43a2e82664f1
owuordove@gmail.com <owuordove@gmail.com>
2023-01-15 02:54:14 +0300

    Update web static markdown and layout

---
+++ web_static/models/amenity.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+""" holds class Amenity"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+
+class Amenity(BaseModel, Base):
+    """Representation of Amenity """
+    if models.storage_t == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
