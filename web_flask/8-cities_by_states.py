a86601a536e633dfaaa6e2fecdfd7afe9b5c2737
Ifiok Ekott <ekottifiok@gmail.com>
2023-01-15 16:56:12 +0100

    started working on task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -2,11 +2,14 @@
 """Write a script that starts a Flask web application:
 Your web application must be listening on 0.0.0.0, port 5000
 Routes:
-    /states_list: display a HTML page: (inside the tag BODY)
+    /cities_by_states: display a HTML page: (inside the tag BODY)
         H1 tag: “States”
-        UL tag: with the list of all State objects present in
-            DBStorage sorted by name (A->Z) tip
-        LI tag: description of one State: <state.id>: <B><state.name></B>
+        UL tag: with the list of all State objects present in DBStorage
+        sorted by name (A->Z) tip
+            LI tag: description of one State: <state.id>: <B><state.name></B>
+            + UL tag: with the list of City objects linked to the State
+            sorted by name (A->Z)
+                LI tag: description of one City: <city.id>: <B><city.name></B>
 """
 
 from flask import Flask, render_template
@@ -23,13 +26,13 @@ def teardown_db(execption):
     storage.close()
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
     """display a HTML page: (inside the tag BODY)"""
     states = sorted(storage.all(State).values(), key=lambda d: d.name)
     for state in states:
         state.cities = sorted(state.cities, key=lambda d: d.name)
-    return render_template('7-states_list.html', states=states)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 if __name__ == '__main__':
