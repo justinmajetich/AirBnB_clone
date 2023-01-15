4833eddc69a88e08223972321bc8c1cb25fd7cf8
Ifiok Ekott <ekottifiok@gmail.com>
2023-01-15 15:22:49 +0100

    started working on task 7 but couldn't test it

--- models/place.py
+++ models/place.py
@@ -6,70 +6,91 @@ from sqlalchemy import Column, Integer, Float, String, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from os import getenv
 
-st = getenv("HBNB_TYPE_STORAGE")
 
-place_amenity = Table("place_amenity",Base.metadata,
-                Column("place_id",
-                    String(60),
-                    ForeignKey("places.id"),
-                    primary_key=True,
-                    nullable=False
-                    ),
-                Column("amenity_id",
-                    String(60),
-                    ForeignKey("amenities.id"),
-                    primary_key=True,
-                    nullable=False
-                    )
-                )
+place_amenity = Table(
+    "place_amenity",
+    Base.metadata,
+    Column(
+        "place_id",
+        String(60),
+        ForeignKey("places.id"),
+        primary_key=True,
+        nullable=False
+    ),
+    Column(
+        "amenity_id",
+        String(60),
+        ForeignKey("amenities.id"),
+        primary_key=True,
+        nullable=False
+    )
+)
+
 
 class Place(BaseModel, Base):
     """ A place to stay """
 
     __tablename__ = "places"
-    
-    if st == "db":
-        city_id = Column(String(60),
-                ForeignKey("cities.id"),
-                nullable=False)
-        user_id = Column(String(60),
-                ForeignKey("users.id"),
-                nullable=False)
-        name = Column(String(128),
-                nullable=False)
-        description = Column(String(1024),
-                nullable=True)
-        number_rooms = Column(Integer,
-                nullable=False,
-                default=0)
-        number_bathrooms = Column(Integer,
-                nullable=False,
-                default=0)
-        max_guest = Column(Integer,
-                nullable=False,
-                default=0)
-        price_by_night = Column(Integer,
-                nullable=False,
-                default=0)
-        latitude = Column(Float,
-                nullable=True)
-        longitude = Column(Float,
-                nullable=True)
-        amenity_ids = []
 
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        city_id = Column(
+            String(60),
+            ForeignKey("cities.id"),
+            nullable=False
+        )
+        user_id = Column(
+            String(60),
+            ForeignKey("users.id"),
+            nullable=False
+        )
+        name = Column(
+            String(128),
+            nullable=False
+        )
+        description = Column(
+            String(1024),
+            nullable=True
+        )
+        number_rooms = Column(
+            Integer,
+            nullable=False,
+            default=0
+        )
+        number_bathrooms = Column(
+            Integer,
+            nullable=False,
+            default=0
+        )
+        max_guest = Column(
+            Integer,
+            nullable=False,
+            default=0
+        )
+        price_by_night = Column(
+            Integer,
+            nullable=False,
+            default=0
+        )
+        latitude = Column(
+            Float,
+            nullable=True
+        )
+        longitude = Column(
+            Float,
+            nullable=True
+        )
+        amenity_ids = []
 
         user = relationship("User", back_populates="places")
         cities = relationship("City", back_populates="places")
         reviews = relationship("Review", back_populates="place")
 
-
         amenities = relationship(
-                "Amenity",
-                secondary=place_amenity,
-                viewonly=False,
-                back_populates="place_amenities")
-
-        
+            "Amenity",
+            secondary=place_amenity,
+            viewonly=False,
+            back_populates="place_amenities"
+        )
 
     else:
         city_id = ""
@@ -95,11 +116,10 @@ class Place(BaseModel, Base):
 
             obj = storage.all()
             lst = list(obj)
-            new =[]
+            new = []
 
             for i in lst:
                 if "Review" in i:
                     temp = Review(obj[i])
                     if temp.place_id == self.id:
                         new.append(temp)
-
--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,14 +10,24 @@ Routes:
 
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(execption):
+    """Closes the database"""
+    storage.close()
 
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """display a HTML page: (inside the tag BODY)"""
-    return render_template('7-states_list.html')
+    return render_template(
+        '7-states_list.html',
+        states=sorted(storage.all(State).values(), key=lambda d: d.name)
+    )
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
