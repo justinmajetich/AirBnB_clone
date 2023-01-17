364642ca90c45cef1f8620ab53e408805bbbccdf
Solomon Enanaw Kebede <101396246+solomonenanaw@users.noreply.github.com>
2023-01-17 12:12:35 +0300

    Update 4-number_route.py
    route

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -4,39 +4,38 @@ starts a Flask web application
 """
 
 from flask import Flask
-
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def index():
-        return "Hello HBNB!"
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return "HBNB"
+    """returns HBNB"""
+    return 'HBNB'
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def C_is_fun(text):
-     return "C " + text.replace("_", " ")
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
 
 
-@app.route("/python", strict_slashes=False)
-def python():
-    return "Python is cool"
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """display “Python ”, followed by the value of the text variable"""
+    return 'Python ' + text.replace('_', ' ')
 
 
-@app.route("/python/<text>", strict_slashes=False)
-def Python_is_magic(text):
-    return "Python " + text.replace("_", " ")
-
-
-@app.route("/number/<int:n>", strict_slashes=False)
-def n_is_a_number(n):
+@app.route('/number/<int:n>', strict_slashes=False)
+def imanumber(n):
+    """display “n is a number” only if n is an integer"""
     return "{:d} is a number".format(n)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
