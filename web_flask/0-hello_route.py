8bac851aa32d6f907543af8f3926ea1bf2d9f257
Solomon Enanaw Kebede <101396246+solomonenanaw@users.noreply.github.com>
2023-01-17 12:46:02 +0300

    Update 0-hello_route.py
    initial commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-'''A simple Flask web application.
-'''
+"""
+starts a Flask web application
+"""
+
 from flask import Flask
-
-
 app = Flask(__name__)
-'''The Flask application instance.'''
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index():
-    '''The home page.'''
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
