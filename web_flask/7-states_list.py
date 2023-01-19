d0c4a9624a288b2339af628a48c192e42bcc06a2
Solomon Enanaw Kebede <101396246+solomonenanaw@users.noreply.github.com>
2023-01-19 04:37:57 +0300

    Update 7-states_list.py
    all

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,33 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
-
+'''A simple Flask web application.
+'''
 from flask import Flask, render_template
-from models import *
+
 from models import storage
+from models.state import State
+
+
 app = Flask(__name__)
+'''The Flask application instance.'''
+app.url_map.strict_slashes = False
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route('/states_list')
 def states_list():
-    """display a HTML page with the states listed in alphabetical order"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states)
+    '''The states_list page.'''
+    all_states = list(storage.all(State).values())
+    all_states.sort(key=lambda x: x.name)
+    ctxt = {
+        'states': all_states
+    }
+    return render_template('7-states_list.html', **ctxt)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def flask_teardown(exc):
+    '''The Flask app/request context end event listener.'''
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
