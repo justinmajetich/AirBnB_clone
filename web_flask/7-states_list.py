d6f6798e5c876abb6fc5de4693d23fbc88543e21
Arinze Nwutobo <tim.arinze@outlook.com>
2023-01-22 02:23:00 +0100

    task

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,20 +5,20 @@ List of States
 from models import storage
 from flask import Flask, render_template
 from models.state import State
+from models import *
 app = Flask(__name__)
-
-
-@app.teardown_appcontext
-def teardown_db(exception):
-    if storage is not None:
-        storage.close()
 
 
 @app.route('/state_list', strict_slashes=False)
 def state_list():
-    data = storage.all(State)
-    return render_template('7-states_list.html', state_list=data.values())
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all(State).values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
