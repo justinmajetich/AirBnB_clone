5862eaa37d85a3858b041b2f9f2dc8c8597cdfc7
Arinze Nwutobo <tim.arinze@outlook.com>
2023-01-22 13:35:29 +0100

    task

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,13 +12,15 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def state_list():
     """display a HTML page with the states listed in alphabetical order"""
-    states_list = sorted(list(storage.all(State)), key = lambda x: x.name)
+    states_list = storage.all(State)
     return render_template('7-states_list.html', states_list=states_list)
+
 
 @app.teardown_appcontext
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
