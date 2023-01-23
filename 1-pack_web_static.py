06c0e572d3977405c6d6e00cb5577a1d16c73d4e
salrealTeknet <salihuabduljawwad.a@gmail.com>
2023-01-23 08:52:50 -0800

    Airbnb CLoneV2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-"""
-Generates a .tgz archive from the contents
-of the web_static folder of the AirBnB Clone repo
-"""
-from fabric.api import local
+# Fabfile to generates a .tgz archive from the contents of web_static.
+import os.path
 from datetime import datetime
+from fabric.api import local
 
 
 def do_pack():
-    """ Function that makes a packs """
-    try:
-        now = datetime.now()
-        date_create = now.strftime("%Y%m%d%H%M%S")
-        local("mkdir -p versions")
-        do_tgz = "web_static_{}.tgz".format(date_create)
-        local("tar -cvzf versions/{} web_static".format(do_tgz))
-        return do_tgz
-    except:
+    """Create a tar gzipped archive of the directory web_static."""
+    dt = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
+    return file
