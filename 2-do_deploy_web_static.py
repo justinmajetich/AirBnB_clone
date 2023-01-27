04d01b4e952de404bb4b9c1797ad1f019affa4fb
Trefania Vhareta <107484364+Trefania@users.noreply.github.com>
2023-01-27 14:58:59 +0200

    static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,6 @@ import os.path
 
 env.hosts = ['54.174.125.138', '54.175.54.26']
 env.user = 'ubuntu'
-env.key_filename = "Tref_personal/school"
 
 def do_deploy(archive_path):
     """distributes an archive to your web servers
@@ -18,12 +17,11 @@ def do_deploy(archive_path):
         put(archive_path, '/tmp/')
         sudo('mkdir -p /data/web_static/releases/{}'.format(base))
         main = "/data/web_static/releases/{}".format(base)
-        sudo('tar -xzfv /tmp/{} -C {}/'.format(arc[1], main))
+        sudo('tar -xzf /tmp/{} -C {}/'.format(arc[1], main))
         sudo('rm /tmp/{}'.format(arc[1]))
         sudo('mv {}/web_static/* {}/'.format(main, main))
         sudo('rm -rf /data/web_static/current')
         sudo('ln -s {}/ "/data/web_static/current"'.format(main))
         return True
-    except Exception as e:
-        print(e)
+    except Exception:
         return False
