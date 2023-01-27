d189170d613b8994ee3ffead8f68b43653114200
Trefania Vhareta <107484364+Trefania@users.noreply.github.com>
2023-01-27 11:39:27 +0200

    deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,49 +1,29 @@
 #!/usr/bin/python3
-
 """web server distribution"""
-
 from fabric.api import *
 import os.path
 
-
-env.hosts = ['54.89.25.106', '52.3.241.66']
+env.hosts = ['54.174.125.138', '54.175.54.26']
 env.user = 'ubuntu'
 env.key_filename = "~/.ssh/id_rsa"
 
 
 def do_deploy(archive_path):
-
-"""distributes an archive to your web servers
-"""
-if os.path.exists(archive_path) is False:
-
-  return False
-
-    try:
-
-        arc = archive_path.split("/")
-
-        base = arc[1].strip('.tgz')
-
-        put(archive_path, '/tmp/')
-
-        sudo('mkdir -p /data/web_static/releases/{}'.format(base))
-
-        main = "/data/web_static/releases/{}".format(base)
-
-        sudo('tar -xzf /tmp/{} -C {}/'.format(arc[1], main))
-
-        sudo('rm /tmp/{}'.format(arc[1]))
-
-        sudo('mv {}/web_static/* {}/'.format(main, main))
-
-        sudo('rm -rf /data/web_static/current')
-
-        sudo('ln -s {}/ "/data/web_static/current"'.format(main))
-
-        return True
-
-    except Exception:
-
+    """distributes an archive to your web servers
+    """
+    if os.path.exists(archive_path) is False:
         return False
-
+    try:
+        arc = archive_path.split("/")
+        base = arc[1].strip('.tgz')
+        put(archive_path, '/tmp/')
+        sudo('mkdir -p /data/web_static/releases/{}'.format(base))
+        main = "/data/web_static/releases/{}".format(base)
+        sudo('tar -xzf /tmp/{} -C {}/'.format(arc[1], main))
+        sudo('rm /tmp/{}'.format(arc[1]))
+        sudo('mv {}/web_static/* {}/'.format(main, main))
+        sudo('rm -rf /data/web_static/current')
+        sudo('ln -s {}/ "/data/web_static/current"'.format(main))
+        return True
+    except Exception:
+        return False
