1d8f0d2daa672621b0214692ab5a09aea3a574c2
Oluwafemi <ogundarearnold@gmail.com>
2023-02-04 12:03:51 +0100

    Done

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states_list: HTML page with a list of all State objects in DBStorage.
-"""
-from models import storage
+""" Program that starts a Flask web application and list the states
+Your web application must be listening on 0.0.0.0, port 5000
+In Routes /states_list: display a HTML page: (inside the tag BODY)"""
 from flask import Flask
 from flask import render_template
-
+from models import storage
+from models.state import State
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
-    States are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
+    """  Function that display a HTML page """
+    # Fetch in a list of all the values available in a given dictionary
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def remove_session(exception):
+    """ After each request you must remove the current SQLAlchemy Session """
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
