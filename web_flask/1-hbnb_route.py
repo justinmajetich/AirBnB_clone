7461517ae1aec0ccb1b41667b91904511084a15a
Trefania <107484364+Trefania@users.noreply.github.com>
2023-02-06 01:24:09 +0200

    hbnb_route

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,10 +11,12 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello():
+    """returning  Hello HBNB str"""
     return("Hello HBNB!")
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """returning  HBNB str"""
     return("HBNB!")
 
 if __name__ == "__main__":
