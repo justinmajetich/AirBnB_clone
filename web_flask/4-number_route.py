4c5eb6bcb9c8fa9be6cc134254584f9f45de5bb7
Trefania Vhareta <107484364+Trefania@users.noreply.github.com>
2023-02-07 01:35:13 +0200

    route

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,41 +1,35 @@
 #!/usr/bin/python3
-""" starts a Flask web application:
+""" Write a script that starts a Flask web application:
 Your web application must be listening on 0.0.0.0, port 5000
-Routes: /: display “Hello HBNB!”
-You must use the option strict_slashes=False in your route definition
 """
 
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask("__name__")
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello():
-    """returning  Hello HBNB str"""
+    """Return a given string"""
     return ("Hello HBNB!")
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """returning  HBNB str"""
+    """Returns a given string"""
     return ("HBNB")
 
 
 @app.route("/c/<text>", strict_slashes=False)
-def c_text(text):
-    """display “C ” followed by the value of the text variable
-    (replace underscore _ symbols with a space )
-    """
+def cText(text):
+    """display C followed by the value of the text variable"""
     return "C {}".format(text.replace("_", " "))
 
 
-app.route("/python", strict_slashes=False)
-app.route("/python/<text>", strict_slashes=False)
-def python_text(text="is cool"):
-    """display “ python” followed by the value of the text variable
-    (replace underscore _ symbols with a space )
-    """
+@app.route('/python', strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pythonText(text="is cool"):
+    """display Python followed by the value of the text variable"""
     return "Python {}".format(text.replace("_", " "))
 
 
@@ -47,4 +41,4 @@ def isNumber(n):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=None)
