e4c0cf07bba3178112b18da8ade05721d8443b25
Trefania <tarirotrefania@gmail.com>
2023-02-06 12:49:28 -0800

    c_route_updates

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -19,7 +19,7 @@ def hello():
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """returning  HBNB str"""
-    return("HBNB!")
+    return("HBNB")
 
 
 @app.route("/c/(<text>)", strict_slashes=False)
