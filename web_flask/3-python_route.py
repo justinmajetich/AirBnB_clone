e2a853660c3f3437b060a351afee8b675405e775
Trefania Vhareta <107484364+Trefania@users.noreply.github.com>
2023-02-07 00:37:02 +0200

    updates

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -29,6 +29,7 @@ def c_text(text):
     """
     return "C {}".format(text.replace("_", " "))
 
+
 @app.route("/python/", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def pyth(text="is cool"):
