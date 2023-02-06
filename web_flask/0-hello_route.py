de4280f78382b1cefb97b3ccd5eac51033f287f2
Trefania <tarirotrefania@gmail.com>
2023-02-06 12:35:43 -0800

    updates

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,10 +9,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
     """returning  Hello HBNB str"""
-    return("Hello HBNB!")
+    return ("Hello HBNB!")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
