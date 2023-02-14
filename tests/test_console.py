ab06c2b6f0cf8f1d8da188978238b31727764882
jbizima <jbizima@alustudent.com>
2023-02-14 21:25:14 +0000

    update error typo

---
+++ tests/test_console.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""test for console"""
+import unittest
+from unittest.mock import patch
+from io import StringIO
+import pep8
+import os
+import json
+import console
+import tests
+from console import HBNBCommand
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.engine.file_storage import FileStorage
+
+
+class TestConsole(unittest.TestCase):
+    """this will test the console"""
