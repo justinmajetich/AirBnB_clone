92adb241c1f6cd68ce6a132fd01b4e2e1d191ac2
Makuo67 <okekemakuo67@gmail.com>
2023-02-16 18:17:28 +0200

    :robot: automated commit of one or many files

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -42,7 +42,7 @@ class FileStorage:
                   }
         try:
             temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
+            with open(FileStorage.__file_path) as f:
                 temp = json.load(f)
                 for key, val in temp.items():
                         self.all()[key] = classes[val['__class__']](**val)
