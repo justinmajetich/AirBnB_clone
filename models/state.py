950f34b16fd2a98bf7f6a1ff3114045eea27a95c
Makuo67 <okekemakuo67@gmail.com>
2023-02-18 01:45:55 +0200

    :robot: automated commit of one or many files

--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", cascade="all, delete", backref="state")
+    else:
+        @property
+        def cities(self):
+            """getter attribute cities"""
+            from models import storage
+            cities_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
