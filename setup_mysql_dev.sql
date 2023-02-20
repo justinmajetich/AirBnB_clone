b8db03c3b73b0436f19648f5bed280855f497e60
Almarat Arnu Ngutulu <99120810+Almah200@users.noreply.github.com>
2023-02-20 22:24:11 +0300

    3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,6 @@
 -- script to prepare mysql server
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db ;
-CREATE DATABASE IF NOT EXISTS performance_schema;
+--CREATE DATABASE IF NOT EXISTS performance_schema;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
