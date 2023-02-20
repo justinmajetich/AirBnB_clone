dda274352b6368c41b38082f45ca02c558fb8b45
Pro <pro@Macbook-pro-de-pro.local>
2023-02-20 21:44:42 +0200

    James submission

--- models/review.py
+++ models/review.py
@@ -4,7 +4,7 @@ from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """ Review classto store review information """
+    """ Review class to store review information """
     place_id = ""
     user_id = ""
     text = ""
