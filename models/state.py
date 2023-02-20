febf1adb8a8444e7a4f1e3bb05f6a2aed58bb299
Makuo67 <okekemakuo67@gmail.com>
2023-02-20 03:42:21 +0200

    :robot: automated commit of one or many files

--- models/state.py
+++ models/state.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from os import getenv
+
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
