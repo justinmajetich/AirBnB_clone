58ede737ef096d5a32bdf1fe5e8228e3431503cb
Nmesoma01 <n.jeff-nwankwo@alustudent.com>
2023-02-21 10:56:52 -0800

    mysql changes

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- Creates user hbnb_test if it doesn't exist already
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
--- Grants all privileges to hbnb_test including the SELECT privilege
+-- Grant all privileges to hbnb_test including the SELECT privilege
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 -- To ensure that changes are applied, flush priviledges
