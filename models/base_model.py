e426bf6bcabc89d962d42b980d4323ce384ccd60
izzeddin62 <i.ishimwe@alustudent.com>
2023-02-22 22:45:10 +0200

    add default date for created_at

--- models/base_model.py
+++ models/base_model.py
@@ -11,7 +11,7 @@ Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), nullable=False, primary_key=True)
-    created_at = Column(DateTime, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
