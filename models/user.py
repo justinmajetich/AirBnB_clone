c7d68361eaf92b290e2fdf21e104166dd5437def
izzeddin62 <i.ishimwe@alustudent.com>
2023-02-23 18:34:46 +0200

    add user column

--- models/user.py
+++ models/user.py
@@ -9,9 +9,9 @@ class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__= "users"
     email = Column(String(128), nullable=False)
-    password = ''
-    first_name = ''
-    last_name = ''
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
     places = relationship("Place", backref='user')
     def __init__(self, *args, **kwargs):
         super().__init__(**kwargs)
