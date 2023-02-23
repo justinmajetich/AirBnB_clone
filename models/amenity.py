d896787bdf976e7a9d97d7090959f9cc0aa0718c
Almarat Arnu Ngutulu <99120810+Almah200@users.noreply.github.com>
2023-02-23 17:59:10 +0300

    10

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import column, String
+from sqlalchemy.orm import relationship
+import os
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """
+    Class of Amenity
+    """
+    __tablename__ = "amenities"
+    if os.getenv("HBNB_TYPE_STORAGE") == 'db':
+        places_amenities = relationship("place",
+                                        secondary='place_amenity')
+        name = column(String(128), nullable=False)
+    else:
+        name=""
