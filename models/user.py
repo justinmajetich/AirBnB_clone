74f88991852eac35498d0e937178f8d22536aca4
izzeddin62 <i.ishimwe@alustudent.com>
2023-02-23 10:33:47 +0200

    add places table

--- models/user.py
+++ models/user.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+    __tablename__="users"
     email = ''
     password = ''
     first_name = ''
     last_name = ''
+    place = relationship("Place", backref='user', cascade="delete")
     def __init__(self, *args, **kwargs):
         super().__init__(**kwargs)
