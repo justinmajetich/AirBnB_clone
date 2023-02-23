e5fc607994b3f47c5f7340fab9698047c8ce8e52
Almarat Arnu Ngutulu <99120810+Almah200@users.noreply.github.com>
2023-02-23 15:00:10 +0300

    user

--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import column, String
+from sqlalchemy import column, String, Table, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 
 
