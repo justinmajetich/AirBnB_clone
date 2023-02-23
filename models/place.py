b8911f4ae45f9b7c092f81fc99f0be588717826b
Almarat Arnu Ngutulu <99120810+Almah200@users.noreply.github.com>
2023-02-23 16:50:01 +0300

    files

--- models/place.py
+++ models/place.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
+""" Place class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import column, String, ForeignKey, Integer, Float, Table
 from sqlalchemy.orm import relationship
-from os import getenv
+import os
 
-storage_type = getenv("HBNB_TYPE_STORAGE")
 
-if storage_type == 'db':
-    place_amenity = Table('place_amenity',Base.metadata,
+place_amenity = Table('place_amenity',Base.metadata,
                           column('place_id', String(60),
                                  ForeignKey('places.id'),
                                  primary_key=True, nullable=False),
@@ -20,6 +18,7 @@ if storage_type == 'db':
 class Place(BaseModel):
     """ A place to stay """
     __tablename__= 'places'
+
     city_id = column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = column(String(60), ForeignKey('user_id'), nullable=False)
     name = column(String(128), nullable=False)
@@ -30,45 +29,8 @@ class Place(BaseModel):
     price_by_night = column(Integer, default=0, nullable=False)
     latitude = column(float, nullable=True)
     longitude = column(Integer, default=0, nullable=True)
-else:
-        city_id = ""
-        user_id = ""
-        name = ""
-        description = ""
-        number_rooms = 0
-        number_bathrooms = 0
-        max_guest = 0
-        price_by_night = 0
-        latitude = 0.0
-        longitude = 0.0
-        amenity_ids = []
 
-def __init__(self, *args, **kwargs):
-        """initializes Place"""
-        super().__init__(*args, **kwargs)
 
-    @property
-    def reviews(self):
-        """getter attribute reviews that
-        returns the list of Review instances
-        with place_id equals to the current Place.id"""
-        from models import storage
-        from models.review import Review
-        reviews = []
-        for review in storage.all(Review).values():
-            if review.place_id == self.id:
-                reviews.append(review)
-        return reviews
 
-    @property
-    def amenities(self):
-        """getter attribute amenities that returns the list of Amenity
-        instances based on the attribute amenity_ids that contains all
-        Amenity.id linked to the Place"""
-        from models import storage
-        from models.amenity import Amenity
-        amenities = []
-        for amenity in storage.all(Amenity).values():
-            if amenity.id in self.amenity_ids:
-                amenities.append(amenity)
-        return amenities
+    
+
