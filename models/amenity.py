0c532b3d8aa4fc404f30dfd99c2d7a4d25c29806
d3ng4 <105013025+d3ng4@users.noreply.github.com>
2023-02-25 00:55:50 +0300

    commit

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, String
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """ The Amenity class, contains name """
+    if models.storage_type == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
