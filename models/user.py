15662d0caba105ff32f381f433f16a6b9aa2f8a8
Almarat <arnualmarat@gmail.com>
2023-02-24 10:20:26 +0300

    7

--- models/user.py
+++ models/user.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import column, String, Table, Integer, ForeignKey, Float
+from sqlalchemy import column, String
 from sqlalchemy.orm import relationship
-import os
+from os import getenv
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__='users'
-
-    email = column(String(128), nullable=False)
-    password = column(String(128), nullable=False)
-    first_name = column(String(128))
-    last_name = column(String(128))
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
+    """
+    This class defines a user by various attributes
+    """
+    if storage_type == 'db':
+         __tablename__='users'
+         email = column(String(128), nullable=False)
+         password = column(String(128), nullable=False)
+         first_name = column(String(128))
+         last_name = column(String(128))
+         places = relationship("Place", backref="user", cascade="delete")
+         reviews = relationship("Review", backref="user", cascade="delete")
+    else:
+       email = ""
+       password =""
+       first_name =""
+       last_name =""
+    
+    def __init__(self, *args, **kwargs):
+        """ initializes user"""
+        super().__init__(*args, **kwargs)
 
