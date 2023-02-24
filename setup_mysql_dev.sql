dde89302da9a0578fdf0072bf43edb2e30cd8020
husnafazal <h.ahmed1@alustudent.com>
2023-02-25 00:39:35 +0300

    new code

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,10 @@
--- script to prepare mysql server
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db ;
---CREATE DATABASE IF NOT EXISTS performance_schema;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+--hbnb_dev_db database 
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+--  hbnb_dev  user in localhost 
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+-- password
+SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
+-- all privileges to the user
+USE hbnb_dev_db;
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performan
