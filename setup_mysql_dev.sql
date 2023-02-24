717c8528a2ffceef4f035b2f55f1061f4632ff83
husnafazal <h.ahmed1@alustudent.com>
2023-02-25 00:44:09 +0300

    commit

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,11 @@
---hbnb_dev_db database 
+-- database called hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
---  hbnb_dev  user in localhost 
+-- a new user called hbnb_dev in localhost
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
 -- password
 SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
 -- all privileges to the user
 USE hbnb_dev_db;
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performan
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
