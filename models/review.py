7af902b8567b3f5044aa16bd3dbb36659986e653
izzeddin62 <i.ishimwe@alustudent.com>
2023-02-24 19:05:13 +0200

    turn text nullable to fals

--- models/review.py
+++ models/review.py
@@ -7,7 +7,7 @@ from sqlalchemy import Column, String, ForeignKey, Integer, Float
 class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__="reviews"
-    text = Column(String(1024), nullable=True)
+    text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     
