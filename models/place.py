cb053002bf785b5dcb3aef32fa167744fd230df1
Nmesoma01 <n.jeff-nwankwo@alustudent.com>
2023-02-24 06:57:28 -0800

    Review

--- models/place.py
+++ models/place.py
@@ -19,3 +19,4 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
+    reviews = relationship("Review", backref='place', cascade="delete")
