51e9b66b8a457c1496fd7eb9fc21c5d434471a59
Oggal <Philip.dan.taylor@gmail.com>
2023-03-02 10:27:52 -0800

    Corrections to Pycode Style in file_storage.py
    Commente debug print in tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -105,5 +105,5 @@ class test_fileStorage(unittest.TestCase):
     def test_storage_var_created(self):
         """ FileStorage object storage created """
         from models.engine.file_storage import FileStorage
-        print(type(storage))
+        #print(type(storage))
         self.assertEqual(type(storage), FileStorage)
