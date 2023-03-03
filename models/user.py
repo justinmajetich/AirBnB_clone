165138c8a4ff4f68e3e690a9af910a16d90d491c
gavintobin <gavintobin714@gmail.com>
2023-03-03 09:25:42 -0800

    task2 first

--- models/user.py
+++ models/user.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
 from sqlalchemy import Column, String
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
