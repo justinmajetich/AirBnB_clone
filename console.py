882f48f182370522cbd6a9da5cb4890a70ba9d1f
FosterClark48 <fosterclark48@yahoo.com>
2023-03-02 20:23:57 -0800

    Updated do_create function to allow for object creation with given parameters

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is '}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -118,13 +118,50 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
-            print("** class doesn't exist **")
-            return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
-        print(new_instance.id)
-        storage.save()
+        else:
+            """ Split args into list of args """
+            args_list = args.split()
+            """ Extracts name of class to be created """
+            class_name = args_list[0]
+            """ Checks if class exists """
+            if class_name not in HBNBCommand.classes:
+                print("** class doesn't exist **")
+                return
+            """
+            Parse parameters: initializing empty dict to hold keyword
+            args for class constructor
+            """
+            kwargs = {}
+            """ Loop over remaining args """
+            for arg in args_list[1:]:
+                """ Begin to parse each arg """
+                try:
+                    """ Split each arg into key = value """
+                    key, value = arg.split('=')
+                    """ Remove any double quotes in value """
+                    value = value.replace('"', '')
+                    """ Replace any escaped underscores with spaces """
+                    value = value.replace('\_', ' ')
+                    """ Checks if value is floating point """
+                    if '.' in value:
+                        """ Converts value to float if floating point """
+                        value = float(value)
+                        """ Checks if value is int """
+                    elif value.isnumeric():
+                        """ Converts value to int if int """
+                        value = int(value)
+                    else:
+                        """ Value is a string """
+                        pass
+                    """ Add parsed parameter to dict """
+                    kwargs[key] = value
+                except ValueError:
+                    """ Skip any malformed parameters """
+                    pass
+            new_instance = HBNBCommand.classes[class_name](*(), **kwargs)
+            storage.save()
+            print(new_instance.id)
+            storage.save()
 
     def help_create(self):
         """ Help information for the create method """
@@ -272,7 +309,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +317,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
