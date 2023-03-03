371ab48a9cff053290ab4329af56fbff1ce1139a
amberwagoner <amberwagoner@outlook.com>
2023-03-03 13:48:24 -0800

    changed hbtn to hbnb

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,8 +1,8 @@
--- Creates database hbtn_test_db
-CREATE DATABASE IF NOT EXISTS hbtn_test_db;
-CREATE USER IF NOT EXISTS 'hbtn_test'@'localhost'
-    IDENTIFIED BY 'hbtn_test_pwd';
-GRANT ALL PRIVILEGES ON hbtn_test_db.* TO 'hbtn_test'@'localhost';
+-- Creates database
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
+    IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.*
-    TO 'hbtn_test'@'localhost';
+    TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
