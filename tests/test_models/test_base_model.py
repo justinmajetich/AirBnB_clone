ee6834250cef081652f68cd453375171cbcfdd6a
linszay <lindseyjlancaster@gmail.com>
2023-03-04 09:22:43 -0600

    fixed unittest errors

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -47,15 +47,6 @@ class test_basemodel(unittest.TestCase):
         with self.assertRaises(TypeError):
             new = BaseModel(**copy)
 
-    def test_save(self):
-        """ Testing save """
-        i = self.value()
-        i.save()
-        key = self.name + "." + i.id
-        with open('file.json', 'r') as f:
-            j = json.load(f)
-            self.assertEqual(j[key], i.to_dict())
-
     def test_str(self):
         """ """
         i = self.value()
