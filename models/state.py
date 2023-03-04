131e8669a5aaf428467e8820e0a043a596403472
linszay <lindseyjlancaster@gmail.com>
2023-03-04 10:50:07 -0600

    updated state.py file

--- models/state.py
+++ models/state.py
@@ -3,9 +3,12 @@
 from models.base_model import BaseModel, Base
 from models.city import City
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
