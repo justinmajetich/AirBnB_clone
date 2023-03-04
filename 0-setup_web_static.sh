a2371a495f53086ed5d2029fd55dff597669e250
izzeddin62 <i.ishimwe@alustudent.com>
2023-03-04 17:38:34 +0200

    add slash on the location

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,5 +11,5 @@ sudo mkdir -p /data/web_static/releases/test/
 echo "<html><head><title>text example</title></head><body>hello world<h1></h1></body></html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '/server_name _;/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+sudo sed -i '/server_name _;/a \\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 sudo service nginx restart
