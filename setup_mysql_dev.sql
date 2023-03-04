fe4c7723c0836b706913315deb2f6bde1a4e0c6b
linszay <lindseyjlancaster@gmail.com>
2023-03-04 00:33:16 -0600

    corrected syntax for MySQL instead of MySqlbd

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,30 +1,6 @@
-#!/usr/bin/python3
-"""
-script prepares a MySQL server for the project
-"""
-
-import sys
-import MySQLdb
-
-if __name__ == '__main__':
-    # MySQL configuration
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    mysql_host = 'localhost'
-    mysql_db = None
-
-    # Connect to MySQL server
-    db = MySQLdb.connect(host=mysql_host,
-                         user=mysql_username, passwd=mysql_password)
-
-    # Create the database if it does not exist
-    cursor = db.cursor()
-    cursor.execute("CREATE DATABASE IF NOT EXISTS hbnb_dev_db;")
-    cursor.execute("GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO\
-     'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';")
-    cursor.execute("GRANT SELECT ON performance_schema.* TO\
-     'hbnb_dev'@'localhost';")
-    cursor.close()
-    db.close()
-
-    print("MySQL server successfully prepared.")
+-- script prepares a MySQL server for the project
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
