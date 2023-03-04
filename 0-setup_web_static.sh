f10eec9d4a44681e18f501a693aed4b4e3c911c3
izzeddin62 <i.ishimwe@alustudent.com>
2023-03-04 17:31:29 +0200

    create folders if they don't exist

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,11 +3,11 @@
 
 sudo apt-get update -y
 sudo apt-get install nginx -y
-sudo mkdir /data/
-sudo mkdir /data/web_static/
-sudo mkdir /data/web_static/releases/
-sudo mkdir /data/web_static/shared/
-sudo mkdir /data/web_static/releases/test/
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 echo "<html><head><title>text example</title></head><body>hello world<h1></h1></body></html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
