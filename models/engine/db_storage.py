6760ac9280211458554dc479c03ea9ef99ffef3f
mwblevins <michaelwblevins@gmail.com>
2023-03-05 19:02:43 -0800

    up to date

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import os
 from os import getenv
 from models.base_model import Base, BaseModel
 from models.review import Review
@@ -12,41 +13,49 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import Base
+
+
 class DBStorage:
     __engine = None
     __session = None
-    
+
     def __init__(self):
-        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".format(getenv('HBNB_MYSQL_USER'),
-                                                                           getenv('HBNB_MYSQL_PWD'),
-                                                                           getenv('HBNB_MYSQL_HOST'),
-                                                                           getenv('HBNB_MYSQL_DB')), pool_pre_ping=True)
-        if getenv('HBNB_ENV') == 'test':
+        # create the engine
+        user = os.environ.get('HBNB_MYSQL_USER')
+        password = os.environ.get('HBNB_MYSQL_PWD')
+        host = os.environ.get('HBNB_MYSQL_HOST', 'localhost')
+        db = os.environ.get('HBNB_MYSQL_DB')
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}:3306/{}'.
+                                       format(user, password, host, db),
+                                       pool_pre_ping=True)
+        # drop all tables if environment variable HBNB_ENV is equal to test
+        if os.environ.get('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
-    
+
     def all(self, cls=None):
-        cls_lst = ["Review", "City", "State", "User", "Place", "Amenity"]
-        obj_lst = []
+        from models import classes
+        objs = {}
         if cls is None:
-            for cls_type in cls_lst:
-                obj_lst.extend(self.__session.query(cls_type).all())
-            else:
-                if type(cls) == str:
-                    cls = eval(cls)
-                obj_lst = self.__session.query(cls).all()
-            return {'{}.{}'.format(type(obj).__name__, obj.id): obj for obj in obj_lst}
-    
+            for c in classes:
+                objs.update({o.__class__.__name__ + '.' + o.id: o for o in self.__session.query(c)})
+        else:
+            objs.update({o.__class__.__name__ + '.' + o.id: o for o in self.__session.query(cls)})
+        return objs
+
     def new(self, obj):
         self.__session.add(obj)
-        
+
     def save(self):
         self.__session.commit()
-        
+
     def delete(self, obj=None):
-        if obj is not None:
+        if obj:
             self.__session.delete(obj)
-            
+
     def reload(self):
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        self.__session = scoped_session(session_factory)
+        self.__session = scoped_session(sessionmaker(bind=self.__engine,
+                                                     expire_on_commit=False))()
