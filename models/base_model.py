f782fce5c94d27f65e6ace49d87b19e897e161cd
Johanna Avila <71939488+jobabyyy@users.noreply.github.com>
2023-03-06 16:02:20 -0600

    updated to_dict to remove key

--- models/base_model.py
+++ models/base_model.py
@@ -7,14 +7,14 @@ from sqlalchemy.ext.declarative import declarative_base
 import shlex  # for splitting lines
 
 
+Base = declarative_base()
+
 class BaseModel:
-    """A base class for all hbnb models"""
-    
-    class BaseModel:
     id = Column(String(60), nullable=False, primary_key=True, default=str(uuid.uuid4()))
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    
+
+    """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
@@ -28,11 +28,9 @@ class BaseModel:
             self.created_at = kwargs.get('created_at', datetime.now())
             self.updated_at = kwargs.get('updated_at', datetime.now())
             if 'created_at' in kwargs:
-                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
             if 'updated_at' in kwargs:
-                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                        '%Y-%m-%dT%H:%M:%S.%f')
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
             if '__class__' in kwargs:
                 del kwargs['__class__']
             self.__dict__.update(kwargs)
@@ -52,12 +50,15 @@ class BaseModel:
     def to_dict(self):
         """returns dict of BaseModel"""
         new_dict = self.__dict__.copy()
+        if '_sa_instance_state' in new_dict:
+            del new_dict['_sa_instance_state']
         new_dict["__class__"] = self.__class__.__name__
         new_dict['created_at'] = self.created_at.isoformat()
         new_dict['updated_at'] = self.updated_at.isoformat()
         return new_dict
-    
+
     def delete(self):
-        """method class to delete instance from storage"""
+        """Deletes the current instance from the storage"""
         from models import storage
         storage.delete(self)
+
