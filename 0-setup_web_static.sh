834189ae61d02185c522e2628ed45c1c98f83507
d3ng4 <105013025+d3ng4@users.noreply.github.com>
2023-03-06 19:52:42 +0300

    commit

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,35 +1,13 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
-
-apt-get update
-apt-get install -y nginx
-
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
-
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
-
-service nginx restart
+# setup for deployment
+sudo apt-get -y update
+sudo apt-get -y install nginx
+SRC="/etc/nginx/sites-available/default"
+STATIC="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static_current/;\n\t}\n"
+sudo mkdir -p "/data/web_static/releases/test/"
+sudo mkdir "/data/web_static/shared/"
+echo "Hello World" | sudo tee "/data/web_static/releases/test/index.html"
+sudo ln -sf "/data/web_static/releases/test/" "/data/web_static/current"
+sudo chown -R ubuntu:ubuntu "/data/"
+sudo sed -i "35i $STATIC" $SRC
+sudo service nginx restart
