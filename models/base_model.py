e700b7cc732f19b5f6813afc0796b1f2a5e3bf40
HH <heather.hayes918@gmail.com>
2023-03-06 14:54:06 -0600

    move things around

--- models/base_model.py
+++ models/base_model.py
@@ -5,8 +5,6 @@ from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
 from models import storage
-
-
 Base = declarative_base()
 
 
@@ -21,18 +19,14 @@ class BaseModel:
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.now()
         else:
-            if 'id' not in kwargs.keys():
-                self.id = str(uuid.uuid4())
-            for key, value in kwargs.items():
-                if key in ['created_at', 'updated_at']:
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                if key != '__class__':
-                    setattr(self, key, value)
-            if 'created_at' not in kwargs.keys():
-                self.created_at = datetime.now()
-                self.updated_at = self.created_at
+            kwargs['update_at'] = datetime.strptime(kwargs['update_at'],
+                                                    '%Y-%m,-%dT%H:%M:%S.%f')
+            kwargs['create_at'] = datetime.strptime(kwargs['create_at'],
+                                                    '%Y-%m,-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -49,13 +43,14 @@ class BaseModel:
         """Convert instance into dict format"""
         dictionary = {}
         dictionary |= self.__dict__
-        dictionary['__class__'] = (str(type(self))
-                                   .split('.')[-1]).split('\'')[0]
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        if '_sa_instance_state' in dictionary:
-            dictionary.pop('_sa_instance_state', None)
-        return dictionary
+        dictionary['__class__'] = (str(type(self)).split('.')[-1]).split('\'')[0]
+        dictionary['create_at'] = self.created_at.isoformat()
+        dictionary['update_at'] = self.updated_at.isoformat()
+
+        dict_copy = dictionary.copy()
+        if "_sa_instance_state" in dict_copy:
+            del dict_copy["_sa_instance_state"]
+        return dict_copy
 
     def delete(self):
         """
