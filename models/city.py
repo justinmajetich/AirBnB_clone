8703abfb9e095bca405e13c726ad587b3d0dc573
mwblevins <michaelwblevins@gmail.com>
2023-03-05 18:46:30 -0800

    typo #4

--- models/city.py
+++ models/city.py
@@ -9,4 +9,4 @@ class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.od'), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
