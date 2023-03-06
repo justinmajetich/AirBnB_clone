4499f266e4c2263cbaeed71e86d2ebc5d3d042c7
FosterClark48 <fosterclark48@yahoo.com>
2023-03-06 12:52:39 -0800

    Updated init file to allow change of storage type directly by using an environment variable

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""
+This module will allow you to change storage type
+directly by using an environment variable
+"""
 from os import getenv
 
-if gentenv('HBNB_TYPE_STORAGE') == 'db':
+if getenv('HBNB_TYPE_STORAGE') == 'db':
     from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.filestorage import FileStorage
+    storage = FileStorage()
 storage.reload()
