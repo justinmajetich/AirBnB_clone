ca0525d0f9ecce5f3e5fb92e8555eb69460c2f93
HH <heather.hayes918@gmail.com>
2023-03-06 13:33:40 -0600

    Pushing console and db fix

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ DB Storage Engine """
 from models.base_model import Base, BaseModel
-from models.base_model import Amenity
-from models.base_model import City
-from models.base_model import Place
-from models.base_model import Review
-from models.base_model import State
-from models.base_model import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from os import getenv
