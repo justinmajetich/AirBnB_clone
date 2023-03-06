ecacff98debf5992bb1a0d316bb91eb2d14cb990
Nachocheeze18 <5581@holbertonstudents.com>
2023-03-06 11:04:27 -0800

    setup my sql file

---
+++ setup_mysql_test.sql
@@ -0,0 +1,9 @@
+-- Creates hbnb_test_db && hbnb_test
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
+IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.*
+TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.*
+TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
