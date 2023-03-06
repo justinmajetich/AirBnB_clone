6816580a472906e486fa3a0bf3b9435fba61b8a9
d3ng4 <105013025+d3ng4@users.noreply.github.com>
2023-03-06 19:42:31 +0300

    commit

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,8 +4,8 @@
 -- Grants all privileges for hbnb_dev on hbnb_dev_db
 -- Grants SELECT privileges for hbnb_dev on performance schema
 
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db.states;
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db.states`.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
