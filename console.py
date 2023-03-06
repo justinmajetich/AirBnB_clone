411f26628cb482a83f31fbf6763a5c8362167036
Nachocheeze18 <5581@holbertonstudents.com>
2023-03-06 09:41:06 -0800

    indentaton error

--- console.py
+++ console.py
@@ -321,4 +321,4 @@ class HBNBCommand(cmd.Cmd):
         print("Usage: update <className> <id> <attName> <attVal>\n")
 
     if __name__ == "__main__":
-    HBNBCommand().cmdloop()
+        HBNBCommand().cmdloop()
