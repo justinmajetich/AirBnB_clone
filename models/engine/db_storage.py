58d61c12acd23a2a24a8d241834495fcbdd510ba
Nachocheeze18 <5581@holbertonstudents.com>
2023-03-06 10:53:44 -0800

    db storage file indention errors

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -17,11 +17,11 @@ class DBStorage:
 
     def __init__(self):
         self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".format(
-                                                getenv('HBNB_MYSQL_USER'),
-                                                getenv('HBNB_MYSQL_PWD'),
-                                                getenv('HBNB_MYSQL_HOST'),
-                                                getenv('HBNB_MYSQL_DB')),
-                                        pool_pre_ping=True)
+                                            getenv('HBNB_MYSQL_USER'),
+                                            getenv('HBNB_MYSQL_PWD'),
+                                            getenv('HBNB_MYSQL_HOST'),
+                                            getenv('HBNB_MYSQL_DB')),
+                                    pool_pre_ping=True)
         if getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
 
