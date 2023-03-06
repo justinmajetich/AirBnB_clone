ead534260cda0c1cb7429514a550a6f6bab1f1df
HH <heather.hayes918@gmail.com>
2023-03-05 19:35:57 -0600

    pycodestyle fix

--- models/city.py
+++ models/city.py
@@ -4,9 +4,11 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
+
 class City(BaseModel, Base):
     """ Represents a city for a MySQL database """
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    places = relationship("Place", back_populates="cities", cascade="delete")
+    places = relationship("Place", backref="cities",
+                          cascade="all, delete, delete-orphan")
