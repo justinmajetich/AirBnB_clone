3b84db106799da065108176c4bd6475d187d18ef
FosterClark48 <fosterclark48@yahoo.com>
2023-03-07 12:28:50 -0800

    updated cities relationship

--- models/state.py
+++ models/state.py
@@ -14,22 +14,4 @@ class State(BaseModel, Base):
     """This class defines a user by name & state_id attributes"""
     name = Column(String(128), nullable=False)
 
-    """Check if env variable is db(sql), if so create rel w/ state"""
-    storageType = getenv("HBNB_TYPE_STORAGE")
-    if storageType == 'db':
-        cities = relationship('City',
-                              cascade="all, delete-orphan",
-                              backref='state'
-                              )
-
-    else:
-        @property
-        def cities(self):
-            """ returns list of City instances with matching state ids"""
-            from models import storage
-
-            Citylist = []
-            for val in storage.all(City).values():
-                if val.state_id == self.id:
-                    Citylist.append(val)
-            return Citylist
+    cities = relationship("City", backref="state", cascade="delete")
