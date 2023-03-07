fa8507c951d73804d7086b4263559c6428c81a89
FosterClark48 <fosterclark48@yahoo.com>
2023-03-07 13:34:25 -0800

    updated to have relationships with place and review

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine, Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
 
+
 class User(BaseModel, Base):
     """Name of table in database to link to"""
     __tablename__ = 'users'
@@ -16,12 +17,8 @@ class User(BaseModel, Base):
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
 
-    """Check if env variable is db(sql), if so create rel w/ place"""
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        places = relationship('Place', cascade='all, delete-orphan', backref='state')
-        """If env variable is not db(sql), its FileStorage"""
-    else:
-        @property
-        def places(self):
-            from models import storage
-            return [place for place in storage.all(Place).values() if place.user_id == self.id]
+    places = relationship('Place', backref='user',
+                          cascade='all, delete, delete-orphan')
+
+    reviews = relationship('Review', backref='user',
+                           cascade='all, delete, delete-orphan')
