f45a8ab242383a1cc9f4a75d06f7a8be6fd26f13
MicoBledsoe <108279441+MicoBledsoe@users.noreply.github.com>
2023-03-07 12:33:19 -0600

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,19 +1,8 @@
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
-from models.city import City
+#!/usr/bin/python3
+""" State Module for HBNB project """
+from models.base_model import BaseModel
 
-class State(BaseModel, Base):
-    __tablename__ = 'states'
 
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade='all, delete-orphan')
-
-    @property
-    def cities(self):
-        from models import storage
-        cities_list = []
-        for city in storage.all(City).values():
-            if city.state_id == self.id:
-                cities_list.append(city)
-        return cities_list
+class State(BaseModel):
+    """ State class """
+    name = ""
