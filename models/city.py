fb143f20f6337d5342eed665f103c3ba290f7ad6
Johanna Avila <71939488+jobabyyy@users.noreply.github.com>
2023-03-07 00:23:05 -0600

    Update city.py
    establishing a link between City and Place models

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """ City Module for HBNB project """
 from sqlalchemy import Column, String, ForeignKey
 from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -9,3 +10,5 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    """ creating one 2 many relationship"""
+    places = relationship('Place', backref='cities')
