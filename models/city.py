0b0931f3388ecf24c9d78ea03f21578c995aafb9
timidgeek <5568@holbertonstudents.com>
2023-03-07 09:45:42 -0800

    updated user and city to have a relationship with places

--- models/city.py
+++ models/city.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.place import Place
 from sqlalchemy import create_engine, Column, String, ForeignKey
-
+from sqlalchemy.orm import relationship
+from os import getenv
 
 class City(BaseModel, Base):
     """Name of table in database to link to"""
@@ -11,3 +13,13 @@ class City(BaseModel, Base):
     """This class defines a user by name & state_id attributes"""
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+
+    """Check if env variable is db(sql), if so create rel w/ place"""
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        places = relationship('Place', cascade='all, delete-orphan', backref='state')
+        """If env variable is not db(sql), its FileStorage"""
+    else:
+        @property
+        def places(self):
+            from models import storage
+            return [place for place in storage.all(Place).values() if place.city_id == self.id]
--- models/user.py
+++ models/user.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
+from models.place import Place
 from sqlalchemy import create_engine, Column, String, ForeignKey
-
+from sqlalchemy.orm import relationship
+from os import getenv
 
 class User(BaseModel, Base):
     """Name of table in database to link to"""
@@ -13,3 +15,13 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
+
+    """Check if env variable is db(sql), if so create rel w/ place"""
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        places = relationship('Place', cascade='all, delete-orphan', backref='state')
+        """If env variable is not db(sql), its FileStorage"""
+    else:
+        @property
+        def places(self):
+            from models import storage
+            return [place for place in storage.all(Place).values() if place.user_id == self.id]
