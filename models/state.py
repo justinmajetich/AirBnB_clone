9d3934b6d9c469e41359c97919e6442a5f25a586
Johanna Avila <71939488+jobabyyy@users.noreply.github.com>
2023-03-07 00:47:09 -0600

    Update state.py

--- models/state.py
+++ models/state.py
@@ -7,7 +7,7 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='states', cascade='all, delete-orphan')
+        cities = relationship('City', backref='state', cascade='all, delete-orphan')
     else:
         @property
         def cities(self):
