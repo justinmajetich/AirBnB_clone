c2a463bfbe971f86b7cc111741c48a42a3f7d177
Maitreya-today <lessismoreclint@gmail.com>
2023-03-07 01:17:33 -0600

    sure

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,7 +3,9 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 REVOKE ALL PRIVILEGES ON hbnb_test_db_fake.* FROM 'hbnb_test'@'localhost';
+
+-- Grant SELECT privilege only on the performance_schema database
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-REVOKE ALL PRIVILEGES ON performance_schema.* FROM 'hbnb_dev'@'localhost';
+REVOKE ALL PRIVILEGES ON *.* FROM 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
