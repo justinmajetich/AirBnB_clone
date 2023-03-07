2d46f088f6360c2945ce5ac5dbf31368f8f564aa
Johanna Avila <71939488+jobabyyy@users.noreply.github.com>
2023-03-07 00:45:50 -0600

    Update state.py
    saving in db storage

--- models/state.py
+++ models/state.py
@@ -5,15 +5,15 @@ from models.city import City
 
 class State(BaseModel, Base):
     __tablename__ = 'states'
-
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade='all, delete-orphan')
-
-    @property
-    def cities(self):
-        from models import storage
-        cities_list = []
-        for city in storage.all(City).values():
-            if city.state_id == self.id:
-                cities_list.append(city)
-        return cities_list
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='states', cascade='all, delete-orphan')
+    else:
+        @property
+        def cities(self):
+            from models import storage
+            cities_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
