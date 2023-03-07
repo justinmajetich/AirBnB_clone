3aa0ed38c13acfe5c46cb6f254ff644c0f892a12
Nachocheeze18 <5581@holbertonstudents.com>
2023-03-06 17:12:19 -0800

    changes to base model file

--- models/base_model.py
+++ models/base_model.py
@@ -10,9 +10,9 @@ Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
-     id = Column(String(60), primary_key=True, nullable=False)
-     created_at = Column(DateTime, default=datetime.now(), nullable=False)
-     updated_at = Column(DateTime, default=datetime.now(), nullable=False)
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.now(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.now(), nullable=False)
 
       def __init__(self, *args, **kwargs):
         self.id = str(uuid4())
