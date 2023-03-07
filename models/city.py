9fcce60ca1a98c2bdab0e383f36c9be6eba70b88
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2023-03-07 11:24:52 -0800

    the class city updated

--- models/city.py
+++ models/city.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from os import getenv
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+import models
+from models.state import State
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    '''
+        Define the class City that inherits from BaseModel.
+    '''
+    __tablename__ = "cities"
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place", backref="cities",
+                              cascade="all, delete, delete-orphan")
+    else:
+        state_id = ""
+        name = ""
