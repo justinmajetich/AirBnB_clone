85713127dad2194297051afb95b558f9e20019ea
kkeas <keaskatrina@gmail.com>
2023-03-07 19:52:29 -0800

    import getenv from os

--- models/city.py
+++ models/city.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy.orm import relationship
+import os
 
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
     state_id = ""
     name = ""
+    places = relationship('Place', backref='cities', cascade='delete', )
