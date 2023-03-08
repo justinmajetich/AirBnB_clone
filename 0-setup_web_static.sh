ed43e3d5500ec80723ce6c96c7a0a69ce387e4be
Almarat Arnu Ngutulu <99120810+Almah200@users.noreply.github.com>
2023-03-08 11:52:24 +0300

    m

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/ env bash
+# A Bash script to prepare the web server
+sudo apt-get update
+sudo apt-get install -y nginx
+sudo mkdir /data/
+sudo mkdir /data/web_static/
+sudo mkdir /data/web_static/releases/
+sudo mkdir /data/web_static/shared/
+sudo mkdir /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+echo "<html><head></head><body> ALU BSE</body></html>" | sudo tee /data/web_static/releases/test/index.html
+sudo chown -R ubuntu:ubuntu /data
+sudo sed -i '37i\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-enabled/default
+sudo service nginx restart
