a9988439520f971fcfe0c1281fcf4d553585a1f1
teylorchapman <5535@holbertonstudent.com>
2023-03-07 20:07:05 -0800

    trying this over

--- models/user.py
+++ models/user.py
@@ -12,4 +12,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    places = relationship('Place', backref='user', cascade='all, delete')
+    places = relationship('Place', backref='user',
+                          cascade='all, delete, delete-orphan')
