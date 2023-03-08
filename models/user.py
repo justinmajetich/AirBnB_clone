88796a65346805a077b030976366999620678cf5
teylorchapman <5535@holbertonstudent.com>
2023-03-07 20:12:27 -0800

    i dont understand why this isnt checking out all the way :/

--- models/user.py
+++ models/user.py
@@ -12,5 +12,6 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
     places = relationship('Place', backref='user',
                           cascade='all, delete, delete-orphan')
