d9af3fee3667eeb8c54d4430cc72ae8acfbfc072
HH <heather.hayes918@gmail.com>
2023-03-08 10:57:38 -0600

    test should be working

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -12,8 +12,3 @@ class test_state(test_basemodel):
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
-
-    def test_name3(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
