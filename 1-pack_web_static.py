d66217b4922e87c5fa20d8b741db5e9e3ccb2e08
Almarat Arnu Ngutulu <99120810+Almah200@users.noreply.github.com>
2023-03-08 15:14:29 +0300

    1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from fabric.api import run, local, put
+import datetime
+
+
+def do_pack():
+    """do pack method"""
+    time = datetime.datetime.now()
+    date = (str(time.year) + str(time.month) + str(time.day) + str(time.hour) +
+            str(time.minute) + str(time.second))
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz ./web_static".format(date))
+        return "./versions/web_static_{}.tgz".format(date)
+    except:
+        return None
