34cbda1b6cfad1eb849b6499496cd9d5546719b4
izzeddin62 <i.ishimwe@alustudent.com>
2023-03-09 15:21:21 +0200

    resolve pep 8 error

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -22,6 +22,5 @@ def do_deploy(archive_path):
         run('rm -rf /data/web_static/current')
         run('ln -s {}/ /data/web_static/current'.format(path_name))
         return True
-    except:
+    except Exception:
         return False
-
