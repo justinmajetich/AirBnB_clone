eee2fc02d1e275414932d3a3e61c23a996df2c2c
Husnafazal <h.ahmed1@alustudent.com>
2023-03-09 18:32:32 +0000

    made some corrections

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,8 +6,10 @@ import os
 
 env.hosts = ['localhost']
 
+
 def do_pack():
-    """Creates a compressed archive of the web_static folder using its contents"""
+    """Creates a compressed archive of the web_static
+  folder using its contents"""
     try:
         if not os.path.exists("versions"):
             local("mkdir versions")
