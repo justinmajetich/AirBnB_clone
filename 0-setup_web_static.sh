ce5c806732c9884ad162b4f4ba414312dec700a9
d3ng4 <105013025+d3ng4@users.noreply.github.com>
2023-03-09 22:26:15 +0300

    commit

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 # setup for deployment
-sudo apt - get - y
-update sudo apt - get - y install nginx SRC = "/etc/nginx/sites-available/default" STATIC = "\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static_current/;\n\t}\n" sudo mkdir - p "/data/web_static/releases/test/" sudo mkdir "/data/web_static/shared/" echo "Hello World" | sudo tee "/data/web_static/releases/test/index.html" sudo ln - sf "/data/web_static/releases/test/" "/data/web_static/current" sudo chown - R ubuntu :ubuntu "/data/" sudo sed - i "35i $STATIC" $SRC sudo service nginx restart
+sudo apt-get -y update
+sudo apt-get -y install nginx
+SRC="/etc/nginx/sites-available/default"
+STATIC="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static_current/;\n\t}\n"
+sudo mkdir -p "/data/web_static/releases/test/"
+sudo mkdir "/data/web_static/shared/"
+echo "Hello World" | sudo tee "/data/web_static/releases/test/index.html"
+sudo ln -sf "/data/web_static/releases/test/" "/data/web_static/current"
+sudo chown -R ubuntu:ubuntu "/data/"
+sudo sed -i "35i $STATIC" $SRC
+sudo service nginx restart
