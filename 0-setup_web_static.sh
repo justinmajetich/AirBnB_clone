d814aa938125056df95aaa161ace54b2243c3cd1
d3ng4 <105013025+d3ng4@users.noreply.github.com>
2023-03-10 23:54:21 +0300

    commit

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
-# setup for deployment
+# web server for deployment
 sudo apt-get -y update
 sudo apt-get -y install nginx
-SRC="/etc/nginx/sites-available/default"
-STATIC="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static_current/;\n\t}\n"
-sudo mkdir -p "/data/web_static/releases/test/"
-sudo mkdir "/data/web_static/shared/"
-echo "Hello World" | sudo tee "/data/web_static/releases/test/index.html"
-sudo ln -sf "/data/web_static/releases/test/" "/data/web_static/current"
-sudo chown -R ubuntu:ubuntu "/data/"
-sudo sed -i "35i $STATIC" $SRC
+
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+
+echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" >/data/web_static/releases/test/index.html
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data
+
+new_str="location /hbnb_static {\nalias /data/web_static/current;\n}"
+sudo sed -i "/# Only/ i $new_str" /etc/nginx/sites-enabled/default
+
 sudo service nginx restart
