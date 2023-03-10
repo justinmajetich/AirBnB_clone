730bdf2e4bb1ad870e6fc1345ad16ece4929891e
d3ng4 <105013025+d3ng4@users.noreply.github.com>
2023-03-10 23:52:19 +0300

    commits

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,60 +1,68 @@
 #!/usr/bin/python3
-"""Fab script"""
-import os
+"""
+Script generates a .tgz archive from web_static folder
+"""
+from fabric.operations import local, run, put, env
 from datetime import datetime
-from fabric.api import *
+import os
 
-env.hosts = ["34.73.5.42", " 34.73.70.108"]
-env.user = "ubuntu"
-env.key_filename = "~/.ssh/holberton"
-env.warn_only = True
+
+env.hosts = ['35.237.142.70', '35.231.108.250']
+env.user = 'ubuntu'
 
 
 def do_pack():
-    """Packs web_static into tgz"""
-    current_time = datetime.now().strftime("%Y%m%d%H%M%S")
-    file_path = "versions/web_static_" + current_time + ".tgz"
+    """
+    function creates a .tgz archive
+    """
+
+    name = "versions/web_static_{}.tgz"
+    name = name.format(datetime.now().strftime("%Y%m%d%H%M%S"))
     local("mkdir -p versions")
-    local("tar -cvzf " + file_path + " web_static")
-    if os.path.exists(file_path):
-        return file_path
+    create = local("tar -cvzf {} web_static".format(name))
+    if create.succeeded:
+        return name
     else:
         return None
 
 
 def do_deploy(archive_path):
-    """Deploys archive to web servers"""
-    if not os.path.exists(archive_path) and not os.path.isfile(archive_path):
+    """
+    function to dist to web server
+    """
+    if not os.path.exists(archive_path):
         return False
-
-    temp = archive_path.split('/')
-    temp0 = temp[1].split(".")
-    f = temp0[0]
-
-    try:
-        put(archive_path, '/tmp')
-        run("sudo mkdir -p /data/web_static/releases/" + f + "/")
-        run("sudo tar -xzf /tmp/" + f + ".tgz" +
-            " -C /data/web_static/releases/" + f + "/")
-        run("sudo rm /tmp/" + f + ".tgz")
-        run("sudo mv /data/web_static/releases/" + f +
-            "/web_static/* /data/web_static/releases/" + f + "/")
-        run("sudo rm -rf /data/web_static/releases/" + f + "/web_static")
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/" + f +
-            "/ /data/web_static/current")
-        return True
-    except:
+    if not put(archive_path, "/tmp/").succeeded:
         return False
-
-    return True
+    filename = archive_path[9:]
+    foldername = "/data/web_static/releases/" + filename[:-4]
+    filename = "/tmp/" + filename
+    if not run('mkdir -p {}'.format(foldername)).succeeded:
+        return False
+    if not run('tar -xzf {} -C {}'.format(filename, foldername)).succeeded:
+        return False
+    if not run('rm {}'.format(filename)).succeeded:
+        return False
+    if not run('mv {}/web_static/* {}'.format(foldername,
+                                              foldername)).succeeded:
+        return False
+    if not run('rm -rf {}/web_static'.format(foldername)).succeeded:
+        return False
+    if not run('rm -rf /data/web_static/current').succeeded:
+        return False
+    return run('ln -s {} /data/web_static/current'.format(
+        foldername)).succeeded
 
 
 def deploy():
-    """Runs both pack and deploy"""
-    f = do_pack()
-    if f:
-        deploy = do_deploy(f)
-        return deploy
-    else:
+    """
+    function to dep
+    """
+    dep = do_pack()
+    if dep is False:
         return False
+    return do_deploy(dep)
+
+
+if __name__ == "__main__":
+    do_deploy()
