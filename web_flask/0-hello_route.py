8b091010633c271b14c008ece97ea3120d478226
Trefania <tarirotrefania@gmail.com>
2023-03-10 17:50:08 +0000

    TASK 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,11 +10,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/airbnb-onepage/", strict_slashes=False)
 def hello():
     """returning  Hello HBNB str"""
     return ("Hello HBNB!")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
