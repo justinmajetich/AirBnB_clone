9a0b1e0dc2a1527dad00680da037657fde4adbb6
root <o.olurinola@alustudent.com>
2023-03-10 22:18:56 +0200

    pep8

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -2,7 +2,8 @@
 from fabric.api import local, run, put, env
 from os.path import exists
 env.hosts = ['<IP web-01>', '<IP web-02>']
- 
+
+
 def do_pack():
     """Creates a compressed archive of the web_static folder"""
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
@@ -12,14 +13,16 @@ def do_pack():
     if exists(archive_file):
         return archive_file
     return None
- 
+
+
 def do_deploy(archive_path):
     """Distributes an archive to a web server"""
     if not exists(archive_path):
         return False
- 
+
     archive_name = archive_path.split("/")[-1]
-    folder_name = "/data/web_static/releases/{}".format(archive_name.split(".")[0])
+    folder_name = "/data/web_static/releases/{}"\
+            .format(archive_name.split(".")[0])
     put(archive_path, "/tmp/")
     run("mkdir -p {}".format(folder_name))
     run("tar -xzf /tmp/{} -C {}".format(archive_name, folder_name))
@@ -29,7 +32,8 @@ def do_deploy(archive_path):
     run("rm -rf /data/web_static/current")
     run("ln -s {} /data/web_static/current".format(folder_name))
     return True
- 
+
+
 def deploy():
     """Creates and distributes an archive to two web servers"""
     archive_path = do_pack()
