681a7425af249969bd7c58124f12381293c3df36
d3ng4 <105013025+d3ng4@users.noreply.github.com>
2023-03-15 17:50:20 +0300

    commit

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -3,46 +3,66 @@ from flask import Flask, render_template
 """
 intializing flask web app
 """
-from flask import Flask, render_template
-
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
+@app.route('/')
+def hello_world():
+    """
+    Display "Hello HBNB!"
+    """
     return ('Hello HBNB!')
 
 
-@app.route('/hbnb', strict_slashes=False)
-def redirect():
+@app.route('/hbnb')
+def hbnb():
+    """
+    route /hbnb displays
+    """
     return ('HBNB')
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
-    return ('C {}'.format(text.replace("_", " ")))
+@app.route('/c/<text>')
+def c(text):
+    """
+    replaces _ with spaces
+    """
+    return ("C {}".format(text.replace('_', ' ')))
 
 
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/<text_py>', strict_slashes=False)
-def python(text_py='is cool'):
-    return ('Python {}'.format(text_py.replace("_", " ")))
+@app.route('/python')
+@app.route('/python/<text>')
+def py(text="is cool"):
+    """
+    default adds is cool and replaces _ with space
+    """
+    return ("Python {}".format(text.replace('_', ' ')))
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def number_route(n):
-    return ('{} is a number'.format(n))
+@app.route('/number/<int:n>')
+def num(n):
+    """
+    displays only if the added is an int
+    """
+    return ("{:d} is a number".format(n))
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def html_number(n):
+@app.route('/number_template/<int:n>')
+def template(n):
+    """
+    routes /number_templates with n integer
+    """
     return (render_template('5-number.html', n=n))
 
 
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def odd_or_even(n):
+@app.route('/number_odd_or_even/<int:n>')
+def odd_even(n):
+    """
+    route odd even to display HTML if n is number
+    """
     return (render_template('6-number_odd_or_even.html', n=n))
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=None)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
