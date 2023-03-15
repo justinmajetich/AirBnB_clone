fdc526c6921a2e96f7277062ca6625bef333b61e
d3ng4 <105013025+d3ng4@users.noreply.github.com>
2023-03-15 18:02:26 +0300

    commit

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,21 +1,6 @@
 #!/usr/bin/python3
 """
-starts a Flask web application:
--Your web application must be listening on 0.0.0.0, port 5000
--Routes:
-        /: display “Hello HBNB!”
-        /hbnb: display “HBNB”
-        /c/<text>: display “C ”, followed by the value of
-         the text variable (replace underscore _ symbols with a space )
-        /python/(<text>): display “Python ”, followed by the value of the text
-        variable (replace underscore _ symbols with a space )
-- The default value of text is “is cool”
-        /number/<n>: display “n is a number” only if n is an integer
-        /number_template/<n>: display a HTML page only if n is an integer:
-- H1 tag: “Number: n” inside the tag BODY
-        /number_odd_or_even/<n>: display a HTML page only if n is an integer:
-- H1 tag: “Number: n is even|odd” inside the tag BODY
-You must use the option strict_slashes=False in your route definition
+initializing flask web app
 """
 from flask import Flask, render_template
 
