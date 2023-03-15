a791e7087e52fa73b826cc774defbf96885de113
Almarat Arnu Ngutulu <99120810+Almah200@users.noreply.github.com>
2023-03-15 15:30:12 +0300

    2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+''' starting a flask application'''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Hello_hbnb module
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    hbnb method
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return f"C {text.replace('_', ' ')}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
