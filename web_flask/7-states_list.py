dc0db80d6cd9a60aa47015a93ca1abe6eab09e61
d3ng4 <105013025+d3ng4@users.noreply.github.com>
2023-03-15 18:10:04 +0300

    commit

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
-"""List all states Module"""
-
-from models import storage
-from models.state import State
 from flask import Flask, render_template
-
+"""
+intializing flask web app to listen on 0.0.0.0:5000
+"""
+from flask import Flask, render_template
+from models import storage, classes
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
 @app.route('/states_list')
 def states_list():
-    """display a HTML the States"""
-    all_states = list(storage.all(State).values())
-    return (render_template('7-states_list.html', all_states=all_states))
+    """
+    Displays html plage with states listed
+    """
+    states = storage.all(classes["State"]).values()
+    return (render_template('7-states_list.html', states=states))
 
 
 @app.teardown_appcontext
-def teardown(self):
-    """function that call close methofd"""
+def teardown_db(exception):
+    """
+    """
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
