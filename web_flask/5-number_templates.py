df97be8b90a2c2181f3b47fa2fd1fa143d37968e
d3ng4 <105013025+d3ng4@users.noreply.github.com>
2023-03-15 17:58:41 +0300

    commit

--- web_flask/5-number_templates.py
+++ web_flask/5-number_templates.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
 """
-starts a Flask web application:
 -Your web application must be listening on 0.0.0.0, port 5000
 -Routes:
-        /: display “Hello HBNB!”
-        /hbnb: display “HBNB”
-        /c/<text>: display “C ”, followed by the value of
-         the text variable (replace underscore _ symbols with a space )
-        /python/(<text>): display “Python ”, followed by the value of the text
-        variable (replace underscore _ symbols with a space )
+    /: display “Hello HBNB!”
+    /hbnb: display “HBNB”
+    /c/<text>: display “C ”, followed by the value of
+     the text variable (replace underscore _ symbols with a space )
+    /python/(<text>): display “Python ”, followed by the value of the
+- text variable (replace underscore _ symbols with a space )
 - The default value of text is “is cool”
-        /number/<n>: display “n is a number” only if n is an integer
-        /number_template/<n>: display a HTML page only if n is an integer:
+    /number/<n>: display “n is a number” only if n is an integer
+    /number_template/<n>: display a HTML page only if n is an integer:
 - H1 tag: “Number: n” inside the tag BODY
-        /number_odd_or_even/<n>: display a HTML page only if n is an integer:
-- H1 tag: “Number: n is even|odd” inside the tag BODY
-You must use the option strict_slashes=False in your route definition
+- You must use the option strict_slashes=False in your route definition
 """
 from flask import Flask, render_template
 
@@ -51,11 +48,6 @@ def number_route(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def html_number(n):
     return (render_template('5-number.html', n=n))
-
-
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def odd_or_even(n):
-    return (render_template('6-number_odd_or_even.html', n=n))
 
 
 if __name__ == '__main__':
