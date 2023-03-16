90c65c7156b5ba74160c814d489276eb297985b8
Almarat Arnu Ngutulu <99120810+Almah200@users.noreply.github.com>
2023-03-16 16:09:59 +0300

    3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -29,7 +29,7 @@ def c_text(text):
 def python(text='is cool'):
     """ python_text function"""
     text = text.replace('_', ' ')
-    return 'python {}'.format(text)
+    return 'Python {}'.format(text)
 
 
 if __name__ == "__main__":
