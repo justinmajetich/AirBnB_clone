430aadf8bea7f25ef0cc1d03feb1e20ae2e64f7b
Almarat Arnu Ngutulu <99120810+Almah200@users.noreply.github.com>
2023-03-16 17:37:50 +0300

    4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" Script for starting a flask application """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Hello_hbnb module"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """hbnb method"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ c_text module that returns c/text """
+    return 'C %s' % text.replace('_', ' ')
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    """python_text function"""
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def number(n):
+    """ number function """
+    if isinstance(n, int):
+        print('n is a number')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
