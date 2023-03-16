f1c9d01c37c01a800375d1e609db8f180d65ffea
Almarat Arnu Ngutulu <99120810+Almah200@users.noreply.github.com>
2023-03-16 13:56:13 +0300

    3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Script for starting a flask application """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Hello_hbnb module"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """hbnb method"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ c_text module that returns c/text """
+    return 'C %s' % text.replace('_', ' ')
+
+
+@app.route('/python/(<text>)', strict_slashes=False)
+def python_text(text='is cool'):
+    """ python_text function"""
+    return 'python %s' % text.replace('_', ' ')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
