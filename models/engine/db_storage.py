75e6025f9893110b55df6ab153373d731039bcf8
Almarat Arnu Ngutulu <99120810+Almah200@users.noreply.github.com>
2023-03-17 11:15:49 +0300

    7

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,83 +1,90 @@
 #!/usr/bin/python3
-"""database storage engine"""
-from os import getenv
-from models.base_model import Base
+""" Database storage module """
 from sqlalchemy import create_engine
+# import declarative base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy import text
-from models.user import User
-from models.state import State
-from models.city import City
+import os
 from models.amenity import Amenity
+from models.base_model import *
 from models.place import Place
 from models.review import Review
+from models.state import State
+from models.city import City
+from models.user import User
 
 
 class DBStorage:
+
+    # private class attributes
     __engine = None
     __session = None
 
     def __init__(self):
-        HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
-        HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
-        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST', default='localhost')
-        HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
-        HBNB_ENV = getenv('HBNB_ENV')
 
-        # self.__engine = create_engine({
-        #     'engine': 'mysql+mysqldb',
-        #     'user': HBNB_MYSQL_USER,
-        #     'password': HBNB_MYSQL_PWD,
-        #     'host': HBNB_MYSQL_HOST,
-        #     'database': HBNB_MYSQL_DB,
-        #     'pool_pre_ping': True
-        # })
-        self.__engine = create_engine(
-            'mysql+mysqldb://' +
-            HBNB_MYSQL_USER +
-            ':' +
-            HBNB_MYSQL_PWD +
-            '@' +
-            HBNB_MYSQL_HOST +
-            '/' +
-            HBNB_MYSQL_DB)
+        # get environment variables
+        user = os.getenv('HBNB_MYSQL_USER')
+        passwd = os.getenv('HBNB_MYSQL_PWD')
+        host = os.getenv('HBNB_MYSQL_HOST')
+        db = os.getenv('HBNB_MYSQL_DB')
+        port = os.getenv('HBNB_MYSQL_PORT')
+        env = os.getenv('HBNB_ENV')
+        # storage_type = os.getenv('HBNB_STORAGE_TYPE')
 
-        if HBNB_ENV == 'test':
-            Base.metadata.drop_all(bind=self.__engine)
+        db_path = ('mysql+mysqldb://{}:{}@{}/{}'
+                   .format(user, passwd, host, db))
+
+        self.__engine = create_engine(db_path, pool_pre_ping=True)
+        # drop all tables if the environment variable HBNB_ENV is equal to test
+        if env == 'test':
+            Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        classes = [User, State, City, Amenity, Place, Review]
-        objects = {}
-
-        if cls is not None:
-            if cls in classes:
-                return {obj.__class__.__name__ + '.' + obj.id:
-                        obj for obj in self.__session.query(cls).all()}
-            else:
-                return {}
+        """ query on the current database session """
+        # create a dictionary
+        obj_dict = {}
+        if cls is None:
+            classes = [State, City, User, Place, Review, Amenity]
+            for class_name in classes:
+                for obj in self.__session.query(class_name):
+                    key = obj.__class__.__name__ + '.' + obj.id
+                    obj_dict[key] = obj
         else:
-            for c in classes:
-                for obj in self.__session.query(text(c.__name__)).all():
-                    objects[obj.__class__.__name__ + '.' + obj.id] = obj
-            return objects
+            for obj in self.__session.query(cls):
+                key = obj.__class__.__name__ + '.' + obj.id
+                obj_dict[key] = obj
+        return obj_dict
 
     def new(self, obj):
+        """ add the object to the current database session """
         self.__session.add(obj)
 
     def save(self):
-        self.__session.commit()
+        """ commit all changes of the current database session """
+        try:
+            self.__session.commit()
+        except Exception:
+            self.__session.rollback()
+        finally:
+            self.__session.close()
 
     def delete(self, obj=None):
+        """ delete from the current database session obj if not None """
         if obj is not None:
             self.__session.delete(obj)
 
     def reload(self):
+        """ create all tables in the database """
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine,
-                                       expire_on_commit=False)
-        Session = scoped_session(session_factory)
+        # create a configured "Session" class
+        sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sec)
+        # create a Session
         self.__session = Session()
 
+        # Base.metadata.create_all(self.__engine)
+        # self.__session = scoped_session(sessionmaker(bind=self.__engine))
+
     def close(self):
-        """ removing """
+        """call remove"""
         self.__session.close()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,30 +10,30 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        if cls:
-            return self.__objects
-        obj_dict = {}
-        for key, value in self.__objects.items():
-            if type(value) == cls:
-                obj_dict[key] = value
-        return obj_dict
+        if cls is not None:
+            temp = {}
+            for key, val in FileStorage.__objects.items():
+                if cls == val.__class__:
+                    temp[key] = val
+            return temp
+
+        return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        # self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
-        key = obj.__class__.__name__ + '.' + obj.id
-        self.__objects[key] = obj
+        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
         """Saves storage dictionary to file"""
-        with open(self.__file_path, mode='w') as file:
-            obj_dict = {}
-            for k, val in self.__objects.items():
-                obj_dict[k] = val.to_dict()
-            json.dump(obj_dict, file)
+        with open(FileStorage.__file_path, 'w') as f:
+            temp = {}
+            temp.update(FileStorage.__objects)
+            for key, val in temp.items():
+                temp[key] = val.to_dict()
+            json.dump(temp, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
+        """Loading storage dictionary"""
         from models.base_model import BaseModel
         from models.user import User
         from models.place import Place
@@ -43,10 +43,10 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
@@ -57,14 +57,13 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
-        """delete obj from __objects, if it's inside
-        if obj == none, do nothing"""
+        """Delete an object from objects"""
         if obj is not None:
-            obj_key = obj.__class__.__name__ + '.' + obj.id
-            if obj_key in self.__objects:
-                del self.__objects[obj_key]
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in self.__objects:
+                del self.__objects[key]
                 self.save()
-    
+
     def close(self):
-        """ Reloading """
+        """Reloading """
         self.reload()
