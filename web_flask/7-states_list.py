52f58de9e72ff267fa679570f3df1f2cb57db14c
root <root@Deng.localdomain>
2023-03-17 22:34:58 +0300

    commit

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
-
-"""list all states module"""
-
-from models import storage
-from models.state import State
 from flask import Flask
+from models import storage, State
 from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states():
-    """Comment"""
-    return render_template('7-states_list.html',
-                           states=storage.all('State').values())
-
-
 @app.teardown_appcontext
-def teardown(self):
-    """Remove the current SQLAlchemy Session"""
+def teardown(exceptions):
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    state_obj = storage.all("State")
+    states = list()
+    for state, value in state_obj.items():
+        states.append(value)
+    return render_template("7-states_list.html", states=states)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
