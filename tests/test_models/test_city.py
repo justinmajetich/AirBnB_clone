95ad0e188fe4092aabb45dde1571a3cc3f5aa3b0
Yoseph <ymymym4634@gmail.com>
2023-03-19 17:17:54 +0300

    AirBnB_clone_v2

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" """
+from tests.test_models.test_base_model import test_basemodel
+from models.city import City
+
+
+class test_City(test_basemodel):
+    """ """
+
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "City"
+        self.value = City
+
+    def test_state_id(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.state_id), str)
+
+    def test_name(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.name), str)
