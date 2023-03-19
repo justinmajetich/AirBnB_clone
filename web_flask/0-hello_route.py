1910fb8977fc04152f6c1da9b30d91b98c738a78
izzeddin62 <i.ishimwe@alustudent.com>
2023-03-19 14:53:45 +0200

    add docs

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""simple flask route"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
+    """return message"""
     return "Hello HBNB!"
 
 if __name__ == "__main__":
