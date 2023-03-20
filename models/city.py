b43c551216d5729faf0923eed84a384044161474
TessierV <5628@holbertonstudents.com>
2023-03-20 07:51:05 -0700

    update classe City and State with relationship

--- models/city.py
+++ models/city.py
@@ -2,10 +2,11 @@
 """ module for class State """
 from sqlalchemy import Column, String, ForeignKey
 from models.base_model import BaseModel, Base
-
+from sqlalchemy.orm import relationship
 
 class City(BaseModel, Base):
     """ class City """
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Place', backref='cities', cascade='delete')
