85cda3ae2282703b04db9826b63053d3a583b0e2
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-20 08:00:30 -0400

    adding delete method to filestorage for task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -47,3 +47,11 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+    
+    def delete(self, obj=None):
+        """Delete obj from __objects if it's inside, if obj is equal to None, do nothing"""
+        if obj is not None:
+            key = f"{obj.__class__.__name__}.{obj.id}"
+            if key in self.__objects:
+                del self.__objects[key]
+                self.save()
