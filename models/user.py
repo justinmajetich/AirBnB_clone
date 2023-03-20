86f8cd6b5e865dc490963c9d11d84bd7d2e48703
Emmanuel Chee-zaram Okeke <ecokeke21@gmail.com>
2023-03-20 15:33:29 +0100

    feat(user): add relationship column `places` to `users` table

--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -15,9 +16,11 @@ class User(BaseModel, Base):
         last_name (str): user's last name
     """
 
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship("Place", backref="user",
+                          cascade="all, delete, delete-orphan")
