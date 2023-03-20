5eac3e863c0e9f3136d2107c76e9ff0705dd3c58
Mumbi1 <mutheemumbi01@gmail.com>
2023-03-20 06:08:38 -0700

    Task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,7 +5,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- gives all privileges on the database hbnb_dev_db
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
---- Gives SELECT privilege on database performance_schema
+-- Gives SELECT privilege on database performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 -- refresh the MySQL server's privilege cache
 FLUSH PRIVILEGES;
