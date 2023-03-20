4bc9ffadbaf513d3b503e103343e195480274e18
Emmanuel Chee-zaram Okeke <ecokeke21@gmail.com>
2023-03-20 14:50:51 +0100

    feat(user): create `users` table with columns in database
    Columns include name, email, first_name, last_name

--- models/user.py
+++ models/user.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """This class defines a user by various attributes
+
+    Attributes:
+        __tablename__ (str): The name of the database table
+        email (str): email address of the user
+        password (str): password of the user
+        first_name (str): user's first name
+        last_name (str): user's last name
+    """
+
+    __tablename__ = 'users'
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
