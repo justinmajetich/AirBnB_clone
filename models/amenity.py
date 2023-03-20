c0b44fef6089a873e50f7ae2d744041f6d647eb7
Emmanuel Chee-zaram Okeke <ecokeke21@gmail.com>
2023-03-20 17:54:04 +0100

    feat(amenity): create table `amenities` with columns in database

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" Amenity Class for HBNB project """
+from models.base_model import BaseModel, Base
+from models import storage_type
+from sqlalchemy import Column, String
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """`Amenity` represents details of an amenity in a place"""
+
+    __tablename__ = "amenities"
+
+    if storage_type == "db":
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
