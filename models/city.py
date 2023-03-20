17b4d6ca4acd8aed810a238928c722c4fe703ba6
Emmanuel Chee-zaram Okeke <ecokeke21@gmail.com>
2023-03-20 17:04:36 +0100

    feat(city): establish relationship between `cities` and `places`

--- models/city.py
+++ models/city.py
@@ -2,10 +2,17 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models import storage_type
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    name = Column(String(128), nullable=False)
+    if storage_type == "db":
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities",
+                              cascade="all, delete, delete-orphan")
+    else:
+        state_id, name = ""
