482129665c2191988d0d37723621bfb1c340dec7
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-20 10:13:35 -0400

    fixing delete method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -61,4 +61,3 @@ class FileStorage:
             key = f"{obj.__class__.__name__}.{obj.id}"
             if key in self.__objects:
                 del self.__objects[key]
-                self.save()
