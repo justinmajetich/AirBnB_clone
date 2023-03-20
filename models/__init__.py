9071fdf9e78ecaedf12242593a160dcfa0d3432c
Ovy Evbodi <evbodiovo@gmail.com>
2023-03-20 22:36:49 +0000

    feat: add database storage to initialization

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+"""This module instantiates an object of class FileStorage/DBStorage,
+depending on the storage type,
+as specified by the environment variable ``HBNB_TYPE_STORAGE``"""
 
 
-storage = FileStorage()
+storage_type = getenv("HBNB_TYPE_STORAGE", "Storage type not set")
+
+if storage_type == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 storage.reload()
