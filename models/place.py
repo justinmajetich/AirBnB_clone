838b2976bdb0ee05cb98a61d239437325c44ec39
Emmanuel Chee-zaram Okeke <ecokeke21@gmail.com>
2023-03-20 16:58:23 +0100

    feat(place): establish relationship between `places` and `reviews`
    For filestorage, we implement a getter method for returning a list of all
    instances of reviews
    whose `place_id` attribute match the current place id

--- models/place.py
+++ models/place.py
@@ -2,20 +2,41 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from models import storage_type
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
 
     __tablename__ = 'places'
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id', nullable=False))
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
-    amenity_ids = []
+    if storage_type == "db":
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id', nullable=False))
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
+        reviews = relationship("Review", backref="place",
+                               cascade="all, delete, delete-orphan")
+    else:
+        city_id = user_id = name = description = ""
+        number_rooms = number_bathrooms = max_guest = price_by_night = 0
+        latitude = longitude = 0.0
+        amenity_ids = []
+
+        @property
+        def reviews(self):
+            """Returns list of `Review` instances with `place_id` == `Place.id`
+            """
+            from models.review import Review
+            from models import storage
+
+            all_reviews = storage.all(Review)
+
+            return [rev_obj for rev_obj in all_reviews.values()
+                    if rev_obj.place_id == self.id]
