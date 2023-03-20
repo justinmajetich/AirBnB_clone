36747ab5e681969e295b70627e82cea2e410eb96
kiki3165 <kikikou82@gmail.com>
2023-03-20 20:01:54 +0000

    test commit

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
---hgyhigyftdrsrd
+--sql
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,5 @@
 -- set db
+--sql
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
