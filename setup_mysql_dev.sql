46242357e1a5ec5a13674fe1e92dc7235a444255
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-20 07:46:50 -0400

    setting up mysql server for task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,10 +2,10 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 -- Create the user if it does not exist
-CREATE USER IF NOT EXISTS 'hbnb_dev@localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grant privileges to the user on hbnb_dev_db
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev@localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- Grant SELECT privilege on performance_schema to the user
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev@localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
