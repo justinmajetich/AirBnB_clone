5ef2dc3449d44013d954bd51879ea56809d99531
kiki3165 <kikikou82@gmail.com>
2023-03-20 15:43:56 +0000

    2nd sql file

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- set db
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+SHOW GRANTS FOR 'hbnb_test'@'localhost';
