566951cb997190fbe10c5274f8776f16062e36b5
Bantamlak <neoluka315@gmail.com>
2023-03-20 21:27:32 +0000

    MYSQL test server is prepared and comment edited

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,7 +3,7 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- Create user if not exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
--- Granting all privileges to the database `hbnb_dev_db`
+-- Grant all privileges to the database `hbnb_dev_db`
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 -- Grant SELECT privilege on the database performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
