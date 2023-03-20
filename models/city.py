71b907ee508b15a9fbb870088453c306bc38d044
Taiwo Babalola <babalolataiwop@gmail.com>
2023-03-20 17:50:37 +0100

    define relationship of cities table to places table

--- models/city.py
+++ models/city.py
@@ -14,4 +14,6 @@ class City(BaseModel, Base):
 
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    # state = relationship("State", back_populates="cities")
+    places = relationship(
+        "Place", cascade="all, delete, delete-orphan", backref="cities"
+    )
