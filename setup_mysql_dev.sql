c0530fd67e414600fc307072e0ab21b53a4b6072
Mumbi1 <mutheemumbi01@gmail.com>
2023-03-20 06:00:56 -0700

    Task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,4 +3,6 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
