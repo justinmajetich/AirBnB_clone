e79c45e267394a6dd61349f9f1f10f6eb494f760
Ubuntu <ubuntu@ip-172-31-8-30.ec2.internal>
2023-03-20 13:09:44 +0000

    Add script to prepare the MySQL server for testing

---
+++ setup_mysql_test.sql
@@ -0,0 +1,23 @@
+-- Prepares a MySQL server for the AirBnB clone development testing
+
+-- Create test database
+DROP DATABASE
+  IF EXISTS hbnb_test_db;
+CREATE DATABASE
+  IF NOT EXISTS hbnb_test_db;
+
+-- Create test user
+CREATE USER
+  IF NOT EXISTS 'hbnb_test'@'localhost'
+  IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant privileges to test user
+GRANT 'ALL PRIVILEGES'
+  ON hbnb_test_db.*
+  TO 'hbnb_test'@'localhost';
+
+GRANT SELECT
+  ON performance_schema.*
+  TO 'hbnb_test'@'localhost';
+
+FLUSH PRIVILEGES;
