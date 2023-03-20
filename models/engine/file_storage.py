296e0389701074b9dbf3e150656fcc55260dedae
Martins Ndifon <martinsndifon@gmail.com>
2023-03-20 20:00:21 +0000

    New added functionalities to the console and setting up database

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if not cls:
+            return FileStorage.__objects
+        objs = {}
+        for k in FileStorage.__objects:
+            if type(FileStorage.__objects[k]) == cls:
+                    objs[k] = FileStorage.__objects[k]
+        return objs
+
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -23,7 +30,7 @@ class FileStorage:
             temp.update(FileStorage.__objects)
             for key, val in temp.items():
                 temp[key] = val.to_dict()
-            json.dump(temp, f)
+            json.dump(temp, f);
 
     def reload(self):
         """Loads storage dictionary from file"""
@@ -48,3 +55,11 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Deletes obj from __objects if it’s contained inside"""
+        if not obj:
+            return
+        obj = obj.to_dict()
+        key = f"{obj['__class__']}.{obj['id']}"
+        del FileStorage.__objects[key]
