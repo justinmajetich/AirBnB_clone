3bbb2832f5479ba8539e6578f3134ca4404e9391
Mumbi1 <mutheemumbi01@gmail.com>
2023-03-20 05:56:03 -0700

    Task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,3 @@
---- script that prepares a MySQL server
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
