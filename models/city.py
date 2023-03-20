0f48da775c01fa03a6a730e54d4e081d526e1ffe
Taiwo Babalola <babalolataiwop@gmail.com>
2023-03-20 10:14:38 +0100

    configure class to map to database table

--- models/city.py
+++ models/city.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """The City class: mapped to cities table,
+    and inherits from BaseModel and Base
+    """
+
+    __tablename__ = "cities"
+
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    # state = relationship("State", back_populates="cities")
--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.engine.file_storage import FileStorage
+from models.city import City
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """State class: mapped to states table"""
+
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete, delete-orphan", backref="state")
+
+    @property
+    def cities(self):
+        """Getter for all City instances with state_id == State.id"""
+        city_list = []
+        fs = FileStorage()
+        all_cities = fs.all(City)
+
+        for key, value in all_cities.items():
+            if key == self.id:
+                city_list.append({key: value})
+        return city_list
