c7a4a672829909a07d59417fb83c4336ead32d39
soniangn <nguyen.sonia@gmail.com>
2023-03-20 05:20:09 -0700

    handle int and test database

---
+++ setup_mysql_test.sql
@@ -0,0 +1,6 @@
+-- prepares a MySQL server
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
