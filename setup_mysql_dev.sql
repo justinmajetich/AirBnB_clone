92b5a3b9fde18f010658cda78e39105c4917cd73
Mumbi1 <mutheemumbi01@gmail.com>
2023-03-20 06:07:19 -0700

    Task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,11 @@
---- script that prepares a MySQL server for the project
+-- script that prepares a MySQL server for the project
+-- creates a dbase
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- creates user if not exists and sets a password
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+-- gives all privileges on the database hbnb_dev_db
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+--- Gives SELECT privilege on database performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+-- refresh the MySQL server's privilege cache
 FLUSH PRIVILEGES;
