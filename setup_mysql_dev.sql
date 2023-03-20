219914728ff9fa7cfa11df8a4be72f80628b9e17
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-03-20 18:34:14 +0000

    added task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,14 +1,11 @@
-/*
-a script that prepares a MySQL server for the project:
-A database hbnb_dev_db
-A new user hbnb_dev (in localhost)
-The password of hbnb_dev should be set to hbnb_dev_pwd
-hbnb_dev should have all privileges on the database hbnb_dev_db (and only this database)
-hbnb_dev should have SELECT privilege on the database performance_schema (and only this database)
-If the database hbnb_dev_db or the user hbnb_dev already exists, the script should not fail
-*/
+-- this script prepares a MySQL server for the project
+-- create project developement database with the name : hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- creating new user named : hbnb_dev with all privileges on the db hbnb_dev_db
+-- with the password : hbnb_dev_pwd if it dosen't exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- granting all privileges to the new user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- granting the SELECT privilege for the user hbnb_dev in the db performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
