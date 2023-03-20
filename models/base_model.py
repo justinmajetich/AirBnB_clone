a652797d1240a1c0b56fe5d07acaf8d0f36a0772
Ovy Evbodi <evbodiovo@gmail.com>
2023-03-20 15:10:33 +0000

    Fix: Add else block in __init__ method

--- models/base_model.py
+++ models/base_model.py
@@ -23,6 +23,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+        else:
             date_fmt = '%Y-%m-%dT%H:%M:%S.%f'
             date_time = ['created_at', 'updated_at']
 
