989a0f825f83ad39a95749436ca16922a69c56d2
Taiwo Babalola <babalolataiwop@gmail.com>
2023-03-20 10:45:14 +0100

    Update package constructor to switch storage engines

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""This module instantiates an object depending on the Storage engine"""
+import os
+from models.engine.db_storage import DBStorage
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
+if os.getenv("HBNB_TYPE_STORAGE") == "db":
+    storage = DBStorage()
+else:
+    storage = FileStorage()
 
-storage = FileStorage()
 storage.reload()
