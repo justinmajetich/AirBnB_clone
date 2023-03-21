738bf3f8364ed1e35aa111af13ae4199b7388e82
H3c7o4 <hectorvladitok@gmail.com>
2023-03-21 11:43:25 +0100

    Updating the do_create(self, arg) function

--- tests/test_console.py
+++ tests/test_console.py
@@ -20,3 +20,64 @@ from models.review import Review
 
 class TestCommand(unittest.TestCase):
     """This class tests the console"""
+
+    @classmethod
+    def setUpClass(cls):
+        """HBNBCommand testing setup.
+        Temporarily rename any existing file.json.
+        Reset FileStorage objects dictionnary.
+        Create an instance of the command interpreter.
+        """
+        try:
+            os.rename("file.json", "tmp")
+        except IOError:
+            pass
+        cls.HBNB = HBNBCommand()
+
+    @classmethod
+    def tearDownClass(cls):
+        """HBNBCommand testing teardown.
+        Restore original file.json.
+        Delete the test HBNBCommand instance.
+        """
+        try:
+            os.rename("tmp", "file.json")
+        except IOError:
+            pass
+        del cls.HBNB
+        if type(models.storage) == DBStorage:
+            models.storage._DBStorage__session.close()
+
+        def setUp(self):
+            """Reset FileStorage objects dictionnary."""
+            FileStorage._FileStorage__objects = {}
+
+        def tearDown(self):
+            """Delete any created file.json"""
+            try:
+                os.remove("file.json")
+            except IOError:
+                pass
+
+        def test_pep8(self):
+            """test PEP8 styling"""
+            style = pep8.StyleGuide(quiet=True)
+            p = style.check_files(["console.py"])
+            self.assertEqual(p.total_errors, 0, "fix Pep8")
+
+        def test_docstrings(self):
+            """check for docstrings."""
+            self.assertIsNotNone(HBNBCommand.__doc__)
+            self.assertIsNotNone(HBNBCommand.emptyline.__doc__)
+            self.assertIsNotNone(HBNBCommand.do_quit.__doc__)
+            self.assertIsNotNone(HBNBCommand.do_EOF.__doc__)
+            self.assertIsNotNone(HBNBCommand.do_create.__doc__)
+            self.assertIsNotNone(HBNBCommand.do_show.__doc__)
+            self.assertIsNotNone(HBNBCommand.do_destroy.__doc__)
+            self.assertIsNotNone(HBNBCommand.do_all.__doc__)
+            self.assertIsNotNone(HBNBCommand.do_update.__doc__)
+            self.assertIsNotNone(HBNBCommand.default.__doc__)
+
+
+if __name__ == "__main__":
+    unittest.main()
