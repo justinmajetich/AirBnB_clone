9eaf427a595961366669e3a72f246dfcf4899d3b
Captressketh001 <shininglite2012@gmail.com>
2023-03-21 02:23:04 +0100

    removed privilege

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -5,6 +5,6 @@ CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
 USE hbnb_test_db;
 GRANT ALL PRIVILEGES on hbnb_test_db.* TO 'hbnb_test'@'localhost';
 USE performance_schema;
-GRANT SELECT PRIVILEGES ON performance_schema.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
 SET FOREIGN_KEY_CHECKS=1;
