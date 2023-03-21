28e10269a4a29fab29f155eb3b6add27e254b62b
dannyEnagu <danielEnagu@gmail.com>
2023-03-21 08:46:34 +0000

    5. Delete object

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-from models import storage
+import models
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
@@ -15,7 +15,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
         else:
             for key, value in kwargs.items():
                 if key != "__class__":
@@ -30,7 +30,7 @@ class BaseModel:
                 self.updated_at = datetime.utcnow()
             if kwargs.get("id", None) is None:
                 self.id = str(uuid.uuid4())
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -40,7 +40,7 @@ class BaseModel:
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
