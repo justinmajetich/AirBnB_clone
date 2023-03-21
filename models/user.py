04b75689e88b97e2bbc6e7f2c65e9638b30b8a21
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-21 16:51:46 -0400

    update to user

--- models/user.py
+++ models/user.py
@@ -14,10 +14,9 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user", cascade="all, delete")
     else:
         email = ""
         password = ""
         first_name = ""
         last_name = ""
-    
-    places = relationship("Place", backref="user", cascade="all, delete")
