54882afd2a8c7417e410ef0f8f521e4d766425ee
Amandine4731 <5618@holbertonstudents.com>
2023-03-21 01:50:27 -0700

    new file with content of task 3 with other user and database name

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,12 +1,12 @@
--- create database 'hbnb_dev_db'
--- create new user 'hbnb_dev' (in localhost) with password 'hbnb_dev_pwd'
--- 'hbnb_dev' should have all privileges on the database 'hbnb_dev_db'
--- 'hbnb_dev' should have SELECT privilege on the database 'performance_schema'
--- If the database hbnb_dev_db or the user 'hbnb_dev' already exists, your script should not fail
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS hbnb_dev@localhost;
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO hbnb_dev@localhost;
-ALTER USER hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
+-- create database 'hbnb_test_db'
+-- create new user 'hbnb_test' (in localhost) with password 'hbnb_test_pwd'
+-- 'hbnb_test' should have all privileges on the database 'hbnb_test_db'
+-- 'hbnb_test' should have SELECT privilege on the database 'performance_schema'
+-- If the database hbnb_test_db or the user 'hbnb_test' already exists, your script should not fail
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS hbnb_test@localhost;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO hbnb_test@localhost;
+ALTER USER hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
 
-GRANT SELECT ON performance_schema.* TO hbnb_dev@localhost;
-ALTER USER hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT ON performance_schema.* TO hbnb_test@localhost;
+ALTER USER hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
