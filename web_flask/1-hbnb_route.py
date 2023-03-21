b4e9472a8eab8014160a440d35e0dff6ddbe5e6e
MaikarfiJesse <j.maikarfi1@alustudent.com>
2023-03-21 22:18:34 +0200

    HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
