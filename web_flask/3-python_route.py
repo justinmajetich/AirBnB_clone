d4c337edd30a8249bf690438974acfbdc7491860
MaikarfiJesse <j.maikarfi1@alustudent.com>
2023-03-22 00:42:08 +0200

    Python is cool!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -16,20 +16,32 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
+    """
+    Returns a greeting message.
+    """
     return 'Hello HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """
+    Returns the string 'HBNB'.
+    """
     return 'HBNB'
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
+    """
+    Returns a string that prepends 'C ' to the given text with underscores replaced by spaces.
+    """
     return 'C ' + text.replace('_', ' ')
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text):
+    """
+    Returns a string that prepends 'Python ' to the given text with underscores replaced by spaces.
+    """
     return 'Python ' + text.replace('_', ' ')
 
-if _name_ == "_main_":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
