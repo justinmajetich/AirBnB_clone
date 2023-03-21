9542951dde96320e778375315a8e9fc3217eebc8
Almarat <arnualmarat@gmail.com>
2023-03-21 21:31:01 +0300

    8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,10 +10,8 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """ 
-    storage to fetch data from the storage enginee
-    remove current SQL Alchemy
-    """
+    """ storage to fetch data from the storage enginee """
+    """remove current SQL Alchemy"""
     states = sorted(list(storage.all(State).values()), 
                     key=lambda state: state.name)
     
