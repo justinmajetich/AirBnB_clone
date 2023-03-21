f8c16772c10bc3694bd89c881dc830705bb0906f
H3c7o4 <hectorvladitok@gmail.com>
2023-03-21 12:13:21 +0100

    Preparing a MySQL server for the project

---
+++ setup_mysql_test.sql
@@ -0,0 +1,9 @@
+-- script that prepares a MySQL server for the project:
+
+CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
+USE `hbnb_test_db`;
+
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_test'@'localhost'
+        IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost'
+        IDENTIFIED BY 'hbnb_dev_pwd';
