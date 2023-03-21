4408e4c169ebd1a543369c9e8b5a7a1b40c00912
MaikarfiJesse <j.maikarfi1@alustudent.com>
2023-03-21 22:41:42 +0200

    Python is cool!

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    return 'C ' + text.replace('_', ' ')
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    return 'Python ' + text.replace('_', ' ')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
