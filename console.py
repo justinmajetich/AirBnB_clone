65eea232c82e5c581bde6c679049bd51890c270c
AsiaGrpr <groupierre.asia@gmail.com>
2023-03-21 03:24:57 -0700

    console.py update def do_create kw l137

--- console.py
+++ console.py
@@ -134,7 +134,7 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
         except NameError:
             print("** class doesn't exist **")
-        new_instance = HBNBCommand.classes[arg_list[0]](**kw)
+        new_instance = HBNBCommand.classes[arg_list[0]](kw)
         new_instance.save()
         print(new_instance.id)
 
