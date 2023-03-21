f812d88a9844aa7e4f604a9483c3eb781ca936ed
Gabriel <Fossecruor@gmail.com>
2023-03-21 14:06:54 +0100

    Resolve conflict

--- models/base_model.py
+++ models/base_model.py
@@ -27,13 +27,16 @@ class BaseModel:
             for key, value in kwargs.items():
                 if not hasattr(self, key):
                     setattr(self, key, value)
-            
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            
+            try:
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                        '%Y-%m-%dT%H:%M:%S.%f')
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                        '%Y-%m-%dT%H:%M:%S.%f')
+                del kwargs['__class__']
+            except KeyError:
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
 
             self.__dict__.update(kwargs)
 
--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -21,6 +21,7 @@ class DBStorage:
         host = os.environ.get("HBNB_MYSQL_HOST")
         database = os.environ.get("HBNB_MYSQL_DB")
         env = os.environ.get("HBNB_ENV")
+        #self.__engine = create_engine(f"mysql+mysqldb://{user}:{password}@{host}:3306/{database}", pool_pre_ping=True)
         self.__engine = create_engine(f"mysql+mysqldb://hbnb_dev:hbnb_dev_pwd@localhost:3306/hbnb_dev_db", pool_pre_ping=True)
         
         Session = sessionmaker(bind=self.__engine)
@@ -30,7 +31,8 @@ class DBStorage:
         if env == "test":
             Base.metadata.drop_all(self.__engine)
         Base.metadata.create_all(self.__engine)
-
+        print(f"S: {self.__session}")
+        print(f"E: {self.__engine}")
 
     def all(self, cls=None): 
         """ query on the current database session """
@@ -41,35 +43,34 @@ class DBStorage:
         #from models.amenity import Amenity
         #from models.review import Review
 
-        Session = sessionmaker(bind=self.__engine)
-        session = Session()
-
         dic = {}
         if cls is None:
-            self.__session = session.query(State).join(City).all()
-            for obj in self.__session:
+            q = self.__session.query(State).all()
+            for obj in q:
                 dic[f"{obj.name}.{obj.id}"] = obj
+                print(dic)
+            print(dic)
+            return dic
         else:
-            self.__session = session.query(cls.__name__).all()
+            q = self.__session.query(cls.__name__).all()
             #dic
         #for obj in self.__session:
         return dic
         
     def new(self, obj):
         """ add the object to the current database session """
-        session.add(obj)
+        self.__session.add(obj)
     
     def save(self):
         """ commit all changes of the current database session """
-        session.commit()
+        self.__session.commit()
         
     def delete(self, obj=None):
         """ delete from the current database session obj if not None """
-
         if obj is None:
             return
         else:
-            session.delete(obj)
+            self.__session.delete(obj)
 
     def reload(self):
         """ create all tables in the database (feature of SQLAlchemy) """
@@ -77,6 +78,7 @@ class DBStorage:
         from models.city import City
         from models.state import State
         
+        Base = declarative_base()
+        s = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        self.__session = scoped_session(s)
         Base.metadata.create_all(self.__engine)
-        self.__session = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        session = scoped_session(self.__session)
