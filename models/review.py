891dc3f8c00de3835c1dbfa0f570ddf5fdcc6624
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2023-03-21 14:58:26 -0700

    Fix bug and pep8

--- models/review.py
+++ models/review.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, ForeignKey, Float, Integer
-
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 class Review(BaseModel, Base):
-    """ Review classto store review information """
+    """ Review class to store review information """
     __tablename__ = "reviews"
-    text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    place_id = Column(
+        String(60),
+        ForeignKey("places.id"),
+        nullable=False
+    )
+    user_id = Column(
+        String(60),
+        ForeignKey("users.id"),
+        nullable=False
+    )
+    text = Column(
+        String(1024),
+        nullable=False
+    )
