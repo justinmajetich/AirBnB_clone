3b82aef43d7a909ed1bb3ccb17b34f61683478d4
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-21 13:40:39 -0400

    adding if/else for class attibutes

--- models/city.py
+++ models/city.py
@@ -2,10 +2,19 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.place import Place
+from os import getenv
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
+        state_id = ""
+
+    places = relationship("Place", backref="cities", cascade="all, delete")
