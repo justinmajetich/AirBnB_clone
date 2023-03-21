8ae680b90816490b6e7afc72eda4b726269fb9c4
Captressketh001 <shininglite2012@gmail.com>
2023-03-21 02:12:49 +0100

    made chamges

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,11 +1,10 @@
--- create database if not exists
+-- a script that prepares a MySQL server for the project
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
--- create user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
--- grant all privileges
+USE hbnb_dev_db;
 GRANT ALL PRIVILEGES on hbnb_dev_db.*  TO 'hbnb_dev'@'localhost';
-
--- grant select privilege on performance schema
-GRANT SELECT ON performance_shema.* TO 'hbnb_dev'@'localhost';
+USE performance_schema;
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
+SET FOREIGN_KEY_CHECKS=1;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,11 +1,10 @@
--- create test db
+-- a script that prepares a MySQL server for the project
+
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-
--- create user
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-
--- Grant all privileges
+CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
+USE hbnb_test_db;
 GRANT ALL PRIVILEGES on hbnb_test_db.* TO 'hbnb_test'@'localhost';
-
--- Grant select privilege on performance_schema
+USE performance_schema;
 GRANT SELECT PRIVILEGES ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
+SET FOREIGN_KEY_CHECKS=1;
