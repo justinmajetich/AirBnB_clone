e3b46f2625586764feeebc389c1686e2b7429cdc
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-03-21 12:17:50 -0700

    add init.py

--- models/city.py
+++ models/city.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Integer, Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
@@ -9,5 +9,6 @@ class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     
     __tablename__ = 'cities'
-    state_id = Column(String(60), nullable=False, ForeignKey('states.id'))
+
     name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
