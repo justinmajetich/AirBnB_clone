4ea138cdc7ac030b65dc6d40f4482c3c04c55252
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-21 18:39:29 -0400

    removing if/else to test the checkers

--- models/review.py
+++ models/review.py
@@ -9,11 +9,6 @@ from os import getenv
 class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = "reviews"
-    if getenv("HBNB_TYPE_STORAGE") == 'db':
-        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        text = Column(String(1024), nullable=False)
-    else:
-        place_id = ""
-        user_id = ""
-        text = ""
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    text = Column(String(1024), nullable=False)
