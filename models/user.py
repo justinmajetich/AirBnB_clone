a596e618c52f01b3a9f453e77240e7e1df682e0a
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2023-03-21 23:06:07 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -3,17 +3,17 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
 import os
 import models
-
+import sqlalchemy
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes
-
+    
         Attributs:
         ====================
+
             email : email of user
                     String, not null
             password: password of compte user
@@ -23,36 +23,35 @@ class User(BaseModel, Base):
             last_name: last name of user
                     String, not null
     """
-
+    
     __tablename__ = "users"
+    #if os.getenv("HBNB_TYPE_STORAGE") == 'db':
 
-    if os.getenv("HBNB_TYPE_STORAGE") == 'db':
-
-        email = Column(
-            String(128),
-            nullable=False)
-        password = Column(
-            String(128),
-            nullable=False
-        )
-        first_name = Column(
-            String(128),
-            nullable=False
-        )
-        last_name = Column(
-            String(128),
-            nullable=False
-        )
-        places = relationship(
-            "Place",
-            backref="user",
-            cascade="all, delete-orphan")
-        reviews = relationship(
-            "Review",
-            backref="user",
-            cascade='all, delete-orphan')
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name =""
+    email = Column(
+        String(128),
+        nullable=False)
+    password = Column(
+        String(128),
+        nullable=False
+    )
+    first_name = Column(
+        String(128),
+        nullable=False
+    )
+    last_name = Column(
+        String(128),
+        nullable=False
+    )
+    places = relationship(
+        "Place",
+        backref="user",
+        cascade="all, delete-orphan")
+    reviews = relationship(
+        "Review",
+        backref="user",
+        cascade='all, delete-orphan')
+    # else:
+    #     email = ""
+    #     password = ""
+    #     first_name = ""
+    #     last_name =""
