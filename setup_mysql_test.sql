16553c4eb0e9269a0f5986ab66320c8ec7e2f7b6
vickouma77 <Vickprestone20@gmail.com>
2023-03-21 17:53:35 +0300

    updating setup_mysql_dev & setup_mysql_test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,5 @@
--- script that prepares a MySQL server for the project:
-
-CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
-USE `hbnb_test_db`;
-
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_test'@'localhost'
-        IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost'
-        IDENTIFIED BY 'hbnb_dev_pwd';
+-- Prepares a MySQL Test server for the project.
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
