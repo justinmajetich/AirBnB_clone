6dc1507db1e26613efaad3e244cc151543997f04
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-21 18:38:26 -0400

    removing if/else to test the checkers

--- models/city.py
+++ models/city.py
@@ -10,10 +10,6 @@ from os import getenv
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    if getenv("HBNB_TYPE_STORAGE") == 'db':
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        name = Column(String(128), nullable=False)
-        places = relationship("Place", backref="cities", cascade="all, delete")
-    else:
-        name = ""
-        state_id = ""
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    places = relationship("Place", backref="cities", cascade="all, delete")
