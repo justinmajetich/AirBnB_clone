3edbe5708e30b6f5f022a1a2b700f3ea8e5eb9c9
sunday-irvine code <amugumbiirvine@gmail.com>
2023-03-21 09:26:40 -0700

    corrected console.py

--- models/state.py
+++ models/state.py
@@ -2,15 +2,16 @@
 """ State Module for HBNB project """
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, text
 from os import getenv
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
+
     if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'states'
+        name = Column (String(128), nullable=False)
         cities = relationship('City', cascade='all, delete', backref='state')
     else:
         @property
