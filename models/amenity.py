3771878fc23deabafe1852192a672ea68dda5b72
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2023-03-21 23:06:52 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 import os
+import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -9,14 +10,12 @@ from sqlalchemy.orm import relationship
 class Amenity(BaseModel, Base):
     """ Class representation of Amenity """
     __tablename__ = 'amenities'
-
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(
-            String(128),
-            nullable=False)
-        place_amenities = relationship('Place', secondary="place_amenity",
-                                   viewonly=False)
-    else:
-        name = ""
-
+    # if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    name = Column(
+        String(128),
+        nullable=False)
+    place_amenities = relationship('Place', secondary="place_amenity",
+                                viewonly=False)
+    # else:
+    #     name = ""
 
