83f90167e47fbc0448c69462b77e9f3b321909a9
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-21 18:32:20 -0400

    removing if/else for class attribute to test checkers

--- models/state.py
+++ models/state.py
@@ -10,11 +10,9 @@ from os import getenv
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state", cascade="all, delete")
-    else:
-        name = ""
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
+    if getenv("HBNB_TYPE_STORAGE") != 'db':
         @property
         def cities(self):
             """ Returns the list of City instances with state_id """
