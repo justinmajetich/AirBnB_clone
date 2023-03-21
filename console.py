f0bed9ce510f9dfa1c7ff9fe94f0a9abd1f230c8
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-03-21 18:54:39 +0100

    fixed backslash in console

--- console.py
+++ console.py
@@ -137,7 +137,7 @@ class HBNBCommand(cmd.Cmd):
                 # Split arguments into key and value
                 item = item.split('=')
                 key = item[0]
-                value = item[1]
+                value = eval(item[1])
 
                 if not isinstance(value, (str, float, int)):
                     continue  # Skip the argument
