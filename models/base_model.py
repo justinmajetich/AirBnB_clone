7d234820d848eeced0b99f90552b9bbf4bd85143
Irvine Sunday <amugumbiirvine@gmail.com>
2023-03-21 20:57:40 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,5 @@
-s module defines a base class for all models in our hbnb clone"""
+#!/usr/bin/python3
+"""This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
@@ -7,54 +8,53 @@ from sqlalchemy import Column, String, DateTime
 Base = declarative_base()
 
 class BaseModel:
-        """A base class for all hbnb models"""
-            id = Column(String(60), nullable=False, primary_key=True)
-                created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-                    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    """A base class for all hbnb models"""
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
-                        def __init__(self, *args, **kwargs):
-                                """Instatntiates a new model"""
-                                        if not kwargs:
-                                                    self.id = str(uuid.uuid4())
-                                                                self.created_at = datetime.utcnow()
-                                                                            self.updated_at = datetime.utcnow()
-                                                                                    else:
-                                                                                                if 'created_at' in kwargs:
-                                                                                                                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                                                                                                                                         '%Y-%m-%dT%H:%M:%S.%f')
-                                                                                                                                                                                     if 'updated_at' in kwargs:
-                                                                                                                                                                                                     kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                                                                                                                                                                                                                              '%Y-%m-%dT%H:%M:%S.%f')
-                                                                                                                                                                                                                                                                          for key, value in kwargs.items():
-                                                                                                                                                                                                                                                                                          if key != '__class__':
-                                                                                                                                                                                                                                                                                                              setattr(self, key, value)
+    def __init__(self, *args, **kwargs):
+        """Instatntiates a new model"""
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
+        else:
+            if 'created_at' in kwargs:
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                         '%Y-%m-%dT%H:%M:%S.%f')
+            if 'updated_at' in kwargs:
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                         '%Y-%m-%dT%H:%M:%S.%f')
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
 
-                                                                                                                                                                                                                                                                                                                  def __str__(self):
-                                                                                                                                                                                                                                                                                                                          """Returns a string representation of the instance"""
-                                                                                                                                                                                                                                                                                                                                  cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-                                                                                                                                                                                                                                                                                                                                          return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+    def __str__(self):
+        """Returns a string representation of the instance"""
+        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
-                                                                                                                                                                                                                                                                                                                                              def save(self):
-                                                                                                                                                                                                                                                                                                                                                      """Updates updated_at with current time when instance is changed"""
-                                                                                                                                                                                                                                                                                                                                                              from models import storage
-                                                                                                                                                                                                                                                                                                                                                                      self.updated_at = datetime.utcnow()
-                                                                                                                                                                                                                                                                                                                                                                              storage.new(self)
-                                                                                                                                                                                                                                                                                                                                                                                      storage.save()
+    def save(self):
+        """Updates updated_at with current time when instance is changed"""
+        from models import storage
+        self.updated_at = datetime.utcnow()
+        storage.new(self)
+        storage.save()
 
-                                                                                                                                                                                                                                                                                                                                                                                          def to_dict(self):
-                                                                                                                                                                                                                                                                                                                                                                                                  """Convert instance into dict format"""
-                                                                                                                                                                                                                                                                                                                                                                                                          dictionary = {}
-                                                                                                                                                                                                                                                                                                                                                                                                                  for key, value in self.__dict__.items():
-                                                                                                                                                                                                                                                                                                                                                                                                                              if key != '_sa_instance_state':
-                                                                                                                                                                                                                                                                                                                                                                                                                                              if isinstance(value, datetime):
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                  dictionary[key] = value.isoformat()
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  else:
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      dictionary[key] = value
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              dictionary['__class__'] = type(self).__name__
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      return dictionary
+    def to_dict(self):
+        """Convert instance into dict format"""
+        dictionary = {}
+        for key, value in self.__dict__.items():
+            if key != '_sa_instance_state':
+                if isinstance(value, datetime):
+                    dictionary[key] = value.isoformat()
+                else:
+                    dictionary[key] = value
+        dictionary['__class__'] = type(self).__name__
+        return dictionary
 
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          def delete(self):
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  """Delete the current instance from the storage"""
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          from models import storage
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  storage.delete(self)
-
+    def delete(self):
+        """Delete the current instance from the storage"""
+        from models import storage
+        storage.delete(self)
