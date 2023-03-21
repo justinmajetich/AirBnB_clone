9180ad2e7ae99c32b49fbf4a53492fc5dacdd9ba
MaikarfiJesse <j.maikarfi1@alustudent.com>
2023-03-21 23:00:20 +0200

    Number template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""from flask import Flask, render_template"""
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    return 'C ' + text.replace('_', ' ')
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    return 'Python ' + text.replace('_', ' ')
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return f'{n} is a number'
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('number.html', n=n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
