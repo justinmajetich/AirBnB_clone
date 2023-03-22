1cdeb29594835ff6ce6b3d3b6b4dd461d7b899ad
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-03-21 18:10:34 -0700

    fix place and state files

--- models/state.py
+++ models/state.py
@@ -5,18 +5,17 @@ from sqlalchemy import Column, String
 from os import getenv
 from sqlalchemy.orm import relationship
 import models
+from models.city import City
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state", cascade="all, delete")
-    else:
-        name = ""
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
 
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
     @property
     def cities(self):
         """
@@ -24,7 +23,7 @@ class State(BaseModel, Base):
             to the current State.id
         """
         list_cities = []
-        for city in models.storage.all("City").values():
+        for city in models.storage.all(City).values():
             if city.state_id == self.id:
                 list_cities.append(city)
         return list_cities
