1efe3bf19870f354d5d3fcea7332cb4e6668fafc
63brown <mutheemumbi01@gmail.com>
2023-03-22 04:49:29 +0000

    update user.py

--- models/user.py
+++ models/user.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from models import storage_engine
 from sqlalchemy import Column, String
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
     if storage_engine == 'db':
-	email = Column(String(128), nullable=False)
-    	password = Column(String(128), nullable=False)
-    	first_name = Column(String(128),nullable=True)
-    	last_name = Column(String(128),nullable=True)
-
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
     else:
- 	email = ""
+        email = ""
         password = ""
         first_name = ""
-        last_name = ""	
+        last_name = ""
