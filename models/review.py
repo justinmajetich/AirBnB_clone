d2a77dc59c4c9252f12cbb77147e3e3f417c65ea
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-03-22 14:05:36 +0000

    Modify
    Imports Base from base_model instead of declarative_base,
    this avoids import duplication as the Base contains information
    needed by all other classes
    
    Moves all relationships to the db_storage.py file, bringing them
    close to the point where the db session starts.

--- models/review.py
+++ models/review.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String, ForeignKey
-
-Base = declarative_base()
 
 
 class Review(BaseModel, Base):
     """ Review class to store review information """
     __tablename__ = 'reviews'
-    user = relationship('User', back_populates='reviews')
-    place = relationship('Place', back_populates='reviews')
 
-    place_id = Column(String(60), ForeignKey(places.id), nullable=False)
-    user_id = Column(String(60), ForeignKey(users.id), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     text = Column(String(1024), nullable=False)
