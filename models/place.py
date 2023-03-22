c9ebe8d02f4760799553870e051ecb5c287bd6de
soniangn <nguyen.sonia@gmail.com>
2023-03-22 08:30:29 -0700

    update branch

--- models/place.py
+++ models/place.py
@@ -20,7 +20,7 @@ class Place(BaseModel, Base):
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
-        description = Column(String(1024), nullable=False)
+        description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, nullable=False, default=0)
         number_bathrooms = Column(Integer, nullable=False, default=0)
         max_guest = Column(Integer, nullable=False, default=0)
@@ -29,12 +29,12 @@ class Place(BaseModel, Base):
         longitude = Column(Float, nullable=True)
         amenity_ids = []
         reviews = relationship("Review", backref="place")
-        amenities = relationship('Amenity', secondary=place_amenity, viewonly=False, overlaps="place_amenities")
+        amenities = relationship('Amenity', secondary='place_amenity', viewonly=False)
     else:
         @property
         def reviews(self):
-            """getter attribute cities that returns the list of City instances with
-            state_id equals to the current State.id"""
+            """getter attribute cities that returns the list of review instances with
+            place_id equals to the current place.id"""
             review_list = []
             for review in storage.all(Review).values():
                 if review.place_id == self.id:
