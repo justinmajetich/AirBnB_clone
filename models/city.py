db590ff700a459695f93c3f7885f3215313e9612
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-03-22 13:42:51 +0000

    updated place, user, city

--- models/city.py
+++ models/city.py
@@ -1,31 +1,21 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-<<<<<<< HEAD
 from models import storage_type
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-
-=======
 from sqlalchemy import Column, String, ForeignKey
 from models import storage_type
->>>>>>> be257265eea6d7fed27d027438956c90f1af5b86
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-<<<<<<< HEAD
+    __tablename__ = 'cities'
     if storage_type == 'db':
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         places = relationship('Place', backref='cities',
-                              cascade='all, delete, delete-orphan')
-=======
-    __tablename__ = 'cities'
-    if storage_type == 'db':
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
->>>>>>> be257265eea6d7fed27d027438956c90f1af5b86
+                              cascade='all, delete, delete-orphan'
     else:
         state_id = ""
         name = ""
