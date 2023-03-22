d59b46644fa06aa315ba5ec1a45411b442912610
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-03-22 00:32:06 +0000

    Modify
    Removes the declarative base class

--- models/state.py
+++ models/state.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from models.city import City
 from sqlalchemy import Column, Integer, String
-
-Base = declarative_base()
 
 
 class State(BaseModel, Base):
--- models/user.py
+++ models/user.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
