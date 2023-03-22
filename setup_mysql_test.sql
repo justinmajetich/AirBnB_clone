3b053f125a590807fb84e2284118ed9d6a520e61
JI-Maina <irungujmaina@gmail.com>
2023-03-22 10:58:40 +0300

    git push resolve

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,4 +3,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO hbnb_devh@localhost;
 GRANT SELECT ON performance_schema.* TO hbnb_dev@localhost;
+
+
+
 FLUSH PRIVILEGES;
