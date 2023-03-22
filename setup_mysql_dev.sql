32b193701aae8d984aebca3791010880976e8cac
MikeRock51 <mikerockmusic51@gmail.com>
2023-03-22 07:23:07 +0100

    Add Task 3

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,13 @@
+-- Write a script that prepares a MySQL server for the project:
+    -- A database hbnb_dev_db
+    -- A new user hbnb_dev (in localhost)
+    -- The password of hbnb_dev should be set to hbnb_dev_pwd
+    -- hbnb_dev should have all privileges on the database hbnb_dev_db (and only this database)
+    -- hbnb_dev should have SELECT privilege on the database performance_schema (and only this database)
+    -- If the database hbnb_dev_db or the user hbnb_dev already exists, your script should not fail
+
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON 'hbnb_dev_db'.* TO 'hbnb_dev';
+GRANT SELECT ON 'performance_schema'.* TO 'hbnb_dev';
