688d4906d72118e06b3d39f035d45d15f7b9c0ca
Bantamlak <neoluka315@gmail.com>
2023-03-22 12:27:22 +0000

    updated user file

--- models/user.py
+++ models/user.py
@@ -4,7 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes
 
     Attributes:
