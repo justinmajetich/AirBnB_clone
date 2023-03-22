095673f1dc9e867fa25eec1b78352dbe5107c808
Simon Tagbor <simontagbor360@gmail.com>
2023-03-22 05:02:06 -0700

    Add test database and create a User Account for Testing.
    *Reason:*
    We need to keep the development database clean, since testing may require the frequent modifications to the database, creating the a separate database(`hbnb_test_db`) for this purpose helps us achieve that

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,18 +1,17 @@
 -- Create database if it does not exist
--- Database hbnb_dev_db.
--- User hbnb_dev with password hbnb_dev_pwd in localhost.
--- Grants all privileges for hbnb_dev on hbnb_dev_db.
--- Grants SELECT privilege for hbnb_dev on performance_schema
+-- Database Name: hbnb_test_db.
+-- User: hbnb_test with password hbnb_test_pwd in localhost.
+-- Grants all privileges for hbnb_test on hbnb_test_db.
+-- Grants SELECT privilege for hbnb_test on performance_schema
 
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER
-    IF NOT EXISTS 'hbnb_dev'@'localhost'
-    IDENTIFIED BY 'hbnb_dev_pwd';
+    IF NOT EXISTS 'hbnb_test'@'localhost'
+    IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES
-   ON `hbnb_dev_db`.*
-   TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+    ON `hbnb_test_db`.*
+    TO 'hbnb_test'@'localhost';
 GRANT SELECT
-   ON `performance_schema`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
+    ON `performance_schema`.*
+    TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
