268abeafd74be938d6bc0f3b612974110637c6e3
FionaG26 <fionamuthoni18@gmail.com>
2023-03-22 08:47:54 -0700

    tasks

--- test_states.py
+++ test_states.py
@@ -1,25 +1,6 @@
 #!/usr/bin/python3
-""" """
-import MySQLdb
 
-# Open database connection
-db = MySQLdb.connect("hostname", "username", "password", "database_name")
+args = 'State name="California"'
 
-# Prepare a cursor object using cursor() method
-cursor = db.cursor()
-
-# Prepare SQL query to get count of records in states table
-sql = "SELECT COUNT(*) FROM states"
-
-try:
-    # Execute the SQL command
-    cursor.execute(sql)
-    # Fetch the result
-    result = cursor.fetchone()
-    # Print the count of records in the states table
-    print("Number of records in the 'states' table:", result[0])
-except:
-    print("Error: Unable to fetch data.")
-
-# Close the database connection
-db.close()
+myclass = args[:args.find(' ')]
+print(myclass)
