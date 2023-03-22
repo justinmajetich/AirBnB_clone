1301bfe77baa3e098e7d2d758a90c8e9ac6f29b6
TessierV <5628@holbertonstudents.com>
2023-03-22 10:13:28 -0700

    pep 8 models and engine db

--- models/state.py
+++ models/state.py
@@ -3,25 +3,23 @@
 from sqlalchemy import Column, String
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
-import os
 
 
 class State(BaseModel, Base):
     """ class State """
     __tablename__ = 'states'
 
-    ##if os.getenv('HBNB_TYPE_STORAGE') == "db":
+    # if os.getenv('HBNB_TYPE_STORAGE') == "db":
     name = Column(String(128), nullable=False)
     cities = relationship('City', cascade='all, delete', backref='state')
-    #else:
+    # else:
 
-        #name = ""
+    # name = ""
     @property
     def cities(self):
         from models import storage
-        from models.city import City
         City_list = []
-        for i in storage.all(City).values():
+        for i in list(storage.all(City).values()):
             if self.id == i.state_id:
                 City_list.append(i)
         return City_list
