a748cd6aa23aab5c0ecfdd073305cf14e670f233
Gabriel <Fossecruor@gmail.com>
2023-03-22 14:18:50 +0100

    remove comment

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -32,8 +32,8 @@ class DBStorage:
         if env == "test":
             Base.metadata.drop_all(self.__engine)
         Base.metadata.create_all(self.__engine)
-        print(f"S: {self.__session}")
-        print(f"E: {self.__engine}")
+        #print(f"S: {self.__session}")
+        #print(f"E: {self.__engine}")
 
     def all(self, cls=None): 
         """ query on the current database session """
