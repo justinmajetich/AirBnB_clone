0b52651195975da07e7f2ac6d2999553c1daa736
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-03-22 00:28:47 +0000

    Modify
    Fix pycodestyle,
    Provides solution to task6

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,5 +1,6 @@
 #!usr/bin/python3
 from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship
 from models.state import State
 from models.user import User
 from models.amenity import Amenity
@@ -8,9 +9,10 @@ from models.review import Review
 from models.place import Place
 import os
 from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from urllib.parse import quote_plus
+from models.base_model import Base
+State.cities = relationship(City, back_populates="states")
+City.states = relationship(State, back_populates="cities")
 
 
 class DBStorage:
@@ -19,23 +21,53 @@ class DBStorage:
     __session = None
 
     def __init__(self):
-        self.__engine = __engine
         self.__session = scoped_session(sessionmaker(bind=self.__engine,
-                                                        expire_on_commit=False))
-        __engine = create_engine('mysql+mysqldb://hbnb_dev:hbnb_dev_pwd@localhost:3306/hbnb_dev_db',
-                                 pool_pre_ping=True)
-        os.environ['HBNB_MYSQL_USER'] = 'hbnb_dev'
-        os.environ['HBNB_MYSQL_PWD'] = 'hbnb_dev_pwd'
-        os.environ['HBNB_MYSQL_HOST'] = 'localhost'
-        os.environ['HBNB_MYSQL_DB'] = 'hbnb_dev_db'
-        if os.environ['HBNB_ENV'] == 'test':
+                                        expire_on_commit=False))
+        HBNB_MYSQL_USER = os.environ['HBNB_MYSQL_USER']
+        HBNB_MYSQL_PWD = os.environ['HBNB_MYSQL_PWD']
+        HBNB_MYSQL_HOST = os.environ['HBNB_MYSQL_HOST']
+        HBNB_MYSQL_DB = os.environ['HBNB_MYSQL_DB']
+        self.__engine = create_engine(f"mysql+mysqldb://"
+                                      f"{quote_plus(HBNB_MYSQL_USER)}"
+                                      f":{quote_plus(HBNB_MYSQL_PWD)}"
+                                      f"@{quote_plus(HBNB_MYSQL_HOST)}"
+                                      f":3306/{quote_plus(HBNB_MYSQL_DB)}",
+                                      pool_pre_ping=True)
+        if os.getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
-    
+
     def all(self, cls=None):
         """Query on the current database session"""
+        dict_obj = {}
         if cls is not None:
-            return self.__session.query(cls).all()
+            all_cls = self.__session.query(cls).all()
+            for obj in all_cls:
+                dict_obj.update({f"{obj.__class__.__name__}.{obj.id}": obj})
         else:
-            return self.__session.query(State).all() + self.__session.query(City).all() + self.__session.query(User).all() + self.__session.query(Place).all() + self.__session.query(Review).all() + self.__session.query(Amenity).all()
-        return dict
-        
+            allState = self.__session.query(State).all()
+            allCity = self.__session.query(City).all()
+            for obj in allState, allCity:
+                for sub_obj in obj:
+                    dict_obj.update({f"{sub_obj.__class__.__name__}"
+                                     f".{sub_obj.id}": sub_obj})
+        return dict_obj
+
+    def new(self, obj):
+        """ Adds the obj to the current database session(self.__session)"""
+        self.__session.add(obj)
+
+    def save(self):
+        """ Commits all changes of the current database session"""
+        self.__session.commit()
+
+    def delete(self, obj):
+        """deletes obj from the current db session if not None"""
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """create all tables in the database"""
+        Base.metadata.create_all(self.__engine)
+        self.__session = scoped_session(sessionmaker(bind=self.__engine,
+                                        expire_on_commit=False))
+        self.__session.commit()
