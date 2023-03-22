4358caefeb83888e11748a343ed2b17d80c94bf7
ezana73 <ezana49@gmail.com>
2023-03-22 07:35:33 -0700

    test_review.py

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -2,28 +2,35 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
+import os
 
 
 class test_review(test_basemodel):
-    """ """
+    """ review test class"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ review class init"""
         super().__init__(*args, **kwargs)
         self.name = "Review"
         self.value = Review
 
     def test_place_id(self):
-        """ """
+        """ testing review place_id attr"""
         new = self.value()
-        self.assertEqual(type(new.place_id), str)
+        self.assertEqual(type(new.place_id), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
     def test_user_id(self):
-        """ """
+        """ testing review user_id attr"""
         new = self.value()
-        self.assertEqual(type(new.user_id), str)
+        self.assertEqual(type(new.user_id), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
     def test_text(self):
-        """ """
+        """ testing review text attr"""
         new = self.value()
-        self.assertEqual(type(new.text), str)
+        self.assertEqual(type(new.text), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
