83d54f33b1a2d4ad7f4ccbddff9e1b47c9b76440
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-03-21 17:03:33 -0700

    fix lattitude and longitude

--- models/place.py
+++ models/place.py
@@ -19,8 +19,8 @@ class Place(BaseModel, Base):
         number_bathrooms = Column(Integer, nullable=False, default=0)
         max_guest = Column(Integer, nullable=False, default=0)
         price_by_night = Column(Integer, nullable=False, default=0)
-        latitude = Column(Float, nullable=False)
-        longitude = Column(Float, nullable=False)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
        
     else:
         city_id = ""
