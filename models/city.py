c4a0f2c219ee24b9d4119e3cfc4b99a5a2eb13d5
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-03-22 00:27:50 +0000

    some minor mods

--- models/city.py
+++ models/city.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from sqlalchemy.orm import declarative_base
 
 
 class City(BaseModel, Base):
