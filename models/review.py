8a982767a623e8cfc6b6040aa4f91ad3684b26de
dannyEnagu <danielEnagu@gmail.com>
2023-03-22 15:07:59 +0000

    Helping Davies fix his code on task nine

--- models/review.py
+++ models/review.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from sqlalchemy import Column, String, ForeignKey
-from models.base_model import BaseModel, Base
-from models import storage_type
+#  from sqlalchemy import Column, String, ForeignKey
+from models.base_model import BaseModel
+#  from models import storage_type
 
-class Review(BaseModel, Base):
+
+class Review(BaseModel):
     """ Review classto store review information."""
-    __tablename__ = 'reviews'
-    if storage_type == 'db':
-        place_id = Column(String(60), nullable=False, ForeignKey("places.id"))
-        user_id = Column(String(60), nullable=False, ForeignKey("users.id"))
-        text = Column(String(1024), nullable=False)
-    else:
-        place_id = ""
-        user_id = ""
-        text = ""
+#    __tablename__ = 'reviews'
+#    if storage_type == 'db':
+#        place_id = Column(String(60), nullable=False, ForeignKey("places.id"))
+#        user_id = Column(String(60), nullable=False, ForeignKey("users.id"))
+#        text = Column(String(1024), nullable=False)
+#    else:
+    place_id = ""
+    user_id = ""
+    text = ""
