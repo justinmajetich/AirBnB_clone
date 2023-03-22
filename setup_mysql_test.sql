872609e91457b9ea777c95ce96988c6031d17ce5
MikeRock51 <mikerockmusic51@gmail.com>
2023-03-22 07:38:45 +0100

    Fix task 4 syntax

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -7,6 +7,6 @@
 -- If the database hbnb_test_db or the user hbnb_test already exists, your script should not fail
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test' @'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db TO 'hbnb_test' @'localhost';
-GRANT SELECT ON performance_schema 'hbnb_test' @'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* 'hbnb_test'@'localhost';
