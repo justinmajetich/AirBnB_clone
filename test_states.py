3b350ae69f314bb52da79151c050790a1c873ac8
FionaG26 <fionamuthoni18@gmail.com>
2023-03-22 09:23:46 -0700

    tasks

--- test_states.py
+++ test_states.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
+# connect to the database
+
+db = MySQLdb.connect(host="HBNB_MYSQL_HOST", user="HBNB_MYSQL_USER", password="HBNB_MYSQL_PWD", db="HBNB_MYSQL_DB")
+
 args = 'State name="California"'
 
 myclass = args[:args.find(' ')]
-print(myclass)
+
+# close the database connection
+db.close()
