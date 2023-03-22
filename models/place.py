4051ab4d773d07be9061d993e4d72a8319b66aba
Redginald Godeau <113888771+RedginaldGodeau@users.noreply.github.com>
2023-03-22 16:02:06 +0100

    Adding import Base

--- models/place.py
+++ models/place.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
 from models.base_model import BaseModel, Base
 from sqlalchemy import ForeignKey
 from sqlalchemy import String
