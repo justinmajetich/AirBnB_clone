a72b4bcefdbe4d9cadd99e6d0c710dd08d93bb8d
Teddy10000 <tedakrong12@gmail.com>
2023-03-22 13:16:25 +0000

    An error fixed

--- models/city.py
+++ models/city.py
@@ -1,107 +1,23 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
+""" City Module for HBNB project """
 import os
-from sqlalchemy import Column, Float, ForeignKey, Integer, String, Table
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 from models.base_model import BaseModel, Base
-from models.review import Review
-from models.amenity import Amenity
 
 
-place_amenity = Table(
-    'place_amenity',
-    Base.metadata,
-    Column(
-        'place_id',
-        String(60),
-        ForeignKey('places.id'),
-        nullable=False,
-        primary_key=True
-    ),
-    Column(
-        'amenity_id',
-        String(60),
-        ForeignKey('amenities.id'),
-        nullable=False,
-        primary_key=True
-    )
-)
-"""Represents the many to many relationship table
-between Place and Amenity records.
-"""
-
-
-class Place(BaseModel, Base):
-    """ A place to stay """
-    __tablename__ = 'places'
-    city_id = Column(
-        String(60), ForeignKey('cities.id'), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    user_id = Column(
-        String(60), ForeignKey('users.id'), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+class City(BaseModel, Base):
+    """ The city class, contains state ID and name """
+    __tablename__ = 'cities'
     name = Column(
         String(128), nullable=False
     ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    description = Column(
-        String(1024), nullable=True
+    state_id = Column(
+        String(60), ForeignKey('states.id'), nullable=False
     ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    number_rooms = Column(
-        Integer, nullable=False, default=0
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else 0
-    number_bathrooms = Column(
-        Integer, nullable=False, default=0
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else 0
-    max_guest = Column(
-        Integer, nullable=False, default=0
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else 0
-    price_by_night = Column(
-        Integer, nullable=False, default=0
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else 0
-    latitude = Column(
-        Float, nullable=True
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else 0.0
-    longitude = Column(
-        Float, nullable=True
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else 0.0
-    amenity_ids = []
-    reviews = relationship(
-        'Review',
-        cascade="all, delete, delete-orphan",
-        backref='place'
+    places = relationship(
+        'Place',
+        cascade='all, delete, delete-orphan',
+        backref='cities'
     ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        amenities = relationship(
-            'Amenity',
-            secondary=place_amenity,
-            viewonly=False,
-            backref='place_amenities'
-        )
-    else:
-        @property
-        def amenities(self):
-            """Returns the amenities of this Place"""
-            from models import storage
-            amenities_of_place = []
-            for value in storage.all(Amenity).values():
-                if value.id in self.amenity_ids:
-                    amenities_of_place.append(value)
-            return amenities_of_place
-
-        @amenities.setter
-        def amenities(self, value):
-            """Adds an amenity to this Place"""
-            if type(value) is Amenity:
-                if value.id not in self.amenity_ids:
-                    self.amenity_ids.append(value.id)
-
-        @property
-        def reviews(self):
-            """Returns the reviews of this Place"""
-            from models import storage
-            reviews_of_place = []
-            for value in storage.all(Review).values():
-                if value.place_id == self.id:
-                    reviews_of_place.append(value)
-            return reviews_of_place
