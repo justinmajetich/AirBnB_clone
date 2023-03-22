bd0069b0e65e9838abb996ddd39e8b84b7457522
ObedRav <obedrayo2356@gmail.com>
2023-03-22 16:13:34 +0000

    Fixing some bugs

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from os import getenv
+from .amenity import Amenity
+from .city import City
+from .place import Place
+from .review import Review
+from .state import State
+from .user import User
 
 env_db = getenv("HBNB_TYPE_STORAGE")
 
--- models/place.py
+++ models/place.py
@@ -8,7 +8,7 @@ from os import getenv
 
 table_associacion = Table("place_amenity", Base.metadata,
                      Column("place_id", String(60), ForeignKey("places.id"), primary_key=True, nullable=False),
-                     Column("amenity_id", String(60), ForeignKey("amenity.id"), primary_key=True, nullable=False))
+                     Column("amenity_id", String(60), ForeignKey("amenities.id"), primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
