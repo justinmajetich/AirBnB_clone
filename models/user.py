ed3e7ec3058ad8b018abc2959202885171e79999
ObedRav <obedrayo2356@gmail.com>
2023-03-22 15:14:00 +0000

    Adding table reviews and relationships

--- models/user.py
+++ models/user.py
@@ -13,3 +13,4 @@ class User(BaseModel, Base):
     first_name = Column(String(128))
     last_name = Column(String(128))
     places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
