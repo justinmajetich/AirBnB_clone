b3967d31077d600df7fb9eecceb9227b0d2b4e50
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-22 15:27:03 -0400

    trying to fix circular import

--- models/user.py
+++ models/user.py
@@ -3,7 +3,6 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import getenv
 
 
 class User(BaseModel, Base):
