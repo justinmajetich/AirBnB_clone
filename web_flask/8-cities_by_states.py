583fe4936f82f7b5a1d5be494f94eacd93157d7a
MaikarfiJesse <j.maikarfi1@alustudent.com>
2023-03-22 10:16:05 +0200

    Cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -8,16 +8,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_route():
     """Renders a template to display all states and their cities."""
     states = storage.all('State')
     return render_template('8-cities_by_states.html', states=states)
 
+
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     """Removes the current SQLAlchemy Session."""
     storage.close()
 
-if __name__ == '__main__':
+if _name_ == "_main_":
     app.run(host='0.0.0.0', port=5000)
