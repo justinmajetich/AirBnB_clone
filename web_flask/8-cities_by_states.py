caa4c352e6f25f3674ddb34e5ea51cd3a51a9482
MaikarfiJesse <j.maikarfi1@alustudent.com>
2023-03-22 10:19:26 +0200

    Cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -8,13 +8,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_route():
     """Renders a template to display all states and their cities."""
     states = storage.all('State')
     return render_template('8-cities_by_states.html', states=states)
-
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
