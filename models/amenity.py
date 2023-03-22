69e125072a48dcbac90fea00a978aab381199f97
henry-onyeka <hmike54@gmail.com>
2023-03-22 23:41:41 +0000

    testing

--- models/amenity.py
+++ models/amenity.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This is the amenity class"""
+""" for us This is the amenity class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
