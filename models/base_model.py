5f971c84b433b2cd8040ab91086ecdf907ad8da9
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-03-22 00:24:45 +0000

    Modify
    This commit:
    creates the declarative_base(), assigns attributes
    that get inherited by subclass
    
    Also, modifies the __str__ method to account for db objects,
    excluding the '_sa_instance_state' property

--- models/base_model.py
+++ models/base_model.py
@@ -2,18 +2,17 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
+Base.id = Column(String(60), nullable=False, primary_key=True)
+Base.created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+Base.updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
 
 class BaseModel:
     """A base class for all hbnb models"""
-    id = Column(Integer(60), nullable=False, primary_key=True)
-    created_at = Column(nullable=False, default=datetime.utcnow())
-    updated_at = Column(nullable=False, default=datetime.utcnow())
-
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
@@ -21,7 +20,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-        else:
+        if kwargs:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
@@ -32,7 +31,10 @@ class BaseModel:
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        parse_dict = self.__dict__
+        if parse_dict.get('_sa_instance_state'):
+            del parse_dict['_sa_instance_state']
+        return '[{}] ({}) {}'.format(cls, self.id, parse_dict)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
@@ -49,6 +51,8 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if dictionary.get('_sa_instance_state'):
+            del dictionary['_sa_instance_state']
         return dictionary
 
     def delete(self):
