ff49f4859d0bcc05ca9c783c28cc0dc6164b6d07
RedginaldGodeau <5657@holbertonstudents.com>
2023-03-22 21:49:18 +0100

    Correction

--- models/base_model.py
+++ models/base_model.py
@@ -11,7 +11,7 @@ Base = declarative_base()
 class BaseModel():
     """A base class for all hbnb models"""
 
-    id = Column(String, primary_key=True, nullable=False)
+    id = Column(String(60), primary_key=True, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
     updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
 
@@ -22,7 +22,6 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -40,6 +39,7 @@ class BaseModel():
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
