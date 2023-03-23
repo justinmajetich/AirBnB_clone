73f1b35e61e01fec82108fae6e3abe5c363da3cd
husnafazal <h.ahmed1@alustudent.com>
2023-03-23 13:27:18 +0300

    PEP8 validation

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-This script starts a Flask web application that listens on port 5000 
-and displays "Hello HBNB!" when you visit the root URL (/) and "HBNB" when you visit the /hbnb route.
+This script starts a Flask web application that listens on port 5000
+and displays "Hello HBNB!" when you visit the root URL
+(/) and "HBNB" when you visit the /hbnb route.
 
 Usage:
     $ python app.py
@@ -16,9 +17,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
