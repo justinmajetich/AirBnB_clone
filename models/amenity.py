3af43d7b965b616f93d9dafb77ee526fe8e43c5f
Bekalu Geta <bekalu2054@gmail.com>
2023-03-23 18:39:57 +0300

    Handle Many to Many relationship with places module

--- models/amenity.py
+++ models/amenity.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-""" Amenity Module """
+""" Amenity Module for HBNB project """
 import os
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+
 from models.base_model import BaseModel, Base
 
 
 class Amenity(BaseModel, Base):
-    '''Represents an amenity data set.'''
-
+    """Represents an amenity data set."""
     __tablename__ = 'amenities'
-
     name = Column(
         String(128), nullable=False
     ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
