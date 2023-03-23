e357de55d84f9f1af9cfd62a3a6dbad1aee60701
TessierV <5628@holbertonstudents.com>
2023-03-23 11:31:25 -0700

    test base model test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -104,3 +104,29 @@ class test_basemodel(unittest.TestCase):
         n = new.to_dict()
         new = BaseModel(**n)
         self.assertFalse(new.created_at == new.updated_at)
+
+    """ part test """
+    def test_init(self):
+        model = BaseModel()
+        self.assertIsInstance(model, BaseModel)
+        self.assertTrue(hasattr(model, "id"))
+        self.assertTrue(hasattr(model, "created_at"))
+        self.assertTrue(hasattr(model, "updated_at"))
+        self.assertIsInstance(model.id, str)
+        self.assertIsInstance(model.created_at, datetime)
+        self.assertIsInstance(model.updated_at, datetime)
+
+    def test_init_with_kwargs(self):
+        now = datetime.utcnow()
+        kwargs = {
+            "id": "123",
+            "created_at": now,
+            "updated_at": now,
+            "test": "test_value"
+        }
+        model = BaseModel(**kwargs)
+        self.assertIsInstance(model, BaseModel)
+        self.assertEqual(model.id, "123")
+        self.assertEqual(model.created_at, now)
+        self.assertEqual(model.updated_at, now)
+        self.assertEqual(model.test, "test_value")
