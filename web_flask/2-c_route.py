9de6ad40024fde67c16c3826ae390eba1a9154ad
husnafazal <h.ahmed1@alustudent.com>
2023-03-23 13:37:54 +0300

    made it excuitable

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/Python3
+
+"""
+This script starts a Flask web application that listens on port 5000 and displays various messages when you visit different routes.
+
+Usage:
+    $ python app.py
+
+Routes:
+    /: display "Hello HBNB!"
+    /hbnb: display "HBNB"
+    /c/<text>: display "C " followed by the value of the text variable (replace underscore _ symbols with a space)
+
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    This function returns a string containing "Hello HBNB!" when called.
+    """
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    This function returns a string containing "HBNB" when called.
+    """
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    
