405701c1196b3346270cb5c4ffcc9d87abdb1036
Redginald Godeau <113888771+RedginaldGodeau@users.noreply.github.com>
2023-03-23 15:22:40 +0100

    change storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -27,7 +27,12 @@ class DBStorage():
         HBNB_MYSQL_DB = os.getenv('HBNB_MYSQL_DB')
 
         self.__engine = create_engine(
-            f"mysql+mysqldb://{HBNB_MYSQL_USER}:{HBNB_MYSQL_PWD}\
+            f"mysql://{HBNB_MYSQL_USER}:{HBNB_MYSQL_PWD}@\
+                {HBNB_MYSQL_HOST}/{HBNB_MYSQL_DB}",
+            pool_pre_ping=True)
+
+        self.__engine = create_engine(
+            f"mysql+pysql://{HBNB_MYSQL_USER}:{HBNB_MYSQL_PWD}\
             @{HBNB_MYSQL_HOST}/{HBNB_MYSQL_DB}",
             pool_pre_ping=True)
 
