7ef277768663ece1c096951e12533494df7d8c83
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-23 14:58:19 -0400

    reuploading original code

--- models/place.py
+++ models/place.py
@@ -57,5 +57,6 @@ class Place(BaseModel, Base):
         @amenities.setter
         def amenities(self, obj):
             """Handles append method for adding an Amenity.id to the attribute amenity_ids"""
+            from models.amenity import Amenity
             if type(obj) == Amenity:
                 self.amenity_ids.append(obj.id)
