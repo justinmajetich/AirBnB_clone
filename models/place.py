b35abc0214cd4e8156ede0ef78acf68bf53e1d74
soniangn <nguyen.sonia@gmail.com>
2023-03-23 05:11:31 -0700

    added overlaps attribute

--- models/place.py
+++ models/place.py
@@ -31,7 +31,7 @@ class Place(BaseModel, Base):
     longitude = Column(Float, nullable=True)
     reviews = relationship("Review", backref="place")
     amenities = relationship('Amenity', secondary=place_amenity,
-                             viewonly=False)
+                             viewonly=False, overlaps="place_amenities")
 
     if os.getenv('HBNB_TYPE_STORAGE') != 'db':
         amenity_ids = []
