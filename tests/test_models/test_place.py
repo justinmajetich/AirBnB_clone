cc5873b5e0dd09b73fb08295db3b2d7be97599c1
soniangn <nguyen.sonia@gmail.com>
2023-03-23 03:00:01 -0700

    added unittest import

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -2,6 +2,7 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.place import Place
+import unittest
 
 
 class test_Place(test_basemodel):
--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -2,6 +2,7 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
+import unittest
 
 
 class test_review(test_basemodel):
