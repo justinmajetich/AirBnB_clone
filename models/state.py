8b397ebbee8671789e3a5e9fb7189ecb36742a15
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-03-23 11:13:16 -0700

    clean code add documentation

--- models/state.py
+++ models/state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""Defines the State class."""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
@@ -9,8 +9,10 @@ from os import getenv
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """Represents a state in the system."""
+
     __tablename__ = "states"
+
     if getenv("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), nullable=False)
         cities = relationship('City', cascade="all, delete", backref="state")
@@ -19,6 +21,7 @@ class State(BaseModel, Base):
 
     @property
     def cities(self):
+        """Returns the list of cities associated with this state."""
         cities_list = []
         for city in models.storage.all(City).values():
             if self.id == city.state_id:
