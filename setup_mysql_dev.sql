ae91dcb1d16f829e07afea7c452dfced8e53b623
Hamida Mahama <mahamahamida@gmail.com>
2023-03-23 00:19:44 +0000

    task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,8 +3,8 @@
 -- set user password
 -- set user to have all priveleges only in this database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_de' @ 'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev' @ 'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_de'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVELEGES;
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVELEGES;
