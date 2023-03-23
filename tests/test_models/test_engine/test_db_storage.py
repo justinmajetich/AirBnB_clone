d32902aa78feb72fce847b42afb90efb339d5a91
Gabriel <Fossecruor@gmail.com>
2023-03-23 20:41:42 +0100

    added one test for db_storage

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -4,8 +4,9 @@ import unittest
 from models.base_model import BaseModel
 from models import storage
 from models.city import City
+from models.user import User
 import os
-
+from MySQLdb import _mysql
 
 class test_dbStorage(unittest.TestCase):
     """ Class to test the db storage method """
@@ -24,5 +25,25 @@ class test_dbStorage(unittest.TestCase):
         self.assertTrue(hasattr(storage, 'delete'))
         self.assertTrue(hasattr(storage, 'reload'))
 
+    def test_models(self):
+        """ test of creating a models user with db """
+        db=_mysql.connect(host="localhost",
+                          port=3306,
+                          user="hbnb_test",
+                          password="hbnb_test_pwd",
+                          database="hbnb_test_db")
+        gab = User(name="Gabriel", password="zizicacamixtape", email="5652@holbertonstudents.com")
+        gab.save()
+        db.query("""SELECT * FROM users""")
+        r=db.store_result()
+        #print(len(r.fetch_row()))
+        self.assertEqual(1, len(r.fetch_row()))
+        sonia = User(name="Sonia", password="devops", email="sonia@holbertonstudents.com")
+        sonia.save()
+        db.query("""SELECT * FROM users""")
+        r=db.store_result()
+        #print(len(r.fetch_row(2,0)))
+        self.assertEqual(2, len(r.fetch_row(2,0)))
+        
 if __name__ == "__main__":
     unittest.main()
