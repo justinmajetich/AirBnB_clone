f76bbb321eac9b3b127a84d89aaab9031c910080
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-03-23 11:28:56 -0700

    add comments amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-""" Amenity Module for HBNB project """
+"""Defines the Amenity class."""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, ForeignKey, Table
 from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """ Amenity class """
+    """Represents an amenity in a property."""
+
     __tablename__ = "amenities"
+
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place", secondary="place_amenity")
