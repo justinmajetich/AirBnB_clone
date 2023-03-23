716f509bf8f3903ac62f84002207a332bdd7947a
husnafazal <h.ahmed1@alustudent.com>
2023-03-23 18:55:30 +0300

    made some changes

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -4,45 +4,38 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Displays Hello HBNB!"""
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Displays HBNB"""
-    return "HBNB"
+    return 'HBNB'
 
 
-@app.route('/c/<text>')
+@app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    """Displays C followed by text variable"""
-    return "C " + text.replace("_", " ")
+    return 'C {}'.format(text.replace('_', ' '))
 
 
-@app.route('/python/')
-@app.route('/python/<text>')
-def python_text(text="is cool"):
-    """Displays Python followed by text variable"""
-    return "Python " + text.replace("_", " ")
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    return 'Python {}'.format(text.replace('_', ' '))
 
 
-@app.route('/number/<int:n>')
+@app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    """Displays n is a number only if n is an integer"""
-    return "{:d} is a number".format(n)
+    return '{} is a number'.format(n)
 
 
-@app.route('/number_template/<int:n>')
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    """Displays HTML page with the value of n"""
     return render_template('5-number.html', n=n)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
