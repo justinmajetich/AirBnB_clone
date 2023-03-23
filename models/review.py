ae23e2787b751a53d74125cc4543e60999aa83b8
Bekalu Geta <bekalu2054@gmail.com>
2023-03-23 18:41:02 +0300

    Add a new feature to interact with DB

--- models/review.py
+++ models/review.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-import os
 from models.base_model import BaseModel, Base
+from os import getenv
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-
 class Review(BaseModel, Base):
     """ Review classto store review information """
-    __tablename__ = "reviews"
+    __tablename__ = 'reviews'
     place_id = Column(
-        String(60), ForeignKey('places.id'), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+        String(60),
+        ForeignKey('places.id'),
+        nullable=False,
+    ) if getenv('HBNB_TYPE_STORAGE') == 'db' else ''
     user_id = Column(
-        String(60), ForeignKey('users.id'), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+        String(60),
+        ForeignKey('users.id'),
+        nullable=False,
+    ) if getenv('HBNB_TYPE_STORAGE') == 'db' else ''
     text = Column(
-        String(1024), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+        String(1024),
+        nullable=False,
+    ) if getenv('HBNB_TYPE_STORAGE') == 'db' else ''
