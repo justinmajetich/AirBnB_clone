a6a6fddabcf346c15915aa3880d7d3e1717fe960
Bekalu Geta <bekalu2054@gmail.com>
2023-03-23 18:29:13 +0300

    Modify the class to inherit Base table and work with db

--- models/city.py
+++ models/city.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-import os
+"""This module contains the City class"""
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
+
+from os import getenv
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+    """This class defines a City by various attributes"""
     __tablename__ = 'cities'
-    state_id = Column(
-        String(60), ForeignKey('states.id'), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
     name = Column(
-        String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+        String(128),
+        nullable=False
+        ) if getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    state_id = Column(
+        String(60),
+        ForeignKey('states.id'),
+        nullable=False
+        ) if getenv('HBNB_TYPE_STORAGE') == 'db' else ''
     places = relationship(
         'Place',
         cascade='all, delete, delete-orphan',
         backref='cities'
-    ) if os.getenv('HBNB_TYPE_DB') == 'db' else None
+    ) if getenv('HBNB_TYPE_STORAGE') == 'db' else None
