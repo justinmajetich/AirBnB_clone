b81d2767beff7580a846c0e4013dad846810d187
husnafazal <h.ahmed1@alustudent.com>
2023-03-23 16:30:00 +0300

    PEP8 validation

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -4,12 +4,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """
     This function returns a string containing "Hello HBNB!" when called.
     """
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
@@ -18,20 +20,25 @@ def hbnb():
     """
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     """
-    This function returns a string containing "C " followed by the value of the text variable with underscores replaced with spaces when called.
+    This function returns a string containing "C " followed by the value 
+    of the text variable with underscores replaced with spaces when called.
     """
     return 'C {}'.format(text.replace('_', ' '))
+
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text):
     """
-    This function returns a string containing "Python " followed by the value of the text variable with underscores replaced with spaces when called.
+    This function returns a string containing "Python " followed by
+      the value of the text variable with underscores replaced with spaces when called.
     """
     return 'Python {}'.format(text.replace('_', ' '))
+
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
