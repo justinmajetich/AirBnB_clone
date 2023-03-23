dc47d1efd4959dad7b132f7cf5971e4723505409
NuelUzoma <emcool17@gmail.com>
2023-03-23 05:06:08 +0100

    Unittests for DBStorage and MySQL using MySQLdb

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -5,20 +5,20 @@ from models.city import City
 
 
 class test_City(test_basemodel):
-    """ """
+    """ Tests for the class City"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ Initialization of the test City"""
         super().__init__(*args, **kwargs)
         self.name = "City"
         self.value = City
 
     def test_state_id(self):
-        """ """
+        """ Test to check for the state id"""
         new = self.value()
         self.assertEqual(type(new.state_id), str)
 
     def test_name(self):
-        """ """
+        """ Test to check for the City name"""
         new = self.value()
         self.assertEqual(type(new.name), str)
---
+++ tests/test_mysql.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+""""Module for testing MySQLdb"""
+import unittest
+import mysql.connector
+import os
+
+HBNB_MYSQL_USER = os.environ('HBNB_MYSQL_USER')
+HBNB_MYSQL_PWD = os.environ('HBNB_MYSQL_PWD')
+HBNB_MYSQL_HOST = os.environ('HBNB_MYSQL_HOST')
+HBNB_MYSQL_DB = os.environ('HBNB_MYSQL_DB')
+
+
+class TestMySQL(unittest.TestCase):
+    """The MySQLdb test class"""
+
+    def setUp(self):
+        """Set up for the tests"""
+        self.connection = mysql.connector.connect(
+            user=HBNB_MYSQL_USER,
+            password=HBNB_MYSQL_PWD,
+            host=HBNB_MYSQL_HOST,
+            database=HBNB_MYSQL_DB
+        )
+
+    def tearDown(self):
+        """Tear down for the tests"""
+        self.connection.close()
+
+    def test_connection(self):
+        """Test the connection"""
+        self.assertTrue(self.connection.is_connected())
+
+    def test_cursor(self):
+        """Test the cursor"""
+        cursor = self.connection.cursor()
+        self.assertTrue(cursor)
+
+    def test_select(self):
+        """Test the select"""
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM states")
+        result = cursor.fetchall()
+        self.assertTrue(result)
+
+    def test_insert(self):
+        """Test the insert"""
+        cursor = self.connection.cursor()
+        cursor.execute("INSERT INTO states (name) VALUES ('California')")
+        self.connection.commit()
+        cursor.execute("SELECT * FROM states WHERE name='California'")
+        result = cursor.fetchall()
+        self.assertTrue(result)
+
+    def test_delete(self):
+        """Test the delete"""
+        cursor = self.connection.cursor()
+        cursor.execute("INSERT INTO states (name) VALUES ('California')")
+        self.connection.commit()
+        cursor.execute("DELETE FROM states WHERE name='California'")
+        self.connection.commit()
+        cursor.execute("SELECT * FROM states WHERE name='California'")
+        result = cursor.fetchall()
+        self.assertFalse(result)
+
+    def test_update(self):
+        """Test the update"""
+        cursor = self.connection.cursor()
+        cursor.execute("INSERT INTO states (name) VALUES ('California')")
+        self.connection.commit()
+        cursor.execute("UPDATE states SET name='New York' "
+                       "WHERE name='California'")
+        self.connection.commit()
+        cursor.execute("SELECT * FROM states WHERE name='New York'")
+        result = cursor.fetchall()
+        self.assertTrue(result)
+
+    def test_select_all(self):
+        """Test the select all"""
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM states")
+        result = cursor.fetchall()
+        self.assertTrue(result)
