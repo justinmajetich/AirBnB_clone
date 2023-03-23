fbb089f24c070f87d199fa53d17fe311816fe67c
Redginald Godeau <113888771+RedginaldGodeau@users.noreply.github.com>
2023-03-23 15:44:16 +0100

    init

--- models/__init__.py
+++ models/__init__.py
@@ -5,8 +5,11 @@ import os
 if os.getenv('HBNB_TYPE_STORAGE') == "db":
     from models.engine.db_storage import DBStorage
 
+    print("Inner DB")
+
     storage = DBStorage()
     storage.reload()
+    print(storage.all())
 else:
     from models.engine.file_storage import FileStorage
 
