aed65154fb661a3d9eea24b307596663a562fefc
soO <444deph12@gmail.com>
2023-03-23 11:00:40 -0700

    small change to amenity

--- models/amenity.py
+++ models/amenity.py
@@ -3,9 +3,14 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, ForeignKey, Table
 from sqlalchemy.orm import relationship
+from os import getenv
+
 
 class Amenity(BaseModel, Base):
     """ Amenity class """
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity")
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "amenities"
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary="place_amenity")
+    else:
+        name = ""
--- models/place.py
+++ models/place.py
@@ -11,11 +11,12 @@ import models
 if getenv('HBNB_TYPE_STORAGE') == 'db':
     place_amenity_table = Table("place_amenity", Base.metadata,
                                 Column("place_id", String(60),
-                                    ForeignKey("places.id"),
-                                    primary_key=True, nullable=False),
+                                       ForeignKey("places.id"),
+                                       primary_key=True, nullable=False),
                                 Column("amenity_id", String(60),
-                                    ForeignKey("amenities.id"),
-                                    primary_key=True, nullable=False))
+                                       ForeignKey("amenities.id"),
+                                       primary_key=True, nullable=False))
+
 
 class Place(BaseModel, Base):
     """ A place to stay """
