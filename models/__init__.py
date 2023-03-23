0279a016385972c770e02566aac5b9808aa8a73d
Bekalu Geta <bekalu2054@gmail.com>
2023-03-23 20:19:47 +0300

    Import all model module for ease handling

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""This module instantiates an object of class FileStorage or DBStorage"""
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.place import Place
 
 from os import getenv
 
