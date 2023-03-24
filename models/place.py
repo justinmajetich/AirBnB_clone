c0f9a57bbd6e13f4d7ee4cbba655de425d24db22
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-24 00:30:16 -0400

    forgetting pycodestyle

--- models/place.py
+++ models/place.py
@@ -6,16 +6,9 @@ from sqlalchemy.orm import relationship
 from os import getenv
 
 
-place_amenity = Table(
-    'place_amenity', Base.metadata,
-    Column(
-        'place_id', String(60), ForeignKey('places.id'),
-        primary_key=True, nullable=False
-    ),
-    Column(
-        'amenity_id', String(60), ForeignKey('amenities.id'),
-        primary_key=True, nullable=False
-    )
+place_amenity = Table('place_amenity', Base.metadata,
+    Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+    Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)
 )
 
 
