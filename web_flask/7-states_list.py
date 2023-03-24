f8368605b9caddc0f87288efb00672a09af9497e
d3ng4 <denggi2000@gmail.com>
2023-03-24 16:11:01 +0000

    commit

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Flask routing template"""
+""" Flask Routing Template """
 from flask import Flask
 from flask import render_template
 from models import storage
@@ -7,14 +7,18 @@ from models.state import State
 
 app = Flask(__name__)
 
+
 @app.route('/states_list',  strict_slashes=False)
 def display_states_list():
     """ retrieve list of states and passes to template"""
+    # states_list = storage.all("State")
     states_list = []
     dic_states = storage.all(State)
     for state in dic_states.values():
+        # for city in dic_cities.values():
         states_list.append(state)
     return render_template('7-states_list.html', states=states_list)
+
 
 @app.teardown_appcontext
 def teardown_db(exception):
