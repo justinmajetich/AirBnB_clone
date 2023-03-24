69a470cd043598b5342af2da4a1de69862f4aca5
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-03-24 06:50:12 -0700

    update tests city and state with getenv

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -2,6 +2,7 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
+from os import getenv
 
 
 class test_state(test_basemodel):
@@ -16,4 +17,7 @@ class test_state(test_basemodel):
     def test_name3(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.name), str)
+        if getenv('HBNB_TYPE_STORAGE') != 'db':
+            self.assertEqual(type(new.name), str)
+        else:
+            self.assertNotEqual(type(new.name), str)
