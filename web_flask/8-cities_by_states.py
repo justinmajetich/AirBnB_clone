28e4f5dadea206c1dabfca35e50681eee6aed977
Cecilia-Banda <c.banda@alustudent.com>
2023-03-24 19:01:49 +0300

    states by state template

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Script starting a Flask web application """
+""" Script starting a Flask web application  """
 
 from flask import Flask, render_template
 from models.state import State
