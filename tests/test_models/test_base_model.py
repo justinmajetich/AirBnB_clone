c87901774a90073fa921558f14264906fb23b2dc
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-03-24 07:35:36 -0700

    update test update

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -93,7 +93,6 @@ class test_basemodel(unittest.TestCase):
     def test_updated_at(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.updated_at), datetime.datetime)
-        n = new.to_dict()
-        new = BaseModel(**n)
+        time.sleep(0.1)
+        new.save()
         self.assertFalse(new.created_at == new.updated_at)
