f7e0068b5b54c498227eade9812f13f3490fa582
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-23 22:25:52 -0400

    adding test for amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+"""Test for Amenity class"""
+import unittest
 from models.amenity import Amenity
 
 
-class test_Amenity(test_basemodel):
-    """ """
+class TestAmenity(unittest.TestCase):
+    def setUp(self):
+        self.amenity = Amenity()
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "Amenity"
-        self.value = Amenity
+    def test_attributes(self):
+        self.assertTrue(hasattr(self.amenity, "name"))
+        self.assertEqual(self.amenity.name, None)
 
-    def test_name2(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+
+if __name__ == '__main__':
+    unittest.main()
