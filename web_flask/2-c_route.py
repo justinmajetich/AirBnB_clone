f166247e82ecea9e34329348e1b7af906f846f35
linszay <lindseyjlancaster@gmail.com>
2023-03-25 01:52:59 -0500

    starts a flask web app

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,24 @@
+#!usr/bin/python3
+"""script starts a flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', methods=['GET'], strict_slashes=False)
+def c(text):
+    return "C {}".format(text.replace("_", " "))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
