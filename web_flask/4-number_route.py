4b3a4544b51ba796a585191f4ef7cf0176b63fa7
linszay <lindseyjlancaster@gmail.com>
2023-03-25 05:18:02 -0500

    added methods=[GET] to my files

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,35 +1,35 @@
-#!usr/bin/python3
-"""script starts a flask web application"""
-
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    return 'Hello HBNB!'
+@app.route('/', methods=['GET'], strict_slashes=False)
+def holla_hbnb():
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
+@app.route('/hbnb', methods=['GET'], strict_slashes=False)
+def hbnbbb():
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route('/c/<text>', methods=['GET'], strict_slashes=False)
 def cisfun(text):
     return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python', methods=['GET'], strict_slashes=False)
+@app.route('/python/<text>', methods=['GET'], strict_slashes=False)
 def python(text='is cool'):
     return "Python {}".format(text.replace("_", " "))
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
+@app.route('/number/<int:n>', methods=['GET'], strict_slashes=False)
 def number(n):
     return '{} is a number'.format(n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
