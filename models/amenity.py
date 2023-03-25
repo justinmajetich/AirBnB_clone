b444f3dd3f6b160b9856b0310f6cd609e739b957
linszay <lindseyjlancaster@gmail.com>
2023-03-25 06:08:02 -0500

    changed from lowercase to uppercase

--- models/amenity.py
+++ models/amenity.py
@@ -4,9 +4,16 @@ from models.base_model import BaseModel, Base
 from models.place import place_amenity
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
+
+duke = getenv('HBNB_TYPE_STORAGE')
 
 
 class Amenity(BaseModel, Base):
-    __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship('Place', secondary=place_amenity)
+    if duke == "db":
+        from models.place import place_amenity
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship('Place', secondary=place_amenity)
+    else:
+        name = ''
