47a9802bf701fa62a39f801605ded904b598d132
linszay <lindseyjlancaster@gmail.com>
2023-03-25 08:18:40 -0500

    updated state.py

--- models/state.py
+++ models/state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 import models
+from models import storage
 from models.base_model import BaseModel, Base
 from models.city import City
 from sqlalchemy import Column, String
