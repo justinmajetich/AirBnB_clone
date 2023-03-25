e2d6cd53042cdefc49c965c47cdb0baa2710a107
linszay <lindseyjlancaster@gmail.com>
2023-03-25 04:45:14 -0500

    updated file to re-check

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,17 +1,17 @@
-#!usr/bin/python3
-"""script starts a flask web application"""
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
+@app.route('/', methods=['GET'], strict_slashes=False)
+def holla_hbnb():
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
+@app.route('/hbnb', methods=['GET'], strict_slashes=False)
+def hbnbbb():
     return "HBNB"
 
 
