5e4975775d69312612930e4a7bf9eaf266d0a8e5
linszay <lindseyjlancaster@gmail.com>
2023-03-25 04:47:14 -0500

    added method to line 18

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,24 +1,24 @@
-#!usr/bin/python3
-"""script starts a flask web application"""
-
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    return 'Hello HBNB!'
+@app.route('/', methods=['GET'], strict_slashes=False)
+def holla_hbnb():
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
+@app.route('/hbnb', methods=['GET'], strict_slashes=False)
+def hbnbbb():
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route('/c/<text>', methods=['GET'], strict_slashes=False)
 def cisfun(text):
     return "C {}".format(text.replace("_", " "))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
