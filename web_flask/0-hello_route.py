9268a40b8f99d7c45af3ad8404c212765e96a569
linszay <lindseyjlancaster@gmail.com>
2023-03-24 22:22:18 -0500

    script starts a flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+# usr/bin/env python3
+"""script starts a flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
