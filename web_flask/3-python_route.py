6950d03f4472c11a2d4d0d7b717b1be6827402d5
linszay <lindseyjlancaster@gmail.com>
2023-03-25 02:18:33 -0500

    updated files and added file 4

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -17,8 +17,14 @@ def hbnb():
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c(text):
+def cisfun(text):
     return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    return "Python {}".format(text.replace("_", " "))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -20,5 +20,16 @@ def hbnb():
 def cisfun(text):
     return "C {}".format(text.replace("_", " "))
 
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return '{} is a number'.format(n)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
