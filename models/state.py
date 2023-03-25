5d9c9561f2cfddee799a59f8cc108cdef19f154d
linszay <lindseyjlancaster@gmail.com>
2023-03-25 08:19:41 -0500

    reverted changes

--- models/state.py
+++ models/state.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 import models
-from models import storage
 from models.base_model import BaseModel, Base
 from models.city import City
 from sqlalchemy import Column, String
