1380484d7d8002e209ade17326b7f13c872872ce
linszay <lindseyjlancaster@gmail.com>
2023-03-25 02:01:34 -0500

    added methods to line 9

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,7 +6,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/', methods=['GET'], strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB!'
 
