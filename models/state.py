1bf5d9211b96d556fbc946be985e01562c8feb87
linszay <lindseyjlancaster@gmail.com>
2023-03-25 07:55:14 -0500

    updated states and added templates file for 7

--- models/state.py
+++ models/state.py
@@ -6,9 +6,26 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
 
+duke = getenv('HBNB_TYPE_STORAGE')
+
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="delete")
+    if duke == 'db':
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade="delete")
+    else:
+        name = ''
+
+        @property
+        def cities(self):
+            """
+                getter method, returns list of City objs from storage
+                linked to the current State
+            """
+            city_list = []
+            for city in models.storage.all("City").values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
