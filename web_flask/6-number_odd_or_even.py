86d930009a6baeb648e2d5a654a73fe7f52aff27
MicoBledsoe <micomods@gmail.com>
2023-03-27 14:44:29 -0500

    .

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -3,53 +3,42 @@
 starts a Flask web application
 """
 from flask import Flask, render_template
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-
 @app.route('/')
 def hello_hbnb():
-    """ returns 'Hello HBNB!'"""
-    return 'Hello HBNB!'
-
+    return "Hello HBNB!"
 
 @app.route('/hbnb')
 def hbnb():
-    """ returns 'HBNB' """
-    return 'HBNB'
-
+    return "HBNB"
 
 @app.route('/c/<text>')
-def c(text):
-    """ returns 'C' followed by the text variable """
-    return "C " + text.replace('_', ' ')
-
+def c_text(text):
+    return "C {}".format(text.replace('_', ' '))
 
 @app.route('/python/')
 @app.route('/python/<text>')
-def python(text='is cool'):
-    """ returns 'Python' followed by the text variable """
-    return "Python " + text.replace('_', ' ')
-
+def python_text(text="is cool"):
+    return "Python {}".format(text.replace('_', ' '))
 
 @app.route('/number/<int:n>')
-def number(n):
-    """ displays "n is a number" only if n is an integer """
-    return "{:d} is a number".format(n)
-
+def text_if_int(n):
+    return "{} is a number".format(n)
 
 @app.route('/number_template/<int:n>')
-def number_template(n):
-    """ displays an HTML page if n is an integer """
+def html_if_int(n):
     return render_template('5-number.html', n=n)
 
-
 @app.route('/number_odd_or_even/<int:n>')
-def number_odd_or_even(n):
-    """ displays an HTML page if n is an integer indicating if it is odd or even"""
-    return render_template('6-number_odd_or_even.html', n=n)
+def html_odd_or_even(n):
+    if n % 2 == 0:
+        num_type = 'even'
+    else:
+        num_type = 'odd'
+    return render_template('6-number_odd_or_even.html', n=n, num_type=num_type)
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
