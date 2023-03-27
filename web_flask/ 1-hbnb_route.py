62109f358059975b95c0f912c987a98fa0e7580b
MicoBledsoe <micomods@gmail.com>
2023-03-27 14:13:29 -0500

    up

--- web_flask/ 1-hbnb_route.py
+++ web_flask/ 1-hbnb_route.py
@@ -14,5 +14,11 @@ def hello():
     return 'Hello HBNB!'
 
 
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """displays HBNB"""
+    return 'HBNB'
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
