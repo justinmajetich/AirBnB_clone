837a65cc363c90b5e00ac643aefe4b34f36023c2
MicoBledsoe <micomods@gmail.com>
2023-03-27 15:56:20 -0500

    .

--- models/state.py
+++ models/state.py
@@ -4,7 +4,7 @@ from os import getenv
 import models
 from models.base_model import BaseModel, Base
 from models.city import City
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
@@ -12,11 +12,8 @@ class State(BaseModel, Base):
     """State class that creates states table"""
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", backref="state", cascade="all, \
-                              delete-orphan")
-
+        cities = relationship("City", backref="state", cascade="all, delete-orphan")
     else:
         @property
         def cities(self):
@@ -26,3 +23,13 @@ class State(BaseModel, Base):
                 if city.state_id == self.id:
                     citiesList.append(city)
             return citiesList
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Getter attribute cities that returns the list of City instances"""
+            citiesList = []
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    citiesList.append(city)
+            return citiesList
+        
