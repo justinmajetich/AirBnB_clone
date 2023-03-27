36d56ce4cae26772e0079c75f92c253306cd685c
MicoBledsoe <micomods@gmail.com>
2023-03-27 16:29:13 -0500

    fix

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,24 +1,28 @@
 #!/usr/bin/python3
-"""Start a Flask web application"""
+""" Script that runs an app with Flask framework """
+from flask import Flask, render_template
 from models import storage
 from models.state import State
 from models.city import City
-from flask import Flask, render_template
+
+
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Close the storage engine"""
+def teardown_session(exception):
+    """ Teardown """
     storage.close()
 
 
 @app.route('/cities_by_states', strict_slashes=False)
-def display_states():
-    """Display a HTML page"""
+def display_html():
+    """ Function called with /states_list route """
     states = storage.all(State)
-    return render_template('8-cities_by_states.html', states=states)
+    return render_template('8-cities_by_states.html',
+                           Table="States",
+                           states=states)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
