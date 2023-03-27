0453e0f715f25c9b446934c9f1e60e79067fa870
timidgeek <5568@holbertonstudents.com>
2023-03-27 16:11:21 -0700

    pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,9 +3,10 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def index():
     return "Hello HBNB!"
 
 if __name__ == "__main__":
-    app.run(host ="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
