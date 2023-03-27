b2d42070c40d5931bda05a76e8b8648b5f47900f
MicoBledsoe <micomods@gmail.com>
2023-03-27 14:36:08 -0500

    [fix]

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -3,65 +3,50 @@
 Starts a Flask web application
 """
 from flask import Flask, render_template
-
-
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
-    """
-    Displays 'Hello HBNB!'
-    """
+    """Displays 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
-    """
-    Displays 'HBNB'
-    """
+    """Displays 'HBNB'"""
     return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    """
-    Displays 'C <text>'
-    """
+@app.route('/c/<text>')
+def c(text):
+    """Displays 'C ' followed by the value of the text variable"""
     return 'C {}'.format(text.replace('_', ' '))
 
 
-@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_text(text):
-    """
-    Displays 'Python <text>'
-    """
+@app.route('/python/')
+@app.route('/python/<text>')
+def python(text='is cool'):
+    """Displays 'Python ' followed by the value of the text variable"""
     return 'Python {}'.format(text.replace('_', ' '))
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
+@app.route('/number/<int:n>')
 def number(n):
-    """
-    Displays '<n> is a number' only if n is an integer
-    """
+    """Displays 'n is a number' only if n is an integer"""
     return '{} is a number'.format(n)
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
+@app.route('/number_template/<int:n>')
 def number_template(n):
-    """
-    Displays a HTML page only if n is an integer
-    """
+    """Displays a HTML page only if n is an integer"""
     return render_template('5-number.html', n=n)
 
 
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+@app.route('/number_odd_or_even/<int:n>')
 def number_odd_or_even(n):
-    """
-    Displays a HTML page only if n is an integer
-    """
+    """Displays a HTML page only if n is an integer"""
     return render_template('6-number_odd_or_even.html', n=n)
 
 
