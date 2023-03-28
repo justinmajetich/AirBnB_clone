7d14ce506b2b1b8f635e540c8a87cbfd12838691
Ahmed Mubarak <ahmed.elia2003@gmail.com>
2023-03-28 19:34:50 +0200

    mysql scripts -- autopep8 py files

--- models/state.py
+++ models/state.py
@@ -6,6 +6,7 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """ State class """
     name = ""
+
     def __init__(self, *args, **kwargs):
         """ Set up an instance with its properties. """
         super().__init__(*args, **kwargs)
--- models/user.py
+++ models/user.py
@@ -9,6 +9,7 @@ class User(BaseModel):
     password = ''
     first_name = ''
     last_name = ''
+
     def __init__(self, *args, **kwargs):
         """ Set up an instance with its properties. """
         super().__init__(*args, **kwargs)
