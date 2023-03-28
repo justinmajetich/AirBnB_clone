de725aa8640fb992625dff17d747a67452730528
timidgeek <5568@holbertonstudents.com>
2023-03-27 17:52:11 -0700

    rookie mistake

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -37,10 +37,5 @@ def tumblr(n):
         return render_template("5-number.html", n=n)
 
 
-@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
-def even_odd(n):
-    if isinstance(n, int):
-        return render_template("5-number.html", n=n)
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
