91b873c526b5ed47c59356bbe2305f592bf8a795
timidgeek <5568@holbertonstudents.com>
2023-03-27 17:53:40 -0700

    pycodestyle, linked wrong html page lol

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -40,7 +40,8 @@ def tumblr(n):
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def even_odd(n):
     if isinstance(n, int):
-        return render_template("5-number.html", n=n)
+        return render_template("6-number_odd_or_even.html", n=n)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
