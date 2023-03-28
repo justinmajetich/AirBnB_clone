ce377c4cb154f057d1bb551f2dfd6b293b9871a4
timidgeek <5568@holbertonstudents.com>
2023-03-27 17:48:41 -0700

    html goodies

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+from flask import Flask, abort, render_template
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def extra_steps():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def simple():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def icky(text):
+    return "C " + text.replace("_", " ")
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def monty(text):
+    return "Python " + text.replace("_", " ")
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def pretty_ricky(n):
+    if isinstance(n, int):
+        return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def tumblr(n):
+    if isinstance(n, int):
+        return render_template("5-number.html", n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def even_odd(n):
+    if isinstance(n, int):
+        return render_template("5-number.html", n=n)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
