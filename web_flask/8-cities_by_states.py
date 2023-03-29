7fe891be8a9baaabceb2d8cb3eee73ee09e528ec
timidgeek <5568@holbertonstudents.com>
2023-03-28 18:27:50 -0700

    pep8?

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -5,10 +5,10 @@ from flask import render_template, Flask
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
     states = storage.all('State')
-    return render_template("7-states_list.html", states=states)
+    return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
---
+++ web_flask/9-states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+from models import storage
+from flask import render_template, Flask
+app = Flask(__name__)
+
+
+@app.route("/states", strict_slashes=False)
+def states():
+    states = storage.all("State")
+    return render_template("9-states.html", state=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
