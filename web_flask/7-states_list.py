ee4f54ba270e2aeddaabcf0fb5596f421878b828
linszay <lindseyjlancaster@gmail.com>
2023-03-28 22:49:10 -0500

    updated states_list method

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -13,8 +13,8 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Displays a list of all the states and their cities."""
-    states = storage.all(State).values()
-    return render_template('7-states_list.html', states)
+    states = storage.all('State')
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
