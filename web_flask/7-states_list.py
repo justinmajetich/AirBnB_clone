a28e2994499766c507b38d7e738302a5bd2a842c
timidgeek <5568@holbertonstudents.com>
2023-03-28 19:30:05 -0700

    does this make a difference....

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Script that starts a Flask web application listening on 0.0.0.0:5000
+"""
+Script that starts a Flask web application listening on 0.0.0.0:5000
 Use storage for fetching data from the storage engine File or DB storage
 from models import storage and storage.all(...)
 After each request, remove current SQLAlchemy Session:
@@ -9,7 +10,8 @@ Route /states_list: display a HTML page
     H1 tag: "States"
     UL tag: list of all State objects present in DBStorage sorted by name
         LI tag: Description of one State: <state.id>: <<B><state.name></B>
-Must use option strict_slashes=False"""
+Must use option strict_slashes=False
+"""
 from models import storage
 from models.state import State
 from flask import render_template, Flask
@@ -19,14 +21,18 @@ app.url_map.strict_slashes = False
 
 @app.route("/states_list", strict_slashes=False)
 def state_list():
-    """Displays HTML formatted list of states from DB"""
+    """
+    Displays HTML formatted list of states from DB
+    """
     states = storage.all(State)
     return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
 def teardown(exc):
-    """Remove current SQLALchemy sesh"""
+    """
+    Remove current SQLALchemy sesh
+    """
     storage.close()
 
 
