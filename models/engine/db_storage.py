c836b90e527884c1c9a7c17df02dca1abf07a120
timidgeek <5568@holbertonstudents.com>
2023-03-29 07:51:46 -0700

    frickin dunder

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -75,7 +75,7 @@ class DBStorage():
         Base.metadata.create_all(self.__engine)
         my_session = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(my_session)
-        self.session = Session()
+        self.__session = Session()
 
     def close(self):
         """Closes a session"""
