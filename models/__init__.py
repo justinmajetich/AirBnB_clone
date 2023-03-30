76ddb1f5efa27a6b251df23ddfe628556533365e
Redginald Godeau <113888771+RedginaldGodeau@users.noreply.github.com>
2023-03-30 21:23:28 +0200

    adding typestorage in state

--- models/__init__.py
+++ models/__init__.py
@@ -4,14 +4,9 @@ import os
 
 if os.getenv('HBNB_TYPE_STORAGE') == "db":
     from models.engine.db_storage import DBStorage
-
-    print("Inner DB")
-
     storage = DBStorage()
-    storage.reload()
-    print(storage.all())
 else:
     from models.engine.file_storage import FileStorage
-
     storage = FileStorage()
-    storage.reload()
+storage.reload()
+
