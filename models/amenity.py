67c5fcc659684b303bf7f7cb16baeafc9899231f
Bantamlak <neoluka315@gmail.com>
2023-04-02 20:26:41 +0000

    amenity added to the database, M-T-M

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """amenity in the database"""
+    __tablename__ = "amenities"
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary="place_amenity")
+    else:
+        name = ""
