ff94c11e33d9122cf7e9117f7ca8064d1ec8a840
GABETROLL <gabriel7icplhappy@gmail.com>
2023-04-03 13:01:46 -0400

    run the website

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,10 +2,15 @@
 """
 Flask website with a single paragraph
 that says "Hello, HBNB!".
+
+When this script is the main script,
+the website is ran and hosted in
+"0.0.0.0" in port 5000.
 """
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
@@ -14,3 +19,7 @@ def hello_hbnb():
     in it.
     """
     return "<p>Hello HBNB!</p>"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
