d250dd46f459034e5eb9efed9fd43206352d09b6
Valentin <5619@holbertonstudents.com>
2023-04-03 02:29:19 -0700

    0. commentaire

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,11 +10,13 @@ app = Flask(__name__)
 # Définir une route pour l'URL racine '/'
 @app.route("/")
 def hello():
-    strict_slashes = False # Désactiver la redirection automatique vers une URL avec une barre oblique à la fin
-    return 'Hello HBNB!' # Retourner une chaîne de caractères en réponse à la requête HTTP
+    # Désac la redirection auto vers une URL avec une barre oblique à la fin
+    strict_slashes = False
+    # Retourner une chaîne de caractères en réponse à la requête HTTP
+    return 'Hello HBNB!'
 
 
 # Exécuter l'application si le script est exécuté directement (pas importé)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True) # Lancer l'application sur l'adresse IP 0.0.0.0 et le port 5000
-
+    # Lancer l'application sur l'adresse IP 0.0.0.0 et le port 5000
+    app.run(host='0.0.0.0', port=5000, debug=True)
