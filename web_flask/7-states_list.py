9684174c86b6a47f25c6ca428e60a60da67d1b42
Gabriel <Fossecruor@gmail.com>
2023-04-03 15:19:27 +0200

    push to test

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" A script that starts a Flask web application with storage """
+from models import storage
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/states_list",strict_slashes=False)
+def listing():
+    from models.state import State 
+
+    State_item = storage.all(State)
+    return render_template("7-states_list.html", state_item=State_item)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    from models.engine.db_storage import DBStorage 
+    if isinstance(storage, DBStorage):
+        storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
