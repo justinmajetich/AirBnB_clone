e1724ede7fbab7c3beb35a8130d33f20b8af0cff
Gabriel <Fossecruor@gmail.com>
2023-04-03 13:01:01 +0200

    added content for file 6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+""" a script that starts a Flask web application """
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def display():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    if "_" in text:
+        text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def python(text):
+    if "_" in text:
+        text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+@app.route("/python", strict_slashes=False)
+def python_default():
+    return f"Python is cool"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_only(n):
+    return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """ using template for the first time """
+    return render_template("5-number.html", n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd(n):
+    """ using template for the first time """
+    return render_template("6-number_odd_or_even.html", n=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
