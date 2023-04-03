21d76ff28fe9cb5d72939c1efc03bc9b2333f570
Gabriel <Fossecruor@gmail.com>
2023-04-03 11:04:45 +0200

    Added content for file 1

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def hello_world():
     return "<p>Hello, world</p>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
