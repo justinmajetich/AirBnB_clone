f3f5b09851b78f871c0c63c669bb352c0936819e
Valentin <5619@holbertonstudents.com>
2023-04-03 02:05:58 -0700

    0. connect flask

---
+++ web_flask/0_hello_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    strict_slashes=False
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=3000, debug=True)
