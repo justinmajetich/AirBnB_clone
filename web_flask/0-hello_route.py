b8b8d7a5312ca1e42eeb4dc091363340d490d63f
Valentin <5619@holbertonstudents.com>
2023-04-03 02:23:17 -0700

    0. wrong port

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,10 +5,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def hello():
-    strict_slashes=False
+    strict_slashes = False
     return 'Hello HBNB!'
 
+
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=3000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
