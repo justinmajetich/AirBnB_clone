d76fb5fc04bfe4b81c2ca3f2b3060ab3e0abc8aa
soO <444deph12@gmail.com>
2023-04-04 09:11:09 -0700

    add of task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -71,5 +71,6 @@ def cities_by_states():
     states.sort(key=lambda state: state.name)
     return render_template('8-cities_by_states.html', states=states)
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
