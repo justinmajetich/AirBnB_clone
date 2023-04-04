ed481eb1e253663671144da2d4616eb800050e10
Gabriel <Fossecruor@gmail.com>
2023-04-04 11:05:36 +0200

    Added content for 8

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" A script that starts a Flask web application with storage """
+from models import storage
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states")
+def cities_by_state():
+    from models.state import State
+    from models.city import City
+
+    state_item = []
+    city_item = []
+    return render_template("8-cities_by_states.html", state_item=state_item, city_item=city_item)
+    pass
+
+
+@app.teardown_appcontext
+def close_db(error):
+    from models.engine.db_storage import DBStorage
+    if isinstance(storage, DBStorage):
+        storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
