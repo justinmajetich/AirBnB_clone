5a8c269f78b3e58b1630e5c26b33dd150b5067b8
Bekalu Geta <bekalu2054@gmail.com>
2023-04-06 00:48:57 +0300

    💹Finish tar fabric

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,11 +11,15 @@ import os
 def do_pack():
     """Function that generates a .tgz archive from the contents of the
     web_static folder"""
-    if not os.path.exists("versions"):
-        if local("mkdir -p versions").failed:
+    if not os.path.isdir("versions"):
+        if local("mkdir versions").failed:
             return None
     date = datetime.now().strftime("%Y%m%d%H%M%S")
     file = "versions/web_static_{}.tgz".format(date)
-    if local("tar -cvzf {} web_static".format(file)).failed:
+    try:
+        print("Packing web_static to {}".format(file))
+        local("tar -cvzf {} web_static".format(file))
+        print("web_static packed: {} -> {}Bytes".format(file, os.path.getsize(file)))
+    except Exception:
         return None
     return file
