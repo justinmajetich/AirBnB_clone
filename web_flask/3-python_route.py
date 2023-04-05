2e34a49955ea4e3a1f617e03c02e1b5d2b33926a
Valentin <5619@holbertonstudents.com>
2023-04-05 01:20:42 -0700

    3. add default value in prototype

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -30,8 +30,8 @@ def c_text(text):
     return text.replace("_", " ")
 
 
-@app.route("/python/<text>", defaults = {"text": "is cool"})
-def python_text(text):
+@app.route("/python/<text>")
+def python_text(text = "is cool"):
     strict_slashes = False
     text = f"Python {text}"
     return text.replace("_", " ")
