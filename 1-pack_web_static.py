821bf71b127b3ea9dbeec3edc4202d27f5a419e5
Bekalu Geta <bekalu2054@gmail.com>
2023-04-06 00:14:41 +0300

    💹Write fabric

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,2 +1,21 @@
 #!/bin/python3
+""" Fabric script that generates a .tgz archive from the contents of the
+    web_static folder of AirBnB Clone version 2 repo, using the function
+    do_pack."""
 
+from fabric.api import local
+from datetime import datetime
+import os
+
+
+def do_pack():
+    """Function that generates a .tgz archive from the contents of the
+    web_static folder"""
+    if not os.path.exists("versions"):
+        if local("mkdir -p versions").failed:
+            return None
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    file = "versions/web_static_{}.tgz".format(date)
+    if local("tar -cvzf {} web_static".format(file)).failed:
+        return None
+    return file
