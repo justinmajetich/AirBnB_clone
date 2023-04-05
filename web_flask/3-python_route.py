6d83eb249ddadf9a5946f5ffb529ed2deaecbfb3
Valentin <5619@holbertonstudents.com>
2023-04-05 02:06:13 -0700

    3. default value is cool

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ connect flask """
 
-from flask import Flask
+from flask import Flask, render_template
 from markupsafe import escape
 
 # Créer une instance de l'application Flask
@@ -9,30 +9,28 @@ app = Flask(__name__)
 
 
 # Définir une route pour l'URL racine '/'
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def hello():
-    # Désac la redirection auto vers une URL avec une barre oblique à la fin
-    strict_slashes = False
     # Retourner une chaîne de caractères en réponse à la requête HTTP
     return 'Hello HBNB!'
 
 
-@app.route("/hbnb")
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    strict_slashes = False
     return 'HBNB'
 
 
-@app.route("/c/<text>")
+@app.route("/c/<text>", strict_slashes=False)
 def c_text(text):
-    strict_slashes = False
-    text = f"C {escape(text)}"
+    text = f"C {(text)}"
     return text.replace("_", " ")
 
 
-@app.route("/python/<text>")
-def python_text(text="is cool"):
-    strict_slashes = False
+@app.route('/python/', strict_slashes=False, defaults={"text": "is cool"})
+# Définition de la première route, qui affiche "Python is cool" par défaut
+@app.route('/python/<text>', strict_slashes=False)
+# seconde route, permet de spécifier un texte à afficher à la place de is cool
+def python_text(text="is_cool"):
     text = f"Python {text}"
     return text.replace("_", " ")
 
