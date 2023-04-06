9c193cfe9e7eade273c7051cec8b155ca8fd445f
bikilaketema <bikilaketema94@gmail.com>
2023-04-06 21:43:20 +0300

    trying task2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -31,6 +31,7 @@ def do_deploy(archive_path):
         run("rm -rf /data/web_static/current")
         run("ln -s {}{} /data/web_static/current".format(releases_dir,
             basename))
+        print('New version deployed!')
         return True
     except Exception:
         return False
