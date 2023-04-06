2251276e77c2b0e25e10e9e5ea7a7944b6587757
bikilaketema <bikilaketema94@gmail.com>
2023-04-06 20:04:56 +0300

    working on Task1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
-from fabric.api import local
+import os
 from datetime import datetime
+from fabric.api import local
 
 
 def do_pack():
-    """Generates a .tgz archive from the contents of the web_static folder"""
+    """
+    Generates a .tgz archive from the contents of the web_static folder
+    and stores it in the versions directory.
+    """
     try:
         # Create the versions directory if it doesn't exist
         local("mkdir -p versions")
 
-        # Create the name of the archive
-        now = datetime.now()
-        archive_name = "web_static_{}{}{}{}{}{}.tgz"
-        .format(now.year, now.month, now.day, now.hour, now.minute, now.second)
+        # Create the name of the archive with the current date and time
+        now = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_name = "web_static_{}.tgz".format(now)
 
         # Create the tar command to compress the files
-        tar_command = "tar -cvzf versions/{} web_static".format(archive_name)
+        tar_command = "tar -czvf {} {}"
+        .format(os.path.join("versions", archive_name), "web_static")
 
         # Execute the tar command
         local(tar_command)
 
         # Return the archive path if the archive has been correctly generated
-        return "versions/{}".format(archive_name)
-    except Exception:
-        # Return None if an error occurred
+        return os.path.join("versions", archive_name)
+
+    except Exception as e:
+        # Print the exception message and return None if an error occurred
+        print("Error: {}".format(e))
         return None
 
