888fbabd33bb479445eca2e16f726fbe559db85e
ezana73 <ezana49@gmail.com>
2023-04-06 16:00:16 +0000

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,7 +10,7 @@ sudo echo "<html>
 	<body>
 		Holberton School
 	</body>
-</html>" | sudo tee /data/web_static?releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
 sudo sed -i '\\tlocation \/hbnb_static {\n\t\t alias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
