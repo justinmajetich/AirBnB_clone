fdf857253c7d518eec8dbaac012c55bbcf2cc1bc
allenkays <allenkays@tutanota.com>
2023-04-06 14:51:40 -0400

    Redirect output to stdout to /dev/null

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,27 +1,30 @@
 #!/usr/bin/env bash
-
-# Install Nginx if not already installed
-if [ ! -x "$(command -v nginx)" ]; then
-    sudo apt-get update
-    sudo apt-get -y install nginx
+# This script prepares nginx to serve a static web page
+# Install Nginx if it's not already installed
+if ! command -v nginx &> /dev/null
+then
+    sudo apt-get update > /dev/null
+    sudo apt-get install -y nginx > /dev/null
 fi
 
-# Create necessary directories
-sudo mkdir -p /data/web_static/releases/test
-sudo mkdir -p /data/web_static/shared
-sudo mkdir -p /data/web_static/current
+# Create the necessary directories
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared > /dev/null
 
 # Create a fake HTML file for testing
-sudo echo "<html><head><title>Testing Nginx configuration</title></head><body><p>Hello World!</p></body></html>" | sudo tee /data/web_static/releases/test/index.html
+echo "<html><head><title>Test</title></head><body><p>This is a test</p></body></html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
-# Create a symbolic link
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
+# Create or update the symbolic link
+sudo rm -f /data/web_static/current > /dev/null
+sudo ln -s /data/web_static/releases/test /data/web_static/current > /dev/null
 
-# Set ownership to the ubuntu user and group
-sudo chown -R ubuntu:ubuntu /data/
+# Set ownership of the directories to ubuntu user and group
+sudo chown -R ubuntu:ubuntu /data/ > /dev/null
 
-# Update Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
-sudo sed -i '/server_name _;/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+# Update Nginx configuration
+sudo sed -i '/server {/a \\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default > /dev/null
 
 # Restart Nginx
-sudo service nginx restart
+sudo service nginx restart > /dev/null
+
+# Exit successfully
+exit 0
