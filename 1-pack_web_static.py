fcf359c67230128dd9c34918de02f315a88c5aff
ezana73 <ezana49@gmail.com>
2023-04-06 07:08:56 -0700

    1-pack_web_static.py

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Script Fabric that generates a .tgz from the content of webstatic"""
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """return the archive path if archive has been correctly
+        generated
+    """
+
+    local("mkdir -p versions")
+    date =datetime.now().strftime("%Y%m%d%H%M%S")
+    archived_f_path = "versions/web_static_{}.tgz".format(date)
+    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
+
+    if t_gzip_archive.succeeded:
+        return archived_f_path
+    else:
+        return None
