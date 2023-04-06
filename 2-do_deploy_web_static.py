6bc0c9e8c51e5a84b9cacc9f3ac9277a2bd7311e
Ahmed Mubarak <ahmed.elia2003@gmail.com>
2023-04-06 05:35:29 +0200

    task 2 -- fixing pep8

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,14 +15,15 @@ def do_deploy(archive_path):
         name = archive_path.split('/')[-1]
         put(archive_path, "/tmp/{}".format(name))
         sudo("mkdir -p /data/web_static/releases/{}".format(name[0:-4]))
-        sudo(
-            "tar -xzvf /tmp/{} -C /data/web_static/releases/{}".format(name, name[0:-4]))
-        sudo("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}".format(
+        sudo("tar - xzvf / tmp/{} - C
+             / data/web_static/releases/{}".format(name, name[0:-4]))
+        sudo("mv / data/web_static/releases/{}/web_static/*
+             / data/web_static/releases/{}".format(
             name[0:-4], name[0:-4]))
         sudo("rm -f /tmp/{}".format(name))
         sudo("rm -f /data/web_static/current")
-        sudo(
-            "ln -sf /data/web_static/releases/{} /data/web_static/current".format(name[0:-4]))
+        sudo("ln - sf / data/web_static/releases/{}
+             / data/web_static/current".format(name[0:-4]))
         return True
     except Exception:
         return False
