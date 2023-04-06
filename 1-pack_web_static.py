6bf6a52b9c3956f8aec7085242b5c0a1edbd3378
bikilaketema <bikilaketema94@gmail.com>
2023-04-06 20:11:58 +0300

    working on Task1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,34 +1,15 @@
 #!/usr/bin/python3
-import os
+'''Fabric script to generate .tgz archive'''
 from datetime import datetime
 from fabric.api import local
+from fabric.decorators import runs_once
 
 
+@runs_once
 def do_pack():
-    """
-    Generates a .tgz archive from the contents of the web_static folder
-    and stores it in the versions directory.
-    """
-    try:
-        # Create the versions directory if it doesn't exist
-        local("mkdir -p versions")
+    '''generates .tgz archive from the contents of the web_static folder'''
+    path = f'versions/web_static_{datetime.now():%Y%m%d%H%M%S}.tgz'
+    result = local(f'tar -cvzf {path} web_static')
 
-        # Create the name of the archive with the current date and time
-        now = datetime.now().strftime("%Y%m%d%H%M%S")
-        archive_name = "web_static_{}.tgz".format(now)
-
-        # Create the tar command to compress the files
-        tar_command = "tar -czvf {} {}"
-        .format(os.path.join("versions", archive_name), "web_static")
-
-        # Execute the tar command
-        local(tar_command)
-
-        # Return the archive path if the archive has been correctly generated
-        return os.path.join("versions", archive_name)
-
-    except Exception as e:
-        # Print the exception message and return None if an error occurred
-        print("Error: {}".format(e))
-        return None
+    return path if result.succeeded else None
 
