4f0651d0f5350091cc6f43912107cd912cefe85c
chineduCoded <chinedujohn172gmail.com>
2023-04-06 10:14:09 -0700

    Tried to fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,7 +15,7 @@ def do_pack():
     """
     Generates a .tgz archive from the contents of the web_static folder.
     """
-     local("mkdir -p versions")
+    local("mkdir -p versions")
     date = datetime.now().strftime("%Y%m%d%H%M%S")
     archived_file_path = "versions/web_static_{}.tgz".format(date)
     t_gzip_archive_file = local("tar -cvzf {} web_static".format(archived_file_path))
