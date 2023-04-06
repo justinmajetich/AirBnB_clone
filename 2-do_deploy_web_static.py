03da95d9d18cc8ccd51cd4497e653c1f47ea4983
salisidris229 <salisudirs229@gmail.com>
2023-04-06 23:28:33 +0000

    2-do_deploy_web_static.py

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Fabric script (based on the file 1-pack_web_static.py) that
+distributes an archive to your web servers, using the function
+do_deploy."""
+
+from datetime import datetime
+from fabric.api import *
+import shlex
+import os
+
+
+env.hosts = ['34.74.243.116', '3.239.3.209']
+env.user = "ubuntu"
+
+
+def do_deploy(archive_path):
+    """ Deploys """
+    if not os.path.exists(archive_path):
+        return False
+    try:
+        name = archive_path.replace('/', ' ')
+        name = shlex.split(name)
+        name = name[-1]
+
+        wname = name.replace('.', ' ')
+        wname = shlex.split(wname)
+        wname = wname[0]
+
+        releases_path = "/data/web_static/releases/{}/".format(wname)
+        tmp_path = "/tmp/{}".format(name)
+
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(releases_path))
+        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
+        run("rm {}".format(tmp_path))
+        run("mv {}web_static/* {}".format(releases_path, releases_path))
+        run("rm -rf {}web_static".format(releases_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(releases_path))
+        print("New version deployed!")
+        return True
+    except:
+        return False
