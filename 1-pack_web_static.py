d6be1dc4db857a55114aa55cec034d497d5f3310
allenkays <allenkays@tutanota.com>
2023-04-06 15:28:15 -0400

    Creating an archive with fabric

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+This script creates an archive of a given folder
+"""
+
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """
+    Generates a .tgz archive from the contents of the web_static folder
+    """
+    try:
+        current_time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+        file_name = "web_static_" + current_time + ".tgz"
+        local("mkdir -p versions")
+        local("tar -cvzf versions/{} web_static".format(file_name))
+        archive_path = "versions/{}".format(file_name)
+        return archive_path
+    except:
+        return None
