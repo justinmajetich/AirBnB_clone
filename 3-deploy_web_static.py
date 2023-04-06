08cdb469063981e467323964582122155e0cda4c
bikilaketema <bikilaketema94@gmail.com>
2023-04-06 22:29:16 +0300

    trying task3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -25,24 +25,27 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """Deploys an archive to a web server"""
-    if not os.path.exists(archive_path):
+    """Distributes an archive to web servers"""
+
+    if not os.path.isfile(archive_path):
         return False
+
     filename = os.path.basename(archive_path)
     basename = os.path.splitext(filename)[0]
+    releases_dir = "/data/web_static/releases/"
+    tmp_dir = "/tmp/"
+
     try:
-        put(archive_path, "/tmp/")
-        run("mkdir -p /data/web_static/releases/{}/".format(basename))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(filename, basename))
-        run("rm /tmp/{}".format(filename))
-        run("mv /data/web_static/releases/{}/web_static/* "
-            "/data/web_static/releases/{}/".format(basename, basename))
-        run("rm -rf /data/web_static/releases/{}/web_static"
-            .format(basename))
+        put(archive_path, tmp_dir)
+        run("mkdir -p {}{}".format(releases_dir, basename))
+        run("tar -xzf {}{} -C {}{}".format(tmp_dir, filename,
+                                           releases_dir, basename))
+        run("rm {}{}".format(tmp_dir, filename))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(releases_dir, basename))
+        run("rm -rf {}{}/web_static".format(releases_dir, basename))
         run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(basename))
+        run("ln -s {}{} /data/web_static/current".format(releases_dir,
+                                                         basename))
         return True
     except Exception:
         return False
