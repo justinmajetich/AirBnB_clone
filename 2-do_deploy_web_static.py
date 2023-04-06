5a261861ff55edb1b0d0f50a537a30b5a8fb8aa7
bikilaketema <bikilaketema94@gmail.com>
2023-04-06 22:00:24 +0300

    trying task2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,9 +2,10 @@
 """
 Fabric script to deploy archive to web servers
 """
+
 import os
 from datetime import datetime
-from fabric.api import env, put, run, runs_once
+from fabric.api import env, put, run
 
 env.hosts = ['35.153.232.20', '52.90.15.8']
 
@@ -23,15 +24,14 @@ def do_deploy(archive_path):
     try:
         put(archive_path, tmp_dir)
         run("mkdir -p {}{}".format(releases_dir, basename))
-        run("tar -xzf {}{} -C {}{}".format(tmp_dir,
-            filename, releases_dir, basename))
+        run("tar -xzf {}{} -C {}{}".format(tmp_dir, filename,
+                                           releases_dir, basename))
         run("rm {}{}".format(tmp_dir, filename))
         run("mv {0}{1}/web_static/* {0}{1}/".format(releases_dir, basename))
         run("rm -rf {}{}/web_static".format(releases_dir, basename))
         run("rm -rf /data/web_static/current")
         run("ln -s {}{} /data/web_static/current".format(releases_dir,
-            basename))
-        print('New version deployed!')
+                                                         basename))
         return True
     except Exception:
         return False
