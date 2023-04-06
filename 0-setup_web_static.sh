d1999040af4c9e95264021facbeaa3510fc5cdd7
allenkays <allenkays@tutanota.com>
2023-04-06 15:13:42 -0400

    Script test

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,30 +1,33 @@
 #!/usr/bin/env bash
-# This script prepares nginx to serve a static web page
-# Install Nginx if it's not already installed
+# This script prepares nginx webserver to host a static site
+# Install Nginx if not already installed
 if ! command -v nginx &> /dev/null
 then
-    sudo apt-get update > /dev/null
-    sudo apt-get install -y nginx > /dev/null
+    echo "Nginx not found. Installing..."
+    sudo apt-get update
+    sudo apt-get -y install nginx
+    echo "Nginx installed successfully"
 fi
 
-# Create the necessary directories
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared > /dev/null
+# Create necessary folders
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared /data/web_static/current
 
-# Create a fake HTML file for testing
-echo "<html><head><title>Test</title></head><body><p>This is a test</p></body></html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+# Create a fake HTML file for testing Nginx configuration
+sudo echo "<html><head><title>Test HTML file</title></head><body><p>This is a test HTML file.</p></body></html>" > /data/web_static/releases/test/index.html
 
-# Create or update the symbolic link
-sudo rm -f /data/web_static/current > /dev/null
-sudo ln -s /data/web_static/releases/test /data/web_static/current > /dev/null
+# Create a symbolic link
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
-# Set ownership of the directories to ubuntu user and group
-sudo chown -R ubuntu:ubuntu /data/ > /dev/null
+# Give ownership of /data/ to ubuntu user and group recursively
+sudo chown -R ubuntu:ubuntu /data/
 
-# Update Nginx configuration
-sudo sed -i '/server {/a \\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default > /dev/null
+# Update Nginx configuration to serve content from /data/web_static/current/
+if ! grep -q "location /hbnb_static/ {" /etc/nginx/sites-available/default
+then
+    sudo sed -i '/^\tserver_name localhost;/a \\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+fi
 
 # Restart Nginx
-sudo service nginx restart > /dev/null
+sudo service nginx restart
 
-# Exit successfully
 exit 0
