92a1c1465e9865816423aa859ae6b7b4ee0110af
chineduCoded <chinedujohn172gmail.com>
2023-04-06 15:41:50 -0700

    fixing an error

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-Fabric script that deletes out-of-date archives using the do_clean function.
+Fabric script that deletes out-of-date archives
 """
-import os
+from os import path
 from fabric.api import *
 
 env.user = 'ubuntu'
@@ -12,24 +12,16 @@ env.hosts = ['54.158.79.124', '54.146.64.149']
 def do_clean(number=0):
     """
     Deletes all unnecessary archives (all archives minus the number to keep)
-    in the versions folder and /data/web_static/releases folder of both web servers.
     Args:
         number (int): The number of archives to keep.
     """
     number = int(number)
     if number < 1:
-        return
-    elif number == 1:
-        local('ls -1t versions/ | tail -n +2 | xargs -I {} rm versions/{}')
+        number = 1
     else:
-        local('ls -1t versions/ | tail -n +{} | xargs -I {} rm versions/{}'
-              .format(number + 1, '{}'))
+        number += 1
+
+    with lcd('./versions'):
+        local("rm $(ls -t | awk 'NR>" + str(number) + "')")
     with cd('/data/web_static/releases'):
-        run('ls -1t | tail -n +{} | xargs -I {} rm -rf /data/web_static/releases/{}'
-            .format(number + 1, '{}'))
-
-
-def deploy():
-    """Deploys the web_static content to the web servers"""
-    # ... previous deploy code ...
-    do_clean(2)
+        run("rm $(ls -t | awk 'NR>" + str(number) + "')")
