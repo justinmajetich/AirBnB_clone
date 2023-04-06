30cce2f5d74dfba101a6806bd679da7f5d880348
bikilaketema <bikilaketema94@gmail.com>
2023-04-06 20:23:21 +0300

    Finally done with task1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,8 +8,7 @@ from fabric.decorators import runs_once
 
 @runs_once
 def do_pack():
-    '''generates .tgz archive from the contents of 
-    the web_static folder to deploy'''
+    '''generates .tgz archive from web_static dir'''
     local("mkdir -p versions")
     filename = "web_static_{}.tgz".format(
         datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")
