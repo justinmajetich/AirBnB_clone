98023fcacb5ca720b5660096af4a860095556096
chineduCoded <chinedujohn172gmail.com>
2023-04-06 10:28:56 -0700

    trid to fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,40 +12,51 @@ env.user = 'ubuntu'
 
 
 def do_pack():
-    """
-    Generates a .tgz archive from the contents of the web_static folder.
-    """
-    local("mkdir -p versions")
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    archived_file_path = "versions/web_static_{}.tgz".format(date)
-    t_gzip_archive_file = local("tar -cvzf {} web_static".format(archived_file_path))
+    """Create a compressed archive of web_static contents"""
 
-    if t_gzip_archive_file.succeeded:
-        return archived_file_path
-    else:
-        return None
+    now = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+    archive_name = 'web_static_' + now + '.tgz'
+    if not os.path.exists('versions'):
+        os.mkdir('versions')
+    local('tar -czvf versions/{} web_static'.format(archive_name))
+    archive_path = 'versions/{}'.format(archive_name)
+    if os.path.exists(archive_path):
+        return archive_path
+    return None
 
 
 def do_deploy(archive_path):
-    """
-    Distributes an archive to your web servers.
-    """
-    if os.path.exists(archive_path):
-        archived_file = archive_path[9:]
-        newest_version = "/data/web_static/releases/" + archived_file[:-4]
-        archived_file = "/tmp/" + archived_file
-        put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(newest_version))
-        run("sudo tar -xzf {} -C {}/".format(archived_file,
-                                             newest_version))
-        run("sudo rm {}".format(archived_file))
-        run("sudo mv {}/web_static/* {}".format(newest_version,
-                                                newest_version))
-        run("sudo rm -rf {}/web_static".format(newest_version))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(newest_version))
+    """Deploy archive to web servers"""
 
-        print("New version deployed!")
-        return True
+    if not os.path.exists(archive_path):
+        return False
 
-    return False
+    # Get archive filename (without extension) from path
+    filename = os.path.basename(archive_path)
+    name = filename.split(".")[0]
+
+    # Upload archive to /tmp/ directory on server
+    put(archive_path, "/tmp/{}".format(filename))
+
+    # Create directory to uncompress archive
+    run("mkdir -p /data/web_static/releases/{}/".format(name))
+
+    # Uncompress archive to directory
+    run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+        .format(filename, name))
+
+    # Remove archive from server
+    run("rm /tmp/{}".format(filename))
+
+    # Move contents of uncompressed archive to proper directory
+    run("mv /data/web_static/releases/{}/web_static/* \
+        /data/web_static/releases/{}/".format(name, name))
+
+    # Remove old symbolic link if it exists
+    run("rm -f /data/web_static/current")
+
+    # Create new symbolic link
+    run("ln -s /data/web_static/releases/{}/ \
+        /data/web_static/current".format(name))
+
+    return True
