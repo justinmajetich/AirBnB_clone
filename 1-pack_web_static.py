41766c88c994f8972a0bc80734f2ea0d8f60780a
b-lemy <lemabrian1234@gmail.com>
2023-04-07 07:31:37 -0700

    deployment configuaration

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from fabric.api import local
+from time import strftime
+
+
+def do_pack():
+    """ A script that generates archive of the  web_static folder"""
+
+    filename = strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(filename))
+
+        return "versions/web_static_{}.tgz".format(filename)
+
+    except Exception as e:
+        return None
