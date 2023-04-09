c6ab2d3647bebdb1501b10f5114d440f24a7a82a
Almarat Arnu Ngutulu <99120810+Almah200@users.noreply.github.com>
2023-04-09 11:55:21 +0300

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,40 +1,41 @@
-#!/usr/bin/env bash
-# using fabric to deploy airbnb web static
+#!/usr/bin/python3
+"""
+script that distributes an archive to your web servers,
+using the function do_deploy
+"""
+import os
+from fabric.api import put, run, env
+env.hosts = ['107.23.168.84', '52.90.109.65']  # list of web servers
 
-# update packages list
-sudo apt-get update
 
-#install nginx if not installed
-if ! command -v nginx &> /dev/null
-then
-  sudo apt-get install nginx -y
-fi
-sudo ufw allow 'Nginx HTTP'
-
-#create a folder /data/ if it doesn't exist
-# the -p helps prevent errors if the directory/subdirectories exists
-sudo mkdir -p /data/web_static
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-
-# create a fake html file
-echo "<html>
-      <head>
-      </head>
-      <body>
-        Holberton School
-      </body>
-    </html>" | sudo tee /data/web_static/releases/test/index.html
-
-# create a symbolic link ..current linked to ..test/ folder
-# if the link exists, should be deleted and recreated
-sudo ln -sf /data/web_static/releases/test/  /data/web_static/current
-
-# give ownership of /data/ folder to ubuntu user and group
-sudo chown -R ubuntu:ubuntu /data/
-
-# update nginx configuration to serve content of (/data/web_static/current/) to (hbnb_static)
-sudo sed -i '/listen 80 default_server;/a \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-enabled/default
-
-# restart nginx
-sudo service nginx restart
+def do_deploy(archive_path):
+    """
+    function that distributes an archive to web servers
+    """
+    if archive_path is None or not os.path.exists(archive_path):
+        return False
+    try:
+        file = archive_path.split("/")[-1]
+        file_no_ext = file.split(".")[0]
+        path = "/data/web_static/releases/"
+        # upload the archive to the /tmp/ directory of the web server
+        put(archive_path, "/tmp/")
+        # create a folder with the same name as the archive
+        # without the extension
+        run("mkdir -p {}{}/".format(path, file_no_ext))
+        # uncompress the archive to the folder
+        run("tar -xzf /tmp/{} -C {}{}/".format(
+            file, path, file_no_ext))
+        # delete the archive from the web server
+        run("rm /tmp/{}".format(file))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(path, file_no_ext))
+        # delete the symbolic link /data/web_static/current from the web server
+        run("rm -rf {}{}/web_static".format(path, file_no_ext))
+        run("rm -rf /data/web_static/current")
+        # create new symbolic link /data/web_static/current on web server
+        # linked to the new version of your code
+        run("ln -s {}{}/ /data/web_static/current".
+            format(path, file_no_ext))
+        return True
+    except:
+        return False
