26decd52762941c9e9ce73b2186d4a0b71e8625f
Almarat Arnu Ngutulu <99120810+Almah200@users.noreply.github.com>
2023-04-09 11:50:07 +0300

    0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,40 @@
 #!/usr/bin/env bash
-# A Bash script to prepare the web server
+# using fabric to deploy airbnb web static
+
+# update packages list
 sudo apt-get update
-sudo apt-get install -y nginx
-sudo mkdir /data/
-sudo mkdir /data/web_static/
-sudo mkdir /data/web_static/releases/
-sudo mkdir /data/web_static/shared/
-sudo mkdir /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-echo "<html><head></head><body> ALU BSE</body></html>" | sudo tee /data/web_static/releases/test/index.html
-sudo chown -R ubuntu:ubuntu /data
-sudo sed -i '37i\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-enabled/default
+
+#install nginx if not installed
+if ! command -v nginx &> /dev/null
+then
+  sudo apt-get install nginx -y
+fi
+sudo ufw allow 'Nginx HTTP'
+
+#create a folder /data/ if it doesn't exist
+# the -p helps prevent errors if the directory/subdirectories exists
+sudo mkdir -p /data/web_static
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+# create a fake html file
+echo "<html>
+      <head>
+      </head>
+      <body>
+        Holberton School
+      </body>
+    </html>" | sudo tee /data/web_static/releases/test/index.html
+
+# create a symbolic link ..current linked to ..test/ folder
+# if the link exists, should be deleted and recreated
+sudo ln -sf /data/web_static/releases/test/  /data/web_static/current
+
+# give ownership of /data/ folder to ubuntu user and group
+sudo chown -R ubuntu:ubuntu /data/
+
+# update nginx configuration to serve content of (/data/web_static/current/) to (hbnb_static)
+sudo sed -i '/listen 80 default_server;/a \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-enabled/default
+
+# restart nginx
 sudo service nginx restart
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,48 +1,40 @@
-#!/usr/bin/python3
-from fabric.api import run, local, put, env
-import datetime
-import os
+#!/usr/bin/env bash
+# using fabric to deploy airbnb web static
 
+# update packages list
+sudo apt-get update
 
-def do_pack():
-    """do pack method"""
-    time = datetime.datetime.now()
-    date = (str(time.year) + str(time.month) + str(time.day) + str(time.hour) +
-            str(time.minute) + str(time.second))
-    try:
-        local("mkdir -p versions")
-        local("tar -cvzf versions/web_static_{}.tgz ./web_static".format(date))
-        return "./versions/web_static_{}.tgz".format(date)
-    except:
-        return None
+#install nginx if not installed
+if ! command -v nginx &> /dev/null
+then
+  sudo apt-get install nginx -y
+fi
+sudo ufw allow 'Nginx HTTP'
 
+#create a folder /data/ if it doesn't exist
+# the -p helps prevent errors if the directory/subdirectories exists
+sudo mkdir -p /data/web_static
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 
-env.hosts = ['35.229.42.147', '54.90.157.131']
+# create a fake html file
+echo "<html>
+      <head>
+      </head>
+      <body>
+        Holberton School
+      </body>
+    </html>" | sudo tee /data/web_static/releases/test/index.html
 
+# create a symbolic link ..current linked to ..test/ folder
+# if the link exists, should be deleted and recreated
+sudo ln -sf /data/web_static/releases/test/  /data/web_static/current
 
-def do_deploy(archive_path):
-    """
-     Fabric script 
-     (based on the file 1-pack_web_static.py) 
-     that distributes an archive to your web servers, 
-     using the function do_deploy
+# give ownership of /data/ folder to ubuntu user and group
+sudo chown -R ubuntu:ubuntu /data/
 
-    """
-    if os.path.exists(archive_path) is False:
-        return False
-    try:
-        upload = put(archive_path, "/tmp/")
-        name = archive_path[9:-4]
-        rmt_path = "/data/web_static/releases/" + name
-        run("mkdir {}".format(rmt_path))
-        run("tar -xvzf /tmp/{}.tgz --directory {}/".format(name, rmt_path))
-        run("rm /tmp/{}.tgz".format(name))
-        run("rm /data/web_static/current")
-        run("ln -nsf /data/web_static/releases/{} /data/web_static/current"
-            .format(name))
-        run("mv {}/web_static/* {}".format(rmt_path, rmt_path))
-        run("rm -d {}/web_static/".format(rmt_path))
-        return True
-    except:
-        return False 
-    
+# update nginx configuration to serve content of (/data/web_static/current/) to (hbnb_static)
+sudo sed -i '/listen 80 default_server;/a \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-enabled/default
+
+# restart nginx
+sudo service nginx restart
