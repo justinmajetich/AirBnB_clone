ca233e97899f1885d1893e8e955e4d306ccf57da
trmrskr <simonalale@gmail.com>
2023-04-10 21:28:51 +0000

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,38 +1,37 @@
 #!/usr/bin/env bash
-# script that configures a Nginx server
-# 	- create folders
-#	- create a fake HTML file
-#	- create a symbolic link linked to test folder
-#	- change the ownership of the /data/ folder
-#	- use Alias in the configuration file
+# This script that configures a physical server using the  Nginx server.
 
 # update apt-get
-apt-get update
+sudo apt-get -y update
 
 # install nginx if it is not already installed
 if [[ ! ("$(sudo which nginx)") ]]; then
-	apt-get install -y nginx
+	sudo apt-get -y install nginx
 fi
 
-# mmake necessary folders and config changes
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
+sudo ufw allow 'Nginx HTTP'
+
+# Create folders and make necessary folders and config changes
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 echo "Ceci n'est pas une page" > /var/www/html/404.html
-printf "%s" "<!DOCTYPE html>
-<html>
-    <head>
-        <title>Test HTML document</title>
-    </head>
-    <body>
-        <p>This is a test file</p>
-    </body>
-</html>" > /data/web_static/releases/test/index.html
-ln -s /data/web_static/current /data/web_static/releases/test/
-chown -R ubuntu:ubuntu /data/
-echo "location /hbtn_static {
-      alias /data/web_static/current/;
-}" >> /etc/nginx/sites-available/default
-printf "%s" "server {
+echo "<h1>bovntyhvnter.tech test file</h1>" > /data/web_static/releases/test/index.html
+
+#--prevent overwrite
+if [ -d "/data/web_static/current" ]:
+then
+    echo "path /data/web_static/current exists"
+    sudo rm -rf /data/web_static/current;
+fi;
+
+# Create a symbolic link linked to test folder
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Change the ownership of the /data/ folder
+sudo chown -hR ubuntu:ubuntu /data/
+
+# Set up and use alias in server configuration file
+SERVER_CONFIG=\
+"server {
     listen 80 default_server;
     listen [::]:80 default_server;
     add_header X-Served-By $HOSTNAME;
@@ -49,4 +48,16 @@ printf "%s" "server {
     location /hbtn_static {
       alias /data/web_static/current/
     }
-}" > /etc/nginx/sites-available/default
+}"
+
+bash -c "echo -e '$SERVER_CONFIG' > /etc/nginx/sites-available/default"
+
+# Symbolically link sites-available to sites-enabled
+sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
+
+# Start server if server not started else restart
+if [ "$(pgrep -c nginx)" -le 0 ]; then
+    service nginx start
+else
+    service nginx restart
+fi
