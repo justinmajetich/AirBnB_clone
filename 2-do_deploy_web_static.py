5d6561d0563c257dcba97ecfa21fb2bc22687fe3
allenkays <allenkays@tutanota.com>
2023-04-10 15:46:23 -0400

    Remove function do_pack

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -14,31 +14,6 @@ from fabric.contrib.files import exists
 env.hosts = ['54.237.210.251', '54.237.14.69']
 
 
-def do_pack():
-    """
-    Compresses the web_static folder into a .tgz archive.
-
-    The archive is stored in the versions folder and
-    is named based on the current date and time.
-
-    Returns:
-        The file path of the compressed archive on success, None otherwise.
-    """
-    try:
-        current_time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
-        file_name = "web_static_" + current_time + ".tgz"
-        local("mkdir -p versions")
-        local("tar -cvzf versions/{} web_static".format(file_name))
-        archive_path = "versions/{}".format(file_name)
-        return archive_path
-    except OSError:
-        print("An OS error occurred while creating the archive.")
-        return None
-    except Exception as e:
-        print("An unexpected error occurred: {}".format(str(e)))
-        return None
-
-
 def do_deploy(archive_path):
     """
     Distributes an archive to web servers.
