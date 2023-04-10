252700ced99f480b12bc7c97eaba96e46ad6c3e9
allenkays <allenkays@tutanota.com>
2023-04-10 14:38:41 -0400

    Edit import and except sections and include docstrinfs

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 """
-This script creates an archive of a given folder
-The script is run by the fabric library
+This script creates a compressed archive of a given folder.
+The archive is named based on the current date and time.
 """
 
+import os
 from fabric.api import local
 from datetime import datetime
 
 
 def do_pack():
     """
-    Generates a .tgz archive from the contents of the web_static folder
-    The archive is named based on current date and time.
-
+    Compresses the web_static folder into a .tgz archive.
+    
+    The archive is stored in the versions folder and is named based on the current date and time.
+    
     Returns:
-        Path to archive if created successfully
-        None otherwise
+        The file path of the compressed archive on success, None otherwise.
     """
     try:
         current_time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
@@ -24,5 +25,9 @@ def do_pack():
         local("tar -cvzf versions/{} web_static".format(file_name))
         archive_path = "versions/{}".format(file_name)
         return archive_path
-    Exception:
+    except OSError:
+        print("An OS error occurred while creating the archive.")
+        return None
+    except Exception as e:
+        print("An unexpected error occurred: {}".format(str(e)))
         return None
