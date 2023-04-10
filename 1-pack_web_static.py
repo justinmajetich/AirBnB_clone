9a82bc46df7351a9c7436a7eda0c6ad747fc4557
allenkays <allenkays@tutanota.com>
2023-04-10 14:21:39 -0400

    Document d_pack function

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,9 +8,7 @@ from datetime import datetime
 
 
 def do_pack():
-    """
-    Generates a .tgz archive from the contents of the web_static folder
-    """
+    """Generates a .tgz archive from the contents of the web_static folder"""
     try:
         current_time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
         file_name = "web_static_" + current_time + ".tgz"
