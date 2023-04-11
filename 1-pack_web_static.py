57cc6d8936dc8399ce05d0bf1a7c050c1807c29c
trmrskr <simonalale@gmail.com>
2023-04-11 08:56:58 -0700

    Compress before sending: pycodestyle correction

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -14,8 +14,8 @@ from fabric.decorators import runs_once
 def do_pack():
     '''Generates .tgz archive from the contents of the web_static folder'''
     local("mkdir -p versions")
-    tgz_pathname = 'versions/web_static_{}.tgz'.format(datetime.strftime(datetime.now(),
-                                                    '%Y%m%d%H%M%S'))
+    tgz_pathname = ('versions/web_static_{}.tgz'
+                    .format(datetime.strftime(datetime.now(),'%Y%m%d%H%M%S')))
     tar_ball_output = local("tar -czvf {} web_static".format(tgz_pathname))
     if tar_ball_output.failed:
         return None
