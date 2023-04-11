4719e88f01df8cf60c08a2effee1ecdb416e5a70
trmrskr <simonalale@gmail.com>
2023-04-11 15:39:28 +0000

    Compress before sending: tar file created

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,16 +7,16 @@ This script hosts a function that packs the content of a file
 
 from fabric.api import local
 from datetime import datetime
-from fabric.decorator import runs_once
+from fabric.decorators import runs_once
 
 
 @runs_once
 def do_pack():
     '''Generates .tgz archive from the contents of the web_static folder'''
     local("mkdir -p versions")
-    tgz_pathname = '/versions/web_static_{}'.format(strftime(datetime.now(),
+    tgz_pathname = 'versions/web_static_{}.tgz'.format(datetime.strftime(datetime.now(),
                                                     '%Y%m%d%H%M%S'))
-    tar_ball_output = local('tar -czvf {} web_static'.format(tgz_pathname))
+    tar_ball_output = local("tar -czvf {} web_static".format(tgz_pathname))
     if tar_ball_output.failed:
         return None
     return tgz_pathname
