fb95539d03944290268e490f93c6e3d0103fad8e
Katemwende <kmwende419@gmail.com>
2023-04-18 20:04:13 +0000

    Removed ' on database name

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,4 @@
--- Create a new database hbnb_test_db
+-- Creates a new database hbnb_test_db
 -- Create a new user hbnb_test
 -- The user's password is hbnb_test_pwd
 -- User should have all priviledges
@@ -6,6 +6,6 @@
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES 'hbnb_test_db'.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON 'performance_schema'.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
