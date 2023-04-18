8fa8ee3bffd8c0adf1457ae89ace32a1bcf43ef6
Tianomiano <patomiano6@gmail.com>
2023-04-18 18:07:08 +0000

    t02

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,10 @@
 -- Creation of hbnb_dev_db database
 -- Creation of new user (hbnb_dev)
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
-SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
-USE hbnb_test_db;
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+CREATE DATABASE IF NOT EXISTS test_HBNB_MYSQL_DB;
+CREATE USER IF NOT EXISTS 'HBNB_MYSQL_USER'@'HBNB_MYSQL_HOST';
+SET PASSWORD FOR 'HBNB_MYSQL_USER'@'HBNB_MYSQL_HOST' = 'HBNB_MYSQL_PWD';
+USE test_HBNB_MYSQL_DB;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'HBNB_MYSQL_USER'@'HBNB_MYSQL_HOST';
+GRANT SELECT ON performance_schema.* TO 'HBNB_MYSQL_USER'@'HBNB_MYSQL_HOST';
 FLUSH PRIVILEGES;
+
