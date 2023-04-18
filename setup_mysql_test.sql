48fba09fdfa689ff046d7dd4da45dfd7832dc8d5
Katemwende <kmwende419@gmail.com>
2023-04-18 15:52:32 +0000

    Remove 'd' from Privileges

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,10 +2,10 @@
 -- Create a new user hbnb_test
 -- The user's password is hbnb_test_pwd
 -- User should have all priviledges
--- hbnb_test should have SELECT priviledge on performance_schema
+-- hbnb_test should have SELECT privilege on performance_schema
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEDGES hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEDGES;
+FLUSH PRIVILEGES;
