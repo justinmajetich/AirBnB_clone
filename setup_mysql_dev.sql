c658b6d54904dd6902b3194dc6abb889e26d5858
KateMwende <113633390+KateMwende@users.noreply.github.com>
2023-04-18 22:53:53 +0300

    Update setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,11 +1,8 @@
 -- Creates a database hbnb_dev_db
 -- Create a new user hbnb_dev
--- The user's password is hbnb_dev_pwd
--- User should have all priviledges
--- hbnb_dev should have SELECT privilege on performance_schema
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON 'hbnb_dev_db'.* TO 'hbnb_dev'@'localhost';
-GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON 'performance_schema'.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
