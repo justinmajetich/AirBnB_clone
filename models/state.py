30d41cb7e76881177706b159626ecc365a71f010
Byron Ochieng <byronochieng01@gmail.com>
2023-04-19 17:14:53 +0300

    State update

--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', cascade='delete', backref='state')
+
+    def cities(self):
+        """
+        returns the list of City instances with state_id equals to the current State.id
+        """
+        from city import City
+        from models import storage
+        every_city = storage.all(City)
+        cities_list = []
+        
+        for key, val in every_city.items():
+            if val.state_id == self.id:
+                cities_list.append(val)
+
+        return cities_list
