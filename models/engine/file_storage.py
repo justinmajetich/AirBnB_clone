1cf81dc38f57fa5b3173cbb2970201def2e3a261
king-obobo <oboboebuka1@gmail.com>
2023-04-19 00:50:42 -0700

    Debugged the console.py file and the file storage file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,11 +9,11 @@ from models.city import City
 from models.amenity import Amenity
 from models.review import Review
 
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+classes = {
+        'BaseModel': BaseModel, 'User': User, 'Place': Place,
+        'State': State, 'City': City, 'Amenity': Amenity,
+        'Review': Review
+        }
 
 
 class FileStorage:
