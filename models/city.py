e0170bdc30e39104c5a5af6552dba4908d04f6f5
itsmraga-hub <ragawilliam570@gmail.com>
2023-04-20 01:08:12 +0300

    Task 8 - Update City class

--- models/city.py
+++ models/city.py
@@ -14,3 +14,5 @@ class City(BaseModel, Base):
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
     state = relationship("State", back_populates="cities")
+    places = relationship("Place", back_populates="places", cascade="all, delete")
+
