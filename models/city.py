264272088a7353c7afc246575bf0743b2e368ee5
itsmraga-hub <ragawilliam570@gmail.com>
2023-04-19 22:38:09 +0300

    Task 6 - Update City class to link to cities table

--- models/city.py
+++ models/city.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
+"""
+    City Module for HBNB project
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    state = relationship("State", back_populates="cities")
