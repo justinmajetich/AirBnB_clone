495c577983ac279a9fe110c46a4af47dc96a43a1
itsmraga-hub <ragawilliam570@gmail.com>
2023-04-19 22:38:36 +0300

    Task 6 - Update State class to link to states table

--- models/state.py
+++ models/state.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy import Column, Integer, String, ForeignKey
+from models.base_model import BaseModel, Base
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", back_populates="state", cascade="all, delete")
