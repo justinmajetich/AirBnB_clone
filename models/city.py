c3823ea43e9acd2443aba5745e65368a39646eec
Astro175 <aspark400@gmail.com>
2023-04-19 12:59:41 +0100

    added changes to cities and states.py

--- models/city.py
+++ models/city.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
-
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String. ForeignKey
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"
+
+    state_id = Column(String(60), nullable=false, foreign_key=True)
+    name = Column(String(60), nullable=false)
--- models/state.py
+++ models/state.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
-
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+
+    name = Column(String(128), nullable=False)
