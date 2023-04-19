dacd289087457e4398626acd025e4f418d59c80f
Tianomiano <patomiano6@gmail.com>
2023-04-19 06:33:30 +0000

    t03

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,8 @@
-reating hbnb_dev_db database
--- Privileges for new user (hbnb_dev)
+-- prepares a MySQL server for the project
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
 
