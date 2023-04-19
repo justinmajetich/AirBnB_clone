4dea1dbd6790552006fd289e917266c2ae6331d0
king-obobo <oboboebuka1@gmail.com>
2023-04-19 04:51:10 -0700

    Debugged the 'setup_myql_test.sql' file

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -6,10 +6,7 @@ CREATE USER
        IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES
       ON `hbnb_test_db`.*
-      TO 'hbnb_test'@'localhost'
-      IDENTIFIED BY 'hbnb_test_pwd';
+      TO 'hbnb_test'@'localhost';
 GRANT SELECT
       ON `performance_schema`.*
-      TO 'hbnb_test'@'localhost'
-      IDENTIFIED BY 'hbnb_test_pwd';
-FLUSH PRIVILEGES;
+      TO 'hbnb_test'@'localhost';
