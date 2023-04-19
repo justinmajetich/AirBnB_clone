50d88eaf94d45f4b2176bf5bf7eb96a200b55a2b
itsmraga-hub <ragawilliam570@gmail.com>
2023-04-20 00:09:38 +0300

    Task 6

--- models/base_model.py
+++ models/base_model.py
@@ -14,24 +14,24 @@ class BaseModel:
     id = Column(Integer, primary_key=True, nullable=False)
     created_at = Column(Date, default=datetime.utcnow(), nullable=False)
     updated_at = Column(Date, default=datetime.utcnow(), nullable=False)
+
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if kwargs:
-            self.name = kwargs["name"]
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
         else:
-            self.name = "California"
-        # if not kwargs:
-            # from models import storage
-            # self.id = str(uuid.uuid4())
-            # self.created_at = datetime.now()
-            # self.updated_at = datetime.now()
-            # storage.new(self)
-        # else:
-            # kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            # kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            # del kwargs['__class__']
+            del kwargs['__class__']
+            for key, value in kwargs.items():
+                if key != 'created_at' or key != 'updated_at':
+                    setattr(self, key, value)
+
             # self.__dict__.update(kwargs)
 
 
@@ -55,8 +55,12 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if '_sa_instance_state' in dictionary.keys():
+            del dictionary['_sa_instance_state']
         return dictionary
 
     def delete(self):
         del self
+        from models import storage
+        storage.delete(self)
 
