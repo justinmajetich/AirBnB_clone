de60eb4a92d0df446e215fdb47cd7803ca2915c2
Katemwende <kmwende419@gmail.com>
2023-04-19 19:07:30 +0000

    Changed multiple files

--- models/review.py
+++ models/review.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
-
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
 class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    """ Review classto store review information
+    place_id: gives the place id
+    user_id: gives id of user
+    text: user's review
+    """
+    __tablename__ = 'reviews'
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    text = Column(String(60), nullable=False)
--- models/user.py
+++ models/user.py
@@ -2,6 +2,8 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes
@@ -10,9 +12,13 @@ class User(BaseModel, Base):
         password: password
         first_name: user's first name
         last_name: user's last name
+        places: has a relationship with Place
+        reciews: relationship with Review
     """
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places =  relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
