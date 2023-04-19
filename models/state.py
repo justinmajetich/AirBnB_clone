bcca154e53f068dab2998d9c81011954cc589327
Astro175 <aspark400@gmail.com>
2023-04-19 19:08:50 +0100

    updated state.py

--- models/state.py
+++ models/state.py
@@ -2,9 +2,24 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-
+from sqlalchemy import relationship
 class State(BaseModel):
     """ State class """
     __tablename__ = "states"
 
     name = Column(String(128), nullable=False)
+
+    if HBNB_TYPE_STORAGE == 'db':
+        cities = relationship("City", back_populates="state",
+                cascade="all, delete-orphan")
+    else:
+        @property
+        def cities(self):
+            list_cities= []
+
+            for city in models.storage.all(City):
+                if city.state_id = self.id:
+                    list_cities.append(city)
+
+            return list_cities
+
