edfaa3e3138147d4beb570cdd46dc34b0e5537c7
itsmraga-hub <ragawilliam570@gmail.com>
2023-04-20 14:47:38 +0300

    Update storage init method

--- models/__init__.py
+++ models/__init__.py
@@ -6,8 +6,8 @@ import os
 if os.getenv("HBNB_TYPE_STORAGE") == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
-    storage.reload()
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-    storage.reload()
+
+storage.reload()
