8ad70446dc494a82e9397772ee3c854b2ae2ccd9
itsmraga-hub <ragawilliam570@gmail.com>
2023-04-20 14:48:30 +0300

    Check running env before initializing db

--- models/city.py
+++ models/city.py
@@ -3,16 +3,19 @@
     City Module for HBNB project
 """
 
-from sqlalchemy import Column, Integer, String, ForeignKey
+import os
+from sqlalchemy import Column, String, ForeignKey
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+    """ The city class, contains state ID and name """ 
     __tablename__ = "cities"
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    state = relationship("State", back_populates="cities")
-    places = relationship("Place", back_populates="places", cascade="all, delete")
-
+    if os.getenv("HBNB_TYPE_STORAGE") == 'db':
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities", cascade="all, delete")
+    else:
+        name = ""
+        state_id = ""
