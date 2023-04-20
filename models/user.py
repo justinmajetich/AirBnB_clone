c67ff1692be4c4b8a3e6198e1b987a7f2ef1074e
itsmraga-hub <ragawilliam570@gmail.com>
2023-04-20 14:49:08 +0300

    Check running env before initializing db

--- models/user.py
+++ models/user.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""
+    This module defines a class User
+"""
+import os
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
-class User(BaseModel):
+
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(126), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    places = relationship("Place", back_populates="place", cascade="all, delete")
-
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(126), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user", cascade="all, delete")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
