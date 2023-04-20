44fa13b794a5a4fe83560c7a2222a66288d1fcd6
itsmraga-hub <ragawilliam570@gmail.com>
2023-04-20 14:47:20 +0300

    Update console all method to use dtorage to load data

--- console.py
+++ console.py
@@ -3,7 +3,7 @@
 import cmd
 import sys
 from models.base_model import BaseModel
-from models.__init__ import storage
+from models import storage
 from models.user import User
 from models.place import Place
 from models.state import State
@@ -134,7 +134,6 @@ class HBNBCommand(cmd.Cmd):
             value = ls[1]
             if value.startswith('"'):
                 if '"' in value[2:-2]:
-                    print("FFFFFFFFFFF")
                     value = value.replace('"', '')
                 value = value
             elif '.' in value:
@@ -237,11 +236,11 @@ class HBNBCommand(cmd.Cmd):
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.all().items():
                 if k.split('.')[0] == args:
                     print_list.append(str(v))
         else:
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.all().items():
                 print_list.append(str(v))
 
         print(print_list)
