7c2dba9cae8ce3d0f4626e0fe034fda2eebf31e0
Arinze Nwutobo <tim.arinze@outlook.com>
2023-04-21 17:15:39 +0100

    task3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -2,7 +2,7 @@
 """
 /python/(<text>): display “Python ”, followed by the value of the text
 variable (replace underscore _ symbols with a space )
-The default value of text is “is cool”
+The default value of text is “is cool” and double route
 """
 from flask import Flask
 app = Flask(__name__)
