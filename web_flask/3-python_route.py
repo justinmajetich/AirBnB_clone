30dcc51d992022daa9cb348972ff6a851992c7ed
bikilaketema <bikilaketema94@gmail.com>
2023-04-21 10:04:30 +0300

    Done with task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This module contains a Flask web application that responds to HTTP requests with various messages.
+This module contains a Flask web application
+that responds to HTTP requests with various messages.
 """
 
 from flask import Flask
@@ -28,7 +29,9 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     """
-    Responds to a GET request with the message "C <text>", where <text> is the value of the 'text' URL parameter,
+    Responds to a GET request with the
+    message "C <text>", where <text> is
+    the value of the 'text' URL parameter,
     with underscores replaced with spaces.
     """
     return 'C {}'.format(text.replace('_', ' '))
@@ -38,12 +41,15 @@ def c(text):
 @app.route('/python/<text>', strict_slashes=False)
 def python(text):
     """
-    Responds to a GET request with the message "Python <text>", where <text> is the value of the 'text' URL parameter,
-    with underscores replaced with spaces. If no text is provided, the default value "is cool" is used instead.
+    Responds to a GET request with the
+    message "Python <text>", where <text> is
+    the value of the 'text' URL parameter,
+    with underscores replaced with spaces. If
+    no text is provided, the default
+    value "is cool" is used instead.
     """
     return 'Python {}'.format(text.replace('_', ' '))
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
