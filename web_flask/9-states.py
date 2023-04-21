fd1af096695440c178f83dc4a8c16f865fb84d62
bikilaketema <bikilaketema94@gmail.com>
2023-04-21 14:43:18 +0300

    Trying task 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -5,17 +5,8 @@ Script that starts a Flask web application
 
 from flask import Flask, render_template
 from models import storage
-from models.state import State
 
 app = Flask(__name__)
-
-
-@app.teardown_appcontext
-def close_session(exception):
-    """
-    Remove the current SQLAlchemy Session after each request
-    """
-    storage.close()
 
 
 @app.route('/states', strict_slashes=False)
@@ -23,7 +14,7 @@ def states():
     """
     Display a HTML page with the list of all State objects
     """
-    states = storage.all(State).values()
+    states = storage.all(State)
     return render_template('9-states.html', states=states_sorted)
 
 
@@ -32,9 +23,17 @@ def states_cities(id):
     """
     Display a HTML page with the list of City objects linked to a State
     """
-    state = storage.get(State, id)
-    return render_template('9-states.html', state=state, cities=cities_sorted)
+     for state in storage.all("State").values():
+        if state.id == id: 
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
 
+@app.teardown_appcontext
+def close_session(exception):
+    """ 
+    Remove the current SQLAlchemy Session after each request
+    """
+    storage.close()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
