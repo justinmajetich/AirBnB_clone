b2687e6f226f440332d467d232d90339a7027b43
bikilaketema <bikilaketema94@gmail.com>
2023-04-21 09:43:34 +0300

    Trying to debug task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
+"""
+A script that starts a Flask web application:
+It is  listening on 0.0.0.0, port 5000
+Routes:
+     /: display “Hello HBNB!”
+     /hbnb: display “HBNB”
+"""
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB'
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
