5ad44219bd3c45f29fd23a7c949f801d544a0917
b-lemy <lemabrian1234@gmail.com>
2023-04-21 17:39:56 +0300

    hello route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,3 +1,18 @@
+# !/usr/bin/python3
+""" This script starts the wsgi application and the web page
+at any ip address of 0.0.0.0 in port 5000
+"""
+
 from flask import Flask
 
-Flask
+apt = Flask(__name__)
+
+
+@apt.route('/', strict_slashes=False)
+def hello():
+    """ This function simply rendesrs the string 'Hello HBNB' """
+    return ('Hello HBNB!')
+
+
+if __name__ == "__main__":
+    apt.run(host='0.0.0.0', port=5000)
