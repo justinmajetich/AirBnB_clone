57fa3b343cc61119f5c3db82078ff0f7c654f45c
Arinze Nwutobo <tim.arinze@outlook.com>
2023-04-22 11:58:05 +0100

    task9

--- models/base_model.py
+++ models/base_model.py
@@ -44,7 +44,7 @@ class BaseModel:
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        return '[{:s}] ({:s}) {:s}'.format(self.__class__.__name__, self.id, self.__dict__)
+        return '[{:s}] ({:s}) {}'.format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
