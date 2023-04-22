3dd404ddc0ffdd5391ea887075bafa3d4b5301ee
Myles2005 <cyrile450@gmail.com>
2023-04-22 01:53:44 -0700

    1-hbnb_route.py

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Write a script that starts a Flask web application
+Your web application must be listening on 0.0.0.0, port 5000
+"""
+from flask import Flask
+
+app = Flask(__name__)
+app.strict_slashes = False
+
+@app.route('/')
+def hello():
+    return "Hello HBNB!"
+
+@app.route('/hbnb')
+def hbnb():
+    return "HBNB"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
