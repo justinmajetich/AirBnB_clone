fe6422c4b48bb4028702c1586dd0177dcac18d6a
Myles2005 <cyrile450@gmail.com>
2023-04-22 04:31:24 -0700

    hotfix

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 """
+Your web application must be listening on 0.0.0.0, port 5000
+Routes:
+/: display “Hello HBNB!”
+/hbnb: display “HBNB”
+/c/<text>: display “C ” followed by the value of the text 
+    variable (replace underscore _ symbols with a space )
 """
 from flask import Flask
 
