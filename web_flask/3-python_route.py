c7d5b37c9930fbdf00e646c433db537308273442
FionaG26 <fionamuthoni18@gmail.com>
2023-04-22 17:41:22 +0000

    tasks

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
+    /python/(<text>): Displays 'Python' followed by the value of <text>.
+"""
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return "C " + escape(text.replace("_", " "))
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    return "Python " + escape(text.replace("_", " "))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
