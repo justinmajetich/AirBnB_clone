279a07e8bf73469864d706173f3620af53f96465
Fortunejite <111015282+Fortunejite@users.noreply.github.com>
2023-04-23 20:05:54 +0100

    M

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python3
-# list of states
+""" this module contains a script that starts a Flask web application
+    the web application must be listening on 0.0.0.0, port 5000
+    Routes: - /states_list """
+from models import *
+from models.base_model import BaseModel, Base
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 from flask import Flask, render_template
-from models import storage
 app = Flask(__name__)
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
-@app.route('/states_list')
-def stateList():
-    # lists states in html
-    return render_template('8-cities_by_states.html',
-                           storage=storage.all('State'))
-
-
-@app.route('/cities_by_states')
-def cityStateList():
-    # lists states in html
-    return render_template('8-cities_by_states.html',
-                           storage=storage.all('State'))
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """ display HTML page with list of cities """
+    states = storage.all(classes["State"]).values()
+    # ^ fetches states data from storage engine, then in line below,
+    # those states are passed into the template
+    return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
-def closer(exception):
+def remove_SQLalc_session(exception):
+    """ close storage when tear down is called """
     storage.close()
 
-
-if __name__ == "__main__":
-    app.url_map.strict_slashes = False
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
