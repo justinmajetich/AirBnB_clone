0220b8776d19fb52d52ecaea5d7feedc28dc3889
chineduCoded <chinedujohn172gmail.com>
2023-04-23 18:12:28 -0700

    tried fixing

--- models/state.py
+++ models/state.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """This is the state class"""
-import os
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String
 import models
 from models.city import City
+import shlex
 
 
 class State(BaseModel, Base):
@@ -16,13 +16,20 @@ class State(BaseModel, Base):
     """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship("City", cascade='all, delete, '
-                                              'delete-orphan', backref="state")
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
 
-    else:
-        @property
-        def cities(self):
-            """public getter method cities to return the list of City"""
-            city_objs = models.storage.all(City).values()
-            return [city for city in city_objs if city.state_id == self.id]
+    @property
+    def cities(self):
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
