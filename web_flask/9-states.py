73b5d8457b0ae668f6f58bfdd3513e1eac106153
chineduCoded <chinedujohn172gmail.com>
2023-04-23 18:25:08 -0700

    Task 9

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
+Listening on 0.0.0.0, port 5000.
 Routes:
     /states: HTML page with a list of all State objects.
     /states/<id>: HTML page displaying the given state with <id>.
