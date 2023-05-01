8c47ad341a417c9e30758eed22d8abd7f0f04a20
chineduCoded <chinedujohn172gmail.com>
2023-04-30 22:15:44 -0700

    Updatte

--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This is the state class"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String
--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,8 +7,14 @@ Routes:
 from models import storage
 from flask import Flask
 from flask import render_template
+from models.state import State
 
 app = Flask(__name__)
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
 
 
 @app.route("/states_list", strict_slashes=False)
@@ -16,14 +22,12 @@ def states_list():
     """Displays an HTML page with a list of all State objects in DBStorage.
     States are sorted by name.
     """
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
-
-
-@app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
-    storage.close()
+    states = storage.all(State)
+    states_list = []
+    for state_id, state in states.items():
+        states_list.append((state.name, state.id))
+    states_list.sort()
+    return render_template('7-states_list.html', states_list=states_list)
 
 
 if __name__ == "__main__":
