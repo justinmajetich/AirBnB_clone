747976eb8d1c1edf72f3893477839a9e32245c85
Bantamlak <bantamlak29@gmail.com>
2023-05-19 22:31:02 +0300

    task 7: first commit

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" A script that lists states"""
+from flask import Flask, render_template
+from models import storage
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Display an HTML page"""
+    states = storage.all('State').values()
+    states_sorted = sorted(states, key=lambda s: s.name)
+    return render_template('7-states_list.html', States=states_sorted)
+
+
+@app.teardown_appcontext
+def teardown_app_context(exception):
+    """It removes the current SQLAlchemy sessions after each requests"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
