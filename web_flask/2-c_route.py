4409b16e1b70195ce57bf2fce009af24d6b3704f
Bantamlak <bantamlak29@gmail.com>
2023-05-19 11:48:56 +0300

    task 2: first commit

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" A script that starts a Flask web application:
+
+    Add route:
+        /c/<text>: display “C ” followed by the value of the text variable
+        (replace underscore _ symbols with a space )
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def Hello_HBNB():
+    """Display Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """Display HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_is(text):
+    """Display C followed by text"""
+    replaced_text = text.replace('_', ' ')
+    return 'C {}'.format(replaced_text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
