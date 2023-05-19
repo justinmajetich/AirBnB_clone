358a531acafa23913ff41eb58f9b1a7d4e4e88b9
NnaemekaAlgorithim <nnaemekanwodo625@gmail.com>
2023-05-19 18:28:48 +0100

    fixed checker error

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
-    return "Hello, HBNB!"
+    return "Hello HBNB!"
 
     if __name__ == "__main__":
         app.run(host="0.0.0.0")
