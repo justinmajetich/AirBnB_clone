82f7b9ed9c30076bf62a7977712af10f263fcc83
Bantamlak <bantamlak29@gmail.com>
2023-05-19 11:40:06 +0300

    Initial commit

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" A script that starts a Flask web application.
+    The web application must be listening on 0.0.0.0, port 5000
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def Hello_HBNB():
+    """Display Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
