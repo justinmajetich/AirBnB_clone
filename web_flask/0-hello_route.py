25776c677ce17151ffdd4c088acbfc08ed40ceb6
FionaG26 <fionamuthoni18@gmail.com>
2023-05-19 11:35:08 +0000

    task o

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,17 +2,17 @@
 """A script that starts a flask web application
 Your web application must be listening on 0.0.0.0, port 5000
 """
-
+# Import Flask
 from flask import Flask
 
-app = Flask("__name__")
+# Create an instance of Flask
+app = Flask(__name__)
 
+# Route decorator to map the URL route to a function
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def hello_route():
+    return 'Hello HBNB!'
 
-@app.route('/airbnb-onepage/')
-def hello():
-    """Return a given string"""
-    return ("Hello HBNB!")
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=None)
+# Run the Flask application
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
