38372e03faaf8b459f97a8fd2fb0b55df7deea6a
FionaG26 <fionamuthoni18@gmail.com>
2023-05-19 13:49:30 +0000

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,15 +2,13 @@
 """A script that starts a Flask web application
 Your web application must be listening on 0.0.0.0, port 5000
 """
-import subprocess
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_route():
-    return 'Hello HBNB!'
+    return 'Hello HBNB!'.strip()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
