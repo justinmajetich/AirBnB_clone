00d1efc1b66110cd35ef105b678662d90be130eb
Bantamlak <bantamlak29@gmail.com>
2023-05-21 14:13:06 +0300

    task 10: documentation

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""A script"""
+"""A script that starts a Flask web application"""
 from flask import Flask, render_template
 from models import storage
 from models import State
