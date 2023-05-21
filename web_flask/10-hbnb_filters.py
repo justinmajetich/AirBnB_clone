0c5b9a0a8add24aa0bd29e7590701080c76ac4d4
Bantamlak <bantamlak29@gmail.com>
2023-05-21 13:29:26 +0300

    task 10: solution files and directory uploaded

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""A script"""
+from flask import Flask, render_template
+from models import storage
+from models import State
+from models import Amenity
+
+app = Flask(__name__)
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    """ Display a HTML page like 6-index.html, which was done during
+        the project 0x01. AirBnB clone - Web static
+    """
+    states = storage.all(State).values()
+    amenities = storage.all(Amenity).values()
+    return render_template('10-hbnb_filters.html', states=states,
+                           amenities=amenities)
+
+
+@app.teardown_appcontext
+def teardown_app_context(exception):
+    """It removes the current SQLAlchemy sessions after each requests"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
