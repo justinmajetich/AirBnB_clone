9a6554d072b822631432274e4212033d42dea509
kalkidanh <kalkidanh89@gmail.com>
2023-05-21 20:49:26 +0300

    edit documentation

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -30,9 +30,9 @@ def p3iscool(text='is cool'):
     return 'Python ' + text.replace('_', ' ')
 
 
-@app.route('/number/<n>', strict_slashes-False)
+@app.route('/number/<int:n>', strict_slashes-False)
 def isanum(n):
-    """Function that returns is a number if n is an integer."""
+    """Function that returns "n is a number" if n is an integer."""
     return "{:d} is a number".format(n)
 
 
