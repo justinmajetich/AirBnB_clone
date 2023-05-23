484435782404b54a75e137d2c1e2a9aebcdbc1c3
TSOlami <tijani.saheed@yahoo.com>
2023-05-23 18:06:55 +0100

    Fixing DB & File Storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -94,4 +94,4 @@ class DBStorage:
 
     def close(self):
         """closes the working SQLAlchemy session"""
-        self.__session.close()
+        self.__session.remove()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
