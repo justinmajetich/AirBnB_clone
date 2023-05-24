940bc7d452f48a4a191b7dc269881364c1e98454
NnaemekaAlgorithim <nnaemekanwodo625@gmail.com>
2023-05-24 10:35:56 +0100

    fixed errors

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
-"""Runs the Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'
-"""
+"""Module - script that starts a Flask web application"""
 from flask import Flask
-
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
-    return "Hello HBNB!"
+    """Handles the root url"""
+    return 'Hello HBNB!'
 
-    if __name__ == "__main__":
-        app.run(host='0.0.0.0:5000')
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", 5000)
