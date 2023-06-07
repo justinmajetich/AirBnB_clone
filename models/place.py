f44734f34950c1a09912d2ade719b1b9e02969c6
Onwuta Ebube Gideon <onwutaebubegideon1555@gmail.com>
2023-06-07 15:13:52 +0000

    Updated docstrings in place.py

--- models/place.py
+++ models/place.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
+"""
+Define the ``Place`` class that inherits from the class ``BaseModel``
+"""
+
 from models.base_model import BaseModel
 
 
 class Place(BaseModel):
-    """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+    """
+    Define the class Place
+    """
+
+    city_id = str()     # City.id
+    user_id = str()     # User.id
+    name = str()
+    description = str()
+    number_rooms = int()
+    number_bathrooms = int()
+    max_guest = int()
+    price_by_night = int()
+    latitude = float()
+    longitude = float()
+    amenity_ids = list(str())   # list of Amenity.id
