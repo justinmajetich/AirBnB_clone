6b430d951b736bf6ecde73fcff604c65eba4ba01
Onwuta Ebube Gideon <onwutaebubegideon1555@gmail.com>
2023-06-07 15:44:25 +0000

    Updated docstrings in file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,25 +1,37 @@
 #!/usr/bin/python3
 """
-This module defines a class to manage file storage for AirBnB clone
+Define the class ``FileStorage`` for serializing and deserailizing
+instances from file to JSON, vice versa
 """
+
 import json
 
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
-    __file_path = 'file.json'
-    __objects = {}
+    """
+    Serialize instances to JSON file and deserialze JSON file to instances
+    """
+    __file_path = "file.json"   # Path to the JSON file
+    __objects = dict()      # Stores all objects by <class name>.id
 
     def all(self):
-        """Returns a dictionary of models currently in storage"""
+        """
+        Return a dictionary of models currently in storage
+        """
         return FileStorage.__objects
 
     def new(self, obj):
-        """Adds new object to storage dictionary"""
+        """
+        Set in ``__objects the ``obj`` with the key ``<obj class name>.id``
+
+        Where, object is an instance of ``<obj class name>``
+        """
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
-        """Saves storage dictionary to file"""
+        """
+        Serialize ``__objects`` to the JSON file ``__file_path``
+        """
         with open(FileStorage.__file_path, 'w') as f:
             temp = {}
             temp.update(FileStorage.__objects)
@@ -28,7 +40,9 @@ class FileStorage:
             json.dump(temp, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
+        """
+        Deserialize the JSON file to ``__objects``
+        """
         from models.base_model import BaseModel
         from models.user import User
         from models.place import Place
