f97ba8db15662101e4145ea392568a6864773c56
Onwuta Ebube Gideon <onwutaebubegideon1555@gmail.com>
2023-06-07 15:19:07 +0000

    Updated docstrings in __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+"""
+Resources for this package
 
+This module instantiates an object of class FileStorage
+"""
+
+# Create a unique FileStorage instance for this application
+from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
