579ee058e1c69a05488d37a99de0464d34a7f060
Dozie2001 <davidchidozie402@gmail.com>
2023-06-11 10:23:40 +0100

    Changed file storage

--- console.py
+++ console.py
@@ -115,17 +115,22 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        if not args:
-            print("** class name missing **")
-            return
-        elif args not in HBNBCommand.classes:
-            print("** class doesn't exist **")
-            return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
-        print(new_instance.id)
-        storage.save()
-
+        try:
+            if not args:
+                raise SyntaxError()
+            arg_list = args.split(" ")
+            kw = {}
+            for arg in arg_list[1:]:
+                arg_splitted = arg.split("=")
+                arg_splitted[1] = eval(arg_splitted[1])
+                if type(arg_splitted[1]) is str:
+                    arg_splitted[1] = arg_splitted[1].replace("_", " ").replace('"', '\\"')
+                kw[arg_splitted[0]] = arg_splitted[1]
+        except SyntaxError:
+            print('** class name missing **')
+        except NameError:
+            print("** Class doesn't exist **")
+        new_instance = HBNBCommand.classes[arg_list[0]](**kw)
     def help_create(self):
         """ Help information for the create method """
         print("Creates a class of any type")
