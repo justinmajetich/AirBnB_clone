2ecd43b9b105a062845b0f4f52f61262695d510e
AvitBrian <113444617+AvitBrian@users.noreply.github.com>
2023-06-12 22:10:55 +0200

    updateinggi

--- models/city.py
+++ models/city.py
@@ -4,6 +4,9 @@ from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    """ The city class, contains state ID, name and places"""
+    __tablename__ = 'cities'
+
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    places = relationship("Place", backref="cities", cascade="delete")
