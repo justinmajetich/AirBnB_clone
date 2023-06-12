266170bfcc8b5b5dccd923d8c7fc70f4caeb917d
AvitBrian <113444617+AvitBrian@users.noreply.github.com>
2023-06-12 21:01:50 +0200

    updates

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
+"""defines a class to manage file storage for hbnb clone"""
 import json
 
 
@@ -8,9 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+        else:
+            temp = {}
+            for key, val in FileStorage.__objects.items():
+                if cls.__name__ in key:
+                    temp[key] = val
+            return temp
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -50,7 +57,7 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
-        """Deletes obj from __objects if it’s inside"""
+        """Deleting stuff from the database."""
         if obj is not None:
             key = obj.__class__.__name__ + '.' + obj.id
             if key in FileStorage.__objects:
