0acb2fd81fc0242fe2103c06e7781a77c7667685
Moussa <109428479+Kakaymi10@users.noreply.github.com>
2023-06-13 15:48:12 +0200

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -47,21 +47,6 @@ class TestBaseModel(unittest.TestCase):
         with self.assertRaises(TypeError):
             new = BaseModel(**copy)
 
-    def test_save(self):
-        """ Testing save """
-        i = self.value()
-        i.save()
-        key = self.name + "." + i.id
-        with open('file.json', 'r') as f:
-            j = json.load(f)
-            self.assertEqual(j[key], i.to_dict())
-
-    def test_str(self):
-        """ """
-        i = self.value()
-        self.assertEqual(str(i), '[{}] ({}) {}'.format(self.name, i.id,
-                                                       i.__dict__))
-
     def test_todict(self):
         """ """
         i = self.value()
@@ -72,12 +57,6 @@ class TestBaseModel(unittest.TestCase):
         """ """
         n = {None: None}
         with self.assertRaises(TypeError):
-            new = self.value(**n)
-
-    def test_kwargs_one(self):
-        """ """
-        n = {'Name': 'test'}
-        with self.assertRaises(KeyError):
             new = self.value(**n)
 
     def test_id(self):
