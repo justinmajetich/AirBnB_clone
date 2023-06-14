15ec579a952380a6d7028e8b20d2ff6b70302512
Onwuta Ebube Gideon <onwutaebubegideon1555@gmail.com>
2023-06-14 16:51:25 +0000

    Updated User model
    Made User class inherit from declarative base and created columns for the model

--- models/user.py
+++ models/user.py
@@ -1,17 +1,37 @@
 #!/usr/bin/python3
 """
 Define the ``User`` class that inherits from the class ``BaseModel``
+and 'Base'
 """
 
 from models.base_model import BaseModel
+from models.base_model import Base
+from models.base_model import (
+        Column,
+        String
+        )
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """
     Define the class User
-    """
 
-    email = str()
-    password = str()
-    first_name = str()
-    last_name = str()
+    * __tablename__: represents tha table name, 'users'
+    * email (String): represents a column containing a string (128 characters)
+        * can't be null
+    * password (String): represents a column containing a string
+        (128 characters)
+        * can't be null
+    * first_name (String): represents a column containing a string
+        (128 characters)
+        * can be null
+    * last_name (String): represents a column containing a string
+        (128 characters)
+        * can be null
+    """
+    __tablename__ = "users"
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
