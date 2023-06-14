401dc16a4ae35792147a57e84d382c6aa712886b
AvitBrian <113444617+AvitBrian@users.noreply.github.com>
2023-06-15 00:52:01 +0200

    updates

--- models/review.py
+++ models/review.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, ForeignKey, String
 
 
