1895bce51545a4c246d4ff08b194bb56cb5c81ad
TSOlami <113607787+TSOlami@users.noreply.github.com>
2023-06-15 21:03:18 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""
-This module defines a class to manage file storage for hbnb clone
-"""
+# This module defines a class to manage file storage for hbnb clone
+
 
 import json
 from models.amenity import Amenity
