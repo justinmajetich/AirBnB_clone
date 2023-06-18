f4ff425a793215a541debe2011d865aae540a0ba
OBI CHIEMERIE     ("THALES") <obiwinner94@gmail.com>
2023-06-18 11:17:38 +0100

    SQL SET UP

---
+++ setup_mysql_test.sql
@@ -0,0 +1,12 @@
+-- This script prepares a MySQL server for the project
+-- Creates project testing database with the name : hbnb_test_db
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- Creating new user named : hbnb_test with all privileges on the db hbnb_test_db
+-- with the password : hbnb_test_pwd if it dosen't exist
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- Granting the SELECT privilege for the new user hbnb_test on the db performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
+-- Granting auto privileges to the new user on hbnb_test_db
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
