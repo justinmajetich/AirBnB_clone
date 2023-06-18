e4ed7726e8c4041f1c3355fb8d2d4b373dd18333
Thatagricguy <samueljohn3999@gmail.com>
2023-06-18 22:01:18 +0100

    test

--- select_states.py
+++ select_states.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     from the database.
     """
     db_connect = MySQLdb.connect(
-        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+        host="localhost", user=argv[1], passwd=argv[2], db=argv[3])
 
     db_cursor = db_connect.cursor()
 
