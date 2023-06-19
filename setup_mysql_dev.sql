f3d2fd9cbe2d4791613f0cb15af00257eae68ce7
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-19 13:32:10 +0000

    removed string lterals

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,8 +7,8 @@ USE DATABASE hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grant all privileges on the database hbnb_dev_db
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- Grant SELECT privileges on the performance schema
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost' ; 
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost' ; 
 FLUSH PRIVILEGES;
