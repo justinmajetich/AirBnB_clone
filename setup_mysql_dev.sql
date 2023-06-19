88e61e2b508db2300621c156f53d0442849c0240
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-06-19 16:07:20 +0100

    (fix) -> modifiy privileges on performance_schema

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,8 +7,9 @@
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 USE hbnb_dev_db;
-CREATE USER IF NOT EXISTS hbnb_dev;
-ALTER USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+ALTER USER 'hbnb_dev'@'localhost'
+      IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT PRIVILEGES ON performance_schema TO 'hbnb_dev'@'localhost';
+GRANT SELECT PRIVILEGES ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
