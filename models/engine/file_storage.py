a93c0c45d0ae736c995524652a55bef5d1a4185d
Akuya Ekorot <akuyaekorot@gmail.com>
2023-06-19 10:53:35 +0300

    delelte object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        temp = {}
+        if cls is None:
+            return FileStorage.__objects
+        for key, val in FileStorage.__objects.items():
+            if cls == val.__class__:
+                temp[key] = val
+        return temp
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -24,6 +30,15 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        """Deletes obj from __objects if it’s inside"""
+        if obj is None:
+            return
+        key = obj.__class__.__name__ + '.' + obj.id
+        if key in self.__objects:
+            del self.__objects[key]
+        self.save()
 
     def reload(self):
         """Loads storage dictionary from file"""
