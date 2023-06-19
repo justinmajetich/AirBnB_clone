e0289051f8391cb2cfca0383e4edc846b5d7d879
nazarenegena <genarene96@gmail.com>
2023-06-19 14:45:51 +0000

    file storage updated with delete function

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,7 +7,7 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         return FileStorage.__objects
 
@@ -49,3 +49,10 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+    
+    def delete(self, obj=None):
+        """deletes existing element"""
+        if obj:
+            keyVal = "{}.{}".format(type(obj).__name__, obj.id)
+            del self.__objects[keyVal]
+
