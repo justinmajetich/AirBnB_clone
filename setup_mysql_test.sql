547841144971314884860f66fac3bc36694257fb
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-19 13:44:31 +0000

    test db

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,13 +3,14 @@
 -- Create a db to test the project
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
--- Create new user
+-- creating new user hbnb_test on db hbnb_test_db
+
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
--- Grant all privileges
-GRANT ALL PRIVILEGES ON hbnb_test-db.* TO 'hbnb_test'@'localhost';
+-- granting the SELECT privilege for the user hbnb_test on the db performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
 
---Grant SELECT Privileges on db=performance_schema
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+-- granting all privileges to new user on hbnb_test_db
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
