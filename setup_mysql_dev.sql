36de85436104e19086354c8a59e4c8d42bb0f89a
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-19 12:00:39 +0000

    script that prepares a MySQL server for the project

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,15 @@
+-- Script that prepares a MySQL server
+-- creating the database hbnb_dev_db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+USE DATABASE hbnb_dev_db;
+
+-- create new user @ localhost
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant all privileges on the database hbnb_dev_db
+GRANT ALL PRIVILEGES ON hbnb_test_db TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
+
+-- Grant SELECT privileges on the performance schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost' ; 
+FLUSH PRIVILEGES;
