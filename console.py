9166221da4a66ff01640dbdda281a84c8aa152b0
desmends27 <desmondmends27@gmail.com>
2023-06-19 15:35:41 +0000

    modified console

--- console.py
+++ console.py
@@ -121,7 +121,6 @@ class HBNBCommand(cmd.Cmd):
         elif args not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        """this will be deleted"""
         new_instance = HBNBCommand.classes[args]()
         storage.save()
         print(new_instance.id)
