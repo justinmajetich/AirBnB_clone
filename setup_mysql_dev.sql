4f68a94f52ce78533ec15c0ab2cd3a4e1eab7613
Robert Ruai Mawich <robertruai4@gmail.com>
2023-06-19 07:52:04 -0700

    minor sql edits

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,9 @@
 -- Create the hbnb_dev_db database if it doesn't exist
-IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'hbnb_dev_db')
-    CREATE DATABASE hbnb_dev_db;
+ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- Create the hbnb_dev user if it doesn't exist
- CREATE LOGIN hbnb_dev WITH PASSWORD = 'hbnb_dev_pwd';
+ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- Grant all privileges on hbnb_dev_db to hbnb_dev
-GRANT ALL PRIVILEGES ON SCHEMA::dbo TO hbnb_dev;
+ GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
 -- Grant SELECT privilege on performance_schema to hbnb_dev
-GRANT SELECT ON SCHEMA::performance_schema TO hbnb_dev;
+ GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+ FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,16 +1,9 @@
 -- Create the hbnb_test_db database if it doesn't exist
-IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'hbnb_test_db')
-    CREATE DATABASE hbnb_test_db;
-
+ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- Create the hbnb_test user if it doesn't exist
-IF NOT EXISTS (SELECT * FROM sys.syslogins WHERE name = 'hbnb_test')
-    CREATE LOGIN hbnb_test WITH PASSWORD = 'hbnb_test_pwd';
-
+ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- Grant all privileges on hbnb_test_db to hbnb_test
-USE hbnb_test_db;
-IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = 'hbnb_test')
-    CREATE USER hbnb_test FOR LOGIN hbnb_test;
-ALTER ROLE db_owner ADD MEMBER hbnb_test;
-
+ GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
 -- Grant SELECT privilege on performance_schema to hbnb_test
-GRANT SELECT ON SCHEMA::performance_schema TO hbnb_test;
+ GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+ FLUSH PRIVILEGES;
