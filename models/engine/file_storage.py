aa5b2c500aa59a1fb03488325b9659b01a20f282
nazarenegena <genarene96@gmail.com>
2023-06-19 14:34:32 +0000

    import file added

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,7 +2,6 @@
 """This module defines a class to manage file storage for hbnb clone"""
 import json
 
-
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
@@ -34,6 +33,8 @@ class FileStorage:
         from models.city import City
         from models.amenity import Amenity
         from models.review import Review
+        import shlex
+
 
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
