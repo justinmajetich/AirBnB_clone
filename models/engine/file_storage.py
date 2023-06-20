cdeeb0a48bcdc5dbf0081ff59ee64f3941abbb97
desmends27 <desmondmends27@gmail.com>
2023-06-20 17:09:57 +0000

    test

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
-
+        object = {}
+        if cls is None:
+            return FileStorage.__objects
+        for key, value in FileStorage.__objects.items():
+            if isinstance(value, cls):
+                object[key] = value
+        return object
     def new(self, obj):
         """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
@@ -27,13 +38,6 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
 
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
@@ -48,3 +52,12 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+    
+    def delete(self, obj=None):
+        if obj is None:
+            pass
+        else:
+            for key,value in FileStorage.__objects.items():
+                if (obj == value):
+                    del FileStorage.__objects[key]
+                    break
