4932b85639fd3ef206ec1b3c1f0df1da125a321d
Laure Tchuente <solaure2t@yahoo.fr>
2023-06-19 19:51:50 -0700

    update task 7

--- models/user.py
+++ models/user.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-import models
-from models.city import City
 from sqlalchemy import Column, Integer, String, MetaData, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 import MySQLdb
-import shlex
 from sqlalchemy.orm import relationship
 from models.place import Place
 
@@ -16,6 +13,6 @@ class User(BaseModel, Base):
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
     places = relationship("Place", cascade='all, delete, delete-orphan', backref="user")
