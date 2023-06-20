5efa56b27aa0dd4875b78f8c86f62c8bf91b0de5
nazarenegena <genarene96@gmail.com>
2023-06-20 07:28:22 +0000

    added dependancies

--- models/user.py
+++ models/user.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
-from sqlalchemy import Column, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from models.review import Review
+from models.place import Place
+
 
 
 class User(BaseModel):
-    """This class defines a user by various attributes"""
+    """The class defines a user by various attributes"""
      __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
 
