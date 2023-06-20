60c40e95ccdf43032a7e7940554643e73eb84102
fatma tnani <fatmatnani08@gmail.com>
2023-06-20 11:32:39 -0700

    sql setup development

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,7 @@
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create the user with the specified privileges
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
