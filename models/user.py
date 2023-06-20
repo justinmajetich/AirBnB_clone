5ecafe805ff373b70a547e7f107ff8173f3856b4
nazarenegena <genarene96@gmail.com>
2023-06-20 07:38:57 +0000

    deleted repositorires

--- models/user.py
+++ models/user.py
@@ -11,7 +11,7 @@ from models.place import Place
 
 class User(BaseModel):
     """The class defines a user by various attributes"""
-     __tablename__ = 'users'
+    __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
