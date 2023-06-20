88df5fff3b65a03499d4678bc9fbdaef74b915de
desmends27 <desmondmends27@gmail.com>
2023-06-20 16:15:03 +0000

    test for strange behaviour

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,6 @@
--- Create database if it doesn't exist
+--Create a user, a database and grant pivileges on the database to the user
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE  USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT on performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
-
