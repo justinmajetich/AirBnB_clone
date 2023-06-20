6952c165b40b86ad81555cac0546e03bd5454bd2
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-20 08:55:55 +0000

    database storage

--- models/user.py
+++ models/user.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-import os
 from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
-storage = os.getenv('HBNB_TYPE_STORAGE')
+from os import getenv
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
 
-    if (storage == 'db'):
-        __tablename__ = "users"
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128))
-        last_name = Column(String(128))
+    if (os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__="users"
+        email=Column(String(128), nullable=False)
+        password=Column(String(128), nullable=False)
+        first_name=Column(String(128))
+        last_name=Column(String(128))
+        places=relationship("Place", backref="user", cascade="delete")
+        review=relationship("Review", backref="user", cascade="delete")
+    else:
+
+        email=""
+        password=""
+        first_name=""
+        last_name=""
+
