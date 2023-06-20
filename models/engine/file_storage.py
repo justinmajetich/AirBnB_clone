e948eea1023f7ddf210fa72cfb5b72f9a6761dc9
Chukwuemeka Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-06-20 21:26:53 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,56 +1,31 @@
 #!/usr/bin/python3
-
+"""This module defines a class to manage file storage for hbnb clone"""
 import json
 
 
 class FileStorage:
+    """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
 
     def all(self, cls=None):
-<<<<<<< HEAD
-=======
         """Returns a dictionary or list of models currently in storage"""
->>>>>>> dd568910edba756fb6434455eb1b9f67ab495477
         if cls is None:
             return FileStorage.__objects
         else:
             filtered_objects = {}
             for key, value in FileStorage.__objects.items():
-<<<<<<< HEAD
-                if isinstance(value, cls):
-=======
                 class_name = key.split('.')[0]
                 if class_name == cls.__name__:
->>>>>>> dd568910edba756fb6434455eb1b9f67ab495477
                     filtered_objects[key] = value
             return filtered_objects
 
     def new(self, obj):
-<<<<<<< HEAD
-=======
         """Adds new object to storage dictionary"""
->>>>>>> dd568910edba756fb6434455eb1b9f67ab495477
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         FileStorage.__objects[key] = obj
 
     def save(self):
-<<<<<<< HEAD
-        temp_dict = {}
-        for key, value in FileStorage.__objects.items():
-            temp_dict[key] = value.to_dict()
-        with open(FileStorage.__file_path, 'w') as file:
-            json.dump(temp_dict, file)
-
-    def reload(self):
-        try:
-            with open(FileStorage.__file_path, 'r') as file:
-                temp_dict = json.load(file)
-                for key, value in temp_dict.items():
-                    cls_name = value['__class__']
-                    cls = eval(cls_name)
-                    FileStorage.__objects[key] = cls(**value)
-=======
         """Saves storage dictionary to file"""
         temp = {}
         for key, val in FileStorage.__objects.items():
@@ -80,19 +55,12 @@ class FileStorage:
                     class_name = val['__class__']
                     obj = classes[class_name](**val)
                     FileStorage.__objects[key] = obj
->>>>>>> dd568910edba756fb6434455eb1b9f67ab495477
         except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
-<<<<<<< HEAD
-        if obj is not None:
-            key = "{}.{}".format(obj.__class__.__name__, obj.id)
-            FileStorage.__objects.pop(key, None)
-=======
         """Deletes obj from __objects if it exists"""
         if obj is not None:
             key = "{}.{}".format(obj.__class__.__name__, obj.id)
             if key in FileStorage.__objects:
                 del FileStorage.__objects[key]
->>>>>>> dd568910edba756fb6434455eb1b9f67ab495477
