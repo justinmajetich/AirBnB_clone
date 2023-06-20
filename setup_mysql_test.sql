8384ec6d0f4c6e9be2ea5d20eb2d339ac79d58f8
fatma tnani <fatmatnani08@gmail.com>
2023-06-20 11:39:05 -0700

    mysql setup test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create the user with the specified privileges
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
