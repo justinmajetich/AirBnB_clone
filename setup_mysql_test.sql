3ea7b1e71506e859d0b9f41973bc07fb681f3320
desmends27 <desmondmends27@gmail.com>
2023-06-20 16:26:49 +0000

    space after the comment

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,4 @@
---Create a database;
+-- Create a database;
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
