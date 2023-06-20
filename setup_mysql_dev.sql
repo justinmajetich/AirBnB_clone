ad7c1a8f9a62f0499832d34e68cf35b385586102
desmends27 <desmondmends27@gmail.com>
2023-06-20 14:45:36 +0000

    removed grant option

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,6 @@
 --Create a user, a database and grant pirvileges on the database to the user
 CREATE  USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-GRANT SELECT on performance_schema.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
