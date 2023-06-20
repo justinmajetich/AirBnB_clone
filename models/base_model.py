7ef72349733e7277df7508661d95f4a240b739c9
Laure Tchuente <solaure2t@yahoo.fr>
2023-06-20 06:41:13 -0700

    update compilation

--- models/base_model.py
+++ models/base_model.py
@@ -39,8 +39,8 @@ class BaseModel:
     def __str__(self):
         """Returns a string representation of the instance"""
         """cls = (str(type(self)).split('.')[-1]).split('\'')[0]"""
-        return '[{}] ({}) {}'
-    .format(type(self).__name__, self.id, self.__dict__)
+        ty = type(self).__name__
+        return '[{}] ({}) {}'.format(ty, self.id, self.__dict__)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
