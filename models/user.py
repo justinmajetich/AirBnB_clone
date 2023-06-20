59af9d681ed84a2e3655091393034efc1f33ef20
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-06-20 19:32:52 +0100

    (fix) -> remove first_name and last_name constraint

--- models/user.py
+++ models/user.py
@@ -22,8 +22,8 @@ class User(BaseModel, Base):
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
 
     places = relationship('Place', backref='user',
                           cascade="all, delete-orphan")
