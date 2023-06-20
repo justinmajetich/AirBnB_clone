66ed961055da3b45437899a7dd2ef4ceaa091950
desmends27 <desmondmends27@gmail.com>
2023-06-20 15:08:16 +0000

    used caps for on

--- test.sql
+++ test.sql
@@ -1,4 +1,4 @@
---Create a user, a database and grant pirvileges on the database to the user
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
