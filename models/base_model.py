c68b62bf73730b2ea0a26152ec3f3954248b87c3
Chukwuemeka Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-06-20 23:27:54 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ import uuid
 from models import storage
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime
 
 Base = declarative_base()
 
