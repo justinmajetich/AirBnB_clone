2538992967c723186dd4dbd3787ea3ae70c15f1b
desmends27 <desmondmends27@gmail.com>
2023-06-20 14:42:54 +0000

    added comments

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,4 @@
+--Create a user, a database and grant pirvileges on the database to the user
 CREATE  USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 GRANT SELECT on performance_schema.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
