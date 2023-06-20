65c10e36be8b8aba863b650a9add4212e9923448
6godpro <erickson5115@gmail.com>
2023-06-20 17:58:26 +0100

    (fix)-> fixed all storage class methods + minor table fix

--- console.py
+++ console.py
@@ -131,7 +131,7 @@ class HBNBCommand(cmd.Cmd):
         new_instance = HBNBCommand.classes[c_name]()
         if dictionary is not None:
             new_instance.__dict__.update(dictionary)
-        storage.save()
+        new_instance.save()
         print(new_instance.id)
 
     def help_create(self):
@@ -214,11 +214,11 @@ class HBNBCommand(cmd.Cmd):
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.all(HBNBCommand.classes[args]).items():
                 if k.split('.')[0] == args:
                     print_list.append(str(v))
         else:
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.all().items():
                 print_list.append(str(v))
 
         print(print_list)
--- models/state.py
+++ models/state.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models.engine import Filestorage
+from models.engine.file_storage import FileStorage
 from models.city import City
-from sqlalchemy Column, String, Integer
+from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import relationship
 from os import getenv
 
@@ -11,7 +11,7 @@ from os import getenv
 class State(BaseModel, Base):
     """ The State class """
     __tablename__ = "states"
-    name = Column(String(128) nullable=False)
+    name = Column(String(128), nullable=False)
     if getenv("HBNB_TYPE_STORAGE") == "db":
         cities = relationship("City", backref="state",
                               cascade="all, delete-orphan")
@@ -19,5 +19,5 @@ class State(BaseModel, Base):
         @property
         def cities(self):
             return [obj for obj
-                    in Filestorage.all(City).values()
+                    in FileStorage.all(City).values()
                     if obj.state_id == self.id]
