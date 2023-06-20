d7a7c944013f0dd95369a1d8fef093459247b81a
belladesign <81829917+belladesign@users.noreply.github.com>
2023-06-20 14:36:19 +0100

    Create more files for web flask

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,8 +6,14 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbn():
-    """ Return Hello HBNB! from 0.0.0.0:5000 """
+    """ Returns Hello HBNB! from 0.0.0.0:5000 """
     return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Returns HBNB from 0.0.0.0:5000/hbnb """
+    return "HBNB"
 
 
 if __name__ == '__main__':
