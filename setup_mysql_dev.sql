172ab19a45d523bc553226a2bd9042e43662e5f8
desmends27 <desmondmends27@gmail.com>
2023-06-20 14:46:25 +0000

    test

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,14 @@
---Create a user, a database and grant pirvileges on the database to the user
-CREATE  USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Create database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Create user if it doesn't exist and set password
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant all privileges on hbnb_dev_db to hbnb_dev
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on performance_schema to hbnb_dev
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Flush privileges to apply changes
 FLUSH PRIVILEGES;
