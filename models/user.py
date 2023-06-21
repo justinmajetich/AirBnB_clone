490ec5db20331657f328fb20a88483e8e00218f9
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-21 08:15:12 +0000

    imported Place and Review

--- models/user.py
+++ models/user.py
@@ -6,7 +6,8 @@ from os import getenv
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-
+from models.place import Place
+from models.review import Review
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
@@ -17,8 +18,8 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128))
         last_name = Column(String(128))
-        places = relationship("Place", backref="user", cascade="delete")
-        review = relationship("Review", backref="user", cascade="delete")
+        places = relationship("Place", backref="user", cascade="all, delete, delete-orphan")
+        review = relationship("Review", backref="user", cascade="all, delete, delete-orphan")
     else:
         email = ""
         password = ""
