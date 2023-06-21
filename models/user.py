e93bac2a81a971aa3112d9637a0c7f4f208dc54d
Chukwuemeka Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-06-21 10:54:02 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import relationship
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-    if storage_type == 'db':
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
