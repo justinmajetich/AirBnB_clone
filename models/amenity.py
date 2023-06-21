2d9a279e9692e18e66899bfad27cda6289a8ece8
Alan Nato <alanwnato@gmail.com>
2023-06-21 14:29:28 -0700

    models

--- models/amenity.py
+++ models/amenity.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
+"""This is the amenity class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from models.place import place_amenity
+
+
+class Amenity(BaseModel, Base):
+    """This is the class for Amenity
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity)#!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
 
