d9b2ebfb74dca81ea39c6fa498611dbf1b24039a
chriss1525 <okothchris15@gmail.com>
2023-06-21 15:53:10 +0300

    update amenities class

--- models/place.py
+++ models/place.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 import models
 from models.review import Review
 
+
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60),
+                             ForeignKey('places.id'),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'),
+                             primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
@@ -21,8 +29,8 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=True, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    # amenity_ids = []
     reviews = relationship('Review', backref='place', cascade='all, delete')
+    amenities = relationship('Amenity', secondary=place_amenity, viewonly=False)
 
     @property
     def reviews(self):
@@ -32,3 +40,18 @@ class Place(BaseModel, Base):
             if value.place_id == self.id:
                 review_list.append(value)
         return review_list
+
+    @property
+    def amenities(self):
+        """ getter for amenities """
+        amenity_list = []
+        for key, value in models.storage.all(Amenity).items():
+            if value.place_id == self.id:
+                amenity_list.append(value)
+        return amenity_list
+
+    @amenities.setter
+    def amenities(self, obj):
+        """ setter for amenities """
+        if type(obj) == Amenity:
+            self.amenity_ids.append(obj.id)
