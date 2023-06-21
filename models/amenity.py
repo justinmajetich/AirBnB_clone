ffba2481d01a6175d6d01b934739481825dd7f6e
Chukwuemeka Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-06-21 20:48:18 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -2,10 +2,6 @@
 """ holds class Amenity"""
 import models
 from models.base_model import BaseModel, Base
-<<<<<<< HEAD
-from sqlalchemy import Column, String
-=======
->>>>>>> f130fcd35520c96a54d594ea8d33bc4debf940dd
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
@@ -24,4 +20,3 @@ class Amenity(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes Amenity"""
         super().__init__(*args, **kwargs)
-        
