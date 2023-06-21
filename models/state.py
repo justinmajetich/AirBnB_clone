c107c8a190f6e300887fd7d607a94360f3f2b972
Robert Ruai Mawich <robertruai4@gmail.com>
2023-06-21 04:07:23 -0700

    task 7, 8, 9,10

--- models/state.py
+++ models/state.py
@@ -13,7 +13,7 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
 
-    # Relationship wit City class
+    # Relationship with City class
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         cities = relationship('City', cascade='all, delete', backref='state')
     else:
