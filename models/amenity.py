d9bb19bb3dc003cf39253f8205574dd4b736952c
Chukwuemeka Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-06-21 20:15:14 +0100

    Updated

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+""" holds class Amenity"""
+import models
 from models.base_model import BaseModel, Base
-from models import storage_type
-from sqlalchemy import Column, String
 from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
 
 class Amenity(BaseModel, Base):
-    '''amenity class'''
-    __tablename__ = 'amenities'
+    """Representation of Amenity """
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
+        __tablename__ = 'amenities'
+        name = Column(String(128),
+                      nullable=False)
     else:
         name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
+        
