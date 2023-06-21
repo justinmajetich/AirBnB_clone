5fdb54e8777f456b5c8813b677004b26a88ad18e
Chukwuemeka Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-06-21 16:39:46 +0100

    Update

--- models/__init__.py
+++ models/__init__.py
@@ -4,12 +4,12 @@ Initialize models package
 """
 
 import os
-from models.engine.db_storage import DBStorage
-from models.engine.file_storage import FileStorage
 
 if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
 
 storage.reload()
