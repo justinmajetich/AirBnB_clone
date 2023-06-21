29402a608fa03ba5d6b057102c0b14dbba397424
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-06-21 13:51:40 +0000

    updated the mysql setup fail

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -8,10 +8,10 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER 
     IF NOT EXISTS 'hbnb_dev'@'localhost'
     IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT
+    ON `performance_schema`.*
+    TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES
-    ON hbnb_dev_db.*
-    TO 'hbhnb_dev'@ WITH GRANT OPTION;
-GRANT SELECT 
-    ON performance_schema.*
-    TO 'hbhnb_dev'@ WITH GRANT OPTION;
+    ON `hbnb_dev_db`.*
+    TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
