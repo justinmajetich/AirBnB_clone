0485cf7aa337874bc8962bd80797abe19437654a
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-21 07:02:32 +0000

    fixed pep8 errors

--- models/user.py
+++ models/user.py
@@ -13,17 +13,16 @@ from os import getenv
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
 
-    if (os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__="users"
-        email=Column(String(128), nullable=False)
-        password=Column(String(128), nullable=False)
-        first_name=Column(String(128))
-        last_name=Column(String(128))
-        places=relationship("Place", backref="user", cascade="delete")
-        review=relationship("Review", backref="user", cascade="delete")
+    if (os.getenv('HBNB_TYPE_STORAGE') == 'db'):
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+        places = relationship("Place", backref="user", cascade="delete")
+        review = relationship("Review", backref="user", cascade="delete")
     else:
-        email=""
-        password=""
-        first_name=""
-        last_name=""
-
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
