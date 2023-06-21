9f06c8e588b8d964c7045b0f22fa1ab8eb6a6643
Aaron Will Djaba <papiliocurtis@gmail.com>
2023-06-21 07:32:33 +0000

    updated states and cities

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage or DBStorage"""
+'''
+    Package initializer
+'''
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+import os
 
-from os import getenv
+classes = {"User": User, "BaseModel": BaseModel,
+           "Place": Place, "State": State,
+           "City": City, "Amenity": Amenity,
+           "Review": Review}
 
-
-if getenv('HBNB_TYPE_STORAGE') == 'db':
+if os.getenv('HBNB_TYPE_STORAGE') == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
     storage.reload()
