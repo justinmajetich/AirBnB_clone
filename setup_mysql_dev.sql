4c722c8cc8ba7406742b9ba5290cc357c665459d
Alan Nato <alanwnato@gmail.com>
2023-06-21 14:46:52 -0700

    MySQL setup development

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,9 @@
+-- Creating hbnb_dev_db database
+-- Privileges for new user (hbnb_dev)
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
+USE hbnb_dev_db;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
