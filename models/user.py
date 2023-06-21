a788f32cd776fdade36dae1b0337b191b92699fb
chriss1525 <okothchris15@gmail.com>
2023-06-21 14:39:14 +0300

    create review instance in database

--- models/user.py
+++ models/user.py
@@ -14,3 +14,4 @@ class User(BaseModel, Base):
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
     places = relationship('Place', backref='user', cascade='all, delete')
+    reviews = relationship('Review', backref='user', cascade='all, delete')
