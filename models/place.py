da0c5b003bb20aec819bb2d6971e705fd44d8817
fatma tnani <fatmatnani08@gmail.com>
2023-06-21 11:02:27 -0700

    db storage place

--- models/place.py
+++ models/place.py
@@ -1,18 +1,49 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy.orm import relationship
+from models import storage_type
 
-
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    __tablename__ = 'places'
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     amenity_ids = []
+
+    if storage_type == "db":
+        reviews = relationship("Review", backref="place", cascade="all, delete")
+    else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+        
+
+        @property
+        def reviews(self):
+            """returns the list of Review instances with place_id equals to the current Place.id"""
+            from models import storage
+            all_reviews = storage.all(Review)
+            place_reviews = []
+            for review in all_reviews.values():
+                if review.place_id == self.id:
+                    place_reviews.append(review)
+            return place_reviews
+
