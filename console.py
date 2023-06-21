eb03d70c134fe3d3b59835b3ee241454ddf227ff
Chukwuemeka Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-06-21 20:56:29 +0100

    Update console.py

--- console.py
+++ console.py
@@ -7,7 +7,7 @@ import os
 import uuid
 from datetime import datetime
 from models.base_model import BaseModel
-from models.__init__ import storage
+from models import storage
 from models.user import User
 from models.place import Place
 from models.state import State
