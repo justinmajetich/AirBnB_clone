2d45d99eb1ba690237889e0ec29210f378e111c1
Alan Nato <alanwnato@gmail.com>
2023-06-21 14:49:21 -0700

    MySQL setup test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,9 @@
+-- Creating hbnb_test_db database
+-- Privileges for new user (hbnb_test)
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+USE hbnb_test_db;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
