b000c6158b8a2c53a65f7fb23596c0d2508e3c4d
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-21 07:39:26 +0000

    using the getenv environmental variables

--- models/user.py
+++ models/user.py
@@ -2,19 +2,17 @@
 
 """This module defines a class User"""
 
-
-from models.base_model import BaseModel
-from models.base_model import Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 from os import getenv
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+    __tablename__ = "users"
 
-    if (os.getenv('HBNB_TYPE_STORAGE') == 'db'):
-        __tablename__ = "users"
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128))
