2e59f79bb5a2bf220ebaae6ba8775fab1d4b6ec7
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-06-21 12:36:11 +0000

    setup mysql database

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,6 +2,6 @@
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbhnb_dev'@'localhost' WITH GRANT OPTION;
-GRANT SELECT ON performance_schema.* TO 'hbhnb_dev'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbhnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbhnb_dev'@'localhost';
+-- FLUSH PRIVILEGES;
