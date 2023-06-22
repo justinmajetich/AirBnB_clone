027404403a1b334c48c57cc1f364b3d93f8352a7
Sering jallow <34775682+Sjallow@users.noreply.github.com>
2023-06-22 00:58:20 +0000

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -13,39 +13,34 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
-
-
 class DBStorage:
-    """Represents a database storage engine.
-
-    Attributes:
-        __engine (sqlalchemy.Engine): The working SQLAlchemy engine.
-        __session (sqlalchemy.Session): The working SQLAlchemy session.
+"""Represents a database storage engine.
+Attributes:
+ __engine (sqlalchemy.Engine): The working SQLAlchemy engine.
+ __session (sqlalchemy.Session): The working SQLAlchemy session.
     """
 
-    __engine = None
-    __session = None
-
-    def __init__(self):
-        """Initialize a new DBStorage instance."""
-        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".
-                                      format(getenv("HBNB_MYSQL_USER"),
-                                             getenv("HBNB_MYSQL_PWD"),
-                                             getenv("HBNB_MYSQL_HOST"),
-                                             getenv("HBNB_MYSQL_DB")),
-                                      pool_pre_ping=True)
-        if getenv("HBNB_ENV") == "test":
-            Base.metadata.drop_all(self.__engine)
-
-    def all(self, cls=None):
-        """Query on the curret database session all objects of the given class.
+__engine = None
+__session = None
+def __init__(self):
+"""Initialize a new DBStorage instance."""
+self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".
+format(getenv("HBNB_MYSQL_USER"),
+getenv("HBNB_MYSQL_PWD"),
+getenv("HBNB_MYSQL_HOST"),
+getenv("HBNB_MYSQL_DB")),
+pool_pre_ping=True)
+if getenv("HBNB_ENV") == "test":
+Base.metadata.drop_all(self.__engine)
+def all(self, cls=None):
+"""Query on the curret database session all objects of the given class.
 
         If cls is None, queries all types of objects.
 
         Return:
             Dict of queried classes in the format <class name>.<obj id> = obj.
         """
-        if cls is None:
+if cls is None:
             objs = self.__session.query(State).all()
             objs.extend(self.__session.query(City).all())
             objs.extend(self.__session.query(User).all())
