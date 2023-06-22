e668957b827a4bff9e3d4387949f36be0ff9c5af
Justine Nicolao <bororio96@gmail.com>
2023-06-22 14:19:20 +0300

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,15 @@
+#!/usr/bin/python3
+""" Script that runs an app with Flask framework """
 from flask import Flask
+
+
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
-   return "Hello HBNB!"
+    """ Function called with / route """
+    return 'Hello HBNB!'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
