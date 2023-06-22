06b7081643aa03181deea459265f483c2712577e
6godpro <erickson5115@gmail.com>
2023-06-22 23:53:09 +0100

    --

--- models/user.py
+++ models/user.py
@@ -19,7 +19,6 @@ class User(BaseModel, Base):
     """
 
     __tablename__ = 'users'
-
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
