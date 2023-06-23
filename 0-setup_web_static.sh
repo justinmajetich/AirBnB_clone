6f2a2651b5e6fa32df53204e4552c6edf24c580f
AvitBrian <113444617+AvitBrian@users.noreply.github.com>
2023-06-23 17:08:07 +0000

    updatesss

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 #web_static development
 
-#check for library updates
 sudo apt-get -y update
-sudo apt-get -y upgrade
-#install nginx
 sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "Konnichiwa! It's a test HTML file." | sudo tee /data/web_static/releases/test/index.html
-sudo rm -rf /data/web_static/current
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
-#permission setting
+sudo service nginx start
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
-#restarting nginx
 sudo service nginx restart
