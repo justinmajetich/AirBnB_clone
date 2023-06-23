1adbf50fd4d4a0eb2f30443a6ac17f3d4e624c73
Moussa <109428479+Kakaymi10@users.noreply.github.com>
2023-06-23 22:23:55 +0200

    Create 2-do_deploy_web_static.py

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Fabric Script that distributes an archives"""
+
+
+from fabric.api import put, run, env
+from os.path import exists
+env.hosts = ['54.82.5.102', '3.94.103.18']
+
+
+def do_deploy(archive_path):
+    """Archives to web-servers"""
+    if exists(archive_path) is False:
+        return False
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except:
+        return False
