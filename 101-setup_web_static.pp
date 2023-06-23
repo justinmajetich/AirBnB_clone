3a4927ec7d33b52cc7cdffe68bc64049c3dee60d
Moussa <109428479+Kakaymi10@users.noreply.github.com>
2023-06-23 22:35:43 +0200

    Create 101-setup_web_static.pp

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,6 @@
+# puppet script to setup web server for deployment of web_static
+
+exec { 'server setup':
+  command  => 'sudo apt-get -y update && sudo apt-get -y install nginx && sudo service nginx start && sudo mkdir -p /data/web_static/shared/ && sudo mkdir -p /data/web_static/releases/test/ && echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html > /dev/null && sudo ln -sf /data/web_static/releases/test/ /data/web_static/current && sudo chown -R ubuntu:ubuntu /data/ && sudo sed -i \'44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\' /etc/nginx/sites-available/default && sudo service nginx restart',
+  provider => shell,
+}
