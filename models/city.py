ed225f0015029ea404561f6c5412ea8fa8ef55b7
Alan Nato <alanwnato@gmail.com>
2023-06-23 10:49:13 +0000

    Models

--- models/city.py
+++ models/city.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 """This is the city class"""
-from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy import ForeignKey
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from models.place import Place
 
 
 class City(BaseModel, Base):
@@ -16,6 +13,11 @@ class City(BaseModel, Base):
     """
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="cities")
+    state_id = Column(String(60),
+                      ForeignKey("states.id",
+                                 ondelete='CASCADE'),
+                      nullable=False)
+    places = relationship(
+        'models.place.Place',
+        backref='cities',
+        cascade='all, delete-orphan')
