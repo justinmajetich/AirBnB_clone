fe8fb5af59bb5e18472d3b4a2023faf05f2327ad
AvitBrian <b.mugisha2@alustudent.com>
2023-06-28 18:08:25 +0200

    updates

---
+++ web_flask/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    return("Hello HBNB!")
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
+ 
