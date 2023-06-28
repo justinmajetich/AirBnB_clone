a914c9140b48f76f6a55aec2edb8fd65672fd3e1
AvitBrian <b.mugisha2@alustudent.com>
2023-06-28 21:13:30 +0200

    updates #201 commits

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,35 +1,31 @@
 #!/usr/bin/python3
-"""
-   A script that starts a Flask web application
-   and adds several routes for navigation.
-   
-"""
+""" Web application listening on the 0.0.0.0, port 5000 """
 from flask import Flask
-
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_hbnb():
-    """ Displays Hello HBNB! """
+    """ Displays Hello HBNB! on home route """
     return "Hello HBNB!"
 
 
 @app.route("/hbnb")
 def hbnb():
-    """ Displays  HBNB! """
+    """ Displays HBNB! on hbnb route """
     return "HBNB"
 
 
 @app.route("/c/<text>")
-def hbnb():
-    """Displays C followed by
-       the value of text 
+def c_is_fun(text):
     """
-    return
+    Displays 'C' followed by the value of text
+    Replaces underscores with a space
+    """
+    return "C {}".format(text.replace("_", " "))
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
     app.url_map.strict_slashes = False
