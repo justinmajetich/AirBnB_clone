d2955d8b7def918874c8f0509fb243946379d211
AvitBrian <b.mugisha2@alustudent.com>
2023-06-28 21:04:18 +0200

    updates #197 commits

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+    a script that starts a Flask web application
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_hbnb():
+    """ Displays Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """ Displays  HBNB! """
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def hbnb():
+    """Displays C followed by
+       the value of text 
+    """
+    return
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
