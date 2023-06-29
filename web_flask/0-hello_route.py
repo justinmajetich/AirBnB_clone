d633aef44934b2db996481f003041cbda799fce6
Moussa <109428479+Kakaymi10@users.noreply.github.com>
2023-06-29 11:19:56 +0200

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes = False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
     """ Display Hello HBNB! """
     return "Hello HBNB!"
