100951ab6c27ad74eeed68f1764490fade760354
Moussa <109428479+Kakaymi10@users.noreply.github.com>
2023-06-29 11:58:59 +0200

    Create 3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Web application listening on 0.0.0.0, port 5000 """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """ Display Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ Display HBNB! """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ Display prompt """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+  
+
+@app.route('/python/<text>', strict_slashes=False)
+def c_text(text):
+    """ Display prompt """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
