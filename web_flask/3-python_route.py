6eeed0ba03d074e44bd7665a741bb9a404925031
Moussa <109428479+Kakaymi10@users.noreply.github.com>
2023-06-29 12:09:17 +0200

    Update 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -25,7 +25,7 @@ def c_text(text):
     return 'C {}'.format(text)
 
 
-@app.route("/python", strict_slashes=False) 
+@app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python_is_cool(text="is cool"):
     """
