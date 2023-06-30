c0bd68949a91386cecc1b72041e677c6041aa1b4
PaintRock <6126@holbertonstudents.com>
2023-06-30 17:55:07 -0500

    time

--- models/base_model.py
+++ models/base_model.py
@@ -3,8 +3,14 @@
 import uuid
 from datetime import datetime
 
+Base = declarative_base()
 
 class BaseModel:
+    """Update for Alchemy"""
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
@@ -13,7 +19,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -31,7 +37,11 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
+        
+    def delete(self):
+        models.storage.delete(self)
 
     def to_dict(self):
         """Convert instance into dict format"""
