52f2fc62c2b5166fd554f30c8d62e7b0a26bc5cb
PaintRock <6126@holbertonstudents.com>
2023-06-30 16:45:06 -0500

    Crystal meth visions

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -8,7 +8,7 @@ GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
 -- Create the user if it doesn't exist
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 -- Grant all privileges on the hbnb_dev_db to hbnb_dev
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
