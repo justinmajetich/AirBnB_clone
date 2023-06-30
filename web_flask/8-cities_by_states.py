584b353ef69f639079d58c9936c01af3a3237920
AvitBrian <b.mugisha2@alustudent.com>
2023-06-30 20:53:56 +0200

    updates

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-""" Web application listening on the 0.0.0.0, port 5000 """
+
+"""Script that starts a Flask web apllication"""
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
-
 app = Flask(__name__)
-app.url_map.strict_slashes = False
-
-
-@app.teardown_appcontext
-def teardown_appcontext(exception):
-    storage.close()
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    states = storage.all(State).values()
-    states_sorted = sorted(states, key=lambda s: s.name)
+    """loads all cities of a State"""
 
-    return render_template('8-cities_by_states.html', states=states_sorted)
+    return render_template('8-cities_by_states.html',
+                           states=storage.all('State').values())
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """closes the current SQLAlchemy Session"""
+    storage.close()
 
 
 if __name__ == '__main__':
