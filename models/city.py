5c50ee2f51ada3670075c92c0a81073b187a3fce
PaintRock <6126@holbertonstudents.com>
2023-06-30 18:57:05 -0500

    6v3

--- models/city.py
+++ models/city.py
@@ -4,7 +4,7 @@ from models.base_model import BaseModel
 from models.base_model import Base
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+    """Creates the table for city class. Relates to state"""
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
