2cba67e143036f3033334ae1a388062b7be5fdc2
PaintRock <6126@holbertonstudents.com>
2023-06-30 20:07:14 -0500

    6 will not be my friend

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,26 +1,36 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
-from models.base_model import Base
+from os import getenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.orm import sessionmaker, scoped_session, relationship
+from models.base_model import BaseModel, Base
+from models.state import State
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 class DBStorage:
     __engine = None
     __session = None
 
     def __init__(self):
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
-                                       format(os.getenv('HBNB_MYSQL_USER'),
-                                              os.getenv('HBNB_MYSQL_PWD'),
-                                              os.getenv('HBNB_MYSQL_HOST'),
-                                              os.getenv('HBNB_MYSQL_DB')),
-                                       pool_pre_ping=True)
-        if os.getenv('HBNB_ENV') == 'test':
+        """ the init that give HBNB envelopes?"""
+        user = getenv('HBNB_MYSQL_USER')
+        pw = getenv('HBNB_MYSQL_PWD')
+        host = getenv('HBNB_MYSQL_HOST')
+        db = getenv('HBNB_MYSQL_DB')
+        env = getenv('HBNB_ENV')
+
+        dir = "mysql+mysqldb://{}:{}@{}/{}"\
+            .format(user, pw, host, db)
+
+        self.__engine = create_engine(dir, pool_pre_ping=True)
+        Base.metadata.create_all(self.__engine)
+        if env == 'test':
             Base.metadata.drop_all(self.__engine)
+
 
     def all(self, cls=None):
         session = self.__session
