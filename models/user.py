635ab1613e18499a0bdb191038492db745d1505b
PaintRock <6126@holbertonstudents.com>
2023-06-30 20:23:07 -0500

    typo? 7v2

--- models/user.py
+++ models/user.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
+     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
