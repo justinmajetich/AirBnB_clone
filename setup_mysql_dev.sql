31fbe7053504f2ccde9f8a7d8f4c38ee2d32941c
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-07-01 12:01:39 -0700

    setupmysql revision

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,4 @@
 -- script that prepares a MySQL server for the project:
--- Create database: hbnb_dev_db
-CREATE database IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 -- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
