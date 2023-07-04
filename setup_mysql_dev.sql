ab5b80e0d3860272271b72d3720432e147247c26
Jabulani-N <jabu.94@gmail.com>
2023-07-04 13:57:22 -0700

    user_creation_works._tests_will_work_once.-added_tests_to_readme

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,9 +5,13 @@ CREATE database IF NOT EXISTS hbnb_dev_db;
 -- creates new user hbnb_dev (in localhost)
 -- password of hbnb_dev should be set to hbnb_dev_pwd
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+ALTER USER IF EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- hbnb_dev should have all privileges on the database hbnb_dev_db (and only this database)
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- hbnb_dev should have SELECT privilege on the database performance_schema (and only this database)
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- ascertain the changes apply
+FLUSH PRIVILEGES;
