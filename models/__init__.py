9f7e2749aba1b0f4b67d38084f816d7e48d7d777
Josh D <joj237@gmail.com>
2023-07-04 16:18:20 -0700

    change init file to import os

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage or DBStorage"""
-from os import getenv
+"""This module instantiates an object of class FileStorage"""
+import os
 
-if getenv('HBNB_TYPE_STORAGE') == 'db':
+
+storage = None
+if os.getenv('HBNB_TYPE_STORAGE') == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
