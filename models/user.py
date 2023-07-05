0a9d49bf17c26910b926267717c089624abf2931
Josh D <joj237@gmail.com>
2023-07-04 18:09:55 -0700

    update reviews and add relationship to user.py

--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     __tablename__ = 'users'
@@ -9,3 +10,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
+    reviews = relationship("Review", backref="user", cascade="all, delete")
