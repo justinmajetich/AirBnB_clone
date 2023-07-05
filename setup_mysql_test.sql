fc168a784b3a19302c7546fac63da182504fc953
Braden <blearnest1995@gmail.com>
2023-07-05 09:07:53 -0700

    SQL server fixes

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -9,11 +9,11 @@ CREATE USER
     IF NOT EXISTS 'hbnb_test'@'localhost'
     IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES
-   ON `hbnb_test_db`.*
+   ON 'hbnb_test_db'.*
    TO 'hbnb_test'@'localhost'
    IDENTIFIED BY 'hbnb_test_pwd';
 GRANT SELECT
-   ON `performance_schema`.*
+   ON 'performance_schema'.*
    TO 'hbnb_test'@'localhost'
    IDENTIFIED BY 'hbnb_test_pwd';
 FLUSH PRIVILEGES;
