0437263c553505c672b47139a8807b4fc26dce60
Christopher <christopher@Christophers-MacBook-Pro.local>
2023-07-05 10:04:37 -0500

    6

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+import os
 
 
-storage = FileStorage()
+storage = None
+if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+
 storage.reload()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
+if os.getenv("HBNB_TYPE_STORAGE") == 'db':
+    class State(BaseModel, Base):
+        """ State class """
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities",
+                              cascade="all, delete, delete-orphan")
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+else:
+
+    class State(BaseModel):
+        """File Storage State"""
+        name = ""
+
+        @property
+        def cities(self):
+            """Returns a list of cities equal to their state ID"""
+            from models import storage
+            list_cities = []
+            for key, i in storage.all(City).items():
+                if i.state_id == self.id:
+                    list_cities.append(i)
+            return list_cities
