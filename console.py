24cb1e46d358b9f3ee3d1e6144b2911d55dbc269
Dominick <6157@holbertonstudents.com>
2023-07-05 12:58:53 -0700

    testing branch

--- console.py
+++ console.py
@@ -10,7 +10,7 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
-
+##does it work? 
 
 class HBNBCommand(cmd.Cmd):
     """ Contains the functionality for the HBNB console"""
