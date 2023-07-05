4792a1828667bab7a39d24299ebf2c2f49521c6e
Josh D <joj237@gmail.com>
2023-07-05 08:59:14 -0700

    made changes to files for unittest

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import place_amenity
+
 
 class Amenity(BaseModel, Base):
     """ Amenities of our places """
--- models/place.py
+++ models/place.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from os import getenv
-from sqlalchemy import Column, Integer, Float, Table
+from sqlalchemy import Column, Integer, Float, Table, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.schema import ForeignKey
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
-    __tablename = 'places'
+    __tablename__ = 'places'
 
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('user.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(Srtring(1024))
+    description = Column(String(1024))
     number_rooms = Column(Integer, default=0, nullable=False)
     number_bathrooms = Column(Integer, default=0, nullable=False)
     max_guest = Column(Integer, default=0, nullable=False)
     price_by_night = Column(Integer, default=0, nullable=False)
     latitude = Column(Float)
-    longitude = Cloumn(Float)
+    longitude = Column(Float)
     amenity_ids = []
