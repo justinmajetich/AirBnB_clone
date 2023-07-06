8ff56c7aba67b14b8c4a15b598aa21c9e53b0718
Gibexz <andrewetedu@gmail.com>
2023-07-06 01:51:10 +0100

    Task 0 AirBnB clone - Deploy static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,11 +6,11 @@ sudo apt -y update
 sudo apt install -y nginx
 # Create the folder /data/web_static/releases/test/ if it doesn’t already exis
 sudo mkdir -p /data/web_static/releases/test/
-#Create the folder /data/web_static/shared/ if it doesn’t already exist
+# Create the folder /data/web_static/shared/ if it doesn’t already exist
 sudo mkdir -p /data/web_static/shared/
-#Create a fake HTML file /data/web_static/releases/test/index.html
+# Create a fake HTML file /data/web_static/releases/test/index.html
 echo "<h1>Static Test</h1>" | sudo tee /data/web_static/releases/test/index.html
-#Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder
+# Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 # Give ownership of the /data/ folder to the ubuntu user AND group
 sudo chown -R ubuntu:ubuntu /data/
