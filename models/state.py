850322fff59627c8d95a10b7e354cedaf36b0d30
Gibexz <andrewetedu@gmail.com>
2023-07-06 01:35:43 +0100

    Task 0 AirBnB clone - Deploy static

--- models/state.py
+++ models/state.py
@@ -5,27 +5,26 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 import shlex
 from models.city import City
-
+from os import getenv
 
 class State(BaseModel):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
 
-    cities = relationship("City", backref="state",
-                          cascade="all, delete-orphan")
+    cities = relationship("City",
+                          cascade="all, delete, delete-orphan", backref="state")
 
-    @property
-    def cities(self):
-        var = models.storage.all()
-        lists = []
-        results = []
-        for key in var:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if (city[0] == 'City'):
-                lists.append(var[key])
-        for elem in lists:
-            if (elem.state_id == self.id):
-                results.append(elem)
-        return (results)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    if getenv('HBNB_TYPE_STORAGE', '') != 'db':
+        @property
+        def cities(self):
+            all_cities = models.storage.all("City")
+            temp = []
+            for c_id in all_cities:
+                if all_cities[c_id].state_id == self.id:
+                    temp.append(all_cities[c_id])
+
+            return temp
