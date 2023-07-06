c306ebef125c0ed8387d7aa2f04a4b1cc7e4fc7f
Alan Nato <alanwnato@gmail.com>
2023-07-06 15:14:17 +0000

    Compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+module : 1-pack_web_static.py
+"""
+
+
+from fabric.api import *
+from datetime import datetime
+
+
+def do_pack():
+    """ generates a .tgz archive from the contents of the...
+    ...web_static folder """
+    local("sudo mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = "versions/web_static_{}.tgz".format(date)
+    result = local("sudo tar -cvzf {} web_static".format(filename))
+    if result.succeeded:
+        return filename
+    else:
+        return None
