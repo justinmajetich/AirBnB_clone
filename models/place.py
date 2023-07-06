67b9289c7146aca391e8c7784503de850bf41be0
Josh D <joj237@gmail.com>
2023-07-06 00:00:16 -0700

    last pycode fix after jbranch changes

--- models/place.py
+++ models/place.py
@@ -19,27 +19,29 @@ place_amenity = Table(
         'amenity_id', String(60),
         ForeignKey('amenities.id'),
         primary_key=True,
-        nullable=False))
+        nullable=False)
+)
 
 
 class Place(BaseModel, Base):
-        """ A place to stay """
-        __tablename__ = 'places'
+    """ A place to stay """
+    __tablename__ = 'places'
 
-        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-        users_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        name = Column(String(128), nullable=False)
-        description = Column(String(1024))
-        number_rooms = Column(Integer, default=0, nullable=False)
-        number_bathrooms = Column(Integer, default=0, nullable=False)
-        max_guest = Column(Integer, default=0, nullable=False)
-        price_by_night = Column(Integer, default=0, nullable=False)
-        latitude = Column(Float)
-        longitude = Column(Float)
-        amenity_ids = []
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    users_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024))
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float)
+    longitude = Column(Float)
+    amenity_ids = []
 
-        amenities = relationship(
-            "Amenity",
-            secondary=place_amenity,
-            viewonly=False,
-            back_populates="places")
+    amenities = relationship(
+        "Amenity",
+        secondary=place_amenity,
+        viewonly=False,
+        back_populates="places"
+    )
