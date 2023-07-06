c5eae6f85f2d6a001aefcb20f6391c06ea29cfd4
ThatAgricBoy <samueljohn3999@gmail.com>
2023-07-06 18:28:10 +0100

    first fabfile

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # Fabfile to generates a .tgz archive from the contents of web_static.
+# Working with fabfile
 import os.path
 from datetime import datetime
 from fabric.api import local
