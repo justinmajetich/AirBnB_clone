e18acb6039ce5f25ed54b204949b4901f8d12ede
Alan Nato <alanwnato@gmail.com>
2023-07-06 15:19:12 +0000

    Deploy archive!

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" a Fabric script (based on the file 1-pack_web_static.py) that distributes..
+    ..an archive to your web servers, using the function do_deploy: """
+
+
+from fabric.api import *
+from datetime import datetime
+from os.path import exists
+
+
+env.hosts = ['107.21.42.87', '34.207.121.230']
+
+
+def do_deploy(archive_path):
+    """ distributes an archive to my web servers
+    """
+    if exists(archive_path) is False:
+        return False
+    filename = archive_path.split('/')[-1]
+    no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
+    tmp = "/tmp/" + filename
+
+    try:
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}/".format(no_tgz))
+        run("tar -xzf {} -C {}/".format(tmp, no_tgz))
+        run("rm {}".format(tmp))
+        run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
+        run("rm -rf {}/web_static".format(no_tgz))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}/ /data/web_static/current".format(no_tgz))
+        return True
+    except Exception as e:
+        return False
