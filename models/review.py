b9954f220345358d02e508c635255730621a1168
Dominick <6157@holbertonstudents.com>
2023-07-05 22:29:28 -0700

    fix bugs

--- models/review.py
+++ models/review.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 
+
 class Review(BaseModel, Base):
     __tablename__ = 'reviews'
     text = Column(String(1024), nullable=False)
