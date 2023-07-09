a9108f029855314832e1817184cd96372c8f5aad
thatagricboy <samueljohn3999@gmail.com>
2023-07-09 17:14:30 +0000

    tarx

--- tests/file_transfer_fabric.py
+++ tests/file_transfer_fabric.py
@@ -7,14 +7,13 @@ def upload(ctx):
         conn.put('testtransfer', '~/')
         print("File uploaded successfully!")
 
+@task
 def download_file(ctx):
     with Connection('your_username@your_server_ip') as conn:
         conn.get('/remote/path/remote_file.txt', 'local_directory/')
         print("File downloaded successfully!")
 
 collection = Collection()
-collection.add_task(upload, 'upload')
-collection.add_task(download_file, 'download_file')
+collection.add_task(upload)
+collection.add_task(download_file)
 
-if __name__ == '__main__':
-    collection.invoke('upload')
