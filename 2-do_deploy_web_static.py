9ae12935c339b80fb02775de6f5e7912dfa02e85
belladesign <estelleokafor@gmail.com>
2023-07-10 11:02:12 +0000

    recent

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,35 +5,30 @@ Fabric script method:
 Usage:
     fab -f 2-do_deploy_web_static.py
     do_deploy:archive_path=versions/web_static_20220928193100.tgz
-    -i my_ssh_private_key -u ubuntu 
+    -i my_ssh_private_key -u ubuntu
 """
 from fabric.api import env, put, run
-import os.path
+#import os.path
+from os.path import exists
 env.hosts = ["54.210.88.216", "52.205.99.41"]
 
 
 def do_deploy(archive_path):
-    """
-    Deploy archive to web server
-    Args:
-    archive_path : path for the archive file
-    """
-    if os.path.isfile(archive_path) is False:
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
     try:
-        filename = archive_path.split("/")[-1]
-        no_ext = filename.split(".")[0]
-        path_no_ext = "/data/web_static/releases/{}/".format(no_ext)
-        symlink = "/data/web_static/current"
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(path_no_ext))
-        run("tar -xzf /tmp/{} -C {}".format(filename, path_no_ext))
-        run("rm /tmp/{}".format(filename))
-        run("mv {}web_static/* {}".format(path_no_ext, path_no_ext))
-        run("rm -rf {}web_static".format(path_no_ext))
-        run("rm -rf {}".format(symlink))
-        run("ln -s {} {}".format(path_no_ext, symlink))
-        print('New version deployed!')
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except:
         return False
