f463be8276cecfc34a0b3fedcff8b47d3bd0af4e
belladesign <estelleokafor@gmail.com>
2023-07-10 09:14:33 +0000

    correct file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Fabric script that generates a tgz archive from the contents of the web_static folder of the AirBnB Clone repo
+Fabric script that generates a tgz archive from the contents
+of the web_static folder of the AirBnB Clone repo
 """
 
 from datetime import datetime
