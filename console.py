9da2ada2c40b650a3cf20690bbf767ef1e3e8dd6
Juliet Ndlamini <112163949+Julietndlamini@users.noreply.github.com>
2023-07-10 22:26:01 +0200

    Update console.py

--- console.py
+++ console.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-""" Console Module """
+"""Defines the HBNB console."""
 import cmd
-import sys
+from shlex import split
+from models import storage
+from datetime import datetime
 from models.base_model import BaseModel
-from models.__init__ import storage
 from models.user import User
-from models.place import Place
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
+from models.place import Place
 from models.review import Review
+
 
 
 class HBNBCommand(cmd.Cmd):
