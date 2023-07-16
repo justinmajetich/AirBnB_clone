0fc909273c6666432a0ad9451aeb382e8bcb23cd
ThatAgricBoy <samueljohn3999@gmail.com>
2023-07-16 01:13:58 +0100

    tests

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,31 +2,31 @@
 # Sets up a web server for deployment of web_static.
 # Devops
 
-apt-get update
-apt-get install -y nginx
+sudo apt-get update
+sudo apt-get install -y nginx
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo sh -c 'echo "Holberton School" > /data/web_static/releases/test/index.html'
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+sudo chown -R ubuntu /data/
+sudo chgrp -R ubuntu /data/
 
-printf %s "server {
+sudo bash -c 'printf %s "server {
     listen 80 default_server;
     listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
+    add_header X-Served-By \$HOSTNAME;
     root   /var/www/html;
     index  index.html index.htm;
 
     location /hbnb_static {
-	alias /data/web_static/current;
-	index index.html index.htm;
+        alias /data/web_static/current;
+        index index.html index.htm;
     }
 
     location /redirect_me {
-	return 301 http://youtube.com/;
+        return 301 http://youtube.com/;
     }
 
     error_page 404 /404.html;
@@ -34,6 +34,6 @@ printf %s "server {
       root /var/www/html;
       internal;
     }
-}" > /etc/nginx/sites-available/default
+}" > /etc/nginx/sites-available/default'
 
-service nginx restart
+sudo service nginx restart
