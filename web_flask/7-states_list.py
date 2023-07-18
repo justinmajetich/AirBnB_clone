cd88e1e76cda26a222d09445c3a735784bba61f9
Jabulani-N <jabu.94@gmail.com>
2023-07-17 17:17:58 -0700

    task 7 partially outlined

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -30,61 +30,10 @@ app = Flask(__name__)
 from models import storage
 
 
-@app.route('/')
-def hello_():
-    """if rout as above,
-    returns as below
-    """
-    return 'Hello HBNB!'
-
-
-@app.route('/hbnb')
-def hello_hbnb():
-    """if rout as above,
-    returns as below
-    """
-    return 'HBNB'
-
-
-@app.route('/c/<text>')
-def hello_c_text(text=''):
-    """if rout as above,
-    returns as below
-    """
-    edited = text.replace('_', ' ')
-    return 'C ' + edited
-
-
-@app.route('/python')
-@app.route('/python/')
-@app.route('/python/<text>')
-def hello_python_text(text='is cool'):
-    """if rout as above,
-    returns as below
-    """
-    edited = text.replace('_', ' ')
-    return 'Python ' + edited
-
-
-@app.route('/number/<int:n>')
-def hello_number(n=0):
-    """if n is int, return it is a number
-    """
-    return str(n) + ' is a number'
-
-
-@app.route('/number_template/<int:n>')
-def hello_number_template(n=0):
-    """if n is int, return a webpage
-    said webpage is in another file
-    """
-    return render_template('5-number.html', n=n)
-
-
 @app.route('/number_odd_or_even/<int:n>')
 def hello_odd_even(n=0):
-    """if n is int, return a webpage
-    said webpage is in another file
+    """this module needs to be updated
+    to perform task 8
     """
     polarity = 'odd'
     if n % 2 == 0:
