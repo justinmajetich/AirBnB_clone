4fcbb466d3fb9c259fc5a514bfbd0c78e889ad25
Gibexz <andrewetedu@gmail.com>
2023-07-21 13:02:32 +0100

    Task 1 1-hbnb_route.py

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+0-hello_route.py: A script that starts a flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+"""Route to display "Hello HBNB!" """
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """funtion that returns Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def helloHBNB():
+    """funtion that returns HBNB"""
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
