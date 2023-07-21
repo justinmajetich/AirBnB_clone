d7dc03005e2b6ed377df24d99b135c1e8dba3ae3
Gibexz <andrewetedu@gmail.com>
2023-07-21 15:32:13 +0100

    Task 4. Is it a number?

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+0-hello_route.py: A script that starts a flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+"""Route to display "Hello HBNB!" """
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """funtion that returns Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def helloHBNB():
+    """funtion that returns HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def helloC(text):
+    """funtion that returns "C <text(i.e a variable)>" """
+    """text = escape(text)"""
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+def helloPython(text="is_cool"):
+    """funtion that returns "Python <text(i.e a variable)>" """
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def helloNumber(n):
+    """funtion that returns "n" is a number if int else an error"""
+    return f"{n} is a number"
+
+
+if __name__ == '__main__':
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
