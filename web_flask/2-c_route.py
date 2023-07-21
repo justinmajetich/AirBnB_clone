ffa6c1fda4076b1bd185d21026eb57211ef14dce
joyce-source <brownjojo96@gmail.com>
2023-07-21 13:05:24 +0000

    c_is_fun

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+from flask import Flask, escape
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Route handler for the root URL ('/').
+
+    Returns:
+        str: A simple greeting message.
+    """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Route handler for the '/hbnb' URL.
+
+    Returns:
+        str: The string "HBNB".
+    """
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """
+    Route handler for the '/c/<text>' URL.
+
+    Args:
+        text (str): The text provided in the URL.
+
+    Returns:
+        str: The string "C " followed by the value of the text variable.
+    """
+    return "C {}".format(escape(text.replace("_", " ")))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
