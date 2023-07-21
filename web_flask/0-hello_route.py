0577f524bbb9570ec2f57b98ef6140c54fe743b5
Gibexz <andrewetedu@gmail.com>
2023-07-21 12:23:29 +0100

    Web_Flask dir, and Task 1 requirements (0-hello_route.py, __init__.py)

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+0-hello_route.py
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+"""Route to display "Hello HBNB!" """
+@app.route('/', strict_slashes=False)
+def hello():
+    """funtion that returns Hello HBNB!"""
+    return "Hello HBNB!"
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0', port=5000)
