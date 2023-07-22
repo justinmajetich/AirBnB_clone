a0e0785a6b34380b56f7094e1a106132962732fa
Bayero-abdul <bayeroa65@gmail.com>
2023-07-22 11:59:21 +0100

    Push with all()

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,7 +8,13 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    
+    def all(self, cls=None):
+        """Returns a dictionary of models currently in storage"""
+        if cls:
+            return {key: obj for key, obj in self.__objects.items()
+                    if isinstance(obj, cls)}
+        return FileStorage.__objects
+
     def new(self, obj):
         """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
