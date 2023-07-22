b5aba40d331aa923f57df3135aeec5559a95ff86
Gibexz <andrewetedu@gmail.com>
2023-07-22 23:29:12 +0100

    Task 7. Improve engines-6

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-
-from os import getenv, environ
-
+"""This module instantiates an object of class FileStorage or Dbstorage"""
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
+from os import getenv
 
-if "HBNB_TYPE_STORAGE" in environ.keys() and \
-        getenv("HBNB_TYPE_STORAGE") == "db":
+store = getenv("HBNB_TYPE_STORAGE")
+
+if store == "db":
     storage = DBStorage()
 else:
     storage = FileStorage()
