9764f24d96112dfe750026800c3e02a90cbc1bbc
fredogudu <fredogudu1@gmail.com>
2023-07-23 09:40:27 -0700

    added a script to start a flask web app '

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,9 @@
+-- this script prepares a MySQL server for the project
+-- create project development database with the name hbnb_dev_db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- creating new user named hbnb_dev with all privileges and password hbnb_dev_pwd
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- granting all privileges to the new user
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- granting the SELECT privilege for the user hbnb_dev in the db performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
