1d683e9df37d863d9cdc0ccf47af102d97d2bb86
Orjihenry <orjihen@yahoo.com>
2023-07-24 19:43:57 +0100

    Number template 👽👽👽

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -40,7 +40,14 @@ def num(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def num_temp(n):
-    return render_template("5-number.html", n=n)
+
+    # Verify if number is even or odd
+    if n % 2 == 0:
+        type = "even"
+    else:
+        type = "odd"
+
+    return render_template('5-number.html', n=n, type=type)
 
 
 if __name__ == '__main__':
