ccdc666248a92ce270cbb668368caf56a79be5ff
Gibexz <andrewetedu@gmail.com>
2023-07-24 03:06:58 +0100

    Task 9 updates

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -4,13 +4,15 @@ starts a Flask web application
 """
 
 from flask import Flask, render_template
-from models import storage, State, City
+from models import storage
+from models.state import State
+from models import *
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_seesion(exception):
+def teardown_db(exception):
     """Teardown method to remove the current SQLAlchemy Session"""
     storage.close()
 
