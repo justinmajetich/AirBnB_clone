901f837cae57a791e81cbe66cd9e70f016045466
Orjihenry <orjihen@yahoo.com>
2023-07-24 16:30:17 +0100

    Python is cool! 👻👻👻

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" Script that starts a Flask web app on :500 """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hi_hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+
+    # Replace _ with a space
+    rep_text = text.replace('_', ' ')
+    return ("C {}".format(rep_text))
+
+
+@app.route('/python/', defaults={'text': 'is cool'},
+           strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def py_text(text):
+
+    # Replace _ with a space
+    rep_text = text.replace('_', ' ')
+    return ("Python {}".format(rep_text))
+
+
+if __name__ == '__main__':
+    app.run()
