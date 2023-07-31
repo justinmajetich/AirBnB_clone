76151a162649ae06018aae45e22ee84ccf9a16fc
JDaniel26-Bory <juanrestrepo246@gmail.com>
2023-07-31 15:34:26 -0500

    update the file console

--- console.py
+++ console.py
@@ -113,17 +113,41 @@ class HBNBCommand(cmd.Cmd):
         """ Overrides the emptyline method of CMD """
         pass
 
-    def do_create(self, arg):
+    def do_create(self, args):
         """ Create an object of any class"""
-        if not arg:
+        if not args:
             print("** class name missing **")
             return
 
-        arg = arg.split(" ")
-        if arg not in HBNBCommand.classes:
+        args = args.split(" ")
+        if args[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes(arg[0])()
+        new_instance = HBNBCommand.classes[args[0]]()
+
+        for pair in args[1:]:
+            attr = pair.split("=", maxsplit=1)
+            key = attr[0]
+            value = ""
+            if attr[1][0] == '\"':
+                value = attr[1][1:-1].replace("_", " ").replace('"', '\"')
+            else:
+                if '.' in attr[1]:
+                    try:
+                        value = float(attr[1])
+                    except:
+                        pass
+                else:
+                    try:
+                        value = int(attr[1])
+                    except:
+                        pass
+            if value != "":
+                setattr(new_instance, key, value)
+            else:
+                pass
+        new_instance.save()
+        print(new_instance.id)
 
     def help_create(self):
         """ Help information for the create method """
