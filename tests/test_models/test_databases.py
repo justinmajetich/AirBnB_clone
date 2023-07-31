b7a1280427d4ad4694be2ba3a40c2e1990c63352
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2023-07-31 10:39:35 -0500

    test

--- tests/test_models/test_databases.py
+++ tests/test_models/test_databases.py
@@ -1,28 +1,38 @@
 import unittest
 import MySQLdb
 
-# Clase que define las pruebas unitarias
-class TestCreateState(unittest.TestCase):
+# Función para conectar con la base de datos MySQL y obtener el número de registros en la tabla states
+def get_number_of_records():
+    conn = MySQLdb.connect(user='hbnb_test', passwd='hbnb_test_pwd', db='hbnb_test_db', host='localhost')
+    cursor = conn.cursor()
+    cursor.execute("SELECT COUNT(*) FROM states;")
+    result = cursor.fetchone()
+    conn.close()
+    return result[0]
 
-    def setUp(self):
-        # Establecer la conexión con la base de datos
-        self.conn = MySQLdb.connect(user='tu_usuario', passwd='tu_contraseña', db='tu_base_de_datos', host='tu_host')
-        self.cursor = self.conn.cursor()
+# Clase que define las pruebas de integración con MySQL
+class TestMySQLIntegration(unittest.TestCase):
 
-        # Asegurarse de que la tabla "states" esté vacía antes de cada prueba
-        self.cursor.execute("DELETE FROM states;")
-        self.conn.commit()
+    @classmethod
+    def setUpClass(cls):
+        # Establecer la conexión con la base de datos antes de ejecutar todas las pruebas
+        cls.conn = MySQLdb.connect(user='hbnb_test', passwd='hbnb_test_pwd', db='hbnb_test_db', host='localhost')
+        cls.cursor = cls.conn.cursor()
 
-    def tearDown(self):
-        # Cerrar la conexión y limpiar después de cada prueba
-        self.cursor.close()
-        self.conn.close()
+        # Asegurarse de que la tabla "states" esté vacía antes de ejecutar todas las pruebas
+        cls.cursor.execute("DELETE FROM states;")
+        cls.conn.commit()
+
+    @classmethod
+    def tearDownClass(cls):
+        # Cerrar la conexión y limpiar después de ejecutar todas las pruebas
+        cls.cursor.close()
+        cls.conn.close()
 
     def test_create_state(self):
         # Obtener el número de registros antes de ejecutar el comando
-        self.cursor.execute("SELECT COUNT(*) FROM states;")
-        initial_records = self.cursor.fetchone()[0]
-
+        initial_records = get_number_of_records()
+        
         # Aquí simulas la ejecución del comando en la consola
         # Supongamos que ejecutas el comando create State name="California" en la consola
         # Por ejemplo, podrías hacer algo como:
@@ -30,8 +40,7 @@ class TestCreateState(unittest.TestCase):
         # self.conn.commit()
 
         # Obtener el número de registros después de ejecutar el comando
-        self.cursor.execute("SELECT COUNT(*) FROM states;")
-        final_records = self.cursor.fetchone()[0]
+        final_records = get_number_of_records()
 
         # Validar que la diferencia sea +1
         self.assertEqual(final_records - initial_records, 1)
