9df6a50c39326be8f948d9d4ed92510666f49f15
Bluebee45 <santi72143@gmail.com>
2023-08-01 13:16:45 -0500

    add file

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,8 @@
--- Create database
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- Create user in localhost identified by password
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
--- Grant all privileges to hbnb_dev on the database
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb'@'localhost';
--- Grant select privilege to hbnb_dev on the database performance_schema
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+-- create a database
+CREATE DATABASE if not exists hbnb_dev_db;
+-- create user
+CREATE USER if not exists 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- all privileges
+GRANT ALL privileges on hbnb_dev_db.* to 'hbnb_dev'@'localhost';
+-- select privilege
+GRANT SELECT on performance_schema.* to 'hbnb_dev'@'localhost';
