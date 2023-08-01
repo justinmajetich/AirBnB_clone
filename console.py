563ec8202c54b35654a228b5e19548ca61d2dd40
JDaniel26-Bory <juanrestrepo246@gmail.com>
2023-08-01 11:35:58 -0500

    update some fix for the projects

--- console.py
+++ console.py
@@ -115,15 +115,39 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, arg):
         """ Create an object of any class"""
-        if not arg:
+        if not args:
             print("** class name missing **")
             return
 
-        arg = arg.split(" ")
-        if arg not in HBNBCommand.classes:
+        args = args.split(" ")
+        if args[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes(arg[0])()
+        new_instance = HBNBCommand.classes[args[0]]()
+
+        for pair in args[1:]:
+            attr = pair.split("=", maxsplit=1)
+            key = attr[0]
+            value = ""
+            if attr[1][0] == '\"':
+                value = attr[1][1:-1].replace("_", " ").replace('"', '\"')
+            else:
+                if '.' in attr[1]:
+                    try:
+                        value = float(attr[1])
+                    except value.DoesNotExist:
+                        pass
+                else:
+                    try:
+                        value = int(attr[1])
+                    except value.DoesNotExist:
+                        pass
+            if value != "":
+                setattr(new_instance, key, value)
+            else:
+                pass
+        new_instance.save()
+        print(new_instance.id)
 
     def help_create(self):
         """ Help information for the create method """
@@ -318,6 +342,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
