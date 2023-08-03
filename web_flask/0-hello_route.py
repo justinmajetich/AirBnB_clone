51167164157e5de10b0da605493154eabbe4ad52
Bashir76 <ameerkn76@gmail.com>
2023-08-03 14:10:33 +0000

    fix file 1

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,12 +9,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def hello():
+        return "Hello HBNB!"
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
-    return "Hello HBNB!"
+    if __name__ == "__main__":
+            app.run(host='0.0.0.0', port=5000)
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
