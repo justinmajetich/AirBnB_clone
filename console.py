7c897f4dc0c8b03df81972967b560f7c3865fa76
Alvarez0Jose <josealejandro220@gmail.com>
2023-08-03 15:40:21 -0400

    completing the changes needed for the console

--- console.py
+++ console.py
@@ -125,14 +125,35 @@ class HBNBCommand(cmd.Cmd):
         if cls not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        if  cls in HBNBCommand.classes:
-            new_dict = {}
+        
+        if cls in HBNBCommand.classes:
+             new_dict = {}
             if len(args) == 1:
                 # print(args)
-        new_instance = HBNBCommand.classes[cls]()
-        # print(new_instance)
-        new_instance.save()
-        print(new_instance.id)
+                new_instance = HBNBCommand.classes[cls]()
+                # print(new_instance)
+                new_instance.save()
+                print(new_instance.id)
+            else:
+                for a in args:
+                    if "=" in a:
+                        key_value_list = a.split('=')
+                        key = key_value_list[0]
+                        value = key_value_list[1]
+                        if value[0] and value[-1] =='"':
+                            value = value[1:-1]
+                            if '_' in value:
+                                value = value.replace('_', ' ')
+                        else:
+                            value = eval(value)
+
+                        new_dict[key] = value
+
+                    new_instance = HBNBCommand.classes[cls]()
+                    new_instance.__dict__.update(new_dict)
+                    new_instance.save()
+                    print(new_instance.id)
+
 
     def help_create(self):
         """ Help information for the create method """
