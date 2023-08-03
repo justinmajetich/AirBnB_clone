e99f62206974ff12f6fc6b7b94eb80a242792d1e
Bluebee45 <santi72143@gmail.com>
2023-08-02 21:48:43 -0500

    upgrade the file city

--- models/city.py
+++ models/city.py
@@ -2,11 +2,17 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship, backref
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
+    places = relationship(
+            "Place",
+            cascade="all",
+            backref=backref("cities", cascade="all"),
+            passive_deletes=True
     state_id = ""
     name = ""
