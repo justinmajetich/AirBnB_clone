bfe61b7e297822b53dc401fc37c75feba45d01e8
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-08-04 08:06:27 -0500

    Update file amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import relationship
 
 class Amenity(BaseModel, Base):
     """
-    Represents an Amenity for a MySQL database.
+    Represents an Amenity for a MySQL.
 
     Inherits from SQLAlchemy Base and links to the MySQL table amenities.
 
