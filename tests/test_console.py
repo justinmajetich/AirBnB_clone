004b8d62c18c3edb0f2ba693cd0d21ae9a2f947c
rubio91147 <rubendariofb@gmail.com>
2023-08-04 11:59:27 -0500

    update test

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,17 +1,53 @@
 #!/usr/bin/python3
-"""
-Test File for Console
-"""
+"""test for console"""
 import unittest
-import pep8
+from unittest.mock import patch
+from io import StringIO
+import os
+import console
+from console import HBNBCommand
 
 
-class tests_Console(unittest.TestCase):
-    """Unittest"""
+class TestConsole(unittest.TestCase):
+    """Test Suite for the console"""
 
-    def test_pep8_conformance(self):
-        """test pep8"""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['console.py'])
-        self.assertEqual(
-            result.total_errors, 0, "Found code style errors (and warnings).")
+    @classmethod
+    def setUpClass(cls):
+        """setup for the test"""
+        cls.consol = HBNBCommand()
+
+    @classmethod
+    def teardown(cls):
+        """at the end of the test this will tear it down"""
+        del cls.consol
+
+    def tearDown(self):
+        """Remove temporary file (file.json) created as a result"""
+        if (os.getenv('HBNB_TYPE_STORAGE') != 'db'):
+            try:
+                os.remove("file.json")
+            except Exception:
+                pass
+
+    def test_docstrings_in_console(self):
+        """checking for docstrings"""
+        self.assertIsNotNone(console.__doc__)
+        self.assertIsNotNone(HBNBCommand.emptyline.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_quit.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_EOF.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_create.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_show.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_destroy.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_all.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_update.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_count.__doc__)
+
+    def test_emptyline(self):
+        """Test empty line"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.onecmd("\n")
+            self.assertEqual('', f.getvalue())
+
+
+if __name__ == "__main__":
+    unittest.main()
