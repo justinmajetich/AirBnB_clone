53fa47bcf26a213414d96cc408e395cd2248659c
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-08-04 08:17:49 -0500

    Update file test_db_stoarge.py

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -5,7 +5,7 @@ Test validation the class TestDBStorage
 import unittest
 import os
 import tempfile
-from models.engine.db_storage import DBStorage, storage
+from models.engine.db_storage import DBStorage
 from models.state import State
 
 
@@ -17,7 +17,7 @@ class TestDBStorage(unittest.TestCase):
         """setting up a test environment for the class"""
         cls.db_fd, cls.db_path = tempfile.mkstemp()
         os.environ['HBNB_TYPE_STORAGE'] = 'db'
-        storage._FileStorage__objects.clear()
+        DBStorage._DBStorage__objects.clear()
         cls.storage = DBStorage()
         cls.storage.reload()
 
@@ -34,25 +34,25 @@ class TestDBStorage(unittest.TestCase):
         removes all objects in the base
         """
         del_list = []
-        for key in storage.all().keys():
+        for key in DBStorage._DBStorage__objects.keys():
             del_list.append(key)
         for key in del_list:
-            storage._DBStorage__session.delete(storage.all()[key])
-            storage._DBStorage__session.commit()
+            DBStorage._DBStorage__session.delete(DBStorage._DBStorage__objects[key])
+            DBStorage._DBStorage__session.commit()
 
     def test_obj_list_empty(self):
         """
         checks that the __objects dictionary of the
         FileStorage class instance is initially empty
         """
-        self.assertEqual(len(storage.all()), 0)
+        self.assertEqual(len(DBStorage._DBStorage__objects), 0)
 
     def test_reload(self):
         """
         checks if reload() of the FileStorage class works
         correctly when the object storage file does not exist
         """
-        self.assertEqual(storage.reload(), None)
+        self.assertEqual(self.storage.reload(), None)
 
     def test_type_objects(self):
         """
@@ -60,7 +60,7 @@ class TestDBStorage(unittest.TestCase):
         of a class that uses a dictionary data storage system
         (storage.all()) is of type dictionary.
         """
-        self.assertEqual(type(storage.all()), dict)
+        self.assertEqual(type(DBStorage._DBStorage__objects), dict)
 
     def test_store(self):
         """checks that the object has been correctly saved in the database"""
@@ -68,9 +68,12 @@ class TestDBStorage(unittest.TestCase):
         new.save()
         _id = new.to_dict()['id']
         self.assertIn(new.__class__.__name__ + '.' + _id,
-                      storage.all(type(new)).keys())
+                      DBStorage._DBStorage__objects.keys())
 
     def test_storage_var_created(self):
         """verifies that an object of class"""
-        from models.engine.db_storage import DBStorage
-        self.assertEqual(type(storage), DBStorage)
+        self.assertEqual(type(self.storage), DBStorage)
+
+
+if __name__ == "__main__":
+    unittest.main()
