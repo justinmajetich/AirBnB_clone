0783d345d62f58516e29d83efd498180bbff0996
Sonnydread <alvaradoguille590@gmail.com>
2023-08-10 21:27:51 -0700

    task8

--- models/user.py
+++ models/user.py
@@ -8,23 +8,13 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from os import getenv
 
-if getenv("HBN_TYPE_STORAGE") == "db":
-    class User(BaseModel, Base):
-        """This class defines a user by various attributes"""
-        __tablename__ = "users"
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
-        places = relationship("Place", backref="user",
-                              cascade="all, delete-orphan")
-        reviews = relationship("Review", backref="user",
-                               cascade="all, delete-orphan")
 
-elif getenv("HBN_TYPE_STORAGE") != "db":
-    class User(BaseModel):
-        """This class defines a user by various attributes"""
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+class User(BaseModel, Base):
+    """This class defines a user by various attributes"""
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
