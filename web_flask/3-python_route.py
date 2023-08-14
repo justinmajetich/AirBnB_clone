db932ec522bf7282cbf31c6d8869ae4c156c4a2e
JDaniel26-Bory <juanrestrepo246@gmail.com>
2023-08-14 14:23:24 -0500

    add the file 3-python_route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Import Modules"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """Main Route"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """hbnb Route"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """c route , return text with spaces"""
+    text_with_spaces = text.replace('_', ' ')
+    return f"C {text_with_spaces}"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text):
+    """python route, return text with spaces"""
+    return "Python {}".format(text.replace('_', ' '))
+
+
+"""Entry Point"""
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
