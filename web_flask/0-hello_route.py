b7109f33585e06cd95cf9aee0a78abdca55cea57
JDaniel26-Bory <juanrestrepo246@gmail.com>
2023-08-14 11:51:57 -0500

    update the file 0-hello_route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+"""Import Modules"""
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_route():
+    """Main Route"""
     return 'Hello HBNB!'
 
 
+"""Entry Point"""
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
