3165aac25e486344cf13023734a5f292d840f515
Ubuntu <samueljohn3999@gmail.com>
2023-08-17 17:00:14 +0000

    application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,18 +2,18 @@
 """Starts a Flask web application.
 The application listens on 0.0.0.0, port 5000.
 Routes:
-    /: Displays 'Hello HBNB!'
+    /airbnb-onepage/: Displays 'Hello HBNB!'
 """
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/airbnb-onepage/", strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -73,5 +73,13 @@ def number_odd_or_even(n):
     return render_template("6-number_odd_or_even.html", n=n)
 
 
+@app.route("/airbnb-dynamic/number_odd_or_even/<int:n>", strict_slashes=False)
+def airbnb_number_odd_or_even(n):
+    """Displays an HTML page only if <n> is an integer.
+    States whether <n> is odd or even in the body.
+    """
+    return render_template("6-number_odd_or_even.html", n=n)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port='5000')
