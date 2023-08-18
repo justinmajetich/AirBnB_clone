c9fb91677ba9c3cb1057b23ce875c13912e3f33c
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-08-18 00:23:03 -0400

    python cool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Script that start a Flask web application"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hell():
+    """return a string"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """prints hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """"replace _ with spaces"""
+    text = text.replace('_', ' ')
+    return f"c {text}"
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text='is cool'):
+    """"replace _ with spaces"""
+    if text is not 'is cool':
+        text = text.replace('_', ' ')
+    return f"Python {text}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
