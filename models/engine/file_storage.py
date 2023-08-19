f608143364edc129dcfc18866d47771060716c50
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-08-18 21:24:42 -0400

    updts to files

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -66,3 +66,7 @@ class FileStorage:
             key = obj.to_dict()['__class__'] + '.' + obj.id
             if key in FileStorage.__objects:
                 del FileStorage.__objects[key]
+
+    def close(self):
+        """deserializing the JSON file to objects"""
+        self.reload()
