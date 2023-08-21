bc742667f55def156916448891b1834f605d8ccc
CJ-The-Black-Child <congomigue@gmail.com>
2023-08-21 10:41:26 +0000

    updated the User model

--- models/user.py
+++ models/user.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
+from models.base import Base
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+    __tablename__ = 'users'
     email = ''
     password = ''
     first_name = ''
