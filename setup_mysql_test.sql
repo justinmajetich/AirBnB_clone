edc7b5539edf9d695c5f2f336cae85dab31f783e
uelkariuki <cratzuel@gmail.com>
2023-08-21 17:27:04 +0000

    Fix Task 4. MySQL setup test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,5 +3,5 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED WITH mysql_native_p
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 USE hbnb_test_db;
 GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
