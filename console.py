b976a14b34945cffd6ec2643dd25307ae4ca487a
MichaelDecent <michaeldecent2@gmail.com>
2023-08-21 22:04:57 +0000

    updated 'do_create' function in console.py

--- console.py
+++ console.py
@@ -74,7 +74,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] =='}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -114,35 +114,35 @@ class HBNBCommand(cmd.Cmd):
         """ Overrides the emptyline method of CMD """
         pass
 
-    def do_create(self, args):
+     def do_create(self, args):
         """ Create an object of any class"""
-        commands = args.split()
-        if not commands:
+        if not args:
             print("** class name missing **")
             return
-        elif commands[0] not in HBNBCommand.classes:
+        commands = args.split()
+        if commands[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-
         new_instance = HBNBCommand.classes[commands[0]]()
-        kwargs = {}
-        for kw in commands[1:]:
+        for kwarg in commands[1:]:
             try:
-                kwarg = kw.split('=')
-                key = kwarg[0]
-                value = kwarg[1]
-                re.sub(r"-", " ", value)
-                value.replace('"', '/"')
-
+                param = kwarg.split("=")
+                key = param[0]
+                val = param[1]
+                """If the key is a valid attribute of the object"""
                 if hasattr(new_instance, key):
-                    setattr(new_instance, key, eval(value))
-                else:
-                    continue
-            except(ValueError, IndexError):
+                    val = val.replace("_", " ")
+                    """Remove quote signs if present in value string"""
+                    try:
+                        val = eval(val)
+                    except NameError:
+                        pass
+                    setattr(new_instance, key, val)
+            except (IndexError, ValueError):
                 pass
-                
         new_instance.save()
         print(new_instance.id)
+        
     def help_create(self):
         """ Help information for the create method """
         print("Creates a class of any type")
@@ -289,7 +289,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -297,10 +297,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
