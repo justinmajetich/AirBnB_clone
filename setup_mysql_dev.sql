21c5ed2f1aaf0d39b1b2e674f0d53fc3e2e5edfe
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-08-21 14:24:15 +0300

    setting up mysql database

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,6 @@
 -- Preparing MySQL server for the project
 -- hbnb_dev_db database
--- new user hbnb_dev in localhost
--- password is hbnb_dev_pwd
+-- new user hbnb_dev in localhost password is hbnb_dev_pwd
 -- grant all privileges for hbnb_dev on hbnb_dev_db
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
