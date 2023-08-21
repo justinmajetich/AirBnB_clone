d542ea0cef2d650b5fe54a4cfbb5d1245157b31c
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-08-21 15:31:50 -0700

    [Update] Apdate the do_quit function

--- console.py
+++ console.py
@@ -96,7 +96,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_quit(self, command):
         """ Method to exit the HBNB console"""
-        exit()
+        return True
 
     def help_quit(self):
         """ Prints the help documentation for quit  """
