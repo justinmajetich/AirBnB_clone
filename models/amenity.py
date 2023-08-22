80ccd781e68ce7a8f6d7dda3234d24e419fef82a
CJ-The-Black-Child <congomigue@gmail.com>
2023-08-22 20:54:00 +0000

    Added task handline  DBStorage - Review

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models import storage_type
+from sqlalchemy import Column, String
 
 
 class Amenity(BaseModel):
-    name = ""
+    """Defines the amenity class"""
+    __tablename__ = 'amenities'
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
