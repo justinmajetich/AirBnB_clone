b550d5badf8a311c7a60684177c32bf0dec1b52f
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-08-22 04:21:36 -0700

    [Pull] Pulling 2 files sql setupe files from yan

---
+++ setup_mysql_test.sql
@@ -0,0 +1,8 @@
+-- Prepare a MySQL server for the project
+-- Creates hbnb_test_db database and users and grant permissions
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+USE hbnb_test_db;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
