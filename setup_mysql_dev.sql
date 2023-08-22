0f9a6513709dad5941ca445f90fe463f9b86c536
horlahuk <olaitan.u.k@gmail.com>
2023-08-22 05:09:51 -0700

    add task 3&4

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,5 @@
 -- script that prepares a MySQL server
--- create a database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
--- create a new user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
---grant privileges
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
