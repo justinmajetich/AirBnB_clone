4e44d43bc8e6dd8fc842208cb394efde171c15ff
horlahuk <olaitan.u.k@gmail.com>
2023-08-22 12:18:57 -0700

    uncomment storage.save() in console

--- console.py
+++ console.py
@@ -136,7 +136,7 @@ class HBNBCommand(cmd.Cmd):
         new_instance = HBNBCommand.classes[args[0]](**kwargs)
         storage.save()
         print(new_instance.id)
-        #storage.save()
+        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
