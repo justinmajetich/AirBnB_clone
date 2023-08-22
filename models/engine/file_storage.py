d95b526f7a746405b5020c036a761fd79af784b0
sosthene <sosthenetimi94@gmail.com>
2023-08-22 13:44:47 +0300

    Storage requirement improvements

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,9 +25,9 @@ class FileStorage:
 
     def save(self):
         """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
+        with open(self.__file_path, 'w') as f:
             temp = {}
-            temp.update(FileStorage.__objects)
+            temp.update(self.__objects)
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
