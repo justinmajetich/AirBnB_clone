10d0ce5e1fae73b4aedb918db58aed440375146f
Nwaigba66 <glorianwaigba@gmail.com>
2023-08-22 12:07:47 -0700

    Added DBStorage - User and DBStorage - Place

--- models/city.py
+++ models/city.py
@@ -3,7 +3,7 @@
 from sqlalchemy import Columns, String, Foreignkey
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
-
+from models.place import Place
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
@@ -11,3 +11,18 @@ class City(BaseModel, Base):
 
     state_id = Column(String=(60), nullable=False)
     name = Column(String=(128), ForeignKey('state_id'), nullable=False)
+
+    """City class for storing city information"""
+
+    if models.storage_type == 'db':
+        places = relationship("Place", cascade="all, delete", back_populates="city")
+    else:
+        @property
+        def places(self):
+            from models import storage
+            from models.place import Place
+            all_places = storage.all(Place)
+            return [place for place in all_places.values() if place.city_id == self.id]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
