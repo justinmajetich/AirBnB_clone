ef24c6a55269111095525ba4f81e44cf09fc8e2c
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-08-23 01:07:52 +0300

    working on the basemodel and state and city

--- models/city.py
+++ models/city.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """This is the city class"""
-from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy import ForeignKey
@@ -16,5 +15,5 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", cascade='all, delete, delete-orphan',
+    places = relationship("Place", cascade='delete',
                           backref="cities")
