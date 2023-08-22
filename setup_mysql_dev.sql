386ffff630e5bd6b5f88642b311818a2ffd2b2bd
Mwadali <wellingtonmwadali@gmail.com>
2023-08-22 15:06:59 -0700

    setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -6,7 +6,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 --username hbnb_dev with all privileges
 --password : hbnb_dev_pwd if it dosen't exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
---grant  all privileges to new user
+--grant  all privileges to neww user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
 --grant the SELECT privilege for user hbnb_dev in db performance_schema
