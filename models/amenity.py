dca9453f644924c2e16c41e24aa6c0723002e96b
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-08-23 01:18:56 +0300

    working on the amenity and place models

--- models/amenity.py
+++ models/amenity.py
@@ -13,5 +13,4 @@ class Amenity(BaseModel, Base):
     """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity",
-                                   viewonly=False)
+    place_amenities = relationship("Place", secondary="place_amenity")
