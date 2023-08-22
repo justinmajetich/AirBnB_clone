a9390ec12225997ee051e369c2cb01ee5d39cd57
horlahuk <olaitan.u.k@gmail.com>
2023-08-22 04:58:48 -0700

    add task 3&4

---
+++ setup_mysql_test.sql
@@ -0,0 +1,11 @@
+-- script that prepares a MySQL server
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- create a new user
+
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+--grant privileges
+
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
