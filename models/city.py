e4a3e1c55232deda271224510fb8aaf4b3fbc52f
IdiJr <idialijnr@gmail.com>
2023-08-23 22:52:08 +0000

    fixed typo

--- models/city.py
+++ models/city.py
@@ -9,4 +9,4 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
 
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'),nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
