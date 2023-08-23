fe07fd62432261dfb45cc94e2ca08c97e7150af4
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-08-23 20:23:49 +0100

    Bug fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,7 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        if cls is None:
+        if cls is not None:
             my_dict = {}
             for key, value in FileStorage.__objects.items():
                 if value.__class__ == cls:
