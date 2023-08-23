c1f6c66b4960d223efe1527981bc0d1e2f9f2754
shield <myshield02@gmail.com>
2023-08-23 14:49:49 +0100

    added instatnce attributes, and handle dbstorage and file storage mechanism

--- models/state.py
+++ models/state.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+import models
+from os import getenv
+from models.city import City
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Get all related city objects"""
+            all_cities = list(models.storage.all(City).values)
+            return [city for city in all_cities if city.state.id == self.id]
