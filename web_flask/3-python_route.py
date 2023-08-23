c574b0268cfaab02e2831026da8f83acd4e03b37
Ademola Ayinde <122974487+AdemolaAyinde@users.noreply.github.com>
2023-08-23 15:58:19 +0100

    Create 3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Starts Flask web app
+Routes:
+    / - display "Hello HBNB!"
+    /hbnb - display "HBNB"
+    /c/<text> - display "C <text>"
+    /python/<text> - display "Python is cool"
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hbnb_route():
+    """prints Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """prints HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_text(text):
+    """prints C followed by <text> content"""
+    text = text.replace("_", " ")
+    return "C %s" % text
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
+def python_text(text="is cool"):
+    """prints Python is cool"""
+    text = text.replace("_", " ")
+    return "Python %s" % text
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
