ba5346e543df367e28f999611ad20ef84052f6b7
Ademola Ayinde <122974487+AdemolaAyinde@users.noreply.github.com>
2023-08-23 14:38:23 +0100

    Create  1-pack_web_static.py

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Generates a .tgz archive from the contents of the web_static folder."""
+from fabric.api import local
+import time
+
+
+def do_pack():
+    """Generate an tgz archive from web_static folder"""
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz web_static/".
+              format(time.strftime("%Y%m%d%H%M%S")))
+        return ("versions/web_static_{}.tgz".format(time.
+                                                    strftime("%Y%m%d%H%M%S")))
+    except:
+        return None
