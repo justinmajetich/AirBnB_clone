88c1aac5ba2b6a1033b76c127d8f904c98d4a1ba
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-08-23 19:59:08 +0100

    Prev Version

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" """
+from tests.test_models.test_base_model import test_basemodel
+from models.amenity import Amenity
+
+
+class test_Amenity(test_basemodel):
+    """ """
+
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "Amenity"
+        self.value = Amenity
+
+    def test_name2(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.name), str)
