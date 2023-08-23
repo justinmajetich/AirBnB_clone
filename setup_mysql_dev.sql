f2202a7dc0dd58286e0ff8a1285cfdb41f7397ee
KOBBY-IM <nyarkotheo2@gmail.com>
2023-08-23 15:36:59 +0000

    task 3

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,8 @@
+-- Script that prepares a MySQL server for the project
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
