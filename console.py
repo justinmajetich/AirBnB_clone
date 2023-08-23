81352846b800e631609c996f3ba39026ce8d92f7
Omar Jammeh <orms45@gmail.com>
2023-08-23 16:18:00 +0000

    Updated do_create method: To handle arguments

--- console.py
+++ console.py
@@ -65,20 +65,20 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline.partition(', ')  # pline convert to tuple
 
                 # isolate _id, stripping quotes
-                _id = pline[0].replace('\"', '')
-                # possible bug here:
-                # empty quotes register as empty _id when replaced
+                if pline[0]:
+                    _id = pline[0].replace('\"', '')
 
                 # if arguments exist beyond _id
-                pline = pline[2].strip()  # pline is now str
-                if pline:
-                    # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
-                            and type(eval(pline)) is dict:
-                        _args = pline
-                    else:
-                        _args = pline.replace(',', '')
-                        # _args = _args.replace('\"', '')
+                if pline[2]:
+                    pline = pline[2].strip()  # pline is now str
+                    if pline:
+                        # check for *args or **kwargs
+                        if pline[0] == '{' and pline[-1] == '}'\
+                                and type(eval(pline)) is dict:
+                            _args = pline
+                        else:
+                            _args = pline.replace(',', '')
+                            # _args = _args.replace('\"', '')
             line = ' '.join([_cmd, _cls, _id, _args])
 
         except Exception as mess:
@@ -115,13 +115,29 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        if not args:
+        # eg. create User name="Omar_Jammeh" address="Busumbala"
+        args = args.split()
+        if not args[0]:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        elif args[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
+        _args = args[1:]
+        kwargs = {k_v.split('=')[0]: k_v.split('=')[1] for k_v in _args}
+
+        new_instance = HBNBCommand.classes[args[0]]()
+        for key, value in kwargs.items():
+            if value.startswith('"'):
+                value = value.replace('_', ' ')
+                value = value.strip('"')
+                if '"' in value:
+                    value.replace('"', '\"')
+                setattr(new_instance, key, value)
+            elif '.' in value:
+                setattr(new_instance, key, float(value)) 
+            elif int(value):
+                setattr(new_instance, key, int(value)) 
         storage.save()
         print(new_instance.id)
         storage.save()
@@ -272,7 +288,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +296,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
