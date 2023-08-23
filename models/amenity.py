63dbd3c9838c45f34d0a54179ca0608bc906c089
Thomas Collins <collinskthomas@gmail.com>
2023-08-23 13:55:22 +0000

    Update Amenity and Place classes with sqlalchemy fields & many-to-many relationship

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """ Class that defines an amenity """
+    __tablename__ = "amenities"
+
+    name = Column(String(128), nullable=False)
