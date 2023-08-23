5949324d00ec84fd4e5141477f034cfbee033157
Thomas Collins <collinskthomas@gmail.com>
2023-08-23 11:04:49 +0000

    Update Place, User and City classes with sqlalchemy fields & table relationships

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -10,3 +11,4 @@ class City(BaseModel, Base):
 
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", backref="cities", cascade="all, delete")
