db981d450726db70ac0c7c447ecc8ff4ef62dc84
salmalah <s.salmalahmidi@gmail.com>
2023-08-23 18:00:56 -0700

    update base_model

--- models/base_model.py
+++ models/base_model.py
@@ -8,19 +8,25 @@ class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+        from models import storage
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.now()
+        if len(kwargs) != 0:
+            self._assign_att(kwargs)
             storage.new(self)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            storage.new(self)
+    def _assign_att(self, att):
+        """
+        Assigns in attrubutes for kwargs
+        """
+        str_format = "%Y-%m-%dT%H:%M:%S.%f"
+        for key, value in att.items():
+            if key == "created_at" or key == "updated_at":
+                t = self.__dict__[key]
+                t = datetime.strptime(value, str_format)
+            else:
+                t = value
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -37,8 +43,7 @@ class BaseModel:
         """Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary.update({"__class__": (str(type(self)).split(".")[-1]).split("'")[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
