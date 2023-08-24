48561dc891160873e20ee98a1f5bb54b6536d6c0
Tassuo7 <abdelhamid.jack@gmail.com>
2023-08-23 19:14:15 -0700

    fix pycodestyle

--- console.py
+++ console.py
@@ -73,9 +73,9 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if (pline[0] == "{" and pline[-1] == "}"\
-                        and type(eval(pline)) is dict
-                        ):
+                    if (pline[0] == "{"
+                            and pline[-1] == "}"
+                            and type(eval(pline)) is dict):
                         _args = pline
                     else:
                         _args = pline.replace(",", "")
