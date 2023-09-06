586c6d7e66d8accccbb5dab1f707778132d881b7
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-09-06 21:44:38 +0300

    web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,6 +11,6 @@ ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 chown -R ubuntu:ubuntu /data/
 chgrp -R ubuntu:ubuntu /data/
-sed -i '\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
+sed -i '40i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
 nginx restart
 exit 0
