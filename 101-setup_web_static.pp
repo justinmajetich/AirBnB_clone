940971368a9e2fa56167269245ac221d433e3d7a
Mohamed Amine <ma.abahmane@gmail.com>
2023-09-06 20:08:28 +0100

    X

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,7 +1,7 @@
 #  Redo the task #0 but by using Puppet:
 
 exec { 'custom http header':
-command  => '
+command  => "
 sudo apt-get update;
 sudo apt-get upgrade -y;
 sudo apt-get install -y nginx;
@@ -9,15 +9,15 @@ sudo apt-get install -y nginx;
 sudo mkdir -p /data/web_static/shared;
 sudo mkdir -p /data/web_static/releases/test;
 
-echo "MA-Abahmane" | sudo tee /data/web_static/releases/test/index.html;
+echo 'MA-Abahmane' | sudo tee /data/web_static/releases/test/index.html;
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current;
 
 sudo chown -R -h ubuntu:ubuntu /data/;
 
 location='location /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n';
-sudo sed -i '40i\\t'"$location" /etc/nginx/sites-available/default;
+sudo sed -i '40i\\t''$location' /etc/nginx/sites-available/default;
 
-sudo service nginx restart;',
+sudo service nginx restart;",
 provider => shell,
 }
