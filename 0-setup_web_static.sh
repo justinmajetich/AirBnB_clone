9c103803c5b96642b3140e8e549857df99702f63
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-09-06 21:41:40 +0300

    web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,8 +9,8 @@ mkdir -p /data/web_static/shared/
 echo "Hello World!" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
-sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
+chown -R ubuntu:ubuntu /data/
+chgrp -R ubuntu:ubuntu /data/
+sed -i '\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
 nginx restart
 exit 0
