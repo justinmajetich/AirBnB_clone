018399b28e780d04a9d6ac8883ecbe9fed6ea358
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-09-07 22:15:25 +0100

    X

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,53 +1,69 @@
-# 
-
+# Redo the task #0 but by using Puppet:
 exec { 'update':
-  command  => 'sudo apt-get -y update',
-}
+  command => '/usr/bin/apt-get update',
+} ->
 
-exec { 'install Nginx':
-  command  => 'sudo apt-get -y install nginx',
-  require  => Exec['update'],
-}
-
-exec { 'start Nginx':
-  command  => 'sudo service nginx start',
-  require  => Exec['install Nginx'],
-}
-
-exec { 'create first directory':
-  command  => 'sudo mkdir -p /data/web_static/releases/test/',
-  require  => Exec['start Nginx'],
-}
-
-exec { 'create second directory':
-  command  => 'sudo mkdir -p /data/web_static/shared/',
-  require  => Exec['create first directory'],
-}
-
-exec { 'content into html':
-  command  => 'echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html',
-  require  => Exec['create second directory'],
-}
-
-exec { 'symbolic link':
-  command  => 'sudo ln -sf /data/web_static/releases/test/ /data/web_static/current',
-  require  => Exec['content into html'],
-}
-
-exec { 'put location':
-  command  => 'sudo sed -i \'38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n\' /etc/nginx/sites-available/default',
-  require  => Exec['symbolic link'],
-}
-
-exec { 'restart Nginx':
-  command  => 'sudo service nginx restart',
-  require  => Exec['put location'],
-}
+package { 'nginx':
+  ensure   => 'present',
+  provider => 'apt'
+} ->
 
 file { '/data/':
-  ensure  => directory,
+    ensure => 'directory',
+    group  => 'ubuntu',
+    owner  => 'ubuntu',
+} ->
+
+file { '/data/web_static/':
+    ensure => 'directory',
+    group  => 'ubuntu',
+    owner  => 'ubuntu',
+} ->
+
+file { '/data/web_static/releases/':
+    ensure => 'directory',
+    group  => 'ubuntu',
+    owner  => 'ubuntu',
+} ->
+
+file { '/data/web_static/shared/':
+    ensure => 'directory',
+    group  => 'ubuntu',
+    owner  => 'ubuntu',
+} ->
+
+file { '/data/web_static/releases/test/':
+    ensure => 'directory',
+    group  => 'ubuntu',
+    owner  => 'ubuntu',
+} ->
+
+file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  path    => '/data/web_static/releases/test/index.html',
   owner   => 'ubuntu',
   group   => 'ubuntu',
-  recurse => true,
-  require => Exec['restart Nginx'],
+  content => "Holberton School Puppet\n",
+} ->
+
+file { '/data/web_static/current':
+  ensure  => 'link',
+  replace => 'yes',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  target  => '/data/web_static/releases/test',
+} ->
+
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
+} ->
+
+exec { 'sed':
+  command => "sed -i \
+  '/^\tlisten 80 default_server;$/i location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+} ->
+
+exec { 'nginx restart':
+  path => '/etc/init.d/'
 }
