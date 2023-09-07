10dc1ac127710c02654114e5d2d047eecea8ad3f
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-09-08 00:53:35 +0100

    FIX

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,4 +1,16 @@
-# Redo the task #0 but by using Puppet:
+# Write a Puppet script that sets up your web servers for the deployment of web_static. It must:
+#   Install Nginx if it not already installed
+#   Create the folder /data/ if it doesn’t already exist
+#   Create the folder /data/web_static/ if it doesn’t already exist
+#   Create the folder /data/web_static/releases/ if it doesn’t already exist
+#   Create the folder /data/web_static/shared/ if it doesn’t already exist
+#   Create the folder /data/web_static/releases/test/ if it doesn’t already exist
+#   Create a fake HTML file /data/web_static/releases/test/index.html (with simple content, to test your Nginx configuration)
+#   Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder. If the symbolic link already exists, it should be deleted and recreated every time the script is ran.
+#   Give ownership of the /data/ folder to the ubuntu user AND group (you can assume this user and group exist). This should be recursive; everything inside should be created/owned by this user/group.
+#   Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static (ex: https://mydomainname.tech/hbnb_static). Don’t forget to restart Nginx after updating the configuration:
+#   Use alias inside your Nginx configuration
+#   Your program should always exit successfully. Don’t forget to run your script on both of your web servers.
 
 exec { 'update':
   command => '/usr/bin/apt-get update',
@@ -65,6 +77,8 @@ exec { 'sed':
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 } ->
 
-exec { 'nginx restart':
-  path => '/etc/init.d/'
+exec { 'nginx_restart':
+  command => '/etc/init.d/nginx restart',
+  path    => '/etc/init.d/',
+  refreshonly => true,
 }
