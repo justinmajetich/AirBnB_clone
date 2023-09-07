ac54fd6bec6bcd04b63fd2bf04317e995fa9cb0d
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-09-07 23:17:05 +0300

    puppet way ofpreparing our web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,10 +6,11 @@ apt-get -y install nginx
 service nginx start
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "Hello World!" > /data/web_static/releases/test/index.html
+echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 chown -R ubuntu:ubuntu /data/
 chgrp -R ubuntu:ubuntu /data/
 sed -i '40i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
 nginx restart
+exit 0
