788a745e4cff26fd113dbb6abfbdc7551c2db998
root <root@DESKTOP-KUT53MR>
2023-09-08 00:58:36 +0000

    pack seb

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 """
-file to practice use of Fabric
+file to use of Fabric
 """
-from fabric.api import *
-import time
+import os.path
+from datetime import datetime
+from fabric.api import local
 
 
 def do_pack():
-    timestr = time.strftime("%Y%m%d%H%M%S")
-    try:
-        local("mkdir -p versions")
-        local("tar -cvzf versions/web_static_{}.tgz web_static/".
-              format(timestr))
-        return ("versions/web_static_{}.tgz".format(timestr))
-    except Exception:
+    """Create a tar gzipped archive of the directory web_static."""
+    dt = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
+    return file
