5030a7779b0e6e0488a0e5fd30b532ac4e524051
root <root@DESKTOP-KUT53MR>
2023-09-08 00:46:47 +0000

    pack web stat

---
+++ 1-pack_web_static.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+file to practice use of Fabric
+"""
+from fabric.api import *
+import time
+
+
+def do_pack():
+    timestr = time.strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz web_static/".
+              format(timestr))
+        return ("versions/web_static_{}.tgz".format(timestr))
+    except:
+        return None
