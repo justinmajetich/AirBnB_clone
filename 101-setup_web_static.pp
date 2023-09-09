849d5055cebf4ae1838905f7af44042e520520cc
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-09-09 11:57:34 +0100

    Update

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -13,72 +13,72 @@
 #   Your program should always exit successfully. Don’t forget to run your script on both of your web servers.
 
 exec { 'update':
-    command => '/usr/bin/apt-get update',
+command => '/usr/bin/apt-get update',
 } ->
 
 package { 'nginx':
-    ensure   => 'present',
-    provider => 'apt'
+ensure   => 'present',
+provider => 'apt'
 } ->
 
 file { '/data/':
-    ensure => 'directory',
-    group  => 'ubuntu',
-    owner  => 'ubuntu',
+ensure => 'directory',
+group  => 'ubuntu',
+owner  => 'ubuntu',
 } ->
 
 file { '/data/web_static/':
-    ensure => 'directory',
-    group  => 'ubuntu',
-    owner  => 'ubuntu',
+ensure => 'directory',
+group  => 'ubuntu',
+owner  => 'ubuntu',
 } ->
 
 file { '/data/web_static/releases/':
-    ensure => 'directory',
-    group  => 'ubuntu',
-    owner  => 'ubuntu',
+ensure => 'directory',
+group  => 'ubuntu',
+owner  => 'ubuntu',
 } ->
 
 file { '/data/web_static/shared/':
-    ensure => 'directory',
-    group  => 'ubuntu',
-    owner  => 'ubuntu',
+ensure => 'directory',
+group  => 'ubuntu',
+owner  => 'ubuntu',
 } ->
 
 file { '/data/web_static/releases/test/':
-    ensure => 'directory',
-    group  => 'ubuntu',
-    owner  => 'ubuntu',
+ensure => 'directory',
+group  => 'ubuntu',
+owner  => 'ubuntu',
 } ->
 
 file { '/data/web_static/releases/test/index.html':
-    owner   => 'ubuntu',
-    group   => 'ubuntu',
-    ensure  => 'present',
-    content => "Holberton School Puppet\n",
-    path    => '/data/web_static/releases/test/index.html',
+owner   => 'ubuntu',
+group   => 'ubuntu',
+ensure  => 'present',
+content => "Holberton School Puppet\n",
+path    => '/data/web_static/releases/test/index.html',
 } ->
 
 file { '/data/web_static/current':
-    owner   => 'ubuntu',
-    group   => 'ubuntu',
-    ensure  => 'link',
-    replace => 'yes',
-    target  => '/data/web_static/releases/test',
+owner   => 'ubuntu',
+group   => 'ubuntu',
+ensure  => 'link',
+replace => 'yes',
+target  => '/data/web_static/releases/test',
 } ->
 
 exec { 'chown -R ubuntu:ubuntu /data/':
-    path => '/usr/bin/:/usr/local/bin/:/bin/'
+path => '/usr/bin/:/usr/local/bin/:/bin/'
 } ->
 
 exec { 'sed':
-    command => "sed -i \
+command => "sed -i \
   '/^\tlisten 80 default_server;$/i location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default",
-    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 } ->
 
 exec { 'nginx_restart':
-  command => '/etc/init.d/nginx restart',
-  path    => '/etc/init.d/',
-  refreshonly => true,
+command     => '/etc/init.d/nginx restart',
+path        => '/etc/init.d/',
+refreshonly => true,
 }
