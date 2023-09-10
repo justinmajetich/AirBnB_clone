f98ffd1834f7fd5ee7a78f0f770bebb2662ca8e9
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-09-10 17:03:45 +0100

    Update

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -26,7 +26,7 @@ def do_clean(number=0):
         to_keep = number
 
     # Delete all unnecessary archives in the local versions folder
-    local(f'cd version ; ls -t | tail -n +{to_keep + 1} | xargs rm -rf')
+    local(f'cd versions ; ls -t | tail -n +{to_keep + 1} | xargs rm -rf')
 
     # Delete all unnecessary archives in the '/data/web_static/releases'
     # folder of both of your web servers
