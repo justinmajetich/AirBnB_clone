ed129fc93353e6280b0f7f2446f1fbfbe04942c6
Kingsley Akpan <54294050+Khingz@users.noreply.github.com>
2023-09-15 19:26:18 +0100

    mysql setup for test envrionment

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- MySQL Setup for test
+
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
