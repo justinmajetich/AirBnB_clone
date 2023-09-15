5e849670abd5f62b24381cb94ec4ccf43c54d834
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-09-15 18:03:12 +0200

    Title: Started Task 2
    Body: Created the Parser and Added new Functionality to the base model
    yet to add tests
    
    Co-authored-by: nuuxcode <cv.bouhlaoui@gmail.com>

--- console.py
+++ console.py
@@ -22,7 +22,18 @@ class HBNBCommand(cmd.Cmd):
                'BaseModel': BaseModel, 'User': User, 'Place': Place,
                'State': State, 'City': City, 'Amenity': Amenity,
                'Review': Review
-              }
+               }
+
+    valid_keys = {
+               'BaseModel': ['id' , 'created_at', 'updated_at'], 
+               'User': ['id' , 'created_at', 'updated_at',  'email' , 'password', 'first_name', 'last_name'], 
+               'City': ['id' , 'created_at', 'updated_at', 'state_id', 'name'],
+               'State': ['id' , 'created_at', 'updated_at', 'name'], 
+               'Place': ['id' , 'created_at', 'updated_at', 'city_id', 'user_id' , 'name','number_rooms', 'number_bathrooms' ,'max_guest' ,'price_by_night' , 'latitude' ,'longitude'], 
+               'Amenity': ['id' , 'created_at', 'updated_at', 'name'],
+               'Review': ['id' , 'created_at', 'updated_at', 'place_id', 'user_id', 'text']
+               }
+              
     dot_cmds = ['all', 'count', 'show', 'destroy', 'update']
     types = {
              'number_rooms': int, 'number_bathrooms': int,
@@ -113,18 +124,55 @@ class HBNBCommand(cmd.Cmd):
         """ Overrides the emptyline method of CMD """
         pass
 
+    def parse_value(self, value):
+        """cast string to float or int if possible"""
+        is_valid_value = True
+        # To be a valid string it must be of at least length 2 i.e. ""
+        # To be a valid string it must begin and end with double quoatation i.e. "sdsds"
+        if len(value) >= 2 and value[0] == '"' and value[len(value) - 1] == '"':
+            value = value[1:-1]
+            value = value.replace('_', ' ')
+            value = value.replace('"', '\"')
+        else:
+            try:
+                if "." in value:
+                    value = float(value)
+                else:
+                    value = int(value)
+            except ValueError:
+                is_valid_value = False
+        
+        if is_valid_value:
+            return value
+        else:
+            return None
+
     def do_create(self, args):
         """ Create an object of any class"""
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        args_array = args.split()
+        class_name = args_array[0]
+        if class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
+        dict_params = {}
+        for param_index in range(1, len(args_array)):
+            param_array = args_array[param_index].split("=")
+            if len(param_array) == 2:
+                key = param_array[0]
+                if key not in HBNBCommand.valid_keys[class_name]:
+                    continue
+                value = self.parse_value(param_array[1])
+                if value is not None:
+                    dict_params[key] = value
+            else:
+                pass
+
+        new_instance = HBNBCommand.classes[class_name](**dict_params)
         storage.save()
         print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
