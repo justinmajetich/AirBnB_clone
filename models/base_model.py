09003cca73da941f667a51c4ea9eab7f8dc86102
PeterEkwere <peterekwere04@gmail.com>
2023-09-16 10:21:32 -0700

    updated base model and city model

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
+import models
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Datetime
 from datetime import datetime
+
+Base = declarative_base()
 
 
 class BaseModel:
@@ -10,15 +16,20 @@ class BaseModel:
         """Instatntiates a new model"""
         if not kwargs:
             from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+            self.id = Column(Integer, primary_key=True, nullable=False)
+            self.created_at = Column(Datetime, default=datetime.utcnow(),
+                                     nullable=False)
+            self.updated_at = Column(Datetime, default=datetime.utcnow(),
+                                     nullable=False)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            for key, value in kwargs.items():
+                if not hasattr(self, key):
+                    setattr(self, key, value)
+            kwargs['updated_at'] = Column(Datetime, default=datetime.utcnow(),
+                                          nullable=False)
+            kwargs['created_at'] = Column(Datetime, default=datetime.utcnow(),
+                                          nullable=False)
+
             del kwargs['__class__']
             self.__dict__.update(kwargs)
 
@@ -31,7 +42,8 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -41,4 +53,7 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        for key, value in dictionary.items():
+            if key == '_sa_instance_state':
+                del dictionary[key]
         return dictionary
--- models/city.py
+++ models/city.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"
+
+    state_id = Column(String(60), Foreign_key('states.id'))
+    name = Column(String(128), nullable=False)
