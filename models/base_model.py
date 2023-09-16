442cb7ba4113392e687ba4d66e1a11abced8d602
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-09-16 10:43:50 +0200

    t2

--- models/base_model.py
+++ models/base_model.py
@@ -17,20 +17,13 @@ class BaseModel:
     
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        """if not kwargs:
+        if not kwargs:
             #from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)"""
-        if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
@@ -42,9 +35,6 @@ class BaseModel:
                 self.created_at = datetime.now()
             if "updated_at" not in kwargs:
                 self.updated_at = datetime.now()
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
