a01af5152eeab44e79d111bc54c152038407f2c2
Khingz <kingsley99m5@gmail.com>
2023-09-16 10:32:29 +0100

    fix: all method filestorage, upsdted state model

--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models import storage
+from models.city import City
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    
+    name = Column(String(128), nullable=False)
+    cities = relationship(
+            "City",
+            backref="state",
+            cascade="all, delete-orphan")
+    
+    @property
+    def cities(self):
+        """returns the list of City instances with state_id"""
+        cities = storage.all(City)
+        city_list = []
+        for city in list(cities.values()):
+            if city.state_id == self.id:
+                    city_list.append(city)
+        return city_list
