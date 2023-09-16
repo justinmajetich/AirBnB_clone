3443f89c85b056bd64c698cdbc7552e9c6f876c2
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-09-16 10:49:22 +0200

    t2

--- models/base_model.py
+++ models/base_model.py
@@ -37,13 +37,17 @@ class BaseModel:
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        """cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)"""
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.__dict__)
+
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
+        #some changes are made
         self.updated_at = datetime.now()
+        models.storage.new(self)
         storage.save()
 
     def to_dict(self):
