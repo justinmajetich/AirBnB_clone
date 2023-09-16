47ad55b91823c44bce77a0af3141307b0e2cd03b
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-09-16 10:33:59 +0200

    t2

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,8 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime
+
 
 
 class BaseModel:
