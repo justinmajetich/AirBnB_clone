5fbccb9cb7dfd64cf16b687098982ff3754b2f28
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-09-17 14:10:57 +0100

    Title: Task8 debugin checker
    Body: Task8 debugin checker
    
    Co-authored-by: Fuzzworth <nyangasi.m@hotmail.com>

--- models/place.py
+++ models/place.py
@@ -17,4 +17,3 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    amenity_ids = []
