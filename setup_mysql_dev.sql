e01fd582e8aeed29b0e9cde9e3a3c1174ec78d21
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-09-18 02:03:42 +0300

    add an .* attribute on database name to refer to all tables in grant privilege statement

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -6,8 +6,8 @@ USE hbnb_dev_db;
 -- create the user `hbnb_dev` with password `hbnb_dev_pwd`
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- grant all privileges on database 'hbnb_dev_db' to user
-GRANT ALL PRIVILEGES ON hbnb_dev_db TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 -- grant SELECT privilege on database 'performance_schema' to user
-GRANT SELECT ON performance_schema TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 -- flush privileges
 FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -6,8 +6,8 @@ USE hbnb_test_db;
 -- create the user `hbnb_test` with password `hbnb_dev_pwd`
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- grant all privileges on database 'hbnb_test_db' to user
-GRANT ALL PRIVILEGES ON hbnb_test_db TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 -- grant SELECT privilege on database 'performance_schema' to user
-GRANT SELECT ON performance_schema TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 -- flush privileges
 FLUSH PRIVILEGES;
