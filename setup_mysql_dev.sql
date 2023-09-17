6b2681737f12fcf9982b124e3931549c927ec9bc
sakachris <sakachris90@gmail.com>
2023-09-17 17:22:41 +0300

    MySQL setup

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,11 @@
+-- Add database hbnb_dev_db
+-- Add new user hbnb_dev (in localhost)
+-- The password of hbnb_dev should be set to hbnb_dev_pwd
+-- hbnb_dev should have all privileges on the database hbnb_dev_db
+-- hbnb_dev should have SELECT privilege on the database performance_schema
+
+CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
---
+++ setup_mysql_test.sql
@@ -0,0 +1,11 @@
+-- Add database hbnb_test_db
+-- Add new user hbnb_test (in localhost)
+-- The password of hbnb_test should be set to hbnb_test_pwd
+-- hbnb_test should have all privileges on the database hbnb_test_db
+-- hbnb_test should have SELECT privilege on the database performance_schema
+
+CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
