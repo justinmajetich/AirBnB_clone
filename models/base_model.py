7f72a00cad6b6f8664cf9eba80d87983fde1d019
zrdouane <ramirdouane@gmail.com>
2023-09-17 10:07:55 +0100

    fix __str__

--- models/base_model.py
+++ models/base_model.py
@@ -43,13 +43,12 @@ class BaseModel:
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split(".")[-1]).split("'")[0]
-        return "[{}] ({}) {}".format(cls, self.id, self.__dict__)
+        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
-
         self.updated_at = datetime.now()
         storage.new(self)
         storage.save()
@@ -58,8 +57,8 @@ class BaseModel:
         """Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
-        dictionary.update({"__class__": (str(type(self)).
-                                         split(".")[-1]).split("'")[0]})
+        dictionary.update({'__class__':
+                          (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary["created_at"] = self.created_at.isoformat()
         dictionary["updated_at"] = self.updated_at.isoformat()
         if "_sa_instance_state" in dictionary.keys():
