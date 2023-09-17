94982c15868fc286a2e1c5e79039ef96afe17eb0
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-09-17 20:43:11 +0300

    Add comment for each query

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,13 @@
--- creates database 'hbnb_dev_db'
--- creates the user `hbnb_dev` with password `hbnb_dev_pwd`, and
--- with all priviledges on database 'hbnb_dev_db' and SELECT priviledge on database 'performance_schema'
+-- prepares a MySQL server for the project
+-- create database 'hbnb_dev_db'
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- use database
 USE hbnb_dev_db;
+-- create the user `hbnb_dev` with password `hbnb_dev_pwd`
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- grant all privileges on database 'hbnb_dev_db' to user
 GRANT ALL PRIVILEGES ON hbnb_dev_db TO 'hbnb_dev'@'localhost';
+-- grant SELECT privilege on database 'performance_schema' to user
 GRANT SELECT ON performance_schema TO 'hbnb_dev'@'localhost';
+-- flush privileges
 FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,13 @@
--- creates database 'hbnb_test_db'
--- creates the user `hbnb_test` with password `hbnb_test_pwd`, and
--- with all priviledges on database 'hbnb_test_db' and SELECT priviledge on database 'performance_schema'
+-- prepares a MySQL server for the project
+-- create database 'hbnb_test_db'
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- use database
 USE hbnb_test_db;
+-- create the user `hbnb_test` with password `hbnb_dev_pwd`
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- grant all privileges on database 'hbnb_test_db' to user
 GRANT ALL PRIVILEGES ON hbnb_test_db TO 'hbnb_test'@'localhost';
+-- grant SELECT privilege on database 'performance_schema' to user
 GRANT SELECT ON performance_schema TO 'hbnb_test'@'localhost';
+-- flush privileges
 FLUSH PRIVILEGES;
