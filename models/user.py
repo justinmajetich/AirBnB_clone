42ad4aaaa6968d8a0ad2951c081ab4d0ca418a86
zrdouane <ramirdouane@gmail.com>
2023-09-17 10:51:19 +0100

    add base

--- models/user.py
+++ models/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
