574954750254bf99874bebf85980f44bd97e6476
zrdouane <ramirdouane@gmail.com>
2023-09-17 10:57:40 +0100

    edit user

--- models/user.py
+++ models/user.py
@@ -18,10 +18,8 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user",
-                              cascade="all, delete-orphan")
-        reviews = relationship("Review", backref="user",
-                               cascade="all, delete-orphan")
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
     else:
         email = ""
         password = ""
