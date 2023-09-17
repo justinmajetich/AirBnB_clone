a2fa4c8adce4ffcb4ad02cb65c2b2a73b46d5e4a
zrdouane <ramirdouane@gmail.com>
2023-09-17 10:55:11 +0100

    can be null handle

--- models/user.py
+++ models/user.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
+from models.place import Place
+from models.review import Review
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
 
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
-class User(BaseModel):
+
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
+    __tablename__ = 'users'
+    if storage_type == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user",
+                              cascade="all, delete-orphan")
+        reviews = relationship("Review", backref="user",
+                               cascade="all, delete-orphan")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
