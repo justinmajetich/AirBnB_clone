885be952bb711993a3b2207b394cc18d746cd585
GhostProTech <akanniwilliams@gmail.com>
2023-09-18 15:55:21 +0100

    'Add commits to sql scripts'

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,3 +1,4 @@
+--Setup for test user
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
