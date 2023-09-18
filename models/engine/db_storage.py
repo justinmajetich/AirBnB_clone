7055f9d534016996f94d3e2df3609fd1e3c6b907
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-09-18 18:22:31 +0200

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,7 +4,7 @@ from os import getenv
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import (create_engine)
 from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import Base
+from models.base_model import BaseModel
 from models.state import State
 from models.city import City
 from models.user import User
