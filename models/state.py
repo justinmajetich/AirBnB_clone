e9700c398228cda7a87ab7abe549743ae649ebcd
Emad Anwer <emadanwer.official@gmail.com>
2023-09-18 21:36:07 +0300

    state class

--- models/state.py
+++ models/state.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', cascade='all, delete', backref='state')
+
+    @property
+    def cities(self):
+        return cities
