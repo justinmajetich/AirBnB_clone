2196b801b30d9b59ec4d3b2b01868b3f6de09005
omarIdhmaid <o.idhmaid@gmail.com>
2023-09-18 23:48:54 +0100

    mySQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,4 @@
+-- mySQL setup development
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
