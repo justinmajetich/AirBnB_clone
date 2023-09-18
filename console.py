eb018e567cd78af3f028f8e2172120c8aa71521f
Dennismwendwa <dennismusembi2@gmail.com>
2023-09-18 07:42:11 +0300

    creaded method create

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] =='}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -115,16 +115,37 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        if not args:
+
+        try:
+            if not args:
+                raise SyntaxError()
+
+            theList = args.split(" ")
+            if len(theList) < 2:
+                raise NameError("** class doesn't exist **")
+
+            myClass = theList[0]
+            if myClass not in self.classes:
+                raise NameError()
+
+            myObject = self.classes[myClass]()
+
+            for name in theList[1:]:
+                key, value = name.split("=")
+
+                value = value.replace('"', '').replace('_', ' ')
+                setattr(myObject, key, value)
+
+            myObject.save()
+            print("{}".format(myObject.id))
+
+
+        except SyntaxError:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
-            print("** class doesn't exist **")
+        except NameError as e:
+            print(e)
             return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
-        print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
@@ -272,7 +293,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +301,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
---
+++ tests/test_console.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+"""This are the tests for console"""
+import unittest
+from unittest.mock import patch
+from io import StringIO
+from console import HBNBCommand
+import os
+import json
+
+import console
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+import models
+
+class TestDoCreate(unittest.TestCase):
+    """class test Do create method"""
+
+    @classmethod
+    def setUp(cls):
+        """setting up for tests"""
+        cls.consolle = HBNBCommand()
+        cls.mock_stdout = StringIO()
+
+    @classmethod
+    def tearDown(cls):
+        """tear down method"""
+        del cls.console
+
+    def tearDown(self):
+        """remove file jsone"""
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
+
+    def test_create_with_bad_input(self):
+        message = "** class name missing **\n"
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.consolle.onecmd("create")
+            self.assertEqual(message, f.getvalue())
+
+    def test_create_no_class(self):
+        message = "** class doesn't exist **\n"
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.consolle.onecmd("create dog")
+            self.assertEqual(message, f.getvalue())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
