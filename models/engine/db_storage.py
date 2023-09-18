95343750137f034d3b0dfb994308887075902d16
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-09-18 19:22:33 +0200

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -55,25 +55,30 @@ class DBStorage:
         return (dic)
 
     def new(self, obj):
-        """add a new element in the table"""
+        """add a new element in the table
+        """
         self.__session.add(obj)
 
     def save(self):
-        """save changes"""
+        """save changes
+        """
         self.__session.commit()
 
     def delete(self, obj=None):
-        """delete an element in the table"""
+        """delete an element in the table
+        """
         if obj:
             self.session.delete(obj)
 
     def reload(self):
-        """configuration"""
+        """configuration
+        """
         Base.metadata.create_all(self.__engine)
         sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(sec)
         self.__session = Session()
 
     def close(self):
-        """ calls remove()"""
+        """ calls remove()
+        """
         self.__session.close()
