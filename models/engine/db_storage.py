9cc5acdb3283f29a2576d510a1ffedc16684acc4
shazaaly <shaza.aly@gmail.com>
2023-09-19 01:32:37 +0300

    DBStorage all()

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,25 +4,48 @@ import os
 from sqlalchemy import Base
 from sqlalchemy import create_engine
 
-# retrieve DB credentials using env variables
-user = os.getenv('HBNB_MYSQL_USER')
-password = os.getenv('HBNB_MYSQL_PWD')
-host = os.getenv('HBNB_MYSQL_HOST')
-database = os.getenv('HBNB_MYSQL_DB')
-env = os.getenv("HBNB_ENV")
-
 
 class DBStorage():
     """create the engine to be linked to the MySQL
     database
     """
+
     __engine = None
     __session = None
 
     def __init__(self):
         """Instantiation of engine"""
-        db_url = "mysql+mysqldb://user:password@host/database, , pool_pre_ping=True"
-        engine = create_engine(db_url)
-        self.__engine = engine(db_url)
+
+        user = os.getenv('HBNB_MYSQL_USER')
+        password = os.getenv('HBNB_MYSQL_PWD')
+        host = os.getenv('HBNB_MYSQL_HOST')
+        database = os.getenv('HBNB_MYSQL_DB')
+        env = os.getenv("HBNB_ENV")
+
+        db_url = "mysql+mysqldb://{}:{}@{}/{}".format(
+            user, password, host, database)
+
+        engine = create_engine(db_url, pool_pre_ping=True)
+        self.__engine = engine
         if env == 'test':
             Base.metadata.drop_all(engine)
+
+    def all(self, cls=None):
+        """query on the current database session
+        (self.__session) all objects depending of the class
+        name (cls)"""
+        obj_list = ['User', 'State', 'City', 'Amenity', 'Place', 'Review']
+        objects = []
+
+        if cls is not None:
+            objects.extend(self.__session.query(cls).all())  # objects
+        else:
+            for item in obj_list:
+                objects.extend(self.__session.query(item).all())
+
+        dict = {}
+        for obj in objects:
+            k = f"{obj.__class__.__name__}.{obj.id}"
+            dict[k] = obj
+
+        return dict
