e7e5b2ecfe5034c9558579a1fc3473fb486808e6
manucho254 <manuchoadero@gmail.com>
2023-09-18 15:29:46 +0000

    bug fix in tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -96,4 +96,5 @@ class test_basemodel(unittest.TestCase):
         self.assertEqual(type(new.updated_at), datetime.datetime)
         n = new.to_dict()
         new = BaseModel(**n)
+        new.save()  # need the save method to update the updated_at value
         self.assertFalse(new.created_at == new.updated_at)
