fe51f19e77188d21b52ba57d6b3cde364ce0d077
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-09-19 00:00:37 +0200

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
-#new code
+"""This is the city class"""
 from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import Base
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 from models.place import Place
 
 
-#new code
-class City(BaseModel):
+class City(BaseModel, Base):
     """This is the class for City
     Attributes:
         state_id: The state id
