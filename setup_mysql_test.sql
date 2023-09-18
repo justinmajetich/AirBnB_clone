ce0b8733bd6edfc4297876d28f2f544261ca0bea
Emad Anwer <emadanwer.official@gmail.com>
2023-09-18 14:56:58 +0300

    task 4 fix

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -5,7 +5,7 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 GRANT ALL PRIVILEGES 
 ON `hbnb_dev_db`.*
-TO 'hbnb_dev'@'localhost';
+TO 'hbnb_test'@'localhost';
 GRANT SELECT 
 ON `performance_schema`.*
-TO 'hbnb_dev'@'localhost';
+TO 'hbnb_test'@'localhost';
