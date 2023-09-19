abc058dfc7e647abf6804177007e300c9d47faf4
Ronnie-Leon76 <ronleon76@gmail.com>
2023-09-19 05:34:55 +0300

    feat: Console improvements

--- models/amenity.py
+++ models/amenity.py
@@ -5,3 +5,8 @@ from models.base_model import BaseModel
 
 class Amenity(BaseModel):
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__()
+        self.name = kwargs.get('name', "")
