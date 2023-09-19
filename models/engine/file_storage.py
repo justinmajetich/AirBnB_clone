b3fcbe72155e35772e2cee9855d70442af3eef5c
Godwin Okpe <okpegodwin18@yahoo.com>
2023-09-19 09:11:26 -0700

    Changed the style of docstrings

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,12 +9,16 @@ import json
 
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
+    """
+    This class manages storage of hbnb models in JSON format
+    """
     __file_path = 'file.json'
     __objects = {}
 
     def all(self, cls=None):
-        """Returns a dictionary of models currently in storage"""
+        """
+        Returns a dictionary of models currently in storage
+        """
         if cls is None:
             return self.__objects
         else:
@@ -27,11 +31,15 @@ class FileStorage:
         return FileStorage.__objects
 
     def new(self, obj):
-        """Adds new object to storage dictionary"""
+        """
+        Adds new object to storage dictionary
+        """
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
-        """Saves storage dictionary to file"""
+        """
+        Saves storage dictionary to file
+        """
         with open(FileStorage.__file_path, 'w') as f:
             temp = {}
             temp.update(FileStorage.__objects)
@@ -40,7 +48,9 @@ class FileStorage:
             json.dump(temp, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
+        """
+        Loads storage dictionary from file
+        """
         from models.base_model import BaseModel
         from models.user import User
         from models.place import Place
