9e981f1154607b4f9f3c0781282a77bf19112ea2
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-09-19 13:22:09 -0700

    updated Review class to map objects to table

--- models/review.py
+++ models/review.py
@@ -2,11 +2,18 @@
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+import os
 
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = 'reviews'
-    text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey(places.id), nullable=False)
-    user_id = Column(String(60), ForeignKey(users.id), nullable=False)
+    if (os.getenv(HBNB_TYPE_STORAGE) == 'db'):
+         text = Column(String(1024), nullable=False)
+         place_id = Column(String(60), ForeignKey(places.id), nullable=False)
+         user_id = Column(String(60), ForeignKey(users.id), nullable=False)
+     else:
+         text = ""
+         place_id = ""
+         user_id ""
+
