a42c981a97304f6bf3cb2f93f4b3de577f214844
thinktank01 <felixayot@gmail.com>
2023-09-19 13:58:33 +0300

    Remove flush privileges

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,4 +4,3 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-FLUSH PRIVILEGES;
