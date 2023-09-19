ce0f8ba2a983eb50631a700027ffd0aeb8f7ba23
shazaaly <shaza.aly@gmail.com>
2023-09-19 18:39:06 +0300

    state.py fix with getenv

--- models/state.py
+++ models/state.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String, Integer
 from models.base_model import BaseModel
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
 from models.base_model import Base
 
@@ -11,18 +12,22 @@ from models.base_model import Base
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', back_populates='state',
-                          cascade='all, delete-orphan')
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
 
-    @property
-    def cities(self):
-        """Returns the list of City instances
-        with state_id equals to the current State.id
-        """
-        import models
-        stored = models.storage.all()
-        for k, v in stored.items():
-            if v[__class__] == 'State':
-                return [city for city in self.cities if
-                        city.state_id == self.id]
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', back_populates='state',
+                              cascade='all, delete-orphan')
+    else:
+        print('no access to cities')
+
+        @property
+        def cities(self):
+            """Returns the list of City instances
+            with state_id equals to the current State.id
+            """
+            import models
+            stored = models.storage.all()
+            for k, v in stored.items():
+                if v[__class__] == 'State':
+                    return [city for city in self.cities if
+                            city.state_id == self.id]
