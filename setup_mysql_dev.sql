c4cd204c891dbc3a71a32fdd1d9fb200ae75f3f6
Nick <nodhiambo01@gmail.com>
2023-09-19 04:59:10 -0700

    MySQL setup development

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,7 @@
+-- Prepares a MySQL server for the project
+-- Creates a new user and grants various privileges
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
