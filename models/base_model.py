4f8c690b12e847aaa50ceb3fd283f8cbd73ab21d
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-09-19 15:15:13 +0000

    Improved method and fixed bugs

--- models/base_model.py
+++ models/base_model.py
@@ -16,35 +16,18 @@ class BaseModel:
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        from models import storage
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            # storage.new(self)
-        else:
-            # if kwargs.get('updated_at') or kwargs.get('created_at'):
-            #     kwargs['updated_at'] = (
-            #         datetime.strptime(
-            #             kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            #     )
-            #     kwargs['created_at'] = (
-            #         datetime.strptime(
-            #             kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            #     )
-            for key in kwargs:
-                if key == '__class__':
-                    del kwargs[key]
-                if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.fromisoformat(kwargs[key]))
-                    # self.key = datetime.fromisoformat(kwargs[key])
-                else:
-                    setattr(self, key, kwargs[key])
-                    # self.key = kwargs[key]
-            if 'id' not in kwargs:
-                self.id = str(uuid.uuid4())
-            self.__dict__.update(kwargs)
+        """ Instantiates a new model """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        if kwargs:
+            if kwargs.get("__class__"):
+                kwargs.pop("__class__")
+            for k, v in kwargs.items():
+                if k == 'created_at' or k == 'updated_at':
+                    setattr(self, k, datetime.fromisoformat(v))
+                    continue
+                setattr(self, k, v)
 
     def __str__(self):
         """Returns a string representation of the instance"""
