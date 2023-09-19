d5142dd5a951bb25b59b11c22f48a13c3ee1b8e2
Nazzcodek <bellnas09@gmail.com>
2023-09-19 08:43:55 -0700

    [feature] delete storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,8 +7,16 @@ class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
-
-    def all(self):
+    '''
+    def all(self, cls=None):
+        """Returns a dictionary of models currently in storage"""
+        if cls is None:
+            return FileStorage.__objects
+        else:
+            return {key: obj for key, obj in \
+                    FileStorage.__objects.items() if isinstance(obj, cls)}
+            '''
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         return FileStorage.__objects
 
@@ -46,5 +54,12 @@ class FileStorage:
                 temp = json.load(f)
                 for key, val in temp.items():
                         self.all()[key] = classes[val['__class__']](**val)
-        except FileNotFoundError:
+        except (FileNotFoundError, json.decoder.JSONDecodeError):
             pass
+
+    def delete(self, obj=None):
+        """Deletes obj from __objects if it's inside"""
+        if obj is not None:
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in FileStorage.__objects:
+                del FileStorage.__objects[key]
