60a2a1a3e0a1e305d4000c26fc992122aa47abda
Nick <nodhiambo01@gmail.com>
2023-09-19 05:14:26 -0700

    MySQL setup test

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,6 +2,6 @@
 -- Creates a new user and grants various privileges
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
