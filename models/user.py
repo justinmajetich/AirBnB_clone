91bda6fba2500e1cda5550c4386973a7268fd32a
bouhvli <bouhali.hamza.2@gmail.com>
2023-09-19 16:36:15 +0100

    update null indecator

--- models/user.py
+++ models/user.py
@@ -10,7 +10,7 @@ class User(BaseModel, Base):
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
     places = relationship('Place', cascade='delete', backref='user')
     reviews = relationship("Review", cascade='delete', backref='user')
