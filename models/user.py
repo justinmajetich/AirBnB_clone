fbad2642ec4b9e9ca96a16e73d9a019148fde4a2
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-09-19 13:20:08 -0700

    updated user class to map objects to tables

--- models/user.py
+++ models/user.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-import sqlalchemy
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import os
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
 
     __tablename__ = 'users'
+    if (os.getenv(HBNB_TYPE_STORAGE) == 'db'):
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
 
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-
-    # email = ''
-    # password = ''
-    # first_name = ''
-    # last_name = ''
+        reviews = relationship(
+                "Reviews", cascade='all, delete' backref='user')
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
