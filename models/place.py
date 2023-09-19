b89597ff52e6e59864767ae28bbdf54709f429e2
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-09-19 19:39:22 +0000

    Added a getter for the file storage

--- models/place.py
+++ models/place.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
+import os
+
 from models.base_model import Base, BaseModel
+from models.review import Review
 from sqlalchemy import Column, String, ForeignKey, Integer, Float
 
 
 class Place(BaseModel, Base):
     """ Class for Place Module """
-    __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024))
-    number_rooms = Column(Integer, default=0, nullable=False)
-    number_bathrooms = Column(Integer, default=0, nullable=False)
-    max_guest = Column(Integer, default=0, nullable=False)
-    price_by_night = Column(Integer, default=0, nullable=False)
-    latitude = Column(Float)
-    longitude = Column(Float)
-    amenity_ids = []
+    if os.getenv("HBNB_TYPE_STORAGE") == 'db':
+        __tablename__ = "places"
+        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024))
+        number_rooms = Column(Integer, default=0, nullable=False)
+        number_bathrooms = Column(Integer, default=0, nullable=False)
+        max_guest = Column(Integer, default=0, nullable=False)
+        price_by_night = Column(Integer, default=0, nullable=False)
+        latitude = Column(Float)
+        longitude = Column(Float)
+    else:
+        @property
+        def reviews(self):
+            """ Returns list of Reviews """
+            from models import storage
+            reviews = storage.all(Review).values()
+            return list(filter(lambda review: review.place_id == self.id,
+                               reviews))
+        amenity_ids = []
