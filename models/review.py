49bac265da789cce4f99783b3c91507b9d07788d
Robel Bekele <robba836@gmail.com>
2023-09-19 13:00:27 -0700

    Task 8 and 9 updated

--- models/review.py
+++ models/review.py
@@ -1,10 +1,25 @@
-#!/usr/bin/python3
-""" Review module for the HBNB project """
+i#!/usr/bin/python3
+"""This script defines the Review class."""
+from models.base_model import Base
 from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
+class Review(BaseModel, Base):
+    """Represents a review in a MySQL database.
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    Inherits from SQLAlchemy Base and is linked to the MySQL table 'reviews'.
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table for storing reviews.
+        text (sqlalchemy String): The review description.
+        place_id (sqlalchemy String): The ID of the place associated with the review.
+        user_id (sqlalchemy String): The ID of the user who made the review.
+    """
+    __tablename__ = "reviews"
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+
