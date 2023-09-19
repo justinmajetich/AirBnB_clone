f93d766e9562876aa77ce8e9659e8c278082bbc8
OGUNSANYA TAOFEEQ ABIDEMI <taofeeq.muhammad22@gmail.com>
2023-09-19 11:26:54 +0100

    file updated

--- models/base_model.py
+++ models/base_model.py
@@ -1,15 +1,26 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
+from sqlalchemy.ext.declarative import declarative_base
 import uuid
 from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime
+from models import storage
 
+Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
+    
+    """ Included this line"""
+    id = Column(String(60), unique=True, nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
+    updated_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
+
+    
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            from models import storage
+            # from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
@@ -29,16 +40,26 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
+        # from models import storage
         self.updated_at = datetime.now()
+        """ Included this line"""
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = {}
+        dictionary = dict(self.__dict__)
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if '_sa_instance_state' in dictionary.keys():
+            del dictionary['_sa_instance_state']
         return dictionary
+    
+    def delete(self):
+        """ delete object
+        """
+        # from models import storage
+        storage.delete(self)
