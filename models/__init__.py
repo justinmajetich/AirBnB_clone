5cb704814a6f0dc0d75b644cbc1b7116d746823b
OUALIID <walidelhadim8@gmail.com>
2023-09-19 23:15:05 +0100

    Updating init file and configuring attributes

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+import os
 
 
 storage = FileStorage()
+storage.reload()
+# Retrieve the value of the environment variable.
+if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    storage = DBStorage()
+    # creates an instance of the `DBStorage` class
+    # and assigns it to `storage`
+else:
+    storage = FileStorage()
+    # creates an instance of the `FileStorage` class
+    # and assigns it to `storage`
 storage.reload()
