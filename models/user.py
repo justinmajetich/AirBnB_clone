a79b6cf9f704084141c3bac0d72b471c8de73ec5
Robel Bekele <robba836@gmail.com>
2023-09-19 13:30:51 -0700

    Task 7 2nd update

--- models/user.py
+++ models/user.py
@@ -1,30 +1,22 @@
 #!/usr/bin/python3
-"""This script defines the User class."""
-from models.base_model import Base
-from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import String
-from sqlalchemy.orm import relationship
+"""Defines the User class."""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 class User(BaseModel, Base):
-    """Represents a user in a MySQL database.
-
-    Inherits from SQLAlchemy Base and is linked to the MySQL table 'users'.
+    """Represents a user for a MySQL database.
+    Inherits from SQLAlchemy Base and links to the MySQL table users.
 
     Attributes:
-        __tablename__ (str): The name of the MySQL table for storing users.
+        __tablename__ (str): The name of the MySQL table to store users.
         email (sqlalchemy String): The user's email address.
         password (sqlalchemy String): The user's password.
         first_name (sqlalchemy String): The user's first name.
         last_name (sqlalchemy String): The user's last name.
-        places (sqlalchemy relationship): The User-Place relationship.
-        reviews (sqlalchemy relationship): The User-Review relationship.
     """
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
 
