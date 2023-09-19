a4369ea8e740bc580f97f80c59aea8370058d1ab
DuncanMoyo <duncanfmoyo@gmail.com>
2023-09-19 09:39:38 +0000

    Pushed changes whilst in the wrong folder

--- models/city.py
+++ models/city.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 
+
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
--- models/state.py
+++ models/state.py
@@ -4,13 +4,15 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
+
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
 
+    cascade = "all, delete-orphan"
     """When a state oblect is deleted, all lonked City objects are deleted"""
-    cities = relationship("City", backref="state", cascade="all, delete-orphan")
+    cities = relationship("City", backref="state", cascade=cascade)
 
     @property
     def cities(self):
