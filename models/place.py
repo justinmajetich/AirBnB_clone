60402109df60ed7fb4b2e8ab928dc33334e025d9
Kingsley Akpan <54294050+Khingz@users.noreply.github.com>
2023-09-19 21:16:25 +0100

    Update place.py
    Modified nullabke property of long and lay attribute

--- models/place.py
+++ models/place.py
@@ -20,8 +20,8 @@ class Place(BaseModel, Base):
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=False)
-    longitude = Column(Float, nullable=False)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     reviews = relationship(
             "Review",
             backref="review",
