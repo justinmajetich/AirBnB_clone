e204626a74a06bc7666026b71fff7ab5d697175a
Robel Bekele <robba836@gmail.com>
2023-09-19 13:36:51 -0700

    update #7

--- models/user.py
+++ models/user.py
@@ -4,12 +4,13 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 
 class User(BaseModel, Base):
-    """Represents a user for a MySQL database.
+    """Represents a user for a database.
+
     Inherits from SQLAlchemy Base and links to the MySQL table users.
 
     Attributes:
         __tablename__ (str): The name of the MySQL table to store users.
-        email (sqlalchemy String): The user's email address.
+        email (sqlalchemy String): The user's email.
         password (sqlalchemy String): The user's password.
         first_name (sqlalchemy String): The user's first name.
         last_name (sqlalchemy String): The user's last name.
@@ -17,6 +18,6 @@ class User(BaseModel, Base):
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
 
