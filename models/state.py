47c5a687a79f2712a942dd2a7473eccd009eaa8b
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-09-19 15:16:42 +0000

    Modified State class and mapped db

--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import os
+
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+
+    if os.getenv("HBNB_TYPE_STORAGE") == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """ Returns a list of city objects """
+            from models import storage
+            cities = storage.all(City).values()
+            return list(filter(lambda city: city.state_id == self.id, cities))
