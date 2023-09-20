ffdccdfab8e856338ae1b43aad32ea1604bccd63
Mojalefa Nkwana <mojalefa.emjay01@gmail.com>
2023-09-20 10:34:29 +0200

    Do task 4, AirBnB_clone_v2

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -4,6 +4,6 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- Creates a new user hbnb_test (in localhost)
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- Set all privileges on the database hbnb_test_db
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 -- Set SELECT privilege on the database performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
