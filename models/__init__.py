d116f72aeb75151c2ed30c84ce92079944bba9a5
bouhvli <bouhali.hamza.2@gmail.com>
2023-09-20 13:04:07 +0100

    update the init folder for *

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from os import getenv
+from models.base_model import BaseModel
+from models.__init__ import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+classes = {
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
 
 dbStorage = getenv('HBNB_TYPE_STORAGE')
 if (dbStorage == 'db'):
