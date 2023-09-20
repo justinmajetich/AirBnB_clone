156e513f1cc1652c37a2e6a0fd026f7e83d3d419
Lionel <ndabagajeanlionel@gmail.com>
2023-09-20 15:24:56 +0000

    def all adjusted to list objects, and def all created

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+    def all(self, cls=None):
+        """Returns a list of objects of one type of class"""
+        temp = {}
+        if cls is None:
+            return FileStorage.__objects
+        for key, val in FileStorage.__objects.items():
+            if cls == val.__class__:
+                temp[key] = val
+        return temp
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +54,12 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Deletes an object from __objects if it's inside"""
+        if obj is None:
+            return
+        key = obj.__class__.__name__ + '.' + obj.id
+        if key in self.all():
+            del self.all()[key]
+            self.save()
