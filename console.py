b0dc98b2593210ab3971d70202cb544b73bae88c
OUALIID <walidelhadim8@gmail.com>
2023-09-20 18:44:11 +0100

    Fixing new line after prompte in isatty

--- console.py
+++ console.py
@@ -33,7 +33,7 @@ class HBNBCommand(cmd.Cmd):
     def preloop(self):
         """Prints if isatty is false"""
         if not sys.__stdin__.isatty():
-            print('(hbnb)')
+            print('(hbnb) ', end="")
 
     def precmd(self, line):
         """Reformat command line for advanced command syntax.
