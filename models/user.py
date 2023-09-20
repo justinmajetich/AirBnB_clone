2c60f7537d5bd0022c2bd857610425424d92c313
Bunje Kelly <kellybunje@gmail.com>
2023-09-20 12:54:41 -0700

    user

--- models/user.py
+++ models/user.py
@@ -8,8 +8,18 @@ import os
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    
+    if os.environ.get("HBNB_TYPE_STORAGE") == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship('Place', backref='user',
+                              cascade='all, delete, delete-orphan')
+        reviews = relationship('Review', backref='user',
+                               cascade='all, delete, delete-orphan')
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
