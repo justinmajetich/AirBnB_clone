04c13ded8c9d65673bb343165b143eea12f89dd1
H-Asmaa <asmaehadar32@gmail.com>
2023-09-19 20:22:09 -0700

    Committing new changes

--- models/place.py
+++ models/place.py
@@ -11,7 +11,7 @@ class Place(BaseModel, Base):
     __tablename__ = 'places'
     id = Column(String(60), primary_key=True, nullable=False, unique=True)
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, nullable=False, default=0)
