2f39536fed66d83bbfd539a87801ade5772b0ef6
vagrant <vagrant@ubuntu-20.04>
2023-09-20 16:41:04 +0000

    Adjust classes

--- models/city.py
+++ models/city.py
@@ -2,12 +2,16 @@
 """ City Module for HBNB project """
 from sqlalchemy import Column, Integer, String, ForeignKey
 from models.base_model import BaseModel, Base
-
+from models import storage_type
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
 
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'),
+    if storage_type == "db":
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'),
                       nullable=False)
+    else:
+        name = ""
+        state_id = ""
--- models/user.py
+++ models/user.py
@@ -2,13 +2,19 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
-
+from models import storage_type
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'User'
 
-    email = Column(String(128), nullable=False)
-    password = = Column(String(128), nullable=False)
-    first_name = = Column(String(128), nullable=False)
-    last_name = = Column(String(128), nullable=False)
+    if storage_type == "db":
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
