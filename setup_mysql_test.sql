91d9420cb7c1db1f3d2b9028f9ae5059a4a78998
Lionel <ndabagajeanlionel@gmail.com>
2023-09-20 10:17:14 +0000

    MySQL setup test successful now

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,15 +1,13 @@
 -- This script is used to create the database and tables for the test
 
--- create project testing hbnb_test_db
+-- Create the database and user if they didn't already exist
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
--- creating new user named : hbnb_test with all privileges on  hbnb_test_db
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
-
--- grant all privileges on hbnb_test_db to hbnb_test
+-- Grant privileges to the user on hbnt_test_db
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
 
--- granting the SELECT privilege to the user
+-- Grant privileges to the user on performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
