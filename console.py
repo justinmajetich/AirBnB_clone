a116cc4a5f3ea6a9e3623c14672a3467e2695e73
Mojalefa Nkwana <mojalefa.emjay01@gmail.com>
2023-09-20 06:06:18 +0200

    Do task 2, AirBnB_clone_v2

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -115,16 +115,36 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        if not args:
+
+        arg_list = args.split()
+        arg_dic = {}
+
+        if len(arg_list) == 0:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        elif arg_list[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
+        
+        if len(arg_list) > 1:
+            for param in arg_list[1:]:
+                print(param)
+                key, val = param.split('=')
+                if val[0] == '"' and val[-1] == '"':
+                    val.replace("_", " ")
+                    arg_dic.update({key: val.strip('"')})
+                elif val.find(".") != -1 and val.replace(".", "", 1).isdigit():
+                    arg_dic.update({key: float(val)})
+                elif val.isdigit():
+                    arg_dic.update({key: int(val)})
+
+            new_instance = HBNBCommand.classes[arg_list[0]]()
+            new_instance.__dict__.update(arg_dic)
+        else:
+            new_instance = HBNBCommand.classes[arg_list[0]]()
+            
         storage.save()
         print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
@@ -176,10 +196,15 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
             return
 
+        """%%% Add a feature to delete all objects. %%%"""
+        if c_name == "all":
+            storage.all().clear()
+            return
+
         if c_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-
+        
         if not c_id:
             print("** instance id missing **")
             return
@@ -272,7 +297,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +305,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
