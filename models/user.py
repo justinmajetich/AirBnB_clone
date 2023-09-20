07106f0723e008554b5ea8f0e48d6ee271997d14
Bunje Kelly <kellybunje@gmail.com>
2023-09-20 13:04:34 -0700

    change

--- models/user.py
+++ models/user.py
@@ -5,21 +5,18 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 import os
 
+
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-    
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+
     if os.environ.get("HBNB_TYPE_STORAGE") == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
         places = relationship('Place', backref='user',
-                              cascade='all, delete, delete-orphan')
+                              cascade='all, delete')
         reviews = relationship('Review', backref='user',
-                               cascade='all, delete, delete-orphan')
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+                               cascade='all, delete')
