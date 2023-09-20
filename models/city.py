3927af98b94ed67182490afbadbd2940c2bbdcca
achrafezzaam <achraf.ezzaam@gmail.com>
2023-09-20 21:05:55 +0100

    Storage in file is broken. Looking for a fix

--- models/city.py
+++ models/city.py
@@ -7,6 +7,6 @@ from sqlalchemy import Column, String, ForeignKey
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
 
-    "__tablename__" = "cities"
-    state_id = Column(String(60), nullable=False, ForeignKey(states.id))
+    __tablename__ = "cities"
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
--- models/state.py
+++ models/state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
@@ -8,6 +8,6 @@ from sqlalchemy.orm import relationship
 class State(BaseModel, Base):
     """ State class """
 
-    "__tablename__" = "states"
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship('City', backref='state', cascade='all, delete')
