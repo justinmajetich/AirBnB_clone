85562dfcb0c835784e15b019685cb44aa278be34
GhostProTech <akanniwilliams@gmail.com>
2023-09-20 06:37:33 +0100

    Review/debug place table creation

--- models/place.py
+++ models/place.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String, Integer, ForeignKey, Float
 from os import getenv
@@ -10,16 +10,16 @@ class Place(BaseModel, Base):
 
     __tablename__ = 'places'
     if 'db' == getenv('HBNB_TYPE_STORAGE=db'):
-        city_id = Column(String(60), nullable=False, ForeignKey='cities.id')
-        user_id = Column(String(60), nullable=False, ForeignKey='users.id')
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
-        description = Column(String(1024), nullable=False)
+        description = Column(String(1024))
         number_rooms = Column(Integer, nullable=False, default=0)
         number_bathrooms = Column(Integer, nullable=False, default=0)
         max_guest = Column(Integer, nullable=False, default=0)
         price_by_night = Column(Integer, nullable=False, default=0)
-        latittude = Column(Float, nullable=True)
-        longitude = Column(Float, nullable=True)
+        latittude = Column(Float)
+        longitude = Column(Float)
 
     else:
         city_id = ""
