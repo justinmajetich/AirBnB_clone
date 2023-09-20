244fa80abfd0cf1a6d6be6f1a436af4eaeba064c
Umuhasna <umuhasna123@gmail.com>
2023-09-20 16:45:14 +0300

    Fix Use the right database, user, password name

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,14 +1,14 @@
 -- script that prepares a MySQL server for the project
--- If the database hbnb_dev_db or the user hbnb_dev already exists, your script should not fail
--- A database hbnb_dev_db
--- A new user hbnb_dev (in localhost)
--- The password of hbnb_dev should be set to hbnb_dev_pwd
--- hbnb_dev should have all privileges on the database hbnb_dev_db (and only this database)
--- hbnb_dev should have SELECT privilege on the database performance_schema (and only this database)
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- If the database hbnb_test_db or the user hbnb_test already exists, your script should not fail
+-- A database hbnb_test_db
+-- A new user hbnb_test (in localhost)
+-- The password of hbnb_test should be set to hbnb_test_pwd
+-- hbnb_test should have all privileges on the database hbnb_test_db (and only this database)
+-- hbnb_test should have SELECT privilege on the database performance_schema (and only this database)
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
 FLUSH PRIVILEGES;
