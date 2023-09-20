862f4b3dc7e845ef3d65a1ff2f1bba4e158be4a3
Mojalefa Nkwana <mojalefa.emjay01@gmail.com>
2023-09-20 09:29:00 +0200

    Revert "Do task 2, AirBnB_clone_v2"
    This reverts commit a116cc4a5f3ea6a9e3623c14672a3467e2695e73.

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] == '{' and pline[-1] == '}'\
+                    if pline[0] is '{' and pline[-1] is'}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -115,34 +115,15 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-
-        arg_list = args.split()
-        arg_dic = {}
-
-        if len(arg_list) == 0:
+        if not args:
             print("** class name missing **")
             return
-        elif arg_list[0] not in HBNBCommand.classes:
+        elif args not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        
-        if len(arg_list) > 1:
-            for param in arg_list[1:]:
-                print(param)
-                key, val = param.split('=')
-                if val[0] == '"' and val[-1] == '"':
-                    val.replace("_", " ")
-                    arg_dic.update({key: val.strip('"')})
-                elif val.find(".") != -1 and val.replace(".", "", 1).isdigit():
-                    arg_dic.update({key: float(val)})
-                elif val.isdigit():
-                    arg_dic.update({key: int(val)})
-
-            new_instance = HBNBCommand.classes[arg_list[0]]()
-            new_instance.__dict__.update(arg_dic)
-        else:
-            new_instance = HBNBCommand.classes[arg_list[0]]()
-            
+        new_instance = HBNBCommand.classes[args]()
+        storage.save()
+        print(new_instance.id)
         storage.save()
 
     def help_create(self):
@@ -195,15 +176,10 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
             return
 
-        """%%% Add a feature to delete all objects. %%%"""
-        if c_name == "all":
-            storage.all().clear()
-            return
-
         if c_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        
+
         if not c_id:
             print("** instance id missing **")
             return
@@ -296,7 +272,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] == '\"':  # check for quoted arg
+            if args and args[0] is '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -304,10 +280,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] != ' ':
+            if not att_name and args[0] is not ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] == '\"':
+            if args[2] and args[2][0] is '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
