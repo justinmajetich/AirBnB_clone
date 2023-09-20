f20703b1f18f004563f65454ac0d084c4839d18c
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-09-20 20:42:48 +0000

    Refactored and handled errors

--- models/base_model.py
+++ models/base_model.py
@@ -42,10 +42,8 @@ class BaseModel:
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__': (str(type(self)).split('.')[-1])
-                          .split('\'')[0]})
+        dictionary = self.__dict__.copy()
+        dictionary['__class__'] = self.__class__.__name__
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         dictionary.pop("_sa_instance_state", None)
--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,6 +3,7 @@
 from os import getenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.exc import SQLAlchemyError
 from models.base_model import Base
 import models.misc as misc
 
@@ -14,59 +15,85 @@ class DBStorage:
 
     def __init__(self):
         """ Initializing instance """
-        self.__engine = create_engine(
-            "mysql+mysqldb://{}:{}@{}/{}".format(
+        try:
+            self.__engine = create_engine(
+                "mysql+mysqldb://{}:{}@{}/{}".format(
                     misc.USERNAME,
                     misc.PASSWORD,
                     misc.HOST,
                     misc.DB_NAME
                 ),
-            pool_pre_ping=True
-        )
+                pool_pre_ping=True
+            )
 
-        if misc.ENV == 'test':
-            Base.metadata.drop_all(self.__engine)
+            if misc.ENV == 'test':
+                Base.metadata.drop_all(self.__engine)
+        except SQLAlchemyError as e:
+            print(e)
+            raise SQLAlchemyError
 
     def all(self, cls=None):
         """ Returns query based on class passed to it """
-        all_objs: dict = {}
+        try:
+            all_objs: dict = {}
 
-        if cls:
-            # querying the class passed as arg.
-            objects = self.__session.query(cls)
-            for obj in objects:
-                key = f"{obj.__class__.__name__}.{obj.id}"
-                all_objs[key] = obj
-        else:
-            # If not i query all
-            for item in misc.classes.values():
-                objects = self.__session.query(item)
+            if cls:
+                objects = self.__session.query(cls)
                 for obj in objects:
                     key = f"{obj.__class__.__name__}.{obj.id}"
                     all_objs[key] = obj
-        return all_objs
+            else:
+                for item in misc.classes.values():
+                    objects = self.__session.query(item)
+                    for obj in objects:
+                        key = f"{obj.__class__.__name__}.{obj.id}"
+                        all_objs[key] = obj
+            return all_objs
+        except SQLAlchemyError as e:
+            print(e)
+        finally:
+            self.close()
 
     def new(self, obj):
         """ Add a new instance entry to the session """
-        self.__session.add(obj)
+        try:
+            self.__session.add(obj)
+        except SQLAlchemyError as e:
+            print(e)
+        finally:
+            self.close()
 
     def save(self):
         """ Saves the instance to the db"""
-        self.__session.commit()
+        try:
+            self.__session.commit()
+        except SQLAlchemyError as e:
+            print(e)
+        finally:
+            self.close()
 
     def delete(self, obj=None):
         """ Deletes the instance from the db """
-        if obj:
-            self.__session.delete(obj)
+        try:
+            if obj:
+                self.__session.delete(obj)
+        except SQLAlchemyError as e:
+            print(e)
+        finally:
+            self.close()
 
     def reload(self):
         """ Creates and reloads the database """
-        Base.metadata.create_all(self.__engine)
-        session = scoped_session(sessionmaker(
-            bind=self.__engine,
-            expire_on_commit=False
-        ))
-        self.__session = session()
+        try:
+            Base.metadata.create_all(self.__engine)
+            session = scoped_session(sessionmaker(
+                bind=self.__engine,
+                expire_on_commit=False
+            ))
+        except SQLAlchemyError as e:
+            print(e)
+        else:
+            self.__session = session()
 
     def close(self):
         self.__session.close()
