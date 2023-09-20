ca88b417b5c235ea8b734789e0336c1495c5f6b2
Reddeath <frankslayer1@gmail.com>
2023-09-20 19:42:52 +0300

    modified console

--- console.py
+++ console.py
@@ -2,7 +2,7 @@
 """ Console Module """
 import cmd
 import sys
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from models.__init__ import storage
 from models.user import User
 from models.place import Place
@@ -19,16 +19,16 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) ' if sys.__stdin__.isatty() else ''
 
     classes = {
-               'BaseModel': BaseModel, 'User': User, 'Place': Place,
-               'State': State, 'City': City, 'Amenity': Amenity,
-               'Review': Review
-              }
+        'BaseModel': BaseModel, 'User': User, 'Place': Place,
+        'State': State, 'City': City, 'Amenity': Amenity,
+        'Review': Review
+    }
     dot_cmds = ['all', 'count', 'show', 'destroy', 'update']
     types = {
-             'number_rooms': int, 'number_bathrooms': int,
-             'max_guest': int, 'price_by_night': int,
-             'latitude': float, 'longitude': float
-            }
+        'number_rooms': int, 'number_bathrooms': int,
+        'max_guest': int, 'price_by_night': int,
+        'latitude': float, 'longitude': float
+    }
 
     def preloop(self):
         """Prints if isatty is false"""
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] is '{' and pline[-1] is'}' \
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -114,120 +114,28 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
-        """Create an object of any class with given parameters"""
-        if not args:
+        """ Create an object of any class"""
+        try:
+            if not args:
+                raise SyntaxError()
+            split1 = args.split(' ')
+            new_instance = eval('{}()'.format(split1[0]))
+            params = split1[1:]
+            for param in params:
+                k, v = param.split('=')
+                try:
+                    attribute = HBNBCommand.verify_attribute(v)
+                except:
+                    continue
+                if not attribute:
+                    continue
+                setattr(new_instance, k, attribute)
+            new_instance.save()
+            print(new_instance.id)
+        except SyntaxError:
             print("** class name missing **")
-            return
-
-        # Split the arguments into class name and parameters
-        args_list = args.split()
-        class_name = args_list[0]
-        params = args_list[1:]
-
-        if class_name not in HBNBCommand.classes:
+        except NameError as e:
             print("** class doesn't exist **")
-            return
-
-        # Create an instance of the specified class
-        new_instance = HBNBCommand.classes[class_name]()
-
-        # Process the parameters
-        for param in params:
-            # Split the parameter into key and value
-            key_value = param.split('=')
-            if len(key_value) != 2:
-                print("** invalid parameter format:", param)
-                continue
-
-            key, value = key_value
-
-            # Process the value based on its type
-            try:
-                # Check if it's a string value
-                if value[0] == '"' and value[-1] == '"':
-                    value = value[1:-1].replace('_', ' ')
-                # Check if it's a float value
-                elif '.' in value:
-                    value = float(value)
-                # Check if it's an integer value
-                else:
-                    value = int(value)
-
-                # Set the attribute of the instance with the processed value
-                setattr(new_instance, key, value)
-            except (ValueError, IndexError):
-                print("** invalid parameter value:", param)
-                continue
-
-        # Save the instance and print its ID
-        storage.save()
-        print(new_instance.id)
-
-        def help_create(self):
-            """ Help information for the create method """
-            print("Creates a class of any type")
-            print("[Usage]: create <className>\n")
-
-        def do_show(self, args):
-            """ Method to show an individual object """
-            new = args.partition(" ")
-            c_name = new[0]
-            c_id = new[2]
-
-            if c_id and ' ' in c_id:
-                c_id = c_id.partition(' ')[0]
-
-            if not c_name:
-                print("** class name missing **")
-                return
-
-            if c_name not in HBNBCommand.classes:
-                print("** class doesn't exist **")
-                return
-
-            if not c_id:
-                print("** instance id missing **")
-                return
-
-            key = c_name + "." + c_id
-            try:
-                print(storage._FileStorage__objects[key])
-            except KeyError:
-                print("** no instance found **")
-
-        def help_show(self):
-            """ Help information for the show command """
-            print("Shows an individual instance of a class")
-            print("[Usage]: show <className> <objectId>\n")
-
-        def do_destroy(self, args):
-            """ Destroys a specified object """
-            new = args.partition(" ")
-            c_name = new[0]
-            c_id = new[2]
-            if c_id and ' ' in c_id:
-                c_id = c_id.partition(' ')[0]
-
-            if not c_name:
-                print("** class name missing **")
-                return
-
-            if c_name not in HBNBCommand.classes:
-                print("** class doesn't exist **")
-                return
-
-            if not c_id:
-                print("** instance id missing **")
-                return
-
-            key = c_name + "." + c_id
-
-            try:
-                del (storage.all()[key])
-                storage.save()
-            except KeyError:
-                print("** no instance found **")
-
 
     def help_create(self):
         """ Help information for the create method """
@@ -302,20 +210,19 @@ class HBNBCommand(cmd.Cmd):
 
     def do_all(self, args):
         """ Shows all objects, or all objects of a class"""
+        obj = storage.all()
         print_list = []
-
         if args:
             args = args.split(' ')[0]  # remove possible trailing args
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in obj.items():
                 if k.split('.')[0] == args:
                     print_list.append(str(v))
         else:
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in obj.items():
                 print_list.append(str(v))
-
         print(print_list)
 
     def help_all(self):
@@ -422,6 +329,22 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
+    @classmethod
+    def verify_attribute(cls, attribute):
+        """
+        Verify if the attribute is correctly formatted
+        """
+        if attribute[0] is attribute[-1] in ['"', "'"]:
+            return attribute.strip('"\'').replace('_', ' ').replace('\\', '"')
+        else:
+            try:
+                try:
+                    return int(attribute)
+                except ValueError:
+                    return float(attribute)
+            except ValueError:
+                return None
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
