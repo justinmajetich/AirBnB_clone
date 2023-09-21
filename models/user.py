97489b9d916553b61f382df2b9afc6453bf3955a
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-09-21 01:51:47 +0000

    Fixed bug

--- models/user.py
+++ models/user.py
@@ -13,13 +13,8 @@ class User(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == "db":
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True, default="")
-        last_name = Column(String(128), nullable=True, default="")
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
 
         places = relationship("Place", backref="user", cascade="all, delete-orphan")
         reviews = relationship("Review", backref="user", cascade="all, delete-orphan")
-    else:
-        first_name = ""
-        email = ""
-        password = ""
-        last_name = ""
