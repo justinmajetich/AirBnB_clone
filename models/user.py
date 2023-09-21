fa3184344a36f94a47bf75aebdd4e3cf4307d409
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-09-21 01:46:35 +0000

    Fixed bug

--- models/user.py
+++ models/user.py
@@ -8,14 +8,18 @@ from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+
     __tablename__ = "users"
-    if getenv("HBNB_TYPE_STORAGE") == 'db':
+    if getenv("HBNB_TYPE_STORAGE") == "db":
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True, default="")
         last_name = Column(String(128), nullable=True, default="")
 
-        places = relationship("Place", backref="user",
-                              cascade="all, delete-orphan")
-        reviews = relationship("Review", backref="user",
-                               cascade="all, delete-orphan")
+        places = relationship("Place", backref="user", cascade="all, delete-orphan")
+        reviews = relationship("Review", backref="user", cascade="all, delete-orphan")
+    else:
+        first_name = ""
+        email = ""
+        password = ""
+        last_name = ""
