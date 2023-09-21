5b1185c3376f7bf938864361d8d247f5f8e4fdc9
H-Asmaa <asmaehadar32@gmail.com>
2023-09-21 03:04:24 +0100

    Committing console.py updates

--- console.py
+++ console.py
@@ -94,7 +94,7 @@ class HBNBCommand(cmd.Cmd):
     def postcmd(self, stop, line):
         """Prints if isatty is false"""
         if not sys.__stdin__.isatty():
-            print('(hbnb) ', end='')
+            print('(hbnb) ')
         return stop
 
     def do_quit(self, command):
