8530de4ce2e7885dfea247739d7242f04ae00ae6
Bunje Kelly <kellybunje@gmail.com>
2023-09-20 19:57:48 -0700

    change

--- models/user.py
+++ models/user.py
@@ -14,5 +14,3 @@ class User(BaseModel, Base):
     last_name = Column(String(128), nullable=True)
     places = relationship("Place", backref="user", cascade="all, delete")
     reviews = relationship("Review", backref="user", cascade="all, delete")
-~
-~
