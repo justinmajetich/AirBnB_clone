c9803c740e9c7a806174bfa9e746a05a0359e10f
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-09-21 01:52:46 +0000

    Fixed bug

--- models/user.py
+++ models/user.py
@@ -18,3 +18,8 @@ class User(BaseModel, Base):
 
         places = relationship("Place", backref="user", cascade="all, delete-orphan")
         reviews = relationship("Review", backref="user", cascade="all, delete-orphan")
+    else:
+        first_name = ""
+        email = ""
+        password = ""
+        last_name = ""
