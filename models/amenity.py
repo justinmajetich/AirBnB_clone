39577cea5cbc597d0ebde78be04819e357443897
OUALIID <walidelhadim8@gmail.com>
2023-09-21 02:56:34 +0100

    Updates for the class amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""
+0x00. AirBnB clone - The console
+"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from models.place_amenity import place_amenity
+from models.place import Place
+import os
+
+STORAGE = os.getenv("HBNB_TYPE_STORAGE")
+
 
 class Amenity(BaseModel, Base):
-    """ A Amenity to stay """
-    __tablename__ = 'amenities'
-    id = Column(String(60), primary_key=True, nullable=False, unique=True)
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place",
-                                   secondary=place_amenity,
-                                   back_populates="amenities",
-                                   viewonly=False)
+    """Permit to add the amenities for places"""
+    __tablename__ = "amenities"
+    if STORAGE == "db":
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship(
+            'Place', secondary=Place.place_amenity,
+            viewonly=False)
+    else:
+        name = ""
+
