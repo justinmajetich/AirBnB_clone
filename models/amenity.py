48b7c3d4cf137172f2b5e4f2c7ea74ef2740f630
ToqaZidan <toqazidan@hotmail.com>
2023-09-21 05:25:39 +0300

    ..

--- models/amenity.py
+++ models/amenity.py
@@ -13,9 +13,9 @@ class Amenity(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == "db":
         __tablename__ = "amenities"
         name = Column(String(128), nullable=False)
-        place_amenities = relationship("Place", secondary='place_amenity',
-                                       viewonly=False,
-                                       backref="amenities")
+        # place_amenities = relationship("Place", secondary='place_amenity',
+        #                                viewonly=False,
+        #                                backref="amenities")
 
     else:
         name = ""
--- models/place.py
+++ models/place.py
@@ -28,7 +28,7 @@ class Place(BaseModel, Base):
         city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
         user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
         name = Column(String(128), nullable=False)
-        description = Column(String(1024))
+        description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, nullable=False, default=0)
         number_bathrooms = Column(Integer, nullable=False, default=0)
         max_guest = Column(Integer, nullable=False, default=0)
@@ -37,9 +37,9 @@ class Place(BaseModel, Base):
         longitude = Column(Float, nullable=True)
         reviews = relationship("Review", cascade="all, delete, delete-orphan",
                                backref="places")
-        # amenities = relationship("Amenity", secondary='place_amenity',
-        #                          viewonly=False,
-        #                          backref="place_amenities")
+        amenities = relationship("Amenity", secondary='place_amenity',
+                                 viewonly=False,
+                                 backref="place_amenities")
     else:
         city_id = ""
         user_id = ""
@@ -60,7 +60,7 @@ class Place(BaseModel, Base):
         @property
         def reviews(self):
             """ Returns to list of reviews.id """
-            val = models.storage.all()("Review").values()
+            val = models.storage.all("Review").values()
             list = []
             for obj in val:
                 if obj.place_id == self.id:
@@ -71,7 +71,7 @@ class Place(BaseModel, Base):
         @property
         def amenities(self):
             """ Returns to list of amenity.id """
-            val = models.storage.all()("Amenity").values()
+            val = models.storage.all("Amenity").values()
             list = []
             for obj in val:
                 if obj.place_id == self.id:
