dd8767a873be2bc4002bca5c69432d4e282fb127
OUALIID <walidelhadim8@gmail.com>
2023-09-21 02:58:54 +0100

    Updates for the class review

--- models/review.py
+++ models/review.py
@@ -2,12 +2,21 @@
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+import os
 
+STORAGE = os.getenv("HBNB_TYPE_STORAGE")
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = 'reviews'
-    id = Column(String(60), primary_key=True, nullable=False, unique=True)
-    text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    if STORAGE == "db":
+        id = Column(String(60), primary_key=True, nullable=False, unique=True)
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
+
