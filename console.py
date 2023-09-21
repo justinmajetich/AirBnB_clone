e7f7d4a39e75b82e0024dc3782aa4504950db0ff
OUALIID <walidelhadim8@gmail.com>
2023-09-21 03:05:17 +0100

    Fixing prompte output

--- console.py
+++ console.py
@@ -94,7 +94,7 @@ class HBNBCommand(cmd.Cmd):
     def postcmd(self, stop, line):
         """Prints if isatty is false"""
         if not sys.__stdin__.isatty():
-            print('(hbnb) ')
+            print('(hbnb) ', end='')
         return stop
 
     def do_quit(self, command):
