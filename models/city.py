4a1f07c98f89dc7e3c4b1789ea03e595cbaaebf4
Umuhasna <umuhasna123@gmail.com>
2023-09-22 12:05:13 -0700

    Update City: (models/city.py)

--- models/city.py
+++ models/city.py
@@ -16,6 +16,7 @@ class City(BaseModel, Base):
     if storage_type == 'db':
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        places = relationship('Place', cascade="all,delete", backref="cities")
     else:
         name = ""
         state_id = ""
