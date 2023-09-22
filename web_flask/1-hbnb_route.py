23cf4bfb188524611bb5ac288595050e18fc5887
AdemolaAyinde <demo4real99@gmail.com>
2023-09-22 02:31:51 -0700

    Web Flask Task

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
 """Starts Flask web app
-Routes:
-    / - display "Hello HBNB!"
-    /hbnb - display "HBNB"
 """
+
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hbnb_route():
-    """prints Hello HBNB"""
+def hello_hbnb():
+    """Displays 'Hello HBNB!'. """
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """prints HBNB"""
+    """Displays 'HBNB'."""
     return "HBNB"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,33 +1,29 @@
 #!/usr/bin/python3
 """Starts Flask web app
-Routes:
-    / - display "Hello HBNB!"
-    /hbnb - display "HBNB"
-    /c/<text> - display "C <text>"
 """
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hbnb_route():
-    """prints Hello HBNB"""
+def hello_hbnb():
+    """Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """prints HBNB"""
+    """Displays 'HBNB'."""
     return "HBNB"
 
 
-@app.route('/c/<string:text>', strict_slashes=False)
-def c_text(text):
-    """prints C followed by <text> content"""
-    text = text.replace("_", " ")
-    return "C %s" % text
+@app.route('/c/<text>', strict_slashes=False)
+def c_with_text(text):
+    """Displays 'C' followed by the value <text>."""
+    formatted_text = text.replace('_', ' ')
+    return "C {}".format(formatted_text)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host='0.0.0.0', port=5000)
