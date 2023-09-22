22345f43bedc739de56fa746c6292379b4fa3f21
smart raysam <raysamtob@gmail.com>
2023-09-22 09:26:03 +0100

    task 10

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" Amenity Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from os import getenv
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """amenity class"""
+    __tablename__ = 'amenities'
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
