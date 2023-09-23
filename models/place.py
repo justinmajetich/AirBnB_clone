20d56ec5a2bd09cb300d1a67a5319898fc9e3df9
Umuhasna <umuhasna123@gmail.com>
2023-09-23 00:01:29 -0700

    Fix class attribute description can be NULL

--- models/place.py
+++ models/place.py
@@ -35,7 +35,7 @@ class Place(BaseModel, Base):
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
-        description = Column(String(1024), nullable=False)
+        description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, nullable=False, default=0)
         number_bathrooms = Column(Integer, nullable=False, default=0)
         max_guest = Column(Integer, nullable=False, default=0)
