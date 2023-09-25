b045bb1915cfec231371188e1909e658f13c5c46
iranziprince01 <p.iranzi@alustudent.com>
2023-09-25 04:30:59 -0700

    models

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """ The Amenity class, contains state ID and name """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
