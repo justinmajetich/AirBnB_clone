fb03a120693e4c5fbd45cf658a73a360e71a7bdf
emmanueladdo <asememmanueladdo@gmail.com>
2023-09-25 05:32:52 +0000

    the state

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-starts a Flask web application 
+starts a Flask web application
 on port 5000
 """
 
@@ -21,6 +21,7 @@ def states_list():
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
