3d8901bcf722231dc1d1f076a92437e1264602e8
emmanueladdo <asememmanueladdo@gmail.com>
2023-09-27 20:00:54 +0000

    new state \state

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -7,13 +7,18 @@ from models.state import State
 
 app = Flask(__name__)
 
-# Define a teardown function to close the SQLAlchemy session after each request.
+# Define a teardown function to close the SQLAlchemy session after each
+# request.
+
+
 @app.teardown_appcontext
 def close_storage_session(exception):
     """Teardown function to close the SQLAlchemy session."""
     storage.close()
 
 # Define two routes for the application.
+
+
 @app.route("/states/", strict_slashes=False)
 @app.route("/states/<id>", strict_slashes=False)
 def display_states_and_cities(id=None):
@@ -23,10 +28,12 @@ def display_states_and_cities(id=None):
     all_states = storage.all(State)
 
     if not id:
-        # Prepare a dictionary for rendering the list of states and their names.
+        # Prepare a dictionary for rendering the list of states and their
+        # names.
         states_dict = {state.id: state.name for state in all_states.values()}
-        
-        # Render the "7-states_list.html" template to display the list of states.
+
+        # Render the "7-states_list.html" template to display the list of
+        # states.
         return render_template("7-states_list.html",
                                Table="States",
                                items=states_dict)
@@ -37,12 +44,16 @@ def display_states_and_cities(id=None):
     if state_key in all_states:
         # If the state is found, render the "9-states.html" template to display
         # the state's name and its associated cities.
-        return render_template("9-states.html",
-                               Table="State: {}".format(all_states[state_key].name),
-                               items=all_states[state_key])
+        return render_template(
+            "9-states.html",
+            Table="State: {}".format(
+                all_states[state_key].name),
+            items=all_states[state_key])
 
-    # If the state is not found, render the same template to display "Not found!"
+    # If the state is not found, render the same template to display "Not
+    # found!"
     return render_template("9-states.html", items=None)
+
 
 if __name__ == "__main__":
     # Start the Flask application.
