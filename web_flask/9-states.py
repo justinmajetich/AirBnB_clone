67bb94a5038f23a237d5f4a079048ca498f32dfd
emmanueladdo <asememmanueladdo@gmail.com>
2023-09-27 13:21:43 +0000

    move states

---
+++ web_flask/9-states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Script to start a Flask web application."""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+def states():
+    """Display a list of states."""
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda x: x.name)
+    return render_template('9-states.html', states=sorted_states)
+
+
+@app.route('/states/<state_id>', strict_slashes=False)
+def state_by_id(state_id):
+    """Display a state and its cities by ID."""
+    state = storage.get(State, state_id)
+    if state:
+        cities = sorted(state.cities, key=lambda x: x.name)
+        return render_template('9-states.html', state=state, cities=cities)
+    return render_template('9-states.html', not_found=True)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """Close the storage engine session."""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
