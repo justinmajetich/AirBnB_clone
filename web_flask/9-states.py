e9874305c1bd957a9793fb6d66723bb967617500
emmanueladdo <asememmanueladdo@gmail.com>
2023-09-27 19:32:00 +0000

    the new state

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -3,26 +3,28 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models.city import City
 
 app = Flask(__name__)
 
 
 @app.route('/states', strict_slashes=False)
-def states():
+def list_states():
     """Display a list of states."""
     states = storage.all(State).values()
     sorted_states = sorted(states, key=lambda x: x.name)
-    return render_template('9-states.html', states=sorted_states)
+    return render_template('7-states_list.html', states=sorted_states)
 
 
 @app.route('/states/<state_id>', strict_slashes=False)
-def state_by_id(state_id):
-    """Display a state and its cities by ID."""
+def state_cities(state_id):
+    """Display cities of a specific state."""
     state = storage.get(State, state_id)
     if state:
         cities = sorted(state.cities, key=lambda x: x.name)
         return render_template('9-states.html', state=state, cities=cities)
-    return render_template('9-states.html', not_found=True)
+    else:
+        return render_template('9-states.html', not_found=True)
 
 
 @app.teardown_appcontext
