cbd55c7ca4e6b60ea881cbfd53f85b5e6f4e9bff
emmanueladdo <asememmanueladdo@gmail.com>
2023-09-27 17:21:19 +0000

    states by

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
+"""Script to start a Flask web application."""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
 
 @app.route('/states', strict_slashes=False)
-@app.route('/states/<id>', strict_slashes=False)
-def states(id=None):
-    '''Returns a template with all the states in storage'''
-    states = storage.all("State").values()
-    if id is None:
-        return render_template('9-states.html', states=states)
-    else:
-        state = next((state for state in states if state.id == id), None)
-        return render_template('9-states.html', state=state)
+def states():
+    """Display a list of states."""
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda x: x.name)
+    return render_template('9-states.html', states=sorted_states)
+
+
+@app.route('/states/<state_id>', strict_slashes=False)
+def state_by_id(state_id):
+    """Display a state and its cities by ID."""
+    state = storage.get(State, state_id)
+    if state:
+        cities = sorted(state.cities, key=lambda x: x.name)
+        return render_template('9-states.html', state=state, cities=cities)
+    return render_template('9-states.html', not_found=True)
 
 
 @app.teardown_appcontext
-def close_session(exception):
-    '''Removes current session after each request'''
+def teardown(exception):
+    """Close the storage engine session."""
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
