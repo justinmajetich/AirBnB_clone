dd7ae5e229fe4eaf454b20368a4ad97d090adbbd
emmanueladdo <asememmanueladdo@gmail.com>
2023-09-27 20:43:33 +0000

    www

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -4,7 +4,7 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-from models.city import City 
+from models.city import City
 
 app = Flask(__name__)
 
