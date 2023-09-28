3e3a8ac19b63f48c186d7d0cc7e1e4f8d305e4f7
TelesphoreUwabera <t.uwabera@alustudent.com>
2023-09-28 08:54:22 +0200

    Create setup_mysql_test.sql

---
+++ setup_mysql_test.sql
@@ -0,0 +1,6 @@
+-- Create database and test user
+-- Bring privileges to user
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
