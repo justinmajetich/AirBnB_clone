62f0070bcd89e268df371979a2c16261725fd2fe
Peter Rigii <theonlyrigii@gmail.com>
2023-09-29 15:50:19 +0300

    added test

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-""" """
+""" test for state class"""
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
+import unittest
 
-
-class test_state(test_basemodel):
-    """ """
+class test_state(unittest.TestCase,test_basemodel):
+    """test state class """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ initialize the state class for testing """
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
 
     def test_name3(self):
-        """ """
+        """ test for name"""
         new = self.value()
         self.assertEqual(type(new.name), str)
