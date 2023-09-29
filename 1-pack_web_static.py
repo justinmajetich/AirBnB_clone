2f289b893bbe2a2aec205c3ab10941c63bc832d7
Islam-19984 <i.hemada@alustudent.com>
2023-09-29 14:19:29 -0700

    1-pack_web_static.py

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''Fabric script to generate .tgz archive'''
+
+from fabric.api import local
+from datetime import datetime
+
+from fabric.decorators import runs_once
+
+
+@runs_once
+def do_pack():
+    '''generates .tgz archive from the contents of the web_static folder'''
+    local("mkdir -p versions")
+    path = ("versions/web_static_{}.tgz"
+            .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
+    result = local("tar -cvzf {} web_static"
+                   .format(path))
+
+    if result.failed:
+        return None
+    return path
