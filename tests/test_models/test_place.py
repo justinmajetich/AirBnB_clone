bdb81a4d7d1da5ace8c625d9bbe2c1b1216d5a08
Prince Iranzi <116654088+iranziprince01@users.noreply.github.com>
2023-10-03 10:45:13 +0200

    Update test_place.py

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -15,53 +15,53 @@ class test_Place(test_basemodel):
 
     def test_city_id(self):
         """ """
-        new = self.value()
+        new = self.value(city_id="fsdfsd-45454-4545-45454")
         self.assertEqual(type(new.city_id), str)
 
     def test_user_id(self):
         """ """
-        new = self.value()
+        new = self.value(user_id="fsdfsd-4554-45654-fdfd")
         self.assertEqual(type(new.user_id), str)
 
     def test_name(self):
         """ """
-        new = self.value()
+        new = self.value(name="hbo")
         self.assertEqual(type(new.name), str)
 
     def test_description(self):
         """ """
-        new = self.value()
+        new = self.value(description="hey you")
         self.assertEqual(type(new.description), str)
 
     def test_number_rooms(self):
         """ """
-        new = self.value()
+        new = self.value(number_rooms=6)
         self.assertEqual(type(new.number_rooms), int)
 
     def test_number_bathrooms(self):
         """ """
-        new = self.value()
+        new = self.value(number_bathrooms=78)
         self.assertEqual(type(new.number_bathrooms), int)
 
     def test_max_guest(self):
         """ """
-        new = self.value()
+        new = self.value(max_guest=8)
         self.assertEqual(type(new.max_guest), int)
 
     def test_price_by_night(self):
         """ """
-        new = self.value()
+        new = self.value(price_by_night=500)
         self.assertEqual(type(new.price_by_night), int)
 
     def test_latitude(self):
         """ """
-        new = self.value()
+        new = self.value(latitude=0.5)
         self.assertEqual(type(new.latitude), float)
 
     def test_longitude(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.latitude), float)
+        new = self.value(longitude=5.6)
+        self.assertEqual(type(new.longitude), float)
 
     def test_amenity_ids(self):
         """ """
