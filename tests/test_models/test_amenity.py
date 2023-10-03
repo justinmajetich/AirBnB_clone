3e89c84e4895a511dfb21bbd2f92f8375ade0537
Prince Iranzi <116654088+iranziprince01@users.noreply.github.com>
2023-10-03 10:42:29 +0200

    Update test_amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
 """ """
 from tests.test_models.test_base_model import test_basemodel
-from models.amenity import Amenity
+from models.review import Review
 
 
-class test_Amenity(test_basemodel):
+class test_review(test_basemodel):
     """ """
 
     def __init__(self, *args, **kwargs):
         """ """
         super().__init__(*args, **kwargs)
-        self.name = "Amenity"
-        self.value = Amenity
+        self.name = "Review"
+        self.value = Review
 
-    def test_name2(self):
+    def test_place_id(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+        new = self.value(place_id="565-5654-45654-45654")
+        self.assertEqual(type(new.place_id), str)
+
+    def test_user_id(self):
+        """ """
+        new = self.value(user_id="dfdg-4545-454-gdg")
+        self.assertEqual(type(new.user_id), str)
+
+    def test_text(self):
+        """ """
+        new = self.value(text="hey you")
+        self.assertEqual(type(new.text), str)
