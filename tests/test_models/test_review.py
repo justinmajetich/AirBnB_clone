3526a43be05c54dbe002c436efcfb85143b2701f
Prince Iranzi <116654088+iranziprince01@users.noreply.github.com>
2023-10-03 10:45:45 +0200

    Update test_review.py

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -15,15 +15,15 @@ class test_review(test_basemodel):
 
     def test_place_id(self):
         """ """
-        new = self.value()
+        new = self.value(place_id="565-5654-45654-45654")
         self.assertEqual(type(new.place_id), str)
 
     def test_user_id(self):
         """ """
-        new = self.value()
+        new = self.value(user_id="dfdg-4545-454-gdg")
         self.assertEqual(type(new.user_id), str)
 
     def test_text(self):
         """ """
-        new = self.value()
+        new = self.value(text="hey you")
         self.assertEqual(type(new.text), str)
