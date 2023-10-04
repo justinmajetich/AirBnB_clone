fd4a08b04bf66b3b9834ecb1ea81fad914fa537d
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-10-04 08:13:13 +0300

    Add task 8

--- models/user.py
+++ models/user.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from os import getenv
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from models.base_model import Base
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
