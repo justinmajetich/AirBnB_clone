13d07c34b84ea726c2c1f9eaec239e8c100bfe32
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-10-04 08:40:47 +0300

    Addd task 8

--- models/user.py
+++ models/user.py
@@ -9,16 +9,16 @@ class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
 
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    # email = ''
+    # password = ''
+    # first_name = ''
+    # last_name = ''
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
