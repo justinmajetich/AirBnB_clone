e6a7ed5f8f33b083f9966109b7203a1629393e1e
azukauteh <aazure263@gmail.com>
2023-10-05 03:10:00 -0700

    script that sets up your web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,51 +1,62 @@
 #!/usr/bin/env bash
+# Installs, configures, and starts the web server
+SERVER_CONFIG="server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
 
-# Create necessary directories
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
+	server_name _;
+	index index.html index.htm;
+	error_page 404 /404.html;
+	add_header X-Served-By \$hostname;
 
-# Create a fake HTML file
-echo "This is a test." > /data/web_static/releases/test/index.html
+	location / {
+		root /var/www/html/;
+		try_files \$uri \$uri/ =404;
+	}
 
-# Create or recreate symbolic link
-rm -rf /data/web_static/current
-ln -s /data/web_static/releases/test/ /data/web_static/current
+	location /hbnb_static/ {
+		alias /data/web_static/current/;
+		try_files \$uri \$uri/ =404;
+	}
 
-# Give ownership to the appropriate user and group
-chown -R  magnificent.tech:magnificent.tech /data/
+	if (\$request_filename ~ redirect_me) {
+		rewrite ^ https://sketchfab.com/bluepeno/models permanent;
+	}
 
-# Update Nginx configuration
-
-nginx_config="server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    server_name magnificent.tech;
-
-    add_header X-Served-By \$HOSTNAME;
-    root /var/www/html;
-    index index.html index.htm;
-
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-        root /var/www/html;
-        internal;
-    }
+	location = /404.html {
+		root /var/www/error/;
+		internal;
+	}
 }"
+HOME_PAGE="<!DOCTYPE html>
+<html lang='en-US'>
+	<head>
+		<title>Home - AirBnB Clone</title>
+	</head>
+	<body>
+		<h1>Welcome to AirBnB!</h1>
+	<body>
+</html>
+"
+# shellcheck disable=SC2230
+if [[ "$(which nginx | grep -c nginx)" == '0' ]]; then
+    apt-get update
+    apt-get -y install nginx
+fi
+mkdir -p /var/www/html /var/www/error
+chmod -R 755 /var/www
+echo 'Hello World!' > /var/www/html/index.html
+echo -e "Ceci n\x27est pas une page" > /var/www/error/404.html
 
-# Save the Nginx configuration to a temporary file
-echo "$nginx_config" > /tmp/nginx_config
-
-# Replace the Nginx default configuration file
-mv /tmp/nginx_config /etc/nginx/sites-available/default
-
-# Restart Nginx
-service nginx restart
+mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo -e "$HOME_PAGE" > /data/web_static/releases/test/index.html
+[ -d /data/web_static/current ] && rm -rf /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -hR ubuntu:ubuntu /data
+bash -c "echo -e '$SERVER_CONFIG' > /etc/nginx/sites-available/default"
+ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
+if [ "$(pgrep -c nginx)" -le 0 ]; then
+	service nginx start
+else
+	service nginx restart
+fi
