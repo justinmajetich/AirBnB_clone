31d0d969be9dccbc013e5858992f5ab29a1333fd
Lebogang <lebogangodeveloper@gmail.com>
2023-10-05 15:07:23 +0200

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,6 +13,20 @@ env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
 
+def do_pack():
+    """Generates a .tgz archive from the contents of the web_static folder."""
+
+    local("mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    archived_f_path = "versions/web_static_{}.tgz".format(date)
+    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
+
+    if t_gzip_archive.succeeded:
+        return archived_f_path
+    else:
+        return None
+
+
 def do_deploy(archive_path):
     """
     Distributes an archive to web servers and deploys it.
