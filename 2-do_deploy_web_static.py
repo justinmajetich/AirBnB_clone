58b67146ad0ea646fef4eddf027746233bc18cd0
Lebogang <lebogangodeveloper@gmail.com>
2023-10-05 15:18:45 +0200

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,13 +4,12 @@ Fabric script that distributes an archive to your web servers
 and deploys it using the function do_deploy.
 """
 
-from datetime import datetime
-from fabric.api import *
 import os
+from fabric.api import *
+from datetime import datetime
 
 env.hosts = ['54.172.84.26', '52.23.177.182']
 env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_pack():
@@ -28,47 +27,23 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """
-    Distributes an archive to web servers and deploys it.
-    """
-    if not os.path.exists(archive_path):
-        return False
-
-    try:
-        archive_filename = os.path.basename(archive_path)
-        archive_no_ext = os.path.splitext(archive_filename)[0]
-
-        # Upload the archive to the /tmp/ directory of the web server
+    """Distributes an archive to web servers and deploys it."""
+    if os.path.exists(archive_path):
+        archived_file = archive_path[9:]
+        newest_version = "/data/web_static/releases/" + archived_file[:-4]
+        archived_file = "/tmp/" + archived_file
         put(archive_path, "/tmp/")
-
-        # Create the release directory
-        run("sudo mkdir -p /data/web_static/releases/{}"
-            .format(archive_no_ext))
-
-        # Uncompress the archive into the release directory
-        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}"
-            .format(archive_filename, archive_no_ext))
-
-        # Remove the uploaded archive
-        run("sudo rm /tmp/{}".format(archive_filename))
-
-        # Move the contents from the extracted folder to the release directory
-        run("sudo mv /data/web_static/releases/{}/web_static/* \
-/data/web_static/releases/{}/".format(archive_no_ext, archive_no_ext))
-
-        # Remove the extracted folder
-        run("sudo rm -rf /data/web_static/releases/{}/web_static"
-            .format(archive_no_ext))
-
-        # Remove the current symbolic link
+        run("sudo mkdir -p {}".format(newest_version))
+        run("sudo tar -xzf {} -C {}/".format(archived_file,
+                                             newest_version))
+        run("sudo rm {}".format(archived_file))
+        run("sudo mv {}/web_static/* {}".format(newest_version,
+                                                newest_version))
+        run("sudo rm -rf {}/web_static".format(newest_version))
         run("sudo rm -rf /data/web_static/current")
-
-        # Create a new symbolic link to the new version
-        run("sudo ln -s /data/web_static/releases/{} /data/web_static/current"
-            .format(archive_no_ext))
+        run("sudo ln -s {} /data/web_static/current".format(newest_version))
 
         print("New version deployed!")
         return True
 
-    except Exception as e:
-        return False
+    return False
