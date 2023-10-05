02520b00fa5b739a864bb2b202189ae6c68bc932
Lebogang <lebogangodeveloper@gmail.com>
2023-10-05 14:24:42 +0200

    Set up web servers for the deployment of web_static using Puppet

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -25,65 +25,29 @@ $nginx_conf = "server {
 package { 'nginx':
   ensure   => 'present',
   provider => 'apt',
-}
+} ->
 
 # Create necessary directories
 file { '/data':
   ensure  => 'directory',
-}
+} ->
 
 file { '/data/web_static':
   ensure => 'directory',
-}
+} ->
 
 file { '/data/web_static/releases':
   ensure => 'directory',
-}
+} ->
 
 file { '/data/web_static/shared':
   ensure => 'directory',
-}
+} ->
 
 file { '/data/web_static/releases/test':
   ensure => 'directory',
-}
-
-# Create a symbolic link
-file { '/data/web_static/current':
-  ensure => 'link',
-  target => '/data/web_static/releases/test',
-}
-
-# Set ownership
-exec { 'chown -R ubuntu:ubuntu /data/':
-  path => '/usr/bin/:/usr/local/bin/:/bin/',
-}
-
-# Create Nginx directories and configuration
-file { '/var/www':
-  ensure => 'directory',
-}
-
-file { '/var/www/html':
-  ensure => 'directory',
-}
-
-file { '/var/www/html/index.html':
+} ->
+file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
-  content => "ALX Africa/Holberton School Nginx\n",
-}
-
-file { '/var/www/html/404.html':
-  ensure  => 'present',
-  content => "Ceci n'est pas une page\n",
-}
-
-file { '/etc/nginx/sites-available/default':
-  ensure  => 'present',
-  content => $nginx_conf,
-}
-
-# Restart Nginx
-exec { 'nginx restart':
-  path => '/etc/init.d/',
-}
+  content => "ALX Africa/Holberton School Puppet\n",
+} ->
