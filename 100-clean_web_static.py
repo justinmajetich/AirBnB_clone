0218d7fcc8a7e0e870a7e21d20f5a9552fab3daf
Lebogang <lebogangodeveloper@gmail.com>
2023-10-05 14:02:25 +0200

    Delete out-of-date archives, using the function do_clean

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -3,40 +3,23 @@
 Fabric script that deletes out-of-date archives, using the function do_clean.
 """
 
-from fabric.api import env, run, local, lcd
-from datetime import datetime
 import os
+from fabric.api import *
 
 env.hosts = ['54.172.84.26', '52.23.177.182']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_clean(number=0):
-    """Deletes out-of-date archives"""
-    try:
-        number = int(number)
-        if number < 0:
-            return False
+    """Delete out-of-date archives"""
+    number = 1 if int(number) == 0 else int(number)
 
-        if number == 0 or number == 1:
-            number = 1
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
 
-        local_archives = local("ls -tr versions", capture=True).split()
-        web_server_archives = run("ls -tr /data/web_static/releases",
-                                  capture=True).split()
-
-        local_archives_to_delete = local_archives[:-number]
-        web_server_archives_to_delete = web_server_archives[:-number]
-
-        with lcd("versions"):
-            for archive in local_archives_to_delete:
-                local("rm -f {}".format(archive))
-
-        for archive in web_server_archives_to_delete:
-            run("rm -rf /data/web_static/releases/{}".format(archive))
-
-        return True
-
-    except Exception:
-        return False
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
