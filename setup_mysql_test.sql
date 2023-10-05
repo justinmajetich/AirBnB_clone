2b1bb6a58b04155981f53f35d34885193d19a739
Prince Iranzi <116654088+iranziprince01@users.noreply.github.com>
2023-10-05 09:35:29 +0200

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,11 +1,9 @@
--- create the data base if it doesnt exit
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-
--- Create a new use in the localhost COMMENT
+-- This is the file that is used to create the database and user for the test suite.
+CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-
--- Grantt the user hbnb_dev all privileges on the database hbnb_dev_db
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-
--- Grant all Select Privileges on performance_schema to hbnb_dev
+GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- Path: setup_mysql_dev.sql
+-- Compare this snippet from setup_mysql_test.sql:
