100521e9c3240902a0d9babbe4c44620ff3c4608
Lebogang <lebogangodeveloper@gmail.com>
2023-10-05 13:53:59 +0200

    Create and distributes an archive to web servers, using the function deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -4,14 +4,12 @@ Fabric script that creates and distributes an archive to web servers
 and deploys it using the function deploy.
 """
 
-from fabric.api import env, run, put
-from os.path import exists
+from fabric.api import env, local, run, put
+from os.path import exists, isdir
 from datetime import datetime
 import os
 
 env.hosts = ['54.172.84.26', '52.23.177.182']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_pack():
@@ -19,8 +17,8 @@ def do_pack():
     try:
         now = datetime.now()
         archive_name = "web_static_{}.tgz".format(now.strftime("%Y%m%d%H%M%S"))
-        local("mkdir -p versions")
-        local("tar -cvzf versions/{} web_static".format(archive_name))
+        local("sudo mkdir -p versions")
+        local("sudo tar -cvzf versions/{} web_static".format(archive_name))
         return "versions/{}".format(archive_name)
     except Exception:
         return None
@@ -36,17 +34,17 @@ def do_deploy(archive_path):
         archive_no_ext = os.path.splitext(archive_filename)[0]
 
         put(archive_path, "/tmp/")
-        run("mkdir -p /data/web_static/releases/{}/".format(archive_no_ext))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(archive_filename, archive_no_ext))
-        run("rm /tmp/ {}".format(archive_filename))
-        run("mv / data/web_static/releases/{}/web_static/*
-            / data/web_static/releases/{} /"
-            .format(archive_no_ext, archive_no_ext))
-        run("rm -rf /data/web_static/releases/{}/web_static"
+        run("sudo mkdir -p /data/web_static/releases/{}/"
             .format(archive_no_ext))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(archive_filename, archive_no_ext))
+        run("sudo rm /tmp/{}".format(archive_filename))
+        run("sudo mv /data/web_static/releases/{}/web_static/* \
+/data/web_static/releases/{}/".format(archive_no_ext, archive_no_ext))
+        run("sudo rm -rf /data/web_static/releases/{}/web_static"
+            .format(archive_no_ext))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current"
             .format(archive_no_ext))
 
         return True
@@ -56,12 +54,7 @@ def do_deploy(archive_path):
 
 
 def deploy():
-    """
-    Deploys the web_static archive to the web servers.
-
-    Returns:
-        bool: True if deployment is successful, False otherwise.
-    """
+    """Deploys the web_static archive to the web server"""
     archive_path = do_pack()
     if not archive_path:
         return False
