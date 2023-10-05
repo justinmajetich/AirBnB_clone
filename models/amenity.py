92881bca57d6be91dd006d8a05716dc3bfd40dd0
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-10-05 04:09:56 +0300

    Add task 10

--- models/amenity.py
+++ models/amenity.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from os import getenv
 from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String
-from models.place import place_amenity
 from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
     '''Amenity class'''
-
     __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship(
-        "Place", secondary=place_amenity, back_populates="amenities")
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        from models.place import place_amenity
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship(
+            "Place", secondary=place_amenity, back_populates="amenities")
 
     def __init__(self, *args, **kwargs):
         '''initializes Amenity'''
--- models/place.py
+++ models/place.py
@@ -7,12 +7,13 @@ from os import getenv
 from sqlalchemy.orm import relationship
 from models import storage
 
-place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id', String(60), ForeignKey('places.id'),
-                             primary_key=True, nullable=False),
-                      Column('amenity_id', String(60),
-                             ForeignKey('amenities.id'),
-                             primary_key=True, nullable=False))
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    place_amenity = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60), ForeignKey('places.id'),
+                                 primary_key=True, nullable=False),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id'),
+                                 primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
