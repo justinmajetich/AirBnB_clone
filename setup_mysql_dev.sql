a5b359278b89bcc0e7cf5b3eb7700d6bd432553e
Prince Iranzi <116654088+iranziprince01@users.noreply.github.com>
2023-10-05 09:34:40 +0200

    Update setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,11 +1,6 @@
--- create the data base if it doesnt exit
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
--- Create a new use in the localhost COMMENT
+-- SET UP DEV DATABASE WITH USER
+CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
--- Grantt the user hbnb_dev all privileges on the database hbnb_dev_db
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
--- Grant all Select Privileges on performance_schema to hbnb_dev
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
