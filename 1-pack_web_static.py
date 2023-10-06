c7371a920714d4ef13eaa0486f555dba91041be2
danjor667 <jordannguepi@gmail.com>
2023-10-06 16:40:23 +0200

    pep8

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,22 +4,25 @@
 from datetime import datetime
 from fabric.api import local
 
+
 def do_pack():
     """
     archieving web_static dir
     """
-    #create  versions directory if not exits
+#   create  versions directory if not exits
     local("mkdir -p versions")
 
     date = datetime.now().strftime("%Y%m%d%H%M%S")
     file_name = f"web_static_{date}.tgz"
     result = local(f"tar -czvf {file_name} web_static ")
     local(f"mv {file_name} ./versions")
-    #checking if success
+#   checking if success
 
     if result.succeeded:
         return f"versions/{file_name}"
     else:
         return None
+
+
 if __name__ == "__main__":
     do_pack()
