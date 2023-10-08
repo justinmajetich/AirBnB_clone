7206cf656b542ce0193e8b83cdfe112936692939
EucTech <euccreative@gmail.com>
2023-10-08 14:46:27 +0100

    fixed

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,22 +10,21 @@ def do_pack():
     """This is a function that will generate the .tgz archive contents
         using the datetime to show when the .tgz is generated.
     """
-    # year = datetime.utcnow().year
-    # month = datetime.utcnow().month
-    # day = datetime.utcnow().day
-    # hour = datetime.utcnow().hour
-    # minute = datetime.utcnow().minute
-    # second = datetime.utcnow().second
+    year = datetime.utcnow().year
+    month = datetime.utcnow().month
+    day = datetime.utcnow().day
+    hour = datetime.utcnow().hour
+    minute = datetime.utcnow().minute
+    second = datetime.utcnow().second
 
-    now = datetime.now()
-    timestamp = now.strftime("%Y%m%d%H%M%S")
+    # now = datetime.now()
+    # timestamp = now.strftime("%Y%m%d%H%M%S")
 
     # To create version directory
     local('mkdir -p versions')
 
-    # content = f"versions/web_static_{year}{month}{day}\
-    #     {hour}{minute}{second}.tgz"
-    content = f"versions/web_static_{timestamp}"
+    content = f"versions/web_static_{year}{month}{day}{hour}{minute}{second}.tgz"
+    # content = f"versions/web_static_{timestamp}"
     # To generate the .tzg content
     result = local(f'tar -czvf {content} web_static/')
     if result.succeeded:
