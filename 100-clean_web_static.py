d395969436bca41b43d34a6a70a717fff77d60b6
azukauteh <aazure263@gmail.com>
2023-10-08 14:10:29 -0700

    deletes out-of-date archives, using the function do_clean:

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 """A module for web application deployment with Fabric."""
+
 import os
 from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
-
-env.hosts = ["100.26.161.114", "54.197.88.255"]
+env.hosts = ["34.73.0.174", "35.196.78.105"]
 """The list of host server IP addresses."""
 
 
 @runs_once
 def do_pack():
-    """Archives the static files."""
+    """Archives the static files.
+
+    Returns:
+        str: Path to the archived static files.
+    """
     if not os.path.isdir("versions"):
         os.mkdir("versions")
     cur_time = datetime.now()
@@ -26,24 +30,29 @@ def do_pack():
     try:
         print("Packing web_static to {}".format(output))
         local("tar -cvzf {} web_static".format(output))
-        archize_size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
+        archive_size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, archive_size))
+        return output
     except Exception:
-        output = None
-    return output
+        return None
 
 
 def do_deploy(archive_path):
     """Deploys the static files to the host servers.
+
     Args:
         archive_path (str): The path to the archived static files.
+
+    Returns:
+        bool: True if successful, False otherwise.
     """
     if not os.path.exists(archive_path):
         return False
+
     file_name = os.path.basename(archive_path)
     folder_name = file_name.replace(".tgz", "")
     folder_path = "/data/web_static/releases/{}/".format(folder_name)
-    success = False
+
     try:
         put(archive_path, "/tmp/{}".format(file_name))
         run("mkdir -p {}".format(folder_path))
@@ -54,14 +63,16 @@ def do_deploy(archive_path):
         run("rm -rf /data/web_static/current")
         run("ln -s {} /data/web_static/current".format(folder_path))
         print('New version deployed!')
-        success = True
+        return True
     except Exception:
-        success = False
-    return success
+        return False
 
 
 def deploy():
     """Archives and deploys the static files to the host servers.
+
+    Returns:
+        bool: True if successful, False otherwise.
     """
     archive_path = do_pack()
     return do_deploy(archive_path) if archive_path else False
@@ -69,24 +80,19 @@ def deploy():
 
 def do_clean(number=0):
     """Deletes out-of-date archives of the static files.
+
     Args:
-        number (Any): The number of archives to keep.
+        number (int): The number of archives to keep.
     """
-    archives = os.listdir('versions/')
-    archives.sort(reverse=True)
-    start = int(number)
-    if not start:
-        start += 1
-    if start < len(archives):
-        archives = archives[start:]
-    else:
-        archives = []
-    for archive in archives:
-        os.unlink('versions/{}'.format(archive))
-    cmd_parts = [
-        "rm -rf $(",
-        "find /data/web_static/releases/ -maxdepth 1 -type d -iregex",
-        " '/data/web_static/releases/web_static_.*'",
-        " | sort -r | tr '\\n' ' ' | cut -d ' ' -f{}-)".format(start + 1)
-    ]
-    run(''.join(cmd_parts))
+    try:
+        number = int(number)
+    except ValueError:
+        print("Invalid number provided. Please provide a valid integer.")
+        return
+
+    if number < 0:
+        print("Number should be a non-negative integer.")
+        return
+
+    local("cd versions; ls -1t | tail -n +{} | xargs -I {{}} rm -rf {{}}".format(number + 1))
+    run("cd /data/web_static/releases/; ls -1t | tail -n +{} | xargs -I {{}} rm -rf {{}}".format(number + 1))
