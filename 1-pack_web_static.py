46f58ac0541404f3327fa4eed618b7f1c4851c82
EucTech <euccreative@gmail.com>
2023-10-08 14:43:45 +0100

    permission

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" This is a Fabric script that generates a .tgz archive from the contents of
+""" This is Fabric script that generates a .tgz archive from the contents of
 the web_static folder of your AirBnB Clone repo, using the function do_pack
 """
 from datetime import datetime
