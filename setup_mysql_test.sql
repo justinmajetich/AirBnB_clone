ad4ba75b3a7ca0a6787019b3e7ddd65fa8b861eb
Prince Iranzi <116654088+iranziprince01@users.noreply.github.com>
2023-10-08 13:34:35 +0200

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,6 @@
--- This is the file that is used to create the database and user for the test suite.
-CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
+-- script that prepares a MySQL server for the project
+-- Creates a database and a user with a password
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-
--- Path: setup_mysql_dev.sql
--- Compare this snippet from setup_mysql_test.sql:
+GRANT ALL ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
