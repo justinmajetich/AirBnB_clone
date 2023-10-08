2f252568a3021ac5f98ddee54b3e8c1a7c682759
azukauteh <aazure263@gmail.com>
2023-10-08 13:33:19 -0700

    .configurer web servers using puppet

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-"""Distribute an archive to your web servers."""
-from fabric.api import env, put, run
+"""
+Fabric script that distributes an archive to your web servers and deploys it.
+"""
+from fabric.api import put, run, env
 from os.path import exists
-from datetime import datetime
 
-env.hosts = ['100.25.145.62','54.197.88.255']
-env.user = 'ubuntu'  
-env.key_filename = '/root/.ssh/school'
+env.hosts = ['52.55.249.213', '54.157.32.137']
+env.user = "ubuntu"
+env.key_filename = "/root/.ssh/school"
 
 
 def do_deploy(archive_path):
-    """Deploy the web_static archive to the web servers."""
+    """Distribute and deploy the archive to web servers."""
     if not exists(archive_path):
         return False
 
     try:
         # Upload the archive to the /tmp/ directory on the server
-        put(archive_path, "/tmp/web_static_20170315003959.tgz")
+        put(archive_path, "/tmp/")
 
         # Get the archive filename without extension
         filename = archive_path.split("/")[-1].split(".")[0]
 
         # Uncompress the archive to the /data/web_static/releases/ directory
-        run("mkdir -p /data/web_static/releases/{}".format(filename))
+        run("mkdir -p /data/web_static/releases/{}/".format(filename))
         run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".
             format(filename, filename))
 
@@ -38,7 +39,7 @@ def do_deploy(archive_path):
         run("rm -rf /data/web_static/current")
 
         # Create a new symbolic link
-        run("ln -s /data/web_static/releases/{} "
+        run("ln -s /data/web_static/releases/{}/ "
             "/data/web_static/current".format(filename))
 
         print("New version deployed!")
@@ -50,7 +51,6 @@ def do_deploy(archive_path):
 
 
 if __name__ == "__main__":
-    # deploy path to your web_static archive
-    archive_path = "/tmp/web_static_20170315003959.tgz"
+    # Replace with the actual path to your web_static archive
+    archive_path = "versions/web_static_20170315003959.tgz"
     do_deploy(archive_path)
-
