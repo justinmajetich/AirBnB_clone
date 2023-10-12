476d79a9c6b1f31939ff080acfd869a9b72c8be1
Lebogang <lebogangodeveloper@gmail.com>
2023-10-12 15:27:35 +0200

    Fix the path to which the symbolic link points

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,12 +5,11 @@
     sudo apt-get -y update
     sudo apt-get -y install nginx
 
-# Create necessary directories if they don't exist
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/current/
 
-# Create HTML file
+# Create necessary directories
+sudo mkdir -p /data/web_static/{releases/test,shared}
+
+# Create a fake HTML file for testing
 echo "<html>
   <head>
   </head>
@@ -19,16 +18,20 @@ echo "<html>
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
-# Create or recreate the symbolic link
+# Create the symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Give ownership to the ubuntu user and group
+# Give ownership to the ubuntu user and group (recursively)
 sudo chown -R ubuntu:ubuntu /data/
 
-# Update Nginx configuration
+# Configure Nginx to serve content
 config_file="/etc/nginx/sites-available/default"
-nginx_config="location /hbnb_static/ {\\n    alias /data/web_static/current/;\\n}"
+nginx_config="
+location /hbnb_static {
+    alias /data/web_static/current/;
+}
+"
 sudo sed -i "/server_name _;/a $nginx_config" $config_file
 
-# Restart the Nginx service to apply the configuration changes
+# Restart Nginx service to apply the configuration changes
 sudo service nginx restart
