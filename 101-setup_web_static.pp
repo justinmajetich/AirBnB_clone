45a22288f185942e1ef8a7b73c2a692abaaa744d
Lebogang <lebogangodeveloper@gmail.com>
2023-10-13 13:46:09 +0000

    Set up web servers for the deployment using Puppet

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -24,30 +24,67 @@ $nginx_conf = "server {
 # Install Nginx
 package { 'nginx':
   ensure   => 'present',
-  provider => 'apt',
+  provider => 'apt'
 } ->
 
 # Create necessary directories
 file { '/data':
-  ensure  => 'directory',
+  ensure  => 'directory'
 } ->
 
 file { '/data/web_static':
-  ensure => 'directory',
+  ensure => 'directory'
 } ->
 
 file { '/data/web_static/releases':
-  ensure => 'directory',
-} ->
-
-file { '/data/web_static/shared':
-  ensure => 'directory',
+  ensure => 'directory'
 } ->
 
 file { '/data/web_static/releases/test':
-  ensure => 'directory',
+  ensure => 'directory'
 } ->
+
+file { '/data/web_static/shared':
+  ensure => 'directory'
+} ->
+
 file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
-  content => "ALX Africa/Holberton School Puppet\n",
+  content => "ALX Africa//Holberton School Puppet setup\n"
 } ->
+
+file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
+} ->
+
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
+}
+
+file { '/var/www':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "ALX Africa//Holberton School Nginx\n"
+} ->
+
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n"
+} ->
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $nginx_conf
+} ->
+
+exec { 'nginx restart':
+  path => '/etc/init.d/'
+}
