d30cecdeb99c29de8c109b9c953ce0883b66185a
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-10-14 23:41:20 -0400

    fix?

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
-"""Script that have 3 routes"""
+"""Script that has 3 routes"""
 from flask import Flask
-
 
 app = Flask(__name__)
 
-
 @app.route('/', strict_slashes=False)
 def hello():
-    """return Hello HBNB"""
+    """Return Hello HBNB"""
     return "Hello HBNB!"
 
-
 @app.route('/hbnb', strict_slashes=False)
-def hello_hbnb():
-    """return HBNB"""
+def hbnb():
+    """Return HBNB"""
     return "HBNB"
 
-
-@app.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
-    """replace _ for spaces"""
-    text = text.replace('_', ' ')
-    return f" C {text}"
-
+@app.route('/c/<text_param>', strict_slashes=False)
+def c_with_text(text_param):
+    """Replace underscores with spaces"""
+    text = text_param.replace('_', ' ')
+    return f"C {text}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
