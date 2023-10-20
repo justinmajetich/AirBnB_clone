8f73e83a181473b90745d0a3d392084e5090992e
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-10-20 22:10:55 +0300

    Add task 101

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,84 @@
+# setup
+
+package { 'nginx':
+  ensure   => installed,
+  provider => 'apt',
+}
+
+file { '/data/web_static/releases/test/index.html':
+  ensure  => 'file',
+  content => '<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>',
+}
+
+file { '/data/web_static/shared':
+  ensure => 'directory',
+}
+
+file { '/data/web_static/current':
+  ensure  => 'link',
+  target  => '/data/web_static/releases/test/',
+  require => File['/data/web_static/releases/test/index.html'],
+}
+
+exec { 'chown':
+  command => 'chown -R ubuntu:ubuntu /data/',
+  path    => ['/bin/', '/usr/bin/'],
+}
+
+# nginx config
+
+
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 http://linktr.ee/firdaus_h_salim/;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'file',
+  content => $nginx_conf,
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+file { 'var/www/html/index.html':
+  ensure  => 'file',
+  content => '<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>',
+}
+
+file { '/var/www/html/404.html':
+  ensure  => 'file',
+  content => "Ceci n'est pas une page\n",
+}
+
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx'],
+}
