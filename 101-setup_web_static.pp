0f4dbae5b7334a33a8aa97151f2cba8193461d45
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-10-20 22:54:17 +0300

    Add taask 101

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -5,23 +5,32 @@ package { 'nginx':
   provider => 'apt',
 }
 
-file { '/data':
+-> file { '/data':
   ensure  => 'directory'
 }
 
-file { '/data/web_static':
+-> file { '/data/web_static':
   ensure => 'directory'
 }
 
-file { '/data/web_static/releases':
+-> file { '/data/web_static/releases':
   ensure => 'directory'
 }
 
-file { '/data/web_static/releases/test':
+-> file { '/data/web_static/releases/test':
   ensure => 'directory'
 }
 
-file { '/data/web_static/releases/test/index.html':
+-> file { '/data/web_static/shared':
+  ensure => 'directory'
+}
+
+-> file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
+}
+
+-> file { '/data/web_static/releases/test/index.html':
   ensure  => 'file',
   content => '<html>
   <head>
@@ -30,16 +39,6 @@ file { '/data/web_static/releases/test/index.html':
     Holberton School
   </body>
 </html>',
-}
-
-file { '/data/web_static/shared':
-  ensure => 'directory',
-}
-
-file { '/data/web_static/current':
-  ensure  => 'link',
-  target  => '/data/web_static/releases/test/',
-  require => File['/data/web_static/releases/test/index.html'],
 }
 
 exec { 'chown':
