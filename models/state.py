fcc729af8b45eece841d2ec963f5dd61661b5581
salmalah <122815714+salmalah@users.noreply.github.com>
2023-10-21 00:14:56 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,30 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
+from models.city import City
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
     """ State class """
     name = ""
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="state",
+                              cascade="all, delete-orphan")
+    else:
+
+        @property
+        def cities(self):
+            """Returns list of City instances with state_id
+            equal to current State.id"""
+
+            from models import storage
+
+            all_cities = storage.all(City)
+            state_cities = []
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    state_cities.append(city)
+            return state_cities
