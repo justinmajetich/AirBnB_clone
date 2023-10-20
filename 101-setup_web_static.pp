f864766318b49bc305b8bc76c235c621cac9bd28
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-10-20 22:39:18 +0300

    Add task 101

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -5,6 +5,22 @@ package { 'nginx':
   provider => 'apt',
 }
 
+file { '/data':
+  ensure  => 'directory'
+}
+
+file { '/data/web_static':
+  ensure => 'directory'
+}
+
+file { '/data/web_static/releases':
+  ensure => 'directory'
+}
+
+file { '/data/web_static/releases/test':
+  ensure => 'directory'
+}
+
 file { '/data/web_static/releases/test/index.html':
   ensure  => 'file',
   content => '<html>
