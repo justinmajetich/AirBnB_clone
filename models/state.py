aad08cba287218949b458b4fb87fd6189edceac3
salmalah <122815714+salmalah@users.noreply.github.com>
2023-10-21 00:22:14 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from sqlalchemy import Column, String, ForeignKey
+"""This is the state class"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
-from os import getenv
+from sqlalchemy import Column, Integer, String
+import models
 from models.city import City
-from models.base_model import BaseModel
+import shlex
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", backref="state",
-                              cascade="all, delete-orphan")
-    else:
+class State(BaseModel, Base):
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
 
-        @property
-        def cities(self):
-            """Returns list of City instances with state_id
-            equal to current State.id"""
-
-            from models import storage
-
-            all_cities = storage.all(City)
-            state_cities = []
-            for city in all_cities.values():
-                if city.state_id == self.id:
-                    state_cities.append(city)
-            return state_cities
+    @property
+    def cities(self):
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
