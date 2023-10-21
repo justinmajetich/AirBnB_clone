dcbb31354d7f86eecbbb6ed0b2eaea0513e70d06
wambui-g <grayciekamau03@gmail.com>
2023-10-21 10:55:12 +0000

    for task 5 delete and add

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         return FileStorage.__objects
+
+    def delete(self, obj=None):
+        """public instance mothod for 5 to delete obj in __objects"""
+        if obj is not None:
+            obj_key = obj.to_dict()['__class__'] + '.' + obj.id
+            if obj_key in self.__objects.keys():
+                del self.__objects[obj_key]
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
