06229820b426d658f34fc780ab7fdf3c93a5cda2
H-Asmaa <asmaehadar32@gmail.com>
2023-10-22 15:28:37 +0100

    Committing a script that returns Python followed by the text given in url

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Importing the flask module
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """A funtion that defines a return value."""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """A function that returns HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def ctext(text):
+    """A function that returns text"""
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def pytext(text):
+    """A function that returns text"""
+    text = text.replace("_", " ")
+    return f"python {text}"
+
+if __name__ == "__main__":
+    app.run()
