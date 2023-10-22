310932069723b5d9dd3530b4d87242cadc531166
jerrynabango <jerrynabango9@gmail.com>
2023-10-22 20:17:07 +0300

    deletes out-of-date archieves

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,36 +1,30 @@
-# Using puppet
+#!/usr/bin/python3
+"""
+A Fabric script (based on the file 3-deploy_web_static.py)
+that deletes out-of-date archives, using the function do_clean
+"""
+import os
+from fabric.api import cd, env, local, run
 
-archieve { 'apt-get-update':
-  command => '/usr/bin/env apt-get -y update',
-}
--> archieve {'nginx':
-  command => '/usr/bin/env apt-get -y install nginx',
-}
+env.hosts = ["34.231.110.206", "3.239.57.196"]
 
--> archieve {'test folder':
-  command => '/usr/bin/env mkdir -p /data/web_static/releases/test/',
-}
 
--> archieve {'shared folder':
-  command => '/usr/bin/env mkdir -p /data/web_static/shared/',
-}
-
--> archieve {'index':
-  command => '/usr/bin/env echo "Welcome to AirBnB" > /data/web_static/releases/test/index.html',
-}
-
--> archieve {'ln -s':
-  command => '/usr/bin/env ln -sf /data/web_static/releases/test /data/web_static/current',
-}
-
--> archieve {'nginx conf':
-  command => '/usr/bin/env sed -i "/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}" /etc/nginx/sites-available/default',
-}
-
--> archieve {'chown:':
-  command => '/usr/bin/env chown -R ubuntu:ubuntu /data',
-}
-
--> archieve {'service':
-  command => '/usr/bin/env service nginx restart',
-}
+def do_clean(number=0):
+    """
+    Deletes out-of-date archives
+    Args:
+        number: is the number of the archives, including the most recent
+    """
+    n = 1 if int(number) == 0 else int(number)
+    files = [f for f in os.listdir('./versions')]
+    files.sort(reverse=True)
+    for f in files[n:]:
+        local("rm -f versions/{}".format(f))
+    remote = "/data/web_static/releases/"
+    with cd(remote):
+        tgz = run(
+            "ls -tr | grep -E '^web_static_([0-9]{6,}){1}$'"
+        ).split()
+        tgz.sort(reverse=True)
+        for d in tgz[n:]:
+            run("rm -rf {}{}".format(remote, d))
