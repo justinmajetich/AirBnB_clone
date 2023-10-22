869aef8c7fd6e376c8327ac793995c6e3083f7ba
akohinmiogbegbile <akohinmi90@gmail.com>
2023-10-22 16:32:48 -0700

    web_flask

---
+++ web_flask/9-states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+
+from flask import Flask, render_template
+from models import *
+from models import storage
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<state_id>', strict_slashes=False)
+def states(state_id=None):
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State")
+    if state_id is not None:
+        state_id = 'State.' + state_id
+    return render_template('9-states.html', states=states, state_id=state_id)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
