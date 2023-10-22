3599cde54b5d2354719b92ee648c9b2b2a6b0122
jerrynabango <jerrynabango9@gmail.com>
2023-10-22 15:34:45 +0300

    rendering

---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+"""
+
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def filters():
+    """
+    Filters the current request
+    """
+    data = {
+        "states": storage.all("State").values(),
+        "amenities": storage.all("Amenity").values(),
+        "places": storage.all("Place").values()
+    }
+    return render_template("100-hbnb.html", models=data)
+
+
+@app.teardown_appcontext
+def tear(exception=None):
+    """
+    Ensures that all the states are closed before returning to the database
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
