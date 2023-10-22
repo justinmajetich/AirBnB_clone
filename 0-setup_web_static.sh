c2dfc80c7b3760210556ea9884690d370a1aaf00
jerrynabango <jerrynabango9@gmail.com>
2023-10-22 16:31:45 +0300

    sets up your web servers for the deployment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,9 +5,9 @@ sudo apt-get update
 sudo apt-get install -y nginx
 sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared
-sudo echo "Welcome to AirBnB" > /data/web_static/releases/test/index.html
+echo "Welcome to AirBnB" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo serve="\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current;\n\t}"
-sudo sed -i "s/^\tserver_name .*;$/&\n$serve/" /etc/nginx/sites-available/default
+serve="\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current;\n\t}"
+sed -i "s/^\tserver_name .*;$/&\n$serve/" /etc/nginx/sites-available/default
 sudo service nginx restart
