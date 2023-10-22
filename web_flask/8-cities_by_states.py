96624ec83f9db60e2e78574159345ca11a9b90bc
EucTech <euccreative@gmail.com>
2023-10-22 22:30:29 +0100

    fixed

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -7,7 +7,7 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route("/cities_by_states", strict_slashes=False)
 def states_list():
     """To print all the cities in the database"""
     states = storage.all(State)
