9a6df328fe98b721dd3e1682b618a9d613467815
Nazzcodek <bellnas09@gmail.com>
2023-10-22 13:59:38 +0100

    advance task

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -5,17 +5,22 @@ This module list all the states in the database
 from models import storage
 from models.state import State
 from models.amenity import Amenity
+from models.place import Place
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb_filters():
     """This method load cities in a state from the storage"""
     states = storage.all(State)
     amenities = storage.all(Amenity)
-    return render_template("10-hbnb_filters.html", states=states, amenities=amenities)
+    places = storage.all(Place)
+    return render_template("100-hbnb.html",
+                           states=states,
+                           amenities=amenities,
+                           places=places)
 
 
 @app.teardown_appcontext
