8201bda39fed6c3ebd6d7fec0dad6f7c94c12c5f
jerrynabango <jerrynabango9@gmail.com>
2023-10-22 11:02:20 +0300

    states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application:
+"""
+
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states():
+    """
+    /states_list: display a HTML page:
+    """
+    states = storage.all("State").values()
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def close_db(exception=None):
+    """
+    Ensures that all the states are closed before returning to the database
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
