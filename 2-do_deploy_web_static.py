0f1ff16fda73f3bfa659d2c3a125472e0d33a19e
jerrynabango <jerrynabango9@gmail.com>
2023-10-22 17:47:23 +0300

    fab script distributes archieve to web server

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,41 +1,41 @@
 #!/usr/bin/python3
 """
-Fabric script (based on the file 1-pack_web_static.py)
-that distributes an archive to your web servers, using the function do_deploy
+Deploy archive!
 """
 from os import path
 from fabric.api import env, put, run
 
-env.hosts = ["34.231.110.206", "3.239.57.196"]
+env.hosts = ["54.167.24.215", "54.82.159.235"]
 
 
 def do_deploy(archive_path):
     """
-    Distributes archives to web servers
+    Fabric script that distributes an archive to your web servers,
+    using the function do_deploy
     """
+
     if not path.exists(archive_path):
         return False
-    compressedFile = archive_path.split("/")[-1]
-    fileName = compressedFile.split(".")[0]
-    upload_path = "/tmp/{}".format(compressedFile)
-    if put(archive_path, upload_path).failed:
+    try:
+        file = archive_path.split("/")[-1]
+        print(file)
+
+        archieve = file.split(".")[0]
+        print(archieve)
+
+        location = "/data/web_static/releases/" + archieve
+        put(archive_path, '/tmp/')
+        run("mkdir -p /data/web_static/releases/{}/".format(archieve))
+        run("tar -zxvf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(file, archieve))
+        run("rm /tmp/{}".format(file))
+        run("mv /data/web_static/releases/{}/web_static/*\
+            /data/web_static/releases/{}/".format(archieve, archieve))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(archieve))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(archieve))
+        return True
+
+    except Exception:
         return False
-    current_release = '/data/web_static/releases/{}'.format(fileName)
-    if run("rm -rf {}".format(current_release)).failed:
-        return False
-    if run("mkdir -p {}".format(current_release)).failed:
-        return False
-    uncompress = "tar -xzf /tmp/{} -C {}".format(
-        compressedFile, current_release
-    )
-    if run(uncompress).failed:
-        return False
-    delete_archive = "rm -f /tmp/{}".format(compressedFile)
-    if run(delete_archive).failed:
-        return False
-    if run("rm -rf /data/web_static/current").failed:
-        return False
-    relink = "ln -s {} /data/web_static/current".format(current_release)
-    if run(relink).failed:
-        return False
-    return True
