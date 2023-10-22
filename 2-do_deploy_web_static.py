a7eacbe7cdd6038062b069adec3c454efd796c17
jerrynabango <jerrynabango9@gmail.com>
2023-10-22 17:13:05 +0300

    fab script distributes archieve to web server

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,6 +7,7 @@ from os import path
 
 env.hosts = ["34.231.110.206", "3.239.57.196"]
 
+
 def do_deploy(archive_path):
     """
     Fabric script (based on the file 1-pack_web_static.py) that distributes an
@@ -14,18 +15,26 @@ def do_deploy(archive_path):
     """
     if not path.exists(archive_path):
         return False
+    try:
+        path = archive_path.split("/")[-1]
+        print(path)
 
-    archieve = archive_path.split("/")[-1]
-    files = archieve.split(".")[0]
-    file_loading = f"/tmp/{archieve}"
+        archieve = path.split(".")[0]
+        print(archieve)
 
-    if put(archive_path, file_loading).failed or \
-        run(f"rm -rf /data/web_static/releases/{files}").failed or \
-        run(f"mkdir -p /data/web_static/releases/{files}").failed or \
-        run(f"tar -xzf /tmp/{archieve} -C /data/web_static/releases/{files}").failed or \
-        run(f"rm -f /tmp/{archieve}").failed or \
-        run("rm -rf /data/web_static/current").failed or \
-        run(f"ln -s /data/web_static/releases/{files} /data/web_static/current").failed:
+        file_path = "/data/web_static/releases/" + archieve
+        put(archive_path, '/tmp/')
+        run("mkdir -p /data/web_static/releases/{}/".format(archieve))
+        run("tar -zxvf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(path, archieve))
+        run("rm /tmp/{}".format(path))
+        run("mv /data/web_static/releases/{}/web_static/*\
+            /data/web_static/releases/{}/".format(archieve, archieve))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(archieve))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(archieve))
+        return True
+
+    except Exception:
         return False
-
-    return True
