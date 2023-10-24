23a9d6842d2ebd73862fb442570be87db687855a
SavageLM <nagol.egavas@gmail.com>
2023-10-23 22:47:27 -0500

    Added db_storage to engine file, to be updated later

---
+++ models/engine/db_storage.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+"""Defines a new class DBStorage"""
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
