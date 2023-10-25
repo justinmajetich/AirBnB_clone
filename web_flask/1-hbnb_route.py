11b4c65884853aec4f2214b2ae1cd3fd915c5d8c
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-10-26 00:54:14 +0300

    Add task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,6 +12,11 @@ def hello_hbnb():
     """ Displays 'Hello HBNB!' """
     return 'Hello HBNB!'
 
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Displays 'HBNB' """
+    return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
