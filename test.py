63a8a6bca6de4abd25cb7f9a57fb45f4804f1478
Zal-atan <109318907+Zal-atan@users.noreply.github.com>
2023-10-26 14:30:07 -0500

    Holding Changes so i can pull

--- test.py
+++ test.py
@@ -3,9 +3,17 @@
 if __name__ == "__main__":
     strings = input("Please type in inputs\n")
     string_list = strings.split()
-    print(string_list)
     for i in range(len(string_list)):
-        kvp = string_list[i].split("=")
+        kvp = string_list[i].partition("=")
         key = kvp[0]
-        value = kvp[1]
+        value = kvp[2]
         print(f"{key}: {value:}")
+        if value[0] == '"':
+            value = value[1:-1]
+            print(f"New Value: {value}")
+        elif '.' in value:
+            value = float(value)
+        else:
+            value = int(value)
+
+        # print(type(value))
