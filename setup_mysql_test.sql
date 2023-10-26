c0f27aa55e41d21cf8a5968b2532d4b06503414c
Jagger Van Winkle <JaggerVanW@gmail.com>
2023-10-26 15:07:01 -0500

    Fixed grant privileges

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,5 +2,5 @@
 -- that grants all privileges to hbnb_test_db and only SELECT on performance_schema
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
