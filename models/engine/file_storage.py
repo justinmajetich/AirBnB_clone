0ea1f5d36187aa08e643ed6cde29b2d924b581d3
tiffanywalker22 <tbcasper16@gmail.com>
2023-10-26 11:32:27 -0700

    adding delete obj

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,7 +8,7 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         return FileStorage.__objects
 
@@ -48,3 +48,10 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+    def delete(self, obj=None):
+        """Deletes object from __objects if it exists"""
+        if obj is not None:
+            key = f"{type(obj).__name}.{obj.id}"
+            if key in self.__objects:
+                del self.__objects[key]
+                self.save()
