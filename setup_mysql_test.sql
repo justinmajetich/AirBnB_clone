1b5ac6874f089c918ed6965a2c5a3c882b74ca9f
michaellgans <michaellgans@Victoria>
2023-10-26 08:36:56 -0500

    M 1:42PM - Adds timestamps

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,6 @@
 -- Prepares MySQL test server for AirBNB
+-- Michael editted 1:40 PM
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON perfomance_schema.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+CREATE USER IF NOT EXISTS "hbnb_test"@"localhost" IDENTIFIED BY "hbnb_test_pwd";
+GRANT ALL PRIVILEGES ON perfomance_schema.* TO "hbnb_test"@"localhost";
+GRANT SELECT ON hbnb_test_db.* TO "hbnb_test"@"localhost";
