89d6929f12e395f167f9bab0decf00e4eff5be1e
mokasp <kasperbouldin@gmail.com>
2023-10-26 13:25:53 -0500

    K 1:25 - fixing git issues

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,5 @@
 -- Prepares MySQL Server for AirBnB
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS "hbnb_dev"@"localhost" IDENTIFIED BY "hbnb_dev_pwd";
+CREATE USER IF NOT EXISTS "hbnb_dev"@"localhost" IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT SELECT ON performance_schema.* TO "hbnb_dev"@"localhost";
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO "hbnb_dev"@"localhost";
