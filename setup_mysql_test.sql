83c190df41000f980dd4f4a16c38cd56d9e901fd
Jagger Van Winkle <JaggerVanW@gmail.com>
2023-10-26 15:04:57 -0500

    MySQL setup test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,6 @@
+-- Creates the database of hbnb_test_db with the user hbnb_dev
+-- that grants all privileges to hbnb_test_db and only SELECT on performance_schema
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
