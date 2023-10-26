6978816f5d8bce3ac9a90df6e29288149d9cd9a9
michaellgans <michaellgans@Victoria>
2023-10-26 07:10:17 -0500

    M - Edits dev

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,6 @@
 -- Prepares MySQL Server for AirBnB
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS "hbnb_dev"@"localhost" IDENTIFIED BY hbnb_dev_pwd;
+CREATE USER IF NOT EXISTS "hbnb_dev"@"localhost"
+SET PASSWORD FOR "hbnb_dev"@"localhost" = "hbnb_dev_pwd"
 GRANT ALL PRIVILEGES ON hbnb_dev_db TO "hbnb_dev"@"localhost";
 GRANT SELECT ON performance_schema TO "hbnb_dev"@"localhost";
-FLUSH PERMISSIONS;
