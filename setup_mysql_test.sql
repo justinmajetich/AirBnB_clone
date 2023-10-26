f439bce3d771e169f2f84b0590c95987c97d6ea9
michaellgans <michaellgans@Victoria>
2023-10-26 08:46:09 -0500

    M 1:50 PM - edits test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,6 @@
--- Prepares MySQL test server for AirBNB
--- Michael editted 1:40 PM
+-- Prepares MySQL Server for AirBnB
+-- Michael editted 1:50 PM
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS "hbnb_test"@"localhost" IDENTIFIED BY "hbnb_test_pwd";
-GRANT ALL PRIVILEGES ON perfomance_schema.* TO "hbnb_test"@"localhost";
-GRANT SELECT ON hbnb_test_db.* TO "hbnb_test"@"localhost";
+GRANT SELECT ON performance_schema.* TO "hbnb_test"@"localhost";
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO "hbnb_test"@"localhost";
