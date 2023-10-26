0ea7381680c6dd14f8749efd73f3038c5b077f11
Jagger Van Winkle <JaggerVanW@gmail.com>
2023-10-26 15:02:09 -0500

    MySQL setup

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,6 @@
+-- Creates the database of hbnb_dev_db with the user hbnb_dev
+-- that grants all privileges to hbnb_dev_db and only SELECT on performance_schema
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
