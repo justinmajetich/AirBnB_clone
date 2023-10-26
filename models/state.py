743b492eb274acf849f898ec1c73c91a854a039a
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-26 13:37:47 -0700

    task 6 written, not yet tested - lots of errors because previous changes were not yet merged

--- models/state.py
+++ models/state.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="state", cascade="all, delete-orphan")
+    if getenv("HBNB_TYPE_STORAGE") == "file":
+        @property
+        def cities(self):
+            """returns the list of City instances"""
+            from models import storage
+            return [city for city in storage.all("City").values() if city.state_id == self.id]
