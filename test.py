2b5593311c7392a4cd4fe717c01184cc43e853c9
Zal-atan <109318907+Zal-atan@users.noreply.github.com>
2023-10-26 14:07:34 -0500

    Fixed authors

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     string_list = strings.split()
     print(string_list)
     for i in range(len(string_list)):
-        kvp = string_list[i].partition("=")
+        kvp = string_list[i].split("=")
         key = kvp[0]
-        value = kvp[2]
-        print(f"{key:}: {value:}")
+        value = kvp[1]
+        print(f"{key}: {value:}")
