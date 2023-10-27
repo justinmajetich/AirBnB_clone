b8257468e86e9adf3d6e50939499dc5d1aa8362c
ally2211 <amendieta.va@gmail.com>
2023-10-26 23:26:01 -0700

    create State name="California"... "

--- models/state.py
+++ models/state.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from datetime import datetime
 
 
 class State(BaseModel):
-    """ State class """
-    name = ""
+    """ 
+    State class
+    need to explicitly declare updated_at and created_at
+    because to_dict is overriding the to_dict in BaseModel
+    """
+    created_at = datetime.now()
+    updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """ Initializes a new instance of State """
+        super().__init__(*args, **kwargs)
+        self.name = ""
+
+    def to_dict(self):
+        """Convert instance into dict format, including the name attribute"""
+        dictionary = super().to_dict()
+        dictionary['name'] = self.name
+        return dictionary
