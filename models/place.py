1a75fb9eac226a7b964daeab037e5c3fba1b59b9
H-Asmaa <asmaehadar32@gmail.com>
2023-10-27 09:41:23 +0100

    Committing place class modification of the many to many relationship

--- models/place.py
+++ models/place.py
@@ -8,7 +8,24 @@ import os
 
 STORAGE = os.getenv("HBNB_TYPE_STORAGE")
 
-
+place_amenity = Table(
+    "place_amenity",
+    Base.metadata,
+    Column(
+        "place_id",
+        String(60),
+        ForeignKey("places.id"),
+        primary_key=True,
+        nullable=False,
+    ),
+    Column(
+        "amenity_id",
+        String(60),
+        ForeignKey("amenities.id"),
+        primary_key=True,
+        nullable=False,
+    )
+)
 class Place(BaseModel, Base):
     """A place to stay"""
 
@@ -30,25 +47,6 @@ class Place(BaseModel, Base):
                             delete-orphan",
             backref="place",
         )
-        place_amenity = Table(
-            "place_amenity",
-            Base.metadata,
-            Column(
-                "place_id",
-                String(60),
-                ForeignKey("places.id"),
-                primary_key=True,
-                nullable=False,
-            ),
-            Column(
-                "amenity_id",
-                String(60),
-                ForeignKey("amenities.id"),
-                primary_key=True,
-                nullable=False,
-            ),
-        )
-
         amenities = relationship(
             "Amenity", secondary=place_amenity,
             viewonly=False, backref="places"
