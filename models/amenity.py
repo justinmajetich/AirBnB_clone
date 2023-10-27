30a608e952d49467ac6a4016b65bfb6158c21613
Your Name <you@example.com>
2023-10-27 12:25:21 -0700

    added to save function on base_model, and added update function to init

--- models/amenity.py
+++ models/amenity.py
@@ -4,4 +4,11 @@ from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    name = ""
+    """This is the class for Amenity
+    Attributes:
+        name: input name
+    """
+    __tablename__ = 'amenities'
+
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", backref="amenities")
--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
+import models
 from datetime import datetime
 
 
@@ -30,6 +31,7 @@ class BaseModel:
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
+	storage.update(self)
         self.updated_at = datetime.now()
         storage.save()
 
