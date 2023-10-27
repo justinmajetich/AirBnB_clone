8518f3137073c0d2ff442d1e599b6b39e29ea7ef
tiffanywalker22 <tbcasper16@gmail.com>
2023-10-27 08:17:35 -0700

    updating basemodel and user

--- models/base_model.py
+++ models/base_model.py
@@ -2,12 +2,20 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy import Column, String
 
+
+Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(string(60)nullable=False, unique=True)
+    created_at = Column(datetime), nullable=False, default = datetime.utcnow()
+    updated_at = Column(datetime), nullable= False, default = datetime.utcnow()
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
+
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
--- models/user.py
+++ models/user.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
+__tablename__= users
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    email = Column(string(128), nullable=False)
+    password = Column(string(128), nullable=False)
+    first_name = Column(string(128), nullable=False)
+    last_name = Column(string(128), nullable=False)
