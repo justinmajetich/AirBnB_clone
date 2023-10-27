d6d0ca60a7b787ed8e5b0b762fdc66472c31cd46
Zal-Atan <ethanzalta@gmail.com>
2023-10-27 10:27:57 -0500

    Added most updates to State, relationship with city

--- models/city.py
+++ models/city.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
     name = Column(string(128), nullable=False)
--- models/state.py
+++ models/state.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import models
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
+
+    #if not db storage? FIX THIS!!!
+    @property
+    def cities(self):
+        list_of_cities = []
+        for object in models.storage.all(City).values():
+            if object.state_id == self.id:
+                list_of_cities.append(object)
+        return object
