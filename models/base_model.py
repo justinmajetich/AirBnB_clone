6bc8c6c6fb6a640afa5687acd4b9d15648d5d132
cpmoore0317 <cpmoore317@gmail.com>
2023-10-27 12:44:33 -0500

    Base model changs for to_dict

--- models/base_model.py
+++ models/base_model.py
@@ -45,12 +45,14 @@ class BaseModel:
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = {}
+        dictionary = self.__dict__.copy()
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if ('_sa_instance_state' in dictionary):
+            del dictionary['_sa_instance_state']
         return dictionary
 
     def delete(self):
