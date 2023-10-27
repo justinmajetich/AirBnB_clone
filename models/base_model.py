b591481301dd05a5182b98186ae4859606c97e93
cpmoore0317 <cpmoore317@gmail.com>
2023-10-27 11:40:01 -0500

    Adding branch and changes to basemodel & place

--- models/base_model.py
+++ models/base_model.py
@@ -2,16 +2,17 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 
 
 Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
-    id = Column(string(60)nullable=False, unique=True)
-    created_at = Column(datetime), nullable=False, default = datetime.utcnow()
-    updated_at = Column(datetime), nullable= False, default = datetime.utcnow()
+    id = Column(String(60), nullable=False, unique=True)
+    created_at = Column(datetime, nullable=False, default = datetime.utcnow())
+    updated_at = Column(datetime, nullable= False, default = datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
@@ -21,12 +22,13 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            for key, val in kwargs.items():
+                if key in ("created_at", "updated_at"):
+                    val = datetime.strptime(kwargs['updated_at'], 
+                                            '%Y-%m-%dT%H:%M:%S.%f')
+
+                if "__class__" not in key:
+                    setattr(self, key, val)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -52,4 +54,5 @@ class BaseModel:
 
     def delete(self):
         """ Delete"""
-        model.storage.delete()
+        from models import storage
+        storage.delete()
