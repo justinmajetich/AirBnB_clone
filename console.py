86ee57f0b597db555b776fb5b55021fd9eddd6d7
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2023-10-26 20:11:15 -0500

    Minor fix for do_create syntax and spacing

--- console.py
+++ console.py
@@ -140,6 +140,7 @@ class HBNBCommand(cmd.Cmd):
         storage.save()
         print(new_instance.id)
 
+
     def help_create(self):
         """ Help information for the create method """
         print("Creates a class of any type")
