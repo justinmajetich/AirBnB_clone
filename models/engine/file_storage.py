c4a8cfde1d53d6ba14272e807e8d69bf0f8f6553
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-10-27 12:25:25 -0500

    updated `def new` in file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,7 +18,9 @@ class FileStorage:
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
 
     def save(self):
         """Saves storage dictionary to file"""
