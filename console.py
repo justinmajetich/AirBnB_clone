6a53d0848778927ecabd6bd3b42fa41446d88c6e
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2023-10-26 20:27:17 -0500

    Final test for do_create tonight

--- console.py
+++ console.py
@@ -137,10 +137,8 @@ class HBNBCommand(cmd.Cmd):
                 value = int(value)
             setattr(new_instance, key, value)
 
-
         storage.save()
         print(new_instance.id)
-
 
     def help_create(self):
         """ Help information for the create method """
