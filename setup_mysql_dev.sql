e020c49d803d2f4a6203ba470f242ed240e7dde5
Moonwalkert3ch <6620@holbertonstudents.com>
2023-10-28 16:42:51 -0700

    created db and usr

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,7 @@
+-- Creates a db hbnb_dev_db with user hbnb_dev in localhost --
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY hbnb_dev_pwd;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
