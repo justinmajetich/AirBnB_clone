68fcc13556f08c6460a2a58d96a58bc90d10f83e
mokasp <kasperbouldin@gmail.com>
2023-10-28 13:46:42 -0500

    K 1:46pm - SQL mapping for class attributes

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+# KASPER edited 1:45pm 10/28/2023
+from models.base_model import BaseModel, Base
+from sqlalchemy import (
+    Column,
+    String
+)
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    # place_amenities = ""  update in task 10
