bfb60ffc050283f40f734f6742b3bbf6d6e0f5c2
LeeWest89 <6683@holbertonstudents.com>
2023-10-29 07:36:45 -0700

    fixed flake8 issues in file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 """File storage class for AirBnBV2"""
 import json
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
 
 
 class FileStorage:
