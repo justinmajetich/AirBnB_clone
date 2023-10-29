d2e2344b83401812ccb42929491cb9591a1f2641
mrktptn <marktipton95@gmail.com>
2023-10-28 20:42:18 -0500

    tried importing models into __init__.py file.

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,12 @@
 """This module instantiates an object of class FileStorage or DBStorage"""
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.review import Review
+from models.state import State
+from models.user import User
 from os import getenv
 
 Env = getenv('HBNB_TYPE_STORAGE')
