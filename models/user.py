d9524e08c2b31dff270deca42e568adc9f939192
tiffanywalker22 <tbcasper16@gmail.com>
2023-10-30 12:20:58 -0700

    updating amenity and fixing even more errors

--- models/user.py
+++ models/user.py
@@ -3,23 +3,13 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import relationship
-from os import getenv
-
-HBNB_TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__= 'users'
-    if HBNB_TYPE_STORAGE == "db":
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
-        places = relationship("Place", backref="user", cascade="delete")
-        reviews = relationship("Review", backref="user", cascade="delete")
-
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
