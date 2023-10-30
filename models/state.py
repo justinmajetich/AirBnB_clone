e29ff27b23cb89a8ecb7c445c96090ec9c4b8dc5
chris85gillis <chris85gillis@gmail.com>
2023-10-30 11:34:09 -0700

    added to file updates to file state and init and created file db_storgage

--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import models
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+
+    name = Column(String(128), nullable=False)
+
+    # For DBStorage
+    if models.storage_type == "db":
+        cities = relationship("City", backref="state", cascade="all, delete-orphan")
+
+    # For FileStorage
+    if models.storage_type == "file":
+        @property
+        def cities(self):
+            city_list = []
+            for city_id, city in models.storage.all("City").items():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
