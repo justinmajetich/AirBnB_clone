282cfdcb5dc1840d4b719e342afa17021aaefb6f
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2023-10-30 17:17:00 -0500

    Update state.py

--- models/state.py
+++ models/state.py
@@ -3,11 +3,25 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import models
+from os import getenv
+from models.city import City
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state",
-                        cascade="all, delete-orphan")
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+            cities = relationship(
+                "City", backref="state", cascade="all, delete"
+            )
+        else:
+            @property
+            def cities(self):
+                """returns list of City instances upon state_id"""
+                cities_list = []
+                for obj in list(models.storage.all(City).values()):
+                    if obj.state_id == self.id:
+                        cities_list.append(obj)
+                return cities_list
