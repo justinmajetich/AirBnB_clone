79be8761537dc5c3f7c6124345fc62dfc0f85b30
root <abrielleperry22@icloud.com>
2023-10-30 14:36:19 -0700

    changes to basc all

--- models/city.py
+++ models/city.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import Base
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship, sessionmaker
 
 
-class City(Base):
+class City(BaseModel, Base):
     __tablename__ = 'cities'
     id = Column(String(60), primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
--- models/review.py
+++ models/review.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
-    __tablename__ == "reviews"
+    __tablename__ = "reviews"
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     text = Column(String(1024), nullable=False)
