ccde93ee4737b503705df1a3155f8d055f5e8cdc
root <abrielleperry22@icloud.com>
2023-10-30 12:47:13 -0700

    fixed ws

--- models/state.py
+++ models/state.py
@@ -10,4 +10,4 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state",
-                          cascade="all, delete-orphan")
+                        cascade="all, delete-orphan")
--- models/user.py
+++ models/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import Base
 
 
 class User(BaseModel):
