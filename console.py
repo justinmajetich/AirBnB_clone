91ddf33c8d698ebd81b34fb5a47be21cd2241e39
matternstsr <matternstsr@gmail.com>
2023-10-30 13:27:57 -0500

    cleaned up test teardown and the imports fixed console file after messing that up

--- console.py
+++ console.py
@@ -1,108 +1,327 @@
 #!/usr/bin/python3
-''' Tests for console'''
-
+"""Console for HBnB project."""
+import cmd
+import json
+from shlex import split
+from datetime import datetime
+from models.__init__ import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 import sys
-import unittest
-import pep8
-from io import StringIO
-from unittest.mock import patch
-import os
-from console import HBNBCommand
 
-class TestConsole(unittest.TestCase):
-    ''' Testing the console'''
 
-    @classmethod
-    def setUpClass(cls):
-        """setup testing"""
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) ' if sys.__stdin__.isatty() else ''
+
+    classes = {
+        'BaseModel': BaseModel, 'User': User, 'Place': Place,
+        'State': State, 'City': City, 'Amenity': Amenity,
+        'Review': Review
+    }
+
+    dot_cmds = ['all', 'count', 'show', 'destroy', 'update']
+
+    types = {
+        'number_rooms': int, 'number_bathrooms': int,
+        'max_guest': int, 'price_by_night': int,
+        'latitude': float, 'longitude': float
+    }
+
+    def preloop(self):
+        if not sys.__stdin__.isatty():
+            print('(hbnb)')
+
+    def precmd(self, line):
+        if '(' not in line or ')' not in line:
+            return line
+
+        cmd_class = line.split('(')
+        if len(cmd_class) != 2:
+            return line
+
+        command = cmd_class[0]
+        args = cmd_class[1].replace(')', '')
+
+        if '.' not in command:
+            return line
+
         try:
-            os.rename("file.json", "tmp")
-        except IOError:
-            pass
-        cls.HBNB = HBNBCommand()
+            class_name, command = command.split('.')
+            if class_name not in HBNBCommand.classes:
+                return line
 
-    @classmethod
-    def tearDownClass(cls):
-        """testing teardown"""
+            if command not in HBNBCommand.dot_cmds:
+                return line
+
+            if args.startswith('{') and args.endswith('}'):
+                try:
+                    args_dict = json.loads(args)
+                except ValueError:
+                    return line
+            else:
+                args_list = split(args)
+                args_dict = dict(item.split('=') for item in args_list)
+
+            obj = storage.all().get(class_name + '.' + args_dict.get('id'))
+            if obj is None:
+                return line
+
+            setattr(obj, 'created_at', datetime.strptime(
+                obj.created_at, '%Y-%m-%dT%H:%M:%S.%f'))
+            setattr(obj, 'updated_at', datetime.strptime(
+                obj.updated_at, '%Y-%m-%dT%H:%M:%S.%f'))
+
+            if command == 'update':
+                setattr(obj, **args_dict)
+                obj.save()
+
+            elif command == 'show':
+                print(obj)
+
+            elif command == 'destroy':
+                storage.delete(obj.id)
+                storage.save()
+
+            elif command == 'all':
+                obj_list = []
+                for key, value in storage.all().items():
+                    if class_name in key:
+                        obj_list.append(value.__str__())
+                print(obj_list)
+
+            elif command == 'count':
+                count = 0
+                for key in storage.all():
+                    if class_name in key:
+                        count += 1
+                print(count)
+
+        except Exception:
+            return line
+
+    def postcmd(self, stop, line):
+        if not sys.__stdin__.isatty():
+            print('(hbnb) ', end='')
+        return stop
+
+    def emptyline(self):
+        pass
+
+    def do_quit(self, line):
+        """Quit command to exit the program"""
+        return True
+
+    def do_create(self, args):
         try:
-            os.rename("tmp", "file.json")
-        except IOError:
-            pass
-        del cls.HBNB
+            if not args:
+                raise SyntaxError("** class name missing **")
+            my_args = args.split(" ")
 
-    def test_pep8_console(self):
-        """Pep8 console.py"""
-        style = pep8.StyleGuide(quiet=False)
-        errors = 0
-        file = (["console.py"])
-        errors += style.check_files(file).total_errors
-        self.assertEqual(errors, 0, 'Need to fix Pep8')
+            kwargs = {}
+            for i in range(1, len(my_args)):
+                key, value = tuple(my_args[i].split("="))
+                if value == '=':
+                    value = value.strip("").replace("_", " ")
+                else:
+                    try:
+                        value = eval(value)
+                    except (SyntaxError, NameError):
+                        continue
+                kwargs[key] = value
+            if kwargs == {}:
+                obj = eval(my_args[0])()
+            else:
+                obj = eval(my_args[0])(**kwargs)
+                storage.new(obj)
+            print(obj.id)
+            obj.save()
+        except SyntaxError:
+            print("** class name missing **")
+        except NameError:
+            print("** class doesn't exist **")
 
-    def setUp(self):
-        """ setup """
-        self.backup = sys.stdout
-        self.capt_out = StringIO()
-        sys.stdout = self.capt_out
+    def help_create(self):
+        print("Creates a class of any type")
+        print("[Usage]: create <className>\n")
 
-    def tearDown(self):
-        """ tearDown """
-        sys.stdout = self.backup
+    def do_show(self, args):
+        new = args.partition(" ")
+        c_name = new[0]
+        c_id = new[2]
 
-    def test_EOF(self):
-        """Test that EOF quits."""
-        with patch("sys.stdout", new=StringIO()) as f:
-            self.assertTrue(self.HBNB.onecmd("EOF"))
+        if c_id and ' ' in c_id:
+            c_id = c_id.partition(' ')[0]
 
-    def test_emptyline(self):
-        """Test empty line input."""
-        with patch("sys.stdout", new=StringIO()) as f:
-            self.HBNB.onecmd("\n")
-            self.assertEqual("", f.getvalue())
+        if not c_name:
+            print("** class name missing **")
+            return
 
-    def test_quit(self):
-        """Test quit command input."""
-        with patch("sys.stdout", new=StringIO()) as f:
-            self.HBNB.onecmd("quit")
-            self.assertEqual("", f.getvalue())
+        if c_name not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
 
-    def test_create_errors(self):
-        """Test create command errors."""
-        with patch("sys.stdout", new=StringIO()) as f:
-            self.HBNB.onecmd("create")
-            self.assertEqual(
-                "** class name missing **\n", f.getvalue())
+        if not c_id:
+            print("** instance id missing **")
+            return
 
-        with patch("sys.stdout", new=StringIO()) as f:
-            self.HBNB.onecmd("create asdfsfsd")
-            self.assertEqual(
-                "** class doesn't exist **\n", f.getvalue())
+        key = c_name + "." + c_id
+        try:
+            print(storage.all()[key])
+        except KeyError:
+            print("** no instance found **")
 
-    def test_show(self):
-        """Test show command."""
-        with patch("sys.stdout", new=StringIO()) as f:
-            self.HBNB.onecmd("show")
-            self.assertEqual(
-                "** class name missing **\n", f.getvalue())
-        with patch("sys.stdout", new=StringIO()) as f:
-            self.HBNB.onecmd("show asdfsdrfs")
-            self.assertEqual(
-                "** class doesn't exist **\n", f.getvalue())
-        with patch("sys.stdout", new=StringIO()) as f:
-            self.HBNB.onecmd("show BaseModel")
-            self.assertEqual(
-                "** instance id missing **\n", f.getvalue())
-        with patch("sys.stdout", new=StringIO()) as f:
-            self.HBNB.onecmd("show BaseModel abcd-123")
-            self.assertEqual(
-                "** no instance found **\n", f.getvalue())
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.HBNB.onecmd("safdsa.show()")
-            self.assertEqual(
-                "** class doesn't exist **\n", f.getvalue())
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.HBNB.onecmd("BaseModel.show(abcd-123)")
-            self.assertEqual(
-                "** no instance found **\n", f.getvalue())
+    def help_show(self):
+        print("Shows an individual instance of a class")
+        print("[Usage]: show <className> <objectId>\n")
+
+    def do_destroy(self, args):
+        new = args.partition(" ")
+        c_name = new[0]
+        c_id = new[2]
+        if c_id and ' ' in c_id:
+            c_id = c_id.partition(' ')[0]
+
+        if not c_name:
+            print("** class name missing **")
+            return
+
+        if c_name not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+
+        if not c_id:
+            print("** instance id missing **")
+            return
+
+        key = c_name + "." + c_id
+
+        try:
+            del (storage.all()[key])
+            storage.save()
+        except KeyError:
+            print("** no instance found **")
+
+    def help_destroy(self):
+        print("Destroys an individual instance of a class")
+        print("[Usage]: destroy <className> <objectId>\n")
+
+    def do_all(self, args):
+        print_list = []
+
+        objects = storage.all()
+        print_list = []
+        if not args:
+            for key in objects:
+                print_list.append(objects[key])
+            print(print_list)
+            return
+        try:
+            args = args.split(" ")
+            if args[0] not in self.classes:
+                raise NameError()
+            for key in objects:
+                name = key.split('.')
+                if name[0] == args[0]:
+                    print_list.append(objects[key])
+            print(print_list)
+        except NameError:
+            print("** class doesn't exist **")
+
+    def help_all(self):
+        print("Shows all objects, or all of a class")
+        print("[Usage]: all <className>\n")
+
+    def do_count(self, args):
+        count = 0
+        for k, v in storage.all().items():
+            if args in k:
+                count += 1
+        print(count)
+
+    def help_count(self):
+        print("Counts the number of instances for a class")
+
+    def do_update(self, args):
+        c_name = c_id = att_name = att_val = kwargs = ''
+
+        args = args.partition(" ")
+        if args[0]:
+            c_name = args[0]
+        else:
+            print("** class name missing **")
+            return
+        if c_name not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+
+        args = args[2].partition(" ")
+        if args[0]:
+            c_id = args[0]
+        else:
+            print("** instance id missing **")
+            return
+
+        key = c_name + "." + c_id
+
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+
+        if '{' in args[2] and '}' in args[2] and type(eval(args[2])) is dict:
+            kwargs = eval(args[2])
+            args = []
+            for k, v in kwargs.items():
+                args.append(k)
+                args.append(v)
+        else:
+            args = args[2]
+            if args and args[0] == '\"':
+                second_quote = args.find('\"', 1)
+                att_name = args[1:second_quote]
+                args = args[second_quote + 1:]
+
+            args = args.partition(' ')
+
+            if not att_name and args[0] != ' ':
+                att_name = args[0]
+            if args[2] and args[2][0] == '\"':
+                att_val = args[2][1:args[2].find('\"', 1)]
+
+            if not att_val and args[2]:
+                att_val = args[2].partition(' ')[0]
+
+            args = [att_name, att_val]
+
+        new_dict = storage.all()[key]
+
+        for i, att_name in enumerate(args):
+            if (i % 2 == 0):
+                att_val = args[i + 1]
+                if not att_name:
+                    print("** attribute name missing **")
+                    return
+                if not att_val:
+                    print("** value missing **")
+                    return
+                if att_name in HBNBCommand.types:
+                    att_val = HBNBCommand.types[att_name](att_val)
+                new_dict.__dict__.update({att_name: att_val})
+                new_dict.save()
+
+    def help_update(self):
+        print("Updates an object with new information")
+        print("Usage: update <className> <id> <attName> <attVal>\n")
+
+    def do_EOF(self, line):
+        """Handle the EOF (Ctrl+D) command to exit the CLI."""
+        print()  # Print a newline for a cleaner exit
+        return True  # Return True to exit the CLI
+
 
 if __name__ == "__main__":
-    unittest.main()
+    HBNBCommand().cmdloop()
--- tests/test_console.py
+++ tests/test_console.py
@@ -7,16 +7,10 @@ import pep8
 from io import StringIO
 from unittest.mock import patch
 import os
-from models.__init__ import DBStorage
 from console import HBNBCommand
-import models
-from io import StringIO
-import models
-from models.engine.file_storage import FileStorage
 
-class test_console(unittest.TestCase):
-    ''' Testing the console'''    
-    """Check for Pep8 style conformance"""
+class TestConsole(unittest.TestCase):
+    ''' Testing the console'''
 
     @classmethod
     def setUpClass(cls):
@@ -35,8 +29,6 @@ class test_console(unittest.TestCase):
         except IOError:
             pass
         del cls.HBNB
-        if type(models.storage) == DBStorage:
-            models.storage._DBStorage__session.close()
 
     def test_pep8_console(self):
         """Pep8 console.py"""
@@ -45,7 +37,7 @@ class test_console(unittest.TestCase):
         file = (["console.py"])
         errors += style.check_files(file).total_errors
         self.assertEqual(errors, 0, 'Need to fix Pep8')
-   
+
     def setUp(self):
         """ setup """
         self.backup = sys.stdout
@@ -55,76 +47,36 @@ class test_console(unittest.TestCase):
     def tearDown(self):
         """ tearDown """
         sys.stdout = self.backup
-    
+
     def test_EOF(self):
         """Test that EOF quits."""
         with patch("sys.stdout", new=StringIO()) as f:
-            self.assertTrue(self.onecmd("EOF"))
+            self.assertTrue(self.HBNB.onecmd("EOF"))
 
     def test_emptyline(self):
         """Test empty line input."""
         with patch("sys.stdout", new=StringIO()) as f:
             self.HBNB.onecmd("\n")
             self.assertEqual("", f.getvalue())
-    
+
     def test_quit(self):
         """Test quit command input."""
         with patch("sys.stdout", new=StringIO()) as f:
             self.HBNB.onecmd("quit")
             self.assertEqual("", f.getvalue())
-               
+
     def test_create_errors(self):
         """Test create command errors."""
         with patch("sys.stdout", new=StringIO()) as f:
             self.HBNB.onecmd("create")
             self.assertEqual(
                 "** class name missing **\n", f.getvalue())
-            
+
         with patch("sys.stdout", new=StringIO()) as f:
             self.HBNB.onecmd("create asdfsfsd")
             self.assertEqual(
                 "** class doesn't exist **\n", f.getvalue())
 
-    def test_docstrings(self):
-        """Check for docstrings."""
-        self.assertIsNotNone(HBNBCommand.__doc__)
-        self.assertIsNotNone(HBNBCommand.emptyline.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_quit.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_EOF.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_create.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_show.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_destroy.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_all.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_update.__doc__)
-        self.assertIsNotNone(HBNBCommand.default.__doc__)
-
-    def test_destroy(self):
-        """Test destroy command input."""
-        with patch("sys.stdout", new=StringIO()) as f:
-            self.HBNB.onecmd("destroy")
-            self.assertEqual(
-                "** class name missing **\n", f.getvalue())
-        with patch("sys.stdout", new=StringIO()) as f:
-            self.HBNB.onecmd("destroy Galaxy")
-            self.assertEqual(
-                "** class doesn't exist **\n", f.getvalue())
-        with patch("sys.stdout", new=StringIO()) as f:
-            self.HBNB.onecmd("destroy User")
-            self.assertEqual(
-                "** instance id missing **\n", f.getvalue())
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.HBNB.onecmd("destroy BaseModel 12345")
-            self.assertEqual(
-                "** no instance found **\n", f.getvalue())
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.HBNB.onecmd("Galaxy.destroy()")
-            self.assertEqual(
-                "** class doesn't exist **\n", f.getvalue())
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.HBNB.onecmd("User.destroy(12345)")
-            self.assertEqual(
-                "** no instance found **\n", f.getvalue())
-    
     def test_show(self):
         """Test show command."""
         with patch("sys.stdout", new=StringIO()) as f:
