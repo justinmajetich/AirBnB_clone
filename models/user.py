d1e843418be2a3282ec9f31e4be89eca9b7fa507
tiffanywalker22 <tbcasper16@gmail.com>
2023-10-30 15:35:11 -0700

    console is working againgit add .!

--- models/user.py
+++ models/user.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
+from models.base_model import Base
+from models.review import Review
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import relationship
+
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
