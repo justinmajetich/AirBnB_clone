537c7375621e58effce6b051c1dfae7406cf255f
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-10-30 11:05:45 -0700

    added spaces around = in obj_dict={}

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,7 +20,7 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        obj_dict={}
+        obj_dict = {}
         if cls is not None:
             if type(cls) == str:
                 cls = eval(cls)
