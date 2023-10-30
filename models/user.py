862a58717aaa1e68fee317877dda72b9d563c68e
cpmoore0317 <cpmoore317@gmail.com>
2023-10-30 11:30:35 -0500

    Updating city, place, and user

--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship, backref
 
 __tablename__= 'users'
 
@@ -11,3 +12,9 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
+    places = relationship(
+        cascade="all,delete",
+        backref=backref("user", cascade="all,delete"),
+        passive_deletes=True,
+        single_parent=True
+    )
