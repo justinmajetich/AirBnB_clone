819a3fe48b61a147e5621b27f508e3c380cd06ea
CanonicCake <n.a.ellis99@gmail.com>
2023-10-30 14:06:23 -0700

    setting name

--- console.py
+++ console.py
@@ -121,7 +121,7 @@ class HBNBCommand(cmd.Cmd):
         elif args not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        create(HBNBCommand(mainkey=) value=)
+        name="My_little_house"
         new_instance = HBNBCommand.classes[args]()
         storage.save()
         print(new_instance.id)
