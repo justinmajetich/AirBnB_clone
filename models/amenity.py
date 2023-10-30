f1dd9a8957db31f6372509efaa94fd75c6983128
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-10-30 17:31:34 -0500

    fixed many errors

--- models/amenity.py
+++ models/amenity.py
@@ -2,6 +2,7 @@
 """ State Module for HBNB project """
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
 class Amenity(BaseModel, Base):
--- models/state.py
+++ models/state.py
@@ -13,15 +13,15 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     if getenv("HBNB_TYPE_STORAGE") == "db":
-            cities = relationship(
-                "City", backref="state", cascade="all, delete"
-            )
-        else:
-            @property
-            def cities(self):
-                """returns list of City instances upon state_id"""
-                cities_list = []
-                for obj in list(models.storage.all(City).values()):
-                    if obj.state_id == self.id:
-                        cities_list.append(obj)
-                return cities_list
+        cities = relationship(
+            "City", backref="state", cascade="all, delete"
+        )
+    else:
+        @property
+        def cities(self):
+            """returns list of City instances upon state_id"""
+            cities_list = []
+            for obj in list(models.storage.all(City).values()):
+                if obj.state_id == self.id:
+                    cities_list.append(obj)
+            return cities_list
