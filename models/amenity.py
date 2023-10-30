56be4b15b06e2091da645238853a76c907b4385b
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2023-10-30 17:13:49 -0500

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 
 class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity")
+    place_amenities = relationship("Place",
+                                   secondary="place_amenity",
+                                   viewonly=False
+                                  )
