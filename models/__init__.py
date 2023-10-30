757082517a797165ece95be930b30062d45fc2da
kier-ious <kier.diemer@gmail.com>
2023-10-29 20:59:42 -0700

    took out repeated backref in state.py

--- models/__init__.py
+++ models/__init__.py
@@ -10,4 +10,3 @@ if os.getenv('HBNB_TYPE_STORAGE') == 'db':
     storage.reload()
 else:
     storage = FileStorage()
-    storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -7,6 +7,7 @@ import uuid
 from datetime import datetime
 from models import storage
 
+Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
@@ -14,40 +15,31 @@ class BaseModel:
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
-    def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-            models.storage.new(self)
-        else:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-            self.updated_at = datetime.utcnow()
-            self.created_at = datetime.utcnow()
-            storage.new(self)
-            models.storage.new(self)
-
-    def __str__(self):
-        """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
-
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
         storage.save()
-        self.updated_at = datetime.utcnow()
-        models.storage.save()
 
     def delete(self):
         """Deletes the current instance from storage"""
         storage.delete(self)
         models.storage.delete(self)
+
+    def __init__(self, *args, **kwargs):
+        """Instatntiates a new model"""
+        for key, value in kwargs.items():
+            if key != "__class__":
+                setattr(self, key, value)
+        self.updated_at = datetime.utcnow()
+        self.created_at = datetime.utcnow()
+        storage.new(self)
+        models.storage.new(self)
+
+    def __str__(self):
+        """Returns a string representation of the instance"""
+        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def to_dict(self):
         """Returns a dict containing all key/values of instance"""
