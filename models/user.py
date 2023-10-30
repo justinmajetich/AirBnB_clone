b255c6cda81c4bbe57b36d67363908e7f69bfa0e
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-10-30 16:56:46 -0500

    aded deletion relationship

--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -9,5 +10,11 @@ class User(BaseModel, Base):
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship(
+        "Place", backref="user", cascade="all, delete"
+    )
+    reviews = relationship(
+        "Review", backref="user", cascade="all, delete-orphan"
+    )
