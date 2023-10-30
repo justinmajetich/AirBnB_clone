7c0075949c9585c85145150040ecaef7b56cfa56
TPoe25 <t_poe25@yahoo.com>
2023-10-30 14:01:15 -0700

    updated state and place model to account for id

--- models/state.py
+++ models/state.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-
+import uuid  # Import the uuid module
 
 class State(BaseModel):
     """ State class """
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        """ Initialize State class """
+        super().__init__(*args, **kwargs)
+
+        # Generate the 'id' attribute if it doesn't exist
+        if not hasattr(self, 'id'):
+            self.id = str(uuid.uuid4())
