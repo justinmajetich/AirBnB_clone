f892d024c1fbc95d957934dc49105d0349e69166
LeeWest89 <6683@holbertonstudents.com>
2023-10-30 08:00:21 -0700

    reverting

--- models/base_model.py
+++ models/base_model.py
@@ -60,8 +60,8 @@ class BaseModel:
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()
 
-        #if my_dict['_sa_instance_state']:
-        my_dict.pop('_sa_instance_state')
+        if my_dict['_sa_instance_state']:
+            my_dict.pop('_sa_instance_state')
 
         return my_dict
 
