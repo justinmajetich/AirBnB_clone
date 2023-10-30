3a06a25fe0f8660a4997707c8abe5c5e7030c06e
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-10-30 11:08:14 -0500

    Updated city to have an id column lol

--- models/city.py
+++ models/city.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel, Base
+from models.base_model import Base
 from sqlalchemy import Column, String, ForeignKey
 
-class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    __tablename__ = "cities"
 
-    state_id = Column(String(60), ForeignKey("state.id"), nullable=False)
+class City(Base):
+    __tablename__ = 'cities'
+    id = Column(String(60), primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
