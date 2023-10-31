5d1e72598a9a8c2263ce305f8bda5b051b225ede
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-10-31 09:35:35 -0700

    finished updating BaseModel and added delete method

--- models/base_model.py
+++ models/base_model.py
@@ -21,7 +21,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -39,6 +38,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
@@ -49,4 +49,10 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if _sa_instance_state in dictionary:
+            del dictionary[_sa_instance_state]
         return dictionary
+
+    def delete(self):
+        """Delete the current instance from storage"""
+        storage.delete(self)
