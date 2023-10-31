972602e4a007fd150bcf5b3057d5a0e079b4a06b
michaellgans <michaellgans@Victoria>
2023-10-31 17:04:50 -0500

    M - adds tests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -2,6 +2,7 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.user import User
+import unittest
 
 
 class test_User(test_basemodel):
@@ -36,3 +37,7 @@ class test_User(test_basemodel):
         new = self.value()
         new.password = "password"
         self.assertEqual(type(new.password), str)
+
+
+if __name__ == "__main__":
+    unittest.main()
