48cb974a3db69d0979c33f79ecdbd9b4e8b7de33
Moonwalkert3ch <6620@holbertonstudents.com>
2023-10-31 12:16:20 -0700

    places

--- models/place.py
+++ models/place.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 
 
 class Place(BaseModel):
     """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
+    __tablename__ = "places"
+    city_id = Column(String(60), nullable=False, ForeignKey("cities.id"))
+    user_id = Column(String(60), nullable=False, ForeignKey("users.id"))
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=False)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float)
     longitude = 0.0
     amenity_ids = []
