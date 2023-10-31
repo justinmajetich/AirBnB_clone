bf37a986fe7d87a681b9d0503fc3b389af6dd583
mokasp <kasperbouldin@gmail.com>
2023-10-31 15:28:20 -0500

    K 3:28 - added check for id in BaseModel and relationship tables in place

--- models/base_model.py
+++ models/base_model.py
@@ -41,6 +41,8 @@ class BaseModel():
                         setattr(self, key, new_value)
                     elif key != "__class__":
                         setattr(self, key, kwargs.get(key))
+                if 'id' not in kwargs:
+                    setattr(self, 'id', str(uuid.uuid4()))
 
     def __str__(self):
         """Returns a string representation of the instance"""
