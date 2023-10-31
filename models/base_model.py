3e797569983138c33c521a418649a1e6f9314558
Frankblation <frankblation@gmail.com>
2023-10-30 19:05:36 -0700

    pycodestyle everywhere, User fro number 7

--- models/base_model.py
+++ models/base_model.py
@@ -8,6 +8,7 @@ from datetime import datetime
 
 Base = declarative_base()
 
+
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), primary_key=True, nullable=False)
--- models/state.py
+++ models/state.py
@@ -15,7 +15,7 @@ class State(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state",
-                            cascade = "all, delete-orphan")
+                              cascade="all, delete-orphan")
     else:
         name = ""
 
