df216981453c3b2eea83e30cdf535fd000c7504f
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-10-31 10:09:54 -0700

    updated state with tablename and relationship with cities for DBStorage, as well as added getter attribute for FileStorage

--- models/state.py
+++ models/state.py
@@ -11,3 +11,14 @@ class State(BaseModel, Base):
     name = ""
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete-orphan")
+
+    @property
+    def cities(self):
+        """Getter attribute to return the list of City instances with state_id
+        equal to the current State.id for FileStorage."""
+        cities = []
+        for city in storage.all("City").values():
+            if city.state_id == self.id:
+                cities.append(city)
+        return cities
