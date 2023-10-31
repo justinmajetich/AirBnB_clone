fd3734595219ab2d6fd03ef9cb6cdd6447e09620
kier-ious <kier.diemer@gmail.com>
2023-10-30 20:59:01 -0700

    resending amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.place import place_amenity
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, Table, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """ Repr of Amenity """
+    __tablename__ = 'amenites'
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+
+    place_amenities = relationship(
+        "Place", secondary="place_amenity", viewonly=False)
