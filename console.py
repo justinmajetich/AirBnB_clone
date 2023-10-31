1b47c7a9e1b2d538a9b850f24f1eff13ac04d70e
TPoe25 <t_poe25@yahoo.com>
2023-10-31 10:24:17 -0700

    fixed push

--- console.py
+++ console.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ Console Module """
 import cmd
-import sys
 from models.base_model import BaseModel
 from models.__init__ import storage
 from models.user import User
@@ -10,9 +9,8 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
-
-
-
+import sys
+import models
 class HBNBCommand(cmd.Cmd):
     """ Contains the functionality for the HBNB console"""
 
