860c53d9a6a2df88d66314dce32d76151ddeb914
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-10-31 09:54:16 -0700

    added correct imports

--- models/city.py
+++ models/city.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel
 from models.base_model import Base
 
--- models/state.py
+++ models/state.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel
+from models.base_model import Base
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
