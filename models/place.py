6d75034ed1eb667566e41df7576757cb4e16cb37
kier-ious <kier.diemer@gmail.com>
2023-10-31 15:49:14 -0700

    corrected more typos

--- models/place.py
+++ models/place.py
@@ -19,17 +19,16 @@ place_amenity = Table(
 class Place(BaseModel, Base):
     __tablename__ = 'places'
 
-    id = Column(String(60), primary_key=True, nullable=False)
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024))
+    description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, default=0, nullable=False)
     number_bathrooms = Column(Integer, default=0, nullable=False)
     max_guest = Column(Integer, default=0, nullable=False)
     price_by_night = Column(Integer, default=0, nullable=False)
-    latitude = Column(Float)
-    longitude = Column(Float)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     amenity_ids = []
 
     # Establish relationships
