4dc72ae27ed8d854117161506d8fef6990f2c993
mokasp <kasperbouldin@gmail.com>
2023-10-31 13:51:52 -0500

    K - 1:51 add sqlalchemy to Review

--- models/review.py
+++ models/review.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
+# KASPER edited 1:48pm 10/31
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import (
+    Column,
+    String,
+    ForeignKey
+)
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = 'reviews'
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
