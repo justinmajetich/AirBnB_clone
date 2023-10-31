b482954ad8f6c7726b77a2ad9a72d318ff29c867
Frankblation <frankblation@gmail.com>
2023-10-30 20:48:55 -0700

    Backref and relationships

--- models/review.py
+++ models/review.py
@@ -12,8 +12,3 @@ class Review(BaseModel, Base):
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     text = Column(String(1024), nullable=False)
-
-    # Define the relationships
-    user = relationship('User', back_populates='reviews')  # Assuming a 'User' model exists with 'reviews' relationship
-    place = relationship('Place', back_populates='reviews')  # Assuming a 'Place' model exists with 'reviews' relationship
-
--- models/user.py
+++ models/user.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
+from models.place import Place
+from models.review import Review
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
@@ -14,4 +16,6 @@ class User(BaseModel, Base):
     first_name = Column(String(128))
     last_name = Column(String(128))
 
-    places = relationship('Place', backref='user', cascade='all, delete-orphan')
+    places = relationship('Place', backref='user',
+                          cascade='all, delete-orphan')
+    reviews = relationship('Review', backref='user')
