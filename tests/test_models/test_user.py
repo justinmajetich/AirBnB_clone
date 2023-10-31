bbe2aa5bcca94a90e4b82437cd5f9bc1b91de454
michaellgans <michaellgans@Victoria>
2023-10-31 16:54:14 -0500

    M 4:54 PM - tests user

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -16,19 +16,23 @@ class test_User(test_basemodel):
     def test_first_name(self):
         """ """
         new = self.value()
+        new.first_name = "name"
         self.assertEqual(type(new.first_name), str)
 
     def test_last_name(self):
         """ """
         new = self.value()
+        new.last_name = "name"
         self.assertEqual(type(new.last_name), str)
 
     def test_email(self):
         """ """
         new = self.value()
+        new.email = "email"
         self.assertEqual(type(new.email), str)
 
     def test_password(self):
         """ """
         new = self.value()
+        new.password = "password"
         self.assertEqual(type(new.password), str)
