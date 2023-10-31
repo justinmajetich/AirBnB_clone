bf08f76e886abe7cfadd1445bc38da9deedeee99
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-31 13:47:50 -0700

    console file changes

--- tests/test_console.py
+++ tests/test_console.py
@@ -28,37 +28,37 @@ class test_console(unittest.TestCase):
     def test_create(self):
         """Test create command inpout"""
         with patch('sys.stdout', new=StringIO()) as f:
-            self.consol.onecmd("create")
+            self.console.onecmd("create")
             self.assertEqual("** class name missing **\n", f.getvalue())
         with patch('sys.stdout', new=StringIO()) as f:
-            self.consol.onecmd("create asdfsfsd")
+            self.console.onecmd("create asdfsfsd")
             self.assertEqual("** class doesn't exist **\n", f.getvalue())
 
         with patch('sys.stdout', new=StringIO()) as f:
-            self.consol.onecmd("create User")
+            self.console.onecmd("create User")
         with patch('sys.stdout', new=StringIO()) as f:
-            self.consol.onecmd("all User")
+            self.console.onecmd("all User")
             self.assertEqual("[[User]", f.getvalue()[:7])
 
     @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db', "DBStorage")
     def test_create_db(self):
         """Test create command inpout DB"""
         with patch('sys.stdout', new=StringIO()) as f:
-            self.consol.onecmd("create")
+            self.console.onecmd("create")
             self.assertEqual("** class name missing **\n", f.getvalue())
         with patch('sys.stdout', new=StringIO()) as f:
-            self.consol.onecmd("create asdfsfsd")
+            self.console.onecmd("create asdfsfsd")
             self.assertEqual("** class doesn't exist **\n", f.getvalue())
 
         with patch('sys.stdout', new=StringIO()) as f:
-            self.consol.onecmd('create State name="California!"')
+            self.console.onecmd('create State name="California!"')
             s = f.getvalue()
         with patch('sys.stdout', new=StringIO()) as f:
-            self.consol.onecmd("all State")
+            self.console.onecmd("all State")
             self.assertEqual("[[State]", f.getvalue()[:8])
         with patch('sys.stdout', new=StringIO()) as foo:
             s = "destroy State " + s
-            self.consol.onecmd(s)
+            self.console.onecmd(s)
 
 
 if __name__ == '__main__':
