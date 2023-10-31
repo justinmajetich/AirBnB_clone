ed1f71c4bbbf9625ba8037b29e09b0562ed4f937
Frankblation <frankblation@gmail.com>
2023-10-30 20:21:15 -0700

    RElationships

--- models/review.py
+++ models/review.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """Review class to store review information"""
+    __tablename__ = 'reviews'  # Table name as specified
+
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    text = Column(String(1024), nullable=False)
+
+    # Define the relationships
+    user = relationship('User', back_populates='reviews')  # Assuming a 'User' model exists with 'reviews' relationship
+    place = relationship('Place', back_populates='reviews')  # Assuming a 'Place' model exists with 'reviews' relationship
+
--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -12,3 +13,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+
+    places = relationship('Place', backref='user', cascade='all, delete-orphan')
