092647e96c764d1b1cdd4cd1f3fd7b74ddcbdc5e
mokasp <kasperbouldin@gmail.com>
2023-10-31 02:27:39 -0500

    K 2:27 AM - remove changes to user

--- models/user.py
+++ models/user.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
-# kasper edited 2:24 10/31/23
+# Kasper edited 2:27 am
 """This module defines a class User"""
-from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
-from sqlalchemy import (
-    Column,
-    String
-)
+from models.base_model import BaseModel
 
 
-class User(BaseModel, Base):
+class User(BaseModel):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    places = relationship("Place", backref="user",
-                          cascade="all, delete-orphan")
-    reviews = relationship("Review", backref="user",
-                           cascade="all, delete-orphan")
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
