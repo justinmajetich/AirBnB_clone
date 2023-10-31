9508f590c3eca1f42302b43050e9b74148f6dce2
CanonicCake <n.a.ellis99@gmail.com>
2023-10-31 15:00:45 -0700

    moving self.name to kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -13,7 +13,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            self.name = setattr('name', None)
             storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
@@ -22,6 +21,7 @@ class BaseModel:
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
             self.__dict__.update(kwargs)
+            self.name = setattr('name', None)
 
     def __str__(self):
         """Returns a string representation of the instance"""
