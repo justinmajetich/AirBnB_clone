9a17104c8fd224f7588e861db87c36ee2580add8
BradBrown6621 <6621@holbertonstudents.com>
2023-10-31 14:58:06 -0700

    Added name to list of kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -20,6 +20,7 @@ class BaseModel:
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
+            self.name = kwargs.get('name', None)
             del kwargs['__class__']
             self.__dict__.update(kwargs)
 
