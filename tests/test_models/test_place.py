c53cd104f7027dcda60271c5f330ea89adc0c48f
mokasp <kasperbouldin@gmail.com>
2023-10-31 21:29:07 -0500

    K 9:29 - filled in tests for place

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """ """
+# KASPER edited at 9:28 pm
 from tests.test_models.test_base_model import test_basemodel
 from models.place import Place
+from models.state import State
+from models.city import City
+from models.user import User
 
 
 class test_Place(test_basemodel):
@@ -15,55 +19,121 @@ class test_Place(test_basemodel):
 
     def test_city_id(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.city_id), str)
+        new_state = State(name="Oklahoma")
+        id_state = new_state.id
+        new_city = City(name="Tulsa", state_id=id_state)
+        id_city = new_city.id
+        new_user = User(nme="Kasper")
+        id_user = new_user.id
+        new_place = Place(city_id=id_city, user_id=id_user, name="home")
+        self.assertEqual(type(new_place.city_id), str)
 
     def test_user_id(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.user_id), str)
+        new_state = State(name="Oklahoma")
+        id_state = new_state.id
+        new_city = City(name="Tulsa", state_id=id_state)
+        id_city = new_city.id
+        new_user = User(nme="Kasper")
+        id_user = new_user.id
+        new_place = Place(city_id=id_city, user_id=id_user, name="home")
+        self.assertEqual(type(new_place.user_id), str)
 
     def test_name(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+        new_state = State(name="Oklahoma")
+        id_state = new_state.id
+        new_city = City(name="Tulsa", state_id=id_state)
+        id_city = new_city.id
+        new_user = User(nme="Kasper")
+        id_user = new_user.id
+        new_place = Place(city_id=id_city, user_id=id_user, name="home")
+        self.assertEqual(type(new_place.name), str)
 
     def test_description(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.description), str)
+        new_state = State(name="Oklahoma")
+        id_state = new_state.id
+        new_city = City(name="Tulsa", state_id=id_state)
+        id_city = new_city.id
+        new_user = User(nme="Kasper")
+        id_user = new_user.id
+        new_place = Place(city_id=id_city, user_id=id_user, name="home", description="a building")
+        self.assertEqual(type(new_place.description), str)
 
     def test_number_rooms(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.number_rooms), int)
+        new_state = State(name="Oklahoma")
+        id_state = new_state.id
+        new_city = City(name="Tulsa", state_id=id_state)
+        id_city = new_city.id
+        new_user = User(nme="Kasper")
+        id_user = new_user.id
+        new_place = Place(city_id=id_city, user_id=id_user, name="home", number_rooms=1)
+        self.assertEqual(type(new_place.number_rooms), int)
 
     def test_number_bathrooms(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.number_bathrooms), int)
+        new_state = State(name="Oklahoma")
+        id_state = new_state.id
+        new_city = City(name="Tulsa", state_id=id_state)
+        id_city = new_city.id
+        new_user = User(nme="Kasper")
+        id_user = new_user.id
+        new_place = Place(city_id=id_city, user_id=id_user, name="home", number_bathrooms=1)
+        self.assertEqual(type(new_place.number_bathrooms), int)
 
     def test_max_guest(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.max_guest), int)
+        new_state = State(name="Oklahoma")
+        id_state = new_state.id
+        new_city = City(name="Tulsa", state_id=id_state)
+        id_city = new_city.id
+        new_user = User(nme="Kasper")
+        id_user = new_user.id
+        new_place = Place(city_id=id_city, user_id=id_user, name="home", max_guest=2)
+        self.assertEqual(type(new_place.max_guest), int)
 
     def test_price_by_night(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.price_by_night), int)
+        new_state = State(name="Oklahoma")
+        id_state = new_state.id
+        new_city = City(name="Tulsa", state_id=id_state)
+        id_city = new_city.id
+        new_user = User(nme="Kasper")
+        id_user = new_user.id
+        new_place = Place(city_id=id_city, user_id=id_user, name="home", price_by_night=60)
+        self.assertEqual(type(new_place.price_by_night), int)
 
     def test_latitude(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.latitude), float)
+        new_state = State(name="Oklahoma")
+        id_state = new_state.id
+        new_city = City(name="Tulsa", state_id=id_state)
+        id_city = new_city.id
+        new_user = User(nme="Kasper")
+        id_user = new_user.id
+        new_place = Place(city_id=id_city, user_id=id_user, name="home", latitude=4.13)
+        self.assertEqual(type(new_place.latitude), float)
 
     def test_longitude(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.latitude), float)
+        new_state = State(name="Oklahoma")
+        id_state = new_state.id
+        new_city = City(name="Tulsa", state_id=id_state)
+        id_city = new_city.id
+        new_user = User(nme="Kasper")
+        id_user = new_user.id
+        new_place = Place(city_id=id_city, user_id=id_user, name="home", longitude=4.13)
+        self.assertEqual(type(new_place.longitude), float)
 
     def test_amenity_ids(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.amenity_ids), list)
+        new_state = State(name="Oklahoma")
+        id_state = new_state.id
+        new_city = City(name="Tulsa", state_id=id_state)
+        id_city = new_city.id
+        new_user = User(nme="Kasper")
+        id_user = new_user.id
+        new_place = Place(city_id=id_city, user_id=id_user, name="home")
+        self.assertEqual(type(new_place.amenity_ids), list)
