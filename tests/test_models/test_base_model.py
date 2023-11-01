8922f6d36f20bc7c148cf3e6de6670b5fbee3614
mokasp <kasperbouldin@gmail.com>
2023-10-31 20:28:50 -0500

    K 8:28pm - fix test_kwargs_one

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -77,8 +77,8 @@ class test_basemodel(unittest.TestCase):
     def test_kwargs_one(self):
         """ """
         n = {'Name': 'test'}
-        with self.assertRaises(KeyError):
-            new = self.value(**n)
+        x = BaseModel(name='test')
+        self.assertEqual(x.name, 'test')
 
     def test_id(self):
         """ """
