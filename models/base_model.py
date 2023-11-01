3019fb06a63a77f53210c0f84edf35a704b504a6
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-31 17:58:39 -0700

    unittests for base model

--- models/base_model.py
+++ models/base_model.py
@@ -25,21 +25,6 @@ class BaseModel:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
-        '''if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
-            for key, value in kwargs.items():
-            if not hasattr(self, key):
-                setattr(self, key, value)'''
 
     def __str__(self):
         """Returns a string representation of the instance"""
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,6 +2,7 @@
 """unittests for base_model"""
 
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 import unittest
 import datetime
 from uuid import UUID
@@ -10,37 +11,67 @@ import os
 
 
 class TestBaseModel(unittest.TestCase):
+    '''Test cases for the test base model class'''
+
+    @classmethod
     def setUp(self):
         # This method is called before each test method
+        try:
+            os.rename("file.json", "temp.json")
+        except IOError:
+            pass
+        FileStorage._FileStorage__objects = {}
+        self.storage = FileStorage()
         self.model = BaseModel()
 
+    @classmethod
     def tearDown(self):
         # This method is called after each test method
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("temp.json", "file.json")
+        except IOError:
+            pass
+        del self.storage
         del self.model
 
     def test_init(self):
-        self.assertIsInstance(self.model.id, str)
-        self.assertIsInstance(self.model.created_at, datetime)
-        self.assertIsInstance(self.model.updated_at, datetime)
+        self.assertEqual(type(self.model.id), str)
+        self.assertEqual(type(self.model.created_at), datetime.datetime)
+        self.assertEqual(type(self.model.updated_at), datetime.datetime)
+        self.assertIsInstance(self.model, BaseModel)
 
     def test_str(self):
-        model_str = str(self.model)
+        '''testing str representation'''
+        model_str = self.model.__str__()
         self.assertTrue(self.model.id in model_str)
         self.assertTrue("created_at" in model_str)
         self.assertTrue("updated_at" in model_str)
 
+    @unittest.skipIf(os.getenv("HBNB_ENV") is not None, "DBStorage")
     def test_save(self):
+        '''Testing save method'''
         original_updated_at = self.model.updated_at
         self.model.save()
         self.assertNotEqual(original_updated_at, self.model.updated_at)
 
     def test_to_dict(self):
         model_dict = self.model.to_dict()
-        self.assertIsInstance(model_dict, dict)
+        self.assertEqual(type(model_dict), dict)
         self.assertIn('id', model_dict)
         self.assertIn('created_at', model_dict)
         self.assertIn('updated_at', model_dict)
         self.assertIn('__class__', model_dict)
+        self.assertEqual(model_dict.get("_sa_instance_state", None), None)
+
+    @unittest.skipIf(os.getenv("HBNB_ENV") is not None, "DBStorage")
+    def test_delete(self):
+        '''Testing the delete method'''
+        self.model.delete()
+        self.assertNotIn(self.model, FileStorage._FileStorage__objects)
 
 if __name__ == '__main__':
     unittest.main()
