8a4ca1d86e8539982d75d0b3a9adc82c37a2da09
Moonwalkert3ch <6620@holbertonstudents.com>
2023-10-31 19:27:45 -0700

    fix typo in base

--- models/base_model.py
+++ models/base_model.py
@@ -28,8 +28,8 @@ class BaseModel:
         """
 
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.utc()
-        self.updated_at = datetime.utc()
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
 
         if kwargs:
             for key, value in kwargs.items():
