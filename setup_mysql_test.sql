a71e1f0ba18ce0859fb6b07cb7c5ecce677a2fbe
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-10-31 21:43:42 -0500

    clean push of all (edited setup mysql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,10 +2,6 @@
 -- Set password for user, grant all privileges on database hbnb_test_db
 -- Select privileges on database performance_schema
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
-IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON 'hbnb_test_db'.* TO 'hbnb_test'@'localhost'
-IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT ON 'performance_schema'.* TO 'hbnb_test'@'localhost'
-IDENTIFIED BY 'hbnb_test_pwd';
-FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL ON hbnb_test_db.* TO hbnb_test@localhost;
+GRANT SELECT ON performance_schema.* TO hbnb_test@localhost;
