75f4601ac1f2aff02eafa1ef718ae8cf2822b6a6
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-11-02 21:48:08 -0500

    Kier (#127)
    * Made a few changes to test file and imports
    
    * made edits to a few files
    
    * imcluded a port to the db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -32,11 +32,12 @@ class DBStorage(FileStorage):
     def __init__(self):
         """ Initialize instance of DBStorage """
         self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".
-                                      format(getenv("HBNB_MYSQL_USER"),
-                                             getenv("HBNB_MYSQL_PWD"),
-                                             getenv("HBNB_MYSQL_HOST"),
-                                             getenv("HBNB_MYSQL_DB")),
-                                      pool_pre_ping=True)
+                                    format(getenv("HBNB_MYSQL_USER"),
+                                            getenv("HBNB_MYSQL_PWD"),
+                                            getenv("HBNB_MYSQL_HOST"),
+                                            getenv("HBNB_MYSQL_DB")),
+                                            getenv('HBNB_MYSQL_PORT', 3306)
+                                    pool_pre_ping=True)
 
         if getenv('HBNB_ENV') == "test":
             Base.metadata.drop_all(self.__engine)
