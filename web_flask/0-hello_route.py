19cfe6de92dcfaa659996d4d0d95522004415a47
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-11-09 13:55:09 -0600

    Wrote the script to start the Flask app and set up the localhost and port

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=True)
+def hello():
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
