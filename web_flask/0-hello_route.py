245d95ebd2fa62f8338299e119c4bbd31540e575
chris85gillis <chris85gillis@gmail.com>
2023-11-09 12:58:34 -0800

    finally fixed spacing issues

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,7 +5,6 @@ from flask import Flask
 app = Flask(__name__)
 
 
-"""Define the route for the root URL '/'"""
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!"""
