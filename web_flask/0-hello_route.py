287933cc5dfa76e78e0261e831b993e40cff8ad0
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-11-09 14:20:25 -0600

    ran pycodestyle and added module documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
+# Set up for Flask app
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
+# Using the strict_slashes set to False so that w/out '/' it wont work
 def hello():
     return "Hello HBNB!"
 
 
+""" Setting the localhost and port"""
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
