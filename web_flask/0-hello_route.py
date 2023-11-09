0567ad8f6268480ece85f2d5e69dd1f6d19dcf1a
chris85gillis <chris85gillis@gmail.com>
2023-11-09 12:37:11 -0800

    working on formatting issues

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """Starts a Flask web app"""
-
 from flask import Flask
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 """Define the route for the root URL '/'"""
 @app.route('/', strict_slashes=False)
