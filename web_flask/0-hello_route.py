709ba1dea1ea238e3a95947bd200930d2066226a
chris85gillis <chris85gillis@gmail.com>
2023-11-09 12:47:59 -0800

    yet again

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,6 +8,8 @@ app = Flask(__name__)
 
 """Define the route for the root URL '/'"""
 @app.route('/', strict_slashes=False)
+
+
 def hello_hbnb():
     """Displays 'Hello HBNB!"""
     return "Hello HBNB!"
