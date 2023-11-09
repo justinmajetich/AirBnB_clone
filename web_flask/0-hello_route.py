cc39085db5739d1452a272e1e79dfefc32ef2342
chris85gillis <chris85gillis@gmail.com>
2023-11-09 12:39:28 -0800

    fixed style error

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,11 +2,12 @@
 """Starts a Flask web app"""
 from flask import Flask
 
-
 app = Flask(__name__)
 
 """Define the route for the root URL '/'"""
 @app.route('/', strict_slashes=False)
+
+
 def hello_hbnb():
     """Displays 'Hello HBNB!"""
     return "Hello HBNB!"
