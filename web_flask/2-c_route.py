331ef84b048fcd30633999c2d2cd03207d3f5f78
tiffanywalker22 <tiffanywalker@LAPTOP-5UJ7IJM0>
2023-11-09 22:39:08 -0600

    adding c is fun

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Script that starts a flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def start():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(textx):
+    c_text = text.replace('_', ' ')
+    return f'C {c_text}'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
