770fa534321185cbdbf71fb54aa21cbdc6b994d9
Moonwalkert3ch <132849866+Moonwalkert3ch@users.noreply.github.com>
2023-11-10 16:14:56 -0600

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-from models.base_model import BaseModel
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
+from datetime import datetime
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
