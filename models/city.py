1256377611d4d84e2a041536e2f5a1038fcbcaba
Moonwalkert3ch <132849866+Moonwalkert3ch@users.noreply.github.com>
2023-11-10 16:08:44 -0600

    Update city.py

--- models/city.py
+++ models/city.py
@@ -3,7 +3,6 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from models.place import Place
 
 
 class City(BaseModel, Base):
