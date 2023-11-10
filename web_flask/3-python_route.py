ba027efc328a55e646294010f853a955fc5d596a
tiffanywalker22 <tiffanywalker@LAPTOP-5UJ7IJM0>
2023-11-09 22:56:38 -0600

    adding python route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Script that starts a flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def start():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    c_text = text.replace('_', ' ')
+    return f'C {c_text}'
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    python_text = text.replace('_', ' ')
+    return f'Python {python_text}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
