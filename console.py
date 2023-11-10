575b1f999dd5ffa84c0ab06f61f5dbc1ef277487
Moonwalkert3ch <132849866+Moonwalkert3ch@users.noreply.github.com>
2023-11-10 16:17:15 -0600

    Update console.py

--- console.py
+++ console.py
@@ -2,7 +2,7 @@
 """ Console Module """
 import cmd
 from models.base_model import BaseModel
-from models.__init__ import storage
+from models import storage
 from models.user import User
 from models.place import Place
 from models.state import State
