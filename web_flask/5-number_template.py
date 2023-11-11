004d0db662591dbe9913ebe21bac46bdcafaaca0
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-11-11 14:40:23 -0600

    OCrrected the Flask route in my HTML to say number instead of 'n' and removed the stuff after the (n, int) in the if statment in the .py file.

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -44,7 +44,7 @@ def number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """ Displays HTML if n is an integer """
-    if isinstance(n, int) and n > 0:
+    if isinstance(n, int):
         return render_template('5-number.html', number=n)
     abort(404)
 
