5f3307af66d134aad902aabaadda15c6b066ad48
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-11-11 14:09:50 -0600

    Chasnge the h1 tag back to Number and changed the documentation in the .py file

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -43,7 +43,7 @@ def number(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    """ Returns if n is an integer """
+    """ Displays HTML if n is an integer """
     n = str(n)
     return render_template('5-number.html', number=n)
 
