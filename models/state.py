93f58a72af0cb152e09d4adbd71c49d155c9593d
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-11-12 13:08:57 -0600

    removed the main.py file and the underscore for the cities relationship in State.py

--- models/state.py
+++ models/state.py
@@ -13,7 +13,7 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
 
     name = Column(String(128), nullable=False)
-    _cities = relationship("City", backref="state", cascade="delete")
+    cities = relationship("City", backref="state", cascade="delete")
 
     if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
