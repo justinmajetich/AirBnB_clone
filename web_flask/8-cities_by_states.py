78df1e86dff97eb09b37b671cd789306b4dd0429
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-11-12 16:28:41 -0600

    Corrected the spelling on the routing. it was cities_by_state instead of cities_by_states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -10,13 +10,13 @@ from models.city import City
 app = Flask(__name__)
 
 
-@app.route('/cities_by_state', strict_slashes=False)
-def cities_by_state():
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
     """ Displays list of states and cities from DB """
     states = sorted(list(storage.all(State).values()), key=lambda x: x.name)
     cities = sorted(list(storage.all(City).values()), key=lambda x: x.name)
     return render_template(
-        '8-cities_by_state.html', states=states, cities=cities)
+        '8-cities_by_states.html', states=states, cities=cities)
 
 
 @app.teardown_appcontext
