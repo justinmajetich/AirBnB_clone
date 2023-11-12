754e61721d4092b6cbaf024ca4393102189432ef
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-11-12 13:04:09 -0600

    added an underscored before the cities relationship

--- models/state.py
+++ models/state.py
@@ -13,7 +13,7 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
 
     name = Column(String(128), nullable=False)
-    """ cities = relationship("City", backref="state", cascade="delete") """
+    _cities = relationship("City", backref="state", cascade="delete")
 
     if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
