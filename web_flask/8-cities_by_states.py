6627187f2a85784eca073bb55659491bce652e2e
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-11-13 16:20:56 -0600

    associated cities with states using state.cities in inner HTML tag and took the comment """ off the cities sorted in the .py file

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -5,6 +5,7 @@
 from flask import Flask, render_template, g
 from models import storage
 from models.state import State
+from models.city import City
 
 app = Flask(__name__)
 
@@ -13,10 +14,9 @@ app = Flask(__name__)
 def cities_by_states():
     """ Displays list of states and cities from DB """
     states = sorted(list(storage.all(State).values()), key=lambda x: x.name)
-    """cities = sorted(list(storage.all(
-        City).values()), key=lambda x: x.name)"""
+    cities = sorted(list(storage.all(City).values()), key=lambda x: x.name)
     return render_template(
-        '8-cities_by_states.html', states=states)
+        '8-cities_by_states.html', states=states, cities=cities)
 
 
 @app.teardown_appcontext
