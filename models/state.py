d549e1fb722230b94526b8370ceb80d49943fb7a
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-11-13 16:33:42 -0600

    corected the typo in state.py from object to city

--- models/state.py
+++ models/state.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
-""" holds class State"""
+""" State Module for HBNB project """
 import models
 from models.base_model import BaseModel, Base
 from models.city import City
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class State(BaseModel, Base):
-    """Representation of state """
-    if models.storage_t == "db":
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
-    else:
-        name = ""
-        cites = []
+    """State class repr states"""
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+    _cities = relationship("City", backref="state", cascade="delete")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """ Lists related city objects """
+            list_cities = []
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    list_cities.append(city)
+            return list_cities
