70ec7959e20e2cb59d6dcde02e17c1c42379d47c
tiffanywalker22 <tiffanywalker@LAPTOP-5UJ7IJM0>
2023-11-13 17:24:34 -0600

    pep8 fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,14 +7,17 @@ from models import storage
 
 app = Flask(__name__)
 
+
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     return render_template('7-states_list.html',
-                            states=storage.all(State))
+                           states=storage.all(State))
+
 
 @app.teardown_appcontext
 def teardown(exception):
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
