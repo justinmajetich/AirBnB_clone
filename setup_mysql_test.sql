76afbcce34142c08f46f4b1c40acadc48475e72e
tiffanywalker22 <tiffanywalker@LAPTOP-5UJ7IJM0>
2023-11-13 17:20:19 -0600

    pushing to checker

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,5 @@
--- Script that prepares a MySQL server
-
+-- Create hbnh_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_test' @'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test' @'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test' @'localhost';
