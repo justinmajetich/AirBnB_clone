892215d5345f3d6eb653b3c21b6f6b3ace373415
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-11-13 16:49:49 -0600

    Created a dict cities_by_state to group cities by their states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -15,6 +15,11 @@ def cities_by_states():
     """ Displays list of states and cities from DB """
     states = sorted(list(storage.all(State).values()), key=lambda x: x.name)
     cities = sorted(list(storage.all(City).values()), key=lambda x: x.name)
+
+    cities_by_state = {state.id: [] for state in states}
+    for city in cities:
+        cities_by_state[city.state_id].append(city)
+
     return render_template(
         '8-cities_by_states.html', states=states, cities=cities)
 
