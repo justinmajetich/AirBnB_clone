edfae02b57ace5418296f7ecd59c5b905f37bad6
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-11-14 14:38:41 -0600

    reverted code in 8.py back to old code

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Starts a Flask web app """
-from flask import Flask, render_template
+from flask import Flask, render_template, g
 from models import storage
 from models.state import State
 
@@ -8,23 +8,17 @@ app = Flask(__name__)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
-def states_list():
+def states_states():
     """ Displays list of states from DB """
-    storage.reload()
-    all_states = storage.all(State)
-
-    states_list = []
-
-    for state in all_states:
-        states_list.append(all_states[state])
-
-    return render_template('8-cities_by_states.html', states=states_list)
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
 def teardown(exception):
     """ Closes current SQLAlchemy Sesh """
-    storage.close()
+    if hasattr(g, 'storage'):
+        g.storage.close()
 
 
 if __name__ == "__main__":
