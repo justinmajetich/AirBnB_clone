85251fb36076f1cccfe03168f1af55c4043dbfec
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-11-14 15:09:37 -0600

    ""

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
 """ Starts a Flask web app """
-from flask import Flask, render_template, g
-from models import storage
+from flask import Flask, render_template
+from models.city import City
 from models.state import State
 
 app = Flask(__name__)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
-def states_states():
+def states_by_states():
     """ Displays list of states from DB """
-    states = storage.all(State).values()
-    return render_template('8-cities_by_states.html', states=states)
+    from models import storage
+    all_states = storage.all(State)
+    list_cities = []
+
+    for x in all_states:
+        list_cities.append(all_states[x])
+
+    return render_template('8-cities_by_states.html', states=list_states)
 
 
 @app.teardown_appcontext
 def teardown(close):
     """ Closes current SQLAlchemy Sesh """
+    from models import storage
     storage.close()
 
 
