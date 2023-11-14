a68b761b433dcafb1abfcf5727a3f0f61258d225
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-11-14 13:33:47 -0600

    Made edits to state.py

--- models/state.py
+++ models/state.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import models
 from models.base_model import BaseModel, Base
-from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import getenv
+from models import storage
 
 
 class State(BaseModel, Base):
     """State class repr states"""
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="delete")
+    cities = relationship(
+        "City", backref="state", cascade="all, delete-orphan",
+        primaryjoin='State.id == City.state_id')
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
-        @property
-        def cities(self):
-            """ Lists related city objects """
-            list_cities = []
-            for city in list(models.storage.all(City).values()):
-                if city.state_id == self.id:
-                    list_cities.append(city)
-            return list_cities
+    @property
+    def cities(self):
+        """ Lists related city objects """
+        current_list = []
+        list_cities = storage.all()
+        for item in current_list:
+            object = current_list[item].to_dict()
+            if object["__class__"] == "City" and object["state_id"] == self.id:
+                list_cities.append(current_list[item])
+        return list_cities
--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """ Starts a Flask web app """
-
-
 from flask import Flask, render_template
 from models import storage
 from models.state import State
