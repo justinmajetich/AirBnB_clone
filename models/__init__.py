95cccd2b9b196bd21b75e3af38119f7017939d6b
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-11-14 14:06:03 -0600

    removed second reload in my __init__ file

--- models/__init__.py
+++ models/__init__.py
@@ -6,8 +6,7 @@ from os import getenv
 if getenv("HBNB_TYPE_STORAGE") == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
-    storage.reload()
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-    storage.reload()
+storage.reload()
