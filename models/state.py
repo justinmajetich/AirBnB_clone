65425145b93141724e43cd1f6be24cb5a55c1118
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-11-14 16:16:07 -0600

    ""

--- models/state.py
+++ models/state.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
+from models import storage
 
 
 class State(BaseModel, Base):
@@ -11,7 +12,7 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        city_list = relationship(
+        cities = relationship(
             "City", backref="state", cascade="all, delete-orphan",
             primaryjoin='State.id == City.state_id')
 
@@ -19,4 +20,9 @@ class State(BaseModel, Base):
         @property
         def cities(self):
             """ Lists related city objects """
-            return self.city_list
+            new_list = []
+            current_list = storage.all("City").values()
+            for item in current_list:
+                if item.state_id == self.id:
+                    new_list.append(item)
+            return new_list
