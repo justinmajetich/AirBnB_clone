ab0483c26ea557edf47856ae177e65fb43a6c237
tiffanywalker22 <tiffanywalker@LAPTOP-5UJ7IJM0>
2023-11-13 20:37:27 -0600

    pushing to checker;saving progress for the night

---
+++ web_flask/9-states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Script that starts a flask web application"""
+from flask import Flask
+from flask import render_template
+from models.state import State
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+def states_list():
+    return render_template('9-states.html',
+                           states=storage.all(State))
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def state_id_list(id):
+    for state in storage.all(State).values():
+        if state.id == id:
+            return render_template('9-states.html', states=storage.all(State))
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
