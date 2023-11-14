6fb2a3c90bc9440db0be013f9c6c03f565337c56
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-11-14 14:19:50 -0600

    Reverted back to my old state.py file

--- models/state.py
+++ models/state.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
-""" holds class State"""
-import models
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models.city import City
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models import storage
 
 
 class State(BaseModel, Base):
-    """Representation of state """
-    if models.storage_t == "db":
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
-    else:
-        name = ""
-    cites = []
+    """State class repr states"""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship(
+        "City", backref="state", cascade="all, delete-orphan",
+        primaryjoin='State.id == City.state_id')
+
+    @property
+    def cities(self):
+        """ Lists related city objects """
+        new_list = []
+        current_list = storage.all()
+        for item in current_list:
+            object = current_list[item].to_dict()
+            if object["__class__"] == "City" and object["state_id"] == self.id:
+                new_list.append(current_list[item])
+        return new_list
