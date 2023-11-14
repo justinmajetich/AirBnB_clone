6a74cbef212178835520d096cc221781d37d6def
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-11-13 21:11:46 -0600

    reverted back to a few things from my old code

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -4,19 +4,20 @@
 
 from flask import Flask, render_template, g
 from models import storage
-from models.state import *
+from models.state import State
+from models.city import City
 app = Flask(__name__)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     """ Displays list of states and cities from DB """
-    states = sorted.storage.all("State").values()
+    states = sorted.storage.all(State).values()
     return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown(exception):
+def teardown(error=None):
     """ Closes current storage """
     current_storage = getattr(g, "storage", None)
     if current_storage is not None:
