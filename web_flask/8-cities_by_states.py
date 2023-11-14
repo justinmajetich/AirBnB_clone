f64018772c7505b5efa7988b11f0929b9a3436a9
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2023-11-13 20:34:42 -0600

    Corrected the dictionary in the render_template function

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -13,15 +13,17 @@ app = Flask(__name__)
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     """ Displays list of states and cities from DB """
-    states = sorted(list(storage.all(State).values()), key=lambda x: x.name)
-    cities = sorted(list(storage.all(City).values()), key=lambda x: x.name)
+    states = sorted(list(storage.all(State).values()))
+    cities = sorted(list(storage.all(City).values()))
 
     cities_by_state = {state.id: [] for state in states}
     for city in cities:
         cities_by_state[city.state_id].append(city)
 
     return render_template(
-        '8-cities_by_states.html', states=states, cities=cities)
+        '8-cities_by_states.html',
+        states=states,
+        cities_by_state=cities_by_state)
 
 
 @app.teardown_appcontext
