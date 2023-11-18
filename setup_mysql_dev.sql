4bcf4101f1c6ae3e13b0753bba65fa57ae5188e7
Chidimma <chidimmamofodum@gmail.com>
2023-11-18 14:48:39 -0500

    docs

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,5 @@
+-- A script to set sql database server
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
