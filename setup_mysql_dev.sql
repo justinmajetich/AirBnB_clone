d9c868715c140bf03b3d9da142323bdd18b5cebb
wagzyAyo <talktojmcvibes@gmail.com>
2023-11-20 06:53:15 -0800

    mysql setup

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,11 @@
+--creates a hbnb_dev_db database if non exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+--create user hbnb_dev if non exist identifiy by password
+CREATE USER IF NOT EXISTS 'hbnb_dev' @ 'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+--Grant all priviledges to hbnb_dev on hbnb_dev_db database
+GRANT ALL PRIVILEDGES ON hbnb_dev_db.* TO 'hbnb_dev' @ 'localhost';
+--grant SELECT priviledge on db perfomance shema
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev' @ 'localhost'
+
+
+--FLUSH PRIVILEDGES
