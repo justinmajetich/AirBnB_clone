5536281954f0e5529ead44b66ad6bd7ee8f4f388
Amanuel <amanuel.demirew@yahoo.com>
2023-11-20 10:12:32 +0300

    indentation fix

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -22,19 +22,19 @@ class DBStorage:
     db = getenv(HBNB_MYSQL_DB)
 
 
-        def __init__(self, *args, **kwargs):
-            self.__enigne = create_engine('mysql+mysqldb://{}:{}@lcoalhost:{}/{}'.format(user, pwd, host, db), pool_pre_ping=True)
-            if (getenv(HBNB_ENV) == 'test'):
-                Base.metadata.dropall()
-             def all(self, cls=None):
-                dic = {}
-                if cls:
-                    if type(cls) is str:
-                        cls = eval(cls)
-                        query = self.__session.query(cls)
-                        for q_object in query:
-                            key = "{}.{}".format(type(q_object).__name__, q_object.id)
-                            dic[key] = q_object
+    def __init__(self, *args, **kwargs):
+        self.__enigne = create_engine('mysql+mysqldb://{}:{}@lcoalhost:{}/{}'.format(user, pwd, host, db), pool_pre_ping=True)
+        if (getenv(HBNB_ENV) == 'test'):
+            Base.metadata.dropall()
+    def all(self, cls=None):
+        dic = {}
+        if cls:
+            if type(cls) is str:
+                cls = eval(cls)
+                query = self.__session.query(cls)
+                for q_object in query:
+                    key = "{}.{}".format(type(q_object).__name__, q_object.id)
+                    dic[key] = q_object
                 else:
                     list_cls = [State, City, User, Place, Review, Amenity]
                     for every_class in list_cls:
@@ -43,40 +43,40 @@ class DBStorage:
                             key = "{}.{}".format(type(every_obj).__name__, every_obj.id)
                             dic[key] = every_obj
                 return dic
-        def new(self, obj):
+    def new(self, obj):
         """
         add new obj in current session db
         """
-            self.__session.add(obj)
+        self.__session.add(obj)
 
-        def save(self):
+    def save(self):
         """
         commit session
         """
-            self.__session.commit()
+        self.__session.commit()
 
-        def delete(self, obj=None):
+    def delete(self, obj=None):
         """
         if obj exist ->  delete obj
         """
-            if obj:
-                self.session.delete(obj)
+        if obj:
+            self.session.delete(obj)
 
-        def reload(self):
+    def reload(self):
         """
         create all tables in the database
         """
-            Base.metadata.create_all(self.__engine)
+        Base.metadata.create_all(self.__engine)
         """
         create the current database session (self.__session) from the engine (self.__engine) by using a sessionmaker
         """
-            Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
-            Session = scoped_session(Session)
-            self.__session = Session()
+        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(Session)
+        self.__session = Session()
 
-        def close(self):
+    def close(self):
         """ 
         close session (if needed)
         """
-            self.__session.close()
+        self.__session.close()
 
