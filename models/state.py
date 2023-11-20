b7b71eeec5d08374268fc45bf0e8b0f3186934e9
GArdennes <kevin.afachao@gmail.com>
2023-11-20 11:41:18 -0800

    Task 6

--- models/state.py
+++ models/state.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from os import getenv
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    if getenv(('HBNB_TYPE_STORAGE')) == 'db':
+      __tablename__ = "states"
+      name = Column(String(128), nullable=False)
+    else:
+      name = ""
