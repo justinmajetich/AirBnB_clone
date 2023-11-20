9e71463c1a880c0ed3d7f4de063025a6b9071655
root <Khadijatrasaq@gmail.com>
2023-11-20 15:27:48 -0800

    modified sql test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,11 +1,11 @@
 -- The script test MYSQL server for our project
 -- Create the use if it doesn't exist
--- Grant all privileges on the hbnb_dev_db to hbnb_dev 
+-- Grant all privileges on the hbnb_test_db to hbnb_test
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
