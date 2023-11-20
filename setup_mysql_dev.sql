c22cfc24b39ef5d3e7a66623755f61cc3522e60c
kahenyamercy <kahenyamercy5@gmail.com>
2023-11-20 19:05:37 +0000

    corrected the typo error

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
-- A script that Prepares a MySQL server for the project.
+-- A script that Prepares a MySQL server for the project.
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
