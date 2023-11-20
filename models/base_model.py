84ff127e4d601995222f7314b9806e581d8bf25f
Amanuel <amanuel.demirew@yahoo.com>
2023-11-20 10:30:09 +0300

    spelling fix

--- models/base_model.py
+++ models/base_model.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
-import uuid
-from datetime import datetime
-import models
 from sqlalchemy.ext.declarative import declarative_base
+import uuid
+import models
+from datetime import datetime
 from sqlalchemy import Column, Integer, String, DateTime
 
-Base = delarative_base()
-
+Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
