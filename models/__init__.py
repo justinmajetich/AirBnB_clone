3a809ddca208b99aa40b43c819fe4ef50fd36520
Amanuel <amanuel.demirew@yahoo.com>
2023-11-20 10:05:42 +0300

    fix import error

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStoragee
+from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 from models.base_model import BaseModel
 from models.user import User
