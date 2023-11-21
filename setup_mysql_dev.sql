5d708638b26f3894db7849c3877c89cb4be08ba3
AustinRado <radoaussy@gmail.com>
2023-11-21 16:00:43 +0300

    setting up database

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,9 @@
---creates a a database hbnb_dev_db
---creates new user hbnb_dev in localhost
---sets password for hbnb_dev to (hbnb_dev_pwd)
---sets all privilages on the hbnb_dev_db for hbnb_dev (only in this db)
---sets SELECT privilages on the db performance_schema
---if db hbnb_dev_db || hbnb_dev exits, script should not fail
+-- creates a a database hbnb_dev_db
+-- creates new user hbnb_dev in localhost
+-- sets password for hbnb_dev to (hbnb_dev_pwd)
+-- sets all privilages on the hbnb_dev_db for hbnb_dev (only in this db)
+-- sets SELECT privilages on the db performance_schema
+-- if db hbnb_dev_db || hbnb_dev exits, script should not fail
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
