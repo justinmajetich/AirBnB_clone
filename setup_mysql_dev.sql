0fd7444ec16b493400dc7e2b6cbfaccba71c20f7
root <root@DESKTOP-KML44A3.localdomain>
2023-11-21 22:49:49 +0100

    resolve merge conflict

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,7 +2,6 @@
 
 -- create database hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-USE hbnb_dev_db;
 -- create a user hbnb_dev
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- grant permissions
