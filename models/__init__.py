f72eae47d3fe1a80079c7befc1f8092734ec481e
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2023-11-21 14:01:13 +0100

    Refactor models/__init__.py to instantiate the appropriate storage object based on the value of the HBNB_TYPE_STORAGE environment variable. This change allows for flexible storage options, such as using a database storage instead of file storage, by simply updating the environment variable

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+import os
 
+type_storage = os.getenv('HBNB_TYPE_STORAGE')
 
-storage = FileStorage()
+if type_storage == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 storage.reload()
