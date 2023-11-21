109f0d12ac70d7a6ac14f1e326f1ca7de6010e6e
tenderheartviv <vivian.emenike@yahoo.com>
2023-11-21 15:38:36 -0800

    mysql_dev

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,17 +2,16 @@ A script that prepares a MySQL for the project.
 
 CREATE USER IF NOT EXISTS
 'hbnb_dev'@'localhost'
-IDENTIFIED BY hbnb_dev_pwd
-IDENTIFIED BY 'hbnb_dev_pwd
+IDENTIFIED BY 'hbnb_dev_pwd';
 
 GRANT SELECT
 ON performanc_scheme.*
-TO 'hbnb_dev'0'localhost
+TO 'hbnb_dev'0'localhost'
 WITH GRANT OPTION;
 
-GRANT SELECT
-ON privrleges
-TO 'hbnb_dev'@'localhost
-WITH GRANT OPTION
+GRANT ALL PRIVILEGES
+ON hbnb_dev_db.*
+TO 'hbnb_dev'@'localhost'
+WITH GRANT OPTION;
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
