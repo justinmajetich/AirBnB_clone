e580ee7e39b7b90abe833c86ad7a5f9486f00c32
Alex Nduta <wasremoved@gmail.com>
2023-11-21 19:31:58 +0300

    did number 3, setup the test database for later use

---
+++ setup_mysql_test.sql
@@ -0,0 +1,11 @@
+-- prepares MySQL sever for the project.
+-- add a new test DB if it does not exist
+-- add a new test user in localhost an set the password
+-- give user privileges to the DB
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+USE hbnb_test_db;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
