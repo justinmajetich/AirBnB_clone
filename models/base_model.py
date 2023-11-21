316d7b8c0ededeb0bd44651e8a31782d61b33c82
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-11-21 11:36:18 +0100

    Fixing the requirement error for task 5

--- models/base_model.py
+++ models/base_model.py
@@ -2,8 +2,6 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, DateTime
 
 
 class BaseModel:
@@ -43,7 +41,6 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        del dictionary['_sa_instance_state']
         return dictionary
 
     def delete(self):
--- models/city.py
+++ models/city.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from models.base_model import BaseModel
 
 
-class City(BaseModel, Base):
+class City(BaseModel):
     """ The city class, contains state ID and name """
     state_id = ""
     name = ""
