99b2110497d9c4292d110b09dee1f8033fa8e8a4
Rophpad <rophenp@gmail.com>
2023-11-21 13:05:11 +0100

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,18 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
+    __tablename__ == 'self'
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(datetime, nullable=False)
+    updated_at = Column(datetime, nullable=False)
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
