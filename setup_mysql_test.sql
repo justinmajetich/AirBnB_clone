e32fb64f3d8ec945cbfa15b1747113d3b0a244de
Tedros <tedsaasfaha@gmail.com>
2023-11-21 11:39:09 -0800

    setup_mysql_test.sql file code updated

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,3 +1,9 @@
+-- Creates a MySQL server with:
+--   Database hbnb_test_db.
+--   User hbnb_test with password hbnb_test_pwd in localhost.
+--   Grants all privileges for hbnb_test on hbnb_test_db.
+--   Grants SELECT privilege for hbnb_test on performance_schema.
+
 -- this script prepares a MySQL server for the project
 -- create project testing database with the name : hbnb_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
