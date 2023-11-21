87df7151407282982db1598262f75cc3b4213124
the1Riddle <elvisotienomboya@gmail.com>
2023-11-21 11:10:25 -0800

    solving console errors

--- models/amenity.py
+++ models/amenity.py
@@ -1,17 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-"""from os import getenv"""
-from models.base_model import BaseModel, Base
-"""from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship"""
+from models.base_model import BaseModel
 
 
-class Amenity(BaseModel, Base):
-    """class for amenities"""
-    __tablename__ = 'amenities'
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        name = Column(String(128), nullable=False)
-        place_amenities = relationship("Place", secondary="place_amenity",
-                                       back_populates="amenities")
-    else:
-        name = ""
+class Amenity(BaseModel):
+    name = ""
