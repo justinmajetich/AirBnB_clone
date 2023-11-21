c7c9a79449e4a169bf9e4b9cf8bc9ba22adf609f
Only1allan <kariukiallan850@gmail.com>
2023-11-21 13:41:19 +0300

    Update mysql feature

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,16 +1,16 @@
--- a script that prepares a test for MySQL server for the project
+-- a script that prepares for MySQL server for the project
 
 -- create a database for the project
-CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
+CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
 
 -- create a user for the project
-CREATE USER IF NOT EXISTS 'hbnb_test_user'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev_user'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- grant all privileges to the new user on the project database
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test_user'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev_user'@'localhost';
 
 -- SELECT privilege on the database performance_schema
-GRANT SELECT ON performance_schema.* TO 'hbnb_test_user'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev_user'@'localhost';
 
 -- flush privileges
 FLUSH PRIVILEGES;
