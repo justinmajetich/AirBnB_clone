ee87bdc8d6d7898743b9d21fe01e0afedf34519b
Amal-Dahshan <amaldahshan11710@gmail.com>
2023-11-21 13:29:39 -0800

    task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,4 @@
+-- create a new database and new user
 CREATE DATABASE  IF NOT EXISTS hbnb_dev_db;
 CREATE USER  IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON 'hbnb_dev_db'.* TO 'hbnb_dev'@'localhost';
