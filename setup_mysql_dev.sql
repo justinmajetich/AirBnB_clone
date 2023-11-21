a3ed167a32a5037fb1ae581c7d431431b505ad1a
AustinRado <radoaussy@gmail.com>
2023-11-21 12:24:31 +0300

    setting up db, user, password, privilages

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,3 +4,11 @@
 --sets all privilages on the hbnb_dev_db for hbnb_dev (only in this db)
 --sets SELECT privilages on the db performance_schema
 --if db hbnb_dev_db || hbnb_dev exits, script should not fail
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES on hbnb_dev_db.* to 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
+
