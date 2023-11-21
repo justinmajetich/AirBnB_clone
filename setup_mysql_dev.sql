f9c7e207e9842ba8b0de754a220b38b6f2ef0cf6
Vulcanric <jneric49@gmail.com>
2023-11-21 14:02:47 +0100

    Fix: removed not so common environmental variable causes syntax error

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,22 +3,16 @@
 -- Create database if it doesn't exists
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
--- Set the password policy to LOW
-SET GLOBAL validate_password.policy = LOW;
-
 -- Create User if it doesn't exist
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED WITH mysql_native_password BY 'hbnb_dev_pwd';
-
--- Set the password policy to default (MEDIUM)
-SET GLOBAL validate_password.policy = MEDIUM;
-
--- Grant User hbnb_dev the ability to use any database
-GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
-
--- Grant User hbnb_dev, permission to perform all operations on the
--- database `hbnb_dev_db` and its tables
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grant User hbnb_dev, the privilege to query `performance_scheme`
 -- And its tables with SELECT
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Grant User hbnb_dev, all the privileges to perform operation on
+-- `hbnb_dev_db` and its tables
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost'
+
+-- Flush privileges
+FLUSH PRIVILEGES;
