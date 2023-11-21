a2e08a1be574506188e5716dfabbedd486f0c39a
timontuitoek <tuitoektimon@gmail.com>
2023-11-21 13:51:40 -0800

    changes

--- console.py
+++ console.py
@@ -82,7 +82,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -141,42 +141,33 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-         arg_list = args.split()
-        class_name = arg_list[0]
 
+        args_list = args.split()
+
+        class_name = args_list[0]
         if class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
 
-        param_dict = {}
-        for param in arg_list[1:]:
-            key_value = param.split('=')
-            if len(key_value) == 2:
-                key, value = key_value
-                if value.startswith('"') and value.endswith('"'):
-                    value = value[1:-1].replace('_', ' ').replace('\\"', '"')
-                elif '.' in value:
-                    try:
-                        value = float(value)
-                    except ValueError:
-                        print(f"Error: Invalid float value for parameter {key}")
-                        return
-                else:
-                    try:
-                        value = int(value)
-                    except ValueError:
-                        print(f"Error: Invalid integer value for parameter {key}")
-                        return
+        params = ' '.join(args_list[1:])
 
-                param_dict[key] = value
+        param_dict = {}
+        try:
+            for param in params.split(','):
+                key, value = param.split('=')
+                key = key.strip()
+                value = value.strip().replace('_', ' ').replace('"', '').replace('\\"', '"')
+        except ValueError:
+            print("** invalid format for parameters **")
+            return
+        param_dict[key] = value
 
         new_instance = HBNBCommand.classes[class_name]()
-    
         storage.save()
         print(new_instance.id)
         storage.save()
 
-        def help_create(self):
+    def help_create(self):
         """
         Help information for the create method
         """
@@ -342,7 +333,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] ==  '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -350,10 +341,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
