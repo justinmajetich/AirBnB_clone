6605ca826664624aeb1d67c54bb9d2750a520812
wagzyAyo <talktojmcvibes@gmail.com>
2023-11-21 00:10:21 -0800

    test sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,11 @@
+-- Create a hbnb_test_db database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create user hbnb_test if it doesn't exist, identified by password
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON  hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Grant all privileges to hbnb_test on hbnb_test_db database
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Grant SELECT privilege on performance_schema database
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
