8e985ec4ed3978f2b928adb9c9c2ac54cf3753dc
Elvis Otieno <125451537+the1Riddle@users.noreply.github.com>
2023-11-21 22:03:01 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from os import getenv
+"""from os import getenv"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+"""from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship"""
 
 
 class Amenity(BaseModel, Base):
