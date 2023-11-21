3e01db4fc801a3bbceb22eead37ec1771cfbb355
Ezra Yeneneh <105113257+ezrablackfish2@users.noreply.github.com>
2023-11-21 21:35:39 +0300

    Create user2.py

---
+++ models/user2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This is the user class"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
+
+
+class User(BaseModel, Base):
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+    """
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
