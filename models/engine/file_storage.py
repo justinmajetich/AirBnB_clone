2a2d7a9170024778904af884e6b67e5253e99bbe
ezra <ezrayeneneh1992@gmail.com>
2023-11-21 03:15:23 -0800

    thank god you are there

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,14 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         return FileStorage.__objects
+    def delete(self, obj=None):
+        """ deletes an object """
+        if obj:
+            key = "{}.{}".format(type(obj).__name__,obj.id)
+            del self.__objects[key]
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
