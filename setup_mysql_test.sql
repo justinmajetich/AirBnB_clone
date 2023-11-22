95c024951fd4a5e5203ca2315eb8864ff6136535
amoskarugo <karugoamos@gmail.com>
2023-11-22 12:45:13 +0000

    #

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- prepares a MySQL server for the project
+-- create a database, a new user and grant privileges
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
