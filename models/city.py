d85b9118556179b084dffd4f56852360dbd65fae
Abdelrahman Magdi <87670345+Abdorithm@users.noreply.github.com>
2023-11-22 18:50:21 +0200

    DBStorage - States and Cities (#13)
    * create with params
    
    * Added setup scripts and updated FileStorage (#9)
    
    * Hazem (#10)
    
    * create with params
    
    * Added doc
    
    * Added doc
    
    ---------
    
    Co-authored-by: Abdelrahman Magdi <abdomagdi300@gmail.com>
    Co-authored-by: Abdelrahman Magdi <87670345+Abdorithm@users.noreply.github.com>
    
    * Hazem (#12)
    
    * create with params
    
    * Added doc
    
    * Added doc
    
    * clean up
    
    * conflict
    
    ---------
    
    Co-authored-by: Abdelrahman Magdi <abdomagdi300@gmail.com>
    Co-authored-by: Abdelrahman Magdi <87670345+Abdorithm@users.noreply.github.com>
    
    * basemodel updated
    
    * basemodel updated
    
    * updated city & state
    
    ---------
    
    Co-authored-by: Hazem Osama <90433929+HazemUsama@users.noreply.github.com>

--- models/city.py
+++ models/city.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """ The city class, contains state ID and name
+
+    Atributes:
+        __tablename__: represents the tablename cities
+        name: represents a column containing a string (128 characters)
+        state_id: represents a column containing a string (60 characters)
+                  and a foreign key to states.id"""
+    __tablename__ = "cities"
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=false)
+    name = Column(String(128), nullable=false)
--- models/state.py
+++ models/state.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
+from models import storage
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """ State class
+
+    Attributes:
+        __tablename__: represents the table name, states
+        name: name of the state
+        cities: relationship to states"""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=false)
+    cities = relationship("City", backref="state", cascade="all, delete")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """getter attributes cities return a list of
+            cities within the current state"""
+            city_list = []
+            for city in list(storage.all(City).values()):
+                if self.id == city.state_id:
+                    city_list.append(city)
+            return city_list
