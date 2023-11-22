c4ec3249672232b7bf1cf6cd6d068af007d9814c
timontuitoek <tuitoektimon.com>
2023-11-22 15:30:22 +0300

    New changes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 
+import json
+
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
---
+++ setup_mysql_dev.sql
@@ -0,0 +1,14 @@
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant all privileges on the database hbnb_dev_db to the user hbnb_dev
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on the performance_schema database to the user hbnb_dev
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Flush privileges to apply the changes
+FLUSH PRIVILEGES;
