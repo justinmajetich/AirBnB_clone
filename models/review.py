c9fad77083bc462f69ad572b7fd351cddfb3ec6b
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2023-11-22 13:49:30 +0100

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,30 +1,10 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from sqlalchemy import ForeignKey
-import models
+from models.base_model import BaseModel
 
-class Review(BaseModel, Base):
-    """Representation of Review """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'reviews'
-        text = Column(String(1024),
-                      nullable=False)
-        place_id = Column(String(60),
-                          ForeignKey('places.id'),
-                          nullable=False)
-        user_id = Column(String(60),
-                         ForeignKey('users.id'),
-                         nullable=False)
-    else:
-        text = ""
-        place_id = ""
-        user_id = ""
 
-    def __init__(self, *args, **kwargs):
-        """initializes Review"""
-        super().__init__(*args, **kwargs)
+class Review(BaseModel):
+    """ Review classto store review information """
+    place_id = ""
+    user_id = ""
+    text = ""
