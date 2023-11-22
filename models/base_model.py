7341fdfa035021a3995a32dad5d1e3640f70f1a2
Elvis Otieno <125451537+the1Riddle@users.noreply.github.com>
2023-11-22 16:03:43 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""This module defines a base class for all models in our hbnb clone"""
+"""
+    This module defines a base class for all models in our hbnb clone
+"""
 import uuid
 from datetime import datetime
 import sys
