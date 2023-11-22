2772837910aadd7ecc31bd8ca40aff1765e7c202
emmanuel tindi <emmanueltindi66@gmail.com>
2023-11-22 23:17:34 +0300

    sql file update

--- console.py
+++ console.py
@@ -246,7 +246,7 @@ class HBNBCommand(cmd.Cmd):
         print(count)
 
     def help_count(self):
-        """ """
+        """help information for count"""
         print("Usage: count <class_name>")
 
     def do_update(self, args):
--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""database storage engine"""
+"""
+New engine DBStorage
+"""
 import os
 from models.base_model import Base
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, class_mapper
+from sqlalchemy.orm import sessionmaker
 from models.city import City
 from models.state import State
 from models.review import Review
@@ -11,61 +13,66 @@ from models.place import Place
 from models.user import User
 from models.amenity import Amenity
 
-user = os.environ['HBNB_MYSQL_USER'] if 'HBNB_MYSQL_USER' in os.environ else None
-password = os.environ['HBNB_MYSQL_PWD'] if 'HBNB_MYSQL_PWD' in os.environ else None
-host = os.environ['HBNB_MYSQL_HOST'] if 'HBNB_MYSQL_HOST' in os.environ else None
-database = os.environ['HBNB_MYSQL_DB'] if 'HBNB_MYSQL_DB' in os.environ else None
+db_user = os.environ['HBNB_MYSQL_USER'] if 'HBNB_MYSQL_USER' in os.environ else None
+db_pwd = os.environ['HBNB_MYSQL_PWD'] if 'HBNB_MYSQL_PWD' in os.environ else None
+db_host = os.environ['HBNB_MYSQL_HOST'] if 'HBNB_MYSQL_HOST' in os.environ else None
+db_name = os.environ['HBNB_MYSQL_DB'] if 'HBNB_MYSQL_DB' in os.environ else None
 test = os.environ['HBNB_ENV'] if 'HBNB_ENV' in os.environ else None
 
-class DBStorage():
-    """database engine"""
+class DBStorage:
+    """Database engine"""
     __engine = None
     __session = None
 
     def __init__(self):
-        """initialisation"""
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(user, password, host, database), pool_pre_ping=True)
+        """Initialisation"""
+
+        self.__engine = create_engine(
+            'mysql+mysqldb://{}:{}@{}/{}'.format(db_user, db_pwd, db_host, db_name),
+            pool_pre_ping=True
+        )
+
+
         if test == 'test':
             Base.metadata.drop_all(self.__engine)
 
         Base.metadata.create_all(self.__engine)
-        Session = sessionmaker(autocommit=False, autoflush=False, bind=self.__engine)
-        self.__session = Session()
 
+        SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=self.__engine)
+        self.__session = SessionLocal()
 
     def all(self, cls=None):
-        """Returns a dictionary of models currently in storage"""
-        objects = dict()
-        all_classes = (User, State, City, Amenity, Place, Review)
-        if cls is None:
-            for class_type in all_classes:
-                query = self.__session.query(class_type)
-                for obj in query.all():
-                    obj_key = '{}.{}'.format(obj.__class__.__name__, obj.id)
-                    objects[obj_key] = obj
+        """Returns a dictionary of all objects of the specified class or all classes"""
+        if cls:
+            objects = self.__session.query(cls).all()
         else:
-            query = self.__session.query(cls)
-            for obj in query.all():
-                obj_key = '{}.{}'.format(obj.__class__.__name__, obj.id)
-                objects[obj_key] = obj
-        return objects
+            objects = []
+            for model_class in Base.__subclasses__():
+                objects.extend(self.__session.query(model_class).all())
+
+        dictionary = {}
+        for obj in objects:
+            key = '{}.{}'.format(obj.__class__.__name__, obj.id)
+            dictionary[key] = obj
+
+        return dictionary
 
     def new(self, obj):
-        """adds new object to the database"""
-        self.__session_add(obj)
+        """Adds new object to the database"""
+        self.__session.add(obj)
 
     def save(self):
-        """commits changes to the current database session"""
+        """Commits all changes to the current database session"""
         self.__session.commit()
 
     def delete(self, obj=None):
-
-        """deletes object in the current session"""
-        self.__session.delete(obj)
-        self.save()
+        """Deletes the object from the current database session"""
+        if obj:
+            self.__session.delete(obj)
 
     def reload(self):
-        """creates all tables in the database"""
-        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        self.__session = Session()
+        """Reloads all models from the database"""
         Base.metadata.create_all(self.__engine)
+
+        SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=self.__engine)
+        self.__session = SessionLocal()
