e3f1296ba8c81fed23cac76d3bacbb0c1f5f3d84
Allison Ogechukwu <44110875+AllisonOge@users.noreply.github.com>
2023-11-22 07:34:59 +0000

    code refactoring

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,7 +20,7 @@ class FileStorage:
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        self.__objects.update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
         """Saves storage dictionary to file"""
