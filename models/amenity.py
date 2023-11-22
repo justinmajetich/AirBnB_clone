fa86de48010f52a836a94c0c3c1c573710000ee3
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2023-11-22 13:55:52 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,22 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-import models
-from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
 
-class Amenity(BaseModel, Base):
-    """Representation of Amenity """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'amenities'
-        name = Column(String(128),
-                      nullable=False)
-    else:
-        name = ""
 
-    def __init__(self, *args, **kwargs):
-        """initializes Amenity"""
-        super().__init__(*args, **kwargs)
+class Amenity(BaseModel):
+    name = ""
