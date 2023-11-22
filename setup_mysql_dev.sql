1949f3d49e7a71a61a676906c569757af60f2037
Only1allan <kariukiallan850@gmail.com>
2023-11-23 00:02:14 +0300

    Fixed checker bug

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,7 +4,7 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 -- create a user for the project
-CREATE USER IF NOT EXISTS 'hbnb_dev_'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- grant all privileges to the new user on the project database
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
