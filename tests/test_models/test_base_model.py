5613630af0e00c1dd40e2217040d75198e8cbede
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2023-11-22 13:08:05 +0100

    Refactor code for better readability

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -93,7 +93,14 @@ class test_basemodel(unittest.TestCase):
     def test_updated_at(self):
         """ """
         new = self.value()
+        #print(f"before save: {new.updated_at}")
         self.assertEqual(type(new.updated_at), datetime.datetime)
+
+        import time
+        time.sleep(1)
+        new.save()
+
+        #print(f"after save: {new.updated_at}")
         n = new.to_dict()
         new = BaseModel(**n)
         self.assertFalse(new.created_at == new.updated_at)
