ad9ad836a8b0ff2bf37670fa248fcb9f8fb3cda9
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-11-22 01:09:34 +0100

    Fixing comment section of mysql script

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,17 +1,18 @@
---A script that prepares a MySQL for the project.
+-- A script that prepares a MySQL for the project.
 
 CREATE USER IF NOT EXISTS
 'hbnb_dev'@'localhost'
 IDENTIFIED BY 'hbnb_dev_pwd';
+
+GRANT ALL PRIVILEGES
+ON hbnb_dev_db.*
+TO 'hbnb_dev'@'localhost'
+WITH GRANT OPTION;
 
 GRANT SELECT
 ON performanc_scheme.*
 TO 'hbnb_dev'@'localhost'
 WITH GRANT OPTION;
 
-GRANT ALL PRIVILEGES
-ON hbnb_dev_db.*
-TO 'hbnb_dev'@'localhost'
-WITH GRANT OPTION;
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -4,14 +4,12 @@ CREATE USER IF NOT EXISTS
 'hbnb_test'@'localhost'
 IDENTIFIED BY 'hbnb_test_pwd';
 
-GRANT SELECT
-ON performance_schema.*
-TO 'hbnb_test'@'localhost'
-WITH GRANT OPTION;
-
 GRANT ALL PRIVILEGES
 ON hbnb_test_db.*
-TO 'hbnb_test'@'localhost'
-WITH GRANT OPTION;
+TO 'hbnb_test'@'localhost';
+
+GRANT SELECT
+ON performance_schema.*
+TO 'hbnb_test'@'localhost';
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
