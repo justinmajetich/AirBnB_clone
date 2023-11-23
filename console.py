b796c2439839d4b61f74554595c0ed08bd0d7de4
nokekip <kipnoke@gmail.com>
2023-11-23 20:18:51 +0300

    import storage and Base

--- console.py
+++ console.py
@@ -6,13 +6,14 @@ Console Module
 
 import cmd
 import sys
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from models.user import User
 from models.place import Place
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
+from models.__init__ import storage
 
 
 
