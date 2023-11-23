2c23c83146ed5166ab819948fb244821304da628
Abdelrahman Magdi <abdomagdi300@gmail.com>
2023-11-23 04:57:16 +0200

    reviews style

--- models/review.py
+++ models/review.py
@@ -3,10 +3,10 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 
+
 class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = 'reviews'
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     text = Column(String(1024), nullable=False)
-
