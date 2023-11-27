f85eed74c2e53f374fc0790f366a46dafb63f01f
Mizuinu30 <juansilva.dvm@gmail.com>
2023-11-27 13:54:26 -0400

    hotfix

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is '}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -114,23 +114,40 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
-        """ Create an object of any class with given parameters"""
-        args = args.split(" ")
-        if not args:
+        """ Create an object of any class"""
+        arg = args.split()
+        if not arg:
             print("** class name missing **")
             return
-        elif args[0] not in HBNBCommand.classes:
+        elif arg[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args[0]]()
-        for arg in arg[1:]:
-            key, value = arg.split("=", 1)
-            if value[0] == '"':
-                value = value[1:-1].replace("_", " ").replace('\\"', '"')
+        new_instance = HBNBCommand.classes[arg[0]]()
+        for param in arg[1:]:
+            key, value = param.split('=')
+            # check if he key is a valid attribute
+            if not hasattr(new_instance, key):
+                continue
+            # Replace underscores with spaces
+            value = value.replace('_', ' ')
+            # Check if value is a string
+            if value[0] == '"' and value[-1] == '"':
+                # Remove quotes
+                value = value[1:-1]
+            # Check if value is a float
             elif '.' in value:
-                value = float(value)
+                try:
+                    value = float(value)
+                    # Check if value is an float
+                except ValueError:
+                    continue
+            # Treat it like an integer
             else:
-                value = int(value)
+                try:
+                    value = int(value)
+                    # Check if value is an integer
+                except ValueError:
+                    continue
             setattr(new_instance, key, value)
         storage.save()
         print(new_instance.id)
@@ -281,7 +298,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -289,10 +306,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
