70333dca284f08a77be539fd0340956d83c0bf9e
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-11-27 10:41:31 -0400

    Updates task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,15 +1,16 @@
 -- Prepare MySQL server
-
 -- Create database
-CREATE DATABSE IF NOT EXISTS 'hbnb_dev_db';
+CREATE DATABASE IF NOT EXISTS 'hbnb_dev_db';
 
 -- Create user
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev' @'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grant privileges
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev' @'localhost';
 
 -- Grant SELECT privileges
-GRANT SELECT ON 'performance_schema'.* TO 'hbnb_dev'@'localhost';
+GRANT
+SELECT
+    ON 'performance_schema'.* TO 'hbnb_dev' @'localhost';
 
 FLUSH PRIVILEGES;
