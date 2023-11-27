d7e74a7faf2f1be93c3e5d037417823b25fa04d4
Mizuinu30 <juansilva.dvm@gmail.com>
2023-11-27 15:51:06 -0400

    corrected gramar

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,9 @@
 --Prepares a MySQL database for the project
 CREATE DATABASE IF NOT EXISTS 'hbnb_dev_db';
 
-CREATE USER IF NOT EXISTS 'hbnb_dev' @'localhost' IDENTIFIED BY hbnb_dev_pwd;
+CREATE USER IF NOT EXISTS 'hbnb_dev' @'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev@localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev' @'localhost';
 
 GRANT
 SELECT
