0549eeb7c97189ee590406f3058d72fb1ed2373f
Mizuinu30 <juansilva.dvm@gmail.com>
2023-11-27 15:51:51 -0400

    corrected gramar

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,5 @@
---Prepares a MySQL database for the project
-CREATE DATABASE IF NOT EXISTS 'hbnb_dev_db';
+-- Prepares a MySQL server for the project.
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 CREATE USER IF NOT EXISTS 'hbnb_dev' @'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
