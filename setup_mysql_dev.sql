859e68b88ae497662e422f8b3e1b2327f2b7e85f
neisnei <neischalyruidiaz@gmail.com>
2023-11-27 18:46:28 +0000

    setup

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,2 @@
 #!/usr/bin/python3
 
-
--- Setup Database (development)
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* to 'hbnb_dev'@'localhost';
-
