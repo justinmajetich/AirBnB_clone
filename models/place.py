c5957736ddaff42a39ff8df4f1ec4f41cb691978
nadeshda18 <nadeshda02@hotmail.com>
2023-11-28 18:48:27 -0400

    added init

--- models/place.py
+++ models/place.py
@@ -5,15 +5,21 @@ from models.base_model import BaseModel
 
 class Place(BaseModel):
     """ A place to stay """
-    id = ""
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.city_id = ""
+        self.user_id = ""
+        self.name = ""
+        self.description = ""
+        self.number_rooms = 0
+        self.number_bathrooms = 0
+        self.max_guest = 0
+        self.price_by_night = 0
+        self.latitude = 0.0
+        self.longitude = 0.0
+        self.amenity_ids = []
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(type(self).__name__,
+                                     self.id, self.__dict__)
