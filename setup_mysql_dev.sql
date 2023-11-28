d96947dbc0f9bc461072e07146f67062b3cc832f
neisnei <neischalyruidiaz@gmail.com>
2023-11-28 18:15:13 +0000

    setup sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,5 +3,8 @@
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+USE hbnb_dev_db;
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+USE performance_schema;
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
