575a82b216f355b7cb21bc4cd8ecedd5631ebc8d
nataliagrivera <nataliagirvera62@gmail.com>
2023-11-28 15:59:58 -0400

    correcting spellng error

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -6,7 +6,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev' @'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grant privileges
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_dev' @'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev' @'localhost';
 
 -- Grant SELECT privileges
 GRANT
