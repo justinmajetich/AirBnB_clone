5459c6aa6d2c97d0c06d41f92a630dd0f9e965be
Joshua7792 <j.santiago39@hotmail.com>
2023-11-28 10:22:05 -0400

    users

--- models/user.py
+++ models/user.py
@@ -3,9 +3,10 @@
 from models.base_model import BaseModel
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
