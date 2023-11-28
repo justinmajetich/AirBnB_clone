abb0c8003ef6a3fea4e22c9964c677bfc67961f6
electrikbox <electrikbox@icloud.com>
2023-11-28 19:46:07 +0100

    update save to have indented json

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -15,15 +15,17 @@ class FileStorage:
     def new(self, obj):
         """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
-
+    
     def save(self):
         """Saves storage dictionary to file"""
         with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
-            json.dump(temp, f)
+            
+            temp = {
+                key: val.to_dict() for key,
+                val in FileStorage.__objects.items()
+            }
+            
+            json.dump(temp, f, indent=4)
 
     def reload(self):
         """Loads storage dictionary from file"""
