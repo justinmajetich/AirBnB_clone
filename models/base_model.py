319900935697eeccce9892dcc5d8fdc7bc8f5e17
neisnei <neischalyruidiaz@gmail.com>
2023-11-28 20:42:53 +0000

    model

--- models/base_model.py
+++ models/base_model.py
@@ -8,6 +8,13 @@ from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
 
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    Base = declarative_base()
+
+else:
+    Base = object
+
+
 Base = declarative_base()
 
 class BaseModel:
@@ -16,17 +23,36 @@ class BaseModel:
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def save(self):
+        self.updated_at = datetime.now()
         models.storage.new(self)
         models.storage.save()
 
     def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            if '__class__' in kwargs:
+                del kwargs['__class__']
+            self.__dict__.update(kwargs)
 
     def to_dict(self):
-        dictionary = self.__dict__.copy()
-        dictionary.pop('_sa_instance_state', None)
+        dictionary = {}
+        dictionary.update(self.__dict__)
+        dictionary.update({'__class__':
+                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
 
     def delete(self):
         models.storage.delete(self)
+
+    def __str__(self):
+        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
