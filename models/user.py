76ed594665dbec74569a28b98f4a2e4a260ddfe6
Joshua7792 <j.santiago39@hotmail.com>
2023-11-28 10:22:55 -0400

    imports

--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
-
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
