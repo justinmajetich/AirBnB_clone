f17028f391667bf47a97715deed4a3bd518acf84
neisnei <neischalyruidiaz@gmail.com>
2023-11-28 20:00:50 +0000

    add fix

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,16 +1,10 @@
-#!/usr/bin/python3
-"""
-script that prepares a MySQL server for the project
-"""
+-- prepares a MySQL server for the project
+-- GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
 
-# Create the database hbnb_dev_db
-echo "CREATE DATABASE hbnb_dev_db;" | mysql -u root -p
-
-# Create the user hbnb_dev
-echo "CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';" | mysql -u root -p
-
-# Grant all privileges on the database hbnb_dev_db to the user hbnb_dev
-echo "GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';" | mysql -u root -p
-
-# Grant SELECT privilege on the database performance_schema to the user hbnb_dev
-echo "GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';" | mysql -u root -p
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+USE hbnb_dev_db;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+USE performance_schema;
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
