e7babc11bcffefb8427a901596391faf6ed9b170
neisnei <neischalyruidiaz@gmail.com>
2023-11-29 14:52:29 +0000

    city

--- models/city.py
+++ models/city.py
@@ -4,6 +4,13 @@ from sqlalchemy import Column, String, ForeignKey
 from models.base_model import BaseModel, Base
 
 class City(BaseModel, Base):
+    """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place", backref="cities", cascade='all, delete')
+
+    else:
+        state_id = ""
+        name = ""
