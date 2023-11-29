89c3ef632af4e571c3e2293df87c6bda30b7db5b
Mizuinu30 <juansilva.dvm@gmail.com>
2023-11-29 17:05:16 -0400

    made changes to stae

--- models/state.py
+++ models/state.py
@@ -1,28 +1,14 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
-from os import getenv
-import models
-from models.city import City
+from models.base_model import BaseModel
+"""
+Module class: State
+"""
 
 
-class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship("City", backref="state",
-                              cascade="all, delete, delete-orphan")
-    else:
+class State(BaseModel):
+    """definition for class State"""
+    name = ""
 
-        @property
-        def cities(self):
-            """Getter attribute cities that returns the list of City instances"""
-            city_list = []
-            all_cities = models.storage.all(City)
-            for city in all_cities.values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+    def __init__(self, *args, **kwargs):
+        """ constructor method """
+        super().__init__(self, *args, **kwargs)
