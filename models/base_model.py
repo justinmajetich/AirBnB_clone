c18aa670923f4292d42eb317a5af5d8a1c88edd9
Mizuinu30 <juansilva.dvm@gmail.com>
2023-11-29 17:20:48 -0400

    tweeked Base

--- models/base_model.py
+++ models/base_model.py
@@ -49,6 +49,8 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if '_sa_instance_state' in dictionary:
+            del dictionary['_sa_instance_state']
         return dictionary
 
     def delete(self):
