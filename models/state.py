8f6ec143466b1406066293067ac45cc1dd82309e
neisnei <neischalyruidiaz@gmail.com>
2023-11-29 15:29:44 +0000

    state

--- models/state.py
+++ models/state.py
@@ -4,18 +4,22 @@ from sqlalchemy import Column, String,. Integer, Foreignkey
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 import models
+from models.city import City
 
 class State(BaseModel, Base):
+    """State Class"""
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-
-    if models.storage_type == 'db':
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
         cities = relationship("City", cascade="all, delete", backref="state")
-    elif models.storage_type == 'file':
+
+    else:
+        name = ' '
         @property
         def cities(self):
             city_list = []
-            for city in models.storage.all(City):
+            all_cities=models.storage.all(City)
+            for city in all_cities.values():
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
