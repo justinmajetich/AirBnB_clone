970e3cb3260a396c703b55a6c33220124ab17b35
neisnei <neischalyruidiaz@gmail.com>
2023-11-29 19:05:02 +0000

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -8,36 +8,41 @@ from datetime import datetime
 import models
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
-Base = declarative_base()
+    Base = declarative_base()
 
 else:
     Base = object
 
+
 class BaseModel:
+    """A base class for all hbnb models"""
     id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+
+    def __init__(self, *args, **kwargs):
+        """Instatntiates a new model"""
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            if '__class__' in kwargs:
+                del kwargs['__class__']
+            self.__dict__.update(kwargs)
 
     def save(self):
-        self.update_at = datetime.now()
+        """Updates updated_at with current time when instance is changed"""
+        self.updated_at = datetime.now()
         models.storage.new(self)
         models.storage.save()
 
-    def delete(self):
-        models.storage.delete(self)
-
-    def __init__(self, *args, **kwargs):
-        if kwargs:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-        if 'id' not in kwargs:
-            self.id = str(uuid.uuid4())
-        if 'created_at' not in kwargs:
-            self.created_at = datetime.utcnow()
-        if 'updated_at' not in kwargs:
-            self.updated_at = datetime.utcnow()
-
     def to_dict(self):
+        """Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
@@ -46,6 +51,11 @@ class BaseModel:
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
 
+    def delete(self):
+        """Delete current instance"""
+        models.storage.delete(self)
+
     def __str__(self):
+        """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
