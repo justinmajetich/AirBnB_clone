9dd99c5d7333560cff529267490231505aed9f38
Mizuinu30 <juansilva.dvm@gmail.com>
2023-11-29 19:05:57 -0400

    Add imports and init for inheritance

--- models/city.py
+++ models/city.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from models.place import Place
+import sqlalchemy
+from sqlalchemy import Column, String, DateTime, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
 
@@ -10,5 +14,8 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", backref="cities",
-                          cascade="all, delete, delete-orphan")
+    places = relationship("Place", backref="cities")
+
+    def __init__(self, *args, **kwargs):
+        """Init for inherited BaseModel"""
+        super().__init__(*args, **kwargs)
