fa46ff5a51a377995bc2a7b457d7a139273ad558
Mizuinu30 <juansilva.dvm@gmail.com>
2023-11-29 17:12:07 -0400

    update __init_

--- models/__init__.py
+++ models/__init__.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """
-module executes whn models package is imported
+Instantiates a storage object.
+
+-> If the environmental variable 'HBNB_TYPE_STORAGE' is set to 'db',
+   instantiates a database storage engine (DBStorage).
+-> Otherwise, instantiates a file storage engine (FileStorage).
 """
 from os import getenv
-from models.engine.db_storage import DBStorage
-from models.engine.file_storage import FileStorage
 
 
-if getenv('HBNB_TYPE_STORAGE') == 'db':
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
-    storage.reload()
-
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
-    storage.reload()
+storage.reload()
