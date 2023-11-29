5384704a6765bebc8d8f4fcce8f2567dcb928b0c
neisnei <neischalyruidiaz@gmail.com>
2023-11-29 12:57:52 +0000

    state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,18 @@
-#!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
-
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    
+    if models.storage_type == 'db':
+        cities = relationship("City", cascade="all, delete", backref="state")
+    elif models.storage_type == 'file':
+        @property
+        def cities(self):
+            city_list = []
+            for city in models.storage.all(City):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
