232f23e4c8e9ed731a2d3a6312b0e93e70c8d5a8
jess6718 <6718@holbertonstudents.com>
2023-11-29 07:18:31 +0100

    docs: no change

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
+"""This module defines a class to manage file storage for hbnb clone."""
 import json
 
 
