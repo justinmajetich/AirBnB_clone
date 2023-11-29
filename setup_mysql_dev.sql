f1c38078839b8ff574c5091e30960a970ca91a13
Gionas361 <giovannicarmona57@gmail.com>
2023-11-29 18:35:02 -0400

    Upload everything to my branch

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,19 @@
+-- Creates a MySQL server with:
+--   Database hbnb_dev_db.
+--   User hbnb_dev with password hbnb_dev_pwd in localhost.
+--   Grants all privileges for hbnb_dev on hbnb_dev_db.
+--   Grants SELECT privilege for hbnb_dev on performance_schema.
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER
+    IF NOT EXISTS 'hbnb_dev'@'localhost'
+    IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES
+   ON `hbnb_dev_db`.*
+   TO 'hbnb_dev'@'localhost'
+   IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT
+   ON `performance_schema`.*
+   TO 'hbnb_dev'@'localhost'
+   IDENTIFIED BY 'hbnb_dev_pwd';
+FLUSH PRIVILEGES;
