fe84fa3101ff0a029d598fd4aec86754a668d801
neisnei <neischalyruidiaz@gmail.com>
2023-11-29 16:38:20 +0000

    console

--- console.py
+++ console.py
@@ -213,8 +213,7 @@ class HBNBCommand(cmd.Cmd):
 
         key = c_name + "." + c_id
 
-        try:
-            del(storage.all()[key])
+        try: del(storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
