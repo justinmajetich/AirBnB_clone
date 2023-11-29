730b43f0a71a1724facb4f74105253a3e5f89bfb
neisnei <neischalyruidiaz@gmail.com>
2023-11-29 18:51:11 +0000

    storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,25 +10,58 @@ from models.amenity import Amenity
 from models.review import Review
 
 class FileStorage:
-
-    def __init__(self):
-        self.__objects = {}
-
-    def new(self, obj):
-        self.__objects[obj.id] = obj
-
-    def save(self):
-        with open("file.json", "w") as f:
-            json.dump(self.__objects, f)
-
-    def get(self, id):
-        return self.__objects[id]
-
-    def delete(self, obj=None):
-        if obj is not None:
-            del self.__objects[obj.id]
+    """This class manages storage of hbnb models in JSON format"""
+    __file_path = 'file.json'
+    __objects = {}
 
     def all(self, cls=None):
+        """Returns a dictionary of models currently in storage, if a class
+        is specified, it returns of objects of said class"""
         if cls is None:
-            return self.__objects.values()
-        return {k: v for k, v in self.__objects.items() if isinstance(v, cls)}
+            return FileStorage.__objects
+        dir_same_cls = {}
+        for key, value in FileStorage.__objects.items():
+            if value.__class__ == cls:
+                dir_same_cls[key] = value
+        return dir_same_cls
+
+    def new(self, obj):
+        """Adds new object to storage dictionary"""
+        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+
+    def save(self):
+        """Saves storage dictionary to file"""
+        with open(FileStorage.__file_path, 'w') as f:
+            temp = {}
+            temp.update(FileStorage.__objects)
+            for key, val in temp.items():
+                temp[key] = val.to_dict()
+            json.dump(temp, f)
+
+    def delete(self, obj=None):
+        """Deletes object from storage"""
+        if obj is None:
+            return
+
+        key = obj.to_dict()['__class__'] + '.' + obj.id
+        if key in FileStorage.__objects:
+            del FileStorage.__objects[key]
+
+    def reload(self):
+        classes = {
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
+        try:
+            temp = {}
+            with open(FileStorage.__file_path, 'r') as f:
+                temp = json.load(f)
+                for key, val in temp.items():
+                    self.all()[key] = classes[val['__class__']](**val)
+        except FileNotFoundError:
+            pass
+
+    def close(self):
+        """ Deserialize JSON to objects """
+        self.reload()
