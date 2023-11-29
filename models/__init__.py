49cc47066299cd0f73e093b28d57c69804958b97
electrikbox <electrikbox@icloud.com>
2023-11-29 22:09:59 +0100

    handle switching storage type

--- models/__init__.py
+++ models/__init__.py
@@ -1,35 +1,18 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
+"""This module instantiates an object of
+class FileStorage
+or
+class DBStorage
+"""
+
+from os import getenv
 
 
-storage = FileStorage()
-storage.reload()
-
-classes = {
-    'BaseModel': BaseModel,
-    'User': User,
-    'Place': Place,
-    'State': State,
-    'City': City,
-    'Amenity': Amenity,
-    'Review': Review
-}
-
-dot_cmds = ['all', 'count', 'show', 'destroy', 'update']
-
-types = {
-    'number_rooms': int,
-    'number_bathrooms': int,
-    'max_guest': int,
-    'price_by_night': int,
-    'latitude': float,
-    'longitude': float
-}
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
