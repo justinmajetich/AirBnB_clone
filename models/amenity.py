f64d82d67f7dae5a18c6c357f37fbee188cc95d2
Uwe Flumm <ucflumm@gmail.com>
2023-12-01 10:16:34 +1100

    Update Amenity model to use db storage and associated imports

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""
+    State Module for HBNB project
+    *Update 1/12/2023: added code to use db storage and associated imports
+        part of question 10
+"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
+import os
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary="place_amenity",
+                                       viewonly=False)
+    else:
+        name = ""
