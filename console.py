acf061518f47c1e5ddad446b84ede248fc7fe35f
jess6718 <6718@holbertonstudents.com>
2023-11-30 04:04:14 +0100

    fix execution

--- console.py
+++ console.py
@@ -4,12 +4,12 @@ import cmd
 import sys
 from models.base_model import BaseModel
 from models.__init__ import storage
-from models.user import User
-from models.place import Place
+# from models.user import User
+# from models.place import Place
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
-from models.review import Review
+# from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
@@ -19,9 +19,8 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) ' if sys.__stdin__.isatty() else ''
 
     classes = {
-               'BaseModel': BaseModel, 'User': User, 'Place': Place,
-               'State': State, 'City': City, 'Amenity': Amenity,
-               'Review': Review
+               'BaseModel': BaseModel,
+               'State': State, 'City': City, 'Amenity': Amenity
               }
     dot_cmds = ['all', 'count', 'show', 'destroy', 'update']
     types = {
--- models/user.py
+++ models/user.py
@@ -1,26 +1,27 @@
-#!/usr/bin/python3
-"""
-    This module defines a class User
-    *Update 30/11/2023: added code to use db storage and associated imports
-"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-import os
+# #!/usr/bin/python3
+# """
+#     This module defines a class User
+#     *Update 30/11/2023: added code to use db storage and associated imports
+# """
+# from models.base_model import BaseModel, Base
+# from sqlalchemy import Column, String
+# from sqlalchemy.orm import relationship
+# import os
 
 
-class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship('Place', cascade='all, delete', backref='user')
-        reviews = relationship('Review', cascade='all, delete', backref='user')
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+# class User(BaseModel, Base):
+#     """This class defines a user by various attributes"""
+#     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+#         __tablename__ = 'users'
+#         email = Column(String(128), nullable=False)
+#         password = Column(String(128), nullable=False)
+#         first_name = Column(String(128), nullable=True)
+#         last_name = Column(String(128), nullable=True)
+#         places = relationship('Place', cascade='all, delete', backref='user')
+#         reviews = relationship('Review', cascade='all, delete',
+# backref='user')
+#     else:
+#         email = ''
+#         password = ''
+#         first_name = ''
+#         last_name = ''
