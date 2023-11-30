56ce46cd75633b9346aceacc2a1ab23b33088a84
jess6718 <6718@holbertonstudents.com>
2023-11-30 03:08:24 +0100

    feat: add state - file storage

--- models/state.py
+++ models/state.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 import os ## double check circular import
 
 class State(BaseModel, Base):
 
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = "states"
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state', cascade='all, delete')
+        cities = relationship('City', backref='state', cascade='all,\
+                              delete-orphan')
     else:
-        name = ""
+        @property
+        def cities(self):
+            """return list of City instances where state_id = State.id"""
+            from models import storage
+            from models.city import City
+            cities_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
