e9046bffc5066bc423684d2b420ae03da9d468fd
Mizuinu30 <juansilva.dvm@gmail.com>
2023-11-30 07:23:44 -0400

    hf to city

--- models/city.py
+++ models/city.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-import models
 from models.base_model import BaseModel, Base
-from models.place import Place
-import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
 
@@ -14,10 +10,5 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", backref="cities")
-
-    def __init__(self, *args, **kwargs):
-        """
-            Init for inherited
-        """
-        super().__init__(*args, **kwargs)
+    places = relationship("Place", backref="cities",
+                          cascade="all, delete, delete-orphan")
