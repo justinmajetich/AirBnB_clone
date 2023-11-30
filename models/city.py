51720c12851a20b307726a01cb79d2a3d575e760
Mizuinu30 <juansilva.dvm@gmail.com>
2023-11-29 21:02:03 -0400

    reverted changes

--- models/city.py
+++ models/city.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-import models
 from models.base_model import BaseModel, Base
-from models.place import Place
-import sqlalchemy
-from sqlalchemy import Column, String, DateTime, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
@@ -14,8 +10,5 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", backref="cities")
-
-    def __init__(self, *args, **kwargs):
-        """Init for inherited BaseModel"""
-        super().__init__(*args, **kwargs)
+    places = relationship("Place", backref="cities",
+                          cascade="all, delete, delete-orphan")
