97a50755ee0bff5b881c7683172290e8bfb4f8e8
Uwe Flumm <ucflumm@gmail.com>
2023-12-01 10:17:09 +1100

    Refactor Amenity class to use 'amenities' as the table name

--- models/amenity.py
+++ models/amenity.py
@@ -11,8 +11,8 @@ import os
 
 
 class Amenity(BaseModel, Base):
+    __tablename__ = 'amenities'
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
         place_amenities = relationship("Place", secondary="place_amenity",
                                        viewonly=False)
