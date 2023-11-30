21d4409f4a25611807a0fd4d3b7cca86a82f2f99
nataliagrivera <nataliagirvera62@gmail.com>
2023-11-30 10:01:53 -0400

    Remove unnecessary blank line in FileStorage class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -28,7 +28,6 @@ class FileStorage:
         """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
-
     def save(self):
         """Saves storage dictionary to file"""
         with open(FileStorage.__file_path, 'w') as f:
