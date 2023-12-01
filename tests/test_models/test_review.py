0713fa60946d79a336ef2e4284fa1c64154e1649
Mizuinu30 <juansilva.dvm@gmail.com>
2023-12-01 18:05:24 -0400

    Add test for inherantance methods and errorhandling

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -2,6 +2,7 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
+from models import BaseModel
 
 
 class test_review(test_basemodel):
@@ -13,17 +14,38 @@ class test_review(test_basemodel):
         self.name = "Review"
         self.value = Review
 
-    def test_place_id(self):
-        """ """
+    def test_place_id_value(self):
+        """Test place_id value of Review"""
         new = self.value()
-        self.assertEqual(type(new.place_id), str)
+        new.place_id = "1234"
+        self.assertEqual(new.place_id, "1234")
 
-    def test_user_id(self):
-        """ """
+    def test_user_id_value(self):
+        """Test user_id value of Review"""
         new = self.value()
-        self.assertEqual(type(new.user_id), str)
+        new.user_id = "5678"
+        self.assertEqual(new.user_id, "5678")
 
-    def test_text(self):
-        """ """
+    def test_text_value(self):
+        """Test text value of Review"""
         new = self.value()
-        self.assertEqual(type(new.text), str)
+        new.text = "Test review"
+        self.assertEqual(new.text, "Test review")
+
+    def test_update_text(self):
+        """Test update_text method of Review"""
+        new = self.value()
+        new.text = "Initial review"
+        new.update_text("Updated review")
+        self.assertEqual(new.text, "Updated review")
+
+    def test_inheritance(self):
+        """Test that Review correctly inherits from BaseModel"""
+        new = self.value()
+        self.assertIsInstance(new, BaseModel)
+
+    def test_place_id_type(self):
+        """Test place_id type error handling of Review"""
+        new = self.value()
+        with self.assertRaises(TypeError):
+            new.place_id = 1234  # Attempt to set place_id to an integer
