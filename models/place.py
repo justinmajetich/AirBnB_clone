c906a28744dda28747e526ca2b42096a4bea91d0
Uwe Flumm <ucflumm@gmail.com>
2023-12-01 12:31:08 +1100

    Commented out unused place_amenity table definition

--- models/place.py
+++ models/place.py
@@ -17,13 +17,13 @@ class Place(BaseModel, Base):
     __tablename__ = 'places'
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
 
-        place_amenity = Table(
-            'place_amenity', Base.metadata,
-            Column('place_id', String(60), ForeignKey('places.id'),
-                   primary_key=True, nullable=False),
-            Column('amenity_id', String(60), ForeignKey('amenities.id'),
-                   primary_key=True, nullable=False)
-        )
+        # place_amenity = Table(
+        #     'place_amenity', Base.metadata,
+        #     Column('place_id', String(60), ForeignKey('places.id'),
+        #            primary_key=True, nullable=False),
+        #     Column('amenity_id', String(60), ForeignKey('amenities.id'),
+        #            primary_key=True, nullable=False)
+        # )
 
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
