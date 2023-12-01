bf28894e2f00efb2ff16af5e8f43d7714ae59f36
jess6718 <6718@holbertonstudents.com>
2023-12-01 13:52:33 +0100

    fix pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -34,7 +34,6 @@ class BaseModel:
             if 'id' not in kwargs:
                 setattr(self, 'id', str(uuid.uuid4()))
 
-
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
