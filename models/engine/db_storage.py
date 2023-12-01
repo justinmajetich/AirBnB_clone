db11c327f9414e2699283881206f918d71740386
Uwe Flumm <ucflumm@gmail.com>
2023-12-01 11:28:03 +1100

    Add Place class to DBStorage all method

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -41,7 +41,7 @@ class DBStorage:
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         if cls is None:
-            classes = [User, State, City, Amenity, Review]
+            classes = [User, State, City, Amenity, Review, Place]
             dict = {}
             for c in classes:
                 for obj in self.__session.query(c):
