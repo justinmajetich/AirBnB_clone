facda7901eafe0cc49628cf08d66f8bd0fd4d8d1
Mizuinu30 <juansilva.dvm@gmail.com>
2023-12-01 20:38:39 -0400

    more tests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -14,6 +14,13 @@ class test_User(test_basemodel):
         self.name = "User"
         self.value = User
 
+    def test_email(self):
+        # Create a new instance of the User model
+        new_user = User(email='test@example.com', other_attribute='value')
+
+        # Access the email attribute and check its type
+        self.assertEqual(type(new_user.email), str)
+
     def test_first_name(self):
         """ """
         new = self.value()
@@ -33,6 +40,13 @@ class test_User(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.password), str)
+
+    def test_first_name(self):
+        # Create a new instance of the User model
+        new_user = User(first_name='John', other_attribute='value')
+
+        # Access the first_name attribute and check its type
+        self.assertEqual(type(new_user.first_name), str)
 
 
 if __name__ == '__main__':
