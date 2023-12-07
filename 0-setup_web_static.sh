341185c55f8293e4e3ad4433bedc4cc98ef4674d
Ayo <106088806+wagzyAyo@users.noreply.github.com>
2023-12-07 12:49:07 +0100

    Create 0-setup_web_static.sh

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# Bash script that sets up your web servers for the deployment of web_static
+
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
+
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello world!</h1>
+</body>
+</html>" | sud0 tee /data/web_static/releases/test/index.html
+
+sudo ln -s -f  /data/web_static/releases/test/  /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
+sudo service nginx restart
