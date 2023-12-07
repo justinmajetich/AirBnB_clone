baf39f55e956f8c43f388aab790f31087c6b4649
ezra <ezrayeneneh1992@gmail.com>
2023-12-07 00:42:24 -0800

    holberton school

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -16,7 +16,7 @@ sudo mkdir /data/web_static/releases/
 sudo mkdir /data/web_static/shared/
 sudo mkdir /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
-sudo echo "<h1> my name is ezra</h1>" >> /data/web_static/releases/test/index.html
+sudo echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu /data/
 sudo chgrp -R ubuntu /data/
