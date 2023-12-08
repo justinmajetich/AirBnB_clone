a977faefc2c37a6e76c90d252462ed100438c2fa
Ayo <106088806+wagzyAyo@users.noreply.github.com>
2023-12-08 15:05:19 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -23,25 +23,24 @@ def do_deploy(archive_path):
 
                 # create target dir
                 timestamp = archive_path[-18:-4]
-                run('sudo mkdir -p /data/web_static/\releases/web_static_{}/
-                '.format(timestamp))
+                run('sudo mkdir -p /data/web_static/\
+releases/web_static_{}/'.format(timestamp))
 
                 # uncompress archive and delete .tgz
                 run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
-                /data/web_static/releases/web_static_{}/'
+/data/web_static/releases/web_static_{}/'
                     .format(timestamp, timestamp))
 
                 # remove archive
                 run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
 
                 # move contents into host web_static
-                run('sudo mv /data/web_static/releases/web_static_{}
-                /web_static/* \/data/web_static/releases/web_static_{}/'
-                .format(timestamp, timestamp))
+                run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
+/data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
 
                 # remove extraneous web_static dir
                 run('sudo rm -rf /data/web_static/releases/\
-                web_static_{}/web_static'
+web_static_{}/web_static'
                     .format(timestamp))
 
                 # delete pre-existing sym link
@@ -49,7 +48,7 @@ def do_deploy(archive_path):
 
                 # re-establish symbolic link
                 run('sudo ln -s /data/web_static/releases/\
-                web_static_{}/ /data/web_static/current'.format(timestamp))
+web_static_{}/ /data/web_static/current'.format(timestamp))
         except:
                 return False
 
