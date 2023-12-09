5020ddaaa5aa2ab332f7569c01cc84ee30f549b8
timontuitoek <tuitoektimon.com>
2023-12-09 22:01:32 +0300

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,30 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# This script sets up web servers for the deployment of web_static.
 
 # Install Nginx if not already installed
-sudo apt-get update
-sudo apt-get -y install nginx
+if ! command -v nginx &> /dev/null; then
+    sudo apt-get -y update
+    sudo apt-get -y install nginx
+fi
 
-# Create necessary directories if they don't exist
+# Create necessary folders if they don't exist
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 
-# Create a fake HTML file for testing
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Test Page for Nginx Deployment
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+# Create a fake HTML file
+echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html
 
-# Create or recreate the symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Create symbolic link, delete if already exists
+sudo rm -rf /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 
-# Give ownership of /data/ to the ubuntu user and group recursively
+# Give ownership of /data/ to ubuntu user and group recursively
 sudo chown -R ubuntu:ubuntu /data/
 
-# Update Nginx configuration to serve content from /data/web_static/current/
-sudo sed -i "/^\s*location \/ {/a \\\talias /data/web_static/current/;" /etc/nginx/sites-available/default
+# Update Nginx configuration
+config="location /hbnb_static {\n\talias /data/web_static/current/;\n\t}\n"
+sudo sed -i "/server_name _;/a $config" /etc/nginx/sites-available/default
 
-# Restart Nginx to apply changes
+# Restart Nginx
 sudo service nginx restart
