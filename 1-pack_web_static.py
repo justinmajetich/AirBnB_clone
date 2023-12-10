297566dfa07b69db3988f7536189f834775970ca
timontuitoek <tuitoektimon.com>
2023-12-10 10:49:59 +0300

    Compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Fabric script to generate a .tgz archive from the contents of the web_static folder.
+"""
+
+from fabric.api import local
+from datetime import datetime
+import os
+
+def do_pack():
+    """
+    Generates a .tgz archive from the web_static folder.
+    """
+    try:
+        # Create versions folder if it doesn't exist
+        local("mkdir -p versions")
+
+        # Generate archive filename
+        now = datetime.utcnow()
+        archive_name = "web_static_{}{}{}{}{}{}.tgz".format(
+            now.year, now.month, now.day, now.hour, now.minute, now.second)
+
+        # Create the .tgz archive
+        local("tar -cvzf versions/{} web_static".format(archive_name))
+
+        # Return the archive path if successful
+        return os.path.join("versions", archive_name)
+    except Exception:
+        return None
