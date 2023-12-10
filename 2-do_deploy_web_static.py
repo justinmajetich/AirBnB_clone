56c5024dadf7745312195ae0045e34408ba95f4e
timontuitoek <tuitoektimon.com>
2023-12-10 14:20:16 +0300

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-"""Fabric script to distribute an archive to web servers"""
+"""
+Fabric script to distribute an archive to web servers.
+"""
 
-from fabric.api import env, run, put
+from fabric.api import env, put, run
 from os.path import exists
 
 env.hosts = ['18.210.15.7', '54.160.102.195']
+env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to web servers"""
+    """
+    Distributes an archive to web servers.
+    """
 
     if not exists(archive_path):
         return False
@@ -27,8 +32,10 @@ def do_deploy(archive_path):
         # Remove the uploaded archive
         run('rm /tmp/{}'.format(archive_filename))
 
-        # Create a symbolic link
+        # Remove the old symbolic link
         run('rm -f /data/web_static/current')
+
+        # Create a new symbolic link
         run('ln -s {} /data/web_static/current'.format(release_path))
 
         print("New version deployed!")
