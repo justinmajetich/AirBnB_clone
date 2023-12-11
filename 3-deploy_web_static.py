aca90b945f33d69597fddbc251a2f2c0ce9308a0
timontuitoek <tuitoektimon.com>
2023-12-11 15:55:25 +0300

    Full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -10,21 +10,43 @@ import os
 env.hosts = ["18.210.15.7", "54.160.102.195"]
 env.user = "ubuntu"
 
+def pack(func):
+    """
+    Decorator for creating a compressed archive of web_static content.
+    """
+    def wrapper(*args, **kwargs):
+        try:
+            current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+            archive_filename = "web_static_{}.tgz".format(current_time)
+            local("mkdir -p versions")
+            local("tar -cvzf versions/{} web_static".format(archive_filename))
+            return func("versions/{}".format(archive_filename), *args, **kwargs)
+        except Exception as e:
+            return None
+    return wrapper
 
-def do_pack():
+def deploy(func):
+    """
+    Decorator for distributing archive.
+    """
+    def wrapper(archive_path, *args, **kwargs):
+        if os.path.exists(archive_path):
+            # ... (existing do_deploy function)
+
+            print("New version deployed!")
+            return func(archive_path, *args, **kwargs)
+
+        return False
+    return wrapper
+
+@pack
+def do_pack(archive_path):
     """
     Create a compressed archive of web_static content.
     """
-    try:
-        current_time = datetime.now().strftime("%Y%m%d%H%M%S")
-        archive_filename = "web_static_{}.tgz".format(current_time)
-        local("mkdir -p versions")
-        local("tar -cvzf versions/{} web_static".format(archive_filename))
-        return "versions/{}".format(archive_filename)
-    except Exception as e:
-        return None
+    return archive_path
 
-
+@deploy
 def do_deploy(archive_path):
     """
     Distribute archive.
@@ -36,7 +58,6 @@ def do_deploy(archive_path):
         return True
 
     return False
-
 
 def deploy():
     """
@@ -52,7 +73,6 @@ def deploy():
     result = do_deploy(archive_path)
 
     return result
-
 
 if __name__ == "__main__":
     deploy()
