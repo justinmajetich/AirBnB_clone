9ef2ff0a45e4cdb2e904bd0012bc59eec5bfee7e
Mizuinu30 <juansilva.dvm@gmail.com>
2023-12-12 19:49:39 -0400

    made chages to the template

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Script that starts a Flask web application"""
+"""
+Script that starts a Flask web application.
+"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -7,18 +9,52 @@ from models.state import State
 app = Flask(__name__)
 
 
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Displays a HTML page with a list of all State objects"""
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda x: x.name)
-
-    return render_template('7-states_list.html', states=sorted_states)
+    """Display a HTML page"""
+    states_li = storage.all(State).values()
+    return render_template('7-states_list.html', states=states_li)
 
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
-    """Closes the current SQLAlchemy session"""
+    """After each request, remove the SQLAlchemy Session"""
     storage.close()
 
 
