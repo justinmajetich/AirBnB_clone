3ebf1532cb6dd9911a8c2f4e7cbc8b2de9e0a124
Mizuinu30 <juansilva.dvm@gmail.com>
2023-12-14 18:08:23 -0400

    styles

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,106 +1,74 @@
 #!/usr/bin/python3
-"""
-Contains the class DBStorage
-"""
+"""New engine DBStorage"""
 
-import models
-from models.amenity import Amenity
-from models.base_model import BaseModel, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from os import getenv
+from models.base_model import Base
+from models.user import User
+from models.state import State
 from models.city import City
 from models.place import Place
 from models.review import Review
-from models.state import State
-from models.user import User
-from os import getenv
-import sqlalchemy
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from models.amenity import Amenity
 
-classes = {"Amenity": Amenity, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
+classes = {"State": State, "City": City, "User": User,
+           "Place": Place, "Review": Review, "Amenity": Amenity}
 
 
 class DBStorage:
-    """interaacts with the MySQL database"""
+    """DBStorage class"""
     __engine = None
     __session = None
 
     def __init__(self):
-        """Instantiate a DBStorage object"""
-        HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
-        HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
-        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
-        HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
-        HBNB_ENV = getenv('HBNB_ENV')
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
-                                      format(HBNB_MYSQL_USER,
-                                             HBNB_MYSQL_PWD,
-                                             HBNB_MYSQL_HOST,
-                                             HBNB_MYSQL_DB))
-        if HBNB_ENV == "test":
+        """DBStorage engine constructor"""
+        user = getenv("HBNB_MYSQL_USER")
+        pwd = getenv("HBNB_MYSQL_PWD")
+        host = getenv("HBNB_MYSQL_HOST")
+        db = getenv("HBNB_MYSQL_DB")
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(user, pwd, host, db),
+                                      pool_pre_ping=True)
+        if getenv('HBNB_ENV') == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """query on the current database session"""
+        """all method"""
         new_dict = {}
-        for clss in classes:
-            if cls is None or cls is classes[clss] or cls is clss:
-                objs = self.__session.query(classes[clss]).all()
-                for obj in objs:
-                    key = obj.__class__.__name__ + '.' + obj.id
+        if cls is None:
+            for key, value in classes.items():
+                for obj in self.__session.query(value).all():
+                    key = obj.__class__.__name__ + "." + obj.id
                     new_dict[key] = obj
-        return (new_dict)
+        else:
+            for obj in self.__session.query(cls).all():
+                key = obj.__class__.__name__ + "." + obj.id
+                new_dict[key] = obj
+        return new_dict
 
     def new(self, obj):
-        """add the object to the current database session"""
+        """new method"""
         self.__session.add(obj)
 
     def save(self):
-        """commit all changes of the current database session"""
+        """save method"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """delete from the current database session obj if not None"""
+        """delete method"""
         if obj is not None:
             self.__session.delete(obj)
 
     def reload(self):
-        """reloads data from the database"""
+        """reload method"""
         Base.metadata.create_all(self.__engine)
-        sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(sess_factory)
-        self.__session = Session
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
 
     def close(self):
-        """call remove() method on the private session attribute"""
-        self.__session.remove()
-
-    def get(self, cls, id):
+        """ calls remove()
         """
-        Returns the object based on the class name and its ID, or
-        None if not found
-        """
-        if cls not in classes.values():
-            return None
-
-        all_cls = models.storage.all(cls)
-        for value in all_cls.values():
-            if (value.id == id):
-                return value
-
-        return None
-
-    def count(self, cls=None):
-        """
-        count the number of objects in storage
-        """
-        all_class = classes.values()
-
-        if not cls:
-            count = 0
-            for clas in all_class:
-                count += len(models.storage.all(clas).values())
-        else:
-            count = len(models.storage.all(cls).values())
-
-        return count
+        self.__session.close()
--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,29 +1,37 @@
 #!/usr/bin/python3
-""" This script that starts a Flask web application """
+"""
+Starts a Flask web application
+"""
 
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models.city import City
 from models.amenity import Amenity
-
+from os import environ
 
 app = Flask(__name__)
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
-def hbnb_filters():
-    """Display filters in HTML page"""
-    state = storage.all(State).values()
-    amenity = storage.all(Amenity).values()
-    return render_template('10-hbnb_filters.html', state=state, amenity=amenity)
-
-
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """ Function that removes the current SQL Alchemy Session after each
-    request. """
+def teardown_appcontext(self):
+    """
+    Teardown method for app context
+    """
     storage.close()
 
 
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    """
+    Display HTML page with filters
+    """
+    states = storage.all(State).values()
+    cities = storage.all(City).values()
+    amenities = storage.all(Amenity).values()
+
+    return render_template('10-hbnb_filters.html', states=states, cities=cities, amenities=amenities)
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
