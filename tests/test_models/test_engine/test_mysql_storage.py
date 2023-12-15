05fb09c6457a5b63ce41eb73e50caba0f24484c5
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-12-15 10:53:50 +0200

    setup_mysql_test.sql: script that prepares a MySQL test server

---
+++ tests/test_models/test_engine/test_mysql_storage.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Module for testing mysql storage"""
+import unittest
+from models.base_model import BaseModel
+import MySQLdb
+import os
+
+
+class test_mysqlStorage(unittest.TestCase):
+    """ Class to test the mysql storage method """
+
+    def setUp(self):
+        """ Set up test environment """
+        host = os.environ.get("HBNB_MYSQL_HOST")
+        user = os.environ.get("HBNB_MYSQL_USER")
+        password = os.environ.get("HBNB_MYSQL_PWD")
+        test_db = os.environ.get("HBNB_MYSQL_DB")
+
+        self.db = MySQLdb.connect(host=host, user=user, passwd=password, db=test_db)
+        self.cursor = self.db.cursor()
+
+    def tearDown(self):
+        """ Close the database connection after the test """
+        self.db.close()
+
