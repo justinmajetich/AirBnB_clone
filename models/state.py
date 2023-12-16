0322e5c80428094a39ea1fa9c92270e996215d09
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-12-16 13:17:08 +0200

    Update City: (models/city.py) && Update State: (models/state.py)

--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
+from models import city
+import os
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref="state",
+                              cascade="all, delete-orphan")
+    else:
+       @property
+       def cities(self):
+            """
+            Getter attribute that returns the list of City instances
+            with state_id equals to the current State.id.
+            """
+            from models import storage
+            city_list = storage.all(city.City).values()
+            return [city for city in city_list if city.state_id == self.id]
