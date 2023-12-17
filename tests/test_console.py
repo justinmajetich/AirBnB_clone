24b9055efce8a3398f6a0aca02d0293286031390
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-12-17 05:56:52 -0800

    Added more classes to do_create unittest

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,9 +1,6 @@
 import unittest
 from unittest.mock import patch
 from console import HBNBCommand
-import unittest
-from unittest.mock import patch
-from console import HBNBCommand
 
 
 class TestCreateFunction(unittest.TestCase):
@@ -28,7 +25,25 @@ class TestCreateFunction(unittest.TestCase):
             self.hbnb_command_instance.do_create("create State name=\"California\"")
         self.assertEqual(str(context.exception), "** class doesn't exist **")
 
-    # Update other tests similarly...
+    def test_create_city(self):
+        with self.assertRaises(HBNBCommand.CommandError) as context:
+            self.hbnb_command_instance.do_create("create City state_id=123 name=\"San Francisco\"")
+        self.assertEqual(str(context.exception), "** class doesn't exist **")
+
+    def test_create_place(self):
+        with self.assertRaises(HBNBCommand.CommandError) as context:
+            self.hbnb_command_instance.do_create("create Place city_id=456 user_id=789 name=\"Modern Loft\" price_per_night=150.5")
+        self.assertEqual(str(context.exception), "** class doesn't exist **")
+
+    def test_create_amenity(self):
+        with self.assertRaises(HBNBCommand.CommandError) as context:
+            self.hbnb_command_instance.do_create("create Amenity name=\"Wi-Fi\"")
+        self.assertEqual(str(context.exception), "** class doesn't exist **")
+
+    def test_create_review(self):
+        with self.assertRaises(HBNBCommand.CommandError) as context:
+            self.hbnb_command_instance.do_create("create Review place_id=101 user_id=202 text=\"Lovely stay!\"")
+        self.assertEqual(str(context.exception), "** class doesn't exist **")
 
 if __name__ == '__main__':
     unittest.main()
