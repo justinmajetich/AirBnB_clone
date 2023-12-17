1e4964b5f9afe40283e1c3bbf89717c01f84b23c
Matrix30 <Matrixboy30@gmail.com>
2023-12-17 23:46:20 +0100

    add place and review relation to user model

--- models/user.py
+++ models/user.py
@@ -1,31 +1,23 @@
 #!/usr/bin/python3
-"""
-    module containing user class
-    module containing user class
-"""
+"""This module defines a class User"""
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine, Column, Integer, String
-from os import environ
-
-storage_engine = environ.get("HBNB_TYPE_STORAGE")
 
 
 class User(BaseModel, Base):
     """
-        User class for the user
-        User class for the user
+    This class defines a user by various attributes
+    which will be mapped to columns on a users table
+    in the database
     """
-    if (storage_engine == 'db'):
-        __tablename__ = "users"
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
+
+    # establishing relationship with place and city models
+    places = relationship("Place", backref="user", cascade="all, delete")
+    reviews = relationship("Review", backref="user", cascade="all, delete")
