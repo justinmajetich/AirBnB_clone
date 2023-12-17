563b4450dba0d14ac7982458821449d2e80efdbd
Matrix30 <Matrixboy30@gmail.com>
2023-12-17 23:19:01 +0100

    fix user.py

--- models/user.py
+++ models/user.py
@@ -6,13 +6,18 @@ from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = 'users'
+    """
+    This class defines a user by various attributes
+    which will be mapped to columns on a users table
+    in the database
+    """
 
+    __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-
-    places = relationship('Place', cascade='all, delete', backref='user')
-    reviews = relationship('Review', cascade='all, delete', backref='user')
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
+    
+    # establishing relationship with place and city models
+    places = relationship("Place", backref="user", cascade="all, delete_orphan")
+    reviews = relationship("Review", backref="user",cascade="all, delete_orphan")
