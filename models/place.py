2a72aef703fc9b19ca6cf47900e3637cac1ac699
Matrix30 <Matrixboy30@gmail.com>
2023-12-17 23:44:04 +0100

    add place and review relation to user model

--- models/place.py
+++ models/place.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Base
 
 
 class Place(BaseModel):
--- models/user.py
+++ models/user.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""
+    module containing user class
+    module containing user class
+"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine, Column, Integer, String
+from os import environ
+
+storage_engine = environ.get("HBNB_TYPE_STORAGE")
 
 
 class User(BaseModel, Base):
     """
-    This class defines a user by various attributes
-    which will be mapped to columns on a users table
-    in the database
+        User class for the user
+        User class for the user
     """
-
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
-
-    # establishing relationship with place and city models
-    places = relationship("Place", backref="user", cascade="all, delete")
-    reviews = relationship("Review", backref="user", cascade="all, delete")
+    if (storage_engine == 'db'):
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
