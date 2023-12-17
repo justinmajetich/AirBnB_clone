125951dfbbc6c5b0fb95f7ba459909a30c28095c
Lelaabk <lolabkari@gmail.com>
2023-12-17 13:38:38 -0800

    Update City and State models

--- models/city.py
+++ models/city.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+import models
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """ The city clas for storing city info """
+    __tablename__ = 'cities'
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    state = relationship('State', back_populates='cities')
+
+    def __init__(self, *args, *kwargs):
+        """ Initialise City instance """
+        super().__init__(*args, *kwargs)
--- models/state.py
+++ models/state.py
@@ -1,8 +1,30 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import models
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """ State class for storing state info """
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    # This for DataBase Storage
+    if storage_type == 'db':
+        cities = relationship('City', cascade='all, delete-orphan',
+                              back_populates='state')
+
+    def __init__(self, *args, **kwargs):
+        """ Initialise State instance """
+        super().__init__(*args, **kwargs)
+
+    @property
+    def cities(self):
+        """ Getter attribute for cities (for FileStorage) """
+        city_list = []
+        for city in models.storage.all(models.Cit)y.values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
