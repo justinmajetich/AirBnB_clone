3523320ef71269e02c27b98f1f42ecb2fa5e4120
effah001 <effahfrancis210@gmail.com>
2023-12-17 15:39:28 -0500

    update

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,4 @@
+-- Create a user with all privileges
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+GRANT SELECT PRIVILEGES ON performance_schema.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION
