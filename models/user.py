9805487425ac3d87e74f7fe14123e387d70aada9
Matrix30 <Matrixboy30@gmail.com>
2023-12-17 23:39:26 +0100

    add place and review relation to user model

--- models/user.py
+++ models/user.py
@@ -17,3 +17,7 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
+
+    # establishing relationship with place and city models
+    places = relationship("Place", backref="user", cascade="all, delete")
+    reviews = relationship("Review", backref="user", cascade="all, delete")
