8efa2c8352739b98bcc38c2c955c3e51d7f136b2
Lamin Jawla <laminjawla1001@gmail.com>
2023-12-17 14:39:47 +0000

    Cosole improvements

--- console.py
+++ console.py
@@ -125,26 +125,30 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """Create an object of any class"""
+        print(args)
         try:
             if not args:
                 raise SyntaxError()
-            arg_list = args.split(" ")
-            kw = {}
-            for arg in arg_list[1:]:
-                arg_splited = arg.split("=")
-                arg_splited[1] = eval(arg_splited[1])
-                if type(arg_splited[1]) is str:
-                    arg_splited[1] = (
-                        arg_splited[1].replace("_", " ").replace('"', '\\"')
+            args = args.split(" ")
+            kwargs = {}
+            for arg in args[1:]:
+                arg = arg.split("=")
+                arg[1] = eval(arg[1])
+                if type(arg[1]) is str:
+                    arg[1] = (
+                        arg[1].replace("_", " ").replace('"', '\\"')
                     )
-                kw[arg_splited[0]] = arg_splited[1]
+                kwargs[arg[0]] = arg[1]
         except SyntaxError:
             print("** class name missing **")
         except NameError:
             print("** class doesn't exist **")
-        new_instance = HBNBCommand.classes[arg_list[0]](**kw)
-        new_instance.save()
-        print(new_instance.id)
+        try:
+            obj = HBNBCommand.classes[args[0]](**kwargs)
+            obj.save()
+            print(obj.id)
+        except KeyError:
+            print("** class doesn't exist **")
 
     def help_create(self):
         """Help information for the create method"""
