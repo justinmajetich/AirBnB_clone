bc8816897bf70ffe94ee7094918f50836896a90f
Matrix30 <Matrixboy30@gmail.com>
2023-12-17 05:48:41 +0100

    finish task 7. DBStorage - User

--- models/user.py
+++ models/user.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
+class User(BaseModel, Base):
+    """
+    This class defines a user by various attributes
+    which will be mapped to columns on a users table
+    in the database
+    """
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
