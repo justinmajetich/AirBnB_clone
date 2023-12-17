d99e524720755e2adb7fc09701acc2ded20292f8
MASMIYEN <masmiyen@gmail.com>
2023-12-17 19:55:25 +0100

    mysql scripts

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,6 @@
+-- setup for mySQL server for this project
+CREATE DATABASE IF NOT EXISTS`hbnb_dev_db`;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT ON performance_schema.* to 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+
