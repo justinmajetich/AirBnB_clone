ac70236b1379ae0f050fb0a845d7ae342e04d66f
Bamiwo Adebayo <bammietop@gmail.com>
2023-12-18 22:53:10 +0100

    DBStorage - Amenity... and BOOM!

--- models/place.py
+++ models/place.py
@@ -6,6 +6,14 @@ from sqlalchemy.orm import clear_mappers
 from sqlalchemy.orm import relationship
 import os
 
+place_amenity = Table(
+        'place_amenity',
+        Base.metadata,
+        Column('place_id', String(60), ForeignKey('places.id'), primary_key=True),
+        Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True)
+)
+
+
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
@@ -24,6 +32,11 @@ class Place(BaseModel, Base):
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
         reviews = relationship("Review", backref="place", cascade="all,\
                                delete-orphan")
+        amenities = relationship(
+                "Amenity",
+                secondary=place_amenity,
+                viewonly=False
+        )
     else:
         @property
         def reviews(self):
@@ -32,3 +45,22 @@ class Place(BaseModel, Base):
                 if review.place_id == self.id:
                     reviews_list.append(review)
             return reviews_list
+
+        @property
+        def amenities(self):
+            """ Getter attribute for amenities in FileStorage. """
+            from models import storage, Amenity
+
+            amenities_list = []
+            for amenity_id in self.amenity_ids:
+                amenity = storage.get(Amenity, amenity_id)
+                if amenity:
+                    amenities_list.append(amenity)
+            return amenities_list
+
+        @amenities.setter
+        def amenities(self, amenity):
+            """Setter attribute for amenities in FileStorage."""
+            if isinstance(amenity, Amenity):
+                if amenity.id not in self.amenity_ids:
+                    self.amenity_ids.append(amenity.id)
