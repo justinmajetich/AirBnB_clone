8f3be097aeb6b1958f70f69dd30561d276df1faa
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-12-18 15:01:24 +0000

    Added .vscode to gitignore

--- console.py
+++ console.py
@@ -2,7 +2,7 @@
 """ Console Module """
 import cmd
 import sys
-import shlex
+#import shlex
 from models.base_model import BaseModel
 from models.__init__ import storage
 from models.user import User
