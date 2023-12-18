127ae379f0c854d7f597d6676e91699c2ec4d8df
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-12-18 12:49:06 -0800

    Updated the console's unittest.

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,6 +1,9 @@
 import unittest
 from unittest.mock import patch
 from console import HBNBCommand
+from io import StringIO
+from models.__init__ import storage
+from models.state import State
 
 
 class TestCreateFunction(unittest.TestCase):
@@ -53,6 +56,52 @@ class TestCreateFunction(unittest.TestCase):
                                                  user_id=202 text=\"Lovely\
                                                  stay!\"")
         self.assertEqual(str(context.exception), "** class doesn't exist **")
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_all_without_args(self, mock_stdout):
+        # Redirect stdout to capture print output
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            # Execute the console command
+            self.hbnb_command_instance.do_all("")
+
+        # Get the captured output
+        result = mock_stdout.getvalue().strip()
+
+        # If there are no instances in storage, the output should be an empty list
+        if not storage.all():
+            self.assertEqual(result, '[]')
+        else:
+            # Check if the output contains the string representation of all objects
+            for model_class in HBNBCommand.classes.values():
+                self.assertIn(str(model_class()), result)
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_all_with_valid_class(self, mock_stdout):
+        # Redirect stdout to capture print output
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            # Execute the console command
+            self.hbnb_command_instance.do_all("all State")
+
+        # Get the captured output
+        result = mock_stdout.getvalue().strip()
+
+        # Check if the output contains the string representation of all State objects
+        for obj_id, obj in storage.all(State).items():
+            self.assertIn(str(obj), result)
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_all_with_invalid_class(self, mock_stdout):
+        # Redirect stdout to capture print output
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            # Execute the console command
+            self.hbnb_command_instance.do_all("all InvalidClass")
+
+        # Get the captured output
+        result = mock_stdout.getvalue().strip()
+
+        # Check if the output contains the error message
+        self.assertIn("** class doesn't exist **", result)
+
 
 
 if __name__ == '__main__':
