ee3fa1f3ac58cfa7a5109fc7a86f9b1d7c852d23
Droid <44108804+Effah001@users.noreply.github.com>
2023-12-18 14:00:00 +0000

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,5 @@
 -- Create a user with all privileges
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
