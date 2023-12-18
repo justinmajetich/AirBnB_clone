f790e4f73883a83ad2f2cc031847ce9dc3866025
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-12-18 10:24:51 -0800

    Updated the User, Place, City, State class and Console.

--- models/user.py
+++ models/user.py
@@ -10,7 +10,7 @@ class User(BaseModel, Base):
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
     place = relationship("Place", backref="user",  cascade="all,\
                          delete-orphan")
