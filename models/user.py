cf116c71a8531e54b01d9c0fca9b6a3b2320193f
Matrix30 <Matrixboy30@gmail.com>
2023-12-18 10:06:43 +0100

    fix user.py

--- models/user.py
+++ models/user.py
@@ -1,11 +1,32 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        """
+        This class defines a user by various attributes
+        which will be mapped to columns on a users table
+        in the database
+        """
+
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+
+        # establishing relationship with place and city models
+        # places = relationship("Place", backref="user", cascade="all, delete")
+        # reviews = relationship("Review", backref="user",
+        # cascade="all, delete")
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
