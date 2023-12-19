834fca7cdf86bed0abc1ec0295104c775a640d0e
marsade <adenijifara@gmail.com>
2023-12-19 20:43:03 +0200

    Add class attributes for db storage

--- models/review.py
+++ models/review.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
+import os
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    if os.getenv("HBTN_TYPE_STORAGE") == "db":    
+        __tablename__ = 'review'
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey("places.id"), nullable=False,)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
