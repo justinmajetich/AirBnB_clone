3cb3b5d5bd4747663077d3fe7fa40d22fb3bbc2b
Bamiwo Adebayo <bammietop@gmail.com>
2023-12-19 19:12:31 +0100

    fixing task 5

--- models/base_model.py
+++ models/base_model.py
@@ -27,16 +27,26 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            """kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+            """ kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')"""
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            """
             self.id = kwargs.get('id', str(uuid.uuid4()))
             self.created_at = kwargs.get('created_at', datetime.utcnow())
+            """if 'created_at' in kwargs:
+                self.created_at = datetime.strptime(kwargs['created_at'],
+                                                    '%Y-%m-%dT%H:%M:%S.%f')
+            else:"""
             self.updated_at = kwargs.get('updated_at', datetime.utcnow())
             if '__class__' in kwargs:
                 del kwargs['__class__']
             self.__dict__.update(kwargs)
+            """for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ('created_at', 'updated_at'):
+                        value = datetime.fromisoformat(value)
+                    setattr(self, key, value)"""
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -52,12 +62,21 @@ class BaseModel:
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = {}
+        """dictionary = {}
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        dictionary['created_at'] = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        dictionary['updated_at'] = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        dictionary.pop('_sa_instance_state', None)
+        return dictionary"""
+        dictionary = {
+                key: value.isoformat() if isinstance(value, datetime) else value
+                for key, value in self.__dict__.items()
+        }
+        dictionary['__class__'] = type(self).__name__
         dictionary.pop('_sa_instance_state', None)
         return dictionary
 
