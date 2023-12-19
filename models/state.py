79eafcd00cd3f8d0f26c69a619cb99d6f11d27c3
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-12-19 15:47:43 +0000

    Add Fixed cities getter and DB cities class attribute in State

--- models/state.py
+++ models/state.py
@@ -4,27 +4,22 @@ import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
 from sqlalchemy.orm import backref, relationship
+import os
 
 
-# class State(BaseModel, Base):
-#     """ State class """
-#     __tablename__ = 'states'
-#     name = Column(String(128), nullable=False)
-#     cities = relationship('City', backref=backref('state'))
-
-#     def cities_g(self):
-#         """getter for cities relationship for FileStorage"""
-#         print(BaseModel.all_classes(BaseModel, 'City'))
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref=backref('state'))
 
+    @property
     def cities(self):
         """getter for cities relationship for FileStorage"""
         City_Class = BaseModel.all_classes(BaseModel, 'City')
         result = models.storage.all(City_Class)
-        selected_city = {k: v for k, v in result.items()
-                         if v.state_id == self.id}
+        selected_city = [v for k, v in result.items()
+                         if v.state_id == self.id]
         return selected_city
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref=backref('state'))
