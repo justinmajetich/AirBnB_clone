8d42b84f93791c00527231091918f157d7fdbbac
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-12-19 13:23:53 +0200

    Adding Task 3 (Setting up MySQL server for the project)

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,5 +3,5 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
     IDENTIFIED WITH authentication_plugin BY 'hbnb_dev_pwd';
-GRANT * ON hbnb_dev_db TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema TO 'hbnb_dev'@'localhost';
+GRANT * ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
