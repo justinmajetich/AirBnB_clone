8b58c02578d60df032a56421fdfb4b28dea7df22
effah001 <effahfrancis210@gmail.com>
2023-12-19 15:30:40 -0500

    update

--- models/base_model.py
+++ models/base_model.py
@@ -24,17 +24,17 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
-        if kwargs:
-            for key, value in kwargs.items():
-                if not hasattr(self, key):
-                    setattr(self, key, value)
+       # if kwargs:
+        #    for key, value in kwargs.items():
+        #        if not hasattr(self, key):
+         #           setattr(self, key, value)
 
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+          #  kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+           # kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            #del kwargs['__class__']
+            #self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
