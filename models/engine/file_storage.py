1f46743b48f713a96df041103644554e50a3bd2f
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-12-19 16:38:42 +0100

    feat: Add delete method to filestorage engine
    Also added a fillter to the all method.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,8 @@
 """This module defines a class to manage file storage for hbnb clone"""
 import json
 
+from models.user import User
+
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
@@ -9,9 +11,17 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+    def all(self, cls=None):
+        """Returns a dictionary of models currently in storage
+            Param:
+                cls: Class name. Filter for the return objects
+        """
+        if cls is None:
+            return FileStorage.__objects
+        return {
+            k: v for k, v in FileStorage.__objects.items()
+            if v.__class__ == cls
+        }
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -25,6 +35,16 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        """Deletes an object from the __objects dict"""
+        if obj is None:
+            return
+        key = "{}.{}".format(obj.__class__, obj.id)
+        try:
+            del self.__objects[key]
+        except KeyError:
+            return
 
     def reload(self):
         """Loads storage dictionary from file"""
