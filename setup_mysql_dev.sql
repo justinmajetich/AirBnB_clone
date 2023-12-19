2f82a0f4566d916f1fa63fa46775cf9553a749b7
deelight-del <akingbenidavidoyin@gmail.com>
2023-12-19 17:13:06 +0000

    remove global setting of validate_pw

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,8 @@
 -- SETTING UP THE DEV SQL PROFILE
 --The script that doess the same
-SET GLOBAL vaidate_password.policy = 0;
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 USE hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO hbnb_dev@localhost;
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@localhost;
 FLUSH PRIVILEGES;
-SET GLOBAL validate_password.policy = 1;
