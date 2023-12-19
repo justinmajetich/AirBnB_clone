19d8483507e5c15cf5aa4e9992344cf598cd821f
Patience Otuke <patienceotuke@gmail.com>
2023-12-19 22:18:57 +0300

    Task 10: Getter and Setter for amenities in Place

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
+from sqlalchemy.orm import backref, relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Class Amenity"""
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
