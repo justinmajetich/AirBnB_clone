2f52114c09b4b443cdeaf113e17b472e06721915
Lelaabk <lolabkari@gmail.com>
2023-12-19 02:39:03 -0800

    Adding delete methode and updating method of All()

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,16 +1,29 @@
-#!/usr/bin/python3
+#!/usr/bin/pythion3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+import shlex
 
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
-    __file_path = 'file.json'
+    __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        dic = {}
+        if cls:
+            dictionary = self.__objects
+            for key in dictionary:
+                part = key.replace('.', ' ')
+                # Here we check if the first part (the class name)
+                # matches the provided class(cls.__name__)
+                part = shlex.split(part)
+                if (part[0] == cls.__name__):
+                    dic[key] = self.__objects[key]
+            return (dic)
+        else:
+            return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +61,8 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+    def delete(self, obj=None)
+    """ Delete element from storage """
+    if obj:
+        key = "{}.{}"..format(type(obj).__name__, obj.id)
+        del self.__objects[key]
