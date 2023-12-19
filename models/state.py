6f93dd5998dc8b96617ef33c37d5d0f03f098e0b
Droid <44108804+Effah001@users.noreply.github.com>
2023-12-19 20:32:47 +0000

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,24 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from models.city import City
 
-Base = declarative_base()
+strg = getenv("HBNB_TYPE_STORAGE")
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
 
-    name = Column(String(128), nullable=False)
-
-    if models.storage_type == 'db':
+    if strg == "db":
+        name = Column(String(128), nullable=False)
         cities = relationship('City', cascade='all, delete-orphan')
 
     else:
+        name = ""
+
         @property
         def cities(self):
             """Getter attribute for cities in FileStorage"""
+            from models import storage
 
             city_list []
             for city in models.storage.all(City).values():
