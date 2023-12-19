258ef8947f7d24d67bacec9a3a66467b6670d6d0
VickArmand <victormaina1962@gmail.com>
2023-12-19 22:35:15 +0300

    MySQL setup development and test users and databases

---
+++ setup_mysql_test.sql
@@ -0,0 +1,8 @@
+-- Create a user hbnb_test with password hbnb_test_pwd
+-- and a database hbnb_test_db
+-- hbnb_test should have all privileges on the database hbnb_test_db
+-- hbnb_test should have SELECT privilege on the database performance_schema
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
