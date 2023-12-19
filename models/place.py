f02d6248f82359d5c1f7fa566ae92e62c4948490
Matrix30 <Matrixboy30@gmail.com>
2023-12-19 04:30:58 +0100

    fix place and state

--- models/place.py
+++ models/place.py
@@ -1,9 +1,8 @@
-#!/usr/bin/python3
-""" Place Module for HBNB project """
-from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, ForeignKey, Float
 from os import getenv
 from sqlalchemy.orm import relationship
+
+from models.base_model import BaseModel, Base
 
 
 class Place(BaseModel, Base):
