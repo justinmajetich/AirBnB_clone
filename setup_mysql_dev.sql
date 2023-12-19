51125a891a10626e699384494a662b91c29b3271
deelight-del <akingbenidavidoyin@gmail.com>
2023-12-19 17:17:32 +0000

    xxx

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,5 @@
 -- SETTING UP THE DEV SQL PROFILE
 --The script that doess the same
-SET GLOBAL validate_password.policy = 0;
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 USE hbnb_dev_db;
 REVOKE ALL PRIVILEGES ON *.* FROM 'hbnb_dev'@'localhost'
@@ -8,4 +7,3 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO hbnb_dev@localhost;
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@localhost;
 FLUSH PRIVILEGES;
-SET GLOBAL validate_password.policy = 1;
