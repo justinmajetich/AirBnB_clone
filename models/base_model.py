72e4311fbc7dcee51cf73f343028a099734eaff7
deelight-del <akingbenidavidoyin@gmail.com>
2023-12-19 22:17:31 +0000

    task db w/o finishing

--- models/base_model.py
+++ models/base_model.py
@@ -2,18 +2,24 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy import Column, Integer, String, create_engine, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from models import storage
 
 
+Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            from models import storage
+            #  from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -29,8 +35,9 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
+        #  from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
@@ -41,4 +48,10 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if dictionary.get('_sa_instance_state', None) is not None:
+            del dictionary['_sa_instance_state']
         return dictionary
+
+    def delete(self):
+        """Public method to delete instance"""
+        storage.delete(self)
