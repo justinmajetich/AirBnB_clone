ab6bbbecd5758a90e984d939adc5ec24b9919f16
Seth Ketsi <christambassador12@gmail.com>
2023-12-19 18:19:15 +0000

    updated

--- console.py
+++ console.py
@@ -255,7 +255,7 @@ class HBNBCommand(cmd.Cmd):
         print(count)
 
     def help_count(self):
-        """ """
+        """help count"""
         print("Usage: count <class_name>")
 
     def do_update(self, args):
