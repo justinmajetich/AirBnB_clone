ba49d6f83bf51dd5a7141e99b560c1dca644ea76
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-12-19 13:22:05 +0200

    Adding Task 3 (Setting up MySQL server for the project)

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,7 @@
+-- This script prepares a MySQL server for the HBNB clone project
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
+    IDENTIFIED WITH authentication_plugin BY 'hbnb_dev_pwd';
+GRANT * ON hbnb_dev_db TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema TO 'hbnb_dev'@'localhost';
