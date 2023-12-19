b36463795e0abd84c75d44daa040de201f6a5cb4
Matrix30 <Matrixboy30@gmail.com>
2023-12-19 04:13:12 +0100

    fix place

--- models/place.py
+++ models/place.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+""" Place Module for HBNB project """
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, ForeignKey, Float
 from os import getenv
 from sqlalchemy.orm import relationship
