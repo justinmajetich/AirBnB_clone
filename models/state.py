7a8f1d9fe0735fb5eab40bd354e8a7d5e0fb01ea
effah001 <effahfrancis210@gmail.com>
2023-12-19 11:58:57 -0500

    update

--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+
+    if models.storage_type == 'db':
+        cities = relationship('City', cascade='all, delete-orphan')
+
+    else:
+        @property
+        def cities(self):
+            """Getter attribute for cities in FileStorage"""
+
+            city_list []
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
