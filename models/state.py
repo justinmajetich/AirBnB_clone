85ad851deb77092a256c3b55a9c2b9d5508af23a
tam-dcreator <tam@THE-7TH>
2023-12-20 14:23:39 +0100

    Abstracted the class to an sqlalchemy table, set the relationships, columns and tablename

--- models/state.py
+++ models/state.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base, Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """ Represents a state
+     Attributes:
+         name (str): The name of the state
+     """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+
+    cities = relationship('City', cascade='all, delete',
+                          back_populates="state")
