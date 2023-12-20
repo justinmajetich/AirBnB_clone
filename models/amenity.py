7a1ef742b376eb9c7a9dde73bc82cf997aadb90c
Patience Otuke <patienceotuke@gmail.com>
2023-12-20 09:03:10 +0300

    Task 10: back_populates in many to  many relationship

--- models/amenity.py
+++ models/amenity.py
@@ -9,4 +9,5 @@ class Amenity(BaseModel, Base):
     """Class Amenity"""
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship('Place', secondary='place_amenity')
+    place_amenities = relationship('Place', secondary='place_amenity',
+                                   back_populates='amenities')
