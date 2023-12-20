4c58887d8f074de8172c3aa18f7300fd53307e49
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-12-20 02:18:42 +0000

    Fixed a few typos and errors

--- models/place.py
+++ models/place.py
@@ -2,9 +2,18 @@
 """ Place Module for HBNB project """
 import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float,  ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import backref, relationship
 import os
+
+place_amenity = Table(
+    name = "place_amenity",
+    metadata = Base.metadata,
+    place_id = Column(String(60), ForeignKey("places.id"),
+                   primary_key=True, nullable=False),
+    amenity_id = Column(String(60), ForeignKey("amenities.id"),
+                      primary_key=True, nullable=False)
+)
 
 
 class Place(BaseModel, Base):
@@ -12,14 +21,14 @@ class Place(BaseModel, Base):
     __tablename__ = "places"
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(60), nullable=False)
-    description = Column(String(60), nullable=True)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, default=0, nullable=False)
     number_bathrooms = Column(Integer, default=0, nullable=False)
     max_guest = Column(Integer, default=0, nullable=False)
     price_by_night = Column(Integer, default=0, nullable=False)
-    latitude = Column(Float, default=0, nullable=True)
-    longitude = Column(Float, default=0, nullable=True)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     amenity_ids = []
 
     @property
@@ -34,17 +43,19 @@ class Place(BaseModel, Base):
     @property
     def amenities(self):
         """getter for amenity relationship for FileStorage"""
-        Amenity_Class = BaseModel.all_classes(BaseModel, 'Review')
+        Amenity_Class = BaseModel.all_classes(BaseModel, 'Amenity')
         result = models.storage.all(Amenity_Class)
         selected_amenities = [v for k, v in result.items()
                               if v.place_id == self.id]
         return selected_amenities
 
     @amenities.setter
-    def amenities(self, cls):
+    def amenities(self, obj):
         """Setter for handling appending ammenity id to ammenity_ids"""
-        if isinstance(cls, models.amenity.Amenity):
-            self.amenity_ids.append(cls.id)
+        if isinstance(obj, models.amenity.Amenity):
+            self.amenity_ids.append(obj.id)
 
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         reviews = relationship('Review', backref=backref('place'))
+        amenities = relationship('Amenity', secondary=place_amenity, viewonly=False)
+
