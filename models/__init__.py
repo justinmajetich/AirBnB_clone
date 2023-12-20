2436736dd6ceefcfec20a65dbd2d1e7f5114c7d2
tam-dcreator <tam@THE-7TH>
2023-12-20 14:20:53 +0100

    Set the storage type to depend on a conditional which decides which database tye to use

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+from os import getenv
 
 
-storage = FileStorage()
-storage.reload()
+HBNB_TYPE_STORAGE = getenv("STORAGE")
+
+if HBNB_TYPE_STORAGE == "db":
+    from models.engine.db_storage import DBStorage
+
+    storage = DBStorage()
+    storage.reload()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
