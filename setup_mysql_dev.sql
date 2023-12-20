e3b49049fb3ea9e79ff9bd439616a6ca16a39fd2
Seth Ketsi <christambassador12@gmail.com>
2023-12-20 15:38:49 +0000

    privileges added

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,14 @@
+-- Create or use the database
+-- Create or use the user
+-- Grant privileges to the user on the database
+-- Grant SELECT privilege on performance_schema
+-- Flush privileges to apply changes
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,14 @@
+-- Create or use the database
+-- Create or use the user
+-- Grant privileges to the user on the database
+-- Grant SELECT privilege on performance_schema
+-- Flush privileges to apply changes
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+FLUSH PRIVILEGES;
