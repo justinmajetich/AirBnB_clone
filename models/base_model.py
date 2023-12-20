112a95c091034d639f2b0846461137a66c8286af
WalaEddine01 <walaaeddine33@gmail.com>
2023-12-20 23:13:10 +0100

    I made the changes on 2023-12-20 23:13:10

--- models/base_model.py
+++ models/base_model.py
@@ -9,6 +9,8 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 Base = declarative_base()
+
+
 class BaseModel:
     """A base class for all hbnb models"""
 
@@ -26,7 +28,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
