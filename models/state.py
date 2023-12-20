d7c23098ff076f323592c1c472e1a304292924b4
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2023-12-21 01:29:18 +0200

    Update: State class to map to table states.

--- models/state.py
+++ models/state.py
@@ -9,13 +9,14 @@ from models.place import Place
 from sqlalchemy import Integer, String, Column, ForeignKey
 from sqlalchemy.orm import relationship
 
+
 class State(BaseModel, Base):
     """ State class definition """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     if getenv("HBNB_TYPE_STORAGE") == "db":
         cities = relationship("City", backref="state", cascade="delete")
-    else: 
+    else:
         @property
         def cities(self):
             """Getter for cities"""
