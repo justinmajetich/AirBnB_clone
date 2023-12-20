56e72757d2f9bd193600b539ffd5699d7d3c5e7e
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-12-20 12:24:03 +0200

    BaseModel: Included logic to dynamically check for valid keys during initialization

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -96,4 +96,5 @@ class test_basemodel(unittest.TestCase):
         self.assertEqual(type(new.updated_at), datetime.datetime)
         n = new.to_dict()
         new = BaseModel(**n)
+        new.save() # save the changes
         self.assertFalse(new.created_at == new.updated_at)
