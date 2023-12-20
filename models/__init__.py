63a1f7803e26a3444902bd43ba82674458b49fd0
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2023-12-20 18:56:21 +0200

    Update: Models init file to handle switching between file and database storage.

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+from os import getenv
 
-
-storage = FileStorage()
+storage = DBStorage() if getenv('HBNB_TYPE_STORAGE') == 'db' else FileStorage()
 storage.reload()
