319a4afb4bf0369eb7a51fc53ef6a6f40434feca
Droid <44108804+Effah001@users.noreply.github.com>
2023-12-20 15:05:38 +0000

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -4,11 +4,8 @@ import uuid
 from datetime import datetime
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-<<<<<<< HEAD
 
-=======
 import models
->>>>>>> master
 
 Base = declarative_base()
 
