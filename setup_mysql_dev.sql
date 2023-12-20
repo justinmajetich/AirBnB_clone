494c815c47068b266464fe18fd54878a0d47dfb7
Joseph Marko <josephmmakaro@gmail.com>
2023-12-20 00:56:20 +0000

    File uploads

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,15 @@
+-- A Script that prepares a MySQL server for the project
+
+-- Database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- A new User
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Setting All privileges for the user
+GRANT ALL PRIVILEGES ON hbnb_dev_db .* TO 'hbnb_dev'@'localhost';
+
+
+GRANT SELECT ON performance_schema TO 'hbnb_dev'@'localhost';
+
+FLUSH PRIVILEGES;
