1cec9d1126be9e60773dfe621d62d74f59621d55
alibayomy <alibayomy12@gmail.com>
2023-12-20 10:44:37 -0800

    adjusting the database files

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,10 +1,5 @@
--- a script that prepares a MySQL server for development
--- create a new database if not exists
-CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
--- create a new user if not exists
-CREATE USER IF NOT EXISTS `hbnb_dev`@`localhost` IDENTIFIED BY 'hbnb_dev_pwd';
--- grant all persmissions to the created user on the created database
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO `hbnb_dev`@`localhost`;
--- grant 'select' permission to the created user on a built-in
--- database as a feature that monitor MySQL server at a low level
-GRANT SELECT ON `performance_schema` TO  `hbnb_dev`@`localhost`;
+-- Prepares a MySQL Test server for the HBNB project.
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
