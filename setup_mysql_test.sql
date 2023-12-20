c9212737b342e467121e25dd6779714b20ea6317
VickArmand <victormaina1962@gmail.com>
2023-12-20 22:33:55 +0300

    corrections v1

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -4,5 +4,5 @@
 -- hbnb_test should have SELECT privilege on the database performance_schema
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
