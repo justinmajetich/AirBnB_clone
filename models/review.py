cb6b54ba1a3b7fb612cce0e209aa1b5f787e5077
Patience Otuke <patienceotuke@gmail.com>
2023-12-20 08:09:26 +0300

    TAsk 7: Changed first|last_name to nullable

--- models/review.py
+++ models/review.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import backref, relationship
 
 
 class Review(BaseModel, Base):
-    """ Review classto store review information """
+    """ Review class to store review information """
     __tablename__ = "reviews"
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
