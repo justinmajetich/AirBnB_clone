00015f8e3c4bfe5058b7c8969638221ed0651f1a
Droid <44108804+Effah001@users.noreply.github.com>
2023-12-20 04:10:07 +0000

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -20,12 +20,11 @@ class BaseModel:
         """Instantiates a new model"""
         from models import storage
 
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
-        else:
+        if kwargs:
             for key, value in kwargs.items():
                 if not hasattr(self, key):
                     setattr(self, key, value)
