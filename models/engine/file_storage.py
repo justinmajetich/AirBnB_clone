784b206e254cf6ae17609084f8fb09f545d3af46
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-12-20 09:57:26 +0100

    dealing with deltete => task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+import shlex
 
 
 class FileStorage:
@@ -8,9 +9,21 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        dic = {}
+        if cls:
+            dictionary = self.__objects
+            for key in dictionary:
+                part = key.replace('.', ' ')
+                # Here we check if the first part (the class name)
+                # matches the provided class(cls.__name__)
+                part = shlex.split(part)
+                if (part[0] == cls.__name__):
+                    dic[key] = self.__objects[key]
+            return (dic)
+        else:
+            return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -51,3 +64,9 @@ class FileStorage:
 
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """ Delete element from storage """
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del self.__objects[key]
