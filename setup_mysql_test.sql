bde40ee22490fd7f9cfccb028df2908dfe231b65
WalaEddine01 <walaaeddine33@gmail.com>
2023-12-20 18:10:51 +0100

    I made the changes on 2023-12-20 18:10:51

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,3 +1,4 @@
+-- this script that prepares a MySQL server for the project
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
