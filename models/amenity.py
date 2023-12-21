f359724e7c953180a2ff462b93dc36c4f335f87d
deelight-del <akingbenidavidoyin@gmail.com>
2023-12-21 10:55:41 +0000

    fix bugs from 21/12 morning

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, Integer, Column, Table
+from sqlalchemy.orm import relationship
 
-
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    #place_amenities
