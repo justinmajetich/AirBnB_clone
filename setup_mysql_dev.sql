25104e7efabd9ade7524a1a0a2601a394896f4e3
Munachi Iwundu <realjones2012@gmail.com>
2023-12-21 02:07:58 +0100

    removed a bug

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,5 @@
 #!/bin/bash
 -- prepares a MySQL server
-SET GLOBAL validate_password.policy=LOW;
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
