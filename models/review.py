edcd384ef05e6401f6d37cc321658ab1113952d5
marsade <adenijifara@gmail.com>
2023-12-22 10:45:12 +0200

    Fix typo

--- models/review.py
+++ models/review.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-import os
-from models.base_model import BaseModel
-from models.base_model import Base
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
+import os
 
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
-    if os.getenv("HBTN_TYPE_STORAGE") == "db":    
-        __tablename__ = 'review'
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":    
+        __tablename__ = 'reviews'
         text = Column(String(1024), nullable=False)
         place_id = Column(String(60), ForeignKey("places.id"), nullable=False,)
         user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
