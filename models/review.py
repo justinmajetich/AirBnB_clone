c605a02162ec8955b883c955f5a60cd64ec7dd2e
Oyebode Habeeb Omobolaji <63916225+omobolaji001@users.noreply.github.com>
2023-12-22 19:23:46 +0100

    modifications

--- models/review.py
+++ models/review.py
@@ -10,7 +10,7 @@ class Review(BaseModel, Base):
 
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'reviews'
-        text = Column(String(1024))
+        text = Column(String(1024), nullable=False)
         place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
         user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
 
