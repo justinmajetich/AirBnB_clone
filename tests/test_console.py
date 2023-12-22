3280449f17c6a64c54620348f77ce9e36c0197eb
Matrix30 <Matrixboy30@gmail.com>
2023-12-22 05:44:53 +0100

    generate more test for console

---
+++ tests/test_console.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Test for console"""
+import unittest
+from console import HBNBCommand
+from unittest.mock import patch
+from io import StringIO
+
+
+class test_console(unittest.TestCase):
+    """Test class for console"""
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_help_count(self, mock_stdout):
+        """testing help count method of console"""
+        HBNBCommand.help_count(self)
+        output = mock_stdout.getvalue()
+        self.assertEqual(output, "Usage: count <class_name>\n")
