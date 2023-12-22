3a270bda258602b07c9fad2efee86e1228c1aeb2
marsade <adenijifara@gmail.com>
2023-12-22 15:48:33 +0200

    Edit nullable arg

--- models/user.py
+++ models/user.py
@@ -11,8 +11,8 @@ class User(BaseModel, Base):
 
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'users'
-        email = Column(String(128), nullable=True)
-        password = Column(String(128), nullable=True)
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
         places = relationship('Place', cascade='all, delete-orphan',
