809cf9172c61aaf5ba46a9adc0b908a93d4dcf57
Ezra Yeneneh <105113257+ezrablackfish2@users.noreply.github.com>
2023-12-23 08:38:06 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-"""This is the review class"""
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey, Float
+"""Defines the Review class."""
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
 class Review(BaseModel, Base):
-    """This is the class for Review
+    """Represents a review for a MySQL database.
+
+    Inherits from SQLAlchemy Base and links to the MySQL table reviews.
+
     Attributes:
-        place_id: place id
-        user_id: user id
-        text: review description
+        __tablename__ (str): The name of the MySQL table to store Reviews.
+        text (sqlalchemy String): The review description.
+        place_id (sqlalchemy String): The review's place id.
+        user_id (sqlalchemy String): The review's user id.
     """
     __tablename__ = "reviews"
     text = Column(String(1024), nullable=False)
