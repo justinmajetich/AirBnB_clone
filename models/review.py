0830cd872ebfa566a8b48c20ad8483d56ccf8bf7
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2023-12-23 19:42:33 +0200

    we can do this

--- models/review.py
+++ models/review.py
@@ -1,24 +1,15 @@
-#!/usr/bin/python
-""" holds class Review"""
-import models
-from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
+#!/usr/bin/python3
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
+from models.base_model import Base, BaseModel
 
 
 class Review(BaseModel, Base):
-    """Representation of Review """
-    if models.storage_t == 'db':
-        __tablename__ = 'reviews'
-        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        text = Column(String(1024), nullable=False)
-    else:
-        place_id = ""
-        user_id = ""
-        text = ""
+    __tablename__ = 'reviews'
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    user = relationship("User", backref="reviews", cascade="all, delete-orphan")
 
-    def __init__(self, *args, **kwargs):
-        """initializes Review"""
-        super().__init__(*args, **kwargs)
+    
