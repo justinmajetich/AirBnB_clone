8f5f37dfbcb6e6ec6d0be9da9c96968a5e3ecad1
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2023-12-23 18:41:49 +0200

    Refactor code for improved performance

--- models/review.py
+++ models/review.py
@@ -1,10 +1,35 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel
+
+"""
+A module that defines ORM class for Review table
+"""
+from os import getenv
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy import String
+from models.base_model import Base, BaseModel
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """
+    Defines attributes for Review table
+    """
+    __tablename__ = 'reviews'
+<<<<<<< HEAD
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+
+
+=======
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        text = Column(String(1024), nullable=False)
+        place_id = Column(
+            String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(
+            String(60), ForeignKey('users.id'), nullable=False)
+    else:
+        text = ''
+        place_id = ''
+        user_id = ''
+>>>>>>> 1770a4fb53446035b89be39655c2c2913e9c6151
