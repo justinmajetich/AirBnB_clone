ab0ebc2592eb282ae17ad121ff91d0bdbc88e60a
Ezra Yeneneh <105113257+ezrablackfish2@users.noreply.github.com>
2023-12-23 08:50:23 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """Defines the State class."""
 import models
-from os import getenv
-from models.base_model import Base
-from models.base_model import BaseModel
-from models.city import City
-from sqlalchemy import Column
-from sqlalchemy import String
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from models.city import City
+from os import getenv
 
 
 class State(BaseModel, Base):
@@ -22,14 +20,20 @@ class State(BaseModel, Base):
     """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City",  backref="state", cascade="delete")
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="state", cascade="all, delete")
+    else:
         @property
         def cities(self):
-            """Get a list of all related City objects."""
+            """Get a list of City instances with
+                state_id equals to the current State.id.
+
+            This is a getter attribute for FileStorage
+                relationship between State and City.
+            """
             city_list = []
-            for city in list(models.storage.all(City).values()):
+            for city in models.storage.all(City).values():
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
