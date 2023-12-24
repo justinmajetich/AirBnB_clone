68b0828987b61ba427e87afde6a153e27ae1796b
Ayo <talktojmcvibes@gmail.com>
2023-12-24 03:28:11 -0800

    number template task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+te flask app"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    """Home route"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_route(text):
+    """C route"""
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+@app.route("/python/", defaults={'text': 'is cool'})
+@app.route("/python/<text>")
+def python_route(text):
+    """Python route"""
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>")
+def number_route(n):
+    """Number route"""
+
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    return render_template('5-number.html', n=n)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
