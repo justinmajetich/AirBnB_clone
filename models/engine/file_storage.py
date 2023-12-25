3b6f1f122bb8f68b08a112f6651bb404d7891528
Ayo <talktojmcvibes@gmail.com>
2023-12-25 01:35:46 -0800

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -74,3 +74,7 @@ class FileStorage:
             key = '{}.{}'.format(type(obj).__name__, obj.id)
             del FileStorage.__objects[key]
             self.save
+
+    def close(self):
+        """Calss the reload method"""
+        self.reload()
