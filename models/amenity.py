b853a35f766b9b3bca208de931bde042311ba416
WalaEddine01 <walaaeddine33@gmail.com>
2023-12-26 13:05:46 +0100

    I made the changes on 2023-12-26 13:05:46

--- models/amenity.py
+++ models/amenity.py
@@ -7,23 +7,6 @@ from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel, Base):
+class Amenity(BaseModel):
     """ Amenity class """
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity,
-                                   viewonly=False)
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        place_amenities = relationship("Place", secondary=place_amenity,
-                                       viewonly=False)
-    else:
-        @property
-        def place_amenities(self):
-            """Getter for place_amenities"""
-            from models import storage
-            from models.place import Place
-            place_list = []
-            for place in storage.all(Place).values():
-                if place.amenity_id == self.id:
-                    place_list.append(place)
-            return place_list
+    name = ""
--- models/place.py
+++ models/place.py
@@ -10,27 +10,4 @@ from sqlalchemy.orm import relationship
 
 class Place(BaseModel):
     """ A place to stay """
-    __tablename__ = 'places'
-    city_id = Column(String(60), ForeignKey('cities.id'))
-    user_id = Column(String(60), ForeignKey('users.id'))
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=False)
-    number_rooms = Column(Integer(), default=0, nullable=False)
-    number_bathrooms = Column(Integer(), default=0, nullable=False)
-    max_guest = Column(Integer(), default=0, nullable=False)
-    price_by_night = Column(Integer(), default=0, nullable=False)
-    latitude = Column(Float(), nullable=False)
-    longitude = Column(Float(), nullable=False)
-
-    place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60),
-                                 ForeignKey('places.id'),
-                                 primary_key=True, nullable=False),
-                                 Column('amenity_id', String(60),
-                                        ForeignKey('amenities.id'),
-                                     primary_key=True, nullable=False))
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        reviews = relationship("Review", backref="place",
-                               cascade="all, delete-orphan")
-        amenities = relationship("Amenity", secondary=place_amenity,
-                                 viewonly=False)
+    name=""
