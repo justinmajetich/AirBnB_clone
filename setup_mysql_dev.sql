7fff91a7f4927d9d281019f804edde7768bfe8eb
Ayo <talktojmcvibes@gmail.com>
2023-12-30 12:29:20 -0800

    move sql file

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,13 +1,11 @@
--- Create a hbnb_dev_db database if it doesn't exist
+--MySQL DMS
+--create database if none exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
--- Create user hbnb_dev if it doesn't exist, identified by password
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
--- Grant all privileges to hbnb_dev on hbnb_dev_db database
+--craete new user and set password
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd'
+-- granting all privileges to the new user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
--- Grant SELECT privilege on performance_schema database
+FLUSH PRIVILEGES;
+-- granting the SELECT privilege for the user hbnb_dev in the db performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-
---FLUSH PRIVILEGES;
+FLUSH PRIVILEGES;
