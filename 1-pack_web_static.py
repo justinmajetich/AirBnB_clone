1fc56eaac6aeef2118f90850859562603b089270
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-12-31 12:42:18 +0300

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Fabric script that generates a .tgz archive from the contents of 
+Fabric script that generates a .tgz archive from the contents of
 the web_static folder of your AirBnB Clone repo, using the function do_pack.
 """
 
