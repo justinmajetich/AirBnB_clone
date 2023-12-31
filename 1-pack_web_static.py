78edbf9354ab03f41993719acb232da57499c576
wxllxngton <jjngugi27@gmail.com>
2023-12-31 03:28:04 -0800

    Fixed code with black

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,14 +7,14 @@ the web_static folder of your AirBnB Clone repo, using the function do_pack.
 from fabric.api import local
 from datetime import datetime
 
+
 def do_pack():
     """Generates a .tgz archive from contents of web_static."""
-    time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+    time = datetime.utcnow().strftime("%Y%m%d%H%M%S")
     file_name = "versions/web_static_{}.tgz".format(time)
     try:
         local("mkdir -p ./versions")
-        local("tar --create --verbose -z --file={} ./web_static"
-              .format(file_name))
+        local("tar --create --verbose -z --file={} ./web_static".format(file_name))
     except Exception as e:
         print(e)
         return None
