ed8452089379df2d9c1779f55e9b532053062aa7
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2024-01-01 10:14:46 +0300

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -42,7 +42,7 @@ def do_deploy(archive_path):
         run("rm /tmp/{}".format(archive))
 
         # Move the contents of web_static to the release directory
-        run("mv {}/{}/web_static/* {}/{}/".format(path, folder[0], path, folder[0]))
+        run(f"mv {path}/{folder[0]}/web_static/* {path}/{folder[0]}/")
 
         # Remove the web_static directory in the release directory
         run("rm -rf {}/{}/web_static".format(path, folder[0]))
