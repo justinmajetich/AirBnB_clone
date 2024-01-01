9941913a0235213597e5f9be108ec8d0969ace57
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2024-01-01 12:06:16 +0300

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -8,8 +8,6 @@ from datetime import datetime
 import os
 
 env.hosts = ["ubuntu@54.146.95.43", "ubuntu@34.229.67.181"]
-created_path = None
-
 
 def do_pack():
     """Generates a .tgz archive from contents of web_static."""
@@ -17,13 +15,12 @@ def do_pack():
     file_name = "versions/web_static_{}.tgz".format(time)
     try:
         local("mkdir -p ./versions")
-        local("tar --create --verbose -z --file={} ./web_static"
-              .format(file_name))
-        return file_name
+        local("tar -czvf {} ./web_static".format(file_name))
     except Exception as e:
         print(e)
         return None
-
+    else:
+        return file_name
 
 def do_deploy(archive_path):
     """
@@ -37,25 +34,43 @@ def do_deploy(archive_path):
     """
     if os.path.isfile(archive_path) is False:
         return False
+
     try:
         archive = archive_path.split("/")[-1]
         path = "/data/web_static/releases"
-        put("{}".format(archive_path), "/tmp/{}".format(archive))
         folder = archive.split(".")
         new_archive = ".".join(folder)
+
+        # Upload the archive to /tmp/ on the server
+        put("{}".format(archive_path), "/tmp/{}".format(archive))
+
+        # Create the release directory
         run("mkdir -p {}/{}/".format(path, folder[0]))
-        run("tar -xzf /tmp/{} -C {}/{}/"
-            .format(new_archive, path, folder[0]))
+
+        # Extract the archive into the release directory
+        run("tar -xzf /tmp/{} -C {}/{}/".format(new_archive, path, folder[0]))
+
+        # Remove the uploaded archive
         run("rm /tmp/{}".format(archive))
+
+        # Move the contents of web_static to the release directory
         run("mv {}/{}/web_static/* {}/{}/"
             .format(path, folder[0], path, folder[0]))
+
+        # Remove the web_static directory in the release directory
         run("rm -rf {}/{}/web_static".format(path, folder[0]))
+
+        # Remove the existing /data/web_static/current symbolic link
         run("rm -rf /data/web_static/current")
+
+        # Create a new symbolic link pointing to the new release directory
         run("ln -sf {}/{} /data/web_static/current".format(path, folder[0]))
-        return True
+
     except Exception as e:
         print(e)
         return False
+    finally:
+        return True
 
 
 def deploy():
@@ -65,9 +80,7 @@ def deploy():
     Return:
         - None
     """
-    global created_path
-    if created_path is None:
-        created_path = do_pack()
-    if created_path is None:
-        return False
-    return do_deploy(created_path)
+	created_archive_path = do_path()
+	if created_archive_path is None:
+		return False
+	return do_deploy(created_archive_path)
