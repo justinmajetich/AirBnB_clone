13a499bb2daca16cc507ce0e69902f8526341857
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2024-01-01 09:55:09 +0100

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,8 +9,7 @@ app = Flask(__name__)
 
 @app.route('/airbnb-onepage/', strict_slashes=False)
 def hello():
-        return 'Hello, Airbnb!'
+    return 'Hello, Airbnb!'
 
-    if __name__ == "__main__":
-            app.run(host="0.0.0.0", port=5000)
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
