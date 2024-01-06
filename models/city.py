7d6c554ee3208a5e1bab864cc3abeccf3dc55c3d
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2024-01-06 12:21:24 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 """ City Module for AirBnB v2 project """
 import os
+import sys
+
+# Get the absolute path to the directory containing your modules
+current_dir = os.path.dirname(os.path.abspath(__file__))
+models_dir = os.path.join(current_dir, 'models')
+
+# Add the directory to the Python path
+sys.path.append(models_dir)
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
