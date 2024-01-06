4e32cc3db9030db2ea1aa1c2be5461c714e27a1b
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2024-01-06 11:12:07 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """ Review module for the BnB v2 project """
 import os
-from sqlalchemy import Column, ForeignKey, String
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, ForeignKey, String
 
 from models.base_model import BaseModel, Base
 
 
 class Review(BaseModel, Base):
-    """ Review class: stores review information """
+    """ Review class: stores the review information """
     __tablename__ = 'reviews'
     place_id = Column(
         String(60), ForeignKey('places.id'), nullable=False
