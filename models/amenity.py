0b554e94528683ec0b05fc1bfda6960912c98aa9
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2024-01-06 14:13:10 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -4,7 +4,7 @@ import os
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
 
-from models.base_model import BaseModel, Base
+from base_model import BaseModel, Base
 
 
 class Amenity(BaseModel, Base):
