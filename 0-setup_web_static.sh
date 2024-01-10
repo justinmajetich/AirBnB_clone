7838b8d130526dca152312b1613aba4545c51012
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-01-10 17:30:16 +0100

    set up the hbnb_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
-# Update, upgrade, and install Nginx
+# sets up the web servers for the deployment of web_static
+config =\
+"<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>"
+
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-# Create directories and index.html file
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
-# Create symbolic link
+echo "$config" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-# Change ownership
 sudo chown -hR ubuntu:ubuntu /data/
-# Configure Nginx
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-# Restart Nginx
-sudo service nginx restart
+sudo service nginx start
