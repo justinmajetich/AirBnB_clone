ec16d7e5ed381a3047009b3b605c6a057f855ec2
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-01-10 23:06:13 +0100

    modifie some issues

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ from fabric.api import task, local, env, put, run
 from datetime import datetime
 import os
 
-env.hosts = ['18.207.1.87', '52.206.189.175']
+env.hosts = ['54.236.30.207', '3.85.168.24']
 
 
 @task
