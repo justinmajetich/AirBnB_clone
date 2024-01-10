2548c4303753e41c8e61f0935cccd650f372974c
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-01-10 19:39:55 +0100

    add the pack

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,7 +3,7 @@
 Fabric script based on the file 1-pack_web_static.py that distributes an
 archive to the web servers
 """
-
+from datetime import datetime
 from fabric.api import run, env, put, local, task
 import os
 
@@ -40,20 +40,12 @@ def do_deploy(archive_path):
     Arguments:
         archive_path: Path to archive to deploy.
     """
-    if not archive_path or not os.path.exists(archive_path):
-        return False
-
-    put(archive_path, '/tmp')
-
-    ar_name = archive_path[archive_path.find("/") + 1: -4]
     try:
         if not os.path.exists(archive_path):
             return False
-
         fn_with_ext = os.path.basename(archive_path)
         fn_no_ext, ext = os.path.splitext(fn_with_ext)
         dpath = "/data/web_static/releases/"
-
         put(archive_path, "/tmp/")
         run("rm -rf {}{}/".format(dpath, fn_no_ext))
         run("mkdir -p {}{}/".format(dpath, fn_no_ext))
@@ -63,9 +55,7 @@ def do_deploy(archive_path):
         run("rm -rf {}{}/web_static".format(dpath, fn_no_ext))
         run("rm -rf /data/web_static/current")
         run("ln -s {}{}/ /data/web_static/current".format(dpath, fn_no_ext))
-
         print("New version deployed!")
         return True
-
     except Exception:
         return False
