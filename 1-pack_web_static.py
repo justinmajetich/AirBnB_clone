1d95fea32d12eaa2d31c24d35f2523c85a0479cc
alibayomy <alibayomy12@gmail.com>
2024-01-11 07:47:49 -0800

    brackets error adjusted

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -15,7 +15,7 @@ the contents of web_static folder"""
         if isdir("versions") is False:
             local("mkdir versions")
         fileName = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static").format(fileName)
+        local("tar -cvzf {} web_static".format(fileName))
         return fileName
     except:
         return None
