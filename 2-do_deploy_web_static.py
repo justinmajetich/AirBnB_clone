8ed2f309e7e5a32e8c60d1aa7bf6217a9d0d3420
alibayomy <alibayomy12@gmail.com>
2024-01-11 08:27:11 -0800

    fixing errors in file num 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,13 +17,13 @@ def do_deploy(archive_path):
         fileName = archive_path.split("/")[-1]
         noExtFile = fileName.split(".")[0]
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}'.format(path, noExtFile))
-        run('tar -xzf /tmp/ {}'.format(fileName))
+        run('mkdir -p {}{}/'.format(path, noExtFile))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(fileName, path, noExtFile))
         run('rm /tmp/{}'.format(fileName))
-        run('mv {0}{1}/web_static/* {0}{1}'.format(path, noExtFile))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, noExtFile))
         run('rm -rf {}{}/web_static'.format(path, noExtFile))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{} /data/web_static/current'.format(path, noExtFile))
+        run('ln -s {}{}/ /data/web_static/current'.format(path, noExtFile))
         return True
     except:
         return False
