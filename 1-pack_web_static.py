580b3858e973ce13ca53c0bb4af7a943436a0765
alibayomy <alibayomy12@gmail.com>
2024-01-11 07:41:10 -0800

    try testing anthr sol

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-"""Archiving a folder using fabric api"""
-
+"""
+Fabric script that generates a tgz archive from the contents of the web_static
+folder of the AirBnB Clone repo
+"""
 
 from datetime import datetime
-from fapric.api import local
+from fabric.api import local
 from os.path import isdir
 
 
 def do_pack():
-    """Generate a .tgz archive from
-        the contents of web_static folder"""
+    """generates a tgz archive"""
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         if isdir("versions") is False:
             local("mkdir versions")
-        fileName = "versions/web_static_{}".format(date)
-        local("tar -cvzf {} web_static").format(fileName)
-        return fileName
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
     except:
         return None
