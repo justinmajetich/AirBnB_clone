f70778750dc0e03b40f4d854107d52034e687e2d
alibayomy <alibayomy12@gmail.com>
2024-01-11 07:21:01 -0800

    adding a script to archive all the files

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Archiving a folder using fabric api"""
+
+
+from datetime import datetime
+from fapric.api import local
+from os.path import isdir
+
+def do_pack():
+    """Generate a .tgz archive from 
+        the contents of web_static folder"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        fileName = "versions/web_static_{}".format(date)
+        local("tar -cvzf {} web_static").format(fileName)
+        return fileName
+    except:
+        return None
