739cb09e2c07ec04bf7bda7d355c080769e56a5a
alibayomy <alibayomy12@gmail.com>
2024-01-11 07:43:16 -0800

    adding my file adjusting fabric synatx

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""
-Fabric script that generates a tgz archive from the contents of the web_static
-folder of the AirBnB Clone repo
-"""
+"""Archiving a folder using fabric api"""
+
 
 from datetime import datetime
 from fabric.api import local
@@ -10,13 +8,14 @@ from os.path import isdir
 
 
 def do_pack():
-    """generates a tgz archive"""
+    """Generate a .tgz archive from
+the contents of web_static folder"""
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         if isdir("versions") is False:
             local("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
+        fileName = "versions/web_static_{}".format(date)
+        local("tar -cvzf {} web_static").format(fileName)
+        return fileName
     except:
         return None
