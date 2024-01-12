b74b2b0d5d874a1815cbc061f5cedd49c5b33981
maqamylee0 <maqamylee0@gmail.com>
2024-01-12 17:38:28 +0300

    create test db, user and priviledges

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE hbnb_dev_db;
-CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS cat setup_mysql_test.sql | mysql -hlocalhost -uroot -p'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
