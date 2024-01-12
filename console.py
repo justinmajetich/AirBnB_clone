ff48a11ff77e752e135eff1d48436389be65818e
Maly Medina <jhamilamedina>
2024-01-11 17:22:04 -0800

    Fix:Desarrollo de configuración MySQL

--- console.py
+++ console.py
@@ -2,8 +2,9 @@
 """ Console Module """
 import cmd
 import sys
+from shlex import split
 from models.base_model import BaseModel
-from models.__init__ import storage
+from datetime import datetime
 from models.user import User
 from models.place import Place
 from models.state import State
@@ -114,23 +115,39 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
-        """ Create an obje<F11>9ct of any class"""
+        """ Usage: create <class> <key 1>=<value 2> <key 2>=<value 2> ...
+        Create a new class instances with given keys/values and print its id.
+        """
         try:
-            if not args:
+            if not line:
                 raise SyntaxError()
-            arg_list = args.split(" ")
-            kw = {}
-            for arg in arg_list[1:]:
-                arg_splited = arg.split("=")
-            arg_splited[1] = eval(arg_splited[1])
-            if type(arg_splited[1]) is str:
-                arg_splited[1] = arg_splited[1].replace("_", " ").replace('"', '\\"')
-            kw[arg_splited[0]] = arg_splited[1]
+            my_list = line.split(" ")
+            
+            kwargs = {}
+            for i in range(1, len(my_list)):
+                key, value = tuple(my_list[i].split("="))
+                if value[0] == '"':
+                    value = value.strip('"').replace("_", " ")
+                else:
+                    try:
+                        value = eval(value)
+                    except (SyntaxError, NameError):
+                        continue
+                kw[key] = value
+
+            if kwargs == {}:
+                obj = eval(my_list[0]) ()
+            else:
+                obj = eval(my_list[0]) (**kwargs)
+                storage.new(obj)
+            print(obj.id)
+            obj.save()
+
         except SyntaxError:
             print("** class name missing **")
         except NameError:
             print("** class doesn't exist **")
-        new_instance = HBNBCommand.classes[args_list[0]](**kw)
+        new_instance = HBNBCommand.classes[my_list[0]](**kwargs)
         new_instance.save()
         print(new_instance.id)
 
