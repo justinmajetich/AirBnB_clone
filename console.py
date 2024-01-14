11ddf762b17947b3ecd99bbd7bacb33a09eac093
wandilemawelela <wandile1997@gmail.com>
2024-01-14 12:48:53 -0800

    [ADD] Enhance object creation in console.py

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -118,13 +118,58 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+
+        # Split the input into class name and parameters
+        args_list = args.split(' ')
+        class_name = args_list[0]
+        param_list = args_list[1:]
+        
+        if class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
-        print(new_instance.id)
-        storage.save()
+
+        # Initialize a dictionary to store the parameters and values
+        params_dict = {}
+
+        try:
+            # Iterate through the parameters and values
+            for param in param_list:
+                # Split the parameter into key and value
+                key, value = param.split('=')
+
+                # Remove quotes from the value if it's a string
+                if value[0] == '\"' and value[-1] == '\"':
+                    value = value[1:-1]
+
+                    # Replace underscores with spaces in the string
+                    value = value.replace('_', ' ')
+
+                    # Unescape double quotes
+                    value = value.replace('\\"', '\"')
+
+                # Check if the key is a valid attribute for the class
+                if key not in HBNBCommand.classes[class_name].__dict__:
+                    print(f"** '{key}' is not a valid attribute for \
+                            {class_name} **")
+                    return
+
+                # Convert the value to the appropriate type if needed
+                if key in HBNBCommand.types:
+                    value = HBNBCommand.types[key](value)
+
+                # Update the parameters dictionary
+                params_dict[key] = value
+
+            # Create an instance of the specified class with the given parameters
+            new_instance = HBNBCommand.classes[class_name](**params_dict)
+            storage.save()
+            print(new_instance.id)
+            storage.save()
+
+        except ValueError:
+            print("** Invalid parameter syntax. Use key=value format. **")
+        except Exception as e:
+            print(f"** Error creating instance: {str(e)} **")
 
     def help_create(self):
         """ Help information for the create method """
@@ -272,7 +317,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +325,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
