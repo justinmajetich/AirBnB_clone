c76ae1783cb64aa1e4c5ce4437804c9d2fc96444
theChosenDevop <tobbey.adesanya@gmail.com>
2024-01-14 13:59:51 -0800

    Task3: defined the setup of mysql

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,11 @@
+-- A script that prepares a MySQL server for the project
+CREATE DATABASE IF NOT EXISTS hbnb_dev_DB;
+
+-- Create user if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- User have all privileges on database
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- User have SELECT privilege on the database performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
