e18e41917fa97c29c0d24132381869c6ac096f39
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-01-15 22:37:34 +0200

    a script that prepares a MySQL server for the project

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,7 @@
---A script that prepares a MySQL server for the project
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE DATABASE IF NOT EXISTS performance_schema;
+-- A script that prepares a MySQL server for the project
+CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
+CREATE DATABASE IF NOT EXISTS `performance_schema`;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
