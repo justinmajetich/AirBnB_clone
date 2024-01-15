a8163ebc6548a3501ef703f4a65fec860ef8116c
Zablon Samba <zablonsamba@gmail.com>
2024-01-15 15:11:30 +0300

    Write a script that prepares a MySQL server for the project

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,10 @@
+-- Create Database If Not Exists
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create User If Not Exists and Set Grant Permissions
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
