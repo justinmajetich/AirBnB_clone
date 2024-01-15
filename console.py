8f1f19c88cc62a6d9076256d2d16517bd3ab3c88
sayv <98850746+sayv-xo@users.noreply.github.com>
2024-01-15 12:34:06 +0000

    update console with some features

--- console.py
+++ console.py
@@ -133,7 +133,6 @@ class HBNBCommand(cmd.Cmd):
         new_instance = HBNBCommand.classes[param[0]](**cls_attr)
         new_instance.save()
         print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
