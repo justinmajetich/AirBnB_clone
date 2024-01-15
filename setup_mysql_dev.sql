ab7da5ba398ee1493f6d54b7e0020d9ac19b4ae9
Love <mzeelovegeneral@gmail.com>
2024-01-15 16:29:46 +0100

    add comment to file

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,9 @@
 --create database, user and grant privileges
+--create database if it doesnt exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+--create a user if not exist already
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+--grant usage access to newly created user
 GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
