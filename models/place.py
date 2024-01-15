936ac6cd30e81f573d96bef29dab5f1f4e8b900b
Joseph Orokpo <josephorokpo@gmail.com>
2024-01-15 22:32:26 +0100

    Update place class

--- models/place.py
+++ models/place.py
@@ -1,18 +1,41 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
+"""This is the place class"""
+
 from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
 
 
-class Place(BaseModel):
-    """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+class Place(BaseModel, Base):
+    """This class defines place
+    Attributes:
+        city_id: city id (string, 60 chars, not null, foreign key to cities.id)
+        user_id: user id (string, 60 chars, not null, foreign key to users.id)
+        name: name (string, 128 chars, not null)
+        description: description (string, 1024 chars, nullable)
+        number_rooms: number of rooms (integer, not null, default: 0)
+        number_bathrooms: number of bathrooms (integer, not null, default: 0)
+        max_guest: maximum number of guests (integer, not null, default: 0)
+        price_by_night: price by night (integer, not null, default: 0)
+        latitude: latitude (float, nullable)
+        longitude: longitude (float, nullable)
+        user: relationship with User object
+    """
+    __tablename__ = 'places'
+
+    city_id = Column(String(60), nullable=False, ForeignKey('cities.id'))
+    user_id = Column(String(60), nullable=False, ForeignKey('users.id'))
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+
+    # Relationship with User object
+    user = relationship('User', backref='places')
