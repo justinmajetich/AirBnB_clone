c94e7c848f280b799cc67f450e0842aff22cacd9
Garrick Larson Potts <ricki.glp@gmail.com>
2024-01-15 05:36:41 -0800

    corretion on task 4 for a misspelt word hbnh to hbnb

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,12 @@
 -- This script prepares a MySQL server
--- The script does not fail if user or database exists
--- The user is granted all privileges on the database
--- The user also gets SELECT privilege on the database performance_schema
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- The script does not fail if user or database exists
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
-IDENTIFIED BY 'hbnh_test_pwd';
+IDENTIFIED BY 'hbnb_test_pwd';
+
+-- The user is granted all privileges on the database
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- The user also gets SELECT privilege on the database performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
