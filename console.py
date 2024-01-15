5fbf87e346cde93ab80b397d4712e640543ac435
Love <mzeelovegeneral@gmail.com>
2024-01-15 14:30:43 +0100

    update do_create method

--- console.py
+++ console.py
@@ -158,9 +158,8 @@ class HBNBCommand(cmd.Cmd):
 
                     setattr(new_instance, key, value)
 
-        storage.save()
+        new_instance.save()
         print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
