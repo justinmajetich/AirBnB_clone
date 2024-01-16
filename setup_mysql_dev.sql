28678112de5930a45469392bd508ec92fc142618
Tajudeen <lwltjdn@gmail.com>
2024-01-16 22:35:07 +0100

    flush all privileges

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -8,3 +8,4 @@ CREATE USER IF NOT EXITS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- grant all pivileges 
 GRANT ALL PIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
