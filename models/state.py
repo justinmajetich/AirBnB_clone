f935f750994fab87a7e21b7ae1a373735915b0a0
ahmed-my <yahaya.mymohammed@gmail.com>
2024-01-16 07:58:01 +0000

    AirBnB clone MySQL

--- models/state.py
+++ models/state.py
@@ -1,8 +1,35 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""This is the state class"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+import models
+from models.city import City
+import shlex
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """The class for State
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
+
+    @property
+    def cities(self):
+        values = models.storage.all()
+        city_list = []
+        city_result = []
+        for i in values:
+            city = i.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                city_list.append(values[i])
+        for element in city_list:
+            if (element.state_id == self.id):
+                result.append(element)
+        return (city_result)
