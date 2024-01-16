970cd123c457c49b3dc8e3331b7dcdf0504c6af4
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-01-16 14:53:31 +0100

    refactor: state using sqlAlchemy

--- models/state.py
+++ models/state.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" State Module for Airbnb project """
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+
+    name = Column(String(128), nullable=False)
+    
+    # For DBStorage
+    cities = relationship('City', backref='state', cascade='all, delete-orphan')
+
+    # For FileStorage
+    @property
+    def cities(self):
+        """ Getter attribute that returns the list of City instances """
+        from models import storage
+        return [city for city in storage.all(City).values() if city.state_id == self.id]
