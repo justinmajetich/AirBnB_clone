4f8131614ab40f4f12b01cd4d14d491b789c0bc8
AtoZconcept <opeyemiolaafeez.com>
2024-01-16 11:09:25 +0100

    add

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] is '{' and pline[-1] is '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -130,8 +130,8 @@ class HBNBCommand(cmd.Cmd):
             value = value.replace("\"", '')
             setattr(new_instance, key, value)
         storage.new(new_instance)
-        print(new_instance.id)
         storage.save()
+        print(new_instance.id)
 
     def help_create(self):
         """ Help information for the create method """
@@ -326,6 +326,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
