a37d1882d8e50be729704bb06e5ff3d8bb0b4d1f
WalaEddine01 <walaaeddine33@gmail.com>
2024-01-16 17:20:37 +0100

    I made the changes on 2024-01-16 17:20:37

---
+++ fabfile.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from fabric import Connection
+from fabric import task
+
+@task
+def uname(c):
+    c = Connection(host='ubuntu@52.87.231.155', connect_kwargs={"password": "~/.ssh/id_rsa"})
+    r = c.run('uname -s')
+    
