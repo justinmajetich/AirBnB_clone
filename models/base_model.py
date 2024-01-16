6194833dab5bad74eadc81969176753c105a981d
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-16 08:38:12 +0100

    fixed unindent error at line 63

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+# !/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
@@ -60,9 +60,8 @@ class BaseModel:
 
         return dictionary
 
-
-     def delete(self):
-        """ 
+    def delete(self):
+        """
         delete the current instance from the storage
         models.storage)
         """
