edb9457034eb85fb8029b48da38bb416cb25da24
Stacy Kerubo <152691377+Stacy-Kerubo@users.noreply.github.com>
2024-01-17 01:54:34 +0300

    Task 5

--- main_delete.py
+++ main_delete.py
@@ -7,7 +7,7 @@ from models.state import State
 fs = FileStorage()
 
 # All States
-all_states = fs.all(State)
+all_states = fs.all(cls=State)
 print("All States: {}".format(len(all_states.keys())))
 for state_key in all_states.keys():
     print(all_states[state_key])
