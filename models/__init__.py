a2f12d64d26350d9706ac0d19f1d9dd62bdf5790
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-16 11:50:44 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,12 @@
 """ This model creates a unique file storage instance for our application
 """
 from models.engine.file_storage import FileStorage
+from models.engine.file_storage import DBStorage
+from os import getenv
+
+storageType = getenv('HBNB_TYPE_STORAGE')
+if storageType == 'db':
+    storage = DBStorage()
+else:
+    storage = FileStorage()
 storage.reload()
