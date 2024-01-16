cb6cd0edb53b4641165de42732393768a2389df6
WalaEddine01 <walaaeddine33@gmail.com>
2024-01-16 22:30:54 +0100

    I made the changes on 2024-01-16 22:30:54

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,6 +6,6 @@ sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chmod -R 755 /data/
+sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "38i \ \tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
 sudo service nginx restart
