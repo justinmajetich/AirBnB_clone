f602fbf38c32eca88e567263b2bc93424bd23bea
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-01-16 15:44:49 +0100

    refactor: __init__.py to dynamically select storage type based on HBNB_TYPE_STORAGE environment variable - If HBNB_TYPE_STORAGE is 'db', import DBStorage, create an instance, and reload. - If not set or set to another value, default to FileStorage and follow the same procedure.

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""This module instantiates an object of class FileStorage or DBStorage"""
+
+import os
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 
+""" Use environment variable to determine storage type"""
+storage_type = os.environ.get('HBNB_TYPE_STORAGE', 'file')
 
-storage = FileStorage()
-storage.reload()
+""" Create storage instance based on the value of HBNB_TYPE_STORAGE"""
+if storage_type == 'db':
+    storage = DBStorage()
+    storage.reload()
+else:
+    storage = FileStorage()
+    storage.reload()
