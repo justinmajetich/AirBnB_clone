f6e25771d7ab6876e8f3c81c0d6db7a891e6ece1
WalaEddine01 <walaaeddine33@gmail.com>
2024-01-16 22:22:31 +0100

    I made the changes on 2024-01-16 22:22:31

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 # Script that sets up your web servers for the deployment of web_static
-sudp apt-get -y update
+sudo apt-get -y update
 sudo apt-get install -y nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "38i \ \tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
 sudo service nginx restart
--- fabfile.py
+++ fabfile.py
@@ -2,4 +2,10 @@
 
 from fabric import Connection
 
-c = Connection(host='ubuntu@52.87.231.155', connect_kwargs={"key_filename": "~/.ssh/id_rsa"})
+c = Connection(host='f4e06185cedc@f4e06185cedc.3d34c52b.alx-cod.online', connect_kwargs={"password": "7471a2378abbb4545797"})
+# Define the remote and local paths
+remote_path = "/home"
+local_path = "/home/wala/AirBnB_clone_v2/0-setup_web_static.sh"
+
+# Use the get method to download files from the remote host to the local machine
+re = c.put(remote=remote_path, local=local_path)
