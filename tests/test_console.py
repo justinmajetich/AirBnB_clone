0bcba4e1f38e23483b9b0cfcf9378bd0d9a4f840
Stacy Kerubo <152691377+Stacy-Kerubo@users.noreply.github.com>
2024-01-16 23:21:07 +0300

    Check for class representation

--- tests/test_console.py
+++ tests/test_console.py
@@ -131,12 +131,11 @@ class TestHBNBCommand(unittest.TestCase):
             call = ('create Place city_id="0001" name="My_house" number_rooms=4 latitude=37.77 longitude=43.434')  # noqa
             self.HBNB.onecmd(call)
             pl = f.getvalue().strip()
-         # Test if the created instance and kwargs are in the
-         #    output of "all" command
+         # Test if the created instance and kwargs are in the output of "all" command
         with patch("sys.stdout", new=StringIO()) as f:
             self.HBNB.onecmd("all Place")
             output = f.getvalue()
-            self.assertIn(pl, output)
+            self.assertIn("[Place] (", output) 
             self.assertIn("'city_id': '0001'", output)
             self.assertIn("'name': 'My house'", output)
             self.assertIn("'number_rooms': 4", output)
