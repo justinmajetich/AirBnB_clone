ea9c92513cdeca584ebc2f8fd91b95dad983ba67
tannyaarr <tanyachiras@gmail.com>
2024-01-17 01:03:02 +0200

    fixed error in sql syntax

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,9 +2,9 @@
 -- script that prepares a MySQl server
 
 -- creates the database if it doesn't exist
-CREATE DATABASE IF NOT EXITS hbnb_dev_db;
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- create the user if it doesn't exit
-CREATE USER IF NOT EXITS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- grant all pivileges 
 GRANT ALL PIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,9 +3,9 @@
 
 -- creates the database if it doesn't exist
 -- database hbnb_test_db
-CREATE DATABASE IF NOT EXITS hbnb_test_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- create the user hbnb_test if it doesn't exit
-CREATE USER IF NOT EXITS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- grant all pivileges
 GRANT ALL PIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
