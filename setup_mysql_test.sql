31434d5ac8e6fec11c0c4139da4ec1a25b4338bf
Chesterkxng <cgoita00@gmail.com>
2024-01-16 12:33:20 +0000

    setup dev and test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,11 +3,12 @@ CREATE DATABASE IF NOT EXISTS hbtn_test_db;
 
 -- create the MySQL server user hbnb_test and grant all privileges.
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY "hbnb_test_pwd";
-GRANT USAGE ON *.* TO 'hbnb_test'@'localhost'
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
 -- grant SELECT privilege on the performance schema table
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
 
 -- reload with new privileges
 FLUSH PRIVILEGES;
