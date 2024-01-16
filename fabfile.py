0f0e7c83260f1a7e6811007de9db04be2422ecb1
WalaEddine01 <walaaeddine33@gmail.com>
2024-01-16 18:58:04 +0100

    I made the changes on 2024-01-16 18:58:04

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
 from fabric import Connection
-from fabric import task
 
-@task
-def uname(c):
-    c = Connection(host='ubuntu@52.87.231.155', connect_kwargs={"password": "~/.ssh/id_rsa"})
-    r = c.run('uname -s')
+c = Connection(host='ubuntu@52.87.231.155', connect_kwargs={"password": "~/.ssh/id_rsa"})
+# Create a directory (i.e. folder)
+# Hostname
+
+re = c.local("hostname")
+# Capture the output of "ls" command
+# Check if command failed
+print(re.failed)
