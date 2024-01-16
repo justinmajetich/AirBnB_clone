30c2da4e0499df5ec452b531215beba900360e54
WalaEddine01 <walaaeddine33@gmail.com>
2024-01-16 22:06:25 +0100

    I made the changes on 2024-01-16 22:06:25

--- fabfile.py
+++ fabfile.py
@@ -2,10 +2,4 @@
 
 from fabric import Connection
 
-c = Connection(host='ubuntu@52.87.231.155', connect_kwargs={"password": "~/.ssh/id_rsa"})
-# Create a directory (i.e. folder)
-# Hostname
-re = c.local("hostname")
-# Capture the output of "ls" command
-# Check if command failed
-print(re.failed)
+c = Connection(host='ubuntu@52.87.231.155', connect_kwargs={"key_filename": "~/.ssh/id_rsa"})
