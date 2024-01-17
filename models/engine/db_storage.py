77bd312e4eb40984ce81b276a603b5451412becc
AtoZconcept <opeyemiolaafeez.com>
2024-01-17 23:16:04 +0100

    pycodestyle

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -33,9 +33,9 @@ class DBStorage:
         HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
         HBNB_ENV = getenv('HBNB_ENV')
         self.__engine = create_engine(
-                                      connect.format(HBNB_MYSQL_USER,
-                                      HBNB_MYSQL_PWD, HBNB_MYSQL_HOST,
-                                      HBNB_MYSQL_DB), pool_pre_ping=True)
+            connect.format(
+                HBNB_MYSQL_USER, HBNB_MYSQL_PWD, HBNB_MYSQL_HOST, HBNB_MYSQL_DB
+                ), pool_pre_ping=True)
 
         if HBNB_ENV == "test":
             Base.metadata.drop_all(self.__engine)
