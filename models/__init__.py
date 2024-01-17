cdc0ca88f825d27b8683335b8d7a94f9b65a5f32
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-01-17 19:00:53 +0000

    Create file storage system

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+"""This module create an object of class file storage"""
+from os import getenv
 
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
-storage = FileStorage()
+if storage_t != "db":
+    from models.engine.file_starage import FileStorage
+    storage = FileStorage()
+else:
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
 storage.reload()
