8a015b7fdfc939fc50c8f5c738d57711581a6587
Dodzi Keglo <stephanekeglo@gmail.com>
2024-01-17 03:17:03 +0000

    Fix database name

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,5 @@
 -- create database if exists not failure
-CREATE DATABASE IF NOT EXISTS hbtn_test_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
 -- create the MySQL server user hbnb_test and grant all privileges.
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY "hbnb_test_pwd";
@@ -8,7 +8,6 @@ GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 -- grant SELECT privilege on the performance schema table
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
-GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
 
 -- reload with new privileges
 FLUSH PRIVILEGES;
