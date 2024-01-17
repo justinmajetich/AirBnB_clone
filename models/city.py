c9abaab3f246372b22f52f2a6dde59e58ca3535c
Stacy Kerubo <152691377+Stacy-Kerubo@users.noreply.github.com>
2024-01-17 22:47:13 +0300

    Upgrade city

--- models/city.py
+++ models/city.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel,Base
+from sqlalchemy import Column, ForeignKey, String
 
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+__tablename__ = "cities"
+# Represents a column containing a string (128 characters)
+name = Column(String(128), nullable=False)
+# Represents a column containing a string (60 characters)
+# It is a foreign key to states.id
+state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    
