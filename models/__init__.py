6135d4436bad82d43b2541694483d3f6e5ee8725
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-01-17 23:27:54 +0000

    Patial update

--- models/__init__.py
+++ models/__init__.py
@@ -2,9 +2,9 @@
 """This module create an object of class file storage"""
 from os import getenv
 
-storage_type = getenv("HBNB_TYPE_STORAGE")
+storage_t = getenv("HBNB_TYPE_STORAGE")
 
-if storage_type != "db":
+if storage_t != "db":
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
 else:
--- models/place.py
+++ models/place.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, Float
 
 
 class Place(BaseModel):
