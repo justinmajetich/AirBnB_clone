57e34be1ba35f7b0411c088deccd4de5e948a028
Stacy Kerubo <152691377+Stacy-Kerubo@users.noreply.github.com>
2024-01-17 23:27:43 +0300

    init.py update

---
+++ models/engine/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module instantiates a storage object
+"""
+from os import getenv
+
+HBNB_TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
+
+if HBNB_TYPE_STORAGE == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
