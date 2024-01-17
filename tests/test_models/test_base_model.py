68b13caca0600a995ad19cb4b6f7ac45b121dfd0
ayaarragab <ayarsk4343@gmail.com>
2024-01-17 15:04:47 +0200

    adding docstrings

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" testing module for base
+"""
+testing module for base
 """
 from models.base_model import BaseModel
 import unittest
@@ -23,27 +24,37 @@ class test_basemodel(unittest.TestCase):
         self.value = BaseModel
 
     def setUp(self):
-        """ setup """
+        """
+        setup
+        """
         pass
 
     def tearDown(self):
-        """tear down"""
+        """
+        tear down
+        """
         os.remove('file.json')
 
     def test_default(self):
-        """ default test"""
+        """
+        default test
+        """
         i = self.value()
         self.assertEqual(type(i), self.value)
 
     def test_kwargs(self):
-        """ kawrgs test"""
+        """
+        kawrgs test
+        """
         i = self.value()
         copy = i.to_dict()
         new = BaseModel(**copy)
         self.assertFalse(new is i)
 
     def test_kwargs_int(self):
-        """kawrgs test"""
+        """
+        kawrgs test
+        """
         i = self.value()
         copy = i.to_dict()
         copy.update({1: 2})
@@ -51,7 +62,9 @@ class test_basemodel(unittest.TestCase):
             new = BaseModel(**copy)
 
     def test_save(self):
-        """ Testing save """
+        """
+        Testing save
+        """
         i = self.value()
         i.save()
         key = self.name + "." + i.id
@@ -60,41 +73,55 @@ class test_basemodel(unittest.TestCase):
             self.assertEqual(j[key], i.to_dict())
 
     def test_str(self):
-        """testing str """
+        """
+        testing str
+        """
         i = self.value()
         self.assertEqual(str(i), '[{}] ({}) {}'.format(self.name, i.id,
                          i.__dict__))
 
     def test_todict(self):
-        """ test_todict"""
+        """
+        test_todict
+        """
         i = self.value()
         n = i.to_dict()
         self.assertEqual(i.to_dict(), n)
 
     def test_kwargs_none(self):
-        """ test_kwargs_none"""
+        """
+        test_kwargs_none
+        """
         n = {None: None}
         with self.assertRaises(TypeError):
             new = self.value(**n)
 
     def test_kwargs_one(self):
-        """ test_kwargs_one"""
+        """
+        test_kwargs_one
+        """
         n = {'Name': 'test'}
         with self.assertRaises(KeyError):
             new = self.value(**n)
 
     def test_id(self):
-        """ test_id"""
+        """
+        test_id
+        """
         new = self.value()
         self.assertEqual(type(new.id), str)
 
     def test_created_at(self):
-        """ test_created_at"""
+        """
+        test_created_at
+        """
         new = self.value()
         self.assertEqual(type(new.created_at), datetime.datetime)
 
     def test_updated_at(self):
-        """ test_updated_at"""
+        """ 
+        test_updated_at
+        """
         new = self.value()
         self.assertEqual(type(new.updated_at), datetime.datetime)
         n = new.to_dict()
