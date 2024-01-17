be443faadedb5223f8e8c5f78b04d469cad4452e
aissa zerrad <jesus.jesus.core@gmail.com>
2024-01-17 14:19:36 -0800

    setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,7 @@
 -- Setup SQL for the project
 -- Create the main database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
 -- Create the main user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
 -- Grant privileges on the main database to the user
 GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
