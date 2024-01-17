afbc83c4a5a950f3a1c92e9817db2bdbb0369332
Wandile Mawelela <wandile1997@gmail.com>
2024-01-18 01:26:43 +0200

    [REV] Revert

--- models/base_model.py
+++ models/base_model.py
@@ -2,17 +2,10 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-from sqlalchemy import Column, String, DateTime
-from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
-    id = Column(String(60), nullable=False, primary_key=True, unique=True)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
@@ -20,36 +13,25 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
         else:
-            # try:
-            #     kwargs['updated_at'] = datetime\
-            #         .strptime(kwargs['updated_at'],
-            #                   '%Y-%m-%dT%H:%M:%S.%f')
-            #     kwargs['created_at'] = datetime.\
-            #         strptime(kwargs['created_at'],
-            #                  '%Y-%m-%dT%H:%M:%S.%f')
-            # except Exception as e:
-            #     print("Error parsing dates: ", kwargs)
-            #
-            # # Ensure that 'id' is present in kwargs or generate a new one
-            # kwargs['id'] = kwargs.get('id', str(uuid.uuid4()))
-            #
-            # if '__class__' in kwargs:
-            #     del kwargs['__class__']
-            #
-            # self.__dict__.update(kwargs)
-            for key, value in kwargs.items():
-                    setattr(self, key, value)
+            try:
+                kwargs['updated_at'] = datetime\
+                    .strptime(kwargs['updated_at'],
+                              '%Y-%m-%dT%H:%M:%S.%f')
+                kwargs['created_at'] = datetime.\
+                    strptime(kwargs['created_at'],
+                             '%Y-%m-%dT%H:%M:%S.%f')
+            except Exception as e:
+                print("Error parsing dates: ", kwargs)
 
-            if 'created_at' in kwargs:
-                self.created_at = datetime.strptime(kwargs['created_at'],'%Y-%m-%dT%H:%M:%S.%f')
+            # Ensure that 'id' is present in kwargs or generate a new one
+            kwargs['id'] = kwargs.get('id', str(uuid.uuid4()))
 
-            if 'updated_at' in kwargs:
-                self.updated_at = datetime.strptime(kwargs['updated_at'],'%Y-%m-%dT%H:%M:%S.%f')
-
-            self.id = kwargs.get('id', str(uuid.uuid4()))
             if '__class__' in kwargs:
                 del kwargs['__class__']
+
+            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -70,7 +52,6 @@ class BaseModel:
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary.pop('_sa_instance_state', None)
-
 
         """
         Ensure that 'created_at' and 'updated_at'
--- models/state.py
+++ models/state.py
@@ -1,29 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from os import getenv
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
 
-class State(BaseModel, Base):
+
+class State(BaseModel):
     """ State class """
-    __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-
-    # For DBStorage
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship("City", backref="state", cascade="all, delete-orphan")
-
-    # For FileStorage
-    @property
-    def cities(self):
-        """ Getter attribute cities that returns the list of City instances with state_id equals to the current State.id """
-        from models import storage
-        cities_list = []
-        for city in storage.all("City").values():
-            if city.state_id == self.id:
-                cities_list.append(city)
-        return cities_list
+    name = ""
 
     def __init__(self, *args, **kwargs):
         super().__init__()
