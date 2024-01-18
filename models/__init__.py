a5530386dd7aeb011899ba4ccd73bf4f8ce08764
Maly Medina <jhamilamedina>
2024-01-17 21:06:00 -0800

    Fix: Mejoras en FileStorage

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-
 # from models.base_model import BaseModel, Base
 from os import getenv
-
-
 """
 This module creates an instance of a FileStorage class object.
 """
@@ -16,5 +13,8 @@ if storage_t == 'db':
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
+    file_storage = FileStorage()
+    new_state_id = file_storage.create_state("New State")
+    print(f"Nueva ID del estado creado: {new_state_id}")
 
 storage.reload()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,7 @@
 """This is the file storage class for AirBnB"""
 
 import json
+import os
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -23,7 +24,7 @@ class FileStorage:
         __objects: objects will be stored
     """
     # string - path to the JSON file
-    __file_path = "file.json"
+    __file_path = os.path.abspath("file.json")
     # dictionary - empty but will store all objects by <class name>.id
     __objects = {}
 
@@ -34,11 +35,11 @@ class FileStorage:
         """
         if cls is not None:
             new_dict = {}
-            for key, value in self.__objects.items():
+            for key, value in FileStorage.__objects.items():
                 if cls == value.__class__ or cls == value.__class__.__name__:
                     new_dict[key] = value
             return new_dict
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """sets __object to given obj
@@ -47,36 +48,49 @@ class FileStorage:
         """
         if obj is not None:
             key = obj.__class__.__name__ + "." + obj.id
-            self.__objects[key] = obj
+            FileStorage.__objects[key] = obj
 
     def save(self):
         """serialize the file path to JSON file path
         """
         json_objects = {}
-        for key in self.__objects:
-            json_objects[key] = self.__objects[key].to_dict()
+        for key in FileStorage.__objects:
+            json_objects[key] = FileStorage.__objects[key].to_dict()
         with open(self.__file_path, 'w', encoding='utf-8') as f:
             json.dump(json_objects, f)
 
     def reload(self):
         """serialize the file path to JSON file path
         """
+        print(f"Trying to open the file: {self.__file_path}")
         try:
             with open(self.__file_path, 'r', encoding='utf-8') as f:
                 jo = json.load(f)
             for key in jo:
-                self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
+                FileStorage.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
         except FileNotFoundError:
-            pass
+            print(f"El archivo {self.__file_path} does not exist. Creating a new one...")
 
     def delete(self, obj=None):
         """ delete an existing element
         """
         if obj is not None:
             key = obj.__class__.__name__ + '.' + obj.id
-            del self.__objects[key]
+            del FileStorage.__objects[key]
+    
+    def create_state(self, state_name):
+        """Crea un nuevo estado y devuelve su ID"""
+        # Generar nueva ID
+        new_id = str(len(FileStorage.__objects) + 1)
 
-    def close(self):
-        """ calls reload()
-        """
-        self.reload()
+        # Crear una nueva instancia de State
+        new_state = State(id=new_id, name=state_name)
+
+        # Agregar el nuevo estado al diccionario de objetos
+        self.new(new_state)
+
+        # Guardar los datos actualizados en el archivo
+        self.save()
+
+        # Devolver la nueva ID
+        return new_id
