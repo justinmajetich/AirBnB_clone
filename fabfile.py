330a11430292336797f5555c9c16c46954fbeaaf
WalaEddine01 <walaaeddine33@gmail.com>
2024-01-18 21:25:15 +0100

    I made the changes on 2024-01-18 21:25:14

--- fabfile.py
+++ fabfile.py
@@ -5,7 +5,7 @@ from fabric import Connection
 c = Connection(host='f4e06185cedc@f4e06185cedc.3d34c52b.alx-cod.online', connect_kwargs={"password": "7471a2378abbb4545797"})
 # Define the remote and local paths
 remote_path = "/home"
-local_path = "/home/wala/AirBnB_clone_v2/0-setup_web_static.sh"
+local_path = "/home/wala/AirBnB_clone_v2/1-pack_web_static.py"
 
 # Use the get method to download files from the remote host to the local machine
 re = c.put(remote=remote_path, local=local_path)
