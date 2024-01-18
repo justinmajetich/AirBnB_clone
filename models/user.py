ba24ea134970192957e6e8889fe240d82e3dab30
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-18 02:12:24 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -15,6 +15,8 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
+        places = relationship(
+            'Place', cascade="all, delete-orphan", backref="user")
     else:
         email = ""
         password = ""
