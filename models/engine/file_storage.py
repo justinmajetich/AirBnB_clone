378fa3a90f7a749a2a7aec363ca29e740b392288
ayaarragab <ayarsk4343@gmail.com>
2024-01-18 11:25:38 +0200

    update on all and did a new method delete

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -65,15 +65,6 @@ class FileStorage:
 
     def delete(self, obj=None):
         """Deletes obj from __objects"""
-        if obj and clsnameid in self.all():
-            clsname = obj.to_dict()['__class__']
-            clsnameid = clsname + '.' + obj.id
-            all_objects = self.all()
-            all_objects.pop(clsnameid)
-            self.save()
-
-    # def delete(self, obj=None):
-    #     ''' delete obj from __objects if it is inside '''
-    #     if obj:
-    #         key = '{}.{}'.format(type(obj).__name__, obj.id)
-    #         del FileStorage.__objects[key]
+        if obj:
+            key = '{}.{}'.format(type(obj).__name__, obj.id)
+            del FileStorage.__objects[key]
