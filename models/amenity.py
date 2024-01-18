aa28b9e162779ad959b3a514391159e9b091507b
lavyatarah <mattlavyy@gmail.com>
2024-01-18 08:40:35 +0300

    update

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+"""define clsaa Amenity"""
+
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
+    """inherits from BaseModel"""
     name = ""
