bcc50d435733548fbd6dddb0183a29f2878f0898
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-01-19 12:21:39 +0000

    Revert "Import mission ones"
    This reverts commit d527b3e914f4dc0c4df433aa7816084ad55a0fc8.

--- models/amenity.py
+++ models/amenity.py
@@ -4,9 +4,4 @@ from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Object of Amenity"""
-    if models.strage_t = 'db':
-        __tablename__ = 'amenities'
-        name = Column(String(128), nullable=False)
-    else:
-        name = ""
+    name = ""
