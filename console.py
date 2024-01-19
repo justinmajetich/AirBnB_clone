28d554f8c055ee4f1790220eece0114cdabd1d9c
Stacy Kerubo <152691377+Stacy-Kerubo@users.noreply.github.com>
2024-01-19 15:50:48 +0300

    Import error

--- console.py
+++ console.py
@@ -3,13 +3,17 @@
 import cmd
 import sys
 from models.base_model import BaseModel
-from models.engine.file_storage import storage
+from models.engine.file_storage import FileStorage
 from models.user import User
 from models.place import Place
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
+
+# Create the storage object
+storage = FileStorage()
+storage.reload()
 
 
 class HBNBCommand(cmd.Cmd):
