6f5dc7f095d63bfa8bc3a6e152248f2a839bcab3
Stace-cmd <stacykerongo@gmail.com>
2024-01-19 04:01:04 -0800

    City update

--- models/city.py
+++ models/city.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel,Base
 from sqlalchemy import Column, ForeignKey, String
+from models.base_model import BaseModel, Base
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and namegit add .
+class City(BaseModel, Base):
     """
-__tablename__ = "cities"
-# Represents a column containing a string (128 characters)
-name = Column(String(128), nullable=False)
-# Represents a column containing a string (60 characters)
-# It is a foreign key to states.id
-state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    
+    >>City inherits from BaseModel and Base (respect the order)
+    >>class attribute __tablename__ -
+            represents the table name, cities
+    >>class attribute name
+            represents a column containing a string (128 characters)
+            cant be null
+    >>class attribute state_id
+            represents a column containing a string (60 characters)
+            cant be null
+            is a foreign key to states.id
+    """
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
