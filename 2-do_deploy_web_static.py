ebe2940bdf436f174b98020bbacd843e3704e6b6
walaeddine01 <walaaeddine33@gmail.com>
2024-01-19 20:29:08 +0100

    I made the changes on 2024-01-19 20:29:08

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Fabric script that distributes an archive to your web servers,
+using the function do_deploy
+"""
+from fabric.api import *
+from os import path
+
+
+
+             
--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 
 from fabric import Connection
-
-c = Connection(host='f4e06185cedc@f4e06185cedc.3d34c52b.alx-cod.online', connect_kwargs={"password": "7471a2378abbb4545797"})
-# Define the remote and local paths
-remote_path = "/home"
-local_path = "/home/wala/AirBnB_clone_v2/1-pack_web_static.py"
-
-# Use the get method to download files from the remote host to the local machine
-re = c.put(remote=remote_path, local=local_path)
+from fabric.api import *
+c = Connection(host='ubuntu@54.237.91.183', connect_kwargs={"password": "~/.ssh/id_rsa"})
