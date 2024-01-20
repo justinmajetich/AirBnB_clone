35a856b2c0db96c0a2230cde754e2c443aa9417f
mburuxx <njihiamburu.3@gmail.com>
2024-01-20 16:11:55 +0300

    Update on amenities class

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-""" Amenity class"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+"""Defines the Amenity class."""
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
-from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
@@ -18,4 +19,5 @@ class Amenity(BaseModel, Base):
     """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity", viewonly=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   viewonly=False)
