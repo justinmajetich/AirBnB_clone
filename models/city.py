b8495d171cc9227de654f4a0d39ea0a58a7790a4
trust <isaactrust447@gmail.com>
2024-01-21 04:33:20 +0100

    DBStorage - Place

--- models/city.py
+++ models/city.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String,Integer, ForeignKey
 from sqlalchemy.orm import relationship
+from models.state import State
+from models.place import Place
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
@@ -11,4 +13,4 @@ class City(BaseModel):
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
 
-    state = relationship('Place', back_populates='cities')
+    places = relationship('Place', back_populates='cities')
