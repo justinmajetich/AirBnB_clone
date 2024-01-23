d936998fd83e581835e960dd2d508c29bdec3221
Obolo emmanuel <e.obolo@alustudent.com>
2024-01-23 21:01:57 +0200

    Update FileStorage: (models/engine/file_storage.py) by adding def delete(self, obj=None) and updating def all(self) to def all(self, cls=None)

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,15 +2,39 @@
 """This module defines a class to manage file storage for hbnb clone"""
 import json
 
-
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+    def delete(self, obj=None):
+        """
+        to delete obj from __objects if its inside-
+        if obj is equal to None, the method should
+        not do anything.
+        """
+        if obj is None:
+            return
+        obj_id = obj.id
+        obj_class_name = type(obj).__name__
+        obj_key = f"{obj_class_name}.{obj_id}"
+        del self.all()[obj_key]
+
+    def all(self, cls=None):
+        """
+        Returns a dictionary of models currently in storage
+        if cls == None or that returns the list of 
+        objects of one type of class if cls == a classname
+        """
+        if cls is None:
+            return FileStorage.__objects
+        obj_list = {
+            key: self.all()[key]
+            for key in list(self.all())
+            if cls.__name__ in key
+        }
+
+        return obj_list
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
