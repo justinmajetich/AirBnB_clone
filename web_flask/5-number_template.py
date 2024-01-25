949ddeaa2aab9b25d5137cf3ae3bd6a8aae14c15
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-01-25 15:53:15 +0100

    Add docstrings to Flask routes

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -8,16 +8,19 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
+    """Returns a greeting message"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """Returns 'HBNB'"""
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
+    """Returns a string with 'C' followed by the value of 'text'"""
     text = text.replace("_", " ")
     return f"C {text}"
 
@@ -25,17 +28,20 @@ def c(text):
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text="is cool"):
+    """Returns a string with 'python' followed by the value of 'text'"""
     text = text.replace("_", " ")
     return "python " + text
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
+    """Returns a string with the value of 'n' followed by 'is a number'"""
     return f"{n} is a number"
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
+    """Renders a template '5-number.html' with the value of 'n'"""
     return render_template('5-number.html', n=n)
 
 
