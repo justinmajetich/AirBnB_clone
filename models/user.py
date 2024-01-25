2a0e51f2dd5e653b4a62715eae9c2ba9eb0dc116
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-01-25 20:34:11 +0200

    Adding Task 7

--- models/user.py
+++ models/user.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = "users"
+    email = Column("email", String(128), nullable=False)
+    password = Column("password", String(128), nullable=False)
+    first_name = Column("first_name", String(128), nullable=True)
+    last_name = Column("last_name", String(128), nullable=True)
