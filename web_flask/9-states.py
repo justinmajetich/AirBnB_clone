1d7dffcdde41fe4308ad26c7a56e22a91d457857
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-01-26 15:56:08 +0100

    Refactor state_id variable to id in states route

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -10,12 +10,12 @@ app = Flask(__name__)
 
 
 @app.route("/states", strict_slashes=False)
-@app.route("/states/<state_id>", strict_slashes=False)
-def states(state_id=None):
+@app.route("/states/<id>", strict_slashes=False)
+def states(id=None):
     states = storage.all(State)
-    if state_id is not None:
-        state_id = "State." + state_id
-    return render_template("9-states.html", states=states, state_id=state_id)
+    if id is not None:
+        id = "State." + id
+    return render_template("9-states.html", states=states, state_id=id)
 
 
 @app.teardown_appcontext
