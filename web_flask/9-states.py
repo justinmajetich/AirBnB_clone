790f8ed41dfa8dbfd0d96853260d6f58f8fe2c93
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-01-26 15:53:54 +0100

    Refactor Flask routes and fix teardown function

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
+"""start a flask app"""
 
 from flask import Flask, render_template
-from models import storage
 from models import *
+from models import storage
 from models.state import State
 
 app = Flask(__name__)
 
 
-@app.route('/states', strict_slashes=False)
-@app.route('/states/<id>', strict_slashes=False)
-def state(id=None):
-    States = storage.all(State)
-    if id is not None:
-        id = "State.{}".format(id)
-    return render_template('9-states.html', states=States, state_id=id)
+@app.route("/states", strict_slashes=False)
+@app.route("/states/<state_id>", strict_slashes=False)
+def states(state_id=None):
+    states = storage.all(State)
+    if state_id is not None:
+        state_id = "State." + state_id
+    return render_template("9-states.html", states=states, state_id=state_id)
 
 
 @app.teardown_appcontext
-def tear_db(exception):
-    return storage.close()
+def tear_db(e):
+    storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
