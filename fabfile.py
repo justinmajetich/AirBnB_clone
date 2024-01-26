ebc005460e18f0eb7df96044b68969febd9ebdd6
walaeddine01 <walaaeddine33@gmail.com>
2024-01-26 21:48:08 +0100

    I made the changes on 2024-01-26 21:48:08

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,12 @@
 #!/home/wala/AirBnB_clone_v2/venv/bin/python3
+from fabric.api import run, env, task
 
-from fabric.api import *
+env.hosts = ['54.237.91.183', '54.173.37.227']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
 
 
-connections()
+def ls():
+    run("uname")
+
+# Run the task
