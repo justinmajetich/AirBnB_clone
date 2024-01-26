5d391375fde60e818b1ece9c4e7d1393269ea863
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-01-26 14:59:53 +0100

    Add Flask web application for displaying cities by states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+
+
+from flask import Flask, render_template
+from models import storage
+from models import *
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Displays an HTML page with render template"""
+    States = storage.all(State)
+    return render_template('8-cities_by_states.html', states=States)
+
+
+@app.teardown_appcontext
+def tear_db(exception):
+    """Closes the storage"""
+    return storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
