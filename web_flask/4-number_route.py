4d7859d01ef501bc3df5c8c47a94dadac7e25fc5
alibayomy <alibayomy12@gmail.com>
2024-01-27 23:06:58 +0000

    display n is aonly if n is an intege

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """ Returns some text. """
+    return 'Hello HBNB!'
+
+
+@app.route("/hbnb", strict_slashes=False)
+def disp_hbnb():
+    """display HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """Display  c follwed by the value text"""
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_text(text='is cool'):
+    """display Python, followed by the value of the text """
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+@app.route('/number/<int:n>')
+def number_root():
+    """display n is a number only if n is an integer"""
+    n = str(n)
+    return '{} is a number'.format(n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
