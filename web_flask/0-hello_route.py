f03bf8b4b2a70458711de754046ded0f0f14dc4a
alibayomy <alibayomy12@gmail.com>
2024-01-27 14:13:34 +0000

    adding func documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,7 +6,8 @@ from flask import Flask
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
+	"""hello hbnb"""
 	return "Hello HBNB!"
 
-if __name__ = "__main__":
+if __name__ == "__main__":
 	app.run(host='0.0.0.0', port='5000')
