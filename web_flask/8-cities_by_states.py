05c548edfd9f80f0817af66d9248a42101f5288c
Ayobami Oludemi <oludemiayobami@gmail.com>
2024-01-27 14:42:22 +0100

    commit

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(error):
+    """Closes the database again at the end of the request."""
+    storage.close()
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def list_states():
+    """Handles the state route"""
+    states = storage.all(State)
+    return render_template("8-cities_by_states.html", states=states.values())
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
