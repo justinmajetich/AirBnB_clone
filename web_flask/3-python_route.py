3bc3623111ddce6040108d54d7c7e31e92af5cba
alibayomy <alibayomy12@gmail.com>
2024-01-27 23:03:13 +0000

    python follwed by text

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -33,5 +33,7 @@ def python_text(text='is cool'):
     """display Python, followed by the value of the text """
     text = text.replace('_', ' ')
     return 'Python {}'.format(text)
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
