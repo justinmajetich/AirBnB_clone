b11a6a5c4b36efff26beef1d4fdef17915d16770
alibayomy <alibayomy12@gmail.com>
2024-01-27 23:04:29 +0000

    adding the / to the path

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -27,7 +27,7 @@ def c_text(text):
     return 'C {}'.format(text)
 
 
-@app.route('python')
+@app.route('/python/')
 @app.route('/python/<text>')
 def python_text(text='is cool'):
     """display Python, followed by the value of the text """
