02e35405a50ebb810e59b0e398f8fbcd6017dc86
walaeddine01 <walaaeddine33@gmail.com>
2024-01-28 21:52:01 +0100

    I made the changes on 2024-01-28 21:52:01

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -3,13 +3,11 @@
 Script that starts a Flask web application
 """
 from flask import Flask
-
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
+def hello_hbnb():
     """
     Displays Hello HBNB!
     """
@@ -28,5 +26,5 @@ if __name__ == '__main__':
     app.run(
         host='0.0.0.0',
         port=5000,
-        debug=True
+        debug=False
         )
