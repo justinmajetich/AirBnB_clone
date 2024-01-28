452fab1a2f675ca8861c44a1ebf8600193afe165
Georgia2292 <georgelina2292@gmail.com>
2024-01-28 11:16:27 -0800

    Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
-"""Corriendo el servicio web con Flask"""
-
+""" Starts a Flash Web Application """
 from flask import Flask
-
-"""Crea una instancia de la clase Flask llamada app"""
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-# Define una funcion home y retorna la cadena Hello HBNB
 def hello_hbnb():
-    return "Hello HBNB!"
-
+    """ Prints a Message when / is called """
+    return 'Hello HBNB!'
 
 if __name__ == "__main__":
-    # Esta funcion asegura que este script de Flask solo se ejecute
-    # cuando ejecutas este script directamente y no cuando importas
-    # como un modulo
+    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
