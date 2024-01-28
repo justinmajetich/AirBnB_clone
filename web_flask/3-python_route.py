a6b21e5c1b8a5e758327a8898211ff63725dff2c
walaeddine01 <walaaeddine33@gmail.com>
2024-01-28 22:09:31 +0100

    I made the changes on 2024-01-28 22:09:31

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Displays Hello HBNB!
+    """
+    return ('Hello HBNB!')
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Displays HBNB!
+    """
+    return ('HBNB')
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    '''
+    display “C ” followed by the value of the text variable
+    '''
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text="is cool"):
+    return f'Python {text.replace("_", " ")}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
