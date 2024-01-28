0b7eef828ad8fcc9df8cc09bdd5c274420a303d3
walaeddine01 <walaaeddine33@gmail.com>
2024-01-28 21:55:42 +0100

    I made the changes on 2024-01-28 21:55:42

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,15 +8,19 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """ Display Hello HBNB! """
-    return ("Hello HBNB!")
+    """
+    Displays Hello HBNB!
+    """
+    return ('Hello HBNB!')
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ Display HBNB """
-    return ("HBNB")
+    """
+    Displays HBNB!
+    """
+    return ('HBNB!')
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=False)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
