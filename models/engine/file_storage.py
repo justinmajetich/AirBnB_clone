72bd96e3460e8da29f73f0cf755c8560f349d0b9
Seth Ketsi <christambassador12@gmail.com>
2024-01-28 17:32:03 +0000

    updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -65,3 +65,7 @@ class FileStorage:
         if key in self.__objects:
             self.__objects.pop(key)
             self.save()
+
+    def close(self):
+        """call reload() method for deserializing the JSON file to objects"""
+        self.reload()
