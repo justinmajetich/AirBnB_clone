72af805237cd4f6f85f88bbee0958c4ff937dec9
walaeddine01 <walaaeddine33@gmail.com>
2024-01-28 22:13:58 +0100

    I made the changes on 2024-01-28 22:13:58

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -30,7 +30,7 @@ def c_text(text):
     text = text.replace("_", " ")
     return f"C {text}"
 
-
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text="is cool"):
     return f'Python {text.replace("_", " ")}'
