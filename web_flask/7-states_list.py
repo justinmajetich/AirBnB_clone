d24b357758b21b83658177dfcacdd72e15065124
walaeddine01 <walaaeddine33@gmail.com>
2024-01-28 23:20:13 +0100

    I made the changes on 2024-01-28 23:20:13

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """
+    Display a HTML page
+    """
+    states = storage.all()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def close_down(self):
+    """
+    Removes the current SQLAlchemy Session
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
