436325c48c46497fc05749222e459d7ec0ceaa5f
Seth Ketsi <christambassador12@gmail.com>
2024-01-28 21:23:46 +0000

    Task 7 updated

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -55,8 +55,9 @@ def number_odd_or_even(n):
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """Display HTML page with list of states"""
-    states = storage.all(State)
-    return render_template("7-states_list.html", states=states)
+    states = storage.all(State).values()
+    states_list = sorted(states, key=lambda state: state.name)
+    return render_template("7-states_list.html", states=states_list)
 
 
 @app.teardown_appcontext
