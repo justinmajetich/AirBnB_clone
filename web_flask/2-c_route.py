fcd12bfb708ce9c9228aeebcaa70c78862584968
Georgia2292 <georgelina2292@gmail.com>
2024-01-28 19:58:29 -0800

    C is fun!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-"""Servivcio Web con Flask"""
+""" 2. Script to start a Flask web application with 3 view functions """
 
 from flask import Flask
 
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
+@app.route('/')
+def hello_world():
+    """ Returns some text. """
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
+@app.route('/hbnb')
+def hello():
+    """ Return other text. """
     return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def mostrar_c(text):
-    # Reemplaza los símbolos de subrayado (_) con un espacio
-    cleaned_text = text.replace('_', ' ')
-    return f'C {cleaned_text}'
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route('/c/<text>')
+def c_text(text):
+    """ replace text with variable. """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
