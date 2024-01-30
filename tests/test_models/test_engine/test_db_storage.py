598064e0ed352cdac76891320a107561f18f7004
joshbam-joshwilly <j.alana@alustudent.com>
2024-01-30 19:01:11 +0100

    the web_flask

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""test for file storage"""
+import unittest
+import pep8
+
+
+class TestDBStorage(unittest.TestCase):
+    '''Tests the DBStorage storage engine'''
+
+    def setUp(self):
+        """SetUp env for test"""
+        pass
+
+    def tearDown(self):
+        """teardown"""
+        pass
+
+    def test_all(self):
+        """Test class method all()"""
+        pass
+
+    def test_new(self):
+        """Test new method"""
+        pass
+
+    def test_save(self):
+        """Test save method
+        """
+        pass
+
+    def test_delete(self):
+        """Tests delete
+        """
+        pass
+
+    def test_reload(self):
+        """Tests reload
+        """
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
