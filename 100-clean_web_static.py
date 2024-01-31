1e881a53d6bde169d2b82764e74014bc173212a5
EligiblePeace01 <enwerempeace@gmail.com>
2024-01-31 02:58:58 -0800

    fix

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,22 +1,33 @@
 #!/usr/bin/python3
-""" Function that deploys """
+"""
+Deletes out-of-date archives
+fab -f 100-clean_web_static.py do_clean:number=2
+    -i ssh-key -u ubuntu > /dev/null 2>&1
+"""
+
+import os
 from fabric.api import *
 
-
-env.hosts = ['35.237.120.205', '44.200.65.242']
-env.user = "ubuntu"
+env.hosts = ['54.157.135.236', '100.25.119.58']
 
 
 def do_clean(number=0):
-    """ CLEANS """
+    """Delete out-of-date archives.
+    Args:
+        number (int): The number of archives to keep.
+    If number is 0 or 1, keeps only the most recent archive. If
+    number is 2, keeps the most and second-most recent archives,
+    etc.
+    """
+    number = 1 if int(number) == 0 else int(number)
 
-    number = int(number)
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
 
-    if number == 0:
-        number = 2
-    else:
-        number += 1
-
-    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
-    path = '/data/web_static/releases'
-    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
