8b257982f9428cfef00bb206ea9a500f9184f9c9
ayaarragab <ayarsk4343@gmail.com>
2024-01-31 17:55:37 +0200

    Iam tired

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -22,19 +22,15 @@ if [ -L "$current_link" ]; then
     rm "$current_link"
 fi
 
-ln -s "$target_folder" "$current_link"
-chown -R ubuntu:ubuntu /data/
+ln -fs "$target_folder" "$current_link"
+chown -hR ubuntu:ubuntu /data/
 #!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployment of web_static
 if ! command -v nginx &> /dev/null; then
     apt-get -y update
     apt-get -y  install nginx
 fi
-mkdir -p /data/
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 touch /data/web_static/releases/test/index.html && echo "<html>
   <head>
   </head>
@@ -50,4 +46,4 @@ fi
 ln -s "$target_folder" "$current_link"
 chown -R ubuntu:ubuntu /data/
 sed -i '14i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-enabled/default
-service nginx restart
+service nginx start
