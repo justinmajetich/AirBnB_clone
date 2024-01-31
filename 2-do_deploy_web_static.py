5bab72f6d9d90cd1813f5308750dabc529660242
Wandile Mawelela <wandile1997@gmail.com>
2024-02-01 00:56:05 +0200

    [ADD] Completed task 2: Write a Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Distributes archive to webservers
+"""
+import os.path
+from fabric.api import *
+from fabric.operations import run, put, sudo
+env.hosts = ['100.26.221.139', '54.237.207.123']
+
+
+def do_deploy(archive_path):
+    """ Deploy module """
+    if (os.path.isfile(archive_path) is False):
+        return False
+
+    try:
+        new_comp = archive_path.split("/")[-1]
+        new_folder = ("/data/web_static/releases/" + new_comp.split(".")[0])
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(new_folder))
+        run("sudo tar -xzf /tmp/{} -C {}".
+            format(new_comp, new_folder))
+        run("sudo rm /tmp/{}".format(new_comp))
+        run("sudo mv {}/web_static/* {}/".format(new_folder, new_folder))
+        run("sudo rm -rf {}/web_static".format(new_folder))
+        run('sudo rm -rf /data/web_static/current')
+        run("sudo ln -s {} /data/web_static/current".format(new_folder))
+        return True
+    except:
+        return False
