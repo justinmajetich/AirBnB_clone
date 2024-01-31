e9dc269a40929b733ee062b35cddbe0514825006
ayaarragab <ayarsk4343@gmail.com>
2024-01-31 18:00:20 +0200

    Iam tired2

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,31 +1,5 @@
 #!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployment of web_static
-
-if ! command -v nginx &> /dev/null; then
-    apt-get -y update
-    apt-get -y install nginx
-fi
-
-mkdir -p /data/web_static/{releases/test,shared}
-touch /data/web_static/releases/test/index.html && echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" > /data/web_static/releases/test/index.html
-
-current_link="/data/web_static/current"
-target_folder="/data/web_static/releases/test/"
-
-if [ -L "$current_link" ]; then
-    rm "$current_link"
-fi
-
-ln -fs "$target_folder" "$current_link"
-chown -hR ubuntu:ubuntu /data/
-#!/usr/bin/env bash
-# Bash script that sets up your web servers for the deployment of web_static
 if ! command -v nginx &> /dev/null; then
     apt-get -y update
     apt-get -y  install nginx
