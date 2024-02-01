84787d60e98bd60387b923c7f5f5391af8d97940
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-02-01 23:54:03 +0200

    Adding Task 0 Trial #4

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,7 +6,6 @@ sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
 
 sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
 sudo mkdir -p /data/web_static/shared
 sudo mkdir -p /data/web_static/current
 
@@ -26,6 +25,8 @@ ubuntu@89-web-01:~/$ curl localhost/hbnb_static/index.html
   </body>
 </html>" > /data/web_static/current/index.html
 
-sudo ln -sf /data/web_static/current/ /data/web_static/releases/test/
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
+
+sudo service nginx restart
