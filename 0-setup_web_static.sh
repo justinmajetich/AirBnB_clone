037241caaa6259ffe63adcd4da90672adaabd962
KangabireMuhozaMerveille <kangabirem@gmail.com>
2024-02-01 14:16:14 -0800

    deploy static" && git push

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+## Bash scrip to set up web servers to deploy web_static
+sudo apt-get update
+sudo apt-get install nginx
+sudo mkdir /data/web_static/shared/
+sudo mkdir /data/web_static/releases/test/
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" |sudo tee /data/web_static/releases/test/index.html
+sudo ln -s/data/web_static/releases/test/ /data/web_static/current/
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '53i \\tlocation \/hbnb_static {\n\t\t alias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
+sudo service nginx restart
