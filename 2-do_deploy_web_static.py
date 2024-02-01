d6b3b75b871e45d6fb9036a0eac8fff762550a85
ayaarragab <ayarsk4343@gmail.com>
2024-02-01 22:48:33 +0200

    2/2/2024^4

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -33,25 +33,24 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """
-    deployes web static 1
-    """
-    if os.path.exists(archive_path):
-        file_name = archive_path.split('/')[1]
-        file_path = '/data/web_static/releases/'
-        releases_path = file_path + file_name[:-4]
-        try:
-            put(archive_path, '/tmp/')
-            run(f'mkdir -p {releases_path}')
-            run(f'tar -xzf /tmp/{file_name} -C {releases_path}')
-            run(f'rm -rf /tmp/{file_name}')
-            run('mv {}/web_static/* {}/'.format(releases_path, releases_path))
-            run('rm -rf {}/web_static'.format(releases_path))
-            run('rm -rf /data/web_static/current')
-            run(f'ln -s {releases_path} /data/web_static/current')
-            print('New version deployed!')
-            return True
-        except:
-            return False
-    else:
+    '''
+    Deploy archive to web server
+    '''
+    if not os.path.exists(archive_path):
+        return False
+    file_name = archive_path.split('/')[1]
+    file_path = '/data/web_static/releases/'
+    releases_path = file_path + file_name[:-4]
+    try:
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}'.format(releases_path))
+        run('tar -xzf /tmp/{} -C {}'.format(file_name, releases_path))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {}/web_static/* {}/'.format(releases_path, releases_path))
+        run('rm -rf {}/web_static'.format(releases_path))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(releases_path))
+        print('New version deployed!')
+        return True
+    except:
         return False
