01bcadb0415c89b271b0e70efffe918346c5a91a
ayaarragab <ayarsk4343@gmail.com>
2024-02-01 22:54:50 +0200

    2/2/2024^r

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,9 +11,9 @@ import os
 from datetime import datetime
 
 
-env['hosts'] = ['107.23.93.54', '34.202.233.91']
-env['user'] = 'ubuntu'
-env['key_filename'] = '~/.ssh/school'
+env.hosts = ['107.23.93.54', '34.202.233.91']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/school'
 
 
 def do_pack():
@@ -33,24 +33,25 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    '''
-    Deploy archive to web server
-    '''
-    if not os.path.exists(archive_path):
-        return False
-    file_name = archive_path.split('/')[1]
-    file_path = '/data/web_static/releases/'
-    releases_path = file_path + file_name[:-4]
-    try:
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}'.format(releases_path))
-        run('tar -xzf /tmp/{} -C {}'.format(file_name, releases_path))
-        run('rm /tmp/{}'.format(file_name))
-        run('mv {}/web_static/* {}/'.format(releases_path, releases_path))
-        run('rm -rf {}/web_static'.format(releases_path))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {} /data/web_static/current'.format(releases_path))
-        print('New version deployed!')
-        return True
-    except:
+    """
+    deployes web static 1
+    """
+    if os.path.exists(archive_path):
+        file_name = archive_path.split('/')[1]
+        file_path = '/data/web_static/releases/'
+        releases_path = file_path + file_name[:-4]
+        try:
+            put(archive_path, '/tmp/')
+            run(f'mkdir -p {releases_path}')
+            run(f'tar -xzf /tmp/{file_name} -C {releases_path}')
+            run(f'rm -rf /tmp/{file_name}')
+            run('mv {}/web_static/* {}/'.format(releases_path, releases_path))
+            run('rm -rf {}/web_static'.format(releases_path))
+            run('rm -rf /data/web_static/current')
+            run(f'ln -s {releases_path} /data/web_static/current')
+            print('New version deployed!')
+            return True
+        except:
+            return False
+    else:
         return False
