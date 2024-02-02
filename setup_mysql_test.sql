75f6a34ebfc52e50fb7d7df1a56f3ebbc890ba0e
joshbam-joshwilly <j.alana@alustudent.com>
2024-02-02 14:52:55 +0100

    sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,5 @@
 -- OUR FILE FOR PREPARING THE MYSQL SERVER FOR THE PROJECT --
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_PWD';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test . * TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_scheme . * TO 'hbnb_test'@'localhost';
