c592b263a3b3712dae83494a8dd7322716b910d4
aminoxlhd <aminoxlhd@gmail.com>
2024-02-01 17:31:56 -0800

    add 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,49 +1,18 @@
 #!/usr/bin/env bash
-# Exit script if any command fails
-set -e
+# sets up your web servers for the deployment of web_static
 
-# Function to create directories if they don't exist
-create_directories() {
-    mkdir -p /data/web_static/{releases/test,shared}
-}
-
-# Function to create fake HTML file
-create_fake_html() {
-    echo "<html>
+apt-get update
+apt-get install -y nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" > /data/web_static/releases/test/index.html
-}
-
-# Function to create symbolic link
-create_symbolic_link() {
-    ln -sf /data/web_static/releases/test /data/web_static/current
-}
-
-# Function to change ownership recursively
-change_ownership() {
-    chown -R root:root /data/
-}
-
-# Function to update Nginx configuration
-update_nginx_config() {
-    sed -i '/alias/s/$/\/;/' /etc/nginx/sites-available/default
-    service nginx restart
-}
-
-# Main function
-main() {
-    create_directories
-    create_fake_html
-    create_symbolic_link
-    change_ownership
-    update_nginx_config
-}
-
-# Trap any errors and exit with success status
-trap 'echo "Error encountered. Exiting with success status."; exit 0' ERR
-
-main
+</html>" | tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data
+sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
+sudo service nginx restart
