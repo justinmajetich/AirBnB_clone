11f1077c0aa78b973c4911091d050ecfa2f4cd29
ayaarragab <ayarsk4343@gmail.com>
2024-02-03 18:35:43 +0200

    change

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -2,54 +2,65 @@
 
 exec { 'apt_update':
   command     => 'apt-get -y update',
-  refreshonly => true,
-}
+} ->
 
-exec { 'install_nginx':
-  command => 'sudo apt-get -y install nginx',
-  require => Exec['apt_update'],
-}
-
-exec { 'start_nginx':
-  command => 'sudo service nginx start',
-  require => Exec['install_nginx'],
-}
-
-exec { 'make_dirs':
-  command => 'sudo mkdir -p /data/ /data/web_static/ /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/',
-}
-
-exec { 'echo_hol':
-  command => 'echo "Holberton School for the win!" | sudo tee /data/web_static/releases/test/index.html > /dev/null',
-  require => Exec['make_dirs'],
-}
-
-exec { 'ln':
-  command => 'sudo ln -s /data/web_static/releases/test/ /data/web_static/current',
-  require => Exec['echo_hol'],
-}
-
-exec { 'chown':
-  command => 'sudo chown -R ubuntu:ubuntu /data/',
-  require => Exec['ln'],
-}
+package { 'nginx':
+ ensure => 'present',
+ provider => 'apt',
+} ->
 
 file { '/data/':
-  ensure  => directory,
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+} ->
+
+file { '/data/web_static/':
+  ensure => 'directory',
+  group  => 'ubuntu',
+  owner  => 'ubuntu',
+} ->
+
+file { '/data/web_static/releases/':
+  ensure => 'directory',
+  group  => 'ubuntu',
+  owner  => 'ubuntu',
+} ->
+
+file { '/data/web_static/releases/test/':
+  ensure => 'directory',
+  group  => 'ubuntu',
+  owner  => 'ubuntu',
+} ->
+
+file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  path    => '/data/web_static/releases/test/index.html',
   owner   => 'ubuntu',
   group   => 'ubuntu',
-  recurse => true,
-  require => Exec['chown'],
-}
+  content => "Holberton School Puppet\n",
+} ->
+
+file { '/data/web_static/current':
+  ensure  => 'link',
+  replace => 'yes',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  target  => '/data/web_static/releases/test',
+} ->
+
+exec { 'sudo chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
+} ->
+
 
 $NEW_STRING = "\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
 
 exec { 'sed':
   command => 'sudo sed -i "38i ${NEW_STRING}" /etc/nginx/sites-available/default',
-  require => Exec['make_dirs'], # Ensure directories are created before modifying the file
-}
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+} ->
 
-exec { 'restart_nginx':
-  command => 'sudo service nginx restart',
-  require => Exec['sed'],
+exec { 'nginx restart':
+  path => '/etc/init.d/'
 }
