07af3d1ba6d14b82b24b796d857ef3b20da3b2de
ayaarragab <ayarsk4343@gmail.com>
2024-02-03 17:12:33 +0200

    removed require

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -8,33 +8,27 @@ exec { 'apt_update':
 
 exec { 'install_nginx':
   command => 'sudo apt-get -y install nginx',
-  require => Exec['apt_update'],
 }
 
 exec {'start nginx':
   provider => shell,
   command  => 'sudo service nginx start',
-  before   => Exec['install_nginx'],
 }
 
 exec { 'make_dirs':
   command => 'sudo mkdir -p /data/ /data/web_static/ /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/',
-  require => Exec['start nginx'],
 }
 
 exec { 'echo_hol':
   command => 'echo "Holberton School for the win!" | sudo tee /data/web_static/releases/test/index.html > /dev/null',
-  require => Exec['make_dirs'],
 }
 
 exec { 'ln':
   command => 'sudo ln -s /data/web_static/releases/test/ /data/web_static/current',
-  require => Exec['echo_hol'],
 }
 
 exec { 'chown':
   command => 'sudo chown -R ubuntu:ubuntu /data/',
-  require => Exec['ln'],
 }
 
 file {'/data/':
@@ -50,10 +44,8 @@ $NEW_STRING="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\
 exec { 'sed':
   provider => shell,
   command => 'sudo sed -i "38i $NEW_STRING" /etc/nginx/sites-available/default',
-  require => Exec['chown'],
 }
 
 exec { 'restart':
   command => 'sudo service nginx restart',
-  require => Exec['sed'],
 }
