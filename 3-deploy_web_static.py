86d89fb3c2966f3db1739f2d5cf62f7dd08fca19
aleushea <aleushea@gmail.com>
2024-02-04 13:32:04 -0800

    0x03. AirBnB clone - Deploy static

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+""" Creates and distributes an archive to web servers,
+using created function deploy and pack"""
+from fabric.api import *
+import os
+do_pack = __import__('1-pack_web_static').do_pack
+# do_deploy = __import__('2-do_deploy_web_static').do_deploy
+
+env.hosts = ['18.234.193.94', '18.234.107.246']
+
+
+def deploy():
+    """Pack and deploy all file """
+    file_path = do_pack()
+    if not file_path:
+        return False
+
+    run_cmd = do_deploy(file_path)
+    return run_cmd
+
+
+def do_deploy(archive_path):
+    """Archive distributor"""
+    try:
+        try:
+            if os.path.exists(archive_path):
+                arc_tgz = archive_path.split("/")
+                arg_save = arc_tgz[1]
+                arc_tgz = arc_tgz[1].split('.')
+                arc_tgz = arc_tgz[0]
+
+                """Upload archive to the server"""
+                put(archive_path, '/tmp')
+
+                """Save folder paths in variables"""
+                uncomp_fold = '/data/web_static/releases/{}'.format(arc_tgz)
+                tmp_location = '/tmp/{}'.format(arg_save)
+
+                """Run remote commands on the server"""
+                run('mkdir -p {}'.format(uncomp_fold))
+                run('tar -xvzf {} -C {}'.format(tmp_location, uncomp_fold))
+                run('rm {}'.format(tmp_location))
+                run('mv {}/web_static/* {}'.format(uncomp_fold, uncomp_fold))
+                run('rm -rf {}/web_static'.format(uncomp_fold))
+                run('rm -rf /data/web_static/current')
+                run('ln -sf {} /data/web_static/current'.format(uncomp_fold))
+                run('sudo service nginx restart')
+                return True
+            else:
+                print('File does not exist')
+                return False
+        except Exception as err:
+            print(err)
+            return False
+    except Exception:
+        print('Error')
+        return False
