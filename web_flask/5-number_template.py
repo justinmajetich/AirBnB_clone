53f423eac5259d717f00b52d66287a752edebbe3
Umuhasna <umuhasna123@gmail.com>
2024-02-08 20:55:06 +0300

    Write a script that starts a Flask web application /number_template/<n>: display a HTML page only if n is an integer H1 tag: “Number: n” inside the tag BODY

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+'''
+Module Docs
+'''
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    '''Method Docs'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''Method Docs'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    '''Method Docs'''
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
+    '''Method Docs'''
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    '''Method Docs'''
+    return f"{n:d} is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    '''Method Docs'''
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
