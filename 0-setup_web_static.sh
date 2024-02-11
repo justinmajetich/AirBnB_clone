1fa02a4f9467831ecfddc7e54d6d7b725353bce6
rmustapha <134163158+Rohaenat-Eniola-Mustapha@users.noreply.github.com>
2024-02-11 11:20:37 +0200

    Create 0-setup_web_static.sh

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Set's up web servers for d eployment of web_static
+
+apt-get update
+apt-get -y install nginx
+ufw allow "Nginx HTTP"
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+service nginx restart
