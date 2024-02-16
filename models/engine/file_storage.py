6e398183cacde7b8449cd22425fdb5bc4139c472
am <aminoxlhd@gmail.com>
2024-02-17 00:39:21 +0100

    add file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -62,3 +62,6 @@ class FileStorage:
         if obj:
             key = "{}.{}".format(type(obj).__name__, obj.id)
             self.__objects.pop(key, None)
+
+    def close(self):
+        self.reload()
