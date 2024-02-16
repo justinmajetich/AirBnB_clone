46742472e50e602053c03dfbe1c5018aac9b3ac9
Dodzi Keglo <stephanekeglo@gmail.com>
2024-02-16 18:36:39 +0000

    Add /number_template/<n> route and template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+"""hello_route
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Display hello message"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display():
+    """Display 'HBNB' """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_text(text):
+    """Display input values
+
+    :param value: value in URL
+    :type value: str, int
+    :return: a string
+    :rtype: str
+    """
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False, defaults={'text': 'is_cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def display_text_1(text):
+    """ Display input text
+
+    :param text: text in URL
+    :type text: str
+    :return: a string
+    :rtype: str
+    """
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_number(n):
+    """ Display number
+
+    :param n: number in URL
+    :type n: str
+    :return: a string
+    :rtype: str
+    """
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def display_html(n):
+    """ Display an HTML page
+
+    :param n: number in URL
+    :type n: str
+    :return: an HTML page
+    :rtype: str
+    """
+    return render_template('5-number.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
