8dd55d4a4fd870e6ff5d01917aed65f53c499ed8
aminoxlhd <aminoxlhd@gmail.com>
2024-02-16 21:15:15 +0000

    add 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-""" 
-starts a Flask web application 
+"""
+Script that starts a Flask web application
 """
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(_name_)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-	""" hello_hbnb fonction """
-	return "Hello HBNB!"
+    """Display 'Hello HBNB!'"""
+    return 'Hello HBNB!'
 
 
-if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=5000)
+if _name_ == '_main_':
+    app.run(host='0.0.0.0', port=5000)
