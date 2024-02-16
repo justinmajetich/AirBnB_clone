47f2966e6d5b36243b850f15e0716153306d0dfe
ayaarragab <ayarsk4343@gmail.com>
2024-02-16 22:37:01 +0200

    0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Start a Flask web application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Display Hello HBNB! """
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    host = '0.0.0.0'
+    port = 5000
+    app.run(host=host, port=port)
