c638cb904f82b40f1e41c1dbf1ea225e0f34f2c9
aminoxlhd <aminoxlhd@gmail.com>
2024-02-16 20:31:22 +0000

    add 1-hbnb_route.py

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" 
+starts a Flask web application 
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+	""" hello_hbnb fonction """
+	return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+	""" hbnb fonction """
+	return 'HBNB'
+
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0")
