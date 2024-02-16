513af343cc8f883d44e8ae383e0c9bbaa93e1316
am <aminoxlhd@gmail.com>
2024-02-17 00:06:04 +0100

    add 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,39 +1,34 @@
 #!/usr/bin/python3
-""" 
-starts a Flask web application 
+"""
+Script that starts a Flask web application
 """
 from flask import Flask
-
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-	""" hello_hbnb fonction """
-	return "Hello HBNB!"
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-	""" hbnb fonction """
-	return 'HBNB'
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-	""" c_text fonction """
-	ftesxt = text.replace('_', ' ')
-	return 'C {}'.format(ftesxt)
+    ftesxt = text.replace('_', ' ')
+    return 'C {}'.format(ftesxt)
 
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
-	"""" python_text fonction """
-	ftesxt = text.replace('_', ' ')
-	return 'Python {}'.format(ftesxt)
+    ftesxt = text.replace('_', ' ')
+    return 'Python {}'.format(ftesxt)
 
 
-if __name__ == "__main__":
-	app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
