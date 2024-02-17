fb0d7cda91a0d704a5339914edd2c62cac2878fa
Mrelshimy <mraafat.elsayed@gmail.com>
2024-02-17 18:13:24 +0200

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,12 +13,9 @@ class FileStorage:
             or a certain class objects"""
             if not cls:
                 return self.__objects
-            elif type(cls) == str:
-                return {k: v for k, v in self.__objects.items()
-                        if v.__class__.__name__ == cls}
             else:
-                return {k: v for k, v in self.__objects.items()
-                        if v.__class__ == cls}
+                return {key: value for key, value in self.__objects.items()
+                        if value.__class__ == cls}
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
