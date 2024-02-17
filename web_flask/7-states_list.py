d731aceb38b56a6e5453a092e70cfc22f84ae522
am <aminoxlhd@gmail.com>
2024-02-17 02:19:23 +0100

    add 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,15 +7,15 @@ from flask import render_template
 app = Flask(__name__)
 
 
+@app.teardown_appcontext
+def teardown(exception):
+    storage.close()
+
+
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     states = storage.all("State")
     return render_template("7-states_list.html", states=states)
-
-
-@app.teardown_appcontext
-def teardown(exception):
-    storage.close()
 
 
 if __name__ == "__main__":
