27544df46a1caaf2722a459ef9c9ffb0b1d2592c
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-02-17 09:17:01 +0000

    Python is cool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Start Flask app"""
+
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def index():
+    ''' Index file'''
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hnb():
+    """sub route content"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_fun(text):
+    new_string = text.replace('_', ' ')
+    return "C " + new_string
+
+
+@app.route("/python/")
+@app.route("/python/<text>")
+def py_fun(text='is cool'):
+    new_str = text.replace('_', ' ')
+    return "Python " + new_str
+
+
+if __name__ == "__main__":
+    '''Main function'''
+    app.run(debug=True, host='0.0.0.0')
