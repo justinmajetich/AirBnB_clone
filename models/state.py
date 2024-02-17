3a400df9b163d7667b7fddc54a0d8a1d9f2eca54
am <aminoxlhd@gmail.com>
2024-02-17 01:48:19 +0100

    add state.py

--- models/state.py
+++ models/state.py
@@ -1,14 +1,16 @@
 from models.base_model import BaseModel, Base
-from models import storage
+import models
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+
 
 class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    
+
     if models.storage_t == 'db':
-        cities = relationship("City", backref="state", cascade="all, delete-orphan")
+        cities = relationship("City", backref="state",
+                              cascade="all, delete-orphan")
     else:
         @property
         def cities(self):
