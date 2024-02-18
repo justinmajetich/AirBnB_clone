5b4f0ccaff21aecaf3a94bc2d563333354c9ef75
Eunice-Serani <eaamable91@gmail.com>
2024-02-18 10:59:12 -0800

    Airbnb clone update

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,5 @@
  -- Create a user with all privileges
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION
