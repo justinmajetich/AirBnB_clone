b15ada58c98d2ed7976c44985af90f99768536be
Mrelshimy <mraafat.elsayed@gmail.com>
2024-02-18 17:35:43 +0200

    New branch

--- models/state.py
+++ models/state.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    # name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state', cascade='all, delete')
+
+    @property
+    def cities(self):
+        from models import storage
+        city_dict = storage.all("City")
+        state_cities = []
+        for value in city_dict.values():
+            if value.state_id == self.id:
+                state_cities.append(value)
+        return state_cities
