6142aa5677c62bbfd36af88d88527741f64cdcde
ayaarragab <ayarsk4343@gmail.com>
2024-02-18 15:49:10 +0200

    task 8 success

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,16 +6,19 @@ from models import storage
 
 app = Flask(__name__)
 
+
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """ Display a HTML page """
     states = storage.all("State")
     return render_template('7-states_list.html', states=states)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """ Remove the current SQLAlchemy Session """
     storage.close()
 
-if __name__ == "__main__":  
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
