3e37dccee5cc47ed40495418e3ffdaa5f554fa26
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-02-18 04:58:33 +0000

    Even or Odd

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -48,10 +48,12 @@ def number_tmp(n):
     except Exception:
         abort(404)
 
+
 @app.route("/number_odd_or_even/<n>")
 def even_odd(n):
     if (int(n) % 2) == 0:
-        return render_template("6-number_odd_or_even.html", number=n + " is even")
+        return render_template("6-number_odd_or_even.html", number=n + " is
+                               even")
     return render_template("6-number_odd_or_even.html", number=n + " is odd")
 
 
