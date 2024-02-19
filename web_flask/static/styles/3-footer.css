bff373a8ec7f2b1b89e5b9433e3c4fa6aa7ab97b
lavyatarah <mattlavyy@gmail.com>
2024-02-18 17:41:15 -0800

    initial

---
+++ web_flask/static/styles/3-footer.css
@@ -0,0 +1,11 @@
+footer {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    background-color: white;
+    height: 60px;
+    border-top: 1px solid #CCCCCC;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
