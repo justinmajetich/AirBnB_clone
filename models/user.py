cdabd3633d9bb0a851ef8df35cce0885d4b64267
am <aminoxlhd@gmail.com>
2024-02-19 01:36:37 +0100

    add user.py

--- models/user.py
+++ models/user.py
@@ -14,4 +14,5 @@ class User(BaseModel, Base):
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
 
-    reviews = relationship('Review', backref='user', cascade='all, delete-orphan')
+    reviews = relationship('Review', backref='user',
+                           cascade='all, delete-orphan')
