4a13c9e4d7a5fdda1b46739dde312a1fbbd11272
KangabireMuhozaMerveille <kangabirem@gmail.com>
2024-02-19 03:36:32 -0800

    starting web flask

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""a script that starts a flask web"""
+from flask import Flask, escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """initials the url"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """returns HBNB"""
+    return "HBNB"
+
+@app.route('/c/<text>')
+def text():
+    """texts"""
+    return 'C ' + escape(text).replace('_', ' ')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
