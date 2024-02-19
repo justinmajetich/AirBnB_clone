3a968baf54fb730a86c37c420e82d827426ae75d
Maddie <115709272+Maddily@users.noreply.github.com>
2024-02-19 08:37:44 +0200

    Relate reviews to places and add a reviews getter

--- models/place.py
+++ models/place.py
@@ -1,18 +1,44 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
+"""Place Module for HBNB project"""
 from models.base_model import BaseModel
+from sqlalchemy.orm import relationship
+from models import type_of_storage, storage
+from models.review import Review
 
 
 class Place(BaseModel):
-    """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+    """A place to stay"""
+
+    if type_of_storage == 'db':
+        reviews = relationship(
+            'Review',
+            cascade='all, delete-orphan',
+            backref='place'
+            )
+    else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+
+        @property
+        def reviews(self):
+            """
+            Returns the list of Review instances with place_id
+            equal to the current Place.id. It's the FileStorage
+            relationship between Place and Review.
+
+            """
+            all_reviews = storage.all(Review)
+            reviews = []
+            for review in all_reviews.values():
+                if review.place_id == self.id:
+                    reviews.append(review)
+            return reviews
