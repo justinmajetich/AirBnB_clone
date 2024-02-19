35bd9b3f2e450ae1b949df3cf1ca66048eebf6b6
SYANDA MNTAMBO's GITHUB <syandamnt@gmail.com>
2024-02-19 14:51:00 +0200

    7 and 8
    made some updates to 7 and 8

--- models/city.py
+++ models/city.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+import models
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    
+    __tablename__ = 'cities'
+
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+
+    place = relationship("Place", backref="cities", cascade="all, delete-orphan")
--- models/user.py
+++ models/user.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+
+import models
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 
 class User(BaseModel):
