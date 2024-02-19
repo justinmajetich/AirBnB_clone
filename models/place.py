18541047cbba30a5b2f68dcedfeb0548f54d0cc8
Your Name <sibongile.nhlema@gmail.com>
2024-02-19 01:59:51 -0800

    updated the amenities and city class

--- models/place.py
+++ models/place.py
@@ -4,16 +4,35 @@ from models.base_model import BaseModel
 from sqlalchemy.orm import relationship
 from models import type_of_storage
 from models.review import Review
+from models.amenities import Amenities
 
 
 class Place(BaseModel):
     """A place to stay"""
 
     if type_of_storage == 'db':
+        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=False)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float, nullable=False)
+        longitude = Column(Float, nullable=False)
+        amenity_ids = []
+
         reviews = relationship(
             'Review',
             cascade='all, delete-orphan',
             backref='place'
+            )
+        amenities = relationship(
+                'Amenity',
+                secondary=place_amenity,
+                viewonly=False,
+                back_populates="place_amenities"
             )
     else:
         city_id = ""
@@ -45,3 +64,14 @@ class Place(BaseModel):
                 if review.place_id == self.id:
                     reviews.append(review)
             return reviews
+
+        @property
+        def amenities(self):
+            ''' returns a list of the amenities '''
+            return self.amenity_ids
+
+        @amenities.setter
+        def amenities(self, obj=None):
+            '''Adds amenity ids to attr '''
+            if type(obj) is Amenity and obj.id not in self.amenity_ids:
+                self.amenity_ids.append(obj.id)
