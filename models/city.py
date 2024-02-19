3aea030db14731b35eca4e938044ea352feb52df
am <aminoxlhd@gmail.com>
2024-02-19 01:21:01 +0100

    edit city.py

--- models/city.py
+++ models/city.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 
+
 class City(BaseModel, Base):
     __tablename__ = 'cities'
-     __tablename__ = "cities"
-    places = relationship("Place", backref="city", cascade="all, delete")
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="cities")
