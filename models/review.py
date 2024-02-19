9342b9c2f3970dc6c9d9777dfba10ab3063fa603
Maddie <115709272+Maddily@users.noreply.github.com>
2024-02-19 09:40:05 +0200

    Move __tablename__ out of if statement

--- models/review.py
+++ models/review.py
@@ -8,8 +8,9 @@ from models import type_of_storage
 class Review(BaseModel, Base):
     """Review class to store review information"""
 
+    __tablename__ = 'reviews'
+
     if type_of_storage == 'db':
-        __tablename__ = 'reviews'
         text = Column(String(1024), nullable=False)
         place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
