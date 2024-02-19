05d8049d575f842a26bee9f3f3d3886ac7dee06b
Maddie <115709272+Maddily@users.noreply.github.com>
2024-02-19 11:21:55 +0200

    Fix indentation

--- models/state.py
+++ models/state.py
@@ -14,8 +14,10 @@ class State(BaseModel, Base):
 
     if type_of_storage == 'db':
         name = Column(String(128), nullable=False)
-        cities = relationship("City", cascade='all, delete-orphan',
-                          backref="state")
+        cities = relationship(
+            "City",
+            cascade='all, delete-orphan',
+            backref="state")
     else:
         name = ''
 
