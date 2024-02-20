023da973876984afc5f5b525832b98f171d3d417
Solomon <kyobenimaru001@gmail.com>
2024-02-20 22:50:20 +0300

    commit for the integration with sqlalchemy

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,33 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+import models
 import uuid
 from datetime import datetime
 
 
+Base = declarative_base()
+
 class BaseModel:
     """A base class for all hbnb models"""
+    
+    """_summary_
+        1.Have id Represent a column containing a Unique string (60 chars)
+            Can't be null
+            Primary key
+        2.Create_at column containing Datetime
+            can't be null
+            default value of time use (datetime.utcnow())
+        3.Updated_at column conatining datetime
+            can't be null
+            default value is current time (datetime.utcnow())
+    """
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    
+    
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
 
@@ -15,22 +37,19 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
         else:
-            if 'updated_at' in kwargs:
-                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                         '%Y-%m-%dT%H:%M:%S.%f')
-            else:
-                kwargs['updated_at'] = datetime.now()
-
-            if 'created_at' in kwargs:
-                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                         '%Y-%m-%dT%H:%M:%S.%f')
-            else:
-                kwargs['created_at'] = datetime.now()
-
-            kwargs.pop('__class__', None)
-            self.__dict__.update(kwargs)
+            """_summary_
+            Updated to handle creation of custom attributes
+            """
+            if 'id' not in kwargs:
+                self.id =str(uuid.uuid4())
+                self.created_at = datetime.utcnow()
+                self.updated_at = datetime.utcnow()
+            for k, v in kwargs.items():
+                if k != '__class__':
+                    # Check if the Key exits, if not create custom key
+                    setattr(self,k , v)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -44,11 +63,14 @@ class BaseModel:
         storage.save()
 
     def to_dict(self):
-        """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+        # """Convert instance into dict format"""
+        # dictionary = {}
+        # dictionary.update(self.__dict__)
+        # dictionary.update({'__class__':
+        #                   (str(type(self)).split('.')[-1]).split('\'')[0]})
+        # dictionary['created_at'] = self.created_at.isoformat()
+        # dictionary['updated_at'] = self.updated_at.isoformat()
+        # return dictionary
+        
+        d = dict(self.__dict__)
+        d['__class__'] = self.__class__.__name__
--- models/state.py
+++ models/state.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column,String
+from sqlalchemy.orm import relationship
+import os
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref="state",cascade="all, delete")
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.name = kwargs.get('name', "")
+        
