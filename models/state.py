d4b76fc06775b9eba39f6d4cb786a328162199ea
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2024-02-20 17:53:13 +0200

    state class, just got improved - to DB storage

--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    
+    __tablename__ = "states"
+
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete-orphan")
+
+    @property
+    def cities(self):
+        """Getter attribute in case of file storage"""
+        from models import storage
+        from models.city import City
+        city_list = []
+        for city in storage.all(City).values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
+
