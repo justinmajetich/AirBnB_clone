86c5ca46e3017a0f0f14d5c0aa6c029830b2f5ec
Solomon <kyobenimaru001@gmail.com>
2024-02-20 09:21:37 +0300

    commit for the main_delete module

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    # def all(self):
+    #     """Returns a dictionary of models currently in storage"""
+    #     return FileStorage.__objects
+    
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return self.__objects
+        else:
+            return {k: v for k, v in self.__objects.items() if isinstance(v, cls)}
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -24,6 +31,12 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+            
+    def delete(self, obj=None):
+        """Deletes obj from __objects if it exists"""
+        if obj is not None:
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            self.__objects.pop(key, None)
 
     def reload(self):
         """Loads storage dictionary from file"""
