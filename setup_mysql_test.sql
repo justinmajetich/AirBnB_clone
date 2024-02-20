dc169a86a9e3f6380952be0226daf0c790c95a37
Babatunde Adeoti <babaolu98@gmail.com>
2024-02-20 11:00:23 +0100

    SQL setup corrections

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,5 @@
 -- Create a user with all privileges
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION
