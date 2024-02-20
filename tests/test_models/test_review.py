6cfdfe052c865707d097f7642e4bbfbb89df6438
Maddie <115709272+Maddily@users.noreply.github.com>
2024-02-20 13:51:04 +0200

    Add tests

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,29 +1,90 @@
 #!/usr/bin/python3
 """ """
-from tests.test_models.test_base_model import test_basemodel
+from models.base_model import BaseModel
 from models.review import Review
+import unittest
+import os
+import pep8
 
 
-class test_review(test_basemodel):
-    """ """
+class TestReview(unittest.TestCase):
+    """Tests the Review class"""
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "Review"
-        self.value = Review
+    @classmethod
+    def setUpClass(cls):
+        """Creates a review instance"""
 
-    def test_place_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.place_id), str)
+        cls.review = Review()
+        cls.review.text = 'It was a wonderful stay.'
+        cls.review.place_id = "8a6s54d3fg"
+        cls.review.user_id = "84s6a5d1f2"
 
-    def test_user_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.user_id), str)
+    @classmethod
+    def tearDownClass(cls):
+        """Delete the review instance"""
 
-    def test_text(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.text), str)
+        del cls.review
+
+    def tearDown(self):
+        """Removes file.json"""
+
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
+
+    def test_attribute_existence(self):
+        """Tests for attribute existence in Review class"""
+
+        self.assertTrue('id' in self.review.__dict__)
+        self.assertTrue('created_at' in self.review.__dict__)
+        self.assertTrue('updated_at' in self.review.__dict__)
+        self.assertTrue('text' in self.review.__dict__)
+        self.assertTrue('place_id' in self.review.__dict__)
+        self.assertTrue('user_id' in self.review.__dict__)
+
+    def test_attribute_type(self):
+        """Tests type of attribute"""
+
+        self.assertEqual(type(self.review.text), str)
+        self.assertEqual(type(self.review.place_id), str)
+        self.assertEqual(type(self.review.user_id), str)
+
+    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") == 'db',
+                     'Database storage is being used')
+    def test_save(self):
+        """Tests save method"""
+
+        self.review.save()
+        self.assertNotEqual(self.review.created_at, self.review.updated_at)
+
+    def test_to_dict(self):
+        """tests to_dict method"""
+
+        dict = self.review.to_dict()
+
+        self.assertIsInstance(dict['created_at'], str)
+        self.assertIsInstance(dict['updated_at'], str)
+        self.assertEqual('to_dict' in dir(self.review), True)
+
+    def test_issubclass(self):
+        """Tests if Review is subclass of Basemodel"""
+
+        self.assertTrue(issubclass(self.review.__class__, BaseModel), True)
+
+    def test_documentation(self):
+        """Tests if the class is documented"""
+
+        self.assertIsNotNone(Review.__doc__)
+
+    def test_following_pep8(self):
+        """Tests if the code follows pep8 style guide"""
+
+        pep8_style = pep8.StyleGuide(quiet=True)
+        result = pep8_style.check_files(['models/review.py'])
+
+        self.assertEqual(result.total_errors, 0, 'Found style errors.')
+
+
+if __name__ == "__main__":
+    unittest.main()
