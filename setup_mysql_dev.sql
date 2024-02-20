d4d69ed9aa2b2681eeb9ab8c545524a11229b0ad
Babatunde Adeoti <babaolu98@gmail.com>
2024-02-20 11:10:10 +0100

    Fixing database mix-up

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,6 @@
- -- Create a user with all privileges
+ -- Create a dev user with all privileges
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost'
+WITH GRANT OPTION;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,7 @@
--- Create a user with all privileges
+-- Create a test user with all privileges
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
+IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost' WITH GRANT OPTION;
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost'
+WITH GRANT OPTION;
