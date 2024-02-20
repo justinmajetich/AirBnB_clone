e96b169ab5fb61b730d1e2fc7a92df4a5725c74e
Babatunde Adeoti <babaolu98@gmail.com>
2024-02-20 12:19:23 +0100

    Fixing database mix-up

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,7 @@
  -- Create a dev user with all privileges
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
+IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost'
 WITH GRANT OPTION;
