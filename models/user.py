46866d2746338d817ec7c29c35f7cf60ae6f14a5
Lamia Hamdi <101535283+Lamia1406@users.noreply.github.com>
2024-02-20 13:59:01 +0100

    update user.py and place.py using sqlalchemy

--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
