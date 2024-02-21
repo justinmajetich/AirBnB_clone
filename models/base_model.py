56684b4356d17d6f8cc64d446b818744052e6054
Destiny-Kay <destinykevogo@gmail.com>
2024-02-21 10:16:20 +0300

    Add: create database models

--- models/base_model.py
+++ models/base_model.py
@@ -3,19 +3,29 @@
 import uuid
 from datetime import datetime
 
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import create_engine, Column, String, Integer, VARCHAR, DateTime
+
+
+Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
+
+    id = Column(VARCHAR(60), default=uuid.uuid4(), primary_key=True, nullable=False)
+    created_at = Column(datetime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-
         self.id = str(uuid.uuid4())
+
         if not kwargs:
-            from models import storage
+            # from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            # storage.new(self)
         else:
             if 'updated_at' in kwargs:
                 kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
@@ -41,14 +51,22 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
+        if dictionary['_sa_instance_state']:
+            del dictionary['_sa_instance_state']
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
+    
+    def delete(self):
+        """Deletes a  model instance from the storage"""
+        from models import storage
+        storage.delete(self)
