e8f148858c51f1032350e698b2c8c397a6623974
Destiny-Kay <destinykevogo@gmail.com>
2024-02-21 10:24:28 +0300

    Add: switch between db and file storage

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
+import os
 
+storage_type = os.environ.get("HBNB_TYPE_STORAGE")
 
-storage = FileStorage()
-storage.reload()
+if storage_type == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
+
