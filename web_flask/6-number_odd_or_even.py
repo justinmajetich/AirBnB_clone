7b6572ec78ca29a3cfceb888c8ce74128e1178a9
KangabireMuhozaMerveille <kangabirem@gmail.com>
2024-02-21 00:27:54 -0800

    flask

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,53 +1,45 @@
-#!/usr/bin/python3
-"""a script that starts a flask web"""
 from flask import Flask, render_template
-from markupsafe import escape
 
 app = Flask(__name__)
 
-
 @app.route('/', strict_slashes=False)
-def index():
-    """initials the url"""
-    return "Hello HBNB!"
-
+def hello_hbnb():
+    return 'Hello HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
-    return "HBNB"
-
+    return 'HBNB'
 
 @app.route('/c/<text>', strict_slashes=False)
-def text(text):
-    """texts"""
-    return 'C ' + escape(text).replace('_', ' ')
-
+def c_text(text):
+    return 'C ' + text.replace('_', ' ')
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python(text):
-    """text"""
-    return 'Python ' + escape(text).replace('_', ' ')
-
+def python_text(text):
+    return 'Python ' + text.replace('_', ' ')
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    """number"""
-    return f'{n} is a number'
-
+    if isinstance(n, int):
+        return f'{n} is a number'
+    else:
+        return 'Not Found', 404
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def template(n):
-    """template"""
-    return render_template("5-number.html", number=n)
-
-
-@app.route('/number_template/<n>', strict_slashes=False)
 def number_template(n):
-    """number template"""
-    number_type = "odd" if n % 2 != 0 else "even"
-    return render_template("6-number_odd_or_even.html", n=n, number_type=number_type)
+    if isinstance(n, int):
+        return render_template('6-number_template.html', number=n)
+    else:
+        return 'Not Found', 404
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    if isinstance(n, int):
+        number_type = "odd" if n % 2 != 0 else "even"
+        return render_template('6-number_odd_or_even.html', n=n, number_type=number_type)
+    else:
+        return 'Not Found', 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
