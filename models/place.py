7291aaab882e9e00fa0f4f7f0ab5fd60aa4ff310
Merch7x <aquamaingi@gmail.com>
2024-02-21 03:24:24 +0300

    Add Base ref

--- models/place.py
+++ models/place.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, String, ForeignKey, Integer, Float
 from sqlalchemy.orm import relationship
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
