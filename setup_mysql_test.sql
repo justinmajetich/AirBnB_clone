2bfc0772b540dce6c15c15ea5c501b6795e0cbe2
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-02-22 19:35:42 +0000

    fix user creation

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,13 +3,8 @@
 -- Check if database hbnb_db exists
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
--- Check if user hbnb_dev exists
-SET @user_exists = (SELECT COUNT(*) FROM mysql.user WHERE user = 'hbnb_test');
-
 -- If user does not exist, create it
-IF @user_exists = 0 THEN
-    CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-END IF;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 -- Grant all privileges on hbnb_dev_db to hbnb_dev
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
