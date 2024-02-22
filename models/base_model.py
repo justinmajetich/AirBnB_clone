4868004e7476a8228e9ca83dc61d04f9b88dd346
Ace <AceQuantumBit@gmail.com>
2024-02-22 16:14:31 -0600

    short push to get to wsl

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+#ace do the thing
 from sqlalchemy import Column, String, DateTime, func
 from sqlalchemy.ext.declarative import declarative_base
 
