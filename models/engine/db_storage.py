424bcd455e37c06f1c9c2c3d1d0868f639c3ae76
Cody <codywalenciak@gmail.com>
2024-02-22 16:11:08 -0600

    Initial db_storage connection details

---
+++ models/engine/db_storage.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+Module containing HBnB Console's Database engine
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from os import environ as env
+
+mydb = "mysql+mysqldb"
+usr = env["HBNB_MYSQL_USER"]
+pwd = env["HBNB_MYSQL_PWD"]
+host = env["HBNB_MYSQL_HOST"]
+db = env["HBNB_MYSQL_DB"]
+environment = env["HBNB_ENV"]
+connect = '{0}://{1}:{2}@{3}:3306/{4}'.format(mydb, usr, pwd, host, db)
+
+class DBStorage:
+    """
+    Engine for the console's database connection
+    Attributes:
+        __engine: the DB connection engine
+        __session: the current DB session
+    """
+
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        engine = create_engine(connect, pool_pre_ping=True)
+        Session = sessionmaker(bind=engine)
+        
