becb92fb46b233bcd35fbda3a6fb9fc41baf8f86
KangabireMuhozaMerveille <kangabirem@gmail.com>
2024-02-22 00:21:27 -0800

    clone

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,6 +25,10 @@ class FileStorage:
                 temp[key] = val.to_dict()
             json.dump(temp, f)
 
+    def close(self):
+        """closes"""
+        self.reload()
+
     def reload(self):
         """Loads storage dictionary from file"""
         from models.base_model import BaseModel
