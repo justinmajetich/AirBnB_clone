0a8f65a5273aaeffcd727c5269eec90d1c03936f
Cody <codywalenciak@gmail.com>
2024-02-22 11:35:20 -0600

    Add documentation to mysql setup script

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,5 @@
+-- SQL DB creation script - creates the hbnb_dev_db database, and hbnb_dev, the superuser of that DB.
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.*  TO 'hbnb_dev'@'localhost';
