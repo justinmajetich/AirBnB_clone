54b67fe80f6ddec784d413090c6a4c7dc5eb9148
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-02-22 21:06:52 +0000

    update city, state

--- models/state.py
+++ models/state.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, DateTime, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column('name', String(128), nullable=False)
+    cities = relationship('City', back_populates='state', cascade='all, delete-orphan')
