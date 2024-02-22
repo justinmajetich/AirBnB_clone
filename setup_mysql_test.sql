06a0fb51429d0450bff3832d1c2f670931740d80
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-02-22 19:03:21 +0000

    documentation in mysql_test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,18 @@
+-- Prepare MySQL Server
+
+-- Check if database hbnb_db exists
+
+
+-- Check if user hbnb_dev exists
+
+
+-- If user does not exist, create it
+
+
+-- Grant all privileges on hbnb_dev_db to hbnb_dev
+
+
+-- Grant SELECT privilege on performance_schema to hbnb_dev
+
+-- Apply changes
+FLUSH PRIVILEGES;
