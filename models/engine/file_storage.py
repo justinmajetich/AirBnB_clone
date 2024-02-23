03b9600e14df6db43cfb8f2b11c442cb10539491
meddizzle316 <meddizzle316@gmail.com>
2024-02-23 13:45:05 -0600

    add all method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,17 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            print("there is no class argument")
+            return FileStorage.__objects
+        class_dict = {}
+        for key, value in FileStorage.__objects.items():
+            if key.split('.')[0] == cls:
+                class_dict.update({key:value})
+        if bool(class_dict):
+            return class_dict
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
