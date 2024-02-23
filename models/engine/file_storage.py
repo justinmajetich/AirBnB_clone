8aeb08454394b2ebf79809b60f8082c18cf6abe1
lukeschula <schulaluke@gmail.com>
2024-02-23 19:38:06 +0000

    added *delete* prototype in file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,6 +25,9 @@ class FileStorage:
                 temp[key] = val.to_dict()
             json.dump(temp, f)
 
+    def delete(self, obj=None):
+
+
     def reload(self):
         """Loads storage dictionary from file"""
         from models.base_model import BaseModel
