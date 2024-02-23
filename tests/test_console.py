c5e3007c617c6e6a944094650ddae229b7553ae3
NolbsAtHolbs <hikikomode@gmail.com>
2024-02-23 12:21:45 -0600

    made basic tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,2 +1,23 @@
 #!/usr/bin/python3
 """Module that tests the console"""
+import unittest
+from models.base_model import BaseModel
+from models import storage
+import os
+
+class test_console(unittest.TestCase):
+    """Class to test the console"""
+    def setUp(self):
+        """Set up test environment"""
+        pass
+
+    def tearDown(self):
+        try:
+            os.remove('file.json')
+        except:
+            pass
+
+    def test_default(self):
+        """Default test"""
+        i = self.value()
+        self.assertEqual(type(i), self.value)
