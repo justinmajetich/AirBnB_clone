3e92d9a09c4b2e019756ddab9addc8296126eac5
Cody Walenciak <codywalenciak@gmail.com>
2024-02-23 21:00:19 +0000

    Add cities backref

--- models/state.py
+++ models/state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 
 
@@ -8,3 +9,5 @@ class State(BaseModel):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
+
+    cities = relationship("City", backref="state", cascade="all, delete-orphan")
