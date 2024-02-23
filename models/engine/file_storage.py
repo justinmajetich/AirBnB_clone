6eb7a993fed16f58e209cec965b0d546610470e9
lukeschula <schulaluke@gmail.com>
2024-02-23 20:01:04 +0000

    created specific parameters if class is not none, returning dict matching cls. if not, cls is none, returning FS

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,13 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        dicts = {}
+        if cls:
+            for key, value in FileStorage.__objects.items():
+                if value.__class == cls:
+                    dicts[key] = value
+                return dicts
+        if FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -24,9 +30,6 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
-
-    def delete(self, obj=None):
-
 
     def reload(self):
         """Loads storage dictionary from file"""
@@ -51,3 +54,5 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+def delete(self, obj=None):
