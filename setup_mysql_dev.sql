3ceb567bbaa4de898276f8fe4ce2db91e9d4ed1d
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-02-23 13:24:11 -0600

    Wrong DB Name

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,5 @@
 --Module for  creating database and users
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'hbnb_dev'@'hbnb_test_db';
+GRANT ALL PRIVILEGES ON *.* TO 'hbnb_dev'@'hbnb_dev_db';
 GRANT SELECT ON *.* TO 'hbnb_dev'@'performance_schema';
