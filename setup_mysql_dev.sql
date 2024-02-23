c9d90c12f9bb9055240e5e50c4ed11f618f576d0
Babatunde Adeoti <babaolu98@gmail.com>
2024-02-23 05:56:36 +0100

    Just a space

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
- -- Create a dev user with all privileges
+-- Create a dev user with all privileges
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
 IDENTIFIED BY 'hbnb_dev_pwd';
