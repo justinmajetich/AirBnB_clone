f4828341d4ccf1e783a9ac6fef976c15109bd803
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-02-23 13:51:39 -0600

    Forogt the space after --

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
---Module for  creating database and users
+-- Module for creating database and users
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
