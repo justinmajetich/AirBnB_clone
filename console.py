6f3640226e2c09e8c40ec9912732ece8899cec0b
Elijah Poole <elijahpoole@Elijahs-MacBook-Pro.local>
2024-02-23 12:41:30 -0600

    fixing more bugs

--- console.py
+++ console.py
@@ -295,7 +295,7 @@ class HBNBCommand(cmd.Cmd):
             if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
