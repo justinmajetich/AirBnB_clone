8e01358994d9bc1fcacd2d4abf2e00d6e13b724b
meddizzle316 <meddizzle316@gmail.com>
2024-02-23 14:42:33 -0600

    adding user

--- models/user.py
+++ models/user.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
+from models.base_model import Base
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-Base = declarative_base()
+
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
