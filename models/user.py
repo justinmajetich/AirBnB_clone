08df98054533d3ae20440835750c8b3267f2a5f6
iamseaniam <7222@holbertonstudents.com>
2024-02-23 13:35:36 -0600

    User model with SQLAlchemy integration

--- models/user.py
+++ models/user.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, String
 
-
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """
+    This class inherits from BaseModel and SQLAlchemy's declarative base,
+    providing a structure for storing user data in a database.
+    """
+    # Define the name of the table in the database
+    __tablename__ = 'users'
+    # Define the email column, cannot be null
+    email = Column(String(128), nullable=False)
+    # Define the password column, cannot be null
+    password = Column(String(128), nullable=False)
+    # Define the first_name column, and can be null
+    first_name = Column(String(128), nullable=True)
+    # Define the last_name column, and can be null
+    last_name = Column(String(128), nullable=True)
