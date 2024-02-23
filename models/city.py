5680b5c50cd8292d8eaf2f9e6baa8f656bc2ad5d
iamseaniam <7222@holbertonstudents.com>
2024-02-23 14:36:45 -0600

    question 8 done. added relationship with places from user.py and city.py

--- models/city.py
+++ models/city.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
     state_id = ""
     name = ""
+    # Define the relationship with Place objects
+    places = relationship("Place", backref="cities", cascade="all, delete")
