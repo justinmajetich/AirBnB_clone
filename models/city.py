9606ed72c9a0b42b406390ff68a409d6c3ecdc0e
Cody Walenciak <codywalenciak@gmail.com>
2024-02-23 20:59:50 +0000

    Add foreignkey import

--- models/city.py
+++ models/city.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 
@@ -8,5 +8,5 @@ from models.base_model import BaseModel, Base
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    state_id = Column(String(60), nullable=False, ForeignKey('states.id'))
+    state_id = Column(String(60), ForeignKey('states.id'))
     name = Column(String(128), nullable=False)
