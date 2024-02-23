95825a6c5edd32c404a4bddac5b5bd278f90d513
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-02-23 13:53:15 -0600

    Typo on db

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,5 @@
 -- Module for creating database and users
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEDGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-GRANT ALL PRIVILEDGES ON hbnb_dev.* TO 'hbnb_dev'@'localhost';
