293416328786fd6f274fc6cee55cd1c8b7c06e7c
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-02-24 01:00:29 +0000

    typos, move imports to module level

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from models import storage
 #ace do the thing
 from sqlalchemy import Column, String, DateTime, func
 from sqlalchemy.ext.declarative import declarative_base
@@ -13,12 +14,11 @@ class BaseModel:
 
     id = Column('id', String(60), primary_key=True)
     created_at = Column('created_at', DateTime, nullable=False, default=datetime.utcnow())
-    updated_at = Column('cupdated_at', DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column('updated_at', DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
@@ -37,7 +37,6 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.now()
         storage.new(self)
         storage.save()
@@ -56,5 +55,4 @@ class BaseModel:
 
     def delete(self):
         """Deletes current instance from storage"""
-        from models import storage
         storage.delete(self)
