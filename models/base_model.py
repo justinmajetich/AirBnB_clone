18c76a86a1f64ba809380885334355dafce7f593
lukeschula <schulaluke@gmail.com>
2024-02-24 23:15:11 +0000

    updated module, added class id attribute

--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,15 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+import models
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), primary_key=True, unique=True)
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
