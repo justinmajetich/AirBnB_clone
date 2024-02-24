02d1cdfbc1ed48aa0d030d6e6c216cb257d5cab6
PimehT <tareerepimeh@gmail.com>
2024-02-24 18:27:04 +0100

    mysql test setup

--- console.py
+++ console.py
@@ -22,7 +22,6 @@ hbnb_mysql_db = env.get('HBNB_MYSQL_DB', 'hbnb_test_db')
 hbnb_type_storage = env.get('HBNB_TYPE_STORAGE', 'db')
 
 
-
 class HBNBCommand(cmd.Cmd):
     """ Contains the functionality for the HBNB console"""
 
@@ -84,7 +83,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] is '{' and pline[-1] is '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -212,7 +211,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del(storage.all()[key])
+            del (storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
@@ -344,6 +343,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
