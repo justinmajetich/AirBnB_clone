1c376903e47c9e29807e3edadfc026b1c1affa7d
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-02-24 11:24:34 -0600

    Fixed typo in sqlalchemy import in base model and user

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
-import uuid
 from datetime import datetime
-from sqlalchmey import Column, String, DateTime
+from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base
 
