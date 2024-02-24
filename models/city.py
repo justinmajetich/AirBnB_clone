da94bcfc68dacf56910319823fa96124331d5d7e
jessasesh <jessica.dison@yahoo.com>
2024-02-24 16:22:51 +0000

    Fix for console

--- models/city.py
+++ models/city.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
