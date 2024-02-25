adc65139ea127b5a96d09472698869c95855a23f
jessasesh <jessica.dison@yahoo.com>
2024-02-25 21:38:15 +0000

    Reconfigured def all

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,6 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 import os
+from os import getenv
+from models.base_model import Base
+from models.user import User
+from models.city import City
+from models.state import State
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
 
 
 class DBStorage:
@@ -10,35 +18,41 @@ class DBStorage:
 
     def __init__(self):
         """Initialization of DBStorage"""
-        user = os.environ.get('HBNB_MYSQL_USER')
-        pwd = os.environ.get('HBNB_MYSQL_PWD')
-        host = os.environ.get('HBNB_MYSQL_HOST', 'localhost')
-        db = os.environ.get('HBNB_MYSQL_DB', 'hbnb_dev_db')
-        env = os.environ.get('HBNB_ENV')
+        user = os.getenv('HBNB_MYSQL_USER')
+        pwd = os.getenv('HBNB_MYSQL_PWD')
+        host = os.getenv('HBNB_MYSQL_HOST', default='localhost')
+        db = os.getenv('HBNB_MYSQL_DB', 'hbnb_dev_db')
+        env = os.getenv('HBNB_ENV')
 
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                      .format(user, pwd, host, db),
-                                      pool_pre_ping=True)
+        self.__engine = create_engine(
+            'mysql+mysqldb://{}:{}@{}/{}'.format(
+                user, pwd, host, db), pool_pre_ping=True)
+
         if env == 'test':
             Base.metadata.drop_all(self.__engine)
+            
+        session_factory = sessionmaker(
+            bind=self.__engine, expire_on_commit=False)
+        self.__session = scoped_session(session_factory)
 
     def all(self, cls=None):
-        """Returns a dictionary of models currently in storage"""
-        from models.base_model import BaseModel
-        from models.state import State
-        from models.city import City
+        """Query on the current database session all objects"""
+        from models import classes
+
+        objects = {}
         if cls:
-            objects = self.__session.query(cls).all()
+            request = self.__session.query(classes[cls])
+            for obj in request:
+                key = '{}.{}'.format(type(obj).__name__, obj.id)
+                objects[key] = obj
         else:
-            objects = (
-                self.__session.query(State).all() +
-                self.__session.query(City).all() +
-                self.__session.query(BaseModel).all()
-            )
-        return {
-            "{}.{}".format(type(obj).__name__, obj.id): obj
-            for obj in objects
-        }
+            for cls in classes.values():
+                request = self.__session.query(cls)
+                for obj in request:
+                    key = '{}.{}'.format(type(obj).__name__, obj.id)
+                    objects[key] = obj
+        return objects
+
 
     def new(self, obj):
         """Adds new object to storage"""
@@ -54,12 +68,8 @@ class DBStorage:
             self.__session.delete(obj)
 
     def reload(self):
-        """Creates all tables and current database session"""
-        from models.base_model import BaseModel
-        from models.state import State
-        from models.city import City
+        """Create all tables in the database and create the current database session"""
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine,
-                                       expire_on_commit=False)
-        Session = scoped_session(session_factory)
-        self.__session = Session()
+        session_factory = sessionmaker(
+            bind=self.__engine, expire_on_commit=False)
+        self.__session = scoped_session(session_factory)
