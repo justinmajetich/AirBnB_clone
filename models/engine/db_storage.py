b1e23fbf677642d89649f239b2cfe9ddb0815eb9
jessasesh <jessica.dison@yahoo.com>
2024-02-25 21:57:10 +0000

    Maybe a fix??

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 import os
 from os import getenv
-from models.base_model import Base
+from models.base_model import BaseModel, Base
 from models.user import User
 from models.city import City
 from models.state import State
--- models/place.py
+++ models/place.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 
 
 class Place(BaseModel):
