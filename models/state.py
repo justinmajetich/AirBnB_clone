16a8d4060361ac72b0cf9617f4364cfe5304dd62
VengeanceIsHis <elijahjpoole@gmail.com>
2024-02-25 10:39:49 -0600

    now has official init

--- models/state.py
+++ models/state.py
@@ -6,7 +6,7 @@ import sys
 class State(BaseModel):
     """ State class """
     name = ""
-    def __init(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         if 'id' not in kwargs:
             self.id = str(uuid4())
--- models/state.py~
+++ models/state.py~
@@ -7,6 +7,6 @@ class State(BaseModel):
     """ State class """
     name = ""
     def __init(self, *args, **kwargs):
-        super().__init__(*args, **kwargs):
+        super().__init__(*args, **kwargs)
         if 'id' not in kwargs:
             self.id = str(uuid4())
