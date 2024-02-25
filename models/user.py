16d12898f68014e94d1acec02414a243aa6eada7
AhmadNawabi <ahmadshamoon0505@gmail.com>
2024-02-25 13:08:45 -0500

    delete a extra import from user.py file

--- models/user.py
+++ models/user.py
@@ -2,10 +2,6 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
-
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
