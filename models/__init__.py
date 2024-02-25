f6459bb2195baaf45327311464afa2d19a816038
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-02-25 23:40:57 +0000

    init change

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
+from .engine.file_storage import FileStorage
+from .engine.db_storage import DBStorage
 import os
 
 storage_type = os.getenv('HBNB_TYPE_STORAGE', 'file')
