7fe0aec12f20ab4a674ef79ef12c005547d160a5
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-02-25 14:16:41 +0000

    reverted changes for place and amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
-from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
-from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
+""" State Module for HBNB project """
+from models.base_model import BaseModel
 
-place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
-                      Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
+
+class Amenity(BaseModel):
+    name = ""
