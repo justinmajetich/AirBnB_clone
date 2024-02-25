ca55c85d79c350aaa36f7559f30407566fb8ee10
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-02-25 13:52:42 +0000

    Updated place and amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" Place Module for HBNB project """
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
-
-class Amenity(BaseModel):
-    name = ""
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+                      Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
