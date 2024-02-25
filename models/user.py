0a9559cf65432320b04cd64526dde78aee4f6fb3
meddizzle316 <meddizzle316@gmail.com>
2024-02-25 15:00:34 -0600

    attempting task 8, mod user, place, state and city

--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
 
 
@@ -11,3 +12,6 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False, default="")
     first_column = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+    places = relationship("Place", backref="user", cascade="all, delete, delete-orphan")
+    #does backref name the reference from a place object to his user?
+    
