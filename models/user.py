82a6ceaab07e705d641a0d78db0bb269f0b08d15
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-02-25 03:42:41 -0600

    Removed defaults on nullable attributes

--- models/user.py
+++ models/user.py
@@ -8,5 +8,5 @@ class User(BaseModel, Base):
     __tablename__ = 'users'
     email = Column(String(128), nullable=False, default="")
     password = Column(String(128), nullable=False, default="")
-    first_column = Column(String(128), nullable=True, default="")
-    last_name = Column(String(128), nullable=True, default="")
+    first_column = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
