9acece49db50015dd47b26cf08c4252b1af77e91
iamseaniam <7222@holbertonstudents.com>
2024-02-26 14:53:41 -0600

    moved import and got rid of nullable

--- models/amenity.py
+++ models/amenity.py
@@ -4,12 +4,11 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel
 from models.base_model import Base
-
+from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
     """somthing"""
-    from models.place import place_amenity
     __tablename__ = "Amenity"
     name = Column(String(128), nullable=False)
     place_amenity = relationship("Place", secondary=place_amenity, backref="amenities")
--- models/place.py
+++ models/place.py
@@ -7,8 +7,8 @@ from models.base_model import Base
 
 
 place_amenity = Table('place_amenity', Base.metadata,
-    Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
-    Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)                      
+    Column('place_id', String(60), ForeignKey('places.id'), primary_key=True),
+    Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True)                      
 )
 
 class Place(BaseModel, Base):
