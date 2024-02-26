01e9e5604811e649002fc0fa3e1a1794de561121
Lavyatarah <mattlavyy@gmail.com>
2024-02-26 12:09:35 +0300

    Remove AirBnB_clone_v2 directory

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-"""define clsaa Amenity"""
+"""This is the amenity class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from models.place import place_amenity
 
-from models.base_model import BaseModel
 
-
-class Amenity(BaseModel):
-    """inherits from BaseModel"""
-    name = ""
+class Amenity(BaseModel, Base):
+    """This is the class for Amenity
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
