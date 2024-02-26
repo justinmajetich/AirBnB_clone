25d9e2ca23abf125778b2a6404e4743aaa4063d8
Collinb190 <collinb190@gmail.com>
2024-02-26 08:55:34 -0600

    adding features to task 6

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from os import getenv
+from .user import User
+from .city import City
+from .place import Place
+from .state import State
+from .review import Review
+from .amenity import Amenity
+
 
 if getenv("HBNB_TYPE_STORAGE") == "db":
     from models.engine.db_storage import DBStorage
--- models/city.py
+++ models/city.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+from os import getenv
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, ForeignKey, String
 
@@ -7,5 +8,9 @@ from sqlalchemy import Column, ForeignKey, String
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__= "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    else:
+        name = ''
+        state_id = ''
