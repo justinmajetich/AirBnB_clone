956418cd5e9576f7eb824c99783a0ebef5839bd2
kvang2 <kevinvaj88@hotmail.com>
2024-02-26 11:30:43 -0600

    updating sates and cities file and base model

--- models/city.py
+++ models/city.py
@@ -5,5 +5,6 @@ from models.base_model import BaseModel
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    state_id = Column(str(60), Foreignkey('state.id'), nullable=False)
+    name = Column(str(128), nullable=False)
+    __tablename__ = cities
