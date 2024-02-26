014f145479c03d00aa7ecb866c3e74cb57ada279
root <root@DESKTOP-FADRK94>
2024-02-26 12:30:14 -0600

    made new relationships

--- models/city.py
+++ models/city.py
@@ -8,9 +8,11 @@ from sqlalchemy.orm import relationship
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
+    #Table set up and columns thereof
     __tablename__ = 'cities'
     name = Column('name', String(128), nullable=False)
     state_id = Column('state_id', String(60), ForeignKey('states.id'), nullable=False)
 
+    #Set up relationships
     state = relationship('State', back_populates='cities')
     places = relationship('Place', back_populates='city', cascade='all, delete-orphan')
