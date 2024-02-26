b1902603c55acab7d354eee6f2aefd95f6a77a06
meddizzle316 <meddizzle316@gmail.com>
2024-02-26 15:54:16 -0600

    changes to all method

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -33,13 +33,17 @@ class DBStorage():
 
     def all(self, cls=None):
         """Gets all instances of cls or of all classes if cls is None"""
+        import copy
         dict = {}
         classes = [User, State, City, Amenity, Place, Review] if cls is None else [cls]
         for cls in classes:
             try:
                 instances = self.__session.query(cls).all()
                 for instance in instances:
-                    dict[f"{cls.__name__}.{instance.id}"] = instance
+                    deep_copy = copy.deepcopy(instance)
+                    if '_sa_instance_state' in deep_copy.__dict__.keys():
+                        del deep_copy.__dict__['_sa_instance_state']
+                    dict[f"{cls.__name__}.{deep_copy.id}"] = deep_copy
             except Exception:
                 pass
         return dict
