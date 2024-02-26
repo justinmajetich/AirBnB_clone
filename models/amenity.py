5b799520b16cc6d310598ce3dae7bef3cb19e591
iamseaniam <7222@holbertonstudents.com>
2024-02-26 14:11:59 -0600

    many to, many relationships. with places and amenities

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
-from models.base_model import Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from models.place import place_amenity
+
 
 
 class Amenity(BaseModel, Base):
     """somthing"""
     __tablename__ = "Amenity"
     name = Column(String(128), nullable=False)
+    place_amenity = relationship("Place", secondary=place_amenity, backref="amenities")
--- models/place.py
+++ models/place.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
-from models.base_model import Base
-from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from models.review import Review
+from models.amenity import Amenity
 
+
+place_amenity = Table('place_amenity', Base.metadata,
+    Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False)
+    Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)                      
+)
 
 class Place(BaseModel, Base):
     """Place model for HBNB"""
@@ -21,9 +27,23 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     reviews = relationship("Review", backref="place", cascade="all, delete")
+    place_amenities = relationship("Amenity", secondary=place_amenity, backref="places")
+    amenities = relationship("Amenity", secondary=place_amenity, backref="places", viewonly=False)
+
     
     @property
     def reviews(self):
         """Getter for reviews"""
         from models.review import Review
         return [review for review in Review.all() if review.place_id == self.id]
+
+     @property
+    def amenities(self):
+        """Getter for amenities"""
+        return [Amenity.all()[id] for id in self.amenity_ids]
+
+    @amenities.setter
+    def amenities(self, value):
+        """Setter for amenities"""
+        if isinstance(value, Amenity):
+            self.amenity_ids.append(value.id)
