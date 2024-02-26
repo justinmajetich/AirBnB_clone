fe719dbc4d0995217a716e0f7fb621ffa34abe3b
AdrianWolfP <wolfy52032@gmail.com>
2024-02-26 16:54:48 -0600

    Worked in test_models folder

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,69 +1,94 @@
 #!/usr/bin/python3
-""" """
+""" Module for place unittesting"""
 from tests.test_models.test_base_model import test_basemodel
 from models.place import Place
+import unittest
+import models.place
+import pep8
 
 
 class test_Place(test_basemodel):
-    """ """
+    """ unittests for place"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ instantiates place for testing"""
         super().__init__(*args, **kwargs)
         self.name = "Place"
         self.value = Place
 
     def test_city_id(self):
-        """ """
+        """ tests proper funcionality of city id"""
         new = self.value()
         self.assertEqual(type(new.city_id), str)
 
     def test_user_id(self):
-        """ """
+        """ tests proper creation of user id"""
         new = self.value()
         self.assertEqual(type(new.user_id), str)
 
     def test_name(self):
-        """ """
+        """ test proper creation of place name"""
         new = self.value()
         self.assertEqual(type(new.name), str)
 
     def test_description(self):
-        """ """
+        """ test proper creation of place description"""
         new = self.value()
         self.assertEqual(type(new.description), str)
 
     def test_number_rooms(self):
-        """ """
+        """ test proper creation of number of rooms"""
         new = self.value()
         self.assertEqual(type(new.number_rooms), int)
 
     def test_number_bathrooms(self):
-        """ """
+        """ test proper creation of number of bathrooms"""
         new = self.value()
         self.assertEqual(type(new.number_bathrooms), int)
 
     def test_max_guest(self):
-        """ """
+        """ test proper creation of number of guests"""
         new = self.value()
         self.assertEqual(type(new.max_guest), int)
 
     def test_price_by_night(self):
-        """ """
+        """ tests proper creation of price by night"""
         new = self.value()
         self.assertEqual(type(new.price_by_night), int)
 
     def test_latitude(self):
-        """ """
+        """ tests proper creation of latitude"""
         new = self.value()
         self.assertEqual(type(new.latitude), float)
 
     def test_longitude(self):
-        """ """
+        """ tests proper creation of longitude"""
         new = self.value()
         self.assertEqual(type(new.latitude), float)
 
     def test_amenity_ids(self):
-        """ """
+        """ tests proper creation amenity id """
         new = self.value()
         self.assertEqual(type(new.amenity_ids), list)
+
+class TestAmenityDoc(unittest.TestCase):
+    """Tests for documentation in place class"""
+
+    def test_module_doc(self):
+        """ checks for module doc"""
+        self.assertGreaterEqual(len(models.place.__doc__), 1)
+
+    def test_class_doc(self):
+        """ checks for class doc"""
+        self.assertGreaterEqual(len(Place.__doc__), 1)
+
+
+class TestPlaceepep8(unittest.TestCase):
+    """ tests place class for pep8 compliance"""
+
+    def test_pep8_compliance(self):
+        """ test to ensure models/places.py is pep8 compliant"""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(["tests/test_models/test_place.py"])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-""" """
+""" test module for state class"""
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
+import unittest
+import pep8
+import models.state
 
 
 class test_state(test_basemodel):
@@ -17,3 +20,33 @@ class test_state(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.name), str)
+
+
+class TestStateDoc(unittest.TestCase):
+    """Tests for documentation in state class"""
+
+    def test_module_doc(self):
+        """ checks for module doc"""
+        self.assertGreaterEqual(len(models.state.__doc__), 1)
+
+    def test_class_doc(self):
+        """ checks for class doc"""
+        self.assertGreaterEqual(len(State.__doc__), 1)
+
+
+class TestReviewpep8(unittest.TestCase):
+    """ tests state class for pep8 compliance"""
+
+    def test_pep8_compliance(self):
+        """ test to ensure models/state.py is pep8 compliant"""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(["models/state.py"])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_pep8_compliance(self):
+        """ test to ensure tests/test_models/test_state.py is pep8 compliant"""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(["tests/test_models/test_state.py"])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
