58de7c33ffa389b3c8cb6415985ff4470f73d9b8
iamseaniam <7222@holbertonstudents.com>
2024-02-26 13:20:22 -0600

    added Column and imports

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, String
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """somthing"""
+    __tablename__ = "Amenity"
+    name = Column(String(128), nullable=False)
