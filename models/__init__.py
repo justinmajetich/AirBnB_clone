5af7b85f219abe4ac1fa63240731e8f998f5a14e
Cody Walenciak <codywalenciak@gmail.com>
2024-02-26 16:48:17 +0000

    dotenv wasn't actually meant to be deleted

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from os import environ as env
+from dotenv import load_dotenv
 
+load_dotenv()
 
 if env['HBNB_TYPE_STORAGE'] == "db":
     from models.engine.db_storage import DBStorage
