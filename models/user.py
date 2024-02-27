c6d0cd8d5b902631149eb78b92328e33c2bea8d0
kvang2 <kevinvaj88@hotmail.com>
2024-02-26 23:55:35 -0600

    update db_storage

--- models/user.py
+++ models/user.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = Column(str(128), nullable=False)
-    password = Column(str(128), nullable=False)
-    first_name = Column(str(128), nullable=True)
-    last_name = Column(str(128), nullable=True)
     __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship("Place", cascade="all, delete", backref="user")
