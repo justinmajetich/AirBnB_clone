0d322a54d87de90abd523dbc39104e64d4c650e1
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-02-27 12:28:39 -0600

    Passing arguments to instance creation in place test

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -15,55 +15,55 @@ class test_Place(test_basemodel):
 
     def test_city_id(self):
         """ """
-        new = self.value()
+        new = self.value(city_id="id")
         self.assertEqual(type(new.city_id), str)
 
     def test_user_id(self):
         """ """
-        new = self.value()
+        new = self.value(user_id="user")
         self.assertEqual(type(new.user_id), str)
 
     def test_name(self):
         """ """
-        new = self.value()
+        new = self.value(name="name")
         self.assertEqual(type(new.name), str)
 
     def test_description(self):
         """ """
-        new = self.value()
+        new = self.value(description="description")
         self.assertEqual(type(new.description), str)
 
     def test_number_rooms(self):
         """ """
-        new = self.value()
+        new = self.value(number_rooms=2)
         self.assertEqual(type(new.number_rooms), int)
 
     def test_number_bathrooms(self):
         """ """
-        new = self.value()
+        new = self.value(number_bathrooms=2)
         self.assertEqual(type(new.number_bathrooms), int)
 
     def test_max_guest(self):
         """ """
-        new = self.value()
+        new = self.value(max_guest=10)
         self.assertEqual(type(new.max_guest), int)
 
     def test_price_by_night(self):
         """ """
-        new = self.value()
+        new = self.value(price_by_night=100)
         self.assertEqual(type(new.price_by_night), int)
 
     def test_latitude(self):
         """ """
-        new = self.value()
+        new = self.value(latitude=1.2)
         self.assertEqual(type(new.latitude), float)
 
     def test_longitude(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.latitude), float)
+        new = self.value(longitude=1.2)
+        self.assertEqual(type(new.longitude), float)
 
     def test_amenity_ids(self):
         """ """
-        new = self.value()
+        new = self.value(amenity_ids=[1, 2, 3])
         self.assertEqual(type(new.amenity_ids), list)
