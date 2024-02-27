a5986fc2420dccd735e6c4818f393ac9398303b4
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-02-27 14:16:11 -0600

    went back to original 'show' logic to retrieve from storage

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""This module instantiates an object of class FileStorage
+    or DBStorage (depending on storage variable)"""
 from os import getenv
 
 
