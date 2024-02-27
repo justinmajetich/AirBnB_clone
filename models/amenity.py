9803aa8501ccb7262fd7bdaea180e4fa8ece3ed2
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-02-27 21:19:04 +0000

    got rid of relationship to place_amenities

--- models/amenity.py
+++ models/amenity.py
@@ -2,7 +2,6 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel
 from .base_model import Base
-from .place_amenities import place_amenity
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 import os
@@ -10,7 +9,6 @@ storage_type = os.getenv('HBNB_TYPE_STORAGE', 'file')
 
 
 class Amenity(BaseModel, Base):
-
     __tablename__ = 'amenities'
     if storage_type == 'db':
         name = Column('name', String(128), nullable=False)
--- models/state.py
+++ models/state.py
@@ -10,8 +10,9 @@ storage_type = os.getenv('HBNB_TYPE_STORAGE', 'file')
 
 class State(BaseModel, Base):
     """ State class """
+    __tablename__ = 'states'
+
     if storage_type == 'db':
-        __tablename__ = 'states'
         name = Column('name', String(128), nullable=False)
     
         cities = relationship('City', back_populates='state', cascade='all, delete-orphan')
