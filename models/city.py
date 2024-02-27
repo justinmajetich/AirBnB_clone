a9662e90b52c8c2f3f9c684448a9515932f8dd8b
kvang2 <kevinvaj88@hotmail.com>
2024-02-26 22:42:57 -0600

    fixing corrections and updated files base_model,city.py, state.py,  db_storage.py

--- models/city.py
+++ models/city.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from sqlalchemy import Column
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = Column(str(60), Foreignkey('state.id'), nullable=False)
-    name = Column(str(128), nullable=False)
-    __tablename__ = cities
-    
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('state.id'), nullable=False)
--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,11 +2,25 @@
 """New engine DBStorage"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import create_engine
-from sqlalchemy import Column
+from sqlalchemy.orm import sessionmaker, scoped_session
+from os import getenv
 
 
-__engine = None
-__session = None
+class DBStorage:
+    """Class that manages DBStorage"""
+    __engine = None
+    __session = None
 
-def __init__(self):
-    """Engine linked to MySQL database and user"""
+    def __init__(self):
+        """Initializes DBStorage"""
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
+                                        format(getenv('HBNB_MYSQL_USER'),
+                                              getenv('HBNB_MYSQL_PWD'),
+                                              getenv('HBNB_MYSQL_HOST'),
+                                              getenv('HBNB_MYSQL_DB')),
+                                        pool_pre_ping=True)
+        if getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """Query on database session"""
