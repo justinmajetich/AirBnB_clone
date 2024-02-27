338b9045df554865d718a1eb9f4818dcf646ef3c
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-02-27 14:37:46 -0600

    more pycode fixes

--- models/place.py
+++ models/place.py
@@ -5,10 +5,11 @@ from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 from sqlalchemy.orm import relationship
 
 place_amenity = Table("place_amenity", Base.metadata,
-                    Column('place_id', String(60), ForeignKey('places.id'),
-                    primary_key=True, nullable=False),
-                    Column('amenity_id', String(60), ForeignKey('amenities.id'),
-                    primary_key=True, nullable=False))
+                      Column('place_id', String(60), ForeignKey('places.id'),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'),
+                             primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
--- models/user.py
+++ models/user.py
@@ -5,7 +5,6 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
