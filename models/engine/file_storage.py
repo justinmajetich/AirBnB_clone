2dbd050d2120a1c8883ca506618cefcbedfa113b
Jordan Arguelles <Jordan.arguelles.ja@gmail.com>
2024-02-27 16:43:02 -0600

    fix errors

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,69 +1,80 @@
 #!/usr/bin/python3
+"""This is the file storage class for AirBnB"""
 import json
-from datetime import datetime
-from models import *
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+import shlex
 
 
 class FileStorage:
+    """This class serializes instances to a JSON file and
+    deserializes JSON file to instances
+    Attributes:
+        __file_path: path to the JSON file
+        __objects: objects will be stored
+    """
     __file_path = "file.json"
     __objects = {}
 
-    def __init__(self):
-        self.reload()
-
     def all(self, cls=None):
-        if cls is None:
-            return FileStorage.__objects
-
-        storage = {}
-        for obj_id in FileStorage.__objects:
-            obj_cls = FileStorage.__objects[obj_id].__class__.__name__
-            if cls == obj_cls:
-                storage[obj_id] = FileStorage.__objects[obj_id]
-
-        return storage
+        """returns a dictionary
+        Return:
+            returns a dictionary of __object
+        """
+        dic = {}
+        if cls:
+            dictionary = self.__objects
+            for key in dictionary:
+                partition = key.replace('.', ' ')
+                partition = shlex.split(partition)
+                if (partition[0] == cls.__name__):
+                    dic[key] = self.__objects[key]
+            return (dic)
+        else:
+            return self.__objects
 
     def new(self, obj):
-        if obj is not None:
-            FileStorage.__objects[obj.id] = obj
+        """sets __object to given obj
+        Args:
+            obj: given object
+        """
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            self.__objects[key] = obj
 
     def save(self):
-        store = {}
-        for k in FileStorage.__objects.keys():
-            store[k] = FileStorage.__objects[k].to_json()
-
-        with open(FileStorage.__file_path, mode="w", encoding="utf-8") as fd:
-            fd.write(json.dumps(store))
-
-    def update(self, cls, obj_id, key, new_value):
-        if obj_id not in FileStorage.__objects:
-            return 0
-
-        obj = FileStorage.__objects[obj_id]
-        setattr(obj, key, new_value)
-        return 1
+        """serialize the file path to JSON file path
+        """
+        my_dict = {}
+        for key, value in self.__objects.items():
+            my_dict[key] = value.to_dict()
+        with open(self.__file_path, 'w', encoding="UTF-8") as f:
+            json.dump(my_dict, f)
 
     def reload(self):
+        """serialize the file path to JSON file path
+        """
         try:
-            with open(FileStorage.__file_path,
-                      mode="r+", encoding="utf-8") as fd:
-                FileStorage.__objects = {}
-                temp = json.load(fd)
-                for k in temp.keys():
-                    cls = temp[k].pop("__class__", None)
-                    cr_at = temp[k]["created_at"]
-                    cr_at = datetime.strptime(cr_at, "%Y-%m-%d %H:%M:%S.%f")
-                    up_at = temp[k]["updated_at"]
-                    up_at = datetime.strptime(up_at, "%Y-%m-%d %H:%M:%S.%f")
-                    FileStorage.__objects[k] = eval(cls)(temp[k])
-        except Exception as e:
+            with open(self.__file_path, 'r', encoding="UTF-8") as f:
+                for key, value in (json.load(f)).items():
+                    value = eval(value["__class__"])(**value)
+                    self.__objects[key] = value
+        except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
-        if obj is None:
-            return
-
-        FileStorage.__objects.pop(obj.id, 0)
+        """ delete an existing element
+        """
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del self.__objects[key]
 
     def close(self):
-        self.save()
+        """ calls reload()
+        """
+        self.reload()
