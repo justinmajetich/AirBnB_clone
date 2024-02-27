ab600ee0b59316f2d4161d1a0e84ab5f01bc05d1
iamseaniam <7222@holbertonstudents.com>
2024-02-27 15:19:40 -0600

    pycode and syntx error with nullable being true when it should be false

--- models/place.py
+++ models/place.py
@@ -9,10 +9,20 @@ from os import getenv
 env = getenv('HBNB_TYPE_STORAGE')
 
 
-place_amenity = Table('place_amenity', Base.metadata,
-    Column('place_id', String(60), ForeignKey('places.id'), primary_key=True),
+place_amenity = Table(
+    'place_amenity',
+    Base.metadata,
     Column(
-        'amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True
+        'place_id',
+        String(60),
+        ForeignKey('places.id'),
+        primary_key=True
+    ),
+    Column(
+        'amenity_id',
+        String(60),
+        ForeignKey('amenities.id'),
+        primary_key=True
     )
 )
 
@@ -23,7 +33,7 @@ class Place(BaseModel, Base):
     if env == 'db':
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        name = Column(String(128), nullable=True)
+        name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, default=0, nullable=False)
         number_bathrooms = Column(Integer, default=0, nullable=False)
