568acd7719a221e4a2f7178ab876a42dd62e6161
NolbsAtHolbs <hikikomode@gmail.com>
2024-02-26 20:36:49 -0600

    monitor MySQL Server execution

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,7 +7,7 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- Grant privileges to user on database
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
--- Grant SELECT on performance_schema to user
+-- Grant select on performance_schema to user
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
 -- Flush to appply changes immediately
