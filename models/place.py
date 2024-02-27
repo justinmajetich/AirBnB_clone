569dd8e13907e1937f01bec01864dec73b225ec0
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-02-27 12:49:40 -0600

    import fix and typo fix

--- models/place.py
+++ models/place.py
@@ -16,8 +16,8 @@ class Place(BaseModel, Base):
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullabe=True)
-    longitude = Column(Float, nullabe=True)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     amenity_ids = []
     #  When 'place' obj is deleted, all orphaned Review instances are too 
     reviews = relationship("Review", cascade="all, delete-orphan",
--- models/user.py
+++ models/user.py
@@ -2,7 +2,7 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy import relationship
+from sqlalchemy.orm import relationship
 
 
 
