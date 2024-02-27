1ed8989d44d71d50c4a8a8d32810bb59b5871dcd
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-02-27 17:01:25 +0000

    amenity and place relationship fixes

--- models/amenity.py
+++ models/amenity.py
@@ -11,4 +11,4 @@ class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     name = Column('name', String(128), nullable=False)
 
-    places = relationship('Place', secondary=place_amenity, back_populates='amenities', cascade='all, delete-orphan')
+    places = relationship('Place', secondary=place_amenity, back_populates='amenities')
