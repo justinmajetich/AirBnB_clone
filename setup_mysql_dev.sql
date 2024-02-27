2052b800841716a2bae3b4b884ccb044cc228372
NolbsAtHolbs <hikikomode@gmail.com>
2024-02-26 20:29:53 -0600

    made sql dev setup

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,2 +1,14 @@
--- Setup MySQL Test
-SELECT 1 WHERE FALSE;
+-- Create database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create user
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant privileges to user on database
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT on performance_schema to user
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Flush to appply changes immediately
+FLUSH PRIVILEGES;
