64e499a2a034f3d2391b892203a8ba4180874d82
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-02-27 07:02:01 -0600

    pycodestyle

--- models/user.py
+++ models/user.py
@@ -12,5 +12,7 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    places = relationship("Place", backref="user", cascade="all, delete, delete-orphan")
-    reviews = relationship("Review", backref="user", cascade="all, delete, delete-orphan")
+    places = relationship("Place", backref="user",
+                          cascade="all, delete, delete-orphan")
+    reviews = relationship("Review", backref="user",
+                           cascade="all, delete, delete-orphan")
