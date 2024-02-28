23d06d9136fda156c499e81ba3604ec09479087d
Rasheed White <143828699+ReyesxRoyal@users.noreply.github.com>
2024-02-28 00:00:17 -0600

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""This is the amenity class"""
 from models.base_model import BaseModel
 
+s = "HBNB_TYPE_STORAGE"
+if s in environ.keys() and environ["HBNB_TYPE_STORAGE"] == "db":
+    class Amenity(BaseModel, Base):
+        """
+        This is the state class
+        """
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
 
-class Amenity(BaseModel):
-    name = ""
+        def __init__(self, **kwargs):
+            setattr(self, "id", str(uuid4()))
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+else:
+    class Amenity(BaseModel):
+        """This is the class for Amenity
+        Attributes:
+            name: input name
+        """
+        name = ""
