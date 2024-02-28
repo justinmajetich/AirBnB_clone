c81ae1a207459c65cc01461b10b7e4716061f0a5
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-02-28 12:52:56 -0600

    Next test

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -9,9 +9,19 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 class DatabaseStorage:
     """Database management of storage for hbnb clone"""
-    def __init__(self) -> None:
-        pass
+    __engine = None
+    __session = None
+    def __init__(self):
+        db_config = {
+            'user': 'HBNB_MYSQL_USER',
+            'password': 'HBNB_MYSQL_PWD',
+            'host': 'HBNB_MYSQL_HOST',
+            'database': 'HBNB_MYSQL_DB'
+        }
+        self.__engine = create_engine(f"mysql+mysqldb://{db_config['user']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}", pool_pre_ping=True)
