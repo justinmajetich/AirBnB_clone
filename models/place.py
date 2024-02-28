75007af12a35457693061062e2a3a7fef2029b62
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-02-27 23:19:36 -0600

    Back to 97%

--- models/place.py
+++ models/place.py
@@ -1,20 +1,9 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
+from sqlalchemy import Column, String, ForeignKey, Integer, Float
 from sqlalchemy.orm import relationship
-import os
 
-
-
-place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id', String(60),
-                             ForeignKey('places.id'), primary_key=True,
-                             nullable=False),
-                             Column('amenity_id', String(60),
-                                    ForeignKey('amenities.id'),
-                                    primary_key=True, nullable=False)
-)
 
 class Place(BaseModel, Base):
 
@@ -33,30 +22,12 @@ class Place(BaseModel, Base):
     longitude = Column(Float, nullable=True)
     reviews = relationship("Review", backref="place",
                            cascade="all, delete, delete-orphan")
-    amenities = relationship("Amenity", secondary=place_amenity,
-                             back_populates="place_amenities", viewonly=False)
 
-    if os.getenv("HBNB_TYPE_STORAGE") != 'db':
-        @property
-        def reviews(self):
-            """Returns the list of Review instances from current place"""
-            from models import storage
-            all_reviews = storage.all(BaseModel.Review)
-            return [review for review in all_reviews.values()
-                    if review.place_id == self.id]
 
-        @property
-        def amenities(self):
-            from models.amenity import Amenity
-            from models.__init__ import storage
-            """Getter attribute amenities for FileStorage."""
-            return [amenity for amenity in storage.all(Amenity).values()
-                    if amenity.id in self.amenity_ids]
-
-        @amenities.setter
-        def amenities(self, obj):
-            """Setter attribute amenities for FileStorage."""
-            from models.amenity import Amenity
-            if isinstance(obj, Amenity):
-                if obj.id not in self.amenity_ids:
-                    self.amenity_ids.append(obj.id)
+    @property
+    def reviews(self):
+        """Returns the list of Review instances from current place"""
+        from models import storage
+        all_reviews = storage.all(BaseModel.Review)
+        return [review for review in all_reviews.values()
+                if review.place_id == self.id]
