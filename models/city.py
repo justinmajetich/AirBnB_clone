9b629e08754fda9fa0c85c8538da6e8825a18bb2
Rasheed White <143828699+ReyesxRoyal@users.noreply.github.com>
2024-02-27 23:50:06 -0600

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,21 +1,30 @@
 #!/usr/bin/python3
 """This is the city class"""
-from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy import ForeignKey
+from sqlalchemy import Column, String, ForeignKey, Integer
 from sqlalchemy.orm import relationship
-from models.place import Place
+from os import environ
+from uuid import uuid4
 
+s = "HBNB_TYPE_STORAGE"
+if s in environ.keys() and environ["HBNB_TYPE_STORAGE"] == "db":
+    class City(BaseModel, Base):
+        '''
+        This is the class for City Attributes
+        '''
+        __tablename__ = "cities"
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities", cascade="all,delete")
 
-class City(BaseModel, Base):
-    """This is the class for City
-    Attributes:
-        state_id: The state id
-        name: input name
-    """
-    __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="cities")
+        def __init__(self, **kwargs):
+            setattr(self, "id", str(uuid4()))
+            for i, j in kwargs.items():
+                setattr(self, i, j)
+else:
+    class City(BaseModel):
+        '''
+        This is the class for City
+        '''
+        state_id = ""
+        name = ""
