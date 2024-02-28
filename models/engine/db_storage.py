e279cd8488509a42c4ca267e86cbd56c3b420af6
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-02-28 14:10:25 -0600

    Big test

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -12,11 +12,12 @@ from models.review import Review
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+Base = declarative_base
 class DatabaseStorage:
     """Database management of storage for hbnb clone"""
     __engine = None
     __session = None
+    __tablename__ = 'states'
     def __init__(self):
         db_config = {
             'user': 'HBNB_MYSQL_USER',
@@ -25,3 +26,17 @@ class DatabaseStorage:
             'database': 'HBNB_MYSQL_DB'
         }
         self.__engine = create_engine(f"mysql+mysqldb://{db_config['user']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}", pool_pre_ping=True)
+        if os.environ.get('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        session = self.__session()
+        if cls is not None:
+            objects = session.query(cls).all()
+        else:
+            objects = []
+            queryclass = [User, State, City, Amenity, Place, Review]
+            for queryclass in classes_to_query:
+                    objects.extend(session.query(queryclass).all())
+        session.close()
+        return objects
