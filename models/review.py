4b766131171e910dc082026b718ddfaf5937398e
Rasheed White <143828699+ReyesxRoyal@users.noreply.github.com>
2024-02-27 23:59:13 -0600

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,10 +1,37 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel
+"""This is the review class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
+from sqlalchemy.orm import relationship
+from os import environ
+from uuid import uuid4
 
+s = "HBNB_TYPE_STORAGE"
+if s in environ.keys() and environ["HBNB_TYPE_STORAGE"] == "db":
+    class Review(BaseModel, Base):
+        """This is the class for Review
+        Attributes:
+        place_id: place id
+        user_id: user id
+        text: review description
+        """
+        __tablename__ = "reviews"
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+        def __init__(self, **kwargs):
+            setattr(self, "id", str(uuid4()))
+            for i, j in kwargs.items():
+                setattr(self, i, j)
+else:
+    class Review(BaseModel):
+        """This is the class for Review
+        Attributes:
+            place_id: place id
+            user_id: user id
+            text: review description
+        """
+        place_id = ""
+        user_id = ""
+        text = ""
