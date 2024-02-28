9504c8c3ea8f55d094346e63234c1318d65d10ba
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-02-28 14:45:12 -0600

    Awesome

--- models/state.py
+++ models/state.py
@@ -10,7 +10,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name"""
