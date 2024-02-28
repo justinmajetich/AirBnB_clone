61e9896dc12e83caed28a23364c17ba9b6cb8e3f
NolbsAtHolbs <hikikomode@gmail.com>
2024-02-28 10:45:16 -0600

    pcs?

--- models/base_model.py
+++ models/base_model.py
@@ -13,9 +13,9 @@ Base = declarative_base()
 class BaseModel:
     """This class will defines all common attributes/methods
     for other classes"""
-    id = Column (String(60), unique=True, nullable=False, primary_key=True)
-    created_at = Column (DateTime, nullable=False, default=(datetime.utcnow()))
-    updated_at = Column (DateTime, nullable=False, default=(datetime.utcnow()))
+    id = Column(String(60), unique=True, nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
+    updated_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
 
     def __init__(self, *args, **kwargs):
         """Instantiation of base model class
