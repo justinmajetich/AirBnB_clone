10f705dafc6ecf67cff0d811059a9a3c74caae4b
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-02-27 22:44:24 -0600

    Removing table

--- models/place.py
+++ models/place.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
+from sqlalchemy import Column, String, ForeignKey, Integer, Float
 from sqlalchemy.orm import relationship
 
-
-place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id', String(60),
-                             ForeignKey('places.id'),
-                             nullable=False, primary_key=True),
-                      Column('amenity_id', String(60),
-                             ForeignKey('amenities.id'),
-                             nullable=False, primary_key=True)
-)
 
 class Place(BaseModel, Base):
     """ A place to stay """
