d499b4babe5d5939a9c78d0fc8cca4b69a1db96b
NolbsAtHolbs <hikikomode@gmail.com>
2024-02-28 10:38:20 -0600

    pcs?

--- models/base_model.py
+++ models/base_model.py
@@ -12,11 +12,10 @@ Base = declarative_base()
 
 class BaseModel:
     """This class will defines all common attributes/methods
-    for other classes
-    """
-    id = Column(String(60), unique=True, nullable=False, primary_key=True)
-    created_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
-    updated_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
+    for other classes"""
+    id = Column (String(60), unique=True, nullable=False, primary_key=True)
+    created_at = Column (DateTime, nullable=False, default=(datetime.utcnow()))
+    updated_at = Column (DateTime, nullable=False, default=(datetime.utcnow()))
 
     def __init__(self, *args, **kwargs):
         """Instantiation of base model class
@@ -26,8 +25,7 @@ class BaseModel:
         Attributes:
             id: unique id generated
             created_at: creation date
-            updated_at: updated date
-        """
+            updated_at: updated date"""
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
@@ -47,19 +45,16 @@ class BaseModel:
     def __str__(self):
         """returns a string
         Return:
-            returns a string of class name, id, and dictionary
-        """
+            returns a string of class name, id, and dictionary"""
         return "[{}] ({}) {}".format(
             type(self).__name__, self.id, self.__dict__)
 
     def __repr__(self):
-        """return a string representaion
-        """
+        """return a string representaion"""
         return self.__str__()
 
     def save(self):
-        """updates the public instance attribute updated_at to current
-        """
+        """updates the public instance attribute updated_at to current"""
         self.updated_at = datetime.now()
         models.storage.new(self)
         models.storage.save()
@@ -67,8 +62,7 @@ class BaseModel:
     def to_dict(self):
         """creates dictionary of the class  and returns
         Return:
-            returns a dictionary of all the key values in __dict__
-        """
+            returns a dictionary of all the key values in __dict__"""
         my_dict = dict(self.__dict__)
         my_dict["__class__"] = str(type(self).__name__)
         my_dict["created_at"] = self.created_at.isoformat()
@@ -78,6 +72,5 @@ class BaseModel:
         return my_dict
 
     def delete(self):
-        """ delete object
-        """
+        """ delete object"""
         models.storage.delete(self)
