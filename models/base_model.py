3fe829f473b97f8b191ead052abaed7567d63c8f
Greentaylor27 <7196@holbertonstudents.com>
2024-02-29 11:42:37 -0600

    Removing indent in line 19

--- models/base_model.py
+++ models/base_model.py
@@ -16,7 +16,7 @@ class BaseModel:
     id = Column(String(60), nullable=False, primary_key=True)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
