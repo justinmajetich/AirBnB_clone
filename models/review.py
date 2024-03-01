c9c6f9e46be8564c83b2f739bf46b7ab06301210
Rasheed White <143828699+ReyesxRoyal@users.noreply.github.com>
2024-02-29 19:34:25 -0600

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,37 +1,31 @@
 #!/usr/bin/python3
-"""This is the review class"""
+""" holds class Review"""
+import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import environ
-from uuid import uuid4
+from sqlalchemy import ForeignKey
 
-s = "HBNB_TYPE_STORAGE"
-if s in environ.keys() and environ["HBNB_TYPE_STORAGE"] == "db":
-    class Review(BaseModel, Base):
-        """This is the class for Review
-        Attributes:
-        place_id: place id
-        user_id: user id
-        text: review description
-        """
-        __tablename__ = "reviews"
-        text = Column(String(1024), nullable=False)
-        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
 
-        def __init__(self, **kwargs):
-            setattr(self, "id", str(uuid4()))
-            for i, j in kwargs.items():
-                setattr(self, i, j)
-else:
-    class Review(BaseModel):
-        """This is the class for Review
-        Attributes:
-            place_id: place id
-            user_id: user id
-            text: review description
-        """
+class Review(BaseModel, Base):
+    """Representation of Review """
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'reviews'
+        text = Column(String(1024),
+                      nullable=False)
+        place_id = Column(String(60),
+                          ForeignKey('places.id'),
+                          nullable=False)
+        user_id = Column(String(60),
+                         ForeignKey('users.id'),
+                         nullable=False)
+    else:
+        text = ""
         place_id = ""
         user_id = ""
-        text = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Review"""
+        super().__init__(*args, **kwargs)
