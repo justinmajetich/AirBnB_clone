32ff77abe13c4078c260308f58931f3d0bd007ef
Rasheed White <143828699+ReyesxRoyal@users.noreply.github.com>
2024-02-29 19:31:05 -0600

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
-"""This is the amenity class"""
-from models.base_model import BaseModel
+""" holds class Amenity"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
-s = "HBNB_TYPE_STORAGE"
-if s in environ.keys() and environ["HBNB_TYPE_STORAGE"] == "db":
-    class Amenity(BaseModel, Base):
-        """
-        This is the state class
-        """
+
+class Amenity(BaseModel, Base):
+    """Representation of Amenity """
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'amenities'
-        name = Column(String(128), nullable=False)
-
-        def __init__(self, **kwargs):
-            setattr(self, "id", str(uuid4()))
-            for k, v in kwargs.items():
-                setattr(self, k, v)
-else:
-    class Amenity(BaseModel):
-        """This is the class for Amenity
-        Attributes:
-            name: input name
-        """
+        name = Column(String(128),
+                      nullable=False)
+    else:
         name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
