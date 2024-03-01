fd3c595ccb64a3b86608cd60c84ab3d605e72e6f
Jordan Arguelles <Jordan.arguelles.ja@gmail.com>
2024-02-29 21:20:49 -0600

    Update errors

--- models/amenity.py
+++ models/amenity.py
@@ -1,22 +1,7 @@
 #!/usr/bin/python3
-""" holds class Amenity"""
-import models
-from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+""" State Module for HBNB project """
+from models.base_model import BaseModel
 
 
-class Amenity(BaseModel, Base):
-    """Representation of Amenity """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'amenities'
-        name = Column(String(128),
-                      nullable=False)
-    else:
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes Amenity"""
-        super().__init__(*args, **kwargs)
+class Amenity(BaseModel):
+    name = ""
