d79bcff91c453b2fa5f5e058abb9c1aa985fbeee
Kalule Joshua <kaljoshkalule@gmail.com>
2024-03-05 22:27:19 +0000

    add test cases for value syntax

--- tests/test_console.py
+++ tests/test_console.py
@@ -15,6 +15,8 @@ from unittest.mock import patch
 from io import StringIO
 
 
+@unittest.skipUnless(os.getenv('HBNB_TYPE_STORAGE') == 'file',
+                    "requires FileStorage type")
 @patch('sys.stdout', new_callable=StringIO)
 class TestConsoleDoCreate(BaseTest):
     """Test the do_create method."""
@@ -36,7 +38,7 @@ class TestConsoleDoCreate(BaseTest):
         self.assertFalse(hasattr(self.objects[obj_id], 'myname'))
 
 
-    def test_string_values_quotes(self, stdout):
+    def test_value_string_quotes(self, stdout):
         """
         "<value>" => starts with a double quote
         """
@@ -45,7 +47,7 @@ class TestConsoleDoCreate(BaseTest):
         obj_id = "State." + stdout.getvalue().replace('\n', '')
         self.assertFalse(hasattr(self.objects[obj_id], 'myname'))
     
-    def test_string_values_underscore(self, stdout):
+    def test_value_tring_values_underscore(self, stdout):
         """
         all underscores _ must be replace by spaces
         """
@@ -55,3 +57,53 @@ class TestConsoleDoCreate(BaseTest):
         attr = getattr(self.objects[obj_id], 'name', None)
         error_msg = "underscores not removed"
         self.assertEqual(attr, "Cape Town", msg=error_msg)
+
+    # TODO: validate 'double quote inside the value escaped with \'
+
+    def test_value_float(self, stdout):
+        """
+        Float: <unit>.<decimal> => contains a dot
+        """
+        command = 'create State name="Kampala" latitude=23.78138'
+        self.onecmd(command)
+        obj_id = "State." + stdout.getvalue().replace('\n', '')
+        attr = getattr(self.objects[obj_id], 'latitude', None)
+        error_msg = "float not converted"
+        self.assertEqual(attr, 23.78138, msg=error_msg)
+
+    def test_value_float_with_non_digit(self, stdout):
+        """
+        value has '.' but contains non digit
+        """
+        command = 'create State name="Kampala" latitude=23.4abc'
+        self.onecmd(command)
+        obj_id = "State." + stdout.getvalue().replace('\n', '')
+        error_msg = "attribute should not be added"
+        self.assertFalse(hasattr(self.objects[obj_id], 'latitude'),
+                                 msg=error_msg)
+
+    def test_value_integer(self, stdout):
+        """
+        Integer: <number> => default case
+        """
+        command = 'create State name="Kampala" population=23000'
+        self.onecmd(command)
+        obj_id = "State." + stdout.getvalue().replace('\n', '')
+        attr = getattr(self.objects[obj_id], 'population', None)
+        error_msg = "interger conversion failed"
+        self.assertEqual(attr, 23000, msg=error_msg)
+
+    def test_value_unrecognized(self, stdout):
+        """ unrecognized values """
+        command = 'create State name="Normal"'
+        self.onecmd(command)
+        obj_id_0 = "State." + stdout.getvalue().replace('\n', '')
+        command = 'create State name="Abnormal" cant_recognize_me'
+        self.clear(stdout)
+        self.onecmd(command)
+        obj_id_1 = "State." + stdout.getvalue().replace('\n', '')
+        len0 = len(self.objects[obj_id_0].__dict__)
+        len1 = len(self.objects[obj_id_1].__dict__)
+        error_msg = "lengths of these 2 objects must be equal"
+        self.assertEqual(len0, len1, msg=error_msg)
+
