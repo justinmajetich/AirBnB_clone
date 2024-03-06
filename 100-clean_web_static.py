8c9e5dc4bae273398ea36bc901a0251152c8f70e
mwanikigachanja <mwanikigachanja@gmail.com>
2024-03-05 22:38:29 -0800

    Deletes out-of-date archives

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -8,8 +8,7 @@ fab -f 100-clean_web_static.py do_clean:number=2
 import os
 from fabric.api import *
 
-env.hosts = ['52.87.155.66', '54.89.109.87']
-
+env.hosts = ['54.236.50.133', '54.236.56.167']
 
 def do_clean(number=0):
     """Delete out-of-date archives.
