932982ea2bb4c377d26f57726ad6b7698d5e15f6
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-03-08 00:08:49 +0400

    5. Puppet for setup

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,50 +1,32 @@
-# setup_web_static.pp
+# Class: setup_web_static
+#
+# This class sets up web servers for the deployment of web_static.
+#
 class setup_web_static {
-  package { 'nginx':
+  $nginx_package_name = 'nginx'
+  $web_static_dirs = ['/data', '/data/web_static', '/data/web_static/releases', '/data/web_static/shared', '/data/web_static/releases/test']
+  $web_static_current_link = '/data/web_static/current'
+  $web_static_test_index = '/data/web_static/releases/test/index.html'
+  $nginx_config_file = '/etc/nginx/sites-available/default'
+
+  package { $nginx_package_name:
     ensure => installed,
   }
 
-  # Define the Nginx configuration
-  $nginx_conf = @(END)
-  server {
-      listen 80 default_server;
-      listen [::]:80 default_server;
-      add_header X-Served-By $hostname;
-      root   /var/www/html;
-      index  index.html index.htm;
-
-      location /hbnb_static {
-          alias /data/web_static/current;
-          index index.html index.htm;
-      }
-
-      location /redirect_me {
-          return 301 https://www.youtube.com/watch?v=3MbaGJN2ioQ;
-      }
-
-      error_page 404 /custom_404.html;
-      location =/custom_404.html {
-        root /var/www/html;
-        internal;
-      }
-  }
-  | END
-
-  file { ['/data', '/data/web_static', '/data/web_static/releases', '/data/web_static/releases/test', '/data/web_static/shared']:
+  file { $web_static_dirs:
     ensure => directory,
     owner  => 'ubuntu',
     group  => 'ubuntu',
   }
 
-  file { '/data/web_static/releases/test/index.html':
+  file { $web_static_test_index:
     ensure  => file,
-    content => "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>\n",
+    content => 'This is a test',
     owner   => 'ubuntu',
     group   => 'ubuntu',
   }
 
-  # Manage symbolic link for current release
-  file { '/data/web_static/current':
+  file { $web_static_current_link:
     ensure => link,
     target => '/data/web_static/releases/test',
     force  => true,
@@ -52,16 +34,15 @@ class setup_web_static {
     group  => 'ubuntu',
   }
 
-  file { '/etc/nginx/sites-available/default':
-    ensure  => file,
-    content => $nginx_conf,
-    require => Package['nginx'],
-    notify  => Service['nginx'],
+  exec { 'update_nginx_config':
+    command => "sed -i '38i\\\\tlocation /hbnb_static/ {\\\\n\\\\t\\\\talias " +
+            "/data/web_static/current/;\\\\n\\\\t}\\\\n' ${nginx_config_file}",
+    require => Package[$nginx_package_name],
   }
 
-  service { 'nginx':
-    ensure     => running,
-    enable     => true,
-    hasrestart => true,
+  service { $nginx_package_name:
+    ensure    => running,
+    enable    => true,
+    subscribe => Exec['update_nginx_config'],
   }
 }
