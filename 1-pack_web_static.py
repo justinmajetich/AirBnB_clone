64a9e55da88742d654cfb9251684819780b6fc3b
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-03-07 07:11:14 +0400

    1. Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,31 +1,24 @@
 #!/usr/bin/python3
 """
-Fabfile to generates a .tgz archive from the contents
-of web_static directory
+Fabric script that generates a .tgz archive from the
+contents of the web_static folder
 """
 import os
 from datetime import datetime
 
-from fabric import Connection
+from fabric.api import local
 
 
 def do_pack():
-    """Generates a .tgz archive from the contents of the web_static folder."""
-    # Create the versions directory if it doesn't exist
-    if not os.path.exists("versions"):
-        os.makedirs("versions")
-
-    # Create a timestamped filename
-    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    filename = "versions/web_static_{}.tgz".format(timestamp)
-
-    # Use tar to create the archive
-    result = Connection("localhost").run(
-            "tar -cvzf {} web_static".format(filename)
-            )
-
-    # Return the archive path if the archive was created successfully
-    if result.ok:
-        return filename
-    else:
+    """Generates a .tgz archive from the contents of the web_static"""
+    dt = datetime.now()
+    file_name = "web_static_{}{}{}{}{}{}.tgz".format(
+        dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second
+    )
+    try:
+        if not os.path.exists("versions"):
+            os.mkdir("versions")
+        local("tar -cvzf versions/{} web_static".format(file_name))
+        return os.path.join("versions", file_name)
+    except Exception:
         return None
