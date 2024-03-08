dbd4cde5e8b79da956e13bae8a93428fd80ed907
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-03-08 22:08:51 +0000

    formatting

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,9 +6,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
-    return('Hello HBNB!')
+    return ('Hello HBNB!')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
