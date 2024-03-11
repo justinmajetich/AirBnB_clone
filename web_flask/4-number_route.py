5b8a022435cb146beb7db664bf0ce5e6712361d1
Luke Schula <schulaluke@gmail.com>
2024-03-11 11:58:07 -0500

    edit to task 3 to make task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_again():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', defaults={"text": "is cool"})
+@app.route('/python/<text>', strict_slashes=False)
+def python_is(text):
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def number_route(n):
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=5000)
