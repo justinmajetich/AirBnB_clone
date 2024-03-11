e895057c7839043894dcc0c0252f0aeabe495c9b
Luke Schula <schulaluke@gmail.com>
2024-03-11 12:34:23 -0500

    pycode fix

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -30,6 +30,7 @@ def python_is(text):
 def number_route(n):
     return f"{n} is a number"
 
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     return render_template('5-number.html', n=n)
