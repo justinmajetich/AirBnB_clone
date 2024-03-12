d4ccb7e7ce6aa595db2ea278db3bdc76bcfd5a06
Kyle Headley <143755756+Y-T-O-1@users.noreply.github.com>
2024-03-12 17:17:55 -0500

    finished task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""placeholder"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/number/<int:n>', methods=['GET'], strict_slashes=False)
+def num_int(n):
+    return "{} is a number".format(n)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def holla_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', methods=['GET'], strict_slashes=False)
+def hbnbbb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', methods=['GET'], strict_slashes=False)
+def c_is_meh(text):
+    return "C " + text.replace("_", " ")
+
+
+@app.route('/python/', defaults={'text': 'is cool'},
+           methods=['GET'], strict_slashes=False)
+@app.route('/python/<text>', methods=['GET'], strict_slashes=False)
+def python_awesome(text):
+    return "Python " + text.replace("_", " ")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
