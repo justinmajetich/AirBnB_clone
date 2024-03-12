8032d1339c59350568185da9865f983dab4fd0a3
Luke Schula <schulaluke@gmail.com>
2024-03-12 11:42:14 -0500

    added public getter

--- models/state.py
+++ models/state.py
@@ -5,6 +5,8 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
 from models.city import City
+import models
+import shlex
 
 
 class State(BaseModel, Base):
@@ -14,3 +16,20 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship("City", cascade="all, delete-orphan",
                           backref="state")
+
+    @property
+    def cities(self):
+        """add a public getter method cities to
+        return the list of City"""
+        val = models.stoarge.all()
+        tmp = []
+        final = []
+        for key in val:
+            cities = key.replace('.', ' ')
+            cities = shlex.split(cities)
+            if (cities[0] == 'City'):
+                tmp.append(val[key])
+        for elem in tmp:
+            if (elem.state_id == self.id):
+                final.append(elem)
+        return (final)
