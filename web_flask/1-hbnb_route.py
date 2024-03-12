9c133312b37786fd7733a2cd4b0963898ca9da11
Kyle Headley <143755756+Y-T-O-1@users.noreply.github.com>
2024-03-12 17:17:19 -0500

    finished task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""placeholder"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def holla_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', methods=['GET'], strict_slashes=False)
+def hbnbbb():
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
