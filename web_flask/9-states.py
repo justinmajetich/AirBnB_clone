e47a43476e2c2a09b033b2117de54543945beeb0
Rasheed White <143828699+ReyesxRoyal@users.noreply.github.com>
2024-03-13 09:28:43 -0500

    update 9  states

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,34 +1,28 @@
 #!/usr/bin/python3
-"""Importing Flask to run the web app"""
+"""
+starts a Flask web application
+"""
+
 from flask import Flask, render_template
+from models import *
 from models import storage
-from models.state import State
-
-
 app = Flask(__name__)
 
 
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<state_id>', strict_slashes=False)
+def states(state_id=None):
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State")
+    if state_id is not None:
+        state_id = 'State.' + state_id
+    return render_template('9-states.html', states=states, state_id=state_id)
+
+
 @app.teardown_appcontext
-def close(self):
-    """ Method to close the session """
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
-
-@app.route('/states', strict_slashes=False)
-def state():
-    """Displays a html page with states"""
-    states = storage.all(State)
-    return render_template('9-states.html', states=states, mode='all')
-
-
-@app.route('/states/<id>', strict_slashes=False)
-def state_by_id(id):
-    """Displays a html page with citys of that state"""
-    for state in storage.all(State).values():
-        if state.id == id:
-            return render_template('9-states.html', states=state, mode='id')
-    return render_template('9-states.html', states=state, mode='none')
-
-
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host='0.0.0.0', port='5000')
