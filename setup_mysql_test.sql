8cfc4498c87cc9e6e20aada73a6bbd39f4127176
haoningng <nick.nghn@gmail.com>
2024-03-16 16:14:13 +1100

    feat: add sql script for taks 4

---
+++ setup_mysql_test.sql
@@ -0,0 +1,10 @@
+-- This script prepares a MySQL server for the project:
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+USE hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+
+GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
