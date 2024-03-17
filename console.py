c54b66cdbbdf018d52dc7a4c15de434875f48d95
haoningng <nick.nghn@gmail.com>
2024-03-17 14:45:46 +1100

    fix: give storage.all() arguments

--- console.py
+++ console.py
@@ -224,7 +224,7 @@ class HBNBCommand(cmd.Cmd):
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage.all().items():
+            for k, v in storage.all(HBNBCommand.classes[args]).items():
                 if k.split('.')[0] == args:
                     print_list.append(str(v))
         else:
