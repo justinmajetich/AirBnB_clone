bb6a62ae97b9bcb86e6ea0af6be5126e5aad1a3d
haoningng <nick.nghn@gmail.com>
2024-03-17 19:43:05 +1100

    feat: update place, user and city model for task 8

--- models/user.py
+++ models/user.py
@@ -2,17 +2,21 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 import os
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
+        places = relationship("Place", back_populates="user",
+                              cascade="all, delete")
+
     else:
         email = ''
         password = ''
