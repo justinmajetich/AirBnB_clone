bdc2454bba80f92e6a880936ae9ced574547dc3a
Juan C Lopez <juancalpz@yahoo.com>
2024-03-17 10:58:07 -0400

    Update - Updated file_storage new delete method and updated all method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,13 +8,21 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+        else:
+            filtered_objects = {}
+            for key, value in FileStorage.__objects.items():
+                if isinstance(value, cls):
+                    filtered_objects[key] =  value
+            return filtered_objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """Saves storage dictionary to file"""
@@ -45,6 +53,15 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
+                    class_name = val['__class__']
+                    obj = classes[class_name](**val)
+                    FileStorage.__objects[key] = obj
         except FileNotFoundError:
             pass
+
+def delete(self, obj=None):
+    """Deletes obj from __objects"""
+    if obj is not None:
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        if key in FileStorage.__objects:
+            del FileStorage.__objects[key]
