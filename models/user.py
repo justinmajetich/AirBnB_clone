0adecbafa9e8fbda9ae7757bb4473dc8e2954a9c
haoningng <nick.nghn@gmail.com>
2024-03-17 16:56:04 +1100

    fix: first_name and last_name change to nullable

--- models/user.py
+++ models/user.py
@@ -11,8 +11,8 @@ class User(BaseModel, Base):
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
     else:
         email = ''
         password = ''
