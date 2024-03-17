9caf5c2220cac37310962a68fe9b5298b2571001
haoningng <nick.nghn@gmail.com>
2024-03-17 14:41:18 +1100

    fix: change _FileStorage__objects to storage.all() in console.py

--- console.py
+++ console.py
@@ -224,11 +224,11 @@ class HBNBCommand(cmd.Cmd):
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.all().items():
                 if k.split('.')[0] == args:
                     print_list.append(str(v))
         else:
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.all().items():
                 print_list.append(str(v))
 
         print(print_list)
