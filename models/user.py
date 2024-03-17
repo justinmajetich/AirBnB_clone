8c5f9d3809abd127790f86a014abdac22b4f3743
haoningng <nick.nghn@gmail.com>
2024-03-17 16:29:14 +1100

    fix: inherit from Base

--- models/user.py
+++ models/user.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 import os
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
