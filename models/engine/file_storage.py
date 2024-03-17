afa14a49c18a418be67760272e8e8cbe3addc7a5
Juan C Lopez <juancalpz@yahoo.com>
2024-03-17 11:12:36 -0400

    Fix - missing indentation

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -59,9 +59,9 @@ class FileStorage:
         except FileNotFoundError:
             pass
 
-def delete(self, obj=None):
-    """Deletes obj from __objects"""
-    if obj is not None:
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        if key in FileStorage.__objects:
-            del FileStorage.__objects[key]
+    def delete(self, obj=None):
+        """Deletes obj from __objects"""
+        if obj is not None:
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            if key in FileStorage.__objects:
+                del FileStorage.__objects[key]
