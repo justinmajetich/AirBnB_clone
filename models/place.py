685f1f91ea08e7a2ef276ea72c2fee672f326e56
Sandy Obare <obaresandy@gmail.com>
2024-03-18 05:59:19 +0000

    place.py commit public class variables initialization using kwargs

--- models/place.py
+++ models/place.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
+import uuid
+from . import storage
+from datetime import datetime
 from models.base_model import BaseModel
 
 
@@ -16,3 +19,19 @@ class Place(BaseModel):
     latitude = 0.0
     longitude = 0.0
     amenity_ids = []
+
+    def __init__(self, *args, **kwargs):
+        self.id = kwargs.get('id', str(uuid.uuid4()))
+        Place.city_id = kwargs.get('city_id', Place.city_id)
+        Place.user_id = kwargs.get('user_id', Place.user_id)
+        Place.name = kwargs.get('name', Place.name)
+        Place.description = kwargs.get('description', Place.description)
+        Place.number_rooms = kwargs.get('number_rooms', Place.number_rooms)
+        Place.number_bathrooms = kwargs.get('number_bathrooms', Place.number_bathrooms)
+        Place.max_guest = kwargs.get('max_guest', Place.max_guest)
+        Place.price_by_night = kwargs.get('price_by_night', Place.price_by_night)
+        Place.latitude = kwargs.get('latitude', Place.latitude)
+        Place.longitude = kwargs.get('longitude', Place.longitude)
+        Place.amenity_ids = kwargs.get('amenity_ids', Place.amenity_ids)
+        super().__init__(*args, **kwargs)
+        storage.new(self)
