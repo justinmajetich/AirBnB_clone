f4605a70efcabe0baed6537371a89477dba7973c
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-03-18 21:07:19 +0000

    Add relationship between User and Place models

--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 
 
@@ -12,3 +13,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
+    places = relationship("Place", backref="user", cascade="all, delete")
