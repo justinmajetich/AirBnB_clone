4b9891b1a8f83424ec63e0c1b64de4912c1a786f
Omar-Algassim <omarggg2@gamil.com>
2024-03-18 21:52:50 +0200

    amenitis and place update

--- models/place.py
+++ models/place.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float
+from sqlalchemy import Column, String, Integer, Float, Table, ForeignKey
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 from os import environ
@@ -20,8 +20,16 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float)
     longitude = Column(Float)
+                          
     if environ['HBNB_TYPE_STORAGE'] == 'db':
+        place_amenity = Table(
+            'place_amenity',
+            Base.metadata,
+            Column('palace__id', String(60), ForeignKey('places.id'), nullable=False),
+            Column('amenity_id', String(60), ForeignKey('amenities.id'), nullable=False)
+        )
         reviews = relationship("Review", cascade="all, delete", backref="place")
+        amenities = relationship('Amenity', secondary='place_amenity', viewonly=False)
     else:
         @property
         def reviews(self):
@@ -31,3 +39,13 @@ class Place(BaseModel, Base):
             return [review for review in review_objects.values()
                     if review.place_id == Place.id]
     amenity_ids = []
+        def amenities(Self):
+             """Return all the amenities associated with the place"""
+            amenity_object = storage.all(Amenity)
+            return [amenity for amenity in self.amenities
+                    if amenity in self.amenities]
+        @amenities.setter
+        def amenities(self, obj):
+            """Set all the amenities associated with the place"""
+            if type(obj) == Amenity:
+                amenities_ids.appedn(obj.id)
