c20dfa950bc2708f454616b9c973bca236398234
bellacocho123 <isabelsky59@gmail.com>
2024-03-18 12:27:52 -0700

    setup testing

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,6 +2,7 @@ i-- Prepares a Mysql server
 CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
 USE `hbnb_dev_db`;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+
 ALTER USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
