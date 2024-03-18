2114f9d0076f8661143786d670f0cfc1e4c36ecb
Juan C Lopez <juancalpz@yahoo.com>
2024-03-18 11:06:59 -0400

    Update - Updated module to intecat with dbstorage engine and added getter

--- models/state.py
+++ models/state.py
@@ -1,8 +1,30 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import os
+import models
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", cascade="all, delete",
+                              passive_deletes=True, backref="state")
+    else:
+        name = ""
+
+    @property
+    def cities(self):
+        """cities property getter"""
+        city_lst = []
+        for key, val in models.storage.all().items():
+            try:
+                if val.state_id == self.id:
+                    city_lst.append(val)
+            except AttributeError:
+                pass
+        return city_lst
