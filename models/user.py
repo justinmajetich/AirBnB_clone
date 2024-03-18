94a58dee3b05aac6f115cce62628952560d03e2f
Juan C Lopez <juancalpz@yahoo.com>
2024-03-18 12:13:24 -0400

    Update - Added db connection  and attributes

--- models/user.py
+++ models/user.py
@@ -3,18 +3,24 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import os
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+     email = Column(String(128), nullable=False)
+     password = Column(String(128), nullable=False)
+     first_name = Column(String(128), nullable=True)
+     last_name = Column(String(128), nullable=True)
 
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-
-    places = relationship("Place", cascade="all, delete",
-                          backref="city", passive_deletes=True)
-    reviews = relationship("Review", cascade="all, delete",
+     places = relationship("Place", cascade="all, delete",
+                           backref="city", passive_deletes=True)
+     reviews = relationship("Review", cascade="all, delete",
                            backref="user", passive_deletes=True)
+    else:
+       email = ""
+       password = ""
+       first_name = ""
+       last_name = ""
