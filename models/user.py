f7bdd9d3aae840bd58cac45cbd3370b5e8e6d4c0
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-03-18 20:43:16 +0000

    Refactor User model to use SQLAlchemy
    Co-authored-by: Deborah Rise <DeborahRise@users.noreply.github.com>

--- models/user.py
+++ models/user.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from models.base_model import BaseModel, Base
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
