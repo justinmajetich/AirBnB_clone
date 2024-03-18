49db11e338bd51cc675b805d7037ddd6363bf514
prodjohnper <JonathanPerez9743@gmail.com>
2024-03-17 20:12:27 -0400

    Update - Updated to implement state_id, name...

--- models/city.py
+++ models/city.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
+class City(BaseModel, Base):
+    """ The city class """
+    __tablename__ = 'cities'
+
     state_id = ""
     name = ""
+
+    places = relationship("Place", cascade="all, delete",
+                          backref="city", passive_deletes=True)
