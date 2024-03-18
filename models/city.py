50e3a4e723dd8c44d988535aa6426da292595bc0
Juan C Lopez <juancalpz@yahoo.com>
2024-03-18 10:51:46 -0400

    Update - Updated to include db storage attributes

--- models/city.py
+++ models/city.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+import os
 
 
 class City(BaseModel, Base):
     """ The city class """
     __tablename__ = 'cities'
-
-    state_id = ""
-    name = ""
-
-    places = relationship("Place", cascade="all, delete",
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        state_id = Column(String(60), ForeignKey('states.id'),nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", cascade="all, delete",
                           backref="city", passive_deletes=True)
+    else:
+        state_id = ""
+        name = ""
