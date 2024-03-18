1336dd6f2a66a7e2561595210684a864957b9dcd
Juan C Lopez <juancalpz@yahoo.com>
2024-03-18 11:58:12 -0400

    Update - Updated added conditional depending on the value of the enviromnment variable HBNB_TYPE_STORAGE

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,26 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+import os
 
-
-storage = FileStorage()
+classes = {"User": User,
+           "BaseModel": BaseModel,
+           "Place": Place,
+           "State": State,
+           "City": City,
+           "Amenity": Amenity,
+           "Review": Review
+           }
+if os.getenv('HBNB_TYPE+STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    storage = FileStorage()
 storage.reload()
