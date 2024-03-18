598e98fa4a099f73214905d656b98df461c3f0e6
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-03-18 18:49:29 +0000

    Refactor storage instantiation in __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
+
+from os import getenv
+from models.engine.db_storage import DBStorage
 from models.engine.file_storage import FileStorage
 
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    storage = DBStorage()
+else:
+    storage = FileStorage()
 
-storage = FileStorage()
 storage.reload()
