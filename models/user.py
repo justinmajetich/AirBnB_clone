9393462ecc6080f85fc2cf68a8fb75cdd5288e81
prodjohnper <JonathanPerez9743@gmail.com>
2024-03-17 20:07:38 -0400

    Update - Updated user class to implement email, password...

--- models/user.py
+++ models/user.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+""" User module for the HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -12,3 +13,8 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
+    places = relationship("Place", cascade="all, delete",
+                          backref="city", passive_deletes=True)
+    reviews = relationship("Review", cascade="all, delete",
+                           backref="user", passive_deletes=True)
