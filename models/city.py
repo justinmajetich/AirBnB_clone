95d4a9435f30781d52b13fda9499f74e407d5459
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-03-18 21:06:47 +0000

    Add relationship between City and Place models

--- models/city.py
+++ models/city.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 
 
@@ -10,3 +11,5 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
     name = Column(String(128), nullable=False)
+
+    places = relationship("Place", backref="cities", cascade="all, delete")
