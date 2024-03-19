12e71172d7a22156314fb27c39d53079ebfea4e4
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-19 13:06:57 +0300

    Deleting

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,7 +8,7 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         return FileStorage.__objects
 
@@ -22,7 +22,12 @@ class FileStorage:
             temp = {}
             temp.update(FileStorage.__objects)
             for key, val in temp.items():
-                temp[key] = val.to_dict()
+                if isinstance(val.created_at, str):
+                      temp[key] = val.created_at
+                elif isinstance(val.updated_at, str):
+                      temp[key] = val.updated_at
+                else:
+                    temp[key] = val.to_dict()
             json.dump(temp, f)
 
     def reload(self):
@@ -48,3 +53,17 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+    def delete(self, obj=None):
+            # from models.base_model import BaseModel
+            self.reload()
+            string = f"{obj.to_dict()['__class__']}.{obj.id}"
+            if string in self.__objects:
+                del self.__objects[string]
+            else:
+                pass
+            print("After delete")
+            for key in self.__objects:
+                print(key)
+            save()
+            for state_key in self.__objects:
+                print("IN FILESTORAGE ", self.__objects[state_key]) 
