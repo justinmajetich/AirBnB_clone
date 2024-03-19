1795dd682eb5c72721b923c4c636cfb807deacff
DennisMakaka <dennismakaka078@gmail.com>
2024-03-19 04:58:47 -0700

    An update of state model and its test file

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,19 +1,54 @@
-#!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+import unittest
 from models.state import State
+from models.city import City
+from models.base_model import BaseModel
+from os import environ
 
 
-class test_state(test_basemodel):
-    """ """
+class TestState(unittest.TestCase):
+    """Test cases for State class"""
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "State"
-        self.value = State
+    def setUp(self):
+        """Set up test environment"""
+        self.state = State()
 
-    def test_name3(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+    def tearDown(self):
+        """Tear down test environment"""
+        del self.state
+
+    def test_inheritance(self):
+        """Test inheritance"""
+        self.assertIsInstance(self.state, BaseModel)
+        self.assertIsInstance(self.state, State)
+
+    def test_attributes(self):
+        """Test instance attributes"""
+        self.assertTrue(hasattr(self.state, 'name'))
+        self.assertTrue(hasattr(self.state, 'cities'))
+
+    def test_name_type(self):
+        """Test name attribute type"""
+        self.assertIsInstance(self.state.name, str)
+
+    def test_cities_relationship_db_storage(self):
+        """Test cities relationship for DBStorage"""
+        if environ.get('HBNB_TYPE_STORAGE') == 'db':
+            city = City(state_id=self.state.id)
+            city.save()
+            self.assertIn(city, self.state.cities)
+            city.delete()
+        else:
+            self.skipTest("Test is only applicable for DBStorage")
+
+    def test_cities_getter_file_storage(self):
+        """Test cities getter for FileStorage"""
+        if environ.get('HBNB_TYPE_STORAGE') != 'db':
+            city = City(state_id=self.state.id)
+            city.save()
+            self.assertIn(city, self.state.cities)
+        else:
+            self.skipTest("Test is only applicable for FileStorage")
+
+
+if __name__ == '__main__':
+    unittest.main()
