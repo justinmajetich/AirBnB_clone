fcc5e421ff9c35edecd855b31a71ac43d0589d7c
Mohammed Ahmed <ma2286572@gmail.com>
2024-03-19 04:53:25 +0000

    Complete the engine creation

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
+from os import environ
 
 Base = declarative_base()
 
@@ -22,18 +23,14 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.now()
+        if kwargs is not None:
+            for k, v in kwargs.items():
+                if k in ['created_at', 'updated_at']:
+                    v = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
+                if k != '__class__':
+                    setattr(self, k, v)
 
     def __str__(self):
         """Returns a string representation of the instance"""
--- models/place.py
+++ models/place.py
@@ -20,18 +20,19 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float)
     longitude = Column(Float)
+    amenity_ids = []
                           
     if environ['HBNB_TYPE_STORAGE'] == 'db':
         place_amenity = Table(
             'place_amenity',
             Base.metadata,
-            Column('palace__id', String(60), ForeignKey('places.id'), primary_key=True,
+            Column('palace_id', String(60), ForeignKey('places.id'), primary_key=True,
                    nullable=False),
             Column('amenity_id', String(60), ForeignKey('amenities.id'),primary_key=True,
                    nullable=False)
         )
         reviews = relationship("Review", cascade="all, delete", backref="place")
-        amenities = relationship('Amenity', secondary='place_amenity', viewonly=False)
+        amenities = relationship('Amenity', secondary='place_amenity', viewonly=False, backref='place_amenities')
     else:
         @property
         def reviews(self):
@@ -40,14 +41,13 @@ class Place(BaseModel, Base):
             review_objects = storage.all(Review)
             return [review for review in review_objects.values()
                     if review.place_id == Place.id]
-        amenity_ids = []
         def amenities(Self):
              """Return all the amenities associated with the place"""
-             amenity_object = storage.all(Amenity)
-             return [amenity for amenity in amenity_ids
-                    if amenity in amenities]
+             amenity_objects = storage.all(Amenity)
+             return [amenity for amenity in amenity_objects
+                    if amenity.id in amenity_ids]
         @amenities.setter
         def amenities(self, obj):
             """Set all the amenities associated with the place"""
             if type(obj) == Amenity:
-                amenities_ids.appedn(obj.id)
+                amenities_ids.append(obj.id)
