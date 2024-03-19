bc6bb192f745597594366f7b2a0a19e8709622f8
CHIGODI <chiegody254@gmail.com>
2024-03-19 21:05:21 +0300

    db storage tests

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Module for testing db storage"""
+from console import HBNBCommand
+import unittest
+from models.state import State
+from models.city import City
+from models import storage
+from os import environ
+import MySQLdb
+
+
+@unittest.skipIf(environ.get('HBNB_TYPE_STORAGE') != 'db', "Db storage tests")
+class test_db_Storage(unittest.TestCase):
+    """Test class for dbstorage"""
+
+    def setUp(self):
+        """ Set up test environment """
+        self.conn = MySQLdb.connect(
+            user=environ.get('HBNB_MYSQL_USER'),
+            passwd=environ.get('HBNB_MYSQL_PWD'),
+            db=environ.get('HBNB_MYSQL_DB'),
+            host=environ.get('HBNB_MYSQL_HOST')
+            )
+        self.cursor = self.db_connection.cursor()
+
+    def tearDown(self):
+        """ Remove storage file at end of tests """
+        try:
+            self.cursor.close()
+            self.conn.close()
+        except:
+            pass
