788f175d9d3b518d96e219faca7e9346db652667
DeborahRise <debie.rise.bookings@gmail.com>
2024-03-19 09:50:51 +0000

    updated the amenity class

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from models.base_model import BaseModel, Base
+from models.place import Place
+import models
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """ Amenities present in the airbnb """
+
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = 
