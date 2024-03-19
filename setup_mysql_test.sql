80f319f672d5faab2b3ca0375062b9bfd4c9d36f
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-19 11:08:57 +0300

    Setup test db

---
+++ setup_mysql_test.sql
@@ -0,0 +1,12 @@
+-- Create database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create user if they don't exist
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant privileges to the user
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- Flush privileges
+FLUSH PRIVILEGES;
