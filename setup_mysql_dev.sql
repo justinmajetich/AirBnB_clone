210b1020cd4d257a33a8623884ada64bf1ee870c
LINUSOBURA <linusobura@gmail.com>
2024-03-19 13:16:31 +0300

    Updated console.py added tests in tests/console and set up mysql setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,9 @@
+-- script that prepares a MySQL server for the project:
+-- Creates a database
+-- Creates a user
+-- Sets password
+-- Sets permisions
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
