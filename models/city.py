a4c263bbd0d7d1371373d6c2117245f09e1f3a50
Juan C Lopez <juancalpz@yahoo.com>
2024-03-19 08:03:24 -0400

    Pycodestyle - Missing indentation and space after ','

--- models/city.py
+++ models/city.py
@@ -10,10 +10,10 @@ class City(BaseModel, Base):
     """ The city class """
     __tablename__ = 'cities'
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        state_id = Column(String(60), ForeignKey('states.id'),nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
         places = relationship("Place", cascade="all, delete",
-                          backref="city", passive_deletes=True)
+                              backref="city", passive_deletes=True)
     else:
         state_id = ""
         name = ""
