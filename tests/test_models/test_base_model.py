69df1fabb13347d3326c9c8dddc727a47bc319ea
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-19 07:44:51 +0300

    Add two tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -25,7 +25,7 @@ class test_basemodel(unittest.TestCase):
     def tearDown(self):
         try:
             os.remove('file.json')
-        except:
+        except Exception:
             pass
 
     def test_default(self):
@@ -69,11 +69,41 @@ class test_basemodel(unittest.TestCase):
         n = i.to_dict()
         self.assertEqual(i.to_dict(), n)
 
+    def test_kwargs_types(self):
+        """Test if types remain the same as attribute
+            types when using kwargs"""
+        # Define a BaseModel instance with known attribute types
+        base_model = BaseModel()
+        attribute_types_before = {key: type(getattr(base_model, key))
+                                  for key in base_model.__dict__.keys()}
+
+        # Create a BaseModel instance with kwargs
+        base_model = BaseModel(id='test_id', __class__='BaseModel',
+                               created_at='2024-01-01T00:00:00.000000',
+                               updated_at='2024-01-01T00:00:00.000000')
+        attribute_types_after = {key: type(getattr(base_model, key))
+                                 for key in base_model.__dict__.keys()}
+
+        # Compare attribute types before and after setting attributes
+        self.assertEqual(attribute_types_before, attribute_types_after)
+
+    def test_empty_kwargs(self):
+        """Test if kwargs is empty"""
+        base_model = BaseModel()
+        self.assertEqual(len(base_model.to_dict()), 4)
+
+    # def test_kwargs_with_none_value(self):
+    #     """Test if kwargs has a value of None for both key and value"""
+    #     base_model = BaseModel(id=None, created_at=None, updated_at=None)
+    #     expected_dict = {'id': None, 'created_at': None, 'updated_at': None}
+    #     self.assertEqual(base_model.__dict__, expected_dict)
+
     def test_kwargs_none(self):
         """ """
         n = {None: None}
         with self.assertRaises(TypeError):
             new = self.value(**n)
+
     @unittest.skip("Skipping test because dictionary is passed")
     def test_kwargs_one(self):
         """ """
