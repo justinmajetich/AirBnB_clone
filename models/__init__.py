0a20d5fc7d923a57c78f8f9fd76de6e42d49189c
Lusanco <lusanco@gmail.com>
2024-03-18 20:25:24 -0400

    Revert "test -  update init"
    This reverts commit 94429db63ea1c455c128b4c20686528f201d77cf.
    updating init breaks things

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
-from os import getenv
 
-storage = getenv("HBNB_TYPE_STORAGE")
 
-if storage == "db":
-    from models.engine.db_storage import DBStorage
-
-    storage = DBStorage()
-else:
-    storage = FileStorage()
-
+storage = FileStorage()
 storage.reload()
