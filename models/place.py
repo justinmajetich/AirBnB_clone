39f41f964d64d492ca1153a437d15ad9e5a88fe3
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-19 04:23:58 +0300

    Add creation using kwargs

--- models/place.py
+++ models/place.py
@@ -5,6 +5,7 @@ from models.base_model import BaseModel
 
 class Place(BaseModel):
     """ A place to stay """
+
     city_id = ""
     user_id = ""
     name = ""
@@ -16,3 +17,15 @@ class Place(BaseModel):
     latitude = 0.0
     longitude = 0.0
     amenity_ids = []
+
+    def __init__(self, **kwargs):
+        super().__init__()
+        for key, value in kwargs.items():
+            if hasattr(self, key) and key != '__class__':
+                attr_type = type(getattr(self, key))
+                # Convert value to the appropriate type
+                if attr_type == int:
+                    value = int(value)
+                elif attr_type == float:
+                    value = float(value)
+                setattr(self, key, value)
