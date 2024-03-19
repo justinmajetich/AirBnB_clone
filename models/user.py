1d4c74cd086d0ae0ade3b84fcd11abcd33417bcb
Juan C Lopez <juancalpz@yahoo.com>
2024-03-19 07:54:33 -0400

    Pycodestyle - Indentation

--- models/user.py
+++ models/user.py
@@ -10,17 +10,17 @@ class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-     email = Column(String(128), nullable=False)
-     password = Column(String(128), nullable=False)
-     first_name = Column(String(128), nullable=True)
-     last_name = Column(String(128), nullable=True)
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
 
-     places = relationship("Place", cascade="all, delete",
-                           backref="city", passive_deletes=True)
-     reviews = relationship("Review", cascade="all, delete",
-                           backref="user", passive_deletes=True)
+        places = relationship("Place", cascade="all, delete",
+                              backref="city", passive_deletes=True)
+        reviews = relationship("Review", cascade="all, delete",
+                               backref="user", passive_deletes=True)
     else:
-       email = ""
-       password = ""
-       first_name = ""
-       last_name = ""
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
