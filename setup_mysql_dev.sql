612b748d1e0848cd5b7e372878cfbc3602b1b0d5
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-19 10:59:21 +0300

    Grant privillages

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,6 +2,7 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 USE hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED  BY 'hbnb_dev';
-GRANT CREATE, ALTER, DROP, INSERT, UPDATE, DELETE, SELECT, REFERENCES, RELOAD on hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+GRANT RELOAD ON *.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+GRANT CREATE, ALTER, DROP, INSERT, UPDATE, DELETE, SELECT, REFERENCES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
 GRANT SELECT on performance_schema.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
 /* GRANT PRIVILEGE ON database.table TO 'hbnb_dev'@'host'; */
