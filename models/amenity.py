6650c72dbcfce5ebc87ba5a8c531d12a7e49b4b3
Lusanco <lusanco@gmail.com>
2024-03-20 14:28:26 -0400

    update/refactor - base model, amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+""""""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
+
 class Amenity(BaseModel, Base):
-    """Amenity class representing an amenity provided by a place."""
-    __tablename__ = 'amenities'
+    """"""
+
+    __tablename__ = "amenities"
 
     name = Column(String(128), nullable=False)
 
--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,5 @@
-
 #!/usr/bin/python3
-""" BACKUP VERSIONN"""
+""""""
 import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
@@ -8,30 +7,29 @@ from sqlalchemy import Column, String, DateTime
 
 Base = declarative_base()
 
+
 class BaseModel:
     """A base class for all hbnb models"""
 
+    # fmt: off
     id = Column(
         String(60),
         primary_key=True,
-        nullable=False
-    )
+        nullable=False)
 
     created_at = Column(
         DateTime,
         nullable=False,
-        default=datetime.utcnow
-    )
+        default=datetime.utcnow)
 
     updated_at = Column(
         DateTime,
         nullable=False,
         default=datetime.utcnow,
-        onupdate=datetime.utcnow
-    )
+        onupdate=datetime.utcnow)
+    # fmt: on
 
     def __init__(self, *args, **kwargs):
-        """Instantiates a new model"""
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
@@ -40,22 +38,30 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
             self.updated_at = datetime.utcnow()
-            if 'created_at' in kwargs:
-                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                          '%Y-%m-%dT%H:%M:%S.%f')
-            if 'updated_at' in kwargs:
-                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                          '%Y-%m-%dT%H:%M:%S.%f')
+            # fmt: off
+            if "created_at" in kwargs:
+                kwargs["created_at"] = datetime.strptime(
+                    kwargs["created_at"],
+                    "%Y-%m-%dT%H:%M:%S.%f")
+            if "updated_at" in kwargs:
+                kwargs["updated_at"] = datetime.strptime(
+                    kwargs["updated_at"],
+                    "%Y-%m-%dT%H:%M:%S.%f")
             self.__dict__.update(kwargs)
+            # fmt: on
 
     def __str__(self):
         """Returns a string representation of the instance"""
         cls_name = type(self).__name__
-        return '[{}] ({}) {}'.format(cls_name, self.id, self.__dict__)
+        # fmt: off
+        return "[{}] ({}) {}".format(
+            cls_name, self.id, self.__dict__)
+        # fmt: on
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
+
         self.updated_at = datetime.utcnow()
         storage.new(self)
         storage.save()
@@ -63,13 +69,14 @@ class BaseModel:
     def delete(self):
         """Delete instance from __objects from storage"""
         from models import storage
+
         storage.delete(self)
 
     def to_dict(self):
         """Convert instance into dict format"""
         dictionary = self.__dict__.copy()
-        dictionary['__class__'] = type(self).__name__
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        dictionary.pop('_sa_instance_state', None)
+        dictionary["__class__"] = type(self).__name__
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+        dictionary.pop("_sa_instance_state", None)
         return dictionary
