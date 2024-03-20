8f3eeef6055c7a239e61e22381e62454d32fdd3a
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-20 17:11:21 +0300

    Add to db_branch

--- models/user.py
+++ models/user.py
@@ -2,14 +2,13 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-
+from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-
     __tablename__ = 'users'
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
+    places = relationship("Place", backref='user', cascade='all, delete, delete-orphan')
