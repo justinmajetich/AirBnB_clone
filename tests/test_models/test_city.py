8f2262f5a9b5d8ad54aaa1deba9b434426f1c682
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-20 03:12:22 +0300

    Modify import from absolute to relative

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ """
-from tests.test_models.test_base_model import test_basemodel
+from .test_base_model import test_basemodel
 from models.city import City
 
 
@@ -16,9 +16,11 @@ class test_City(test_basemodel):
     def test_state_id(self):
         """ """
         new = self.value()
+        new.state_id = '0001'
         self.assertEqual(type(new.state_id), str)
 
     def test_name(self):
         """ """
         new = self.value()
+        new.name = 'City'
         self.assertEqual(type(new.name), str)
