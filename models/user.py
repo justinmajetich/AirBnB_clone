752ae946d4e0a44709254cd5cc3a25b82a8eaadc
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-20 20:32:17 +0300

    Add Places entity

--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -13,3 +14,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
+    places = relationship("Place", backref='user',
+                          cascade='all, delete, delete-orphan')
