7103f74ae474369b5d58cca4df1dc49a6e0d0727
DennisMakaka <dennismakaka078@gmail.com>
2024-03-20 14:44:20 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Defines the User class"""
+"""Defines the User class."""
 
+# Importing necessary modules
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
@@ -9,12 +10,25 @@ from models.place import Place
 from models.review import Review
 
 class User(BaseModel, Base):
-    """Represents a user in the system"""
+    """Represents a user in the system.
+    
+    Attributes:
+        email: The email address of the user.
+        password: The password for user login.
+        first_name: The first name of the user.
+        last_name: The last name of the user.
+    """
+    
+    # Database table name
     __tablename__ = "users"
+    
+    # User attributes
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+    
+    # Relationships
     places = relationship("Place", cascade='all, delete, delete-orphan',
                           backref="user")
     reviews = relationship("Review", cascade='all, delete, delete-orphan',
