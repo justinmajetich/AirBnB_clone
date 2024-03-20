45c25b03391b801ae23d4386224612246be6e2d4
Sandy Obare <obaresandy@gmail.com>
2024-03-20 06:53:48 +0000

    review.py commit Base class import from models.BaseModel

--- models/review.py
+++ models/review.py
@@ -8,6 +8,6 @@ class Review(BaseModel, Base):
     """ Review classto store review information """
     from sqlalchemy import String, Column, ForeignKey
     __tablename__ = 'reviews'
-    place_id = Column(String(60), ForeignKey('places.id'), nullable=ForeignKey)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     text = Column(String(1024), nullable=False)
