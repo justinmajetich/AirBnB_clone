5a9ca60d4778148cdd4ea612ab9a20bd2c944d27
Lusanco <lusanco@gmail.com>
2024-03-20 13:47:07 -0400

    update/refactor - new, all

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,7 +5,8 @@ import json
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
-    __file_path = 'file.json'
+
+    __file_path = "file.json"
     __objects = {}
 
     def all(self, cls=None):
@@ -13,16 +14,23 @@ class FileStorage:
         if cls is None:
             return FileStorage.__objects
         else:
-            return {key: obj for key, obj in FileStorage.__objects.items()\
-                    if isinstance(obj, cls)}
+            # fmt: off
+            return {key: my_object 
+                    for key, my_object
+                    in FileStorage.__objects.items()
+                    if isinstance(my_object, cls)}
+            # fmt: on
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        # fmt: off
+        self.all().update(
+            {obj.to_dict()["__class__"] + "." + obj.id: obj})
+        # fmt: on
 
     def save(self):
         """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
+        with open(FileStorage.__file_path, "w") as f:
             dict_for_json = {}
             dict_for_json.update(FileStorage.__objects)
             for key, obj in dict_for_json.items():
@@ -40,18 +48,18 @@ class FileStorage:
         from models.review import Review
 
         classes_dict = {
-            'BaseModel': BaseModel,
-            'User': User,
-            'Place': Place,
-            'State': State,
-            'City': City,
-            'Amenity': Amenity,
-            'Review': Review
-            }
+            "BaseModel": BaseModel,
+            "User": User,
+            "Place": Place,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Review": Review,
+        }
 
         try:
             dict_from_json = {}
-            with open(FileStorage.__file_path, 'r') as f:
+            with open(FileStorage.__file_path, "r") as f:
                 dict_from_json = json.load(f)
                 for key, obj_dictionary in dict_from_json.items():
                     class_name = obj_dictionary["__class__"]  # Get class name
