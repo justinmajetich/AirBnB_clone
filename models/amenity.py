bdcaff829b4ea57f9d65188236515afd312e574a
Sandy Obare <obaresandy@gmail.com>
2024-03-20 06:47:26 +0000

    DBStorage - Amenity Table column definitions Place - Amenity: Many-to-many relationship definition place.py many-to-many association table definititon place.py amenities class attribute representing place - amenity relationship place.py amenities getter attribute definititon place.py amenities setter attribute definititon

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""
+This module contains the Amenity class, which inherits
+from the BaseModel class 
+"""
+
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """
+    This class describes the Amenity model, a child of BaseClass on
+    which amenity objects are based
+    """
+
+    __tablename__ = 'amenities'
+
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity")
