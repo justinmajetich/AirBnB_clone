29aed5c5f2a87850c7f4881397c5fc421ac051de
Love <mzeelovegeneral@gmail.com>
2024-03-21 00:37:55 +0100

    feat(bash): script to set up servers for deployment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# script that sets up web servers for the deployment of web_static
+# script that sets up web servers for the deployment of AirBnB web_static
 sudo apt-get update
 sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
@@ -19,7 +19,6 @@ sudo echo "<html>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
-
 sudo chown -R ubuntu:ubuntu /data/
 
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
