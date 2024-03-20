46bf34634dedb09b503fc2c508efebccd23bd918
Lusanco <lusanco@gmail.com>
2024-03-20 11:01:39 -0400

    update - db engine

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Engine for db"""
+"""Engine for database"""
 
 
 from os import getenv
@@ -13,54 +13,65 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 
-class DBStorage():
+
+class DBStorage:
     __engine = None
     __session = None
 
     def __init__(self):
 
-        user = getenv("HBNB_MYSQL_USER")
-        psswd = getenv("HBNB_MYSQL_PWD")
-        host = getenv("HBNB_MYSQL_HOST")
-        db = getenv("HBNB_MYSQL_DB")
-        environ = getenv("HBNB_ENV")
-
-        engine = create_engine(f"mysql+mysqldb://{user}:{psswd}@{host}/{db}")
+        username = getenv("HBNB_MYSQL_USER") + ":"
+        password = getenv("HBNB_MYSQL_PWD") + "@"
+        hostname = getenv("HBNB_MYSQL_HOST") + "/"
+        database = getenv("HBNB_MYSQL_DB")
+        hbnb_env = getenv("HBNB_ENV")
+        dialdriv = "mysql+mysqldb://"
+        myengine = dialdriv + username + password
+        myengine = myengine + hostname + database
+        engine = create_engine(myengine)
         self.__engine = engine
 
-        if environ == "test":
+        if hbnb_env == "test":
             Base.metadata.dropall(self.__engine)
 
     def all(self, cls=None):
         """Returns dict of current database"""
         db_dict = {}
         classes = {
-               'BaseModel': BaseModel, 'User': User, 'Place': Place,
-               'State': State, 'City': City, 'Amenity': Amenity,
-               'Review': Review
-              }
+            "BaseModel": BaseModel,
+            "User": User,
+            "Place": Place,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Review": Review,
+        }
         if cls:
             for key in classes.keys():
                 if cls.__name__ == key:
-                    objects = (self.__session.query(classes[key]).all())
+                    objects = self.__session.query(classes[key]).all()
                     obj_class = key
                     break
             for obj in objects:
                 id = obj.id
-                obj_key = '{}.{}'.format(obj_class, id)
+                obj_key = "{}.{}".format(obj_class, id)
                 db_dict[obj_key] = obj
             return db_dict
-        all_objects = (self.__session.query
-                            (City, State, User, Place, Review, Amenity)
-                            .filter(City.state_id == State.id,
-                                    Place.user_id == User.id,
-                                    Place.city_id == City.id,
-                                    Review.place_id == Place.id,
-                                    Review.user_id == User.id).all())
+        all_objects = (
+            self.__session.query(City, State, User, Place, Review, Amenity)
+            .filter(
+                City.state_id == State.id,
+                Place.user_id == User.id,
+                Place.city_id == City.id,
+                Review.place_id == Place.id,
+                Review.user_id == User.id,
+            )
+            .all()
+        )
         for objs in all_objects:
             for obj in range(0, len(objs)):
                 id = objs[obj].id
-                obj_key = '{}.{}'.format(objs[obj].__class__, id)
+                obj_key = "{}.{}".format(objs[obj].__class__, id)
                 db_dict.update({obj_key: objs[obj]})
         return db_dict
 
@@ -86,8 +97,7 @@ class DBStorage():
     def reload(self):
         """Reload, create all tables"""
         Base.metadata.create_all(self.__engine)
-        session_factory = (sessionmaker(bind=self.__engine,
-                                        expire_on_commit=False))
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
 
