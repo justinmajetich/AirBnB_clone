446592b353287d1c1c39309d2967d299f942fc23
Prudence Wambui <wambuiprudy@gmail.com>
2024-03-20 07:31:07 +0300

    remove syntax error

--- console.py
+++ console.py
@@ -126,8 +126,8 @@ class HBNBCommand(cmd.Cmd):
                 arg_splited = arg.split("=")
                 arg_splited[1] = eval(arg_splited[1])
                 if type(arg_splited[1]) is str:
-                    arg_splited[1] = arg_splited[1].replace("_", " ")
-                    .replace('"', '\\"')
+                    arg_splited[1] = arg_splited[1].replace("_", " ")\
+                            .replace('"', '\\"')
                 kw[arg_splited[0]] = arg_splited[1]
         except SyntaxError:
             print("** class name missing **")
