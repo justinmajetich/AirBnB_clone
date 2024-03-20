b57c9ea19b432462f1d44a3bcf034d721104713b
Lusanco <lusanco@gmail.com>
2024-03-20 14:13:47 -0400

    test - init

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-"""BACKUP VERSIOON"""
-
-
 from models.engine.file_storage import FileStorage
-import os
+from os import getenv
 
-storage = os.getenv("HBNB_TYPE_STORAGE")
+storage = getenv("HBNB_TYPE_STORAGE")
 
 if storage == "db":
     from models.engine.db_storage import DBStorage
+
     storage = DBStorage()
 else:
     storage = FileStorage()
