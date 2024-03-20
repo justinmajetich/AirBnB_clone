9acad635497147e57ceb45919a276e8b27be901a
Etta-Diego <onyinyechiikediego.gmail.com>
2024-03-20 22:42:48 +0100

    added setup_mysql_test.sql file

---
+++ tests/test_models/test_engine/setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- Prepares a MySQL server for the project
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test_db'@'localhost' IDENTIFIED BY 'hbnb_test_db';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
