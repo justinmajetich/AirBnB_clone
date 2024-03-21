a6073d31c48be1e2c1c88436fd28cfeb6e78fd34
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-21 03:31:30 +0300

    Add reviews

--- models/place.py
+++ models/place.py
@@ -2,11 +2,16 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, Float, String, ForeignKey
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.review import Review
+from os import getenv
 
 
 class Place(BaseModel, Base):
     """ The Place class, contains infor about a BnB"""
     __tablename__ = 'places'
+
 
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
@@ -18,3 +23,18 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0, nullable=False)
     latitude = Column(Float, default=0)
     longitude = Column(Float, default=0)
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        reviews = relationship("Review", backref='place',
+                            cascade='all, delete, delete-orphan')
+    else:
+        @property
+        def reviews(self):
+            """Gets reviews from FileStorage"""
+            from models import storage
+
+            review_list = []
+            for review in list(storage.all(Review).values()):
+                if review.place_id == self.id:
+                    review_list.append(review)
+            return review_list
--- models/user.py
+++ models/user.py
@@ -16,3 +16,6 @@ class User(BaseModel, Base):
     last_name = Column(String(128), nullable=False)
     places = relationship("Place", backref='user',
                           cascade='all, delete, delete-orphan')
+    reviews = relationship("Review", backref='user',
+                           cascade='all, delete, delete-orphan')
+    
