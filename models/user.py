deaaef26622636c46aebea9b35197c3942b021df
Elizabeth <elizabeth.zabala3@gmail.com>
2024-03-21 12:29:43 -0400

    Task 7

--- models/user.py
+++ models/user.py
@@ -5,6 +5,9 @@ from sqlalchemy import Column, String
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
+
+    __tablename__ = 'users'
+
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
