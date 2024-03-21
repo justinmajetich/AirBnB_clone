c8f682a4cfd7944fff95a2234b4a67786c662bfc
Evanie <evaniewares@gmail.com>
2024-03-21 14:36:35 +0000

    Updated place.py

--- models/place.py
+++ models/place.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, Float, String, ForeignKey
 from sqlalchemy import Column, String, ForeignKey, Table
 from sqlalchemy.orm import relationship
+from models.amenity import Amenity
 
 # from models.review import Review
 from os import getenv
@@ -11,21 +12,21 @@ from os import getenv
 
 metadata = Base.metadata
 place_amenity = Table(
-    "user",
+    "place_amenity",
     metadata,
     Column(
         "place_id",
         String(60),
         ForeignKey("places.id"),
-        nullable=False,
         primary_key=True,
+        nullable=False
     ),
     Column(
         "amenity_id",
         String(60),
         ForeignKey("amenities.id"),
-        nullable=False,
         primary_key=True,
+        nullable=False
     ),
 )
 
@@ -35,20 +36,17 @@ class Place(BaseModel, Base):
 
     __tablename__ = "places"
 
-    city_id = Column(
-        String(60), ForeignKey("cities.id"), nullable=False
-    )
-    user_id = Column(
-        String(60), ForeignKey("users.id"), nullable=False
-    )
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024))
-    number_rooms = Column(Integer, default=0, nullable=False)
-    number_bathrooms = Column(Integer, default=0, nullable=False)
-    max_guest = Column(Integer, default=0, nullable=False)
-    price_by_night = Column(Integer, default=0, nullable=False)
-    latitude = Column(Float, default=0)
-    longitude = Column(Float, default=0)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float)
+    longitude = Column(Float)
+    amenity_ids = []
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
         reviews = relationship(
@@ -81,20 +79,17 @@ class Place(BaseModel, Base):
             """Getter attribute amenities that
             returns the list of Amenity instances"""
             from models import storage
-            from models.amenity import Amenity
 
-            return [
-                amenity
-                for amenity in storage.all(Amenity).values()
-                if amenity.place_id == self.id
-            ]
+            amenity_list = []
+            for amenity in list(storage.all(Amenity).values()):
+                if amenity.id in self.amenity_ids:
+                    amenity_list.append(amenity)
+            return amenity_list
 
         @amenities.setter
         def amenities(self, value):
             """Setter attribute amenities that handles
             append method for adding an Amenity.id"""
-            from models.amenity import Amenity
 
-            amenities = []
             if isinstance(value, Amenity):
-                amenities.append(value.id)
+                self.amenity_ids.append(value.id)
