d8cf360241a5b9fd818c24060ba874292a6e8719
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2024-03-21 17:57:12 -0400

    task 7

--- models/user.py
+++ models/user.py
@@ -1,34 +1,28 @@
 #!/usr/bin/python3
-"""
-This module defines a class User
-"""
-
-import models
-import sqlalchemy
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer, Float
+"""This is the user class"""
 from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
 
 
 class User(BaseModel, Base):
-    """
-    This class defines a user by various attributes
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
     """
     __tablename__ = "users"
-
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-
-    places = relationship('Place', backref='user', cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
-
-    def __init__(self, *args, **kwargs):
-        """
-        inherit from base and BaseModel init
-        """
-        super().__init__(*args, **kwargs)
-
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
 
