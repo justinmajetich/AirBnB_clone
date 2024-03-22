758954a64c23976514bba8f9a97d2f9b97cfe09b
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-03-22 12:13:58 -0400

    changing user to what it was

--- models/user.py
+++ models/user.py
@@ -2,18 +2,13 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = "users"
+
+    __tablename__ = 'users'
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-
-    def __init__(self, *args, **kwargs):
-        """Initialize User"""
-        super().__init__(*args, **kwargs)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
