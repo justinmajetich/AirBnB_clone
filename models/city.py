35de04bf8be97cf118148bd187d0de1387dcdf0b
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2024-03-22 15:23:11 -0400

    Task

--- models/city.py
+++ models/city.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
-
+from sqlalchemy import Column, String
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
