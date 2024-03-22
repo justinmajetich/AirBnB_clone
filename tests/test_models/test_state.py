e3eca0f9a99c12c88a38a04de5b4295d956aab02
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2024-03-22 13:08:11 -0400

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,35 +1,19 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-import models
-from models.base_model import BaseModel, Base
-from models.city import City
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
+""" """
+from tests.test_models.test_base_model import test_basemodel
+from models.state import State
 
 
-class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state")
+class test_state(test_basemodel):
+    """ """
 
     def __init__(self, *args, **kwargs):
-        """
-        init inherited
-        """
+        """ """
         super().__init__(*args, **kwargs)
+        self.name = "State"
+        self.value = State
 
-    if models.storage_type != "db":
-        @property
-        def cities(self):
-            """getter for cities that return
-            a list of city instance equale to
-            curent state id
-            """
-            list_city = []
-            all_inst_c = models.storage.all(City)
-            for value in all_inst_c.values():
-                if value.state_id == self.id:
-                    list_city.append(value)
-            return list_city
+    def test_name3(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.name), str)
