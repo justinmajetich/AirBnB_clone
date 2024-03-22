8a452cde0105de8806e3823c4ef90205c58d9a23
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2024-03-22 14:37:19 -0400

    Back to what it was

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,9 +14,6 @@ classes = {
     'State': State, 'City': City, 'Amenity': Amenity,
     'Review': Review
 }
-classes = {'BaseModel': BaseModel, 'User': User, 'Place': Place,
-    'State': State, 'City': City, 'Amenity': Amenity,
-    'Review': Review}
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
@@ -29,7 +26,6 @@ class FileStorage:
         if cls:
             for key, value in FileStorage.__objects.items():
                 if isinstance(value, cls):
-                if value.__class__ == cls:
                     new_dict[key] = value
             return new_dict
         return FileStorage.__objects
@@ -45,24 +41,11 @@ class FileStorage:
         for key, val in FileStorage.__objects.items():
             temp[key] = val.to_dict()
         with open(FileStorage.__file_path, 'w') as f:
-        """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
-
-    def save(self):
-        """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
             json.dump(temp, f)
 
     def reload(self):
         """Loads storage dictionary from file"""
         try:
-
-        try:
-            temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
@@ -76,7 +59,3 @@ class FileStorage:
             key = '{}.{}'.format(type(obj).__name__, obj.id)
             del FileStorage.__objects[key]
 
-        '''delete obj from __objects'''
-        if obj:
-            key = '{}.{}'.format(type(obj).__name__, obj.id)
-            del FileStorage.__objects[key]
