cd75d756796d5d229501ddd54e578f9ae52589a9
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2024-03-22 13:12:36 -0400

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,29 +1,9 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-import models
-from models.base_model import BaseModel, Base
-from models.place import Place
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
 
 
-class City(BaseModel, Base):
+class City(BaseModel):
     """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", backref="cities")
-
-    if models.storage_type != "db":
-        @property
-        def places(self):
-            """getter for places that return
-            a list of place instance equale to
-            curent city id
-            """
-            list_place = []
-            all_inst_p = models.storage.all(Place)
-            for value in all_inst_p.values():
-                if value.city_id == self.id:
-                    list_place.append(value)
-            return list_place
+    state_id = ""
+    name = ""
