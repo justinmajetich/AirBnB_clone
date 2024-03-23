8128df876fcc44441e872e613eba3238ae07c4f3
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-03-23 22:00:46 +0400

    Forget to sort state list

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,6 +12,7 @@ app = Flask(__name__)
 def states_list():
     """Display a HTML page with all State objects present in DBStorage"""
     states = storage.all(State)
+    states = sorted(states.values(), key=lambda state: state.name)
     return render_template("7-states_list.html", states=states)
 
 
