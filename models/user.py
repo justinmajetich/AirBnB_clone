7ccb0118a8e7143af72cc88252528779a7e24ce0
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-03-23 17:29:15 -0400

    task 7 fixing error

--- models/user.py
+++ models/user.py
@@ -8,7 +8,7 @@ class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
 
-    email = Column('email', String(128), nullable=False)
-    password = Column('password', String(128), nullable=False)
-    first_name = Column('first_name', String(128), nullable=False)
-    last_name = Column('last_name', String(128), nullable=False)
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
