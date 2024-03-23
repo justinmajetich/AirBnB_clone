e9e37461efb289f9e0ebeae657b494ef6feffa27
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-03-23 22:48:06 +0400

    Add routes from prev. tasks

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,6 +8,51 @@ from models.state import State
 app = Flask(__name__)
 
 
+@app.route("/", strict_slashes=False)
+def hello():
+    """Simple route to return a string"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Simple route to return a string"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """C Followed by the variable value"""
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text):
+    """Python route with default text variable value"""
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_n(n):
+    """Displays the number if n is an integer"""
+    return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """Display a HTML page only if n is an interger"""
+    return render_template("5-number.html", n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even(n):
+    """Display a HTML only if n is an integer and it's type even|odd"""
+    return render_template("6-number_odd_or_even.html", n=n)
+
+
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """Display a HTML page with all State objects present in DBStorage"""
