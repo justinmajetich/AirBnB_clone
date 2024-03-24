5556b5cbd74ae42d3d3b4d5ebbf943c083d9ccb7
Tashydaije <mbuguanatasha01@gmail.com>
2024-03-24 13:40:45 -0700

    add close() func

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -83,3 +83,7 @@ class DBStorage:
                                        expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+    def close(self):
+        """Close"""
+        self.__session.close()
