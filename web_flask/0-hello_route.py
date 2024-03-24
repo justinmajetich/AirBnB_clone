1e9ccabe467111cac5de85283e2475bdc0d92c1e
Tashydaije <mbuguanatasha01@gmail.com>
2024-03-24 01:47:18 -0700

    resolve conflict

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """route that displays 'Hello HBNB!'"""
+    """Route that displays 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
