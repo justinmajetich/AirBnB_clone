671c8fa87dc711db455031e842e6f03cbe845b2a
Tashydaije <mbuguanatasha01@gmail.com>
2024-03-24 05:30:57 -0700

    add: /number/<n>: display “n is a number” only if n is an integer

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+from flask import Flask, abort
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Route that displays 'Hello HBNB!'"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Route that displays 'HBNB'"""
+    return 'HBNB'
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Display C followed by the value of the text variable"""
+    text = str(text).replace('_', ' ')
+    return "C {}".format(escape(text))
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
+def python(text='is cool'):
+    """Display “Python ”, followed by the value of the text variable"""
+    text = str(text).replace('_', ' ')
+    return 'Python {}'.format(escape(text))
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def number(n):
+    """Display “n is a number” only if n is an integer"""
+    if n.isdigit():
+        return '{} is a number'.format(n)
+    else:
+        abort(404)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
