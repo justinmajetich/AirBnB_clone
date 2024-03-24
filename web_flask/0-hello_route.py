0dfe1f2686aac7643ea070195b9f4b6365fc12ab
Tashydaije <mbuguanatasha01@gmail.com>
2024-03-24 01:28:30 -0700

    task0: add script that starts flask app

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Route that displays 'Hello HBNB!'"""
+    """route that displays 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
 
-if __name__ == '__main__':
+if __name__ = '__main__':
     app.run(host='0.0.0.0', port=5000)
