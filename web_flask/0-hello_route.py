c7d582c332394b8ea0b5c6d35f69dde0c7149d61
ClintonCMOs <orengecmo@gmail.com>
2024-03-24 11:38:57 +0300

    starts a Flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,10 +5,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes = False)
+
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Called a function with / route"""
     return 'Hello HBNB!'
 
+
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 5000)
+    app.run(host='0.0.0.0', port=5000)
