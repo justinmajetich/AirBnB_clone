11b27e0b2f52df8530b616012962fbad27e3f6e3
Elizabeth <elizabeth.zabala3@gmail.com>
2024-03-24 07:54:31 -0400

    working on task 10

--- models/amenity.py
+++ models/amenity.py
@@ -2,10 +2,13 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from models.place import place_amenity
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
     '''Amenity Class'''
-    __tablename__ = 'amenities'
+    __tablename__ = "amenities"
 
     name = Column(String(128), nullable=False)
+    place_ameniies = relationship("Place", secondary=place_amenity)
