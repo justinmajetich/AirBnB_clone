178a7eb96e0324f5e046a7f7651329981e74ef94
Elizabeth <elizabeth.zabala3@gmail.com>
2024-03-24 15:28:29 -0400

    pycodestyle models

--- models/base_model.py
+++ models/base_model.py
@@ -27,12 +27,12 @@ class BaseModel:
 
             if 'updated_at' in kwargs:
                 kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                          '%Y-%m-%dT%H:%M:%S.%f')
+                                                        '%Y-%m-%dT%H:%M:%S.%f')
             else:
                 self.updated_at = datetime.now()
             if 'created_at' in kwargs:
                 kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                          '%Y-%m-%dT%H:%M:%S.%f')
+                                                        '%Y-%m-%dT%H:%M:%S.%f')
             else:
                 self.created_at = datetime.now()
             if '__class__' in kwargs:
@@ -63,7 +63,7 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
-    
+
     def delete(self):
         """Deletes the current instance from the storage"""
         from models import storage
