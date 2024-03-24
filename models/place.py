321c9317201b3cb46047a992d1afda9ffcc166de
Elizabeth <elizabeth.zabala3@gmail.com>
2024-03-24 07:40:41 -0400

    working on task 10

--- models/place.py
+++ models/place.py
@@ -5,6 +5,9 @@ from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 from sqlalchemy.orm import relationship
 from models.review import Review
 from os import getenv
+from models import storage
+from models.review import Review
+from models.amenity import Amenity
 
 
 place_amenity = Table("place_amenity", Base.metadata,
@@ -29,14 +32,35 @@ class Place(BaseModel, Base):
     longitude = Column(Float, nullable=True)
     amenity_ids = []
 
-    reviews = relationship("Review", cascade="all, delete", backref="place")
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        reviews = relationship(
+            'Review', cascade='all, delete', backref='place')
+        amenities = relationship(
+            'Amenity', secondary=place_amenity, viewonly=False,
+            backref='place_amenities')
+    else:
 
-    @property
-    def reviews(self):
-        """Getter attribute that returns the list of Reviews"""
-        from models import storage
-        reviews_list = []
-        for review in storage.all(Review).values():
-            if review.place_id == self.id:
-                reviews_list.append(review)
-        return reviews_list
+        @property
+        def reviews(self):
+            """Getter attribute that returns the list of Reviews"""
+            reviews_list = []
+            new_dict = storage.all(Review)
+            for review in new_dict.values():
+                if review.place_id == self.id:
+                    reviews_list.append(review)
+            return reviews_list
+        
+        @property
+        def amenities(self):
+            '''
+            Returns the list of `Amenity` instances
+            '''
+            return self.amenity_ids
+
+        @amenities.setter
+        def amenities(self, obj=None):
+            '''
+            amenities
+            '''
+            if type(obj) is Amenity and obj.id not in self.amenity_ids:
+                self.amenity_ids.append(obj.id)
