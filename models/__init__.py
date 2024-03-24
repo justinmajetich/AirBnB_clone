16b6fe7f734205c401d674be00b0592a3ffa2746
William <titanbabrisa@gmail.com>
2024-03-24 01:04:12 -0400

    lot of changes... I have backup

--- models/__init__.py
+++ models/__init__.py
@@ -7,7 +7,6 @@ if os.getenv('HBNB_TYPE_STORAGE') == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
     storage.reload()
-
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
