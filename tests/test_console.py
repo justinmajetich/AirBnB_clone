f5fb0ceeaf5a39c792e1fe574f4beea9e2341e09
Elizabeth <elizabeth.zabala3@gmail.com>
2024-03-24 16:00:45 -0400

    pycodestyle tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -35,7 +35,7 @@ class TestHBNBCommand(unittest.TestCase):
             self.assert_stdout("(hbnb) \n(hbnb) \n", self.console.cmdloop)
 
     def test_create_state_city(self):
-        """Test create State name="California" + create City 
+        """Test create State name="California" + create City
         state_id="<new state ID>" name=San_Francisco"""
         with patch("sys.stdin", StringIO("create \
                                          State name=\"California\"\ncreate \
@@ -57,27 +57,35 @@ class TestHBNBCommand(unittest.TestCase):
     def test_create_state_city_user_place(self):
         """Test create State name="California" + create City \
             state_id="<new state ID>" name="San_Francisco_is_super_cool" \
-                + create User email="my@me.com" password="pwd" first_name="FN" \
-                    last_name="LN" + create Place city_id="<new city ID>" user_id="<new user ID>" \
-                        name="My_house" description="no_description_yet" number_rooms=4 \
-                            number_bathrooms=1 max_guest=3 price_by_night=100 latitude=120.12\
-                                  longitude=101.4 + show Place <new place ID>"""
+                + create User email="my@me.com" password="pwd" \
+                    first_name="FN" \
+                    last_name="LN" + create Place \
+                        city_id="<new city ID>" user_id="<new user ID>" \
+                        name="My_house" description="no_description_yet"\
+                              number_rooms=4 \
+                            number_bathrooms=1 max_guest=3 \
+                                price_by_night=100 latitude=120.12\
+                                  longitude=101.4 + show Place \
+                                    <new place ID>"""
         commands = [
             "create State name=\"California\"\n",
             "create City state_id=\"<new state ID>\" \
                 name=\"San_Francisco_is_super_cool\"\n",
-            "create User email=\"my@me.com\" password=\"pwd\" first_name=\"FN\" \
+            "create User email=\"my@me.com\" \
+                password=\"pwd\" first_name=\"FN\" \
                 last_name=\"LN\"\n",
             "create Place city_id=\"<new city ID>\" user_id=\"<new user ID>\" \
             name=\"My_house\" description=\"no_description_yet\" \
-                number_rooms=4 number_bathrooms=1 max_guest=3 price_by_night=100 \
+                number_rooms=4 number_bathrooms=1 max_guest=3 \
+                    price_by_night=100 \
                     latitude=120.12 longitude=101.4\n",
             "show Place <new place ID>\n",
             "quit\n"
         ]
         with patch("sys.stdin", StringIO("".join(commands))):
             self.assert_stdout(
-                "(hbnb) \n(hbnb) \n(hbnb) \n(hbnb) \n(hbnb) \n(hbnb) \n", self.console.cmdloop)
+                "(hbnb) \n(hbnb) \n(hbnb) \n(hbnb) \n(hbnb) \
+                    \n(hbnb) \n", self.console.cmdloop)
 
 
 if __name__ == "__main__":
