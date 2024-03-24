8a6cfeb62debb0d1d4210f7c37f7ef2f51c297c1
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-03-24 05:40:52 +0400

    11. HBNB filters

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,131 @@
+#!/usr/bin/python3
+"""Simple Flask Module instance"""
+from flask import Flask, render_template
+
+from models import storage
+from models.city import City
+from models.state import State
+from models.amenity import Amenity
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Simple route to return a string"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Simple route to return a string"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """C Followed by the variable value"""
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text):
+    """Python route with default text variable value"""
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_n(n):
+    """Displays the number if n is an integer"""
+    return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """Display a HTML page only if n is an interger"""
+    return render_template("5-number.html", n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even(n):
+    """Display a HTML only if n is an integer and it's type even|odd"""
+    return render_template("6-number_odd_or_even.html", n=n)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """Display a HTML page with all State objects present in DBStorage"""
+    states = storage.all(State)
+    states = sorted(states.values(), key=lambda state: state.name)
+    return render_template("7-states_list.html", states=states)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """Display a HTML page with all State and City present in DBStorage"""
+    states = storage.all(State)
+    states = sorted(states.values(), key=lambda state: state.name)
+    cities = storage.all(City)
+    return render_template(
+            "8-cities_by_states.html",
+            states=states,
+            cities=cities
+            )
+
+
+@app.route("/states", strict_slashes=False)
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id=None):
+    """Display a HTML page with a State and its Cities present in DBStorage"""
+    states = storage.all(State)
+    if id is None:
+        # Sort the states by name and render the template
+        states = sorted(states.values(), key=lambda state: state.name)
+        return render_template("9-states.html", states=states, id=id)
+    else:
+        # Find the state with the given id
+        state = None
+        for state_obj in states.values():
+            if state_obj.id == id:
+                state = state_obj
+                break
+        if state:
+            cities = sorted(state.cities, key=lambda city: city.name)
+        else:
+            cities = None
+        return render_template(
+                "9-states.html",
+                state=state,
+                cities=cities,
+                id=id
+                )
+
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filters():
+    """Display a HTML page with all State, City, Amenity objects in Storage"""
+    states = storage.all(State)
+    states = sorted(states.values(), key=lambda state: state.name)
+    cities = storage.all(City)
+    cities = sorted(cities.values(), key=lambda city: city.name)
+    amenities = storage.all(Amenity)
+    amenities = sorted(amenities.values(), key=lambda amenity: amenity.name)
+    return render_template(
+            "10-hbnb_filters.html",
+            states=states,
+            cities=cities,
+            amenities=amenities
+            )
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
