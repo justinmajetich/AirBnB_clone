a6d106f0f368be3e4324c22e1d75563ff088dc40
William <titanbabrisa@gmail.com>
2024-03-25 07:37:57 -0400

    .-.

--- models/review.py
+++ models/review.py
@@ -2,15 +2,23 @@
 """ Review module for the HBNB project """
 from sqlalchemy import Column, String, ForeignKey
 from models.base_model import BaseModel, Base
+from os import getenv
 
+
+storage_type = getenv('HBNB_TYPE_STORAGE')
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
-    __tablename__ = "reviews"
+    if storage_type == 'db':
+        __tablename__ = "reviews"
 
-    text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    else:
+        place_id = ''
+        user_id = ''
+        text = ''
 
     def __init__(self, *args, **kwargs):
         """This method runs as soon as a instance is craeted"""
