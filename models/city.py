2e7caec396887051dd12c354177d96bded0e3067
William <titanbabrisa@gmail.com>
2024-03-25 07:29:19 -0400

    .-.

--- models/city.py
+++ models/city.py
@@ -3,12 +3,20 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from os import getenv
 
+
+storage_type = getenv('HBNB_TYPE_STORAGE')
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    name = Column(String(128), nullable=False)
 
-    places = relationship("Place", cascade="all, delete", backref="cities")
+    if storage_type == 'db':
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+
+        places = relationship("Place", cascade="all, delete", backref="cities")
+    else:
+        state_id = ''
+        name = ''
