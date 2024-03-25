c84a08f7ec146fb0fb2316cb031a88875de9fc9a
William <titanbabrisa@gmail.com>
2024-03-25 09:05:27 -0400

    trying to set it back where I was stuck

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Table, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
+class Amenity(BaseModel, Base):
+    """ Amenity class to store amenity information """
+    __tablename__ = "amenities"
 
-class Amenity(BaseModel):
-    name = ""
+    name = Column(
+            String(128), nullable=False
+            ) if getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   backref="amenities", viewonly=False)
+    def __init__(self, *args, **kwargs):
+        """This method happens as soon as a instance is created"""
+        super().__init__(*args, **kwargs)
