f5c0ee38f601c418a2c51b5bdbdb49fb1bc3c304
William <titanbabrisa@gmail.com>
2024-03-25 03:28:23 -0400

    rollback

--- models/amenity.py
+++ models/amenity.py
@@ -11,7 +11,8 @@ class Amenity(BaseModel, Base):
 
     name = Column(String(128), nullable=False)
 
-    place_amenities = relationship("Place", secondary=place_amenity)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   backref="amenities", viewonly=False)
     def __init__(self, *args, **kwargs):
         """This method happens as soon as a instance is created"""
         super().__init__(*args, **kwargs)
