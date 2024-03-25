283b9c6fc7701d49c2f2f7cebd1bacda2ab3248b
mburuxx <njihiamburu.3@gmail.com>
2024-03-26 02:43:04 +0300

    Update ip address

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ from fabric.api import env
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ["34.239.248.93", "54.172.40.197"]
 
 
 def do_deploy(archive_path):
