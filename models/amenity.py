e0701d40b9709992feebaa79cecf991132b95815
William <titanbabrisa@gmail.com>
2024-03-25 05:03:06 -0400

    probably gonna undo alot here

--- models/amenity.py
+++ models/amenity.py
@@ -3,15 +3,16 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
+
+
+storage_type = getenv('HBNB_TYPE_STORAGE')
 
 class Amenity(BaseModel, Base):
     """ Amenity class to store amenity information """
     __tablename__ = "amenities"
 
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity",
-                                    backref="amenities", viewonly=False)
+    name = Column(String(128), nullable=False) if storage_type == 'db' else ''
+#    place_amenities = relationship("Place", secondary="place_amenity",
+#                                    backref="amenities", viewonly=False)
 
-    def __init__(self, *args, **kwargs):
-        """ Initializes a new instance of Amenity """
-        super().__init__(*args, **kwargs)
