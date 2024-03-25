7ef1aa5032a57f51bcbc25fad59e0b0e22455936
Jeykop22 <nolanbinet2@gmail.com>
2024-03-25 12:07:46 +0000

    setup_mysql_dev.sql

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,6 @@
+-- Prepare mysql server for project
+-- Create Database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
