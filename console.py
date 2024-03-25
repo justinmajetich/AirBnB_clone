5f63ac7e2dfbf26d35a3995bc43c2316ac162e10
Spacefox95 <nathan.raynal@gmail.com>
2024-03-25 14:38:44 +0100

    Creating new branch

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -118,10 +118,31 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        
+        #Get the first element and set it as class_name
+        arg_list = args.split()
+        c_name = arg_list[0]
+        items = arg_list[1:]
+        # check that class_name is known
+        if c_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
-            return
-        new_instance = HBNBCommand.classes[args]()
+            return        
+        # Create a dictionnary for kwargs
+        new_dict = []
+        # read the args
+        for parameters in items:
+            #check for parameters and split them if needed
+            key, value = parameters.split('=')
+            if value.startswith('"') and value.endswith('"'):
+                value = value[1:-1].replace('_', ' ')
+            elif '.' in value:
+                #Cast value as a float if needed (ie longitude/latitude)
+                value = float(value)
+            elif value.isdigit():
+                #Cast value as a number if needed (ie number_rooms...)
+                value = int(value)
+            new_dict.append(value)
+        new_instance = HBNBCommand.classes[c_name](*new_dict)
         storage.save()
         print(new_instance.id)
         storage.save()
@@ -212,8 +233,9 @@ class HBNBCommand(cmd.Cmd):
         else:
             for k, v in storage._FileStorage__objects.items():
                 print_list.append(str(v))
-
-        print(print_list)
+        
+        for item in print_list:
+            print(item)
 
     def help_all(self):
         """ Help information for the all command """
@@ -272,7 +294,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +302,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and not args[0] == ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
