ceea9dd5073c3169ff36cc26b1f7fe89a59677cd
William <titanbabrisa@gmail.com>
2024-03-25 03:58:17 -0400

    .-. weeeeeeee

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Table, ForeignKey
+from sqlalchemy import Column, String
+from models.place import place_amenity
 from sqlalchemy.orm import relationship
 
 class Amenity(BaseModel, Base):
@@ -9,9 +10,5 @@ class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
 
     name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
 
-    place_amenities = relationship("Place", secondary="place_amenity",
-                                   backref="amenities", viewonly=False)
-    def __init__(self, *args, **kwargs):
-        """This method happens as soon as a instance is created"""
-        super().__init__(*args, **kwargs)
--- models/place.py
+++ models/place.py
@@ -6,13 +6,16 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from models.review import Review
 from models.amenity import Amenity
+from os import getenv
 
 
 place_amenity = Table('place_amenity', Base.metadata,
                       Column('place_id', String(60),
-                              ForeignKey('places.id'), primary_key=True),
+                              ForeignKey('places.id'), primary_key=True,
+                              nullable=False),
                       Column('amenity_id', String(60),
-                              ForeignKey('amenities.id'), primary_key=True))
+                              ForeignKey('amenities.id'), primary_key=True,
+                              nullable=False))
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -20,48 +23,36 @@ class Place(BaseModel, Base):
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
+    description = Column(String(1024))
     number_rooms = Column(Integer, nullable=False, default=0)
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
+    latitude = Column(Float)
+    longitude = Column(Float)
     amenity_ids = []
 
-    def __init__(self, *args, **kwargs):
-        """This method happens as soon as a instance is created"""
-        if 'amenity_ids' not in kwargs:
-            kwargs['amenity_ids'] = []
-        super().__init__(*args, **kwargs)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        reviews = relationship("Review", cascade="all, delete", backref="place")
+        amenities = relationship("Amenity", secondary=place_amenity,
+                                backref='places', viewonly=False)
+    else:
+        @property
+        def reviews(self):
+            """Getter attribute that returns the list of Reviews"""
+            from models import storage
+            reviews_list = []
+            for review in storage.all(Review).values():
+                if review.place_id == self.id:
+                    reviews_list.append(review)
+            return reviews_list
 
-    reviews = relationship("Review", cascade="all, delete", backref="place")
-    amenities = relationship("Amenity", secondary=place_amenity,
-                             backref='places', viewonly=False)
+        @property
+        def amenities(self):
+            """getter for the linked amenities"""
+            return self.amenity_ids
 
-    @property
-    def reviews(self):
-        """Getter attribute that returns the list of Reviews"""
-        from models import storage
-        reviews_list = []
-        for review in storage.all(Review).values():
-            if review.place_id == self.id:
-                reviews_list.append(review)
-        return reviews_list
-
-    @property
-    def amenities(self):
-        """getter for the linked amenities"""
-        from models import storage
-        amenity_list = []
-        for amenity_id in self.amenity_ids:
-            amenity_key = f"Amenity.{amenity_id}"
-            if amenity_key in storage.all(Amenity):
-                amenity_list.append(storage.all(Amenity)[amenity_key])
-        return amenity_list
-
-    @amenities.setter
-    def amenities(self, value):
-        if isinstance(value, Amenity):
-            if value.id not in self.amenity_ids:
+        @amenities.setter
+        def amenities(self, value=None):
+            if type(value) is Amenity and value.id not in self.amenity_ids:
                 self.amenity_ids.append(value.id)
