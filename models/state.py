d54423fd5efbd0e36c9d3184d10e806a5c8ce210
Julieed-971 <julie.dedieu1@gmail.com>
2024-03-26 16:27:11 +0100

    Updated city and state

--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from os import getenv
+from models.city import City
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    
+    if getenv('HBNB_TYPE_STORAGE') =='db':
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", back_populates="state")
+    
+    else:
+        name = ""
+    
+        @property
+        def get_cities(self):
+            from models import storage
+            """Return the list of City instances"""
+            cities_list = [items for items in storage.all(City) if City.state_id == self.id]
+            return cities_list
