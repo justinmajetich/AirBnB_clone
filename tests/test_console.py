c02d0e9ca23e4437eb78c635d4d21696253ea8e0
William <titanbabrisa@gmail.com>
2024-03-25 21:31:02 -0400

    commit

--- tests/test_console.py
+++ tests/test_console.py
@@ -44,6 +44,7 @@ class TestHBNBCommand(unittest.TestCase):
             self.assert_stdout("(hbnb) \n(hbnb) \
                                \n(hbnb) \n", self.console.cmdloop)
 
+    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") == "db", "No apply for db")
     def test_create_state_multiple_cities(self):
         """Test create State name="California" + \
             create City state_id="<new state ID>" name=Fremont"""
