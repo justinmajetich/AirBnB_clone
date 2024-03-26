5bb5e13045e4eaab6643dc700eb1e82580365929
Julieed-971 <julie.dedieu1@gmail.com>
2024-03-26 16:29:50 +0100

    FIxed pycodestyle

--- models/city.py
+++ models/city.py
@@ -8,14 +8,12 @@ from sqlalchemy import Column, String, ForeignKey
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    if getenv('HBNB_TYPE_STORAGE') =='db':
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'cities'
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         state = relationship("State", back_populates="cities")
-        
+
     else:
         state_id = ""
         name = ""
-    
- 
--- models/state.py
+++ models/state.py
@@ -9,18 +9,19 @@ from sqlalchemy import Column, String
 
 class State(BaseModel, Base):
     """ State class """
-    
-    if getenv('HBNB_TYPE_STORAGE') =='db':
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
         cities = relationship("City", back_populates="state")
-    
+
     else:
         name = ""
-    
+
         @property
         def get_cities(self):
             from models import storage
             """Return the list of City instances"""
-            cities_list = [items for items in storage.all(City) if City.state_id == self.id]
+            cities_list = [items for items in
+                           storage.all(City) if City.state_id == self.id]
             return cities_list
