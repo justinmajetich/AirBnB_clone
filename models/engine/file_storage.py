118ba90a6443e8439436f9a834200530277e6454
ksyv <viais.kevin@hotmail.fr>
2024-03-26 12:51:48 +0100

    fix pycodestyle

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -17,6 +17,7 @@ class FileStorage:
                     n_Dic[key] = value
             return n_Dic
         return self.__objects
+
     def new(self, obj):
         """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
@@ -50,7 +51,7 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
