5b6e6748919d99b1423ef138ca734c80b029d187
tjennat <theo.jennat98@gmail.com>
2024-03-26 10:32:07 +0100

    FIXING PEP8

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] is '{' and pline[-1] is '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -114,7 +114,8 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, arg):
-    args = arg.split()
+        args = arg.split()
+
     if len(args) == 0:
         print("** class name missing **")
         return
@@ -198,7 +199,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del(storage.all()[key])
+            del (storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
@@ -330,6 +331,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
