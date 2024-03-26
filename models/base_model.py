a35bc8d1a3c307b190e89cc471214f400edde9c5
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-03-26 16:10:21 +0100

    Update

--- models/base_model.py
+++ models/base_model.py
@@ -34,6 +34,7 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
+        """Instantiates a new model"""
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -79,6 +80,7 @@ class BaseModel:
         from models import storage
         self.updated_at = datetime.now()
         storage.new(self)
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
@@ -92,6 +94,11 @@ class BaseModel:
         if '_sa_instance_state' in dictionary.keys():
              del dictionary['_sa_instance_state']
         return dictionary
+
+    def delete(self):
+        """Delete current instance from the storage"""
+        from models import storage
+        storage.delete(self)
 
     def delete(self):
         """Delete current instance from the storage"""
