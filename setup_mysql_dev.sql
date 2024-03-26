fd99fa335987112969c36abc2023f1dd156dabdb
Adolberton <7557@holbertonstudents.com>
2024-03-26 16:48:39 +0100

    fix syntax

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -6,9 +6,8 @@ USE hbnb_dev_db;
 
 -- Create the user 'hbnb_dev' with the password 'hbnb_dev_pwd' if it does not already exist
 -- and set the authentication method to 'mysql_native_password'
-CREATE USER IF NOT EXISTS 'hbnb_dev' @'localhost' IDENTIFIED
-WITH
-    mysql_native_password BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev' @'localhost' 
+IDENTIFIED WITH mysql_native_password BY 'hbnb_dev_pwd';
 
 -- Grant all privileges on the 'hbnb_dev_db' database to the 'hbnb_dev' user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev' @'localhost';
