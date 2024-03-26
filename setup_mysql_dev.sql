4a950571ca312c2f91188967c4cc134c1f1cc6da
leila7575 <leila.louajri@gmail.com>
2024-03-26 22:33:02 +0100

    adding file for MySQL server

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,6 @@
+-- creates database hbnb_dev_db if it doesn't already exists 
+-- creates the MySQL server user hbnb_dev with all privileges on hbnb_dev_db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
