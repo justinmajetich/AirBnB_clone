78b026decfefb9c0cebf41ba65ebc51cc7e7f489
ksyv <viais.kevin@hotmail.fr>
2024-03-26 16:43:44 +0100

    delete last change for console

--- console.py
+++ console.py
@@ -29,20 +29,6 @@ class HBNBCommand(cmd.Cmd):
              'max_guest': int, 'price_by_night': int,
              'latitude': float, 'longitude': float
             }
-    
-    def num_or_float(self, arg: str):
-        """
-        Method to convert str to int or float
-        """
-        try:
-            return int(arg)
-        except Exception:
-            pass
-
-        try:
-            return float(arg)
-        except Exception:
-            return arg
 
     def preloop(self):
         """Prints if isatty is false"""
@@ -87,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] == '{' and pline[-1] =='}'\
+                    if pline[0] is '{' and pline[-1] is'}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -132,23 +118,13 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-        args = args.split(' ')
-        className = args[0]
-        if className not in HBNBCommand.classes:
+        elif args not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        attributes = {}
-        for attr in args[1:]:
-            newDict = attr.split('=', 1)
-            attributes[newDict[0]] = newDict[1]
-        new_instance = HBNBCommand.classes[className]()
-        for key, value in attributes.items():
-            value = value.strip("\"'").replace("_", " ")
-            value = self.num_or_float(value)
-            setattr(new_instance, key, value)
-
+        new_instance = HBNBCommand.classes[args]()
+        storage.save()
         print(new_instance.id)
-        new_instance.save()
+        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
@@ -230,13 +206,12 @@ class HBNBCommand(cmd.Cmd):
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            objects = storage.all(args)
-            for obj in objects.values():
-                print_list.append(str(obj))
+            for k, v in storage._FileStorage__objects.items():
+                if k.split('.')[0] == args:
+                    print_list.append(str(v))
         else:
-            objects = storage.all()
-            for obj in objects.values():
-                print_list.append(str(obj))
+            for k, v in storage._FileStorage__objects.items():
+                print_list.append(str(v))
 
         print(print_list)
 
@@ -297,7 +272,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] == '\"':  # check for quoted arg
+            if args and args[0] is '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -305,10 +280,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] != ' ':
+            if not att_name and args[0] is not ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] == '\"':
+            if args[2] and args[2][0] is '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
