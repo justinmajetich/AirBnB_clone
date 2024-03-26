6b6ba300c17a7cc1750101de340e98f4262bc761
nicolas <nicolas.quoirez@hotmail.fr>
2024-03-26 15:59:31 +0100

    Update modul place, review, user to DB, task 9

--- models/place.py
+++ models/place.py
@@ -2,12 +2,17 @@
 """ This module defines the class PLace """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
+import os
+import models
+from models.review import Review
 
 
 class Place(BaseModel, Base):
     """Represents a Place for a MySQL database.
 
     Public class attributes (with sqlalchemy):
+
         __tablename__ (str): Name of MySQL table to store places.
 
         city_id (Columns: Str): Foreign key to 'cities.id'.
@@ -24,6 +29,8 @@ class Place(BaseModel, Base):
         longitude (Columns: Float): The place's longitude.
 
         amenity_ids (Columns: list): An id list of all linked amenities.
+
+        reviews (Relationship Place --> Review): Links place with reviews.
     """
     __tablename__ = "places"
 
@@ -31,13 +38,23 @@ class Place(BaseModel, Base):
     user_id = Column(String(60), ForeignKey("user.id"), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=False)
-
     number_rooms = Column(Integer, default=0, nullable=False)
     number_bathrooms = Column(Integer, default=0, nullable=False)
     max_guest = Column(Integer, default=0, nullable=False)
     price_by_night = Column(Integer, default=0, nullable=False)
-
     latitude = Column(Float, nullable=False)
     longitude = Column(Float, nullable=False)
-
     amenity_ids = []
+
+    reviews = relationship("Review", backref="place", cascade="delete")
+
+    if os.getenv("HBNB_TYPE_STORAGE", None) != "db":
+
+        @property
+        def reviews(self):
+            """Return a list of all linked Reviews."""
+            review_list = []
+            for review in list(models.storage.all(Review).values()):
+                if review.place_id == self.id:
+                    review_list.append(review)
+            return review_list
--- models/user.py
+++ models/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""This module defines the class User"""
-from models.base_model import Base, BaseModel
+""" This module defines the class User """
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
@@ -9,13 +9,16 @@ class User(BaseModel, Base):
     """Represents an user for a MySQL database.
 
     Public class attributes (with sqlalchemy):
+
         __tablename__ (str): Name of MySQL table to store users.
+
         email: (Columns: String): User's email address.
         password (Columns: String): User's password.
         first_name (Columns: String): User's first name.
         last_name (Columns: String): User's last name.
 
-        places (Relationship User --> Place): Linked User to places.
+        places (Relationship User --> Place): Links user with places.
+        reviews (Relationship User --> Review): Links user with reviews.
     """
     __tablename__ = "users"
 
@@ -25,3 +28,4 @@ class User(BaseModel, Base):
     last_name = Column(String(128))
 
     places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
