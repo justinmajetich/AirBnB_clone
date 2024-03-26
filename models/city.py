86b9a7db0ca78234b4a272c24e2b8fbbc9f27388
ksyv <viais.kevin@hotmail.fr>
2024-03-26 15:56:22 +0100

    add db storage and update _init__

--- models/city.py
+++ models/city.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.base_model import Base, BaseModel
@@ -9,4 +10,4 @@ class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullabke=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
