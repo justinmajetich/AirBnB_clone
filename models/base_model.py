0b752edf855e3e3c0768dbb5461af068cf93ba5e
Dloyr <d.loyer59870@gmail.com>
2024-03-26 11:18:43 +0100

    add verification if an updated_at or created_at exist

--- models/base_model.py
+++ models/base_model.py
@@ -14,12 +14,20 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
-        else:
+
+        if 'updated_at' in kwargs:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
+        else:
+            self.updated_at = datetime.now()
+
+        if 'created_at' in kwargs:
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+        else:
+            self.created_at = datetime.now()
+
+        if kwargs:
             self.__dict__.update(kwargs)
 
     def __str__(self):
