cfc10bd0ef65400b43507d2a06653dd33383983c
SpeedCash <tcravero@gmail.com>
2024-03-26 10:10:04 +0000

    city updated

--- models/city.py
+++ models/city.py
@@ -1,24 +1,34 @@
-#!/usr/bin/python
-""" holds class City"""
+#!/usr/bin/python3
+""" holds class State"""
 import models
 from models.base_model import BaseModel, Base
+from models.city import City
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-class City(BaseModel, Base):
-    """Representation of city """
+class State(BaseModel, Base):
+    """Representation of state """
     if models.storage_t == "db":
-        __tablename__ = 'cities'
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        places = relationship("Place", backref="cities")
+        cities = relationship("City", backref="state")
     else:
-        state_id = ""
         name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes city"""
+        """initializes state"""
         super().__init__(*args, **kwargs)
+
+    if models.storage_t != "db":
+        @property
+        def cities(self):
+            """getter for list of city instances related to the state"""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
