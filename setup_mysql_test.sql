c68ddb46b4aa8bdfbb69c01a76c7c93abb17a905
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-03-26 13:07:30 +0100

    Update

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,5 @@
--- This script that prepares a MySQL server for the project
+-- Description: This script is used to create a test database and user for the hbnb project.
+
 CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
 CREATE USER IF NOT EXISTS `hbnb_test`@`localhost` IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO `hbnb_test`@`localhost`;
