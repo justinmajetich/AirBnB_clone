28c0e36d1cac2713e6d7bd7edff49e93a1e515c1
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-03-26 13:26:01 +0100

    Update file sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,7 @@
 -- Description: This script is used to create a test database and user for the hbnb project.
 
-CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS `hbnb_test`@`localhost` IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO `hbnb_test`@`localhost`;
-GRANT SELECT `performance_schema`.* TO `hbnb_test`@`localhost`;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO `hbnb_test`@`localhost`;
+GRANT SELECT performance_schema.* TO `hbnb_test`@`localhost`;
 FLUSH PRIVILEGES;
