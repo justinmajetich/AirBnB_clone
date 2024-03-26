b91f255888f3a1cf7a39e8eda891e0d0c36fe4db
benjaminparentt <p.benjamin620@icloud.com>
2024-03-26 17:16:05 +0100

    add sql

---
+++ models/engine/db_storage.py
@@ -0,0 +1,41 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from models.base_model import Base
+from os import getenv
+
+class DBStorage:
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        user = getenv('HBNB_MYSQL_USER')
+        pwd = getenv('HBNB_MYSQL_PWD')
+        host = getenv('HBNB_MYSQL_HOST')
+        db = getenv('HBNB_MYSQL_DB')
+        self.__engine = create_engine(f'mysql+mysqldb://{user}:{pwd}@{host}/{db}', pool_pre_ping=True)
+        if getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """Queries all objects by class name."""
+        # Implementation depends on the project requirements
+
+    def new(self, obj):
+        """Adds the object to the current database session."""
+        self.__session.add(obj)
+
+    def save(self):
+        """Commits all changes of the current database session."""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """Deletes from the current database session obj if not None."""
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """Creates all tables in the database and the session."""
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
