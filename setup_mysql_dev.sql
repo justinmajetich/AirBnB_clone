a4b3ec41f8f182f08802ffab2cacfd81316962e5
Sebas-crypt <usefullseba17@gmail.com>
2024-03-26 16:16:24 -0400

    fixing minor mysqldev errors and adding mysqltest

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE IF NOT EXIST hbnb_dev_db;
-CREATE USER IF NOT EXIST hbnb_dev;
-SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd'
-GRANT PRIVILEGES ON *hbnb_dev_db* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+CREATE USER IF NOT EXIST 'hbnb_dev'@'localhost'
+IDENTIFIED BY 'hbnb_dev_pwd'
+GRANT ALL PRIVILEGES ON *hbnb_dev_db* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
 GRANT SELECT ON *performance_schema* TO 'hbnb_dec'@'localhost';
