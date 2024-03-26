d686b4763b45d5f557d5914a20bb22e8a81aaf51
nicolas <nicolas.quoirez@hotmail.fr>
2024-03-26 14:44:16 +0100

    Update User to task 7 & 8, update Place task 8 --> until City

--- models/place.py
+++ models/place.py
@@ -1,18 +1,43 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
-from models.base_model import BaseModel
+""" This module defines the class PLace """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 
 
-class Place(BaseModel):
-    """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+class Place(BaseModel, Base):
+    """Represents a Place for a MySQL database.
+
+    Public class attributes (with sqlalchemy):
+        __tablename__ (str): Name of MySQL table to store places.
+
+        city_id (Columns: Str): Foreign key to 'cities.id'.
+        user_id (Columns: Str): Foreign key to 'user.id'.
+        name (Columns: Str): Name of the place.
+        description (Columns: Str): Description of the place.
+
+        number_rooms (Columns: Integer): Number of room. Default 0.
+        number_bathrooms (Columns: Integer): Number of bathrooms. Default 0.
+        max_guest (Columns: Integer): Max guest. Default 0.
+        price_by_night (Columns: Integer): Price by night. Default 0.
+
+        latitude (Columns: Float): The place's latitude.
+        longitude (Columns: Float): The place's longitude.
+
+        amenity_ids (Columns: list): An id list of all linked amenities.
+    """
+    __tablename__ = "places"
+
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("user.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=False)
+
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+
+    latitude = Column(Float, nullable=False)
+    longitude = Column(Float, nullable=False)
+
     amenity_ids = []
--- models/user.py
+++ models/user.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+"""This module defines the class User"""
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """Represents an user for a MySQL database.
+
+    Public class attributes (with sqlalchemy):
+        __tablename__ (str): Name of MySQL table to store users.
+        email: (Columns: String): User's email address.
+        password (Columns: String): User's password.
+        first_name (Columns: String): User's first name.
+        last_name (Columns: String): User's last name.
+
+        places (Relationship User --> Place): Linked User to places.
+    """
+    __tablename__ = "users"
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+
+    places = relationship("Place", backref="user", cascade="delete")
