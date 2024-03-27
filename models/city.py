30037397188940a14ad45e60653fafcc59ebd477
Arweenn <arwendumont25@outlook.fr>
2024-03-27 20:44:31 +0100

    Task 8: DBStorage - Place

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel):
@@ -10,3 +11,5 @@ class City(BaseModel):
 
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
+
+    places = relationship("Place", backref="cities", cascade="delete")
--- models/user.py
+++ models/user.py
@@ -2,12 +2,17 @@
 """This module defines a class User"""
 from models.base_model import BaseModel
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
+
+    __tablename__ = "users"
+
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    __tablename__ = "users"
+
+    places = relationship("Place", backref="place", cascade="all, delete")
