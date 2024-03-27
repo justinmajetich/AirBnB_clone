7d202e883151ef68a89eb7bec3f11695b602cc49
leila7575 <leila.louajri@gmail.com>
2024-03-27 02:53:18 +0100

    adding setup_mysql_test file

---
+++ setup_mysql_test.sql
@@ -0,0 +1,6 @@
+-- creates database hbnb_test_db if it doesn't already exists 
+-- creates the MySQL server user hbnb_test with all privileges on hbnb_test_db
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
