1d6cbb5328037e6d94c7566831b9286541ae0c91
nicolas <nicolas.quoirez@hotmail.fr>
2024-03-27 11:58:58 +0100

    update city task 8

--- models/city.py
+++ models/city.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.base_model import Base, BaseModel
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+    """ Represents a city for a MySQL database.
+    
+    Public class atributes:
+        __tablename__ (str): Name of MySQL table to store cities.
+
+        name (Columns: Str): Name of the city.
+        state_id (Columns: Str): Foreign key to 'states.id'.
+    """
     __tablename__ = 'cities'
+
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
+    places = relationship("Place", backref="cities", cascade="delete")
