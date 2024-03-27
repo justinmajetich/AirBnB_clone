77772c15038ea23131625e1795f5c04496879fb4
Spacefox95 <nathan.raynal@gmail.com>
2024-03-27 09:29:10 +0100

    Update_mysql_files'

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,8 @@
+-- Create a new Mysql database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- Create a new user 'hbnb_dev'
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Grant all privileges for user 'hbnb_dev'
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- Grant select privilege for user 'hbnb_dev'
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
