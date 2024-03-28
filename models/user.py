4862803385a86b5418d82416d6ddb68ed242d162
Jeykop22 <nolanbinet2@gmail.com>
2024-03-28 11:53:28 +0000

    review.py

--- models/user.py
+++ models/user.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from sqlalchemy.orm import relationship
+""" This module defines the class User """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = 'users'
+    """Represents an user for a MySQL database.
+    """
+    __tablename__ = "users"
+
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    places = relationship(
-        "Place",
-        backref='user',
-        cascade="all, delete",
-        passive_deletes=True)
-    reviews = relationship(
-        "Review",
-        backref='user',
-        cascade="all, delete",
-        passive_deletes=True)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+
+    places = relationship("Place", backref="user",
+                          cascade="all, delete", passive_deletes=True)
+    reviews = relationship("Review", backref="user",
+                           cascade="all, delete", passive_deletes=True)
