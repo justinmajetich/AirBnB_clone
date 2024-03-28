76b0ff1d377b7e6a4712d1c7eff2c06093bda331
SpeedCash <tcravero@gmail.com>
2024-03-28 13:26:01 +0000

    the last commit

--- models/place.py
+++ models/place.py
@@ -1,65 +1,29 @@
+# Correct the shebang line at the top of your place.py file
 #!/usr/bin/python3
-"""Holds class Place"""
-
-from sqlalchemy.orm import relationship, backref
-from sqlalchemy.sql.schema import Table
-from sqlalchemy.sql.sqltypes import Float, Integer
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-import models
-from os import getenv
-from models.amenity import Amenity
-
-
-# Association table for the Many-To-Many relationship between Place and Amenity
-place_amenity = Table(
-    'place_amenity', Base.metadata,
-    Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
-    Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)
-)
-
+""" holds class Place """
+# Import statements as you have them, corrected
 
 class Place(BaseModel, Base):
-    """A place to stay"""
+    """ A place to stay """
     __tablename__ = 'places'
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024))
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float)
-    longitude = Column(Float)
-    amenity_ids = []
-
-    reviews = relationship(
-        'Review', backref='place',
-        cascade="all, delete, delete-orphan"
-    )
-    amenities = relationship(
-        "Amenity", secondary=place_amenity, viewonly=False,
-        backref="places"
-    )
-
-    @property
-    def reviews(self):
-        """Getter that returns the list of Review instances"""
-        new = []
-        for review in models.storage.all(Review).values():
-            if review.place_id == self.id:
-                new.append(review)
-        return new
-
+    # Other columns as you have them defined
+    
+    # amenities relationship defined for DBStorage
+    amenities = relationship("Amenity", secondary='place_amenity', viewonly=False, back_populates="places")
+    # Assuming Amenity model has 'places' back_populates defined for bidirectional relationship
+    
     if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
-        def amenities(self):
-            """FileStorage: Getter that returns the list of Amenity instances"""
-            return [models.storage.all(Amenity)[id] for id in self.amenity_ids]
+        def reviews(self):
+            """ FileStorage: Getter that returns the list of Review instances """
+            # Your FileStorage specific implementation
 
+        @property
+        def amenities(self):
+            """ FileStorage: Getter that returns the list of Amenity instances """
+            # FileStorage specific implementation
+        
         @amenities.setter
         def amenities(self, obj):
-            """FileStorage: Setter to add an Amenity.id to the amenity_ids list"""
-            if type(obj) == Amenity:
-                self.amenity_ids.append(obj.id)
+            """ FileStorage: Setter to add an Amenity.id to the amenity_ids list """
+            # FileStorage specific implementation
