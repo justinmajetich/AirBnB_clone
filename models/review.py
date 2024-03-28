54ee3c4eb671606a0ae1ac4aecea17a893843675
NidiMeliodas <nidigashan@gmail.com>
2024-03-28 14:37:04 +0100

    chmod

--- models/review.py
+++ models/review.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import ForeignKey
+from sqlalchemy import Float
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """ Review class to store review information """
+    __tablename__ = 'reviews'
+
+    text = Column(String(1024),nullable=False)
+    place_id = Column(String(60),ForeignKey('place.id'),nullable=False)
+    user_id = Column(String(60), ForeignKey('user.id'), nullable=False)
